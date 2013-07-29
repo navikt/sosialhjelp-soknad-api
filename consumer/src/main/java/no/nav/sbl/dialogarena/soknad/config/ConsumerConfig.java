3d6be57583c400dc44d6c7b3b9b1d068aa305975
Andreas Svendby <Andreas.Svendby@nav.no>
2013-07-29 13:58:55 +0000

    Checkstyle

--- consumer/src/main/java/no/nav/sbl/dialogarena/soknad/config/ConsumerConfig.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/soknad/config/ConsumerConfig.java
@@ -3,8 +3,6 @@ package no.nav.sbl.dialogarena.soknad.config;
 import no.nav.tjeneste.domene.brukerdialog.sendsoknad.v1.SendSoknadPortType;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 import org.apache.cxf.ws.addressing.WSAddressingFeature;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
