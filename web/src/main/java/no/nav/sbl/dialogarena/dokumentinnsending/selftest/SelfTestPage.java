1c9f7840622da6e32d74da409b7fe04410f8e9ab
Ketil S Velle <Ketil.S.Velle@nav.no>
2013-08-23 07:12:34 +0000

    Fikset en warning

--- web/src/main/java/no/nav/sbl/dialogarena/dokumentinnsending/selftest/SelfTestPage.java
+++ web/src/main/java/no/nav/sbl/dialogarena/dokumentinnsending/selftest/SelfTestPage.java
@@ -1,19 +1,21 @@
 package no.nav.sbl.dialogarena.dokumentinnsending.selftest;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
 import no.nav.modig.wicket.selftest.SelfTestBase;
 import no.nav.sbl.dialogarena.dokumentinnsending.config.InternalStsConfig;
 import no.nav.tjeneste.domene.brukerdialog.henvendelsesbehandling.v1.HenvendelsesBehandlingPortType;
-import no.nav.tjeneste.domene.brukerdialog.oppdaterehenvendelsesbehandling.v1.OppdatereHenvendelsesBehandlingPortType;
+
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.springframework.context.annotation.Import;
 
-import javax.inject.Inject;
-import java.util.List;
-
 @Import({InternalStsConfig.class})
 public class SelfTestPage extends SelfTestBase {
-    @Inject
-    private OppdatereHenvendelsesBehandlingPortType oppdatereHenvendelsesBehandlingPortType;
+    
+//	@Inject
+//    private OppdatereHenvendelsesBehandlingPortType oppdatereHenvendelsesBehandlingPortType;
 
     @Inject
     private HenvendelsesBehandlingPortType henvendelsesBehandlingPortType;
