f66e38a6a8efdac290d404583f219b9f8e970faa
Anders Hefre <Anders.Hefre@nav.no>
2013-08-28 16:16:28 +0000

    [pmd] Fjernet ubrukt variabel.

--- web/src/test/java/no/nav/sbl/dialogarena/dokumentinnsending/fixture/regresjon/SendSoknadDriver.java
+++ web/src/test/java/no/nav/sbl/dialogarena/dokumentinnsending/fixture/regresjon/SendSoknadDriver.java
@@ -1,27 +1,17 @@
 package no.nav.sbl.dialogarena.dokumentinnsending.fixture.regresjon;
 
-import javax.inject.Inject;
-
 import no.nav.modig.test.fitnesse.fixture.SpringAwareDoFixture;
-import no.nav.modig.wicket.test.FluentWicketTester;
-import no.nav.sbl.dialogarena.dokumentinnsending.WicketApplication;
 import no.nav.sbl.dialogarena.dokumentinnsending.config.FitNesseApplicationConfig;
-
 import org.springframework.test.context.ContextConfiguration;
 
 @ContextConfiguration(classes = FitNesseApplicationConfig.class)
 public class SendSoknadDriver extends SpringAwareDoFixture {
 
-	@Inject
-	private FluentWicketTester<WicketApplication> wicketTester;
-	
-	
-	public SendSoknadDriver() throws Exception {
-		super.setUp();
-	}
-	
-	public long startSoeknad() {
-		return 1L;
-	}
-	      
+    public SendSoknadDriver() throws Exception {
+        super.setUp();
+    }
+
+    public long startSoeknad() {
+        return 1L;
+    }
 }
