63c54527e08d0e4b215b3ef189ca63ec78131c8f
Ketil S Velle <Ketil.S.Velle@nav.no>
2013-08-28 07:14:55 +0000

    [POC] Laget enkel startside.

---
+++ web/src/main/java/no/nav/sbl/dialogarena/websoknad/pages/sendsoknad/startside/StartSoknadPage.java
@@ -0,0 +1,9 @@
+package no.nav.sbl.dialogarena.websoknad.pages.sendsoknad.startside;
+
+import no.nav.sbl.dialogarena.websoknad.pages.basepage.BasePage;
+
+public class StartSoknadPage extends BasePage {
+
+	public StartSoknadPage() {
+	}
+}
