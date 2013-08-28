ae15aa382f513953c090cd5a7c29253fa72d86f4
Anders Hefre <Anders.Hefre@nav.no>
2013-08-28 15:24:56 +0000

    [selftest] Lagt til selftest for sendsoknad.

--- web/src/main/java/no/nav/sbl/dialogarena/websoknad/selftest/SelfTestPage.java
+++ web/src/main/java/no/nav/sbl/dialogarena/websoknad/selftest/SelfTestPage.java
@@ -1,10 +1,30 @@
 package no.nav.sbl.dialogarena.websoknad.selftest;
 
-import org.apache.wicket.markup.html.WebPage;
+import no.nav.modig.wicket.selftest.SelfTestBase;
+import no.nav.sbl.dialogarena.dokumentinnsending.config.InternalStsConfig;
+import no.nav.tjeneste.domene.brukerdialog.henvendelsesbehandling.v1.HenvendelsesBehandlingPortType;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.springframework.context.annotation.Import;
 
-public class SelfTestPage extends WebPage {
+import javax.inject.Inject;
+import java.util.List;
 
-    public SelfTestPage() {
+@Import({InternalStsConfig.class})
+public class SelfTestPage extends SelfTestBase {
 
+    @Inject
+    private HenvendelsesBehandlingPortType henvendelsesBehandlingPortType;
+
+    public SelfTestPage(PageParameters params) {
+        super("sendsoknad", params);
+    }
+
+    @Override
+    protected void addToStatusList(List<AvhengighetStatus> statusList) {
+        new ServiceStatusHenter("HenvendelsesBehandling") {
+            public void ping() {
+                henvendelsesBehandlingPortType.ping();
+            }
+        }.addStatus(statusList);
     }
 }
