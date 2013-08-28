1f58d712f7988163e2315bfc726b67e7c52d2dfc
Ketil S Velle <Ketil.S.Velle@nav.no>
2013-08-28 08:26:10 +0000

    [POC] Lagt til enkelt barnebidrag-søknad og avbryt-søknad funksjonalitet.

--- web/src/main/java/no/nav/sbl/dialogarena/websoknad/pages/sendsoknad/SendSoknadPage.java
+++ web/src/main/java/no/nav/sbl/dialogarena/websoknad/pages/sendsoknad/SendSoknadPage.java
@@ -1,7 +1,5 @@
 package no.nav.sbl.dialogarena.websoknad.pages.sendsoknad;
 
-import org.apache.wicket.devutils.stateless.StatelessComponent;
-
 import no.nav.sbl.dialogarena.websoknad.pages.basepage.BasePage;
 
 public class SendSoknadPage extends BasePage {
--- web/src/main/java/no/nav/sbl/dialogarena/websoknad/pages/sendsoknad/SendSoknadServicePage.java
+++ web/src/main/java/no/nav/sbl/dialogarena/websoknad/pages/sendsoknad/SendSoknadServicePage.java
@@ -12,55 +12,69 @@ import no.nav.sbl.dialogarena.websoknad.domain.Faktum;
 import no.nav.sbl.dialogarena.websoknad.domain.WebSoknad;
 import no.nav.sbl.dialogarena.websoknad.service.WebSoknadService;
 
-
-
 import org.apache.wicket.markup.html.WebPage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class SendSoknadServicePage extends WebPage{
+public class SendSoknadServicePage extends WebPage {
 	@Inject
 	private WebSoknadService soknadService;
 
-
 	Logger log = LoggerFactory.getLogger(SendSoknadServicePage.class);
-	
+
 	public SendSoknadServicePage() {
-		HttpServletRequest request = (HttpServletRequest) getRequest().getContainerRequest();
-		HttpServletResponse response = (HttpServletResponse) getResponse().getContainerResponse();
-		
-		if(request.getMethod().equalsIgnoreCase("GET")) {
+		HttpServletRequest request = (HttpServletRequest) getRequest()
+				.getContainerRequest();
+		HttpServletResponse response = (HttpServletResponse) getResponse()
+				.getContainerResponse();
+
+		if (request.getMethod().equalsIgnoreCase("GET")) {
 			Long soknadId;
-			if(request.getParameter("soknadId") == null){
+
+			if (request.getParameter("soknadId") == null) {
 				soknadId = soknadService.startSoknad("test");
 			} else {
 				soknadId = Long.valueOf(request.getParameter("soknadId"));
 			}
+
 			WebSoknad soknad = soknadService.hentSoknad(soknadId);
 			response.setHeader("soknadId", soknadId.toString());
-			
+
 			Map<String, Faktum> fakta = soknad.getFakta();
 			for (Entry<String, Faktum> entry : fakta.entrySet()) {
-				response.setHeader(entry.getKey(), entry.getValue().getValue());						
+				response.setHeader(entry.getKey(), entry.getValue().getValue());
 			}
-						
-		} else if(request.getMethod().equalsIgnoreCase("POST")) {
-			
-			
+
+		} else if (request.getMethod().equalsIgnoreCase("POST")) {
+			String avbrytParameter = request.getParameter("avbryt");
 			String bekreftetParameter = request.getParameter("bekreftet");
-			if(bekreftetParameter != null && bekreftetParameter.equals("ja")) {
-				haandtereKvitteringsPost(request,response);
+			if (avbrytParameter != null && avbrytParameter.equals("Avbryt")) {
+				haandtereAvbrytPost(request,response);
+			} else if (bekreftetParameter != null && bekreftetParameter.equals("ja")) {
+				haandtereKvitteringsPost(request, response);
 			} else {
 				haandtereOppsumeringsPost(request, response);
 			}
 		}
 	}
 
+	private void haandtereAvbrytPost(HttpServletRequest request,
+			HttpServletResponse response) {
+		Long soknadId = Long.valueOf(request.getParameter("soknadId"));
+		soknadService.avbrytSoknad(soknadId);
+		try {
+			response.sendRedirect("startSoknad");
+		} catch (IOException e) {
+			log.info("Klarte ikke sende redirect til startsiden.");
+		}
+		
+	}
+
 	private void haandtereKvitteringsPost(HttpServletRequest request,
-			HttpServletResponse response) {	
+			HttpServletResponse response) {
 		Long soknadId = Long.valueOf(request.getParameter("soknadId"));
 		soknadService.sendSoknad(soknadId);
-		
+
 		try {
 			response.sendRedirect("soknadKvittering");
 		} catch (IOException e) {
@@ -68,16 +82,18 @@ public class SendSoknadServicePage extends WebPage{
 		}
 	}
 
-	private void haandtereOppsumeringsPost(HttpServletRequest request, HttpServletResponse response) {
+	private void haandtereOppsumeringsPost(HttpServletRequest request,
+			HttpServletResponse response) {
 		Long soknadId = Long.valueOf(request.getParameter("soknadId"));
 		Map<String, String[]> parameterMap = request.getParameterMap();
 		for (Entry<String, String[]> entry : parameterMap.entrySet()) {
 			String value = request.getParameter(entry.getKey());
 			soknadService.lagreSoknadsFelt(soknadId, entry.getKey(), value);
-			log.debug("Soknadid: " + soknadId + " key: " + entry + " value: " + value);
+			log.debug("Soknadid: " + soknadId + " key: " + entry + " value: "
+					+ value);
 		}
 		try {
-			response.sendRedirect("oppsumering?soknadId="+soknadId);
+			response.sendRedirect("oppsumering?soknadId=" + soknadId);
 		} catch (IOException e) {
 			log.info("Klarte ikke sende redirect til oppsumeringssiden.");
 		}
