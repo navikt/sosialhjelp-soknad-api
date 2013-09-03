4563b2c6f018c292763dc856ba40a76e4691f795
Ketil S Velle <Ketil.S.Velle@nav.no>
2013-09-03 08:14:10 +0000

    [ANGULARPOC] under arbeid.

--- web/src/main/java/no/nav/sbl/dialogarena/websoknad/pages/sendsoknad/startside/StartSoknadPage.java
+++ web/src/main/java/no/nav/sbl/dialogarena/websoknad/pages/sendsoknad/startside/StartSoknadPage.java
@@ -1,10 +1,21 @@
 package no.nav.sbl.dialogarena.websoknad.pages.sendsoknad.startside;
 
-import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.util.string.StringValue;
 
+import no.nav.sbl.dialogarena.websoknad.pages.basepage.BasePage;
 
-public class StartSoknadPage extends WebPage {
+public class StartSoknadPage extends BasePage {
 
-	public StartSoknadPage() {
+	
+	public StartSoknadPage(PageParameters pageParameters) {
+		StringValue soknadsType = pageParameters.get("soknad");
+		if(soknadsType.equals("dagpenger")) {
+			System.out.println(soknadsType);
+		}else if (soknadsType.equals("barnebidrag")) {
+			System.out.println(soknadsType);
+		}
+		
+		
 	}
 }
