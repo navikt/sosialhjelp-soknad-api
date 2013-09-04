3c07b30638480469f0a4323cdbe2a06e0a05eb7f
Ketil S Velle <Ketil.S.Velle@nav.no>
2013-09-04 08:23:43 +0000

    [Kaizen] Fikset warnings

--- web/src/main/java/no/nav/sbl/dialogarena/websoknad/pages/sendsoknad/startside/StartSoknadPage.java
+++ web/src/main/java/no/nav/sbl/dialogarena/websoknad/pages/sendsoknad/startside/StartSoknadPage.java
@@ -1,21 +1,13 @@
 package no.nav.sbl.dialogarena.websoknad.pages.sendsoknad.startside;
 
-import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.util.string.StringValue;
-
 import no.nav.sbl.dialogarena.websoknad.pages.basepage.BasePage;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 public class StartSoknadPage extends BasePage {
 
 	
 	public StartSoknadPage(PageParameters pageParameters) {
-		StringValue soknadsType = pageParameters.get("soknad");
-		if(soknadsType.equals("dagpenger")) {
-			System.out.println(soknadsType);
-		}else if (soknadsType.equals("barnebidrag")) {
-			System.out.println(soknadsType);
-		}
-		
 		
 	}
 }
--- web/src/main/java/no/nav/sbl/dialogarena/websoknad/servlet/ValideringsResultat.java
+++ web/src/main/java/no/nav/sbl/dialogarena/websoknad/servlet/ValideringsResultat.java
@@ -1,8 +1,5 @@
 package no.nav.sbl.dialogarena.websoknad.servlet;
 
-
-import no.nav.sbl.dialogarena.websoknad.domain.Faktum;
-
 public class ValideringsResultat {
     public boolean resultat;
     public String beskjed;
