12ecb9ff5c64a2af39be64926f675cae202aa49e
Ingvild Indrebø <ingvild.indrebo@nav.no>
2013-09-06 16:13:35 +0200

    [POC] litt mer validering paa soknadskjemaer

--- web/src/main/webapp/js/app/controllers.js
+++ web/src/main/webapp/js/app/controllers.js
@@ -9,14 +9,27 @@ function PersonaliaCtrl($scope){
     postnummer: '0123',
     poststed: 'Oslo',
     adresse: 'Majorstuen 1',
-    landskode:'+47'
+    landskode:'+47',
+    barnUtland:'',
+    barnUtlandNavn: '',
+    barnUtlandEtternavn: ''
 	};
+}
+
+function VilligCtrl($scope){
+  $scope.villig = {
+    jobb: '',
+    pendle: '',
+    deltid:'',
+    helse:'',
+    deltidProsent:''
+  }
 }
 
 function ValidationCtrl($scope){
   $scope.ePost ="";
   $scope.feilmeldinger = {
-    paakreves: 'Må fylles ut',
+    paakreves: '*',
     feil: 'Ikke gyldig'
   };
 }
