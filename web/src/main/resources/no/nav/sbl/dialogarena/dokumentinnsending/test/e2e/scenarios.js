3d84991774d302e5f6169bb6d762edd4e43d4a67
Ketil Jensen <ketil.jensen@nav.no>
2013-09-06 08:14:22 +0200

    Lagt til Karma

---
+++ web/src/main/resources/no/nav/sbl/dialogarena/dokumentinnsending/test/e2e/scenarios.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('Send soknad app', function() {
+
+	beforeEach(function() {
+			browser().navigateTo('../../js/index.html');
+	});
+
+	it('should be true', fuction() {
+			expect(true).toBe(true);
+	});
+
+	it('skal redirigere index.html til index.html#/soknadiste', function(){
+		expect(browser().location().url()).toBe('/soknadliste');
+	});
+});
