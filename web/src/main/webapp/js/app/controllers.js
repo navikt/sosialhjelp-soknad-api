844bfca502b65c62efb635b0e037efa36b96a2e9
Ketil Jensen <ketil.jensen@nav.no>
2013-09-10 12:55:42 +0200

    POC: Rest kommunikasjon fungerer i Wicket. virker enda ikke fra local javascript server. En test å rette opp

--- web/src/main/webapp/js/app/controllers.js
+++ web/src/main/webapp/js/app/controllers.js
@@ -1,16 +1,8 @@
 'use strict';
 
 /* Controllers */
-function PersonaliaCtrl($scope){
-	$scope.personalia = {
-		fornavn: 'Ingvild',
-    etternavn: 'Indrebø',
-    fnr: '12345123456',
-    postnummer: '0123',
-    poststed: 'Oslo',
-    adresse: 'Majorstuen 1',
-    landskode:'+47',
-	};
+function PersonaliaCtrl($scope, HentSoknadService){
+	$scope.personalia = HentSoknadService.get({id: 1});
 }
 
 function ungerUtlandCtrl($scope){
--- web/src/main/webapp/test/unit/controllersSpec.js
+++ web/src/main/webapp/test/unit/controllersSpec.js
@@ -6,10 +6,24 @@ describe('Controllers', function() {
 
 	describe('PersonaliaCtrl', function() {
 
+		var $rootScope, $httpBackend, createController;
+
+		beforeEach(inject(function($injector) {
+			$httpBackend = $injector.get('$httpBackend');
+			$httpBackend.when('GET', '/sendsoknad/rest/soknad/:soknadId').respond({soknadId: '1', gosysId: 'Dagpenger'});
+
+			$rootScope = $injector.get('$controller');
+			createController = function() {
+				return $controller('PersonaliaCtrl',  {'$scope' : $rootScope});
+			};
+			
+		}));
+
 		it('skal returnere personalia for bruker', function(){
-			var scope = {},
-				ctrl = new PersonaliaCtrl(scope);
-			expect(scope.personalia.fornavn).toEqual('Ingvild');
+			$httpBackend.expectGET('/sendsoknad/rest/soknad/:soknadId');
+			var controller = createController();
+			$httpBackend.flush(); 
+			expect(scope.personalia.gosysId).toEqual('Dagpenger');
 		});
 	});
  
