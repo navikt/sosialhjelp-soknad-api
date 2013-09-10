d6133f42cdf016f4b00fa2ad0902bd3e9254d7d2
Ingvild Indrebø <ingvild.indrebo@nav.no>
2013-09-10 09:43:20 +0200

    [POC] lagt inn innhold i arbeidsforholdsteget i dagpengersoknaden

--- web/src/main/webapp/js/app/controllers.js
+++ web/src/main/webapp/js/app/controllers.js
@@ -10,7 +10,6 @@ function PersonaliaCtrl($scope){
     poststed: 'Oslo',
     adresse: 'Majorstuen 1',
     landskode:'+47',
-    barnUtland:''
 	};
 }
 
@@ -36,6 +35,27 @@ function VilligCtrl($scope){
     deltidProsent:''
   }
 }
+
+function ArbeidsforholdCtrl($scope){
+  $scope.arbeidsforhold = {
+    arbeidsgiverNavn: '',
+    varighetFra: '',
+    varighetTil:'',
+    land:'',
+    sluttaarsaken: ''
+   };
+
+   $scope.sluttaarsak = [
+   {id: 1, navn:'Sagt opp selv'},
+   {id: 2, navn:'Sagt opp av arbeidsgiver'},
+   {id: 3, navn:'Avskjediget'},
+   {id: 4, navn:'Kontrakt utgått'},
+   {id: 5, navn:'Permitert'},
+   {id: 6, navn:'Konkurs'},
+   {id: 7, navn:'Redusert arbeidstid med mer enn 50% (40% for fiskere)'}
+   ];
+}
+
 
 function ValidationCtrl($scope, soknadService, $location){
   $scope.data =  soknadService.data;
