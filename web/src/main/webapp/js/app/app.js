64f958c52f126cb272f795efcf6562d63f45c806
Ingvild Indrebø <ingvild.indrebo@nav.no>
2013-09-11 08:54:26 +0200

    [POC] erstattet dagpenger med personalia

--- web/src/main/webapp/js/app/app.js
+++ web/src/main/webapp/js/app/app.js
@@ -6,7 +6,7 @@ var sendsoknad = angular.module('sendsoknad', ['ngRoute', 'ngResource']);
 sendsoknad.config(function($routeProvider) {
     $routeProvider
   	  .when('/soknadliste', {templateUrl: '../html/templates/soknadliste.html'})
-  	  .when('/dagpenger', {templateUrl: '../html/templates/dagpenger.html', controller: PersonaliaCtrl})
+  	  .when('/personalia', {templateUrl: '../html/templates/personalia.html', controller: PersonaliaCtrl})
       .when('/reell-arbeidssoker', {templateUrl: '../html/templates/reell-arbeidssoker.html', controller: PersonaliaCtrl})
       .when('/arbeidsforhold', {templateUrl: '../html/templates/arbeidsforhold.html', controller: PersonaliaCtrl})
       .when('/utslagskriterier', {templateUrl: '../html/templates/utslagskriterier.html', controller: PersonaliaCtrl})
