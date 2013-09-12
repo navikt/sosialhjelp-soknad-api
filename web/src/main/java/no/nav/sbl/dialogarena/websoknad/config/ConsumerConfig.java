35d7f16cf6ebfaf5e9dcb2064663a1de26cfafac
w139306 <eirik.wang@nav.no>
2013-09-12 13:20:28 +0200

    Consumer Config/appconfig for sendsøknad.

--- web/src/main/java/no/nav/sbl/dialogarena/websoknad/config/ConsumerConfig.java
+++ web/src/main/java/no/nav/sbl/dialogarena/websoknad/config/ConsumerConfig.java
@@ -38,37 +38,17 @@ import static org.apache.cxf.ws.security.SecurityConstants.MUST_UNDERSTAND;
 @Configuration
 @Import(value = {KodeverkConfig.class,
         ConsumerConfig.ServicesConfig.class,
-        ConsumerConfig.OppdaterHenvendelseBehandlingWSConfig.class,
-        ConsumerConfig.BrukerBehandlingWSConfig.class,
-        ConsumerConfig.BrukerProfilWSConfig.class,
-        ConsumerConfig.BrukerProfilMockWSConfig.class,
+        no.nav.sbl.dialogarena.dokumentinnsending.config.ConsumerConfig.class,
         ConsumerConfig.SendSoknadWSConfig.class})
 @ImportResource({"classpath:META-INF/cxf/cxf.xml", "classpath:META-INF/cxf/cxf-servlet.xml"})
 public class ConsumerConfig {
     private static final Logger LOGGER = LoggerFactory.getLogger(ConsumerConfig.class);
 
-    @Bean
-    public KodeverkIntegrasjon kodeverkIntegrasjon() {
-        return new KodeverkIntegrasjon();
-    }
-
     @Configuration
-    @Import(KodeverkConfig.class)
     public static class ServicesConfig {
-
-        @Bean
-        public DefaultSoknadService soknadService() {
-            return new DefaultSoknadService();
-        }
-
         @Bean
         public WebSoknadService webSoknadService() {
             return new WebSoknadService();
-        }
-
-        @Bean
-        public DefaultBrukerBehandlingServiceIntegration brukerBehandlingServiceIntegration() {
-            return new DefaultBrukerBehandlingServiceIntegration();
         }
     }
 
@@ -88,117 +68,15 @@ public class ConsumerConfig {
         }
     }
 
-    @Configuration
-    public static class BrukerBehandlingWSConfig {
 
-        @Value("${dokumentinnsending.webservice.henvendelse.henvendelsesbehandlingservice.url}")
-        private URL henvendelserEndpoint;
-
-        @Bean
-        public JaxWsProxyFactoryBean henvendelsesBehandlingPortTypeFactory() {
-            return getJaxWsProxyFactoryBean(henvendelserEndpoint, HenvendelsesBehandlingPortType.class, "classpath:HenvendelsesBehandling.wsdl");
-        }
-
-        @Bean
-        public HenvendelsesBehandlingPortType brukerBehandlingPortType() {
-            return konfigurerMedHttps(henvendelsesBehandlingPortTypeFactory().create(HenvendelsesBehandlingPortType.class));
-        }
-
-    }
-
-    @Configuration
-    public static class OppdaterHenvendelseBehandlingWSConfig {
-
-        @Value("${dokumentinnsending.webservice.henvendelse.oppdaterehenvendelsesbehandlingservice.url}")
-        private URL henvendelserEndpoint;
-
-        @Bean
-        public JaxWsProxyFactoryBean oppdaterHenvendelsesBehandlingServicePortTypeFactory() {
-            return getJaxWsProxyFactoryBean(henvendelserEndpoint, OppdatereHenvendelsesBehandlingPortType.class, "classpath:Oppdaterehenvendelsesbehandling.wsdl");
-        }
-
-        @Bean
-        public OppdatereHenvendelsesBehandlingPortType oppdatereHenvendelsesBehandlingPortType() {
-            return konfigurerMedHttps(oppdaterHenvendelsesBehandlingServicePortTypeFactory().create(OppdatereHenvendelsesBehandlingPortType.class));
-        }
-
-    }
-
-    @Configuration
-    public static class BrukerProfilWSConfig {
-
-        @Value("${dokumentinnsending.webservice.brukerprofil.url}")
-        private URL brukerprofilEndpoint;
-
-        @Bean
-        public JaxWsProxyFactoryBean brukerBehandlingServicePortTypeFactory() throws Exception {
-            JaxWsProxyFactoryBean proxyFactoryBean = getJaxWsProxyFactoryBean(brukerprofilEndpoint, BrukerprofilPortType.class, "classpath:brukerprofil/no/nav/tjeneste/virksomhet/brukerprofil/v1/Brukerprofil.wsdl");
-
-            Map<String, Object> props = new HashMap<>();
-            props.put("schema-validation-enabled", "false");
-            proxyFactoryBean.setProperties(props);
-            LOGGER.info("TPS URL: {}", brukerprofilEndpoint.getHost() + ":" + brukerprofilEndpoint.getPort());
-            return proxyFactoryBean;
-        }
-
-        @Bean
-        public BrukerprofilPortType brukerBehandlingServicePortType() throws Exception {
-            return brukerBehandlingServicePortTypeFactory().create(BrukerprofilPortType.class);
-        }
-    }
-
-    @Configuration
-    @Profile("mock")
-    public static class BrukerProfilMockWSConfig {
-
-        @Value("${brukerprofil.webservice.mock.brukerprofil.url}")
-        private URL brukerprofilMockEndpoint;
-
-        @Bean
-        public JaxWsProxyFactoryBean brukerBehandlingServicePortTypeMock() {
-            LOGGER.info("Mock URL: {}", brukerprofilMockEndpoint.getHost() + ":" + brukerprofilMockEndpoint.getPort());
-            return getJaxWsProxyFactoryBean(brukerprofilMockEndpoint, BrukerprofilPortType.class, "classpath:brukerprofil/no/nav/tjeneste/virksomhet/brukerprofil/v1/Brukerprofil.wsdl");
-        }
-
-        @Bean
-        public BrukerprofilPortType brukerBehandlingServicePortType() throws Exception {
-            return brukerBehandlingServicePortTypeMock().create(BrukerprofilPortType.class);
-        }
-    }
-
-    @Configuration
-    public static class SelfTestStsConfig {
-        @Inject
-        private HenvendelsesBehandlingPortType henvendelsesBehandlingPortType;
-        @Inject
-        private BrukerprofilPortType brukerprofilPortType;
-        @Inject
-        private SendSoknadPortType sendSoknadPortType;
-
-        //@PostConstruct
-        public void setupSts() {
-            STSConfigurationUtility.configureStsForSystemUser(ClientProxy.getClient(henvendelsesBehandlingPortType));
-            STSConfigurationUtility.configureStsForSystemUser(ClientProxy.getClient(brukerprofilPortType));
-            STSConfigurationUtility.configureStsForSystemUser(ClientProxy.getClient(sendSoknadPortType));
-        }
-    }
 
     @Configuration
     public static class ExternalStsConfig {
-        @Inject
-        private OppdatereHenvendelsesBehandlingPortType oppdatereHenvendelsesBehandlingPortType;
-        @Inject
-        private HenvendelsesBehandlingPortType henvendelsesBehandlingPortType;
-        @Inject
-        private BrukerprofilPortType brukerprofilPortType;
         @Inject
         private SendSoknadPortType sendSoknadPortType;
 
         @PostConstruct
         public void setupSts() {
-            STSConfigurationUtility.configureStsForExternalSSO(ClientProxy.getClient(oppdatereHenvendelsesBehandlingPortType));
-            STSConfigurationUtility.configureStsForExternalSSO(ClientProxy.getClient(henvendelsesBehandlingPortType));
-            STSConfigurationUtility.configureStsForExternalSSO(ClientProxy.getClient(brukerprofilPortType));
             STSConfigurationUtility.configureStsForExternalSSO(ClientProxy.getClient(sendSoknadPortType));
         }
     }
