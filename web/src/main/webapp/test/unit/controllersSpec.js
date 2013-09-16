5b603fd1c4b9cd51ac2d322e9ac1ba950aae6416
Ketil Jensen <ketil.jensen@nav.no>
2013-09-16 14:44:13 +0200

    Tester kjører grønt. Noen e2e tester må refaktorers

--- web/src/main/webapp/test/unit/controllersSpec.js
+++ web/src/main/webapp/test/unit/controllersSpec.js
@@ -4,44 +4,50 @@
 
 describe('Controllers', function() {
 
+	var $scope;
+	var $controller;
+
+	beforeEach(module('app.controllers'));
+	
+	beforeEach(inject(function ($injector) {
+		$scope = $injector.get('$rootScope');
+
+		$controller = $injector.get('$controller');
+	}));
+
 	describe('PersonaliaCtrl', function() {
 
+		var scope, ctrl;
+		beforeEach(function(){
+			scope = $scope;
+			ctrl = $controller('PersonaliaCtrl', {$scope: scope});
+		});
+
 		it('skal returnere personalia for bruker', function(){
-			var scope = {},
-				ctrl = new PersonaliaCtrl(scope);
 			expect(scope.personalia.fornavn).toEqual('Ingvild');
 		});
 
+		
 		it('skal returnere false for ung arbeidsøker', function() {
-			var scope = {};
-			var ctrl = new PersonaliaCtrl(scope);
 			scope.personalia.alder = 17;
 			expect(scope.isGyldigAlder()).toEqual(false);
 		});
 
+		
 		it('skal returnere false for gammel arbeidsøker', function() {
-			var scope = {};
-			var ctrl = new PersonaliaCtrl(scope);
 			scope.personalia.alder = 67;
 			expect(scope.isGyldigAlder()).toEqual(false);
 		});
 
-
 		it('skal returnere true for myndig arbeidsøker', function() {
-			var scope = {};
-			var ctrl = new PersonaliaCtrl(scope);
 			scope.personalia.alder = 18;
 			expect(scope.isGyldigAlder()).toEqual(true);
 		});
 
-
 		it('skal returnere false for på grensen til for gammel arbeidsøker', function() {
-			var scope = {};
-			var ctrl = new PersonaliaCtrl(scope);
 			scope.personalia.alder = 66;
 			expect(scope.isGyldigAlder()).toEqual(true);
 		});
-
 	});
  
 });
