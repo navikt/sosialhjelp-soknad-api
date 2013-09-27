829074a128981bd75f20c7874dbd81a11fe8b687
Ingvild Indrebø <ingvild.indrebo@nav.no>
2013-09-27 13:26:46 +0200

    Fikset validering på checkboxene i reell-arbeidssoker

--- web/src/main/webapp/js/app/routes.js
+++ web/src/main/webapp/js/app/routes.js
@@ -4,7 +4,7 @@ angular.module('app.routes',['ngRoute'])
     $routeProvider
   	  .when('/soknadliste', {templateUrl: '../html/templates/soknadliste.html'})
   	  .when('/personalia', {templateUrl: '../html/templates/personalia.html', controller: 'SoknadDataCtrl'})
-      .when('/reell-arbeidssoker', {templateUrl: '../html/templates/reell-arbeidssoker.html', controller: 'SoknadDataCtrl'})
+      .when('/reell-arbeidssoker', {templateUrl: '../html/templates/reell-arbeidssoker.html', controller: 'ValidationCtrl'})
       .when('/arbeidsforhold', {templateUrl: '../html/templates/arbeidsforhold.html', controller: 'GrunnlagsdataCtrl'})
       .when('/informasjonsside', {templateUrl: '../html/templates/informasjonsside.html', controller: 'StartSoknadCtrl'})
       .when('/utslagskriterier', {templateUrl: '../html/templates/utslagskriterier.html', controller: 'GrunnlagsdataCtrl'})
