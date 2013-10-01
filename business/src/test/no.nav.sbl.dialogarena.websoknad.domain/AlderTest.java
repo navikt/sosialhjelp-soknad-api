11eb54786e88b7a267705e19683d411af9a06195
Ingvild Indrebø <ingvild.indrebo@nav.no>
2013-10-01 13:25:38 +0200

    [4838] fikset alder med tilhørende tester

--- business/src/test/no.nav.sbl.dialogarena.websoknad.domain/AlderTest.java
+++ business/src/test/no.nav.sbl.dialogarena.websoknad.domain/AlderTest.java
@@ -1,5 +1,8 @@
-import no.nav.sbl.dialogarena.websoknad.domain.Alder;
+import no.nav.sbl.dialogarena.websoknad.domain.PersonAlder;
 import static org.junit.Assert.assertEquals;
+
+import org.joda.time.DateTimeUtils;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -10,9 +13,36 @@ import org.junit.Test;
  * To change this template use File | Settings | File Templates.
  */
 public class AlderTest {
+
+    // 01.10.2013
+    private static final long IDAG = 1380625958979L;
+
+    @Before
+    public void init() {
+        DateTimeUtils.setCurrentMillisFixed(IDAG);
+    }
+
     @Test
-    public void alderSkalVaere24ForPersonMedFNR_08122413838() {
-        Alder alder = new Alder("08122413838");
-        assertEquals(24, alder.getAlder());
+    public void alderSkalVaere100ForPersonMedFNR_16051329123() {
+        PersonAlder alder = new PersonAlder("16051329132");
+        assertEquals(100, alder.getAlder());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void skalFaaExceptionDersomFnrErUgyldig() {
+        PersonAlder alder = new PersonAlder("16051329332");
+        assertEquals(100, alder.getAlder());
+    }
+
+    @Test
+    public void alderSkalVaere105ForPersonMedFNR_06030849092() {
+        PersonAlder alder = new PersonAlder("06030849092");
+        assertEquals(105, alder.getAlder());
+    }
+
+    @Test
+    public void alderSkalVaere5ForPersonMedFNR_12040886859() {
+        PersonAlder alder = new PersonAlder("12040886859");
+        assertEquals(5, alder.getAlder());
     }
 }
