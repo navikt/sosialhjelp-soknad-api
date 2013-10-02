a4b9f34ecae321d3dc60ca61ff7e943dcd4e1f57
Ingvild Indrebø <ingvild.indrebo@nav.no>
2013-10-02 12:44:11 +0200

    Fikset lagring, igjen

--- web/src/main/webapp/js/app/directives.js
+++ web/src/main/webapp/js/app/directives.js
@@ -115,16 +115,6 @@ angular.module('app.directives', ['app.services'])
   };
 })
 
-.directive('modFaktum', function($rootScope) {
-  return function( $scope, element, attrs ) {
-    element.bind('blur', function() {
-      $rootScope.soknadData.fakta[attrs.name] = {"soknadId":$scope.soknadData.soknadId,"key":attrs.name,"value":element.val(), "type": "BRUKERREGISTRERT"};
-      $rootScope.$apply();
-    });
-  };
-})
-
-
 .directive('knapprad', function ($location, $routeParams) {
     return {
         restrict: "E",
