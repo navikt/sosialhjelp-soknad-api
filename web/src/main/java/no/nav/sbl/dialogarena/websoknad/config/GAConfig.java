b820df966707893248e061a3751e7cc9890343d2
Hein Haraldsen <Hein.Haraldsen@nav.no>
2013-10-04 10:40:04 +0200

    Ryddet opp: fjernet overflødig .properties-fil og deprecated GA-attributt.

--- web/src/main/java/no/nav/sbl/dialogarena/websoknad/config/GAConfig.java
+++ web/src/main/java/no/nav/sbl/dialogarena/websoknad/config/GAConfig.java
@@ -46,7 +46,6 @@ public class GAConfig {
                 "_gaq.push(['_trackPageview']);  " +
                 "_gaq.push(['_gat._anonymizeIp']);" +
                 "_gaq.push(['_setDomainName', 'nav.no']);" +
-                "_gaq.push(['_trackPageLoadTime']);" +
                 "(function() {    " +
                 "var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;    " +
                 "ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';   " +
