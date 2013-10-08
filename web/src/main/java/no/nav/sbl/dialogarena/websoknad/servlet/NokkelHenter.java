cf8ef723f86a456826020c4ac96a78f9373cc44d
Ketil Velle <ketil.s.velle@nav.no>
2013-10-08 10:31:09 +0200

    [KAIZEN] Fikset paa NokkelHenter

--- web/src/main/java/no/nav/sbl/dialogarena/websoknad/servlet/NokkelHenter.java
+++ web/src/main/java/no/nav/sbl/dialogarena/websoknad/servlet/NokkelHenter.java
@@ -7,22 +7,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import no.nav.modig.core.exception.ApplicationException;
+
 public class NokkelHenter {
-    public static List<String> hentNokler(String side) {
+        public static List<String> hentNokler(String side) {
         List<String> nokler = new ArrayList<>();
 
         String filnavn = format("/templatekeys/%s.txt", side);
-        InputStream ressursStrom = NokkelHenter.class.getResourceAsStream(filnavn);
-        Scanner scanner = null;
-        try {
-            scanner = new Scanner(ressursStrom);
+        try(InputStream ressursStrom = NokkelHenter.class.getResourceAsStream(filnavn);
+        		Scanner scanner = new Scanner(ressursStrom);) {
             while (scanner.hasNextLine()) {
                 nokler.add(scanner.nextLine());
             }
-        } finally {
-            if (scanner != null) {
-                scanner.close();
-            }
+        } catch (Exception e) {
+			throw new ApplicationException("Fant ikke fil",e);
         }
         return nokler;
     }
