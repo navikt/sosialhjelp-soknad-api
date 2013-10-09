42f7ba6c63d8ca57671e4889d5dfbfb45761fdfb
Anders Hefre <anders.hefre@nav.no>
2013-10-09 10:13:52 +0200

    [Checkstyle] Mellomrom mellom parenteser! LOL.

--- consumer/src/test/java/no/nav/sbl/dialogarena/InMemorySoknadInnsendingRepositorytTest.java
+++ consumer/src/test/java/no/nav/sbl/dialogarena/InMemorySoknadInnsendingRepositorytTest.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
+
 public class InMemorySoknadInnsendingRepositorytTest {
 
     private InMemorySoknadInnsendingRepository repo;
