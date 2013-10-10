2c0190bf6655aa0964a28ba02937132f6f4a4abe
Andreas Svendby <andreas.svendby@nav.no>
2013-10-10 09:03:09 +0200

    Lagt til test for startsoknadpage

--- web/src/test/java/no/nav/sbl/dialogarena/websoknad/pages/soknadliste/SoknadListePageTest.java
+++ web/src/test/java/no/nav/sbl/dialogarena/websoknad/pages/soknadliste/SoknadListePageTest.java
@@ -1,15 +1,29 @@
 package no.nav.sbl.dialogarena.websoknad.pages.soknadliste;
 
-import javax.inject.Inject;
-
 import no.nav.modig.wicket.test.FluentWicketTester;
+import no.nav.modig.wicket.test.internal.Parameters;
 import no.nav.sbl.dialogarena.websoknad.WicketApplication;
 import no.nav.sbl.dialogarena.websoknad.config.FitNesseApplicationConfig;
-
+import no.nav.sbl.dialogarena.websoknad.pages.startsoknad.StartSoknadPage;
+import org.apache.wicket.protocol.http.WebApplication;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.mock.web.MockMultipartHttpServletRequest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.inject.Inject;
+import javax.servlet.ServletContext;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.InputStream;
+
+import static no.nav.modig.wicket.test.matcher.ComponentMatchers.withId;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @ContextConfiguration(classes = { FitNesseApplicationConfig.class })
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -18,10 +32,29 @@ public class SoknadListePageTest {
 	@Inject
 	private FluentWicketTester<WicketApplication> wicketTester;
 
+    @Before
+    public void setup() {
+        String testHtmlFolder = (new MockMultipartHttpServletRequest()).getSession().getServletContext().getRealPath("/") + "/html";
+        ServletContext servletContextMock = mock(ServletContext.class);
+        wicketTester.tester.getApplication().setServletContext(servletContextMock);
+        when(servletContextMock.getRealPath("/html")).thenReturn(testHtmlFolder);
+
+        InputStream inputStream = new ByteArrayInputStream("liksomHTML".getBytes());
+        when(servletContextMock.getResourceAsStream("/html/Dagpenger.html")).thenReturn(inputStream);
+    }
 
 	@Test
-	public void shouldRenderHomePage() {
+	public void skalApneSoknadLisePage() {
 		wicketTester.goTo(SoknadListePage.class);
 	}
 
+    @Test
+    public void skalStarteNySoknadOmDagpenger() {
+        wicketTester
+                .goTo(SoknadListePage.class)
+                .click()
+                .link(withId("dagpenger"))
+                .should()
+                .beOn(StartSoknadPage.class);
+    }
 }
