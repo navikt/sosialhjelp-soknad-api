effc3645a5e2a4f34fd917e00119052f53453d8b
Ingvild Indrebø <ingvild.indrebo@nav.no>
2013-10-16 16:36:51 +0200

    [MOD-4797] lagt til checkbokser og checkbox-directive og ytelserssiden

--- web/src/main/webapp/js/app/directives/navinput/navinputModule.js
+++ web/src/main/webapp/js/app/directives/navinput/navinputModule.js
@@ -12,6 +12,18 @@ angular.module('nav.input',['nav.cmstekster'])
             templateUrl: '../js/app/directives/navinput/navradioTemplate.html'
         }
     }])
+    .directive('navcheckbox', [function() {
+        return {
+            restrict: "E",
+            replace: true,
+            scope: {
+                model: '=',
+                name: '@',
+                label: '@'
+            },
+            templateUrl: '../js/app/directives/navinput/navcheckboxTemplate.html'
+        }
+    }])
     .directive('navButtonSpinner', [function() {
         return {
             restrict: "E",
@@ -25,4 +37,20 @@ angular.module('nav.input',['nav.cmstekster'])
             },
             templateUrl: '../js/app/directives/navinput/navbuttonspinnerTemplate.html'
         }
+    }])
+
+    .directive('booleanVerdi', [function(){
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            link: function(scope, element, attr, ngModel){
+                function fraTekst(tekst) {
+                    if(tekst === "true") {
+                        return true;
+                    }
+                    return false;
+                }
+                ngModel.$formatters.push(fraTekst);
+            }
+        };
     }]);
