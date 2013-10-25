9c1d4e9a4e3fc3da370a1f9d903f80fe6ef4a56c
Eirik L. Wang <eirik.wang@nav.no>
2013-10-25 14:50:20 +0200

    laget NavMessageSource som extender ReloadableMessageSource. Den henter properties fra filer, og overskriver disse om de finnes i  enonic. Støtter automatisk flere språk om dette confes opp.

--- web/src/main/java/no/nav/sbl/dialogarena/websoknad/servlet/MessageController.java
+++ web/src/main/java/no/nav/sbl/dialogarena/websoknad/servlet/MessageController.java
@@ -1,35 +1,29 @@
 package no.nav.sbl.dialogarena.websoknad.servlet;
 
-import org.apache.commons.lang3.StringUtils;
-import org.apache.wicket.model.StringResourceModel;
+import no.nav.sbl.dialogarena.websoknad.config.NavMessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.inject.Inject;
 import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import java.util.Properties;
 
 @Controller
 @RequestMapping("/enonic")
-public class EnonicController {
+public class MessageController {
 
     private Map<String, String> tekster = new LinkedHashMap<>();
+    @Inject
+    private NavMessageSource navMessageSource;
 
     @RequestMapping(value = "/{side}", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody()
-    public Map<String, String> hentTekster(@PathVariable String side) {
-
-        List<String> nokler = NokkelHenter.hentNokler(side);
-
-        for (String nokkel : nokler) {
-            if (StringUtils.isNotBlank(nokkel)) {
-                tekster.put(nokkel, new StringResourceModel(nokkel, null).getString());
-            }
-        }
-
-        return tekster;
+    public Properties hentTekster(@PathVariable String side, Locale locale) {
+        return navMessageSource.getBundleFor(side, new Locale("nb", "NO"));
     }
 }
