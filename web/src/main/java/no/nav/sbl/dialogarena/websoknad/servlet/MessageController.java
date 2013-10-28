faf2edee9d6b1b00cc064eab8329b41a6da2b0e8
Eirik L. Wang <eirik.wang@nav.no>
2013-10-28 09:12:52 +0100

    pmdfix og ny test

--- web/src/main/java/no/nav/sbl/dialogarena/websoknad/servlet/MessageController.java
+++ web/src/main/java/no/nav/sbl/dialogarena/websoknad/servlet/MessageController.java
@@ -8,22 +8,30 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.inject.Inject;
-import java.util.LinkedHashMap;
 import java.util.Locale;
-import java.util.Map;
 import java.util.Properties;
 
 @Controller
 @RequestMapping("/enonic")
 public class MessageController {
 
-    private Map<String, String> tekster = new LinkedHashMap<>();
     @Inject
     private NavMessageSource navMessageSource;
+
+    private boolean tvingNorsk = true;
 
     @RequestMapping(value = "/{side}", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody()
     public Properties hentTekster(@PathVariable String side, Locale locale) {
-        return navMessageSource.getBundleFor(side, new Locale("nb", "NO"));
+        Locale loc = locale;
+        if (tvingNorsk) {
+            loc = new Locale("nb", "NO");
+        }
+
+        return navMessageSource.getBundleFor(side, loc);
+    }
+
+    public void setTvingNorsk(boolean tvingNorsk) {
+        this.tvingNorsk = tvingNorsk;
     }
 }
