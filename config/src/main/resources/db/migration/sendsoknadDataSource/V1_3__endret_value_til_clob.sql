22eeae5ea489c39615b11239fa1fc276889211d3
Ketil Jensen <ketil.jensen@nav.no>
2013-10-28 10:15:18 +0100

    MOD-5004: Ng-include virker nå inne i liste. Ny versjon av angular. 1.2.0-rc3

--- config/src/main/resources/db/migration/sendsoknadDataSource/V1_3__endret_value_til_clob.sql
+++ config/src/main/resources/db/migration/sendsoknadDataSource/V1_3__endret_value_til_clob.sql
@@ -1,1 +1,4 @@
-ALTER TABLE SoknadBrukerData MODIFY (VALUE CLOB);
+ALTER TABLE SoknadBrukerData ADD (foo CLOB);
+UPDATE SoknadBrukerData SET foo = value;
+ALTER TABLE SoknadBrukerData drop column value;
+ALTER TABLE SoknadBrukerData RENAME column foo to value;
