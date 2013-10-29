01ebff1416edc9f273a117d83dcadf41a9fd3178
Andreas Svendby <andreas.svendby@nav.no>
2013-10-29 12:44:43 +0100

    [MOD-5041]: Lagt til funksjonalitet for å gå mellom tabs vha knapper

--- web/src/main/resources/no/nav/sbl/dialogarena/websoknad/js/all.js
+++
@@ -1,7 +0,0 @@
-function scrollToElement(element, offset) {
-    if (offset === undefined) {
-        offset = 100;
-    }
-
-    $(window).scrollTop(Math.max(element.offset().top - offset, 0), 0);
-}
