d946336ce389dbac1d0aa62c6490a18dea975d82
Eirik L. Wang <eirik.wang@nav.no>
2013-11-04 14:51:43 +0100

    MOD-4665: Startet backend jobb for å finne hvilke vedlegg som skal lastes opp.

---
+++ web/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/oppsett/SoknadStruktur.java
@@ -0,0 +1,68 @@
+package no.nav.sbl.dialogarena.soknadinnsending.oppsett;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Denne klassen fungerer som en oversikt over en søknad. Den Lister ut en søknad med tilhørende felter og avhengigheter.
+ */
+
+
+@XmlRootElement(name = "soknad")
+public class SoknadStruktur implements Serializable {
+    private String gosysId;
+
+    private List<Faktum> fakta = new ArrayList<>();
+    private List<Vedlegg> vedlegg = new ArrayList<>();
+
+    @XmlAttribute
+    public String getGosysId() {
+        return gosysId;
+    }
+
+    public void setGosysId(String gosysId) {
+        this.gosysId = gosysId;
+    }
+
+    @XmlElement(name = "faktum")
+    public List<Faktum> getFakta() {
+        return fakta;
+    }
+
+    public void setFakta(List<Faktum> fakta) {
+        this.fakta = fakta;
+    }
+
+    public List<Vedlegg> getVedlegg() {
+        return vedlegg;
+    }
+
+    public void setVedlegg(List<Vedlegg> vedlegg) {
+        this.vedlegg = vedlegg;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("SoknadStruktur{");
+        sb.append("gosysId='").append(gosysId).append('\'');
+        sb.append(", fakta=").append(fakta);
+        sb.append(", vedlegg=").append(vedlegg);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    public static void main(String[] args) throws JAXBException {
+        JAXBContext context = JAXBContext.newInstance(SoknadStruktur.class);
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        SoknadStruktur unmarshal = (SoknadStruktur) unmarshaller.unmarshal(SoknadStruktur.class.getResourceAsStream("/soknader/Dagpenger.xml"));
+        System.out.println(unmarshal);
+
+    }
+}
