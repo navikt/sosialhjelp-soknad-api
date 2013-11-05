fc7a022c1dfa816157085a16cd7f2db3b1708a5c
Ingvild Indrebø <ingvild.indrebo@nav.no>
2013-11-05 12:45:40 +0100

    [MOD-5046] fikset IE8kompatibilitet ved å flytte ng-app

--- web/src/main/webapp/js/app/directives/autoresizetextarea/autoresize-textarea.js
+++ web/src/main/webapp/js/app/directives/autoresizetextarea/autoresize-textarea.js
@@ -1,12 +1,12 @@
 angular.module('nav.autoresizetextarea', [])
     .directive('autoresizetextarea', [function () {
         return {
-            link: function (scope, element, attrs) {
-                $('#frivillig-textarea').on('keyup', 'textarea', function (e) {
+            link: function (scope, element) {
+                $(element).on('keyup', 'textarea', function (e) {
                     $(this).css('height', '0px');
                     $(this).height(this.scrollHeight);
                 });
-                $('#frivillig-textarea').find('textarea').keyup();
+                $(element).find('textarea').keyup();
             }
         }
     }]);
