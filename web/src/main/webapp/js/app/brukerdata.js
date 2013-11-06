f356f8b815a745fb9ee8409d9404387d10291286
Hein Haraldsen <Hein.Haraldsen@nav.no>
2013-11-06 14:50:48 +0100

    C/O-adresse continued

--- web/src/main/webapp/js/app/brukerdata.js
+++ web/src/main/webapp/js/app/brukerdata.js
@@ -72,7 +72,7 @@ angular.module('app.brukerdata', ['app.services'])
 
             $scope.harMidlertidigAdresseEier = function () {
 
-                return $scope.personaliaData.midlertidigAdresse.adresseEier != undefined;
+                return $scope.personalia.fakta.adresser[$scope.personaliaData.midlertidigAdresse].adresseEier != undefined;
             }
 
             $scope.harBostedsadresseOgIngenMidlertidigAdresse = function() {
