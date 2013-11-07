bd94cc1fe679a72617b7f09bc9bf75ce176e1a56
Ingvild Indrebø <ingvild.indrebo@nav.no>
2013-11-07 12:40:24 +0100

    [MOD-5046] laget et direktiv for textarea som skal resizes dynamisk uten scrollbar, har en maksbegrenings av antall tegn og som viser hvor mange tegn som er igjen.

---
+++ web/src/main/webapp/js/app/directives/navtextarea/navtextarea.js
@@ -0,0 +1,43 @@
+angular.module('nav.textarea', [])
+    .directive('navtextarea', [function () {
+        return {
+            replace: true,
+            require: 'ngModel',
+            scope: {
+                model: '=ngModel',
+                nokkel: '@',
+                maxlengde: '@'
+            },
+            controller: function($scope) {
+                $scope.sporsmal = $scope.nokkel + ".sporsmal";
+                $scope.feilmelding = $scope.nokkel + ".feilmelding";
+                $scope.tellertekst = $scope.nokkel + ".tellertekst";
+                $scope.counter = $scope.maxlengde;
+                $scope.fokus = false;
+                $scope.oppdaterTeller = function() {
+                    if($scope.model) {
+                        $scope.counter = $scope.maxlengde - $scope.model.length;
+                    } else {
+                        $scope.counter = $scope.maxlengde;
+                    }
+                }
+            },
+            templateUrl: '../js/app/directives/navtextarea/navtextareaTemplate.html',
+            link: function ($scope, element, attrs) {
+                $(element).on('keyup', 'textarea', function (e) {
+                    $(this).css('height', '0px');
+                    $(this).height(this.scrollHeight);
+                });
+                $(element).find('textarea').keyup();
+
+                element.find('textarea').bind('focus', function(){
+                    $scope.fokus = true;
+                    $scope.$apply(attrs.onFocus);
+                })
+                element.find('textarea').bind('blur', function() {
+                    $scope.fokus = false;
+                    $scope.$apply(attrs.onBlur)
+                })
+            }
+        };
+    }]);
