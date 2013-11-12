fda71d16fd520a8a6fb3785b1c7e3ccfb2dc7b33
Ketil Jensen <ketil.jensen@nav.no>
2013-11-12 09:58:34 +0100

    MOD-5040: La til templates for de forskjellige statusene. Litt arbeid gjenstår, spesielt for GUI

--- web/src/main/webapp/js/app/controllers/dagpenger/forsettsenereController.js
+++ web/src/main/webapp/js/app/controllers/dagpenger/forsettsenereController.js
@@ -1,51 +1,47 @@
-angular.module('nav.forsettsenere',[])
+angular.module('nav.forsettsenere',['nav.cmstekster'])
     .controller('FortsettSenereCtrl', ['$scope', 'soknadService', '$routeParams', '$http', '$location', 
         function ($scope, soknadService, $routeParams, $http,  $location) {
-        $scope.soknadData;
-
-		soknadService.get({param: $routeParams.soknadId}).$promise.then(function (result) {
-            $scope.soknadData = result;
-
-            	/*
-            $scope.soknadData.fakta.personalia = {};
-            $scope.soknadData.fakta.personalia.epost = { 
-            	key: "epost",
-				soknadId: $scope.soknadData.soknadId,
-				type: "SYSTEM",
-				value: "ketil.s.velle@nav.no"
-			};*/
-
-			console.log($scope.soknadData);
-        });
 
         $scope.forsettSenere = function() {
             var soknadId = $routeParams.soknadId;
 			$http.post('/sendsoknad/rest/soknad/' + soknadId +'/fortsettsenere', $scope.soknadData.fakta.personalia.epost.value)
 				.success(function(data) {
-					console.log("mail sendt til " + $scope.soknadData.fakta.personalia.epost.value);
                     $location.path('kvittering-fortsettsenere/' + soknadId);
 				});
-        	console.log("sendt fortsett senere");
-        }
-
-        $scope.soknadErUnderArbeid = function() {
-        	return $scope.soknadData.status == 'UNDER_ARBEID';
-        }
-
-        $scope.soknadErFerdig = function() {
-            var status =  $scope.soknadData.status;
-            if (status == 'FERDIG') {
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-        $scope.soknadErValidert = function() {
-            return $scope.soknadData.status == 'VALIDERT';
         }
         
-}]);
+    }])
+
+    .directive('navGjenoppta', ['$compile','data', function($compile, data) {
+        
+        var getTemplateUrl =  function(status) {
+            var templateUrl = '';
+            switch (status) {
+                    case "UNDER_ARBEID":
+                        templateUrl =  "../html/templates/gjenoppta/skjema-under-arbeid.html";
+                        break;
+                    case "FERDIG":
+                        templateUrl =  "../html/templates/gjenoppta/skjema-ferdig.html";
+                        break;
+                    case "VALIDERT":
+                        break;
+                    case "VALIDERT_VEDLEGG":
+                        break;
+                }
+                return templateUrl;
+        }
+
+
+        var linker = function(scope,element, attrs){
+            return getTemplateUrl(data.soknad.status);
+        }
+
+        return{ 
+            restrict: "A",
+            replace: true,
+            templateUrl: linker
+        }
+    }])
 
 
         
