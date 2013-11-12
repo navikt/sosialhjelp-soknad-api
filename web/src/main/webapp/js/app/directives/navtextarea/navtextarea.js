59b8d9a96f60462c3c646d9ee3cc2cfddceb93cb
Ingvild Indrebø <ingvild.indrebo@nav.no>
2013-11-12 16:15:49 +0100

    [MOD-5046] fikset bug med oppdatering av høyden, lagrer nå på for mange tegn og space og enter teller som tegn

--- web/src/main/webapp/js/app/directives/navtextarea/navtextarea.js
+++ web/src/main/webapp/js/app/directives/navtextarea/navtextarea.js
@@ -21,43 +21,26 @@ angular.module('nav.textarea', [])
                 scope.fokus = false;
                 scope.feil = false;
 
-
                 var tmpElementName = 'tmpName';
                 fiksNavn(element, scope.inputname, tmpElementName);
-
-                $(element).on('keyup', 'textarea', function (e) {
-                    $(this).css('height', '0px');
-                    $(this).height(this.scrollHeight);
-                });
-                $(element).find('textarea').keyup();
-
-                element.find('textarea').bind('focus', function () {
-                    scope.fokus = true;
-                    scope.$apply(attrs.onFocus);
-
-                    $(this).css('height', '0px');
-                    $(this).height(this.scrollHeight);
-
-                })
-                element.find('textarea').bind('blur', function () {
-                    scope.fokus = false;
-                    scope.$apply(attrs.onBlur)
-                    validerAntallTegn();
-                    var verdi = element.find('textarea').val().toString();
-
-                    if (scope.counter > -1) {
-                        scope.$emit("OPPDATER_OG_LAGRE", {key: element.find('textarea').attr('name'), value: verdi});
-                    }
-                })
-
-                scope.oppdaterTeller = function () {
-                    if (scope.model) {
-                        scope.counter = scope.maxlengde - scope.model.length;
+            },
+            templateUrl: '../js/app/directives/navtextarea/navtextareaTemplate.html'
+        };
+    }])
+    .directive('validateTextarea', [function() {
+        return {
+            require: 'ngModel',
+            link: function(scope, element, attrs, ctrl) {
+                scope.$watch(function() {
+                    return ctrl.$viewValue;
+                }, function() {
+                    if (ctrl.$viewValue != undefined) {
+                        element.css('height', '0px');
+                        element.height(element[0].scrollHeight);
+                        scope.counter = scope.maxlengde - ctrl.$viewValue.length;
                         validerAntallTegn();
-                    } else {
-                        scope.counter = scope.maxlengde;
                     }
-                }
+                });
 
                 function validerAntallTegn() {
                     if (scope.counter < 0) {
@@ -69,8 +52,18 @@ angular.module('nav.textarea', [])
                     }
                 }
 
-                validerAntallTegn();
-            },
-            templateUrl: '../js/app/directives/navtextarea/navtextareaTemplate.html'
-        };
+                element.bind('focus', function () {
+                    scope.fokus = true;
+                    scope.$apply(attrs.onFocus);
+                })
+
+                element.bind('blur', function () {
+                    scope.fokus = false;
+                    scope.$apply(attrs.onBlur)
+                    validerAntallTegn();
+                    var verdi = element.val().toString();
+                    scope.$emit("OPPDATER_OG_LAGRE", {key: element.attr('name'), value: verdi});
+                })
+            }
+        }
     }]);
