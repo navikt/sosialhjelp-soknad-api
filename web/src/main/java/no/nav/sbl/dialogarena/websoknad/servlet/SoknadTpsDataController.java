180281414dd40336faa199844537149f209a7eca
unknown <s134733@A34DUVW22273.devillo.no>
2013-11-12 09:16:53 +0100

    [Kaizen] minifiks

--- web/src/main/java/no/nav/sbl/dialogarena/websoknad/servlet/SoknadTpsDataController.java
+++ web/src/main/java/no/nav/sbl/dialogarena/websoknad/servlet/SoknadTpsDataController.java
@@ -1,22 +1,21 @@
 package no.nav.sbl.dialogarena.websoknad.servlet;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.inject.Inject;
-
 import no.nav.modig.core.context.SubjectHandler;
 import no.nav.sbl.dialogarena.kodeverk.Kodeverk;
 import no.nav.sbl.dialogarena.person.Person;
 import no.nav.sbl.dialogarena.person.PersonService;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.inject.Inject;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.Arrays.asList;
 
 @Controller
 @RequestMapping("/soknad")
@@ -38,13 +37,11 @@ public class SoknadTpsDataController {
     @ResponseBody()
     public Map<String, List<String>> hentLandkodeListe() {
         HashMap<String, List<String>> hashMap = new HashMap<String, List<String>>();
-        ArrayList<String> mockLand = new ArrayList<>();
-        
-        mockLand.add("Norge");
-        mockLand.add("Sverige");
-        mockLand.add("Danmark");
-        
-        
+        List<String> mockLand = asList(
+                "Norge",
+                "Sverige",
+                "Danmark"
+        );
         hashMap.put("result", mockLand);
     	return hashMap;
     }
