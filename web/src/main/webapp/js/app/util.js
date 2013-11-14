a8e4bce6e773df1a57edbfbc83332339d0eb6a80
Andreas Svendby <andreas.svendby@nav.no>
2013-11-14 10:28:43 +0100

    [MOD-5041]: Lagt til scrolling. Begynt å jobbe med å underline det som søket treffer på

--- web/src/main/webapp/js/app/util.js
+++ web/src/main/webapp/js/app/util.js
@@ -1,3 +1,24 @@
+if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function(elt) {
+        var len = this.length >>> 0;
+
+        var from = Number(arguments[1]) || 0;
+
+        from = (from < 0) ? Math.ceil(from) : Math.floor(from);
+
+        if (from < 0) {
+            from += len;
+        }
+
+        for(; from < len; from++) {
+            if (from in this && this[from] === elt) {
+                return from;
+            }
+        }
+        return -1;
+    }
+}
+
 if (!Array.prototype.last) {
     Array.prototype.last = function() {
         return this[this.length -1];
@@ -34,9 +55,7 @@ function scrollToElement(element) {
     var animationSpeed = 200;
     var offset = 100;
     var scrollPos = Math.max(element.offset().top - offset, 0);
-    $('body, html').animate({
-        scrollTop: scrollPos
-    }, animationSpeed);
+    $('body, html').scrollToPos(scrollPos, animationSpeed);
 }
 
 function fiksNavn(element, navn, tmpNavn) {
@@ -85,3 +104,14 @@ function stringContainsNotCaseSensitive(str, query) {
     var re = new RegExp(query, "i");
     return re.test(str);
 }
+
+(function($) {
+    $.fn.scrollToPos = function(position, speed) {
+        if (speed === undefined) {
+            speed = 'fast';
+        }
+        $(this).animate({
+            scrollTop: position
+        }, speed);
+    }
+})(jQuery);
