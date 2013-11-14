60da96d9622ef8f863eba2d6edb2276441da7cef
Hein Haraldsen <Hein.Haraldsen@nav.no>
2013-11-14 16:01:55 +0100

    Lagt på noen opsjoner etc.

--- web/src/main/webapp/js/app/controllers/dagpenger/utdanningController.js
+++ web/src/main/webapp/js/app/controllers/dagpenger/utdanningController.js
@@ -3,7 +3,11 @@ angular.module('nav.utdanning',[])
         $scope.navigering = {nesteside: 'ytelser'};
         $scope.sidedata = {navn: 'utdanning'};
 
-        $scope.validerUtdanning = function(form) {
+    var nokler = ['underUtdanningKveld', 'underUtdanningKveldPaabegynt', 'underUtdanningFulltid', 'underUtdanningKortvarig', 'underUtdanningNorsk', 'ingenYtelse' ];
+
+    $scope.validerUtdanning = function(form) {
+            var minstEnAvhuket = $scope.erCheckboxerAvhuket(nokler);
+            form.$setValidity("utdanning.minstEnAvhuket.feilmelding", minstEnAvhuket);
             $scope.validateForm(form.$invalid);
             $scope.runValidation();
         }
@@ -28,4 +32,15 @@ angular.module('nav.utdanning',[])
             }
             return false;
         }
+
+    $scope.erCheckboxerAvhuket = function(checkboxNokler) {
+        var minstEnAvhuket = false;
+        for(var i= 0; i < checkboxNokler.length; i++) {
+            var nokkel = checkboxNokler[i];
+            if ($scope.soknadData.fakta[nokkel] && checkTrue($scope.soknadData.fakta[nokkel].value)) {
+                minstEnAvhuket = true;
+            }
+        }
+        return minstEnAvhuket;
+    }
     }]);
