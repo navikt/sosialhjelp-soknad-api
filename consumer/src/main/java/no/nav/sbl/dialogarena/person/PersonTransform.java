bc932c2674607569c7c6058780cb4536f3f1a849
Hein Haraldsen <Hein.Haraldsen@nav.no>
2013-11-15 08:40:42 +0100

    PMD-feil:

--- consumer/src/main/java/no/nav/sbl/dialogarena/person/PersonTransform.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/person/PersonTransform.java
@@ -86,8 +86,7 @@ public class PersonTransform {
     private void addMidlertidigAdresse(long soknadId, List<Adresse> result, XMLMidlertidigPostadresse midlertidigPostadresse) {
         if (midlertidigPostadresse instanceof XMLMidlertidigPostadresseNorge) {
             XMLMidlertidigPostadresseNorge xmlMidlPostAdrNorge = (XMLMidlertidigPostadresseNorge) midlertidigPostadresse;
-            result.add(retrieveAdresse(soknadId, xmlMidlPostAdrNorge, xmlMidlPostAdrNorge.getStrukturertAdresse()));
-        }
+            result.add(retrieveAdresse(soknadId, xmlMidlPostAdrNorge, xmlMidlPostAdrNorge.getStrukturertAdresse()));}
         else if (midlertidigPostadresse instanceof XMLMidlertidigPostadresseUtland) {
             Adresse midlertidigAdresse = new Adresse(soknadId, MIDLERTIDIG_POSTADRESSE_UTLAND);
             setLandkodeOgGyldighetsDatoer(midlertidigAdresse, (XMLMidlertidigPostadresseUtland) midlertidigPostadresse);
@@ -181,7 +180,7 @@ public class PersonTransform {
         return adresselinjer;
     }
 
-    private void addIfNotNull(ArrayList<String> adresselinjer, String adresselinje) {
+    private void addIfNotNull(List<String> adresselinjer, String adresselinje) {
         if (adresselinje != null) {
             adresselinjer.add(adresselinje);
         }
