76ae8ef37e41e5c544c7c0f0bb94f605344cd87c
Kari B. Schjønsby <Kari.Bergh.Schjonsby@nav.no>
2013-11-18 15:47:51 +0100

    MOD-5048: XML-gen - endret metodenavn, ryddet

--- web/src/test/java/no/nav/sbl/dialogarena/print/XMLGeneratorTest.java
+++ web/src/test/java/no/nav/sbl/dialogarena/print/XMLGeneratorTest.java
@@ -1,53 +1,21 @@
 package no.nav.sbl.dialogarena.print;
 
-import org.junit.Before;
+import no.nav.sbl.dialogarena.print.helper.XMLTestData;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.w3c.dom.Document;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
 
 public class XMLGeneratorTest {
-    String resource;
-    String filePath = "c:/test/min.xml";
-    private String baseUrl;
 
-    @Before
-    public void setUp() throws Exception {
-        String path = getClass().getResource("/html/people.xsl").getPath();
-        baseUrl = getClass().getResource("/html/").getPath();
-        //resource = path.substring(1);
-        resource = "people.xsl";
-    }
-
+    @Ignore
     @Test
-    public void generateXMLFileDocument() throws Exception {
-        Document document = XMLGenerator.createDocument(resource);
-        XMLGenerator.createXMLFile(filePath, document);
+    public void generateHTMLFileDocument() throws Exception {
+        String xslPath = getClass().getResource("/html/people.xsl").getPath();
+        xslPath = xslPath.substring(1);
+        String filePath = "c:/test/min.xml";
+
+        Document document = XMLTestData.createDocument();
+        XMLGenerator.transformToHTML(filePath, document, xslPath);
     }
-
-    @Test
-    public void generateImageFromDocument() throws Exception {
-        Document document = XMLGenerator.createDocument(resource);
-        BufferedImage image = PDFGenerator.getBufferedImageFromDocument(document, baseUrl, 700, BufferedImage.TYPE_BYTE_BINARY);
-        saveToPng(image, "c:/test/minDocXmlPng.png");
-    }
-
-    @Test
-    public void generateImageFromXML() throws Exception {
-        BufferedImage bufferedImage = PDFGenerator.createBuf(filePath, 500);
-        saveToPng(bufferedImage, "c:/test/minXmlPng.png");
-
-        bufferedImage = PDFGenerator.renderHTMLToImage(filePath );
-        ImageIO.write(bufferedImage, "PNG", new File("c:/test/minXmlPng_style.png"));
-
-    }
-
-    private void saveToPng(BufferedImage bufferedImage, String pathname) throws IOException {
-        ImageIO.write(bufferedImage, "PNG", new File(pathname));
-    }
-
 }
