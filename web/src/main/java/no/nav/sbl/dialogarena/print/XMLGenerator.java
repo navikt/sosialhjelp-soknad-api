7640a21555a37081e3f09cd6493f3c1d56d5f842
Kari B. Schjønsby <Kari.Bergh.Schjonsby@nav.no>
2013-11-18 15:47:51 +0100

    MOD-5048: XML-gen - endret metodenavn, ryddet

--- web/src/main/java/no/nav/sbl/dialogarena/print/XMLGenerator.java
+++ web/src/main/java/no/nav/sbl/dialogarena/print/XMLGenerator.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.ProcessingInstruction;
+import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -15,53 +16,17 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 
 public class XMLGenerator {
     private static final Logger LOG = LoggerFactory.getLogger(XMLGenerator.class);
 
-    public static Document createDocument(String cssFilePath) {
-        Document doc = null;
-        try {
-            doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
-            Element rootElement = doc.createElement("people");
-            doc.appendChild(rootElement);
-            if(cssFilePath != null && !cssFilePath.isEmpty()) {
-                doc.setXmlStandalone(true);
-                ProcessingInstruction pi = doc.createProcessingInstruction("xml-stylesheet", "type=\"text/css\" href=\"" + cssFilePath + "\"");
-                doc.insertBefore(pi, rootElement);
-                System.out.println("Add CSS..." + cssFilePath);
-                System.out.println("pi.getNodeValue() = " + pi.getNodeValue());
-            }
 
-            createPerson(doc, rootElement, "1", "Harald Olsen", "32");
-            createPerson(doc, rootElement, "2", "Trine Person", "30");
-            createPerson(doc, rootElement, "3", "Kjell Hansen", "2");
-
-        } catch (ParserConfigurationException e) {
-            LOG.info("Kunne ikke parse", e);
-        }
-
-        return doc;
-    }
-
-    public static void insertCSS() {
-
-    }
-
-    public static void createXMLFile(String filePath, Document doc) {
-         try {
-             Transformer transformer = TransformerFactory.newInstance().newTransformer();
-             DOMSource domSource = new DOMSource(doc);
-             StreamResult result = new StreamResult(new File(filePath));
-             transformer.transform(domSource, result);
-        } catch (TransformerException e) {
-             LOG.info("Kunne ikke transformere", e);
-        }
-    }
-
-    public static void createHTMLFile(String outputHTML, Document doc, String xsl) {
+    public static void transformToHTML(String outputHTML, Document doc, String xsl) {
         try {
             Transformer transformer = getTransformer(xsl);
             DOMSource domSource = new DOMSource(doc);
@@ -72,51 +37,29 @@ public class XMLGenerator {
         }
     }
 
-    public static void createXMLFile(String xslPath, String inputFile, String outputPath) {
+    public static String transformToHTML(Document doc, String xsl) throws UnsupportedEncodingException {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
         try {
-            Transformer transformer = getTransformer(xslPath);
-
-            StreamSource xml = new StreamSource(new File(inputFile));
-            StreamResult result = new StreamResult(new File(outputPath));
-            transformer.transform(xml, result);
+            Transformer transformer = getTransformer(xsl);
+            DOMSource domSource = new DOMSource(doc);
+            StreamResult result = new StreamResult(out);
+            transformer.transform(domSource, result);
         } catch (TransformerException e) {
             LOG.info("Kunne ikke transformere", e);
         }
+        String str = out.toString("UTF-8");
+        try {
+            out.close();
+        } catch (IOException e) {
+            LOG.info("Kunne ikke lukke stream", e);
+        }
+        return str;
     }
 
     private static Transformer getTransformer(String xslPath) throws TransformerConfigurationException {
         TransformerFactory factory = TransformerFactory.newInstance();
-        StreamSource xsl = new StreamSource(new File(xslPath));
-        return factory.newTransformer(xsl);
+        StreamSource domSource = new StreamSource(new File(xslPath));
+        return factory.newTransformer(domSource);
     }
 
-
-    public static void addXSLToDocument(String xslPath, String outputFile, Document doc) {
-        try {
-            Transformer transformer = getTransformer(xslPath);
-
-            doc.setXmlStandalone(true);
-            DOMSource domSource = new DOMSource(doc);
-
-            StreamResult result = new StreamResult(new File(outputFile));
-            transformer.transform(domSource, result);
-
-        } catch (TransformerException e) {
-            LOG.info("Kunne ikke transformere", e);
-        }
-    }
-
-    private static void createPerson(Document doc, Element rootElement, String idValue, String nameValue, String ageValue) {
-        Element person = doc.createElement("person");
-        person.setAttribute("id", idValue);
-        rootElement.appendChild(person);
-
-        Element name=  doc.createElement("name");
-        name.appendChild(doc.createTextNode(nameValue));
-        person.appendChild(name);
-
-        Element age=  doc.createElement("age");
-        age.appendChild(doc.createTextNode(ageValue));
-        person.appendChild(age);
-    }
 }
