5379d2bf4585f6436b6e35c905b4af209edaeb20
Ketil Jensen <ketil.jensen@nav.no>
2013-11-19 16:24:30 +0100

    TG-henvendelse-integrasjon

--- business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/service/SoknadService.java
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/service/SoknadService.java
@@ -71,18 +71,7 @@ public class SoknadService implements SendSoknadService {
 
 
     public Long startSoknad(String navSoknadId) {
-        logger.debug("Starter ny søknad");
-        //TODO: Sende et signal til Henvendelse om at søknaden er startet
         String behandlingsId = UUID.randomUUID().toString();
-        logger.debug("Start søknad");
-
-        //TODO-KJ: Denne operasjonen er ikke klar enda.Vil kommenteres inn når den er
-//        try {
-//            behandlingsId = sendSoknadService.startBehandling(SubjectHandler.getSubjectHandler().getUid());
-//        } catch (SOAPFaultException e) {
-//            logger.error("Feil ved oppretting av søknad med ID", navSoknadId, e);
-//            throw new ApplicationException("Kunne ikke opprette ny søknad", e);
-//        }
 
         WebSoknad soknad = WebSoknad.startSoknad().
                 medBehandlingId(behandlingsId).
