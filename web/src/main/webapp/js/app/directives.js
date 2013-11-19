385a479ca5ce3c3a88b3f27e79df9c706167737e
Andreas Svendby <andreas.svendby@nav.no>
2013-11-19 17:20:56 +0100

    [MOD-5041]: Lagt til hjelpetekst og stylet masse.

--- web/src/main/webapp/js/app/directives.js
+++ web/src/main/webapp/js/app/directives.js
@@ -5,7 +5,7 @@
  * Description
  */
 
-angular.module('app.directives', ['app.services', 'nav.booleanradio', 'nav.cmstekster', 'nav.input', 'nav.feilmeldinger', 'nav.sporsmalferdig', 'nav.markup', 'nav.stegindikator', 'nav.textarea', 'nav.fremdriftsindikator', 'nav.validering', 'nav.sistlagret', 'nav.select'])
+angular.module('app.directives', ['app.services', 'nav.booleanradio', 'nav.cmstekster', 'nav.input', 'nav.feilmeldinger', 'nav.sporsmalferdig', 'nav.markup', 'nav.stegindikator', 'nav.textarea', 'nav.fremdriftsindikator', 'nav.validering', 'nav.sistlagret', 'nav.select', 'nav.hjelpetekst'])
     /*Hva med casene 1-242 osv? */
 
     .directive('landskodevalidering', function () {
