6c4912549d6d6c029d7c8af94674c387c018cedc
Kari B. Schjønsby <Kari.Bergh.Schjonsby@nav.no>
2013-11-20 15:41:30 +0100

    MOD-5048: Lager pdf fra riktig testdata(WebSoknad-json) med mal (WebSoknadHtml.hbs)

--- web/src/main/java/no/nav/sbl/dialogarena/print/HandleBarRunner.java
+++ web/src/main/java/no/nav/sbl/dialogarena/print/HandleBarRunner.java
@@ -2,6 +2,8 @@ package no.nav.sbl.dialogarena.print;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.jknack.handlebars.Handlebars;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -9,20 +11,37 @@ import java.util.Map;
 
 
 public class HandleBarRunner {
+    private static final Logger LOG = LoggerFactory.getLogger(HandleBarRunner.class);
 
     public static String getHTML(String jsonSource, String html) {
         String out = "";
         try {
             Map<String, Object> map = new ObjectMapper().readValue(jsonSource, HashMap.class);
-            out = apply(html, map);
+            out = applyInline(html, map);
         } catch (IOException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            LOG.info("Kunne ikke legge json inn i mal", e);
         }
         return out;
     }
 
-    public static String apply(String input, Map<String, Object> hash) throws IOException {
+    public static String applyInline(String input, Map<String, Object> hash) throws IOException {
         return new Handlebars().compileInline(input).apply(hash);
     }
+
+    public static String applyTemplate(String jsonSource, String htmlFile) {
+        String out = "";
+        try {
+            Map<String, Object> map = new ObjectMapper().readValue(jsonSource, HashMap.class);
+            out = apply(htmlFile, map);
+        } catch (IOException e) {
+            LOG.info("Kunne ikke legge json inn i mal", e);
+        }
+        return out;
+    }
+
+    public static String apply(String htmlFile, Map<String, Object> hash) throws IOException {
+        return new Handlebars().compile(htmlFile).apply(hash);
+    }
+
 
 }
