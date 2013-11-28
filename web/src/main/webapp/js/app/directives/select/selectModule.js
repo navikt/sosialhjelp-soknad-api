fb7413cfca1fe37b150d5427515274f49775246e
Andreas Svendby <andreas.svendby@nav.no>
2013-11-28 11:51:27 +0100

    [MOD-5041]: Fikset feilmeldinger for datepicker og selectboks

--- web/src/main/webapp/js/app/directives/select/selectModule.js
+++ web/src/main/webapp/js/app/directives/select/selectModule.js
@@ -27,7 +27,7 @@ angular.module('nav.select', ['ngSanitize'])
                 requiredFeilmelding: '@',
                 ugyldigFeilmelding: '@',
                 label: '@',
-                ngRequired: '=',
+                erRequired: '=',
                 ngModel: '='
             },
             replace: true,
@@ -171,7 +171,7 @@ angular.module('nav.select', ['ngSanitize'])
                 }
 
                 scope.harRequiredFeil = function() {
-                    return scope.ngRequired && !scope.inputVerdi && !scope.listeErApen && !input.is(':focus');
+                    return scope.erRequired && !scope.inputVerdi && !scope.listeErApen && !input.is(':focus');
                 }
 
                 scope.inneholderIkkeSkrevetTekst = function() {
