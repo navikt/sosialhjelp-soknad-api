547ff536c4702d625a72f7ddaf9f68df78b3602c
Ingvild Indrebø <ingvild.indrebo@nav.no>
2013-11-28 10:35:08 +0100

    [MOD-5040Småfiks] lagt til en sjekk for om fakta.epost er definert, og flyttet joda time til avhengigheter for compile i pomfilen

--- web/src/main/java/no/nav/sbl/dialogarena/websoknad/util/DateTimeSerializer.java
+++ web/src/main/java/no/nav/sbl/dialogarena/websoknad/util/DateTimeSerializer.java
@@ -2,12 +2,11 @@ package no.nav.sbl.dialogarena.websoknad.util;
 
 import java.lang.reflect.Type;
 
-import org.joda.time.DateTime;
-
 import com.google.gson.JsonElement;
 import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
+import org.joda.time.DateTime;
 
 public class DateTimeSerializer implements JsonSerializer<DateTime> {
 
