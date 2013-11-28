9d85349b905f1d4cba12d4a50f4c5850818630a6
Andreas Svendby <andreas.svendby@nav.no>
2013-11-28 16:43:55 +0100

    [TG]: Fjernet ubrukte direktiv

--- web/src/main/webapp/js/app/directives.js
+++ web/src/main/webapp/js/app/directives.js
@@ -1,50 +1,5 @@
 'use strict';
-/**
- *  Module
- *
- * Description
- */
-
 angular.module('app.directives', ['app.services', 'nav.booleanradio', 'nav.cmstekster', 'nav.input', 'nav.feilmeldinger', 'nav.sporsmalferdig', 'nav.markup', 'nav.stegindikator', 'nav.textarea', 'nav.fremdriftsindikator', 'nav.validering', 'nav.sistlagret', 'nav.select', 'nav.hjelpetekst', 'nav.datepicker'])
-    /*Hva med casene 1-242 osv? */
-
-    .directive('landskodevalidering', function () {
-        return {
-            require: 'ngModel',
-            link: function (scope, elm, attrs, ctrl) {
-                ctrl.$parsers.unshift(function (viewValue) {
-                    var INTEGER_REGEX = /^\-?\d*$/;
-                    var kode = viewValue.slice(1, viewValue.length);
-                    if (viewValue.charAt(0) === '+' && INTEGER_REGEX.test(kode)) {
-                        ctrl.$setValidity('feil', true);
-                    } else {
-                        ctrl.$setValidity('feil', false);
-                    }
-                });
-            }
-        };
-
-    })
-
-    .directive('mobilnummer', function () {
-        return {
-            require: 'ngModel',
-            link: function (scope, elm, attrs, ctrl) {
-                ctrl.$parsers.unshift(function (viewValue) {
-                    var INTEGER_REGEX = /^\-?\d*$/;
-                    if (INTEGER_REGEX.test(viewValue) && viewValue.length === 8) {
-                        ctrl.$setValidity('feil', true);
-                        return parseFloat(viewValue.replace(',', '.'));
-                    } else {
-                        ctrl.$setValidity('feil', false);
-                        return undefined;
-                    }
-                });
-            }
-        };
-
-    })
-
     .directive('prosent', function () {
         return {
             replace: true,
