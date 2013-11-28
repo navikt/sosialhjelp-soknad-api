6cd3676c52a8031746ec2e34b12deb13759f015e
Ketil Velle <ketil.s.velle@nav.no>
2013-11-28 13:00:41 +0100

    MOD-4859 ønsket barnestruktur.. maa jobbes med

--- web/src/main/webapp/js/app/controllers/dagpenger/barnetilleggController.js
+++ web/src/main/webapp/js/app/controllers/dagpenger/barnetilleggController.js
@@ -7,6 +7,40 @@ angular.module('nav.barnetillegg',[])
             barnetillegg: false
         }
 
+        //ønskelig struktur
+        /*var myJson= {
+            barn: 
+            {
+                id: 18706,
+                soknadId: 275,
+                vedleggId: 0,
+                key: "barn",
+                type: "BRUKERREGISTRERT",
+                value: {
+                    sammensattnavn: "Jens August Aker Hansen",
+                    fnr: ***REMOVED***,
+                    alder: 7,
+                    barnetillegg: false
+                }
+            },
+            barn:
+            {
+                id: 18707,
+                soknadId: 275,
+                vedleggId: 0,
+                key: "barn",
+                type: "BRUKERREGISTRERT",
+                value: {
+                    sammensattnavn: "Lisa Kristin Normann Olsen",
+                    fnr: 12129623623,
+                    alder: 11,
+                    barnetillegg: false
+                }
+            }
+        };
+        $scope.barn = angular.fromJson(myJson);*/
+        
+
         $scope.validerBarnetillegg = function(form) {
             $scope.validateForm(form.$invalid);
             $scope.runValidation();
