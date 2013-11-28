33673208f36f771427c773dec09c3f4b386b388b
Andreas Svendby <andreas.svendby@nav.no>
2013-11-28 16:46:05 +0100

    [TG]: Flyttet prosent-direktiv ut til common

--- web/src/main/webapp/js/app/directives.js
+++ web/src/main/webapp/js/app/directives.js
@@ -1,21 +1,2 @@
 'use strict';
-angular.module('app.directives', ['app.services', 'nav.booleanradio', 'nav.cmstekster', 'nav.input', 'nav.feilmeldinger', 'nav.sporsmalferdig', 'nav.markup', 'nav.stegindikator', 'nav.textarea', 'nav.fremdriftsindikator', 'nav.validering', 'nav.sistlagret', 'nav.select', 'nav.hjelpetekst', 'nav.datepicker'])
-    .directive('prosent', function () {
-        return {
-            replace: true,
-            require: 'ngModel',
-
-            link: function (scope, elm, attrs, ctrl) {
-                ctrl.$parsers.unshift(function (viewValue) {
-                    var INTEGER_REGEX = /^\-?\d*$/;
-                    if (INTEGER_REGEX.test(viewValue) && viewValue <= 100 && viewValue >= 0) {
-                        ctrl.$setValidity('prosent', true);
-                        return viewValue;
-                    } else {
-                        ctrl.$setValidity('prosent', false);
-                        return undefined;
-                    }
-                });
-            }
-        };
-    });
+angular.module('app.directives', ['app.services', 'nav.booleanradio', 'nav.cmstekster', 'nav.input', 'nav.feilmeldinger', 'nav.sporsmalferdig', 'nav.markup', 'nav.stegindikator', 'nav.textarea', 'nav.fremdriftsindikator', 'nav.validering', 'nav.sistlagret', 'nav.select', 'nav.hjelpetekst', 'nav.datepicker', 'nav.prosent']);
