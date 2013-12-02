04771ec23f1c826c023e654ede0b80a09c8aa380
Andreas Svendby <andreas.svendby@nav.no>
2013-12-02 15:27:19 +0100

    [MOD-5041]: Fikset styling og markup i redusert arbeidstid

--- web/src/main/webapp/js/common/directives/datepicker/datepickerModule.js
+++ web/src/main/webapp/js/common/directives/datepicker/datepickerModule.js
@@ -50,6 +50,7 @@ angular.module('nav.datepicker', [])
                 tilDato: '=',
                 fraDato: '=',
                 tilDatoFeil: '=',
+                endret: '&',
                 label: '@',
                 requiredErrorMessage: '@'
             },
@@ -108,6 +109,12 @@ angular.module('nav.datepicker', [])
                 scope.harFeil = function() {
                     return scope.harRequiredFeil() || scope.harFormatteringsFeil() || scope.harTilDatoFeil();
                 }
+
+                scope.$watch('ngModel', function(newVal, oldVal) {
+                    if (newVal != oldVal && scope.endret) {
+                        scope.endret();
+                    }
+                });
 
                 function datepickerOptions() {
                     return angular.extend({}, datepickerConfig, scope.options);
