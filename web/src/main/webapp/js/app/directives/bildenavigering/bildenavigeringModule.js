27d7947a71f20f2a5810889bc2fac315d7ffacfd
Eirik L. Wang <eirik.wang@nav.no>
2013-12-05 11:12:12 +0100

    mod-4665: Fikset forhåndsvisning i stort og lite format. lagt på caching headere av preview bilder.

--- web/src/main/webapp/js/app/directives/bildenavigering/bildenavigeringModule.js
+++ web/src/main/webapp/js/app/directives/bildenavigering/bildenavigeringModule.js
@@ -7,9 +7,21 @@ angular.module('nav.bildenavigering', [])
                 vedlegg: '='
             },
             link: function (scope, element) {
+
+                var bilder = [];
                 scope.side = 0;
+                scope.bilder = bilder;
+                scope.range = function (til) {
+                    var r = [];
+                    for (var i = 0; i < til; i++) r.push(i);
+                    return r;
+                }
+                scope.sideErSynlig = function (index) {
+                    console.log("synlig" + index + ":" + scope.side + ": " + (index == scope.side))
+                    return index == scope.side;
+                }
                 scope.naviger = function (retning) {
-                    element.find('img').attr('src', '');
+                    //element.find('img').attr('src', '');
                     scope.side = scope.side + retning;
                     if (scope.side < 0) {
                         scope.side = scope.vedlegg.antallSider - 1;
@@ -17,7 +29,17 @@ angular.module('nav.bildenavigering', [])
                         scope.side = 0;
                     }
                 }
+
             },
-            templateUrl: '../js/app/directives/bildenavigering/bildenavigeringTemplate.html'
+            templateUrl: function (element, attr) {
+                console.log('\'' + attr.selvstendig + '\'' + (attr.selvstendig)?'ja':'nei')
+                if (attr.selvstendig) {
+                    console.log('stor')
+                    return '../js/app/directives/bildenavigering/bildenavigeringTemplateStor.html';
+                } else {
+                    console.log('liten')
+                    return '../js/app/directives/bildenavigering/bildenavigeringTemplateLiten.html';
+                }
+            }
         }
     }]);
