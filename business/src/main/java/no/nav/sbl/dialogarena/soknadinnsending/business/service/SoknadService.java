091c3dfa815c711e98ea9d50d1d5a9b4e8e2badb
Ketil Velle <ketil.s.velle@nav.no>
2013-12-06 10:35:42 +0100

    MOD-4859 Lagrer naa barnetilegg for systemfaktum, men er noe problem med rekkefolgen av checkboxene ved refresh

--- business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/service/SoknadService.java
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/service/SoknadService.java
@@ -2,17 +2,6 @@ package no.nav.sbl.dialogarena.soknadinnsending.business.service;
 
 import static no.nav.modig.core.context.SubjectHandler.getSubjectHandler;
 
-import java.awt.Dimension;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
-import javax.inject.Inject;
-import javax.inject.Named;
-
 import no.nav.sbl.dialogarena.detect.IsImage;
 import no.nav.sbl.dialogarena.detect.IsPdf;
 import no.nav.sbl.dialogarena.pdf.ConvertToPng;
@@ -30,137 +19,157 @@ import org.apache.commons.io.IOUtils;
 import org.joda.time.DateTime;
 import org.springframework.stereotype.Component;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import java.awt.Dimension;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Component
 public class SoknadService implements SendSoknadService {
 
-    private static final String BRUKERREGISTRERT_FAKTUM = "BRUKERREGISTRERT";
-    private static final String SYSTEMREGISTRERT_FAKTUM = "SYSTEMREGISTRERT";
+	private static final String BRUKERREGISTRERT_FAKTUM = "BRUKERREGISTRERT";
+	private static final String SYSTEMREGISTRERT_FAKTUM = "SYSTEMREGISTRERT";
 
+	@Inject
+	@Named("soknadInnsendingRepository")
+	private SoknadRepository repository;
+	@Inject
+	@Named("vedleggRepository")
+	private VedleggRepository vedleggRepository;
 
-    @Inject
-    @Named("soknadInnsendingRepository")
-    private SoknadRepository repository;
-    @Inject
-    @Named("vedleggRepository")
-    private VedleggRepository vedleggRepository;
+	@Override
+	public WebSoknad hentSoknad(long soknadId) {
+		return repository.hentSoknadMedData(soknadId);
+	}
 
-    @Override
-    public WebSoknad hentSoknad(long soknadId) {
-        return repository.hentSoknadMedData(soknadId);
-    }
+	@Override
+	public Faktum lagreSoknadsFelt(Long soknadId, Faktum faktum) {
+		Long faktumId = repository.lagreFaktum(soknadId, new Faktum(soknadId,
+				faktum.getFaktumId(), faktum.getKey(), faktum.getValue(),
+				BRUKERREGISTRERT_FAKTUM));
+		return repository.hentFaktum(soknadId, faktumId);
+	}
 
-    @Override
-    public Faktum lagreSoknadsFelt(Long soknadId, Faktum faktum) {
-        Long faktumId = repository.lagreFaktum(soknadId, new Faktum(soknadId, faktum.getFaktumId(), faktum.getKey(), faktum.getValue(), BRUKERREGISTRERT_FAKTUM));
-        return repository.hentFaktum(soknadId, faktumId);
-    }
+	@Override
+	public Faktum lagreSystemSoknadsFelt(Long soknadId, String key, String value) {
+		Faktum faktum = repository.hentSystemFaktum(soknadId, key,
+				SYSTEMREGISTRERT_FAKTUM);
+		Long faktumId = repository.lagreFaktum(soknadId, new Faktum(soknadId,
+				faktum.getFaktumId(), key, value, SYSTEMREGISTRERT_FAKTUM));
+		return repository.hentFaktum(soknadId, faktumId);
+	}
 
-    @Override
-    public Faktum lagreSystemSoknadsFelt(Long soknadId, String key, String value) {
-    	Faktum faktum = repository.hentSystemFaktum(soknadId, key, SYSTEMREGISTRERT_FAKTUM);
-    	Long faktumId = repository.lagreFaktum(soknadId, new Faktum(soknadId, faktum.getFaktumId(), key, value, SYSTEMREGISTRERT_FAKTUM));
-    	return repository.hentFaktum(soknadId, faktumId);
-    }
+	@Override
+	public void sendSoknad(long soknadId) {
+		repository.avslutt(new WebSoknad().medId(soknadId));
+	}
 
+	@Override
+	public List<Long> hentMineSoknader(String aktorId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    @Override
-    public void sendSoknad(long soknadId) {
-        repository.avslutt(new WebSoknad().medId(soknadId));
-    }
+	@Override
+	public void avbrytSoknad(Long soknadId) {
+		repository.avbryt(soknadId);
+	}
 
-    @Override
-    public List<Long> hentMineSoknader(String aktorId) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	public Long startSoknad(String navSoknadId) {
+		String behandlingsId = UUID.randomUUID().toString();
 
-    @Override
-    public void avbrytSoknad(Long soknadId) {
-        repository.avbryt(soknadId);
-    }
+		WebSoknad soknad = WebSoknad.startSoknad()
+				.medBehandlingId(behandlingsId).medGosysId(navSoknadId)
+				.medAktorId(getSubjectHandler().getUid())
+				.opprettetDato(DateTime.now());
+		return repository.opprettSoknad(soknad);
+	}
 
-    public Long startSoknad(String navSoknadId) {
-        String behandlingsId = UUID.randomUUID().toString();
+	public static final String PDF_PDFA = "-dNOPAUSE -dBATCH -dSAFER -dPDFA -dNOGA -sDEVICE=pdfwrite -sOutputFile=%stdout%  -q -c \"30000000 setvmthreshold\" -_ -c quit";
+	private static final String IMAGE_RESIZE = "- -units PixelsPerInch -density 150 -quality 50 -resize 1240x1754 jpeg:-";
 
-        WebSoknad soknad = WebSoknad.startSoknad().
-                medBehandlingId(behandlingsId).
-                medGosysId(navSoknadId).
-                medAktorId(getSubjectHandler().getUid()).
-                opprettetDato(DateTime.now());
-        return repository.opprettSoknad(soknad);
-    }
+	private static final String IMAGE_PDFA = "-  pdfa:-";
 
-    public static final String PDF_PDFA = "-dNOPAUSE -dBATCH -dSAFER -dPDFA -dNOGA -sDEVICE=pdfwrite -sOutputFile=%stdout%  -q -c \"30000000 setvmthreshold\" -_ -c quit";
-    private static final String IMAGE_RESIZE = "- -units PixelsPerInch -density 150 -quality 50 -resize 1240x1754 jpeg:-";
+	public Long lagreVedlegg(Vedlegg vedlegg, InputStream inputStream) {
+		try {
+			byte[] bytes = IOUtils.toByteArray(inputStream);
+			if (new IsImage().evaluate(bytes)) {
+				if (ScriptRunner.IM_EXISTS) {
+					bytes = new ScriptRunner(ScriptRunner.Type.IM,
+							IMAGE_RESIZE, null, new ByteArrayInputStream(bytes))
+							.call();
+					bytes = new ScriptRunner(ScriptRunner.Type.IM, IMAGE_PDFA,
+							null, new ByteArrayInputStream(bytes)).call();
+				} else {
+					bytes = new ImageToPdf().transform(bytes);
+				}
+			} else if (new IsPdf().evaluate(bytes)) {
+				if (ScriptRunner.GS_EXISTS) {
+					bytes = new ScriptRunner(ScriptRunner.Type.GS, PDF_PDFA,
+							null, new ByteArrayInputStream(bytes)).call();
+				}
+			}
+			bytes = new PdfWatermarker().applyOn(bytes, "TEST-IDENT MOCK");
+			return vedleggRepository.lagreVedlegg(vedlegg, bytes);
+		} catch (Exception e) {
+			throw new RuntimeException("Kunne ikke lagre vedlegg: " + e, e);
+		}
+	}
 
-    private static final String IMAGE_PDFA = "-  pdfa:-";
+	public List<Vedlegg> hentVedleggForFaktum(Long soknadId, Long faktumId) {
+		return vedleggRepository.hentVedleggForFaktum(soknadId, faktumId);
+	}
 
+	public Vedlegg hentVedlegg(Long soknadId, Long vedleggId, boolean medInnhold) {
+		if (medInnhold) {
+			return vedleggRepository.hentVedleggMedInnhold(soknadId, vedleggId);
+		} else {
+			return vedleggRepository.hentVedlegg(soknadId, vedleggId);
+		}
+	}
 
-    public Long lagreVedlegg(Vedlegg vedlegg, InputStream inputStream) {
-        try {
-            byte[] bytes = IOUtils.toByteArray(inputStream);
-            if (new IsImage().evaluate(bytes)) {
-                if (ScriptRunner.IM_EXISTS) {
-                    bytes = new ScriptRunner(ScriptRunner.Type.IM, IMAGE_RESIZE, null, new ByteArrayInputStream(bytes)).call();
-                    bytes = new ScriptRunner(ScriptRunner.Type.IM, IMAGE_PDFA, null, new ByteArrayInputStream(bytes)).call();
-                } else {
-                    bytes = new ImageToPdf().transform(bytes);
-                }
-            } else if (new IsPdf().evaluate(bytes)) {
-                if (ScriptRunner.GS_EXISTS) {
-                    bytes = new ScriptRunner(ScriptRunner.Type.GS, PDF_PDFA, null, new ByteArrayInputStream(bytes)).call();
-                }
-            }
-            bytes = new PdfWatermarker().applyOn(bytes, "TEST-IDENT MOCK");
-            return vedleggRepository.lagreVedlegg(vedlegg, bytes);
-        } catch (Exception e) {
-            throw new RuntimeException("Kunne ikke lagre vedlegg: " + e, e);
-        }
-    }
+	public void slettVedlegg(Long soknadId, Long vedleggId) {
+		vedleggRepository.slettVedlegg(soknadId, vedleggId);
+	}
 
-    public List<Vedlegg> hentVedleggForFaktum(Long soknadId, Long faktumId) {
-        return vedleggRepository.hentVedleggForFaktum(soknadId, faktumId);
-    }
+	public byte[] lagForhandsvisning(Long soknadId, Long vedleggId) {
+		try {
+			return new ConvertToPng(new Dimension(600, 800),
+					ImageScaler.ScaleMode.SCALE_TO_FIT_INSIDE_BOX)
+					.transform(IOUtils.toByteArray(vedleggRepository
+							.hentVedleggStream(soknadId, vedleggId)));
+		} catch (IOException e) {
+			throw new RuntimeException("Kunne ikke generere thumbnail " + e, e);
+		}
+	}
 
-    public Vedlegg hentVedlegg(Long soknadId, Long vedleggId, boolean medInnhold) {
-        if (medInnhold) {
-            return vedleggRepository.hentVedleggMedInnhold(soknadId, vedleggId);
-        } else {
-            return vedleggRepository.hentVedlegg(soknadId, vedleggId);
-        }
-    }
+	public Long genererVedleggFaktum(Long soknadId, Long faktumId) {
+		List<Vedlegg> vedleggs = vedleggRepository.hentVedleggForFaktum(
+				soknadId, faktumId);
+		List<byte[]> bytes = new ArrayList<byte[]>();
+		for (Vedlegg vedlegg : vedleggs) {
+			InputStream inputStream = vedleggRepository.hentVedleggStream(
+					soknadId, vedlegg.getId());
+			try {
+				bytes.add(IOUtils.toByteArray(inputStream));
+			} catch (IOException e) {
+				throw new RuntimeException("Kunne ikke merge filer", e);
+			}
 
-    public void slettVedlegg(Long soknadId, Long vedleggId) {
-        vedleggRepository.slettVedlegg(soknadId, vedleggId);
-    }
-
-    public byte[] lagForhandsvisning(Long soknadId, Long vedleggId) {
-        try {
-            return new ConvertToPng(new Dimension(600, 800), ImageScaler.ScaleMode.SCALE_TO_FIT_INSIDE_BOX)
-                    .transform(IOUtils.toByteArray(vedleggRepository.hentVedleggStream(soknadId, vedleggId)));
-        } catch (IOException e) {
-            throw new RuntimeException("Kunne ikke generere thumbnail " + e, e);
-        }
-    }
-
-    public Long genererVedleggFaktum(Long soknadId, Long faktumId) {
-        List<Vedlegg> vedleggs = vedleggRepository.hentVedleggForFaktum(soknadId, faktumId);
-        List<byte[]> bytes = new ArrayList<byte[]>();
-        for (Vedlegg vedlegg : vedleggs) {
-            InputStream inputStream = vedleggRepository.hentVedleggStream(soknadId, vedlegg.getId());
-            try {
-                bytes.add(IOUtils.toByteArray(inputStream));
-            } catch (IOException e) {
-                throw new RuntimeException("Kunne ikke merge filer", e);
-            }
-
-        }
-        byte[] doc = new PdfMerger().transform(bytes);
-        Vedlegg vedlegg = new Vedlegg(null, soknadId, faktumId, "faktum.pdf", Long.valueOf(doc.length), doc);
-        vedleggRepository.slettVedleggForFaktum(soknadId, faktumId);
-        Long opplastetDokument = vedleggRepository.lagreVedlegg(vedlegg, doc);
-        vedleggRepository.knyttVedleggTilFaktum(soknadId, faktumId, opplastetDokument);
-        return opplastetDokument;
-    }
+		}
+		byte[] doc = new PdfMerger().transform(bytes);
+		Vedlegg vedlegg = new Vedlegg(null, soknadId, faktumId, "faktum.pdf",
+				Long.valueOf(doc.length), doc);
+		vedleggRepository.slettVedleggForFaktum(soknadId, faktumId);
+		Long opplastetDokument = vedleggRepository.lagreVedlegg(vedlegg, doc);
+		vedleggRepository.knyttVedleggTilFaktum(soknadId, faktumId,
+				opplastetDokument);
+		return opplastetDokument;
+	}
 }
