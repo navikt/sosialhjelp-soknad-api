8c7c27ff00276d1d576d985e94428d904b75ff90
Ketil Velle <ketil.s.velle@nav.no>
2013-12-06 12:19:59 +0100

    MOD-4859 Lagring av barnetilegg for Systemfaktum-barn paa plass. Mangler naa oppsummeringsmodus, samt legge til nye barn

--- web/src/main/webapp/js/app/controllers/dagpenger/barnetilleggController.js
+++ web/src/main/webapp/js/app/controllers/dagpenger/barnetilleggController.js
@@ -5,62 +5,33 @@ angular.module('nav.barnetillegg',[])
                 angular.forEach($scope.soknadData.fakta.barn.valuelist, function(value) { 
                     $scope.barn.push(angular.fromJson(value));
                 });
-                
             }
 
         if ($scope.soknadData.fakta.barnetillegg) {
-            $scope.barnetillegg = [];
+            $scope.barnetillegg = {};
             angular.forEach($scope.soknadData.fakta.barnetillegg.valuelist, function(value) { 
-                $scope.barnetillegg.push(angular.fromJson(value).valgt);
-
+                $scope.barnetillegg[angular.fromJson(value).fnr] = angular.fromJson(value).valgt;
             });
-            console.log("har no greier her da vettu");
         } else {
             $scope.soknadData.fakta.barnetillegg = {};
-             $scope.barnetillegg = [];
+             $scope.barnetillegg = {};
         }
-        /*$scope.barn =
-        {
-            id: 18706,
-            soknadId: 275,
-            key: "barn",
-            type: "BRUKERREGISTRERT",
-            valuelist: [
-                {
-                    id: 18707,
-                    vedleggId: 0,
-                    sammensattnavn: "Jens August Aker Hansen",
-                    fnr: ***REMOVED***,
-                    kjonn: "gutt",
-                    alder: 7,
-                    barnetillegg: false
-                },
-                {
-                    id: 18708,
-                    vedleggId: 0,
-                    sammensattnavn: "Lisa Kristin Normann Olsen",
-                    fnr: 12129623623,
-                    kjonn: "jente",
-                    alder: 11,
-                    barnetillegg: false
-                }
-            ]
-        }*/
         
         $scope.lagreBarnetilegg = function(barn, index , event) {
             var result = {};
             result["fnr"] = barn.fnr;
             result["valgt"] = event.target.checked;
-            if($scope.soknadData.fakta.barnetillegg.valuelist && $scope.soknadData.fakta.barnetillegg.valuelist.length >= index && $scope.soknadData.fakta.barnetillegg.valuelist[index]) {
-                result["faktumId"] = angular.fromJson($scope.soknadData.fakta.barnetillegg.valuelist[index]).faktumId;
-            }
-           /* result["key"] = 'barnetillegg';
-            result["soknadId"] = $scope.soknadData.soknadId;*/
 
-           
-                $scope.$emit("LAGRE_BARNETILLEGG", {key: 'barnetillegg', value: angular.toJson(result)});
-           
+            leggTilFaktumIdIResultDersomDenFinnes(barn, result);
+            $scope.$emit("LAGRE_BARNETILLEGG", {key: 'barnetillegg', value: angular.toJson(result)});
+        }
 
+        function leggTilFaktumIdIResultDersomDenFinnes(barn, result) {
+            angular.forEach($scope.soknadData.fakta.barnetillegg.valuelist, function(value) { 
+                if(angular.fromJson(value).fnr == barn.fnr) {
+                    result["faktumId"] = angular.fromJson(value).faktumId;
+                }
+            });
         }
 
         $scope.erGutt = function(barn) {
