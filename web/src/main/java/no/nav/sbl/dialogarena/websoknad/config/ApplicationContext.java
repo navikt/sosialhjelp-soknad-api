c183f5ce730922491b2119c2a2d65b28c008b532
Andreas Svendby <andreas.svendby@nav.no>
2013-12-09 15:46:14 +0100

    [TG]: Flyttet fortsett senere og avbryt søknad inn i sticky. Slått sammen funksjonalitet i ferdig-knapp med neste-knapp. Fjernet stegindikator fra bunn. Endret tekst for å gå videre fra skjemasiden. Lagt til label for tekstfelt og fikset en del styling

--- web/src/main/java/no/nav/sbl/dialogarena/websoknad/config/ApplicationContext.java
+++ web/src/main/java/no/nav/sbl/dialogarena/websoknad/config/ApplicationContext.java
@@ -40,13 +40,11 @@ public class ApplicationContext {
     private String navigasjonslink;
 
 
-    @Value("{$dokumentinnsending.smtpServer.port}")
-    private String smtpServerPort;
-            //= "25";
+//    @Value("{$dokumentinnsending.smtpServer.port}")
+    private String smtpServerPort = "25";
 
-    @Value("${dokumentinnsending.smtpServer.host}")
-    private String smtpServerHost;
-            //= "smtp.test.local";
+//    @Value("${dokumentinnsending.smtpServer.host}")
+    private String smtpServerHost = "smtp.test.local";
 
     @Bean
     public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
