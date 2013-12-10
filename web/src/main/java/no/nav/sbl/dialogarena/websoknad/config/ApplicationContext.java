74bcff4207adc71578a9b010183936e96de38194
Hein Haraldsen <Hein.Haraldsen@nav.no>
2013-12-10 13:53:48 +0100

    Fjernet smtp.Port som variabel

--- web/src/main/java/no/nav/sbl/dialogarena/websoknad/config/ApplicationContext.java
+++ web/src/main/java/no/nav/sbl/dialogarena/websoknad/config/ApplicationContext.java
@@ -43,11 +43,8 @@ public class ApplicationContext {
 
     //TODO Når FASIT funker må dette fikses
 
-    @Value("${dokumentinnsending.smtpServer.host}")
-    private String smtpServerHost;
-            //= "smtp.test.local";
-
-
+   // @Value("${dokumentinnsending.smtpServer.host}")
+    private String smtpServerHost = "smtp.test.local";
 
     @Value("{$dokumentinnsending.smtpServer.port}")
     private String smtpServerPort;
