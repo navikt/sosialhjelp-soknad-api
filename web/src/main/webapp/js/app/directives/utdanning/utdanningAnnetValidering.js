c8c25484ab8b36cc332d7f19c2e4fc0423855a53
Hein Haraldsen <Hein.Haraldsen@nav.no>
2013-12-10 09:57:55 +0100

    Testutskrifter

--- web/src/main/webapp/js/app/directives/utdanning/utdanningAnnetValidering.js
+++ web/src/main/webapp/js/app/directives/utdanning/utdanningAnnetValidering.js
@@ -1,21 +1,25 @@
-angular.module('nav.utdanningAnnet', [])
+angular.module('nav.utdanningannet', [])
     .directive('validerUtdanningAnnet', ['data', function (data) {
     return {
 
         require: ['^form'],
         link: function (scope, element, attrs, ctrl) {
             var eventString = 'RUN_VALIDATION' + ctrl[0].$name;
-            console.log("nne i utdanning_validering");
+
             scope.$on(eventString, function () {
+                console.log("Utdanning1");
                 fadeBakgrunnsfarge(element, element.find('.utdanning-annet-melding'), 'feil');
             })
 
             scope.$watch(function () {
+                console.log("Utdanning2");
                 return scope.ytelser.skalViseFeilmeldingForUtdanningAnnet;
             }, function () {
                 if (scope.ytelser.skalViseFeilmeldingForUtdanningAnnet) {
+                    console.log("Utdanning3");
                     element.addClass('feil');
                 } else {
+                    console.log("Utdanning4");
                     fadeBakgrunnsfarge(element, element.find('.utdanning-annet-melding'), 'feil');
                 }
             });
