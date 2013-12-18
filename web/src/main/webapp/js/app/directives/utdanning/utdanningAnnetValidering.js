8b3feafdbfb7eb75d73fb6b42c3dc14f19899673
Ketil Velle <ketil.s.velle@nav.no>
2013-12-18 14:51:52 +0100

    MOD-4859 Fikset problemer etter merge

--- web/src/main/webapp/js/app/directives/utdanning/utdanningAnnetValidering.js
+++ web/src/main/webapp/js/app/directives/utdanning/utdanningAnnetValidering.js
@@ -5,16 +5,13 @@ angular.module('nav.utdanningannet', [])
         link: function (scope, element, attrs, ctrl) {
             var eventString = 'RUN_VALIDATION' + ctrl[0].$name;
             scope.$on(eventString, function () {
-            fadeFeilmelding(element, element.find('.utdanning-annet-melding'), 'feil');
-
+                fadeBakgrunnsfarge(element, element.find('.utdanning-annen-melding'), 'feil', scope);
             })
             scope.$watch(function () {
                 return scope.utdanning.skalViseFeilmeldingForUtdanningAnnet;
             }, function () {
                 if (scope.utdanning.skalViseFeilmeldingForUtdanningAnnet) {
                     element.addClass('feil');
-                } else {
-                    fadeFeilmelding(element, element.find('.utdanning-annet-melding'), 'feil');
                 }
             });
         }
