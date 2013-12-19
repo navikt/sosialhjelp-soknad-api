404d7e20054439b295317f7e595e1d84992edf99
Ketil Velle <ketil.s.velle@nav.no>
2013-12-19 15:11:36 +0100

    TG fikset feil metodekall, mulig en merge som har gått feil

--- web/src/main/webapp/js/app/directives/ytelser/ingenNAVYtelserValidering.js
+++ web/src/main/webapp/js/app/directives/ytelser/ingenNAVYtelserValidering.js
@@ -5,7 +5,7 @@ angular.module('nav.ingennavytelser', [])
             link: function (scope, element, attrs, ctrl) {
                 var eventString = 'RUN_VALIDATION' + ctrl[0].$name;
                 scope.$on(eventString, function () {
-                    fadeBakgrunnsfarge(element, element.find('.ingen-navytelse-melding'), 'feil', scope);
+                    fadeFeilmelding(element, element.find('.ingen-navytelse-melding'), 'feil', scope);
                 })
                 scope.$watch(function () {
                     return scope.ytelser.skalViseFeilmeldingForIngenNavYtelser;
@@ -14,7 +14,7 @@ angular.module('nav.ingennavytelser', [])
                         console.log("Legger til feil");
                         element.addClass('feil');
                     } else {
-                        fadeBakgrunnsfarge(element, element.find('.ingen-navytelse-melding'), 'feil', scope);
+                        fadeFeilmelding(element, element.find('.ingen-navytelse-melding'), 'feil', scope);
                     }
                 });
             }
