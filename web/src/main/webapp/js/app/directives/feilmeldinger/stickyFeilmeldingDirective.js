94f6c27304e19c741003627ed449c97e4084b9a5
Ingvild Indrebø <ingvild.indrebo@nav.no>
2014-01-02 12:14:32 +0100

    [TG-5062FjernetOppsummeringsmodus] fjernet oppsummeringsmodus fra alle bolkene utenom arbeidsforhold og barnetillegg

--- web/src/main/webapp/js/app/directives/feilmeldinger/stickyFeilmeldingDirective.js
+++ web/src/main/webapp/js/app/directives/feilmeldinger/stickyFeilmeldingDirective.js
@@ -28,6 +28,7 @@ angular.module('nav.stickyFeilmelding', [])
                     scope.feil.antallFeilMedKlasseFeil = elem.find('.form-linje.feil').length;
                     scope.feil.antallFeilMedKlasseFeilstyling = elem.find('.form-linje.feilstyling').length;
 
+                    console.log(elem);
                     var idBolkerMedFeil = [];
                     var idAlleBolker = [];
 
