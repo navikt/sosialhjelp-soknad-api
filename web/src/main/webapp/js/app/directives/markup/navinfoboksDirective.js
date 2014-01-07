01e727ced10c73405574f596c193ae729e6c86d9
Andreas Svendby <andreas.svendby@nav.no>
2014-01-07 12:52:17 +0100

    [TG]: Refaktorert utdanning

--- web/src/main/webapp/js/app/directives/markup/navinfoboksDirective.js
+++ web/src/main/webapp/js/app/directives/markup/navinfoboksDirective.js
@@ -2,9 +2,6 @@ angular.module('nav.markup.navinfoboks', [])
     .directive('navinfoboks', [function () {
         return {
             restrict: 'A',
-            scope: {
-            	nokkel: '@'
-            },
             replace: true,
             transclude: true,
             templateUrl: '../js/app/directives/markup/navinfoboksTemplate.html'
