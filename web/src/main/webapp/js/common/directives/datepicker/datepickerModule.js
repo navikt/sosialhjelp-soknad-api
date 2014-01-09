77ea1d25660bb28a3583881ab307e1257fc0f76e
Andreas Svendby <andreas.svendby@nav.no>
2014-01-09 10:29:00 +0100

    [TG-GUI]: Fikset format på lagret dato. Fikset litt styling på datepicker. Fikset litt styling for skjema.

--- web/src/main/webapp/js/common/directives/datepicker/datepickerModule.js
+++ web/src/main/webapp/js/common/directives/datepicker/datepickerModule.js
@@ -41,10 +41,9 @@ angular.module('nav.datepicker', [])
         altFormat: 'dd.MM.yyyy',
         dateFormat: 'dd.mm.yy',
         changeMonth: true,
-        changeYear: true,
-        maxDate: new Date()
+        changeYear: true
     })
-    .directive('navDato', ['$timeout', 'datepickerConfig', function ($timeout, datepickerConfig) {
+    .directive('navDato', ['$timeout', 'datepickerConfig', '$filter', function ($timeout, datepickerConfig, $filter) {
         return {
             restrict: "A",
             require: '^form',
@@ -71,6 +70,11 @@ angular.module('nav.datepicker', [])
                 var harHattFokus = false;
                 var datepickerErLukket = true;
                 scope.harFokus = false;
+
+                scope.options = {};
+                if (!scope.erFremtidigdatoTillatt) {
+                    scope.options['maxDate'] = new Date();
+                }
 
                 scope.toggleDatepicker = function () {
                     if ($('#ui-datepicker-div').is(':hidden')) {
@@ -158,26 +162,23 @@ angular.module('nav.datepicker', [])
 
                 function datepickerOptions() {
                     var currentDefaultDate = defaultDate;
+                    console.log(scope.ngModel);
+                    console.log(new Date(scope.ngModel));
                     defaultDate = scope.ngModel ? new Date(scope.ngModel) : currentDefaultDate;
 
                     if (currentDefaultDate.getTime() != defaultDate.getTime()) {
                         scope.options = angular.extend({}, {defaultDate: defaultDate}, scope.options);
                     }
 
-                    if(scope.erFremtidigdatoTillatt) {
-                        var config = deepClone(datepickerConfig);
-                        config["maxDate"] = undefined;
-                        return angular.extend({}, config, scope.options);
-                    } else {
-                        return angular.extend({}, datepickerConfig, scope.options);
-                    }
+                    return angular.extend({}, datepickerConfig, scope.options);
                 };
 
                 function leggTilDatepicker() {
                     var opts = datepickerOptions();
 
                     opts.onSelect = function () {
-                        scope.ngModel = datepickerInput.datepicker("getDate");
+                        var dato = datepickerInput.datepicker("getDate");
+                        scope.ngModel = $filter('date')(dato, "yyyy.MM.dd");
                     };
 
                     opts.beforeShow = function () {
@@ -276,7 +277,12 @@ angular.module('nav.datepicker', [])
                 });
 
                 ngModel.$formatters.unshift(function (dato) {
-                    return $filter('date')(dato, "dd.MM.yyyy");
+                    if (dato) {
+                        var datoSomDateObjekt = new Date(dato);
+                        return $filter('date')(datoSomDateObjekt, "dd.MM.yyyy");
+                    } else {
+                        return '';
+                    }
                 });
 
                 var gammelInputVerdi = '';
@@ -317,7 +323,7 @@ angular.module('nav.datepicker', [])
                     ngModel.$render();
                     settCaretPosisjon(element, caretPosisjon);
 
-                    return konverterStringFraNorskDatoformatTilDateObjekt(datoInput);
+                    return reverserNorskDatoformat(datoInput);
                 });
 
                 scope.$watch(
