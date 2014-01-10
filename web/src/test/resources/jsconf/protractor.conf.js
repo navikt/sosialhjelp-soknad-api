c0b5ada8ba296b01f9b9f40b40ef6855e0fd372f
Andreas Svendby <andreas.svendby@nav.no>
2014-01-10 15:29:29 +0100

    [TG]: Fikset script for å kjøre protractor og endre config pittelitt

--- web/src/test/resources/jsconf/protractor.conf.js
+++ web/src/test/resources/jsconf/protractor.conf.js
@@ -1,13 +1,5 @@
-/*
- * npm install protractor -g --save-dev
- * npm install selenium -g --save-dev
- * npm install selenium-webdriver -g --save-dev
- * npm install selenium-chromedriver -g --save-dev
- */
-
 exports.config = {
     seleniumAddress: 'http://a34apvl016.devillo.no:4444/wd/hub',
-    baseUrl: 'http://a34duvw22201.devillo.no:8181/sendsoknad/soknad/Dagpenger',
 
     capabilities: {
         'browserName': 'firefox'
