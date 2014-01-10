0170cfbee3c749d49f90d7830a53db965f00fff4
Andreas Svendby <andreas.svendby@nav.no>
2014-01-10 14:22:56 +0100

    [TG-Protractor] Fikset tester etter endringer

--- web/src/test/js/test/protractor/util/skjemapage/arbeidsforholdBolk.js
+++ web/src/test/js/test/protractor/util/skjemapage/arbeidsforholdBolk.js
@@ -2,8 +2,19 @@ module.exports = {
     get: function() {
         return {
             elem: element(by.id('arbeidsforhold')),
+            open: function() {
+                element(by.id('arbeidsforhold')).getAttribute('class').then(function(klasse) {
+                    if (klasse.indexOf('open') < 0) {
+                        element(by.css('#arbeidsforhold .accordion-toggle')).click();
+                    }
+                });
+            },
             validerbolk: element(by.css('#arbeidsforhold .spm-knapper button')),
-            ikkejobbet: element(by.css('#arbeidsforhold #har-ikke-jobbet input'))
+            harjobbet: {
+                nei: element.all(by.css('[data-nav-faktum=arbeidstilstand] input')).get(0),
+                varierende: element.all(by.css('[data-nav-faktum=arbeidstilstand] input')).get(1),
+                ja: element.all(by.css('[data-nav-faktum=arbeidstilstand] input')).get(2)
+            }
         }
     }
 };
--- web/src/test/js/test/protractor/util/skjemapage/reellArebeidssokerBolk.js
+++ web/src/test/js/test/protractor/util/skjemapage/reellArebeidssokerBolk.js
@@ -2,22 +2,29 @@ module.exports = {
     get: function() {
         return {
             elem: element(by.id('reellarbeidssoker')),
+            open: function() {
+                element(by.id('reellarbeidssoker')).getAttribute('class').then(function(klasse) {
+                    if (klasse.indexOf('open') < 0) {
+                        element(by.css('#reellarbeidssoker .accordion-toggle')).click();
+                    }
+                });
+            },
             validerbolk: element(by.css('#reellarbeidssoker .spm-knapper button')),
             deltid: {
-                ja: element.all(by.css('[data-nav-faktum=villigdeltid] input')).get(0),
-                nei: element.all(by.css('[data-nav-faktum=villigdeltid] input')).get(1)
+                ja: element.all(by.css('[data-nav-faktum*=villigdeltid] input')).get(0),
+                nei: element.all(by.css('[data-nav-faktum*=villigdeltid] input')).get(1)
             },
             pendle: {
-                ja: element.all(by.css('[data-nav-faktum=villigpendle] input')).get(0),
-                nei: element.all(by.css('[data-nav-faktum=villigpendle] input')).get(1)
+                ja: element.all(by.css('[data-nav-faktum*=villigpendle] input')).get(0),
+                nei: element.all(by.css('[data-nav-faktum*=villigpendle] input')).get(1)
             },
             helse: {
-                ja: element.all(by.css('[data-nav-faktum=villighelse] input')).get(0),
-                nei: element.all(by.css('[data-nav-faktum=villighelse] input')).get(1)
+                ja: element.all(by.css('[data-nav-faktum*=villighelse] input')).get(0),
+                nei: element.all(by.css('[data-nav-faktum*=villighelse] input')).get(1)
             },
             jobb: {
-                ja: element.all(by.css('[data-nav-faktum=villigjobb] input')).get(0),
-                nei: element.all(by.css('[data-nav-faktum=villigjobb] input')).get(1)
+                ja: element.all(by.css('[data-nav-faktum*=villigjobb] input')).get(0),
+                nei: element.all(by.css('[data-nav-faktum*=villigjobb] input')).get(1)
             }
         }
     }
