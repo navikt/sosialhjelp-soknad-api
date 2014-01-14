15e345b2ef50c2cffe5169df850e98adc5c53632
Kjell Arne Brødreskift <kjell.arne.brodreskift@nav.no>
2014-01-14 12:05:14 +0100

    Rettet jshint-feil.

--- web/src/main/webapp/js/app/directives/validering/valideringModule.js
+++ web/src/main/webapp/js/app/directives/validering/valideringModule.js
@@ -1,229 +1,228 @@
 angular.module('nav.validering', ['nav.cmstekster'])
-    .directive('blurValidate', ['cms', function (cms) {
-        return {
-            require: ['ngModel', '^form'],
-            link: function (scope, element, attrs, ctrls) {
-                var feil, revaliderFeilObjekt;
+	.directive('blurValidate', ['cms', function (cms) {
+		return {
+			require: ['ngModel', '^form'],
+			link   : function (scope, element, attrs, ctrls) {
+				var feil, revaliderFeilObjekt;
 
-                var ngModel = ctrls[0];
-                var form = ctrls[1];
-                var eventString = 'RUN_VALIDATION' + form.$name;
-                var valideringsMetoder = [];
-                var formElem = element.closest('.form-linje');
+				var ngModel = ctrls[0];
+				var form = ctrls[1];
+				var eventString = 'RUN_VALIDATION' + form.$name;
+				var valideringsMetoder = [];
+				var formElem = element.closest('.form-linje');
 
+				try {
+					feil = scope.$eval(attrs.errorMessages);
+				} catch (e) {
+					feil = attrs.errorMessages;
+				}
 
-                try {
-                    feil = scope.$eval(attrs.errorMessages);
-                } catch (e) {
-                    feil = attrs.errorMessages;
-                }
+				// Rekkefølgen på setup-metodene bestemmer prioriteten på valideringsmetodene
+				valideringsMetoder.push(new RequiredValidator(attrs));
+				valideringsMetoder.push(new PatternValidator(attrs));
+				valideringsMetoder.push(new LengthValidator(attrs));
 
-                // Rekkefølgen på setup-metodene bestemmer prioriteten på valideringsmetodene
-                valideringsMetoder.push(new RequiredValidator(attrs));
-                valideringsMetoder.push(new PatternValidator(attrs));
-                valideringsMetoder.push(new LengthValidator(attrs));
+				scope.$on(eventString, function () {
+					if (!sjekkOmInputErGyldig() && element.is(':visible')) {
+						formElem.addClass('feil');
+					}
+				});
 
-                scope.$on(eventString, function () {
-                    if (!sjekkOmInputErGyldig() && element.is(':visible')) {
-                        formElem.addClass('feil');
-                    }
-                });
+				element.bind('blur', function () {
+					if (!sjekkOmInputErGyldig()) {
+						formElem.addClass('feil');
+					}
+				});
 
-                element.bind('blur', function () {
-                    if (!sjekkOmInputErGyldig()) {
-                        formElem.addClass('feil');
-                    }
-                });
+				scope.$watch(function () {
+					return element.is(':visible');
+				}, function (erSynlig) {
+					if (!erSynlig) {
+						formElem.removeClass('feil');
+					}
+				});
 
-                scope.$watch(function () {
-                    return element.is(':visible');
-                }, function (erSynlig) {
-                    if (!erSynlig) {
-                        formElem.removeClass('feil');
-                    }
-                });
+				scope.$watch(function () {
+					return ngModel.$viewValue;
+				}, function () {
+					if (revaliderFeilObjekt && revaliderFeilObjekt.validate(ngModel.$viewValue) === true) {
+						formElem.removeClass('feil');
+					}
+				});
 
-                scope.$watch(function () {
-                    return ngModel.$viewValue;
-                }, function () {
-                    if (revaliderFeilObjekt && revaliderFeilObjekt.validate(ngModel.$viewValue) == true) {
-                        formElem.removeClass('feil');
-                    }
-                });
+				function sjekkOmInputErGyldig() {
+					for (var i = 0; i < valideringsMetoder.length; i++) {
+						var valideringReturVerdi = valideringsMetoder[i].validate(ngModel.$viewValue);
 
-                function sjekkOmInputErGyldig() {
-                    for (var i = 0; i < valideringsMetoder.length; i++) {
-                        var valideringReturVerdi = valideringsMetoder[i].validate(ngModel.$viewValue);
+						if (valideringReturVerdi !== true) {
+							revaliderFeilObjekt = valideringsMetoder[i];
+							settFeilmeldingsTekst(valideringReturVerdi);
+							return false;
+						}
+					}
+					return true;
+				}
 
-                        if (valideringReturVerdi != true) {
-                            revaliderFeilObjekt = valideringsMetoder[i];
-                            settFeilmeldingsTekst(valideringReturVerdi);
-                            return false;
-                        }
-                    }
-                    return true;
-                }
+				function settFeilmeldingsTekst(feilNokkel) {
+					if (meldingIkkeInneholderFeilmelding()) {
+						var feilmeldingsNokkel = feil;
 
-                function settFeilmeldingsTekst(feilNokkel) {
-                    if (meldingIkkeInneholderFeilmelding()) {
-                        var feilmeldingsNokkel = feil;
+						if (typeof feil === 'object') {
+							feilmeldingsNokkel = feil[feilNokkel];
+						}
 
-                        if (typeof feil === 'object') {
-                            feilmeldingsNokkel = feil[feilNokkel];
-                        }
+						var feilmeldingTekst = cms.tekster[feilmeldingsNokkel];
+						formElem.find('.melding').text(feilmeldingTekst);
+					}
 
-                        var feilmeldingTekst = cms.tekster[feilmeldingsNokkel];
-                        formElem.find('.melding').text(feilmeldingTekst);
-                    }
+				}
 
-                }
+				function meldingIkkeInneholderFeilmelding() {
+					return !(formElem.find('.melding')[0] && formElem.find('.melding')[0].hasAttribute('data-cmstekster'));
+				}
+			}
+		}
+	}])
 
-                function meldingIkkeInneholderFeilmelding() {
-                    return !(formElem.find('.melding')[0] && formElem.find('.melding')[0].hasAttribute('data-cmstekster'));
-                }
-            }
-        }
-    }])
+	.directive('clickValidate', ['$timeout', 'cms', function ($timeout, cms) {
+		return {
+			require: ['ngModel', '^form'],
+			link   : function (scope, element, attrs, ctrls) {
+				var ngModel = ctrls[0];
+				var form = ctrls[1];
+				var eventString = 'RUN_VALIDATION' + form.$name;
 
-    .directive('clickValidate', ['$timeout', 'cms', function ($timeout, cms) {
-        return {
-            require: ['ngModel', '^form'],
-            link: function (scope, element, attrs, ctrls) {
-                var ngModel = ctrls[0];
-                var form = ctrls[1];
-                var eventString = 'RUN_VALIDATION' + form.$name;
+				scope.$on(eventString, function () {
+					if (sjekkOmFeltetErRequired() && !sjekkOmFeltetErSvart()) {
+						element.closest('.form-linje').addClass('feil');
+					}
+				});
 
-                scope.$on(eventString, function () {
-                    if (sjekkOmFeltetErRequired() && !sjekkOmFeltetErSvart()) {
-                        element.closest('.form-linje').addClass('feil');
-                    }
-                });
+				scope.$watch(function () {
+					return ngModel.$viewValue;
+				}, function () {
+					if (ngModel.$viewValue && element.closest('.form-linje').hasClass('feil')) {
+						fadeFeilmelding(element.closest('.form-linje'), element.closest('.form-linje').find('.melding'), 'feil', scope);
+					}
+				});
 
-                scope.$watch(function () {
-                    return ngModel.$viewValue;
-                }, function () {
-                    if (ngModel.$viewValue && element.closest('.form-linje').hasClass('feil')) {
-                        fadeFeilmelding(element.closest('.form-linje'), element.closest('.form-linje').find('.melding'), 'feil', scope);
-                    }
-                });
+				function sjekkOmFeltetErRequired() {
+					return element[0].hasAttribute('required');
+				}
 
-                function sjekkOmFeltetErRequired() {
-                    return element[0].hasAttribute("required");
-                }
+				function sjekkOmFeltetErSvart() {
+					if (!ngModel.$modelValue) {
+						settFeilmeldingsTekst();
+						return false;
+					}
+					return true;
+				}
 
-                function sjekkOmFeltetErSvart() {
-                    if (!ngModel.$modelValue) {
-                        settFeilmeldingsTekst();
-                        return false;
-                    }
-                    return true;
-                }
-
-                function settFeilmeldingsTekst() {
-                    var feilmeldingsNokkel = element[0].getAttribute('data-error-messages').toString();
-                    //hack for å fjerne dobbeltfnuttene rundt feilmeldingsnokk
-                    var feilmeldingTekst = cms.tekster[feilmeldingsNokkel.substring(1, feilmeldingsNokkel.length - 1)];
-                    element.closest('.form-linje').find('.melding').text(feilmeldingTekst);
-                }
-            }
-        }
-    }])
+				function settFeilmeldingsTekst() {
+					var feilmeldingsNokkel = element[0].getAttribute('data-error-messages').toString();
+					//hack for å fjerne dobbeltfnuttene rundt feilmeldingsnokk
+					var feilmeldingTekst = cms.tekster[feilmeldingsNokkel.substring(1, feilmeldingsNokkel.length - 1)];
+					element.closest('.form-linje').find('.melding').text(feilmeldingTekst);
+				}
+			}
+		}
+	}])
 //    direktivet skal brukes på div-en som ligger rundt en checkboksgruppe og som skal ha inlinevalidering
-    .directive('checkboxValidate', ['cms', function (cms) {
-        return {
-            require: ['^form'],
-            link: function (scope, element, attrs, ctrl) {
-                var eventString = 'RUN_VALIDATION' + ctrl[0].$name;
+	.directive('checkboxValidate', ['cms', function (cms) {
+		return {
+			require: ['^form'],
+			link   : function (scope, element, attrs, ctrl) {
+				var eventString = 'RUN_VALIDATION' + ctrl[0].$name;
 
-                scope.$on(eventString, function () {
+				scope.$on(eventString, function () {
 
-                    if (element.find("input:checked").length > 0) {
-                        element.closest('.form-linje').removeClass('feil');
-                    } else if (element.find("input:checked").length == 0 && element.is(':visible')) {
-                        if (element.closest('.form-linje').hasClass('checkbox')) {
-                            element.closest('.form-linje').addClass('feilstyling');
-                        } else {
-                            element.closest('.form-linje').addClass('feil');
-                        }
-                    }
-                })
+					if (element.find('input:checked').length > 0) {
+						element.closest('.form-linje').removeClass('feil');
+					} else if (element.find('input:checked').length === 0 && element.is(':visible')) {
+						if (element.closest('.form-linje').hasClass('checkbox')) {
+							element.closest('.form-linje').addClass('feilstyling');
+						} else {
+							element.closest('.form-linje').addClass('feil');
+						}
+					}
+				});
 
-                scope.$watch(function () {
-                    return element.find("input:checked").length;
-                }, function () {
-                    if (element.find("input:checked").length > 0 && element.closest('.form-linje').hasClass('feil')) {
-                        fadeFeilmelding(element.closest('.form-linje'), element.closest('.form-linje').find('.melding'), 'feil', scope);
-                    } else if (element.find("input:checked").length > 0 && element.closest('.form-linje').hasClass('feilstyling')) {
-                        fadeFeilmelding(element.closest('.form-linje'), element.closest('.form-linje.feilstyling').children('.melding'), 'feilstyling', scope);
-                    }
-                });
-            }
-        }
-    }])
-    .directive('dateValidate', ['cms', function (cms) {
-        return {
-            require: ['ngModel', '^form'],
-            link: function (scope, element, attrs, ctrls) {
-                var ngModel = ctrls[0];
-                var form = ctrls[1];
-                var eventString = 'RUN_VALIDATION' + form.$name;
+				scope.$watch(function () {
+					return element.find('input:checked').length;
+				}, function () {
+					if (element.find('input:checked').length > 0 && element.closest('.form-linje').hasClass('feil')) {
+						fadeFeilmelding(element.closest('.form-linje'), element.closest('.form-linje').find('.melding'), 'feil', scope);
+					} else if (element.find('input:checked').length > 0 && element.closest('.form-linje').hasClass('feilstyling')) {
+						fadeFeilmelding(element.closest('.form-linje'), element.closest('.form-linje.feilstyling').children('.melding'), 'feilstyling', scope);
+					}
+				});
+			}
+		}
+	}])
+	.directive('dateValidate', ['cms', function (cms) {
+		return {
+			require: ['ngModel', '^form'],
+			link   : function (scope, element, attrs, ctrls) {
+				var ngModel = ctrls[0];
+				var form = ctrls[1];
+				var eventString = 'RUN_VALIDATION' + form.$name;
 
-                scope.$on(eventString, function () {
-                    if (sjekkOmFeltetErRequired() && !sjekkOmFeltetErSvart()) {
-                        element.closest('.form-linje').addClass('feil');
-                    }
-                });
+				scope.$on(eventString, function () {
+					if (sjekkOmFeltetErRequired() && !sjekkOmFeltetErSvart()) {
+						element.closest('.form-linje').addClass('feil');
+					}
+				});
 
-                element.bind('blur', function () {
-                    angular.forEach(form.$error, function (verdi, feilNokkel) {
-                        var feilmeldignsnavn = attrs.id.substring(0, attrs.id.lastIndexOf('.'));
-                        angular.forEach(verdi, function (feil) {
-                            if (feil === undefined && feilNokkel.indexOf(feilmeldignsnavn) !== -1) {
-                                settFraTilDatoFeilmeldingstekst(feilNokkel)
-                                //fjerner feilmeldingen om at fra må være før til, så ved lagring vil brukeren få beskjed om required-feilmeldingen i stedet
-                                form.$setValidity(feilNokkel, true);
-                            }
-                        })
-                    })
-                });
+				element.bind('blur', function () {
+					angular.forEach(form.$error, function (verdi, feilNokkel) {
+						var feilmeldignsnavn = attrs.id.substring(0, attrs.id.lastIndexOf('.'));
+						angular.forEach(verdi, function (feil) {
+							if (feil === undefined && feilNokkel.indexOf(feilmeldignsnavn) !== -1) {
+								settFraTilDatoFeilmeldingstekst(feilNokkel);
+								//fjerner feilmeldingen om at fra må være før til, så ved lagring vil brukeren få beskjed om required-feilmeldingen i stedet
+								form.$setValidity(feilNokkel, true);
+							}
+						})
+					})
+				});
 
-                scope.$watch(function () {
-                    return ngModel.$viewValue;
-                }, function () {
-                    if (ngModel.$viewValue) {
-                        fjernFeilKlassen();
-                    }
-                });
+				scope.$watch(function () {
+					return ngModel.$viewValue;
+				}, function () {
+					if (ngModel.$viewValue) {
+						fjernFeilKlassen();
+					}
+				});
 
-                function sjekkOmFeltetErRequired() {
-                    return element[0].hasOwnProperty("required");
-                }
+				function sjekkOmFeltetErRequired() {
+					return element[0].hasOwnProperty('required');
+				}
 
-                function sjekkOmFeltetErSvart() {
-                    if (!ngModel.$modelValue) {
-                        settRequiredFeilmeldingsTekst();
-                        return false;
-                    }
-                    return true;
-                }
+				function sjekkOmFeltetErSvart() {
+					if (!ngModel.$modelValue) {
+						settRequiredFeilmeldingsTekst();
+						return false;
+					}
+					return true;
+				}
 
-                function settRequiredFeilmeldingsTekst() {
-                    var feilmeldingsNokkel = element[0].getAttribute('error-messages').toString();
-                    //hack for å fjerne dobbeltfnuttene rundt feilmeldingsnokk
-                    var feilmeldingTekst = cms.tekster[feilmeldingsNokkel.substring(1, feilmeldingsNokkel.length - 1)];
-                    element.closest('.form-linje').find('.melding').text(feilmeldingTekst);
-                }
+				function settRequiredFeilmeldingsTekst() {
+					var feilmeldingsNokkel = element[0].getAttribute('error-messages').toString();
+					//hack for å fjerne dobbeltfnuttene rundt feilmeldingsnokk
+					var feilmeldingTekst = cms.tekster[feilmeldingsNokkel.substring(1, feilmeldingsNokkel.length - 1)];
+					element.closest('.form-linje').find('.melding').text(feilmeldingTekst);
+				}
 
-                function settFraTilDatoFeilmeldingstekst(feilNokkel) {
-                    var feilmeldingTekst = cms.tekster[feilNokkel];
-                    var tilElement = element.closest('.varighet').find('.til');
-                    tilElement.find('.melding').text(feilmeldingTekst);
-                    tilElement.addClass('feil');
-                }
+				function settFraTilDatoFeilmeldingstekst(feilNokkel) {
+					var feilmeldingTekst = cms.tekster[feilNokkel];
+					var tilElement = element.closest('.varighet').find('.til');
+					tilElement.find('.melding').text(feilmeldingTekst);
+					tilElement.addClass('feil');
+				}
 
-                function fjernFeilKlassen() {
-                    element.closest('.form-linje').removeClass('feil');
-                }
-            }
-        }
-    }]);
+				function fjernFeilKlassen() {
+					element.closest('.form-linje').removeClass('feil');
+				}
+			}
+		}
+	}]);
