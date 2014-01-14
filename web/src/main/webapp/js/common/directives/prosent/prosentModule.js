5c0981f12350ebc7ca3f1828e88ab8a09e43a018
Kjell Arne Brødreskift <kjell.arne.brodreskift@nav.no>
2014-01-14 13:23:33 +0100

    Rettet jshint-feil.

--- web/src/main/webapp/js/common/directives/prosent/prosentModule.js
+++ web/src/main/webapp/js/common/directives/prosent/prosentModule.js
@@ -1,20 +1,20 @@
 angular.module('nav.prosent', [])
-    .directive('prosent', [function () {
-        return {
-            replace: true,
-            require: 'ngModel',
+	.directive('prosent', [function () {
+		return {
+			replace: true,
+			require: 'ngModel',
 
-            link: function (scope, elm, attrs, ctrl) {
-                ctrl.$parsers.unshift(function (viewValue) {
-                    var INTEGER_REGEX = /^\-?\d*$/;
-                    if (INTEGER_REGEX.test(viewValue) && viewValue <= 100 && viewValue >= 0) {
-                        ctrl.$setValidity('prosent', true);
-                        return viewValue;
-                    } else {
-                        ctrl.$setValidity('prosent', false);
-                        return undefined;
-                    }
-                });
-            }
-        };
-    }]);
+			link: function (scope, elm, attrs, ctrl) {
+				ctrl.$parsers.unshift(function (viewValue) {
+					var INTEGER_REGEX = /^\-?\d*$/;
+					if (INTEGER_REGEX.test(viewValue) && viewValue <= 100 && viewValue >= 0) {
+						ctrl.$setValidity('prosent', true);
+						return viewValue;
+					} else {
+						ctrl.$setValidity('prosent', false);
+						return undefined;
+					}
+				});
+			}
+		};
+	}]);
