4cfb4a42d993df968c1f0da5783f2aebc1f87408
Kjell Arne Brødreskift <kjell.arne.brodreskift@nav.no>
2014-01-14 13:13:56 +0100

    Rettet jshint-feil.

--- web/src/main/webapp/js/common/directives/hjelpetekst/hjelpetekstModule.js
+++ web/src/main/webapp/js/common/directives/hjelpetekst/hjelpetekstModule.js
@@ -1,40 +1,39 @@
 angular.module('nav.hjelpetekst', ['nav.animation'])
-    .directive('navHjelpetekstelement', ['$document', function ($document) {
-        return {
-            replace: true,
-            scope: {
-                tittel: '@',
-                tekst: '@'
-            },
-            templateUrl: '../js/common/directives/hjelpetekst/hjelpetekstTemplate.html',
-            link: function (scope) {
-                scope.visHjelp = false;
-                scope.toggleHjelpetekst = function() {
-                    scope.visHjelp = !scope.visHjelp;
-                }
+	.directive('navHjelpetekstelement', ['$document', function ($document) {
+		return {
+			replace    : true,
+			scope      : {
+				tittel: '@',
+				tekst : '@'
+			},
+			templateUrl: '../js/common/directives/hjelpetekst/hjelpetekstTemplate.html',
+			link       : function (scope) {
+				scope.visHjelp = false;
+				scope.toggleHjelpetekst = function () {
+					scope.visHjelp = !scope.visHjelp;
+				};
 
-                scope.lukk = function () {
-                    scope.visHjelp = false;
-                }
+				scope.lukk = function () {
+					scope.visHjelp = false;
+				};
 
-                scope.stoppKlikk = function(event) {
-                    event.stopPropagation();
-                }
+				scope.stoppKlikk = function (event) {
+					event.stopPropagation();
+				};
 
-                $document.bind('click', function() {
-                    scope.visHjelp = false;
-                });
-            }
-        }
-    }])
-    .directive('navHjelpetekstTooltip', ['$timeout', function ($timeout) {
-        return function (scope, element) {
-            $timeout(function() {
-                var posisjon = element.prev().position();
-
-                var topp = posisjon.top - element.outerHeight() - 15;
-                var venstre = posisjon.left - 20;
-                element.css({top: topp + "px", left: venstre + "px"});
-            });
-        }
-    }]);
+				$document.bind('click', function () {
+					scope.visHjelp = false;
+				});
+			}
+		}
+	}])
+	.directive('navHjelpetekstTooltip', ['$timeout', function ($timeout) {
+		return function (scope, element) {
+			$timeout(function () {
+				var posisjon = element.prev().position();
+				var topp = posisjon.top - element.outerHeight() - 15;
+				var venstre = posisjon.left - 20;
+				element.css({top: topp + 'px', left: venstre + 'px'});
+			});
+		}
+	}]);
