05db2752f22fa2fbe55a6bf5fa22339fd208964f
Kjell Arne Brødreskift <kjell.arne.brodreskift@nav.no>
2014-01-14 11:43:28 +0100

    Rettet jshint-feil.

--- web/src/main/webapp/js/app/directives/dagpenger/scrollDirective.js
+++ web/src/main/webapp/js/app/directives/dagpenger/scrollDirective.js
@@ -1,35 +1,32 @@
 angular.module('nav.scroll.directive', [])
-    .directive('scrollTilbakeDirective', [function () {
-        return {
-            replace: false,
-            scope: true,
-            controller: ['$scope', '$attrs', '$timeout', '$cookieStore', function ($scope, $attrs, $timeout, $cookieStore) {
-                var cookiename = $attrs.scrollTilbakeDirective;
+	.directive('scrollTilbakeDirective', [function () {
+		return {
+			replace   : false,
+			scope     : true,
+			controller: ['$scope', '$attrs', '$timeout', '$cookieStore', function ($scope, $attrs, $timeout, $cookieStore) {
+				var cookiename = $attrs.scrollTilbakeDirective;
 
-                $timeout(function () {
-                    var cookie = $cookieStore.get(cookiename);
-                    if (cookie) {
-                        $scope.$emit("CLOSE_TAB", "reell-arbeidssoker");
-                        $scope.$emit("OPEN_TAB", cookie.aapneTabs);
+				$timeout(function () {
+					var cookie = $cookieStore.get(cookiename);
+					if (cookie) {
+						$scope.$emit('CLOSE_TAB', 'reell-arbeidssoker');
+						$scope.$emit('OPEN_TAB', cookie.aapneTabs);
 
-                        var faktumId = cookie.faktumId;
-                        var element;
-                        if (faktumId) {
-                            element = angular.element("#"+cookiename + faktumId);
-                        } else {
-                            element = angular.element("#"+cookiename);
-                        }
+						var faktumId = cookie.faktumId;
+						var element;
+						if (faktumId) {
+							element = angular.element('#' + cookiename + faktumId);
+						} else {
+							element = angular.element('#' + cookiename);
+						}
 
-                        $timeout(
-                            function () {
-                                scrollToElement(element, 0);
-                                fadeBakgrunnsfarge(element.parent(), $scope, 255, 255, 255);
-                            }
-                            , 600);
-                        $cookieStore.remove(cookiename);
-                    }
-                })
-                
-            }]
-        }
-    }])
+						$timeout(function () {
+							scrollToElement(element, 0);
+							fadeBakgrunnsfarge(element.parent(), $scope, 255, 255, 255);
+						}, 600);
+						$cookieStore.remove(cookiename);
+					}
+				});
+			}]
+		}
+	}]);
