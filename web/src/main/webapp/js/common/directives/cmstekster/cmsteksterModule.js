06d63dc4f6269c78782298481d6900f169e182c1
Kjell Arne Brødreskift <kjell.arne.brodreskift@nav.no>
2014-01-14 13:02:31 +0100

    Rettet jshint-feil.

--- web/src/main/webapp/js/common/directives/cmstekster/cmsteksterModule.js
+++ web/src/main/webapp/js/common/directives/cmstekster/cmsteksterModule.js
@@ -1,56 +1,55 @@
 angular.module('nav.cmstekster', ['app.services'])
-    .directive('cmsvedlegg', ['cms', '$compile', function (cms, $compile) {
-        return {
-            scope: false,
-            required: 'navFaktum',
-            link: {
-                pre: function (scope, elem, attr, faktum) {
-                    if (scope.forventning.property) {
-                        scope.cmsProps = {};
-                        scope.cmsProps[scope.forventning.property] = scope.forventning.faktum.properties[scope.forventning.property];
-                    }
-                }
-            }
-        }
-    }])
-    .directive('cmstekster', ['cms', '$compile', function (cms, $compile) {
+	.directive('cmsvedlegg', ['cms', '$compile', function (cms, $compile) {
+		return {
+			scope   : false,
+			required: 'navFaktum',
+			link    : {
+				pre: function (scope, elem, attr, faktum) {
+					if (scope.forventning.property) {
+						scope.cmsProps = {};
+						scope.cmsProps[scope.forventning.property] = scope.forventning.faktum.properties[scope.forventning.property];
+					}
+				}
+			}
+		}
+	}])
+	.directive('cmstekster', ['cms', '$compile', function (cms, $compile) {
 
-        return {
-            scope: false,
-            link: function (scope, element, attrs) {
-                var nokkel = attrs['cmstekster'];
+		return {
+			scope: false,
+			link : function (scope, element, attrs) {
+				var nokkel = attrs['cmstekster'];
+				var cmstekst = cms.tekster[nokkel];
 
-                var cmstekst = cms.tekster[nokkel];
-                if (cmstekst === undefined) {
-                    return;
-                }
+				if (cmstekst === undefined) {
+					return;
+				}
 
-                if (scope.cmsProps) {
-                    Object.keys(scope.cmsProps).forEach(function (attr) {
-                        cmstekst = cmstekst.replace('${' + attr + '}', scope.cmsProps[attr], 'i');
-                    });
-                }
+				if (scope.cmsProps) {
+					Object.keys(scope.cmsProps).forEach(function (attr) {
+						cmstekst = cmstekst.replace('${' + attr + '}', scope.cmsProps[attr], 'i');
+					});
+				}
 
-                if (element.is('input')) {
-                    element.attr('value', cmstekst);
-                } else {
-                    element.text(cmstekst);
-
-                }
-            }
-        };
-    }])
-    .directive('cmshtml', ['cms', function (cms) {
-        return function ($scope, element, attrs) {
-            var nokkel = attrs['cmshtml'];
-            element.html(cms.tekster[nokkel]);
-        };
-    }])
-    .directive('cmslenketekster', ['cms', function (cms) {
-        return function ($scope, element, attrs) {
-            var nokkel = attrs['cmslenketekster'];
-            if (element.is('a')) {
-                element.attr('href', cms.tekster[nokkel]);
-            }
-        };
-    }]);
+				if (element.is('input')) {
+					element.attr('value', cmstekst);
+				} else {
+					element.text(cmstekst);
+				}
+			}
+		};
+	}])
+	.directive('cmshtml', ['cms', function (cms) {
+		return function ($scope, element, attrs) {
+			var nokkel = attrs['cmshtml'];
+			element.html(cms.tekster[nokkel]);
+		};
+	}])
+	.directive('cmslenketekster', ['cms', function (cms) {
+		return function ($scope, element, attrs) {
+			var nokkel = attrs['cmslenketekster'];
+			if (element.is('a')) {
+				element.attr('href', cms.tekster[nokkel]);
+			}
+		};
+	}]);
