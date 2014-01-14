6be9851d14138c5b41c992effa542bac72c5067a
Andreas Svendby <andreas.svendby@nav.no>
2014-01-14 12:28:57 +0100

    [TG] Småfikser

--- web/src/main/webapp/js/app/i18n/i18n.js
+++ web/src/main/webapp/js/app/i18n/i18n.js
@@ -1,16 +1,4 @@
 $.datepicker.regional['no'] = {
-<<<<<<< HEAD
-	monthNames: ['Januar', 'Februar','Mars', 'April','Mai', 'Juni','Juli', 'August','September', 'Oktober','November', 'Desember'],
-	monthNamesShort: ['Jan', 'Feb','Mar', 'Apr','Mai', 'Jun','Jul', 'Aug','Sep', 'Okt','Nov', 'Des'],
-    closeText: 'Lukk',
-    prevText: 'Forrige',
-    nextText: 'Neste',
-    currentText: 'Denne',
-    dayNames: ['Søndag', 'Mandag','Tirsdag','Onsdag','Torsdag','Fredag','Lørdag'],
-    dayNamesMin: ['Sø', 'Ma','Ti','On','To','Fr','Lø'],
-    dateFormat: 'dd.mm.yy',
-    firstDay: 1
-=======
 	monthNames     : ['Januar', 'Februar', 'Mars', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Desember'],
 	monthNamesShort: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Des'],
 	closeText      : 'Lukk',
@@ -21,7 +9,6 @@ $.datepicker.regional['no'] = {
 	dayNamesMin    : ['Sø', 'Ma', 'Ti', 'On', 'To', 'Fr', 'Lø'],
 	dateFormat     : 'dd.mm.yy',
 	firstDay       : 1
->>>>>>> master
 };
 
 $.datepicker.setDefaults($.datepicker.regional['no']);
