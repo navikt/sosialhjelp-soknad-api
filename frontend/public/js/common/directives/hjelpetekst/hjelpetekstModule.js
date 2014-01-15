25654ad98d19dfac52244d35863eaffe0818bf75
Kjell Arne Brødreskift <kjell.arne.brodreskift@nav.no>
2014-01-15 08:23:46 +0100

    Modul frontend

---
+++ frontend/public/js/common/directives/hjelpetekst/hjelpetekstModule.js
@@ -0,0 +1,40 @@
+angular.module('nav.hjelpetekst', ['nav.animation'])
+    .directive('navHjelpetekstelement', ['$document', function ($document) {
+        return {
+            replace: true,
+            scope: {
+                tittel: '@',
+                tekst: '@'
+            },
+            templateUrl: '../js/common/directives/hjelpetekst/hjelpetekstTemplate.html',
+            link: function (scope) {
+                scope.visHjelp = false;
+                scope.toggleHjelpetekst = function() {
+                    scope.visHjelp = !scope.visHjelp;
+                };
+
+                scope.lukk = function () {
+                    scope.visHjelp = false;
+                };
+
+                scope.stoppKlikk = function(event) {
+                    event.stopPropagation();
+                };
+
+                $document.bind('click', function() {
+                    scope.visHjelp = false;
+                });
+            }
+        };
+    }])
+    .directive('navHjelpetekstTooltip', ['$timeout', function ($timeout) {
+        return function (scope, element) {
+            $timeout(function() {
+                var posisjon = element.prev().position();
+
+                var topp = posisjon.top - element.outerHeight() - 15;
+                var venstre = posisjon.left - 20;
+                element.css({top: topp + "px", left: venstre + "px"});
+            });
+        };
+    }]);
