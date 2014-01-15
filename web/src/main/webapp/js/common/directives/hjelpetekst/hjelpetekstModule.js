8de02c3bca0f3f50055fe3defb9c3998317dc8d2
Andreas Svendby <andreas.svendby@nav.no>
2014-01-15 16:12:55 +0100

    [PK-8380]: Fikset plassering av tooltip

--- web/src/main/webapp/js/common/directives/hjelpetekst/hjelpetekstModule.js
+++ web/src/main/webapp/js/common/directives/hjelpetekst/hjelpetekstModule.js
@@ -7,7 +7,9 @@ angular.module('nav.hjelpetekst', ['nav.animation'])
 				tekst : '@'
 			},
 			templateUrl: '../js/common/directives/hjelpetekst/hjelpetekstTemplate.html',
-			link       : function (scope) {
+			link: function (scope) {
+                var lukkEventTimestamp = 0;
+
 				scope.visHjelp = false;
 				scope.toggleHjelpetekst = function () {
 					scope.visHjelp = !scope.visHjelp;
@@ -18,22 +20,51 @@ angular.module('nav.hjelpetekst', ['nav.animation'])
 				};
 
 				scope.stoppKlikk = function (event) {
-					event.stopPropagation();
+                    lukkEventTimestamp = event.timeStamp;
 				};
 
-				$document.bind('click', function () {
-					scope.visHjelp = false;
+				$document.bind('click', function (event) {
+                    if (lukkEventTimestamp !== event.timeStamp) {
+                        scope.visHjelp = false;
+                    }
 				});
 			}
 		}
 	}])
-	.directive('navHjelpetekstTooltip', ['$timeout', function ($timeout) {
+	.directive('navHjelpetekstTooltip', ['$timeout', '$document', function ($timeout, $document) {
 		return function (scope, element) {
-			$timeout(function () {
-				var posisjon = element.prev().position();
-				var topp = posisjon.top - element.outerHeight() - 15;
-				var venstre = posisjon.left - 20;
-				element.css({top: topp + 'px', left: venstre + 'px'});
-			});
+            scope.skalVisesOverInfoIkon = function() {
+                var skalPlasseresOver = erScrolletLangtNokNedTilAViseHjelpetekstOverIkonet();
+                plasserTooltipIXRetning();
+                if (skalPlasseresOver) {
+                    element.css({top: -element.height() - 30});
+                }
+
+                return skalPlasseresOver;
+            }
+
+            scope.skalVisesUnderInfoIkon = function() {
+                var skalPlasseresUnder = !scope.skalVisesOverInfoIkon();
+
+                if (skalPlasseresUnder) {
+                    element.css({top: element.prev().height() + 20});
+                }
+                return skalPlasseresUnder;
+            }
+
+            function plasserTooltipIXRetning() {
+                var plassSomMangleTilHoyre = element[0].getBoundingClientRect().right + 40 - (window.innerWidth || document.documentElement.clientWidth);
+                if (plassSomMangleTilHoyre > 0) {
+                    var venstre = 20 + plassSomMangleTilHoyre
+                    element.css({left: -venstre});
+                    $('<style/>', {text: '.hjelpetekst .hjelpetekst-tooltip:before {left: ' + venstre + 'px !important};'}).appendTo('head');
+                } else if (element.css('left') === '-20px') {
+                    $('style:contains(.hjelpetekst .hjelpetekst-tooltip:before)').remove();
+                }
+            }
+
+            function erScrolletLangtNokNedTilAViseHjelpetekstOverIkonet(){
+                return element[0].getBoundingClientRect().top - 20 > 0;
+            }
 		}
 	}]);
