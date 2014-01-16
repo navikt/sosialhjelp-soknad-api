129c94ed5e9fbe61fdbb5154d0eb3c94309b2735
Andreas Svendby <andreas.svendby@nav.no>
2014-01-16 13:48:55 +0100

    [PK-8380]: Fikset plassering horisontalt og fikset overgang mellom mobil og ikke mobil

--- web/src/main/webapp/js/common/directives/hjelpetekst/hjelpetekstModule.js
+++ web/src/main/webapp/js/common/directives/hjelpetekst/hjelpetekstModule.js
@@ -1,5 +1,5 @@
 angular.module('nav.hjelpetekst', ['nav.animation'])
-	.directive('navHjelpetekstelement', ['$document', function ($document) {
+	.directive('navHjelpetekstelement', ['$document', '$window', function ($document, $window) {
 		return {
 			replace    : true,
 			scope      : {
@@ -17,6 +17,7 @@ angular.module('nav.hjelpetekst', ['nav.animation'])
 
 				scope.lukk = function () {
 					scope.visHjelp = false;
+                    $($window).unbind('resize');
 				};
 
 				scope.stoppKlikk = function (event) {
@@ -31,31 +32,52 @@ angular.module('nav.hjelpetekst', ['nav.animation'])
 			}
 		}
 	}])
-	.directive('navHjelpetekstTooltip', ['$timeout', '$document', function ($timeout, $document) {
+	.directive('navHjelpetekstTooltip', ['$timeout', '$document', '$window', function ($timeout, $document, $window) {
 		return function (scope, element) {
+            $('style:contains(.hjelpetekst .hjelpetekst-tooltip:before)').remove();
             $timeout(function() {
                 plasserTooltipHorisontalt();
                 scrollDersomNodvendig();
             });
 
+            function plasserTooltipVertikalt() {
+                element.css({top: -element.height() - 30});
+            }
+
             function plasserTooltipHorisontalt() {
                 var plassSomMangleTilHoyre = element[0].getBoundingClientRect().right + 40 - (window.innerWidth || document.documentElement.clientWidth);
-                if (plassSomMangleTilHoyre > 0) {
-                    var venstre = 20 + plassSomMangleTilHoyre
-                    element.css({left: -venstre});
-                    $('<style/>', {text: '.hjelpetekst .hjelpetekst-tooltip:before {left: ' + venstre + 'px !important};'}).appendTo('head');
-                } else if (element.css('left') === '-20px') {
-                    $('style:contains(.hjelpetekst .hjelpetekst-tooltip:before)').remove();
-                }
+                var venstre = Math.min(element.position().left - plassSomMangleTilHoyre, -20);
+                element.css({left: venstre});
+                settPilStyling(venstre);
             };
 
             function scrollDersomNodvendig() {
-                element.css({top: -element.height() - 30});
+                plasserTooltipVertikalt();
                 var diff = element[0].getBoundingClientRect().top - 20;
                 if (diff < 0) {
                     var animationSpeed = 200;
                     $('body, html').scrollToPos($document.scrollTop() + diff, animationSpeed);
                 }
             };
+
+            function settPilStyling(venstre) {
+                if ($('style:contains(.hjelpetekst .hjelpetekst-tooltip:before)').length === 0) {
+                    $('<style/>', {text: '.hjelpetekst .hjelpetekst-tooltip:before {left: ' + -venstre + 'px !important};'}).appendTo('head');
+                } else {
+                    $('style:contains(.hjelpetekst .hjelpetekst-tooltip:before)').text('.hjelpetekst .hjelpetekst-tooltip:before {left: ' + -venstre + 'px !important};');
+                }
+            };
+            
+            $($window).data('over767', $window.innerWidth > 767);
+            $($window).bind('resize', function() {
+                if ($window.innerWidth > 767) {
+                    plasserTooltipHorisontalt();
+
+                    if (!$($window).data('over767')) {
+                        plasserTooltipVertikalt();
+                    }
+                }
+                $($window).data('over767', $window.innerWidth > 767);
+            });
 		}
 	}]);
