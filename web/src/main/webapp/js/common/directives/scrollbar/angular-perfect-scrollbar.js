d17ca0f17c0d59b167bccfe4caf1121b8bea81b5
Andreas Svendby <andreas.svendby@nav.no>
2014-01-16 15:46:09 +0100

    [PK-8380]: Fikset en del for responsiv tooltip

--- web/src/main/webapp/js/common/directives/scrollbar/angular-perfect-scrollbar.js
+++ web/src/main/webapp/js/common/directives/scrollbar/angular-perfect-scrollbar.js
@@ -2,10 +2,8 @@
 angular.module('nav.scrollbar', []).directive('navScroll', ['$parse', '$timeout', function ($parse, $timeout) {
 	return function (scope, elem, attr) {
 		elem.perfectScrollbar({
-			wheelSpeed        : $parse(attr.wheelSpeed)() || 50,
-			wheelPropagation  : $parse(attr.wheelPropagation)() || false,
-			minScrollbarLength: $parse(attr.minScrollbarLength)() || false,
-			suppressScrollY   : true
+			minScrollbarLength: $parse(attr.minScrollbarLength)() || 10,
+            suppressScrollX   : $parse(attr.suppressScrollX)() || true
 		});
 
 		$timeout(function () {
