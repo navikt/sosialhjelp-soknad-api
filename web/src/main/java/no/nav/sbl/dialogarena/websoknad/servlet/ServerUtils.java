0763e000b9d31ea67b6f1d4cb149617bec84a393
Hein Haraldsen <hein.haraldsen@nav.no>
2014-01-17 15:28:42 +0100

    Fikset Fortsett Senere-url

--- web/src/main/java/no/nav/sbl/dialogarena/websoknad/servlet/ServerUtils.java
+++ web/src/main/java/no/nav/sbl/dialogarena/websoknad/servlet/ServerUtils.java
@@ -6,7 +6,7 @@ public class ServerUtils {
 
 	public static String getGjenopptaUrl(String requestUrl, long soknadId) {
 		String fullServerPath =  requestUrl.split("/rest/")[0];
-		String gjenopptaUrl = "/soknad/Dagpenger#/";
+		String gjenopptaUrl = "/soknad/Dagpenger#/dagpenger/";
 		return fullServerPath.concat(gjenopptaUrl).concat(valueOf(soknadId));
 	}
 }
