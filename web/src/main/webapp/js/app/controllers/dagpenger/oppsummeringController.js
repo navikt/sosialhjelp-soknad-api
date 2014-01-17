198fec77841f6388e064a82ae5443e356cec456c
Ingvild Indrebø <ingvild.indrebo@nav.no>
2014-01-17 08:30:12 +0100

    [MOD-4670] fikset checkboksen med styling, validering og feilmelding

--- web/src/main/webapp/js/app/controllers/dagpenger/oppsummeringController.js
+++ web/src/main/webapp/js/app/controllers/dagpenger/oppsummeringController.js
@@ -2,17 +2,33 @@ angular.module('nav.oppsummering', [])
     .controller('OppsummeringCtrl', ['$scope', '$location', '$routeParams', 'soknadService', 'personalia', 'oppsummeringService', function ($scope, $location, $routeParams, soknadService, personalia, oppsummeringService) {
         $scope.personalia = personalia;
         $scope.oppsummeringHtml = '';
-        $scope.harbekreftet = {value: false};
+        $scope.harbekreftet = {value: ''};
+        $scope.skalViseFeilmelding = {value: false};
 
         $scope.soknadId = $routeParams.soknadId;
         oppsummeringService.get($scope.soknadId).then(function(markup) {
             $scope.oppsummeringHtml = markup;
         });
 
+        console.log($scope.skalViseFeilmelding.value)
+
+        $scope.$watch(function () {
+            if($scope.harbekreftet) {
+                return $scope.harbekreftet.value;
+            }
+        }, function () {
+            $scope.skalViseFeilmelding.value = false;
+            console.log($scope.skalViseFeilmelding.value)
+        })
+
         $scope.sendSoknad = function () {
             if ($scope.harbekreftet.value) {
                 console.log("HEI");
+                $scope.skalViseFeilmelding.value = false;
+            } else {
+                $scope.skalViseFeilmelding.value = true;
             }
+            console.log($scope.skalViseFeilmelding.value)
 
 //            soknadService.send({param: $scope.soknadId, action: 'send'});
 //            $location.path('kvittering');
