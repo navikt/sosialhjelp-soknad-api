40a123d01ded880897ce403e6a2b8a9ad864e80d
Jarle Svendsrud <jarle.svendsrud@nav.no>
2014-01-17 13:09:52 +0100

    Benytter ny versjon av fillager som tar inn fødselsnummer som parameter og tillater maskin-token

--- consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/fillager/FillagerConnector.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/fillager/FillagerConnector.java
@@ -1,13 +1,16 @@
 package no.nav.sbl.dialogarena.soknadinnsending.consumer.fillager;
 
 
+import no.nav.modig.core.exception.ApplicationException;
 import no.nav.tjeneste.domene.brukerdialog.fillager.v1.FilLagerPortType;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import javax.activation.DataHandler;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.mail.util.ByteArrayDataSource;
+import javax.xml.ws.soap.SOAPFaultException;
+import java.io.IOException;
 import java.io.InputStream;
 
 @Component
@@ -17,16 +20,13 @@ public class FillagerConnector {
     @Named("fillagerService")
     private FilLagerPortType portType;
 
-    private static final Logger logger = LoggerFactory.getLogger(FillagerConnector.class);
-
-    public void lagreFil(String behandlingsId, String uid, InputStream fil) {
-//        try {  /
-//            portType.lagre(behandlingsId, uid, new DataHandler(new ByteArrayDataSource(fil, "application/octet-stream")));
-//        } catch (IOException e) {
-//            throw new ApplicationException("Kunne ikke lagre fil: " + e, e);
-//        } catch (SOAPFaultException ws) {
-//            throw new ApplicationException("Feil i kommunikasjon med fillager: " + ws, ws);
-//        }
-            logger.info("Later som jeg lagrer fil {} for behandlingsid {} og uuio {}", fil, behandlingsId, uid);
+    public void lagreFil(String behandlingsId, String uid, String fnr, InputStream fil) {
+        try {
+            portType.lagre(behandlingsId, uid, fnr, new DataHandler(new ByteArrayDataSource(fil, "application/octet-stream")));
+        } catch (IOException e) {
+            throw new ApplicationException("Kunne ikke lagre fil: " + e, e);
+        } catch (SOAPFaultException ws) {
+            throw new ApplicationException("Feil i kommunikasjon med fillager: " + ws, ws);
+        }
     }
 }
