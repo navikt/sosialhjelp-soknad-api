c55fff936a0614fe27a169a3af1876426ee0893d
Jarle Svendsrud <jarle.svendsrud@nav.no>
2014-01-17 15:37:00 +0100

    Tok bort MDC fra Fillagertjenesten ettersom det skal være et maskinkall

--- consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/ConsumerConfig.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/ConsumerConfig.java
@@ -85,7 +85,6 @@ public class ConsumerConfig {
                     .withWsdl("classpath:FilLager.wsdl")
                     .build()
                     .withHttpsMock()
-                    .withMDC()
                     .withSystemSecurity();
         }
 
