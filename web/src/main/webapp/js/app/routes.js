c4070219bfc585712e3d9b56cd604a00e90d6555
Ketil Velle <ketil.s.velle@nav.no>
2014-01-20 12:09:26 +0100

    PK-8667 endret url

--- web/src/main/webapp/js/app/routes.js
+++ web/src/main/webapp/js/app/routes.js
@@ -11,7 +11,7 @@ angular.module('app.routes', ['ngRoute'])
 					}
 				}
 			})
-			.when('/behandling/:behandlingId', {
+			.when('/:behandlingId', {
 				templateUrl: '../html/templates/informasjonsside.html',
 				controller: 'BehandlingCtrl',
 				resolve    : {
