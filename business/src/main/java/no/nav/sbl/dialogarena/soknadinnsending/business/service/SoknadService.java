11bfa1bb1892023ccaf5d28f5be08005256ed55f
Hein Haraldsen <hein.haraldsen@nav.no>
2014-01-21 11:37:45 +0100

    Lagt på mock igjen på SendSoknad midl. for å få app. til å fungere

--- business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/service/SoknadService.java
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/service/SoknadService.java
@@ -39,6 +39,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.UUID;
 
 import static java.lang.String.format;
@@ -167,8 +168,8 @@ public class SoknadService implements SendSoknadService, VedleggService {
 
     @Override
     public Long startSoknad(String navSoknadId) {
-        String behandlingsId = henvendelseConnector.startSoknad(getSubjectHandler().getUid());
-//       String behandlingsId = "MOCK" + new Random().nextInt(100000000);
+        //String behandlingsId = henvendelseConnector.startSoknad(getSubjectHandler().getUid());
+       String behandlingsId = "MOCK" + new Random().nextInt(100000000);
         WebSoknad soknad = WebSoknad.startSoknad().
                 medBehandlingId(behandlingsId).
                 medskjemaNummer(navSoknadId).
