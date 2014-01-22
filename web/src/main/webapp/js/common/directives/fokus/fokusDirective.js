6a986e0aef6ca687d1a3f1749eb18659bf68c968
Ingvild Indrebø <ingvild.indrebo@nav.no>
2014-01-22 13:33:38 +0100

    [MOD-5062] fikset tabing for hjelpetekster

--- web/src/main/webapp/js/common/directives/fokus/fokusDirective.js
+++ web/src/main/webapp/js/common/directives/fokus/fokusDirective.js
@@ -3,6 +3,11 @@ angular.module('nav.fokus', [])
         return {
             replace: true,
 
+            /*
+            Brukes for å sette fokus til neste element da spesielt mtp elementer
+            som blir fjernet fra DOMen og bruker tabber seg videre. Hvis ikke denne brukes
+            vil bruker havne på toppen og må tabbe seg tilbake
+             */
             link: function (scope, elm) {
                 elm.bind("click", function() {
                     settFokusTilNesteElement(elm);
