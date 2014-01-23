4c56ed705221206fef068ddfe667243aa0898f35
Ketil Velle <ketil.s.velle@nav.no>
2014-01-23 12:35:42 +0100

    TG-Envconfiglenker controller og service for aa hente ut config fra envconfig og lignende. Ligger i angularland paa data.config

--- web/src/main/java/no/nav/sbl/dialogarena/websoknad/servlet/ConfigController.java
+++ web/src/main/java/no/nav/sbl/dialogarena/websoknad/servlet/ConfigController.java
@@ -2,16 +2,19 @@ package no.nav.sbl.dialogarena.websoknad.servlet;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+import no.nav.sbl.dialogarena.soknadinnsending.business.service.ConfigService;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 
-import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -19,16 +22,16 @@ import java.util.Map;
  * 
  */
 @Controller
+@ControllerAdvice()
 public class ConfigController {
     Logger log = LoggerFactory.getLogger(ConfigController.class);
 
+    @Inject
+    ConfigService configService;
+    
     @RequestMapping(value = "/getConfig", method = RequestMethod.GET, produces = APPLICATION_JSON_VALUE)
         @ResponseBody()
         public Map<String,String> sendEpost(HttpServletRequest request) {
-           Map<String, String> result = new HashMap<String, String>();
-            
-           
-            
-            return result;
+           return configService.getConfig();
         }
 }
