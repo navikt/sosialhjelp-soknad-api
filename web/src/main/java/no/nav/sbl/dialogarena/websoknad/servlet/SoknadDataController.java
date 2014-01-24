42ca614e28fd00dc5dd9e0ad330cb2b4e419762b
Andreas Svendby <andreas.svendby@nav.no>
2014-01-24 15:23:53 +0100

    [PK-8372]: Lagt til PDF når søknad sendes inn

--- web/src/main/java/no/nav/sbl/dialogarena/websoknad/servlet/SoknadDataController.java
+++ web/src/main/java/no/nav/sbl/dialogarena/websoknad/servlet/SoknadDataController.java
@@ -1,7 +1,9 @@
 package no.nav.sbl.dialogarena.websoknad.servlet;
 
+import no.nav.modig.core.exception.ApplicationException;
 import no.nav.sbl.dialogarena.kodeverk.Kodeverk;
 import no.nav.sbl.dialogarena.print.HandleBarKjoerer;
+import no.nav.sbl.dialogarena.print.PDFFabrikk;
 import no.nav.sbl.dialogarena.soknadinnsending.business.domain.Faktum;
 import no.nav.sbl.dialogarena.soknadinnsending.business.domain.Vedlegg;
 import no.nav.sbl.dialogarena.soknadinnsending.business.domain.VedleggForventning;
@@ -24,6 +26,7 @@ import javax.inject.Inject;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -111,7 +114,16 @@ public class SoknadDataController {
     @RequestMapping(value = "/send/{soknadId}", method = RequestMethod.POST, consumes = "application/json")
     @ResponseBody()
     public void sendSoknad(@PathVariable Long soknadId) {
-        soknadService.sendSoknad(soknadId);
+        WebSoknad soknad = soknadService.hentSoknad(soknadId);
+        String oppsummeringMarkup = null;
+        try {
+            oppsummeringMarkup = new HandleBarKjoerer(kodeverk).fyllHtmlMalMedInnhold(soknad, "/skjema/dagpenger");
+
+        } catch (IOException e) {
+            throw new ApplicationException("Kunne ikke lage markup av søknad", e);
+        }
+        byte[] outputStream = PDFFabrikk.lagPdfFil(oppsummeringMarkup);
+        soknadService.sendSoknad(soknadId, outputStream);
     }
 
     @RequestMapping(value = "/lagre/{soknadId}", method = RequestMethod.POST, consumes = "application/json")
