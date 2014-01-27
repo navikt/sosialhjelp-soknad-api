d66ec9ef40dd12392e2002c3a08c612921178d84
Ketil Velle <ketil.s.velle@nav.no>
2014-01-27 10:22:24 +0100

    PK-9196 Feilmelding naar man ikke faar svar fra TPS

--- business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/person/PersonaliaService.java
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/person/PersonaliaService.java
@@ -1,7 +1,15 @@
 package no.nav.sbl.dialogarena.soknadinnsending.business.person;
 
+import no.nav.sbl.dialogarena.soknadinnsending.consumer.exceptions.IkkeFunnetException;
+
+import javax.xml.ws.WebServiceException;
+
+import no.nav.tjeneste.virksomhet.brukerprofil.v1.HentKontaktinformasjonOgPreferanserSikkerhetsbegrensning;
+
+import no.nav.tjeneste.virksomhet.brukerprofil.v1.HentKontaktinformasjonOgPreferanserPersonIkkeFunnet;
+
 public interface PersonaliaService {
-    Personalia hentPersonalia(String fodselsnummer);
+    Personalia hentPersonalia(String fodselsnummer) throws IkkeFunnetException, HentKontaktinformasjonOgPreferanserPersonIkkeFunnet, HentKontaktinformasjonOgPreferanserSikkerhetsbegrensning, WebServiceException;
 
     Personalia lagrePersonaliaOgBarn(String fodselsnummer, Long soknadId);
 }
