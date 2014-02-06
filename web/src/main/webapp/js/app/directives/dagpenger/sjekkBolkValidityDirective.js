aba29d9964b50ed724737efc9fedd0adf484582b
Andreas Svendby <andreas.svendby@nav.no>
2014-02-06 10:30:12 +0100

    Fjernet post fra direktiv siden standard link er post

--- web/src/main/webapp/js/app/directives/dagpenger/sjekkBolkValidityDirective.js
+++ web/src/main/webapp/js/app/directives/dagpenger/sjekkBolkValidityDirective.js
@@ -1,51 +1,49 @@
 angular.module('nav.sjekkBoklerValiditet', [])
     .directive('sjekkValidert', ['data', function (data) {
         return {
-            link: {
-                post: function (scope, element, attrs) {
-                    var skalSettesTilValidVedForsteApning = scope.$eval(attrs.sjekkValidert);
-                    var erValidert = data.finnFaktum('bolker').properties[attrs.id] === "true";
-                    if (erValidert) {
-                        element.addClass('validert');
-                    };
+            link: function (scope, element, attrs) {
+                var skalSettesTilValidVedForsteApning = scope.$eval(attrs.sjekkValidert);
+                var erValidert = data.finnFaktum('bolker').properties[attrs.id] === "true";
+                if (erValidert) {
+                    element.addClass('validert');
+                };
 
-                    if (!skalSettesTilValidVedForsteApning) {
-                        scope.$watch(
-                            function() {
-                                var form = element.find('[data-ng-form]');
-                                return form.length > 0 && form.is('.ng-dirty');
-                            },
-                            function(newVal, oldVal) {
-                                if (newVal === oldVal) {
-                                    return;
-                                }
-                                if (newVal) {
-                                    var bolkerFaktum = data.finnFaktum('bolker');
-                                    bolkerFaktum.properties[attrs.id] = "false";
-                                    bolkerFaktum.$save();
+                if (!skalSettesTilValidVedForsteApning) {
+                    scope.$watch(
+                        function() {
+                            var form = element.find('[data-ng-form]');
+                            return form.length > 0 && form.is('.ng-dirty');
+                        },
+                        function(newVal, oldVal) {
+                            if (newVal === oldVal) {
+                                return;
+                            }
+                            if (newVal) {
+                                var bolkerFaktum = data.finnFaktum('bolker');
+                                bolkerFaktum.properties[attrs.id] = "false";
+                                bolkerFaktum.$save();
 
-                                    if (element.hasClass('validert')) {
-                                        element.removeClass('validert');
-                                    }
+                                if (element.hasClass('validert')) {
+                                    element.removeClass('validert');
                                 }
                             }
-                        );
-                    } else if (!erValidert) {
-                        var unregister = scope.$watch(
-                            function() {
-                                return scope.$eval(attrs.isOpen);
-                            },
-                            function(newVal) {
-                                if (newVal) {
-                                    var bolkerFaktum = data.finnFaktum('bolker');
-                                    bolkerFaktum.properties[attrs.id] = "true";
-                                    bolkerFaktum.$save();
-                                    element.addClass('validert');
-                                    unregister();
-                                }
+                        }
+                    );
+                } else if (!erValidert) {
+                    var unregister = scope.$watch(
+                        function() {
+                            return scope.$eval(attrs.isOpen);
+                        },
+                        function(newVal) {
+                            if (newVal) {
+                                var bolkerFaktum = data.finnFaktum('bolker');
+                                bolkerFaktum.properties[attrs.id] = "true";
+                                bolkerFaktum.$save();
+                                element.addClass('validert');
+                                unregister();
                             }
-                        );
-                    }
+                        }
+                    );
                 }
             }
         }
