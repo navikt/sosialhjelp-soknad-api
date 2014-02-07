ce87c421ed1372e65496e0bb8cb3eeb63f3c0670
Hein Haraldsen <hein.haraldsen@nav.no>
2014-02-07 13:56:52 +0100

    Lagt på makslengde på fritekstfelt

--- web/src/main/webapp/js/app/controllers/dagpenger/dagpengerController.js
+++ web/src/main/webapp/js/app/controllers/dagpenger/dagpengerController.js
@@ -29,7 +29,9 @@ angular.module('nav.dagpenger', [])
 			$scope.$broadcast('VALIDER_EGENNAERING', form.egennaeringForm);
 			$scope.$broadcast('VALIDER_VERNEPLIKT', form.vernepliktForm);
 			$scope.$broadcast('VALIDER_REELLARBEIDSSOKER', form.reellarbeidssokerForm);
+            $scope.$broadcast('VALIDER_FRITEKST', form.fritekstForm);
 			$scope.$broadcast('VALIDER_DAGPENGER', form);
+
             $scope.fremdriftsindikator.laster = true;
 			$timeout(function () {
 				$scope.validateForm(form.$invalid);
