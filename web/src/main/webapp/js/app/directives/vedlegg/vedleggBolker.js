f57bf4add2cce33c408578e99f6ea52349df0959
Ingvild Indrebø <ingvild.indrebo@nav.no>
2014-02-07 14:11:57 +0100

    [TG-vedlegFeilm] åpner annet-bolk når den blir lagt til

--- web/src/main/webapp/js/app/directives/vedlegg/vedleggBolker.js
+++ web/src/main/webapp/js/app/directives/vedlegg/vedleggBolker.js
@@ -2,17 +2,17 @@ angular.module('nav.vedleggbolker', [])
     .directive('triggBolker', ['$timeout', function ($timeout) {
         return {
             require: 'form',
-            link: function($scope, element, attrs, ctrl) {
-                $timeout(function() {
-                   var bolker = $(".accordion-group");
-                   var bolkerMedFeil = finnBolkerMedFeil(bolker);
+            link: function ($scope, element, attrs, ctrl) {
+                $timeout(function () {
+                    var bolker = $(".accordion-group");
+                    var bolkerMedFeil = finnBolkerMedFeil(bolker);
 
                     apneForsteBolkMedFeil(bolkerMedFeil[0]);
                 }, 500);
 
                 //Bolker med feil og som er lukket skal åpnes ved validering av hele vedleggsiden
-                element.find('#til-oppsummering').bind('click', function() {
-                    if(ctrl.$invalid) {
+                element.find('#til-oppsummering').bind('click', function () {
+                    if (ctrl.$invalid) {
                         var alleBolker = finnAlleBolker();
                         var alleBolkerMedFeil = finnBolkerMedFeil(alleBolker);
                         apneBolkerSomIkkeErApenFraFor(alleBolkerMedFeil);
@@ -20,9 +20,9 @@ angular.module('nav.vedleggbolker', [])
                 });
 
                 function apneBolkerSomIkkeErApenFraFor(bolker) {
-                    for(var i =0; i<bolker.length; i++) {
+                    for (var i = 0; i < bolker.length; i++) {
                         var bolk = $(bolker[i]);
-                        if(!(bolk.hasClass('open'))) {
+                        if (!(bolk.hasClass('open'))) {
                             bolk.find('.accordion-toggle').trigger('click');
                         }
                     }
@@ -39,16 +39,28 @@ angular.module('nav.vedleggbolker', [])
                 function finnAlleBolker() {
                     return $(".accordion-group");
                 }
+
                 function finnBolkerMedFeil(bolker) {
                     var bolkerMedFeil = [];
 
-                    for(var i = 0; i<bolker.length; i++) {
-                        if(bolkHarFeil(bolker[i])) {
+                    for (var i = 0; i < bolker.length; i++) {
+                        if (bolkHarFeil(bolker[i])) {
                             bolkerMedFeil.push(bolker[i]);
                         }
                     }
                     return bolkerMedFeil;
                 }
+            }
+        };
+    }])
+    .directive('apneAnnetVedlegg', ['$timeout', function ($timeout) {
+        return {
+            link: function ($scope, element) {
+                element.bind('click', function () {
+                    $timeout(function () {
+                        $(".accordion-toggle").last().trigger('click');
+                    }, 200)
+                })
             }
         };
     }]);
