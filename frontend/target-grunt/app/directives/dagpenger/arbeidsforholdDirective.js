a40df692ea0dd1f7c16d5fac48f284291f370e11
Andreas Svendby <andreas.svendby@nav.no>
2014-02-11 16:30:52 +0100

    [TG] Lagt til nye moduler med npm. Lagt med target-grunt

---
+++ frontend/target-grunt/app/directives/dagpenger/arbeidsforholdDirective.js
@@ -0,0 +1,30 @@
+angular.module('nav.arbeidsforhold.directive',[])
+	.directive('lagreArbeidsforhold', function () {
+        return function ($scope, element, attrs) {
+            var eventType;
+            switch (element.attr('type')) {
+                case 'radio':
+                case 'checkbox':
+                    eventType = 'change';
+                    break;
+                default:
+                    eventType = 'blur';
+            }
+
+            element.bind(eventType, function () {
+                var verdi = element.val();
+                if (element.attr('type') === 'checkbox') {
+                    verdi = element.is(':checked');
+                }
+                $scope.$emit('OPPDATER_OG_LAGRE_ARBEIDSFORHOLD');
+            });
+        };
+    })
+
+    .directive('legg-til-arbeidsforhold', function () {
+        return function ($scope, element, attrs) {
+            element.click(function () {
+                
+            })
+        }
+    });
