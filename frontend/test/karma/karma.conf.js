a64a702a716994a996ba1d975f6f561d52a4e49c
Andreas Svendby <andreas.svendby@nav.no>
2014-02-12 10:18:47 +0100

    [TG-Grunt]: Fikset tester. Fjernet tester fra web

--- frontend/test/karma/karma.conf.js
+++ frontend/test/karma/karma.conf.js
@@ -69,8 +69,7 @@ module.exports = function (config) {
         // - Safari (only Mac)
         // - PhantomJS
         // - IE (only Windows)
-        browsers: ['Chrome'],
-//        browsers: ['Chrome', 'Firefox', 'IE'],
+        browsers: ['Chrome', 'Firefox', 'IE'],
 
         //plugins: ['karma-phantomjs-runner', 'karma-jasmine'],
 
