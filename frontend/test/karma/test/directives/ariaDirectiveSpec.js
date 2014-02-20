aa653ba8fe106f43186d55174caafe557b091826
Ketil Velle <ketil.s.velle@nav.no>
2014-02-20 14:39:08 +0100

    test av branchcoverage

--- frontend/test/karma/test/directives/ariaDirectiveSpec.js
+++ frontend/test/karma/test/directives/ariaDirectiveSpec.js
@@ -14,6 +14,8 @@ describe('ariaModule', function () {
 
     describe("navAriaHidden", function() {
         it("should add class aria-hidden", function() {
+            $scope.$apply();
+            $scope.$digest();
             $scope.isOpen = false;
             $scope.$apply();
             expect(element.attr("aria-hidden")).toBe("true");
