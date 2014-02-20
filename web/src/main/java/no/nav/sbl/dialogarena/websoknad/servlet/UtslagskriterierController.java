b889f572df1fa0e813400f4ce678f90145f76d87
Hein Haraldsen <hein.haraldsen@nav.no>
2014-02-20 16:14:43 +0100

    Utslagskriterier fra TPS

--- web/src/main/java/no/nav/sbl/dialogarena/websoknad/servlet/UtslagskriterierController.java
+++ web/src/main/java/no/nav/sbl/dialogarena/websoknad/servlet/UtslagskriterierController.java
@@ -4,6 +4,7 @@ import no.nav.sbl.dialogarena.soknadinnsending.business.domain.PersonAlder;
 import no.nav.sbl.dialogarena.soknadinnsending.business.person.Personalia;
 import no.nav.sbl.dialogarena.soknadinnsending.business.person.PersonaliaService;
 import no.nav.sbl.dialogarena.soknadinnsending.consumer.personinfo.PersonInfoConnector;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,10 +15,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static no.nav.modig.core.context.SubjectHandler.getSubjectHandler;
+import static org.slf4j.LoggerFactory.getLogger;
 
 @Controller
 public class UtslagskriterierController {
-
+    private static final Logger logger = getLogger(UtslagskriterierController.class);
     @Inject
     private PersonaliaService personaliaService;
 
@@ -33,9 +35,16 @@ public class UtslagskriterierController {
         PersonInfoConnector.Status status = personInfoConnector.hentArbeidssokerStatus(uid);
         utslagskriterierResultat.put("registrertArbeidssøker", status.name());
 
-        Personalia personalia;
+        Personalia personalia = null;
 
-            personalia = personaliaService.hentPersonalia(uid);
+        try
+        {
+               personalia = personaliaService.hentPersonalia(uid);
+        }
+        catch (Exception e)
+        {
+            logger.error("Kunne ikke hente personalia" + e.getMessage());
+        }
             utslagskriterierResultat.put("gyldigAlder", new PersonAlder(uid).sjekkAlder().toString());
             utslagskriterierResultat.put("bosattINorge", harNorskAdresse(personalia).toString());
             utslagskriterierResultat.put("registrertAdresse", personalia.getGjeldendeAdresse().getAdresse());
