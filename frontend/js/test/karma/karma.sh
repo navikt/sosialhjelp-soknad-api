419fec7d787326ff43168b1f1b68a53bc70bbc92
Ketil Velle <ketil.s.velle@nav.no>
2014-02-20 08:51:31 +0100

    Nytt sonarforsok med egen pom

--- frontend/js/test/karma/karma.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo ""
-echo "Starting Karma Server (http://karma-runner.github.io)"
-echo "-------------------------------------------------------------------"
-
-karma start karma.conf.js $*
