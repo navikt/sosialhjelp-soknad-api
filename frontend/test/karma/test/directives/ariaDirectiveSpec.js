3e4b46a1cc1a95514f7996081046ddda0a2ed3d2
Ingvild Indrebø <ingvild.indrebo@nav.no>
2014-02-26 10:28:28 +0100

    Fkset semicolon

--- frontend/test/karma/test/directives/ariaDirectiveSpec.js
+++ frontend/test/karma/test/directives/ariaDirectiveSpec.js
@@ -10,22 +10,22 @@ describe('ariaModule', function () {
         element = angular.element('<div nav-aria-expanded="isOpen" nav-aria-hidden="!isOpen">tester aria</div>');
         $compile(element)($scope);
         $scope.$apply();
-    }))
+    }));
 
     describe("navAriaHidden", function() {
         it("should add class aria-hidden", function() {
             $scope.isOpen = false;
             $scope.$apply();
             expect(element.attr("aria-hidden")).toBe("true");
-        })
-    })
+        });
+    });
 
      describe("navAriaHidden", function() {
         it("should add class aria-hidden", function() {
             $scope.isOpen = false;
             $scope.$apply();
             expect(element.attr("aria-expanded")).toBe("false");
-        })
-    })
+        });
+    });
 });
 
