0c691dcd050c416a0ae4ccdaf5a32f39007fe897
Ingvild Indrebø <ingvild.indrebo@nav.no>
2014-02-26 10:08:59 +0100

    Flyttet personalia ut til personaliaCtrlSpec

--- frontend/test/karma/test/controllers/personaliaControllerSpec.js
+++ frontend/test/karma/test/controllers/personaliaControllerSpec.js
@@ -1,122 +1,196 @@
-describe('Personalia domene', function () {
+(function () {
+    'use strict';
 
-    beforeEach(
-        module('app.services', 'app.brukerdata', 'app.controllers')
-    );
+    describe('DagpengerControllere', function () {
+        var scope, ctrl, form, element, barn, $httpBackend, event, location, epost;
+        event = $.Event("click");
 
-    describe('Personalia controller', function () {
+        beforeEach(module('ngCookies', 'app.services'));
+        beforeEach(module('app.controllers', 'nav.feilmeldinger'));
 
-        var scope, ctrl, routeParams;
+        beforeEach(module(function ($provide) {
+            var fakta = [
+                {}
+            ];
 
-        beforeEach(inject(function ($rootScope, $controller) {
-            routeParams = {};
-
-            scope = $rootScope.$new();
-            routeParams.soknadId = 1;
-            ctrl = $controller('PersonaliaCtrl', {
-                $scope: scope,
-                $routeParams: routeParams,
-                cms: {},
-                data: {},
-                personalia: {
-                    fakta: {
-                        fnr: {
-                            soknadId: 1,
-                            key: "fnr",
-                            value: "06025800174",
-                            type: "System"
-                        },
-                        sammensattnavn: {
-                            soknadId: 1,
-                            key: "sammensattnavn",
-                            value: "ENGELSK TESTFAMILIEN",
-                            type: "System"
-                        },
-                        mellomnavn: {
-                            soknadId: 1,
-                            key: "mellomnavn",
-                            value: "",
-                            type: "System"
-                        },
-                        fornavn: {
-                            soknadId: 1,
-                            key: "fornavn",
-                            value: "ENGELSK",
-                            type: "System"
-                        },
-                        gjeldendeAdresseType: {
-                            soknadId: 1,
-                            key: "gjeldendeAdresseType",
-                            value: "BOSTEDSADRESSE",
-                            type: "System"
-                        },
-                        etternavn: {
-                            soknadId: 1,
-                            key: "etternavn",
-                            value: "TESTFAMILIEN",
-                            type: "System"
+            $provide.value("data", {
+                fakta: fakta,
+                finnFaktum: function (key) {
+                    var res = null;
+                    fakta.forEach(function (item) {
+                        if (item.key == key) {
+                            res = item;
                         }
-                    }
+                    });
+                    return res;
+                },
+                finnFakta: function (key) {
+                    var res = [];
+                    fakta.forEach(function (item) {
+                        if (item.key === key) {
+                            res.push(item);
+                        }
+                    });
+                    return res;
+                },
+                leggTilFaktum: function (faktum) {
+                    fakta.push(faktum);
+                },
+                land: 'Norge',
+                soknad: {
+                    soknadId: 1
+                },
+                config: {"soknad.sluttaarsak.url": "sluttaarsakUrl",
+                    "dittnav.link.url": "dittnavUrl",
+                    "soknad.lonnskravskjema.url": "lonnskravSkjemaUrl",
+                    "soknad.permitteringsskjema.url": "permiteringUrl",
+                    "minehenvendelser.link.url": "minehenvendelserUrl",
+                    "soknad.inngangsporten.url": "inngangsportenUrl",
+                    "soknad.skjemaveileder.url": "skjemaVeilederUrl",
+                    "soknad.brukerprofil.url": "brukerprofilUrl",
+                    "soknad.reelarbeidsoker.url": "reelArbeidsokerUrl",
+                    "soknad.alderspensjon.url": "alderspensjonUrl",
+                    "soknad.dagpengerbrosjyre.url": "dagpengerBrosjyreUrl" },
+
+                slettFaktum: function (faktumData) {
+                    fakta.forEach(function (item, index) {
+                        if (item.faktumId === faktumData.faktumId) {
+                            fakta.splice(index, 1);
+                        }
+                    });
+                },
+                utslagskriterier: {
                 }
             });
+            $provide.value("cms", {'tekster': {'barnetillegg.nyttbarn.landDefault': ''}});
+            $provide.value("$routeParams", {});
+        })
+        );
+
+        beforeEach(inject(function ($injector, $rootScope, $controller, $compile) {
+            $httpBackend = $injector.get('$httpBackend');
+            $httpBackend.expectGET('../js/app/directives/feilmeldinger/feilmeldingerTemplate.html').
+                respond('');
+
+            scope = $rootScope;
+            scope.runValidationBleKalt = false;
+            scope.runValidation = function () {
+                scope.runValidationBleKalt = true;
+            };
+
+            scope.apneTab = function () {
+            };
+            scope.lukkTab = function () {
+            };
+            scope.settValidert = function () {
+            };
+            scope.leggTilStickyFeilmelding = function () {
+            };
+
+            element = angular.element(
+                '<form name="form">' +
+                    '<div form-errors></div>' +
+                    '<input type="text" ng-model="scope.barn.properties.fodselsdato" name="alder"/>' +
+                    '<input type="hidden" data-ng-model="underAtten.value" data-ng-required="true"/>' +
+                    '<input type="email" data-ng-model="epost.value" name="epost" data-ng-required="true"/>' +
+                    '</form>'
+            );
+
+            $compile(element)(scope);
+            scope.$digest();
+            form = scope.form;
+            barn = form.alder;
+            epost = form.epost;
+            element.scope().$apply();
         }));
+        describe('PersonaliaCtrl', function () {
+            beforeEach(inject(function ($controller, data) {
+                scope.data = data;
+                var faktum = {
+                    key: 'personalia',
+                    properties: {
+                        alder: "61",
+                        kjonn: 'm',
+                        statsborgerskap: 'NOR'
+                    },
+                    faktumId: 111
+                };
+                scope.data.leggTilFaktum(faktum);
+                ctrl = $controller('PersonaliaCtrl', {
+                    $scope: scope
+                });
+            }));
 
-        // TODO: Skriv om tester og muligens controller
-//        it('skal ha bostedsadresse', function () {
-//            scope.personalia.fakta.adresser = [
-//                {
-//                    soknadId: 1,
-//                    type: "BOSTEDSADRESSE",
-//                    gatenavn: "",
-//                    husnummer: "8",
-//                    husbokstav: "",
-//                    postnummer: "1878",
-//                    poststed: "HÆRLAND",
-//                    land: "NOR",
-//                    gyldigFra: null,
-//                    gyldigTil: null,
-//                    postboksNavn: null,
-//                    postboksNummer: null,
-//                    adresseEier: null,
-//                    utenlandsAdresse: null
-//                }
-//            ];
-//            scope.$apply();
-//            expect(scope.harBostedsAdresse()).toBe(true);
-//            expect(scope.harMidlertidigAdresse()).toBe(false);
-//        })
+            it('personalia skal inneholde data', function () {
+                expect(scope.personalia).toNotBe(undefined);
+            });
+            it('brukerprofilUrl skal bli satt til riktig url', function () {
+                expect(scope.brukerprofilUrl).toBe("brukerprofilUrl");
+            });
+            it('hvis personen er en mann så skal erMann returnere true', function () {
+                expect(scope.erMann()).toEqual(true);
+            });
+            it('hvis personen er en mann så skal erKvinne returnere false', function () {
+                expect(scope.erKvinne()).toEqual(false);
+            });
+            it('hvis personalia sa skal harHentetPersonalia returnere true', function () {
+                expect(scope.harHentetPersonalia()).toEqual(true);
+            });
+            it('hvis statsborgerskap er Norge sa skal erUtenlandskStatsborger returnere false', function () {
+                expect(scope.erUtenlandskStatsborger()).toEqual(false);
+            });
+            it('skal kjøre metodene lukkTab og settValidert nar valider kjores', function () {
+                spyOn(scope, "lukkTab");
+                spyOn(scope, "settValidert");
+                scope.valider(false);
+                expect(scope.lukkTab).toHaveBeenCalledWith('personalia');
+                expect(scope.settValidert).toHaveBeenCalledWith('personalia');
+            });
+        });
+        describe('PersonaliaCtrlKvinne', function () {
+            beforeEach(inject(function ($controller, data) {
+                scope.data = data;
+                var faktum = {
+                    key: 'personalia',
+                    properties: {
+                        alder: "61",
+                        kjonn: 'k'
+                    }
+                };
+                scope.data.leggTilFaktum(faktum);
+                ctrl = $controller('PersonaliaCtrl', {
+                    $scope: scope
+                });
+            }));
 
-//        it('har postboksadresse hvis postboksnummer er satt', function () {
-//            expect(scope.harPostboksAdresse()).toBe(true);
-//        })
-//
-//        it('har postboksadresse hvis postboksnavn er satt', function () {
-//            expect(scope.harPostboksAdresse()).toBe(true);
-//        })
+            it('hvis personen er en kvinne så skal erMann returnere false', function () {
+                expect(scope.erMann()).toEqual(false);
+            });
+            it('hvis personen er en kvinne så skal erKvinne returnere true', function () {
+                expect(scope.erKvinne()).toEqual(true);
+            });
+        });
+        describe('PersonaliaCtrlIkkeKjonn', function () {
+            beforeEach(inject(function ($controller, data) {
+                scope.data = data;
+                var faktum = {
+                    key: 'personalia',
+                    properties: {
+                        alder: "61"
+                    }
+                };
+                scope.data.leggTilFaktum(faktum);
+                ctrl = $controller('PersonaliaCtrl', {
+                    $scope: scope
+                });
+            }));
 
-//        it('skal ikke ha folkeregistrert adresse når man har midlertidig adresse i utlandet', function () {
-//            expect(scope.harBostedsadresseOgIngenMidlertidigAdresse()).toBe(false);
-//            expect(scope.harUtenlandskAdresse()).toBe(false);
-//        });
-
-//        it('skal ikke ha folkeregistrert adresse når man har midlertidig omrodeadresse', function () {
-//            expect(scope.harMidlertidigAdresse()).toBe(true);
-//            expect(scope.harUtenlandskPostAdresse()).toBe(false);
-//            expect(scope.harUtenlandskAdresse()).toBe(false);
-//        })
-
-        //TODO: For adresse-testing
-//            $scope.personalia.fakta.adresser.push({"soknadId":1,"type":"MIDLERTIDIG_POSTADRESSE_NORGE","gatenavn":"Kirkeveien","husnummer":"55","husbokstav":"D","postnummer":"7000","poststed":"Trondheim","land":null,"gyldigTil":1412373600000,"gyldigFra":1380895717011,"postboksNavn":"POSTBOKS","postboksNummer":"1234","adresseEier":"Per P. Nilsen","utenlandsAdresse":null});
-//            $scope.personalia.fakta.adresser.push({"soknadId":1,"type":"MIDLERTIDIG_POSTADRESSE_NORGE","gatenavn":"Kirkeveien","husnummer":"55","husbokstav":"D","postnummer":"7000","poststed":"Trondheim","land":null,"gyldigTil":1412373600000,"gyldigFra":1380895717011,"postboksNavn":null,"postboksNummer":null,"adresseEier":"Per P. Nilsen","utenlandsAdresse":null});
-//            $scope.personalia.fakta.adresser.push({"soknadId":1,"type":"MIDLERTIDIG_POSTADRESSE_UTLAND","gatenavn":null,"husnummer":null,"husbokstav":null,"postnummer":null,"poststed":null,"land":"SVERIGE","gyldigTil":1412373600000,"gyldigFra":1380895717011,"postboksNavn":null,"postboksNummer":null,"adresseEier":"Per P. Nilsen","utenlandsAdresse":["Öppnedvägen 22","1234, Udevalla"]});
-//            Mangler eksempel på mildertidig omrodeadresse
-
-//             $scope.personalia.fakta.adresser = [];
-//             $scope.personalia.fakta.adresser.push({"soknadId":1,"type":"MIDLERTIDIG_POSTADRESSE_NORGE","gatenavn":"Kirkeveien","husnummer":"55","husbokstav":"D","postnummer":"7000","poststed":"Trondheim","land":null,"gyldigTil":1412373600000,"gyldigFra":1380895717011,"postboksNavn":null,"postboksNummer":null,"adresseEier":"Per P. Nilsen","utenlandsAdresse":null});
-//             $scope.personalia.fakta.adresser.push({"soknadId":1,"type":"MIDLERTIDIG_POSTADRESSE_UTLAND","gatenavn":null,"husnummer":null,"husbokstav":null,"postnummer":null,"poststed":null,"land":"SVERIGE","gyldigTil":1412373600000,"gyldigFra":1380895717011,"postboksNavn":null,"postboksNummer":null,"adresseEier":"Per P. Nilsen","utenlandsAdresse":["Öppnedvägen 22","1234, Udevalla"]});
-//             $scope.personalia.fakta.adresser.push({"soknadId":1,"type":"UTENLANDSK_ADRESSE","gatenavn":null,"husnummer":null,"husbokstav":null,"postnummer":null,"poststed":null,"land":"SVERIGE","gyldigTil":1412373600000,"gyldigFra":1380895717011,"postboksNavn":null,"postboksNummer":null,"adresseEier":"Per P. Nilsen","utenlandsAdresse":["Öppnedvägen 22","1234, Udevalla"]});
-//            $scope.personalia.fakta.adresser.push({"soknadId":1,"type":"BOSTEDSADRESSE","gatenavn":"Blåsbortveien","husnummer":"24","husbokstav":"","postnummer":"0368","poststed":"Malmö","land":"SVERIGE","gyldigFra":null,"gyldigTil":null,"utenlandsAdresse":null,"adresseEier":null,"postboksNummer":null,"postboksNavn":null});
-
-
+            it('hvis personen ikke har et kjønn så skal erMann returnere false', function () {
+                expect(scope.erMann()).toEqual(false);
+            });
+            it('hvis personen ikke har et kjønn så skal erKvinne returnere true', function () {
+                expect(scope.erKvinne()).toEqual(false);
+            });
+        });
     });
-});
+}());
