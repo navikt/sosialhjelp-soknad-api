a639961be030368bc64b62ceed23229cfaeb64ab
Ingvild Indrebø <ingvild.indrebo@nav.no>
2014-03-05 08:48:02 +0100

    Tester dagpengerDirectiveSpec

---
+++ frontend/test/karma/test/directives/dagpengerDirectiveSpec.js
@@ -0,0 +1,72 @@
+describe('stegindikator', function () {
+    var element, scope, timeout;
+
+    beforeEach(module('nav.dagpengerdirective', 'nav.cmstekster', 'templates-main'));
+
+    beforeEach(module(function ($provide) {
+        $provide.value("cms", {'tekster': {'tittel.key': 'Min tittel'}});
+        $provide.value("data", {});
+        $provide.value("$cookieStore", {
+            get: function() {
+                return false;
+            }
+        });
+    }));
+
+    beforeEach(inject(function ($compile, $rootScope, $timeout) {
+        element = angular.element(
+            '<form name="">' +
+                '<div data-apne-bolker> ' +
+            '</form>');
+
+        $compile(element)($rootScope);
+        $rootScope.$apply();
+        scope = element.find('div').scope();
+        scope.apneTab = function(){};
+        timeout = $timeout;
+    }));
+
+    describe('apneBolker', function() {
+        it('apneTab skal åpne første invalid bolk hvis cookien ikke er satt', function() {
+            spyOn(scope, 'apneTab');
+            timeout.flush();
+            expect(scope.apneTab).toHaveBeenCalled();
+        });
+    });
+});
+describe('stegindikator', function () {
+    var element, scope, timeout;
+
+    beforeEach(module('nav.dagpengerdirective', 'nav.cmstekster', 'templates-main'));
+
+    beforeEach(module(function ($provide) {
+        $provide.value("cms", {'tekster': {'tittel.key': 'Min tittel'}});
+        $provide.value("data", {});
+        $provide.value("$cookieStore", {
+            get: function() {
+                return true;
+            }
+        });
+    }));
+
+    beforeEach(inject(function ($compile, $rootScope, $timeout) {
+        element = angular.element(
+            '<form name="">' +
+                '<div data-apne-bolker> ' +
+                '</form>');
+
+        $compile(element)($rootScope);
+        $rootScope.$apply();
+        scope = element.find('div').scope();
+        scope.apneTab = function(){};
+        timeout = $timeout;
+    }));
+
+    describe('apneBolker', function() {
+        it('apneTab skal ikke kalles hvis cookien er satt', function() {
+            spyOn(scope, 'apneTab');
+            expect(scope.apneTab).wasNotCalled();
+        });
+    });
+});
+
