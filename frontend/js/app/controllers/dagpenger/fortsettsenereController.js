661769cd268a9e4c5ab83b90e177c46fdc153f58
Hein Haraldsen <hein.haraldsen@nav.no>
2014-03-05 13:58:22 +0100

    Endret navn på Fortsett Senere-lenke

--- frontend/js/app/controllers/dagpenger/fortsettsenereController.js
+++ frontend/js/app/controllers/dagpenger/fortsettsenereController.js
@@ -20,7 +20,7 @@ angular.module('nav.fortsettsenere', ['nav.cmstekster'])
 
             $scope.soknadId = data.soknad.soknadId;
 
-            $scope.dittNavUrl = data.config["dittnav.link.url"];
+            $scope.dittNavBaseUrl = data.config["dittnav.link.url"];
 
             $scope.fortsettSenere = function (form) {
                 $scope.$broadcast('RUN_VALIDATION' + form.$name);
