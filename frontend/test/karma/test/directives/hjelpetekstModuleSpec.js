223d52ec23c682cc3f268316a6a51c5df55b9de8
Ingvild Indrebø <ingvild.indrebo@nav.no>
2014-03-10 16:08:55 +0100

    Tester hjelpetekst

---
+++ frontend/test/karma/test/directives/hjelpetekstModuleSpec.js
@@ -0,0 +1,80 @@
+describe('hjelpetekst', function () {
+    var rootScope, element, scope, timeout, form, compile, event, event2;
+    event = $.Event("click");
+    event2 = $.Event("click");
+    event2.timeStamp = 1;
+
+    beforeEach(module('nav.hjelpetekst', 'nav.cmstekster', 'templates-main'));
+
+    beforeEach(module(function ($provide) {
+        $provide.value("cms", {'tekster':
+        {'hjelpetekst.tittel': 'Tittel hjelpetekst',
+        'hjelpetekst.tekst': 'Hjelpetekst tekst' }
+        });
+    }));
+
+    beforeEach(inject(function ($compile, $rootScope, $timeout) {
+        rootScope = $rootScope;
+        compile = $compile;
+        timeout = $timeout;
+        element = angular.element(
+            '<form name="form" data-trigg-bolker>' +
+                '<span data-nav-hjelpetekstelement data-tittel="{{ \'hjelpetekst.tittel\' | cmstekst }}"' +
+                'data-tekst="{{ \'hjelpetekst.tekst\' | cmstekst }}"></span>' +
+                '</form>');
+
+        $compile(element)(rootScope);
+        rootScope.$apply();
+
+        scope = element.find('div').first().isolateScope();
+        form = scope.form;
+    }));
+
+    beforeEach(function () {
+        jasmine.Clock.useMock();
+    });
+
+    describe('hjelpetekst', function () {
+        it('tittel og tekst skal bli satt til henholdsvis hjelpeteksttittelen og tekst', function () {
+            scope.toggleHjelpetekst();
+            scope.$apply();
+            var tittel = element.find('.liten-strek');
+            expect(tittel.text()).toBe("Tittel hjelpetekst");
+
+            var tekst = element.find('.mini');
+            expect(tekst.text()).toBe("Hjelpetekst tekst");
+        });
+        it('visHjelp skal være false', function () {
+            expect(scope.visHjelp).toBe(false);
+        });
+        it('visHjelp skal være true hvis false og toggleHjelpetekst()', function () {
+            expect(scope.visHjelp).toBe(false);
+            scope.toggleHjelpetekst();
+            expect(scope.visHjelp).toBe(true);
+        });
+        it('visHjelp skal være false hvis true og lukk())', function () {
+            expect(scope.visHjelp).toBe(false);
+            scope.toggleHjelpetekst();
+            expect(scope.visHjelp).toBe(true);
+            scope.lukk(event);
+            expect(scope.visHjelp).toBe(false);
+        });
+        it('hvisHjelp skal settes til false hvis det klikkes i dokumentet', function () {
+            expect(scope.visHjelp).toBe(false);
+            scope.toggleHjelpetekst();
+            expect(scope.visHjelp).toBe(true);
+            $(document).click();
+            timeout.flush();
+            expect(scope.visHjelp).toBe(false);
+        });
+        it('hvisHjelp skal ikke settes til false hvis det klikkes i dokumentet og timestampen til klikket er det samme som lukkEventTimestamp', function () {
+            expect(scope.visHjelp).toBe(false);
+            scope.toggleHjelpetekst();
+            expect(scope.visHjelp).toBe(true);
+            scope.stoppKlikk(event2);
+            $(document).click(event2);
+            timeout.flush();
+            expect(scope.visHjelp).toBe(true);
+        });
+    });
+});
