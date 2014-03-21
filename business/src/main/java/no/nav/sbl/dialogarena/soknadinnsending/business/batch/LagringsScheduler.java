dccf56cb4a4aa9c42016707f271027009522ca4b
Andreas Svendby <andreas.svendby@nav.no>
2014-03-21 14:24:13 +0100

    Småfikser

--- business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/batch/LagringsScheduler.java
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/batch/LagringsScheduler.java
@@ -2,6 +2,7 @@ package no.nav.sbl.dialogarena.soknadinnsending.business.batch;
 
 import no.nav.modig.lang.option.Optional;
 import no.nav.sbl.dialogarena.soknadinnsending.business.db.SoknadRepository;
+import no.nav.sbl.dialogarena.soknadinnsending.business.domain.SoknadInnsendingStatus;
 import no.nav.sbl.dialogarena.soknadinnsending.business.domain.WebSoknad;
 import no.nav.sbl.dialogarena.soknadinnsending.consumer.fillager.FillagerConnector;
 import org.joda.time.DateTime;
