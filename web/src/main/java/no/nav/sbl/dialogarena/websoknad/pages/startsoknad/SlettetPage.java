726d02fdbfc6538bca9f3abe67b7194e171c26ff
Andreas Svendby <andreas.svendby@nav.no>
2014-03-21 12:59:02 +0100

    Lagt til side for feilmelding dersom søknad er avbrutt av bruker

---
+++ web/src/main/java/no/nav/sbl/dialogarena/websoknad/pages/startsoknad/SlettetPage.java
@@ -0,0 +1,10 @@
+package no.nav.sbl.dialogarena.websoknad.pages.startsoknad;
+
+import no.nav.sbl.dialogarena.websoknad.pages.basepage.BasePage;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class SlettetPage extends BasePage {
+    public SlettetPage(PageParameters parameters) {
+        super(parameters);
+    }
+}
