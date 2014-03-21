c6d6c5d3c6cb9fd4bdbb50192d76c86ee4225254
Andreas Svendby <andreas.svendby@nav.no>
2014-03-21 12:57:09 +0100

    Fikset masse på håndtering av avluttede søknader
    Conflicts:
    
    	web/src/main/java/no/nav/sbl/dialogarena/websoknad/pages/startsoknad/StartSoknadPage.java

---
+++ web/src/main/java/no/nav/sbl/dialogarena/websoknad/pages/startsoknad/RedirectTilKvitteringPage.java
@@ -0,0 +1,9 @@
+package no.nav.sbl.dialogarena.websoknad.pages.startsoknad;
+
+import org.apache.wicket.markup.html.pages.RedirectPage;
+
+public class RedirectTilKvitteringPage extends RedirectPage {
+    public RedirectTilKvitteringPage(CharSequence url) {
+        super(url);
+    }
+}
