5f06e7cee67178746228982b131acf924feace16
Andreas Svendby <andreas.svendby@nav.no>
2014-03-21 12:57:09 +0100

    Fikset masse på håndtering av avluttede søknader

--- web/src/main/java/no/nav/sbl/dialogarena/websoknad/pages/startsoknad/StartSoknadPage.java
+++ web/src/main/java/no/nav/sbl/dialogarena/websoknad/pages/startsoknad/StartSoknadPage.java
@@ -1,18 +1,23 @@
 package no.nav.sbl.dialogarena.websoknad.pages.startsoknad;
 
+import no.nav.sbl.dialogarena.soknadinnsending.business.domain.exception.SoknadAvbruttException;
+import no.nav.sbl.dialogarena.soknadinnsending.business.domain.exception.SoknadAvsluttetException;
 import no.nav.sbl.dialogarena.soknadinnsending.business.service.SendSoknadService;
 import no.nav.sbl.dialogarena.soknadinnsending.sikkerhet.XsrfGenerator;
 import no.nav.sbl.dialogarena.websoknad.pages.basepage.BasePage;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.util.cookies.CookieUtils;
 import org.apache.wicket.util.string.StringValue;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.inject.Inject;
 
 public class StartSoknadPage extends BasePage {
     @Inject
-
     private SendSoknadService soknadService;
+
+    @Value("${minehenvendelser.link.url}")
+    private String mineHenvendelserUrl;
 
     public StartSoknadPage(PageParameters parameters) {
         super(parameters);
@@ -20,7 +25,13 @@ public class StartSoknadPage extends BasePage {
         StringValue brukerbehandlingId = getPageParameters().get("brukerbehandlingId");
         if (!brukerbehandlingId.isEmpty()) {
             new CookieUtils().remove("XSRF-TOKEN");
-            new CookieUtils().save("XSRF-TOKEN", XsrfGenerator.generateXsrfToken(soknadService.hentSoknadMedBehandlinsId(brukerbehandlingId.toString())));
+            try {
+                new CookieUtils().save("XSRF-TOKEN", XsrfGenerator.generateXsrfToken(soknadService.hentSoknadMedBehandlinsId(brukerbehandlingId.toString())));
+            } catch (SoknadAvsluttetException e) {
+                setResponsePage(new RedirectTilKvitteringPage(mineHenvendelserUrl + "?behandlingsId=" + brukerbehandlingId));
+            } catch (SoknadAvbruttException e) {
+                setResponsePage(SlettetPage.class);
+            }
         }
     }
 }
