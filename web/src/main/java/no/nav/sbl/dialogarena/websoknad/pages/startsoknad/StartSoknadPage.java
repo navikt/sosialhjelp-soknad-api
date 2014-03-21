cbb51f332d6a4c6a14e9a6161840699ad0ca5c85
Andreas Svendby <andreas.svendby@nav.no>
2014-03-21 13:33:28 +0100

    Fikset navn på metode i soknadservice

--- web/src/main/java/no/nav/sbl/dialogarena/websoknad/pages/startsoknad/StartSoknadPage.java
+++ web/src/main/java/no/nav/sbl/dialogarena/websoknad/pages/startsoknad/StartSoknadPage.java
@@ -26,7 +26,7 @@ public class StartSoknadPage extends BasePage {
         if (!brukerbehandlingId.isEmpty()) {
             new CookieUtils().remove("XSRF-TOKEN");
             try {
-                new CookieUtils().save("XSRF-TOKEN", XsrfGenerator.generateXsrfToken(soknadService.hentSoknadMedBehandlinsId(brukerbehandlingId.toString())));
+                new CookieUtils().save("XSRF-TOKEN", XsrfGenerator.generateXsrfToken(soknadService.hentSoknadMedBehandlingsId(brukerbehandlingId.toString())));
             } catch (SoknadAvsluttetException e) {
                 setResponsePage(new RedirectTilKvitteringPage(mineHenvendelserUrl + "?behandlingsId=" + brukerbehandlingId));
             } catch (SoknadAvbruttException e) {
