b2ac5d071e775fd46508b1f0614493cc38f4122b
Andreas Svendby <andreas.svendby@nav.no>
2014-03-25 09:57:08 +0100

    [HL1-Bugfix]: Lagt til feilmelding for startdato for kortvarig utdanning. Fikset sjekk for vedlegg for barnetillegg

--- frontend/test/karma/test/controllers/controllersSpec.js
+++ frontend/test/karma/test/controllers/controllersSpec.js
@@ -422,29 +422,6 @@
                 };
                 expect(scope.norskBarnIkkeFunnetITPS(barn)).toEqual(false);
             });
-            it('hvis barnet har inntekt så kreves vedlegg', function () {
-                var barn = {
-                    key: 'barn',
-                    properties: {ikkebarneinntekt: 'false',
-                        barnetillegg: 'true'}
-                };
-                expect(scope.kreverVedlegg(barn)).toEqual(true);
-            });
-            it('hvis barnet ikke har inntekt så kreves ikke vedlegg', function () {
-                var barn = {
-                    key: 'barn',
-                    properties: {ikkebarneinntekt: 'true'}
-                };
-                expect(scope.kreverVedlegg(barn)).toEqual(false);
-            });
-            it('hvis barnet er norsk og ikke funnet i tps så kreves vedlegg', function () {
-                var barn = {
-                    key: 'barn',
-                    properties: {land: 'NOR'},
-                    type: 'BRUKERREGISTRERT'
-                };
-                expect(scope.kreverVedlegg(barn)).toEqual(true);
-            });
             it('erGutt skal returnere true for barn med hannkjønn og false for barn med hunnkjønn', function () {
                 var gutt = {
                     key: 'barn',
