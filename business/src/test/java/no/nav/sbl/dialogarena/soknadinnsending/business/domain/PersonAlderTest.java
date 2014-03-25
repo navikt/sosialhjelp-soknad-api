85fbadb65965660df486a72766478b26b468c5cd
Ketil Velle <ketil.s.velle@nav.no>
2014-03-25 12:55:43 +0100

    HL1-fix fikset feil i personAlder, brukte tidligere alltid utslagskriteriealder (forste dag i maaneden)

--- business/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/business/domain/PersonAlderTest.java
+++ business/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/business/domain/PersonAlderTest.java
@@ -19,31 +19,31 @@ public class PersonAlderTest {
     @Test
     public void alderSkalVaere100ForPersonMedFNR_***REMOVED***() {
         PersonAlder alder = new PersonAlder("***REMOVED***");
-        assertEquals(100, alder.getAlder());
+        assertEquals(100, alder.getUtslagsAlder());
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void skalFaaExceptionDersomFnrErUgyldig() {
         PersonAlder alder = new PersonAlder("***REMOVED***");
-        assertEquals(100, alder.getAlder());
+        assertEquals(100, alder.getUtslagsAlder());
     }
 
     @Test
     public void alderSkalVaere105ForPersonMedFNR_***REMOVED***() {
         PersonAlder alder = new PersonAlder("***REMOVED***");
-        assertEquals(105, alder.getAlder());
+        assertEquals(105, alder.getUtslagsAlder());
     }
 
     @Test
     public void alderSkalVaere5ForPersonMedFNR_***REMOVED***() {
         PersonAlder alder = new PersonAlder("***REMOVED***");
-        assertEquals(5, alder.getAlder());
+        assertEquals(5, alder.getUtslagsAlder());
     }
 
     @Test
     public void skalReturnere66AarForPersonSomFyller67AarDenneMaaneden() {
         PersonAlder alder = new PersonAlder("***REMOVED***");
-        assertEquals(66, alder.getAlder());
+        assertEquals(66, alder.getUtslagsAlder());
     }
 
     @Test
@@ -52,13 +52,13 @@ public class PersonAlderTest {
         DateTimeUtils.setCurrentMillisFixed(nyIdag);
 
         PersonAlder alder = new PersonAlder("***REMOVED***");
-        assertEquals(67, alder.getAlder());
+        assertEquals(67, alder.getUtslagsAlder());
     }
 
     @Test
     public void alderSkalVaere100ForPersonMedDNummer_46071175367() {
         PersonAlder alder = new PersonAlder("46071175367");
-        assertEquals(2, alder.getAlder());
+        assertEquals(2, alder.getUtslagsAlder());
     }
     
     @Test
