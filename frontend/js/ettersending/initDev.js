0374b31a9059195a9efb8672a862cad532bb5d63
Andreas Svendby <andreas.svendby@nav.no>
2014-03-31 14:24:23 +0200

    [ES]: Lagt til mulighet for å legge til nye vedlegg. Lagt til mulighet for å slette vedlegg man har lagt til

--- frontend/js/ettersending/initDev.js
+++ frontend/js/ettersending/initDev.js
@@ -1,4 +1,7 @@
 angular.module('ettersending')
     .run(['$http', '$templateCache', function ($http, $templateCache) {
-
+        $http.get('../views/ettersending/nytt-vedlegg.html', {cache: $templateCache});
+        $http.get('../js/common/directives/markup/modalsideTemplate.html', {cache: $templateCache});
+        $http.get('../js/common/directives/markup/navinfoboksTemplate.html', {cache: $templateCache});
+        $http.get('../js/common/directives/markup/panelStandardBelystTemplate.html', {cache: $templateCache});
     }]);
