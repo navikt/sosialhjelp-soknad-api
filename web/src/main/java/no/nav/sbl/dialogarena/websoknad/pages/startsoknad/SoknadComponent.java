e0c9a65350062e0813e0a4f9a9526686a5a17333
Andreas Svendby <andreas.svendby@nav.no>
2014-04-02 14:21:01 +0200

    [ES] Fikset så SoknadComponent sjekke om man er i development mode eller ikke, og velger rett bootstrap-fil

--- web/src/main/java/no/nav/sbl/dialogarena/websoknad/pages/startsoknad/SoknadComponent.java
+++ web/src/main/java/no/nav/sbl/dialogarena/websoknad/pages/startsoknad/SoknadComponent.java
@@ -1,5 +1,6 @@
 package no.nav.sbl.dialogarena.websoknad.pages.startsoknad;
 
+import org.apache.wicket.RuntimeConfigurationType;
 import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.MarkupStream;
 import org.apache.wicket.markup.html.WebComponent;
@@ -30,21 +31,32 @@ public class SoknadComponent extends WebComponent {
 
     @Override
     public void onComponentTagBody(MarkupStream markupStream, ComponentTag openTag) {
-        String file = "META-INF/resources/views/bootstrap.html";
 
+
+        if (RuntimeConfigurationType.DEVELOPMENT.equals(getApplication().getConfigurationType())) {
+            bootstrapDev(markupStream, openTag);
+        } else {
+            bootstrapProd(markupStream, openTag);
+        }
+    }
+
+    private void bootstrapDev(MarkupStream markupStream, ComponentTag openTag) {
+        try {
+            File basedir = new File(WebApplication.get().getServletContext().getResource("/").toURI());
+            File devDir = new File(basedir, "../../../../frontend/views/built/bootstrapDev.html");
+            InputStream content = new FileInputStream(devDir);
+            replaceComponentTagBody(markupStream, openTag, copyToString(content, forName("UTF-8")));
+        } catch (IOException |URISyntaxException e) {
+            logger.warn("Problem med ressurslasting" + e);
+        }
+    }
+
+    private void bootstrapProd(MarkupStream markupStream, ComponentTag openTag) {
+        String file = "META-INF/resources/views/bootstrap.html";
         try (InputStream content = this.getClass().getClassLoader().getResourceAsStream(file)) {
             replaceComponentTagBody(markupStream, openTag, copyToString(content, forName("UTF-8")));
         } catch (IllegalArgumentException| IOException e) {
-            try {
-                File basedir = new File(WebApplication.get().getServletContext().getResource("/").toURI());
-                File devDir = new File(basedir, "../../../../frontend/views/built/bootstrapDev.html");
-                try(InputStream content = new FileInputStream(devDir)){
-                    replaceComponentTagBody(markupStream, openTag, copyToString(content, forName("UTF-8")));
-                }
-            } catch (IOException |URISyntaxException e2) {
-                logger.warn("Problem med ressurslasting " + file, e2);
-                //throw new ApplicationException("feilet under lasting av markup", e);
-            }
+            logger.warn("Problem med ressurslasting " + file, e);
         }
     }
 }
