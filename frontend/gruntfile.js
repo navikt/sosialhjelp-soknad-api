99e72883a10d939f29625db148ed6b1bbf1743ea
Andreas Svendby <andreas.svendby@nav.no>
2014-04-03 09:20:05 +0200

    [ES] Fikset path for bygging av bootstrap.html

--- frontend/gruntfile.js
+++ frontend/gruntfile.js
@@ -50,7 +50,7 @@ module.exports = function (grunt) {
             },
             test: {
                 src: 'views/bootstrapTemplate.html',
-                dest: 'target/classes/META-INF/resources/built/views/bootstrap.html',
+                dest: 'target/classes/META-INF/resources/views/built/bootstrap.html',
                 options: {
                     beautify: true,
                     relative: false,
@@ -81,7 +81,7 @@ module.exports = function (grunt) {
             },
             prod: {
                 src: 'views/bootstrapTemplateProd.html',
-                dest: 'target/classes/META-INF/resources/built/views/bootstrap.html',
+                dest: 'target/classes/META-INF/resources/views/built/bootstrap.html',
                 options: {
                     beautify: true,
                     relative: false,
