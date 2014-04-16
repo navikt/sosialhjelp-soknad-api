6318ff2b17205111cc3570deecc564cff5a46ab5
Andreas Svendby <andreas.svendby@nav.no>
2014-04-16 14:13:51 +0200

    [ES]: Fikset noen småting

--- frontend/js/common/controllers/bekreftelsesController.js
+++ frontend/js/common/controllers/bekreftelsesController.js
@@ -1,7 +1,7 @@
 angular.module('nav.bekreftelse', [])
     .controller('BekreftelsesCtrl', function ($scope, config, $window, $timeout, $routeParams) {
         $timeout(function() {
-            var mineHenveldelserBaseUrl = config["minehenvendelser.link.url"];
-            $window.location.href = mineHenveldelserBaseUrl + "?behandlingsId=" + $routeParams.behandlingsId;
+            var mineHenveldelserBaseUrl = config['minehenvendelser.link.url'];
+            $window.location.href = mineHenveldelserBaseUrl + '?behandlingsId=' + $routeParams.behandlingsId;
         }, 3000);
     });
