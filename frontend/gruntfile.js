d69ce2263c5c29af98b41688f5ca6b837e5f7065
Andreas Svendby <andreas.svendby@nav.no>
2014-04-16 14:39:52 +0200

    [ES] Skrudd av mangle på minifisering av css siden det tryner i legg til arbeidsforhold

--- frontend/gruntfile.js
+++ frontend/gruntfile.js
@@ -177,7 +177,7 @@ module.exports = function (grunt) {
 		uglify: {
 			options: {
 				banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> */\n',
-                mangle: true
+                mangle: false
 			},
 			my_target: {
 				files: {
