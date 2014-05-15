6444efb86c286be44e1e531e8d08f17b3d21bb22
Andreas Svendby <andreas.svendby@nav.no>
2014-05-15 10:13:34 +0200

    Fikset feilende tester. Måtte fjerne test for bodydirective, som ikke lengre setter klassen på body...

--- frontend/test/karma/test/common/directives/bodyDirectiveSpec.js
+++ frontend/test/karma/test/common/directives/bodyDirectiveSpec.js
@@ -1,27 +1,3 @@
-describe('stylingHelperDirective', function () {
-    var element;
-    var $scope;
-
-    beforeEach(module('nav.markup.bodydirective'));
-
-    beforeEach(inject(function ($compile, $rootScope, $timeout) {
-        $scope = $rootScope;
-        element = angular.element('<div styling-helper>' +
-                    '<div class="modalBoks">tester modalboks</div>' +
-                '</div>');
-        
-        $compile(element)($scope);
-        $scope.$apply();
-        $timeout.flush(1);
-    }));
-
-    describe("stylingHelper", function() {
-        it("should add class modalside på body", function() {
-            expect(angular.element("body").hasClass('modalside')).toBe(true);
-        });
-    });
-});
-
 describe('avbrytHelperDirective', function () {
     var element;
     var $scope;
