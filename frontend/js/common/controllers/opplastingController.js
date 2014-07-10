309b44216656bcd152a5e0d6e289df5245576c79
Andreas Svendby <andreas.svendby@nav.no>
2014-07-10 15:10:03 +0200

    Lagt til feilmelding dersom serveren gir generell feil på opplasting

--- frontend/js/common/controllers/opplastingController.js
+++ frontend/js/common/controllers/opplastingController.js
@@ -88,6 +88,12 @@ angular.module('nav.opplasting.controller', ['blueimp.fileupload', 'opplasting.m
                 } else {
                     errorCode = data.response().errorThrown;
                 }
+
+                if (errorCode === 'generell') {
+                    // Generell feil fra server fordi filstørrelsen er for stor. Sett rett key for CMS
+                    errorCode = 'opplasting.feilmelding.maksstorrelse';
+                }
+
                 $scope.fremdriftsindikator.laster = false;
                 $scope.data.opplastingFeilet = cms.tekster[errorCode];
                 $.each(data.files, function (index, file) {
