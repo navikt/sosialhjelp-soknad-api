91db2efac028e8739b9443f9a6cdf5fb259473d1
Ingvild Indrebø <ingvild.indrebo@nav.no>
2014-10-28 07:47:56 +0100

    fikset tester

--- frontend/test/karma/test/dagpenger/controllers/informasjonssideControllerSpec.js
+++ frontend/test/karma/test/dagpenger/controllers/informasjonssideControllerSpec.js
@@ -2,10 +2,10 @@
     'use strict';
 
     describe('InformasjonsSideCtrl', function () {
-        var scope, ctrl, form, element, $httpBackend, location;
+        var scope, ctrl, location;
 
-        beforeEach(module('ngCookies', 'sendsoknad.services', 'nav.modal'));
-        beforeEach(module('sendsoknad.controllers', 'nav.feilmeldinger'));
+        beforeEach(module('sendsoknad.services'));
+        beforeEach(module('sendsoknad.controllers'));
 
         beforeEach(module(function ($provide) {
             var fakta = [
@@ -23,65 +23,34 @@
                     });
                     return res;
                 },
-                finnFakta: function (key) {
-                    var res = [];
-                    fakta.forEach(function (item) {
-                        if (item.key === key) {
-                            res.push(item);
-                        }
-                    });
-                    return res;
-                },
                 leggTilFaktum: function (faktum) {
                     fakta.push(faktum);
                 },
-                land: 'Norge',
                 soknad: {
                     soknadId: 1
-                },
-                config: {"soknad.sluttaarsak.url": "sluttaarsakUrl",
-                    "dittnav.link.url": "dittnavUrl",
-                    "soknad.lonnskravskjema.url": "lonnskravSkjemaUrl",
-                    "soknad.permitteringsskjema.url": "permiteringUrl",
-                    "saksoversikt.link.url": saksoversiktUrl,
-                    "soknad.skjemaveileder.url": "skjemaVeilederUrl",
-                    "soknad.brukerprofil.url": "brukerprofilUrl",
-                    "soknad.reelarbeidsoker.url": "reelArbeidsokerUrl",
-                    "soknad.alderspensjon.url": "alderspensjonUrl",
-                    "soknad.dagpengerbrosjyre.url": "dagpengerBrosjyreUrl" },
-
-                slettFaktum: function (faktumData) {
-                    fakta.forEach(function (item, index) {
-                        if (item.faktumId === faktumData.faktumId) {
-                            fakta.splice(index, 1);
-                        }
-                    });
-                },
-                utslagskriterier: {
                 }
             });
-            $provide.value("cms", {'tekster': {'barnetillegg.nyttbarn.landDefault': ''}});
-            $provide.value("$routeParams", {});
+            $provide.value("cms", {});
         }));
 
-
-        beforeEach(inject(function ($controller, data) {
+        beforeEach(inject(function ($controller, $rootScope, data, $location, soknadService) {
+            scope = $rootScope;
+            location = $location;
             scope.data = data;
 
             ctrl = $controller('InformasjonsSideCtrl', {
                 $scope: scope
             });
 
-            scope.$apply();
+            scope.$digest();
         }));
 
-        it('harlestbrosjyre skal være satt til false hvis pathen ikke inneholder sendsoknad/soknad', function () {
+        it('harlestbrosjyre skal være satt til false hvis brosjyren ikke er lest', function () {
             expect(scope.utslagskriterier.harlestbrosjyre).toEqual(false);
         });
-        it('soknadErIkkeStartet skal returnere true hvis soknadErStartet', function () {
+        it('soknadErIkkeStartet skal returnere true hvis ikke soknadErStartet', function () {
             expect(scope.soknadErIkkeStartet()).toEqual(true);
         });
-
         it('soknadErIkkeFerdigstilt skal returnere true hvis soknadErFerdigstilt ikke er true', function () {
             expect(scope.soknadErIkkeFerdigstilt()).toEqual(true);
         });
@@ -107,42 +76,47 @@
             expect(scope.harLestBrosjyre).toHaveBeenCalled();
             expect(scope.harLestBrosjyre()).toNotBe(true);
         });
-        describe('InformasjonsSideCtrlAndreUtslagskriterier', function () {
 
-            beforeEach(inject(function ($controller, data, $location) {
-                scope.data = data;
-                location = $location;
-                ctrl = $controller('InformasjonsSideCtrl', {
-                    $scope: scope
-                });
+        it('soknadErStartet skal returnere false hvis erSoknadStartet ikke er true', function () {
+            expect(scope.soknadErStartet()).toEqual(false);
+        });
+        it('soknadErFerdigstilt skal returnere true hvis data.soknad sin status er ferdig', function () {
+            scope.data.soknad = {
+                status: "FERDIG"
+            }
+            scope.$digest();
+            expect(scope.soknadErFerdigstilt()).toEqual(true);
+        });
+        it('soknadErIkkeFerdigstilt skal returnere false hvis data.soknad sin status er ferdig', function () {
+            scope.data.soknad = {
+                status: "FERDIG"
+            }
+            scope.$digest();
 
-                scope.data.utslagskriterier.harlestbrosjyre = true;
+            expect(scope.soknadErIkkeFerdigstilt()).toEqual(false);
+        });
+        it('harLestBrosjyre skal returnere true hvis lest brosjyre', function () {
+            scope.utslagskriterier.harlestbrosjyre = true;
+            scope.$digest();
 
-                scope.$apply();
-            }));
+            expect(scope.harLestBrosjyre()).toEqual(true);
+        });
+        it('startSoknadDersomBrosjyreLest skal kalle startSoknad dersom bruker har lest brosjyre', function () {
+            spyOn(scope, 'startSoknad');
+            scope.utslagskriterier.harlestbrosjyre = true;
+            scope.$digest();
 
-            it('soknadErStartet skal returnere false hvis erSoknadStartet ikke er true', function () {
-                expect(scope.soknadErStartet()).toEqual(false);
-            });
-            it('soknadErFerdigstilt skal returnere true hvis data.soknad sin status er ferdig', function () {
-                expect(scope.soknadErFerdigstilt()).toEqual(true);
-            });
-            it('soknadErIkkeFerdigstilt skal returnere false hvis data.soknad sin status er ferdig', function () {
-                expect(scope.soknadErIkkeFerdigstilt()).toEqual(false);
-            });
-            it('harLestBrosjyre skal returnere true hvis lest brosjyre', function () {
-                expect(scope.harLestBrosjyre()).toEqual(true);
-            });
-            it('startSoknadDersomBrosjyreLest skal kalle startSoknad dersom bruker har lest brosjyre', function () {
-                spyOn(scope, 'startSoknad');
-                scope.startSoknadDersomBrosjyreLest();
-                expect(scope.startSoknad).toHaveBeenCalled();
-            });
-            it('forsettSoknadDersomBrosjyreLest skal endre path til /soknad dersom brosjyre er lest', function () {
-                spyOn(location, 'path');
-                scope.startSoknad();
-                expect(location.path).toHaveBeenCalledWith("/soknad");
-            });
+            scope.startSoknadDersomBrosjyreLest();
+            expect(scope.startSoknad).toHaveBeenCalled();
+        });
+        it('startSoknad skal sette fremdriftsindikator til true', function () {
+            scope.startSoknad();
+            expect(scope.fremdriftsindikator.laster).toBe(true);
+        });
+        it('startSoknad skal starte en ny søknad', function () {
+            scope.startSoknad();
+
+            expect(scope.fremdriftsindikator.laster).toBe(true);
         });
     });
-});
+}());
