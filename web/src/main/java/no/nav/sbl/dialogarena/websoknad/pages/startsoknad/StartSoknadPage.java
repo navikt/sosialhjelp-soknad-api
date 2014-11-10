e4ebaf9adeb37021f123c49bc8f7717f869da3db
Steffen Hageland <steffen.hageland@nav.no>
2014-11-10 10:49:38 +0100

    endringer som skulle blitt lagt ved forrige commit

--- web/src/main/java/no/nav/sbl/dialogarena/websoknad/pages/startsoknad/StartSoknadPage.java
+++ web/src/main/java/no/nav/sbl/dialogarena/websoknad/pages/startsoknad/StartSoknadPage.java
@@ -1,7 +1,8 @@
 package no.nav.sbl.dialogarena.websoknad.pages.startsoknad;
 
-import no.nav.sbl.dialogarena.websoknad.pages.SkjemaBootstrapFile;
 import no.nav.sbl.dialogarena.websoknad.pages.basepage.BasePage;
+import org.apache.wicket.behavior.AttributeAppender;
+import org.apache.wicket.markup.html.WebComponent;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 import static no.nav.sbl.dialogarena.websoknad.pages.SkjemaBootstrapFile.DAGPENGER;
@@ -10,7 +11,21 @@ import static no.nav.sbl.dialogarena.websoknad.pages.SkjemaBootstrapFile.GJENOPP
 public class StartSoknadPage extends BasePage {
     public StartSoknadPage(PageParameters parameters) {
         super(parameters);
-        SkjemaBootstrapFile skjematype = parameters.get("skjemanummer").toString().equals("NAV04-01.03") ? DAGPENGER : GJENOPPTAK;
-        add(new SoknadComponent("soknad", skjematype));
+        WebComponent soknad;
+        switch(parameters.get("skjemanummer").toString()) {
+            case "NAV04-01.03":
+                soknad = new SoknadComponent("soknad", DAGPENGER);
+                soknad.add(new AttributeAppender("data-ng-app", "sendsoknad"));
+                add(soknad);
+                break;
+            case "NAV04-16.03":
+                soknad = new SoknadComponent("soknad", GJENOPPTAK);
+                soknad.add(new AttributeAppender("data-ng-app", "gjenopptak"));
+                add(soknad);
+                break;
+            default:
+                add(new FeilsidePanel("soknad"));
+                break;
+        }
     }
 }
