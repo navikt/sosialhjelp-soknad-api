526d1f2088c6fdc55c189256208338d7b6859482
Andreas Svendby <andreas.svendby@nav.no>
2014-11-20 12:00:48 +0100

    [PK-15179]: Startet på å trekke ut tester for personalia i egen spec

--- frontend/js/modules/personalia/personaliaModule.js
+++ frontend/js/modules/personalia/personaliaModule.js
@@ -1,5 +1,6 @@
 angular.module('nav.personalia', [
     'nav.personalia.controller',
     'nav.personalia.directive',
-    'nav.personalia.service'
+    'nav.personalia.service',
+    'nav.cmstekster'
 ]);
