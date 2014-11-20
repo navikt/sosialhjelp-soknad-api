cd130f73f2da50ae5726ea1797d4ca69f2ab9479
Andreas Svendby <andreas.svendby@nav.no>
2014-11-20 09:31:07 +0100

    [PK-15179] Flyttet personaliaResolver inn i personaliaService

--- frontend/js/modules/personalia/personaliaService.js
+++ frontend/js/modules/personalia/personaliaService.js
@@ -6,4 +6,44 @@ angular.module('nav.personalia.service', [])
                 lagre   : { method: 'POST' }
             }
         );
+    })
+    .factory('PersonaliaResolver', function ($q, BehandlingIdResolver, Personalia) {
+
+        var personaliaDeferer = $q.defer();
+        BehandlingIdResolver
+            .then(function(result) {
+                var soknadId = result;
+                Personalia.lagre({soknadId: soknadId},
+                    'true',
+                    function(result) {
+                        personaliaDeferer.resolve(result);
+                    }
+                );
+            })
+            .catch(function() {
+                // TODO: Håndtere dersom man ikke kunne hente søknadsid
+                personaliaDeferer.reject("Kunne ikke hente søknadsId for behandlingsId");
+            });
+
+        return personaliaDeferer.promise;
+    })
+    .factory('EttersendingPersonaliaResolver', function ($q, BehandlingIdResolver, Personalia) {
+
+        var personaliaDeferer = $q.defer();
+        BehandlingIdResolver
+            .then(function(result) {
+                var soknadId = result;
+                Personalia.lagre({soknadId: soknadId},
+                    '',
+                    function(result) {
+                        personaliaDeferer.resolve(result);
+                    }
+                );
+            })
+            .catch(function() {
+                // TODO: Håndtere dersom man ikke kunne hente søknadsid
+                personaliaDeferer.reject("Kunne ikke hente søknadsId for behandlingsId");
+            });
+
+        return personaliaDeferer.promise;
     });
