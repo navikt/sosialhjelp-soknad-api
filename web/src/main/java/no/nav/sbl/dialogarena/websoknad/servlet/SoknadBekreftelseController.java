494fc030d809ec5dddebdc6fa17109a97de19b83
Ingvild Indrebø <ingvild.indrebo@nav.no>
2014-11-21 10:15:49 +0100

    [PK-16506] fjernet ettersendingslenke fra epost for ettersending

--- web/src/main/java/no/nav/sbl/dialogarena/websoknad/servlet/SoknadBekreftelseController.java
+++ web/src/main/java/no/nav/sbl/dialogarena/websoknad/servlet/SoknadBekreftelseController.java
@@ -50,7 +50,7 @@ public class SoknadBekreftelseController {
             String innhold = messageSource.getMessage("sendtSoknad.sendEpost.epostInnhold", new Object[]{saksoversiktUrl, ettersendelseUrl}, new Locale("nb", "NO"));
 
             if(soknadBekreftelse.getErEttersendelse()) {
-                innhold = messageSource.getMessage("sendEttersendelse.sendEpost.epostInnhold", new Object[]{saksoversiktUrl, ettersendelseUrl}, new Locale("nb", "NO"));
+                innhold = messageSource.getMessage("sendEttersendelse.sendEpost.epostInnhold", new Object[]{saksoversiktUrl}, new Locale("nb", "NO"));
             }
 
             emailService.sendEpostEtterInnsendtSoknad(soknadBekreftelse.getEpost(), subject, innhold, behandlingId);
