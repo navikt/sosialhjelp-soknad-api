c174e2d25ee78d18827415837156ee06e2408ac9
Steffen Hageland <steffen.hageland@nav.no>
2014-11-21 09:53:05 +0100

    [PK-15249] La til turnus-sporsmaal i oppsummeringen for bolken

--- frontend/js/common/arbeidsforhold/turnusDirective.js
+++ frontend/js/common/arbeidsforhold/turnusDirective.js
@@ -1,5 +1,5 @@
 angular.module('nav.arbeidsforhold.turnus.directive',[])
-    .directive('turnusblokk', function() {
+    .directive('turnusblokk', function(cms) {
         return {
             templateUrl: '../js/common/arbeidsforhold/templates/undersporsmaal/turnusblokkTemplate.html',
             replace: true,
@@ -8,18 +8,25 @@ angular.module('nav.arbeidsforhold.turnus.directive',[])
             },
             link: function (scope) {
                 scope.parentFaktum = scope.arbeidsforhold;
+                scope.harValgtRotasjon = function() {
+                    var rotasjonTekst = cms.tekster['arbeidsforhold.rotasjonskiftturnus.sporsmaal.alternativ.jarotasjon'];
+                    return scope.arbeidsforhold.properties.rotasjonskiftturnus == rotasjonTekst;
+                };
             }
         };
     })
-    .directive('turnusblokkOppsummering', function() {
+    .directive('turnusblokkOppsummering', function(cms) {
         return {
             templateUrl: '../js/common/arbeidsforhold/templates/oppsummeringer/turnusblokkOppsummeringTemplate.html',
             replace: true,
             scope: {
                 arbeidsforhold: '='
             },
-            link: function (scope) {
-                scope.parentFaktum = scope.arbeidsforhold;
+            link: function(scope) {
+                scope.harValgtRotasjon = function() {
+                    var rotasjonTekst = cms.tekster['arbeidsforhold.rotasjonskiftturnus.sporsmaal.alternativ.jarotasjon'];
+                    return scope.arbeidsforhold.properties.rotasjonskiftturnus == rotasjonTekst;
+                };
             }
         };
     });
