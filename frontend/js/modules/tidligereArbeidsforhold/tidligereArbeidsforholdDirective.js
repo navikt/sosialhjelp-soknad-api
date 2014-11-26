c91b5c058415d7341242f2cfdede51f9c57b944b
Steffen Hageland <steffen.hageland@nav.no>
2014-11-26 16:59:18 +0100

    [PK-14428] La t il spørsmål om man var permittert sist man mottok dagpenger.

--- frontend/js/modules/tidligereArbeidsforhold/tidligereArbeidsforholdDirective.js
+++ frontend/js/modules/tidligereArbeidsforhold/tidligereArbeidsforholdDirective.js
@@ -4,4 +4,11 @@ angular.module('nav.tidligerearbeidsforhold.directive', [])
             replace: true,
             templateUrl: '../js/modules/tidligereArbeidsforhold/templates/tidligereArbeidsforholdTemplate.html'
         };
+    })
+    .directive('permittertSporsmal', function () {
+        return {
+            replace: true,
+            scope: true,
+            templateUrl: '../js/modules/tidligereArbeidsforhold/templates/varpermittertsist.html'
+        };
     });
