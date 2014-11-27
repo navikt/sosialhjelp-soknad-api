3157d7d96bb5875abaaf64d4064b211b47ca10e9
Ingvild Indrebø <ingvild.indrebo@nav.no>
2014-11-27 09:26:40 +0100

    [PK-16506] endret bekreftelseepostutsendelse til å bruke mimeMessage slik at man kan sende htmlcontent

--- web/src/main/java/no/nav/sbl/dialogarena/websoknad/config/ApplicationConfig.java
+++ web/src/main/java/no/nav/sbl/dialogarena/websoknad/config/ApplicationConfig.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.multipart.MultipartResolver;
@@ -44,7 +44,7 @@ public class ApplicationConfig {
     }
 
     @Bean
-    public MailSender mailSender() {
+    public JavaMailSender mailSender() {
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
         javaMailSender.setDefaultEncoding("UTF-8");
         javaMailSender.setHost(smtpServerHost);
--- web/src/test/java/no/nav/sbl/dialogarena/websoknad/service/EmailServiceTest.java
+++ web/src/test/java/no/nav/sbl/dialogarena/websoknad/service/EmailServiceTest.java
@@ -9,8 +9,13 @@ import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.core.task.SyncTaskExecutor;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.mail.MailException;
-import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+
+import javax.mail.Message;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.refEq;
@@ -23,7 +28,7 @@ public class EmailServiceTest {
     @InjectMocks
     private EmailService emailService;
     @Mock
-    private MailSender mailSender;
+    private JavaMailSender mailSender;
     @Spy
     private TaskExecutor taskExecutor = new SyncTaskExecutor();
 
@@ -36,6 +41,23 @@ public class EmailServiceTest {
         message.setText("innhold");
         message.setFrom("ikke-svar@nav.no");
         verify(mailSender).send(refEq(message));
+    }
+
+    @Test
+    public void sendEpostEtterInnsendtSoknad() {
+        emailService.sendEpostEtterInnsendtSoknad("til", "subject", "innhold", "123");
+
+        final String htmlInnhold = "<p>" + "innhold" + "</p>";
+
+        MimeMessagePreparator preparator = new MimeMessagePreparator() {
+            public void prepare(MimeMessage mimeMessage) throws Exception {
+                mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress("til"));
+                mimeMessage.setFrom("ikke-svar@nav.no");
+                mimeMessage.setContent(htmlInnhold, "text/html;charset=utf-8");
+                mimeMessage.setSubject("subject");
+            }
+        };
+        verify(mailSender).send(any(MimeMessagePreparator.class));
     }
 
     @Test
