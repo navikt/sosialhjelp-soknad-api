173b25f116ac44ab66b4b0b8ea710ae9c7163164
Steffen Hageland <steffen.hageland@nav.no>
2014-11-28 09:55:24 +0100

    [PK-14428] La til spørsmål om man fortsatt jobber og/eller har jobbet hos arbeidsgiveren siden sist du mottok dagpenger.

--- frontend/js/modules/tidligereArbeidsforhold/tidligereArbeidsforholdDirective.js
+++ frontend/js/modules/tidligereArbeidsforhold/tidligereArbeidsforholdDirective.js
@@ -5,10 +5,28 @@ angular.module('nav.tidligerearbeidsforhold.directive', [])
             templateUrl: '../js/modules/tidligereArbeidsforhold/templates/tidligereArbeidsforholdTemplate.html'
         };
     })
-    .directive('permittertSporsmal', function () {
+    .directive('permittertSporsmal', function (data) {
         return {
             replace: true,
             scope: true,
-            templateUrl: '../js/modules/tidligereArbeidsforhold/templates/varpermittertsist.html'
+            templateUrl: '../js/modules/tidligereArbeidsforhold/templates/varpermittertsist.html',
+            link: function(scope) {
+                scope.varPermittert = data.finnFaktum('tidligerearbeidsforhold.permittert');
+                scope.erPermittert = function() {
+                    return scope.varPermittert.value == "permittert";
+                }
+            }
+        };
+    })
+    .directive('tidligereArbeidsgiver', function(data) {
+        return {
+            replace: true,
+            templateUrl: '../js/modules/tidligereArbeidsforhold/templates/tidligereArbeidsgiver.html',
+            link: function(scope) {
+                scope.cmsArgumenter = {
+                    modelFaktum: data.finnFaktum('tidligerearbeidsforhold.tidligerearbeidsgiver'),
+                    default: "[arbeidsgiver]"
+                };
+            }
         };
     });
