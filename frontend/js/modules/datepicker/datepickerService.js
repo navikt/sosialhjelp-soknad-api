9e1dacf1f162c6f335a6f57bb6c648b16c6d78e6
Andreas Svendby <andreas.svendby@nav.no>
2014-12-01 11:59:49 +0100

    [kaizen] Refaktorert en del av logikken rundt input til egen service

--- frontend/js/modules/datepicker/datepickerService.js
+++ frontend/js/modules/datepicker/datepickerService.js
@@ -3,12 +3,31 @@ angular.module('nav.datepicker.service', [])
         var allowedKeys = datepickerInputKeys.concat(datepickerNonInputKeys);
 
         return {
-            isValidInput: function(keyCode, currentLength, maxLength) {
+            isValidInput: function(keyCode, currentLength, maxLength, caretPosition) {
                 function isAllowedPeriod() {
-                    return (keyCode === 190 && (currentLength === 2 || currentLength === 5) || keyCode !== 190);
+                    return (keyCode === 190 && (caretPosition === 2 || caretPosition === 5) || keyCode !== 190);
                 }
 
                 return allowedKeys.contains(keyCode) && (datepickerNonInputKeys.contains(keyCode) || currentLength < maxLength) && isAllowedPeriod();
+            },
+            addPeriodAtRightIndex: function(input, oldInput, caretPosition) {
+                var start = caretPosition - (input.length - oldInput.length);
+                var slutt = caretPosition;
+
+                for (var i = start; i < slutt && i < input.length; i++) {
+                    if (i === 1 || i === 4) {
+                        if (input[i + 1] === '.') {
+                            caretPosition++;
+                            i++;
+                        } else {
+                            input = input.splice(i + 1, 0, '.');
+                            caretPosition++;
+                            i++;
+                            slutt++;
+                        }
+                    }
+                }
+                return [input, caretPosition];
             }
         };
     });
