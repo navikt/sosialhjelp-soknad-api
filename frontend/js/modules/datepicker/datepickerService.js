b07166cfb5a0651621d4fd9fe4bef5e33f404474
Andreas Svendby <andreas.svendby@nav.no>
2014-12-03 15:33:45 +0100

    [kaizen] Maaaaaaaaaaaaaaasse validering

--- frontend/js/modules/datepicker/datepickerService.js
+++ frontend/js/modules/datepicker/datepickerService.js
@@ -83,4 +83,72 @@ angular.module('nav.datepicker.service', [])
                 return value;
             }
         };
+    })
+    .factory('guidService', function() {
+        return {
+            getGuid: function() {
+                return 'xxxxxxxx'.replace(/[x]/g, function(c) {
+                    var r = Math.random()*16| 0, v = c == 'x' ? r : (r&0x3|0x8);
+                    return v.toString(16);
+                })
+            }
+        };
+    })
+    .factory('dateService', function() {
+        var dateFormatRegExp = new RegExp(/^\d\d\.\d\d\.\d\d\d\d$/);
+        return {
+            isValidDate: function(date) {
+                if (date) {
+                    // stackoverflow.com/questions/5812220/test-if-date-is-valid
+                    var bits = date.split('.');
+                    var aar = bits[2];
+                    var maaned = bits[1];
+                    var dag = bits[0];
+                    var dagerIMaaned = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+                    // Skuddår
+                    if (((aar % 4) === 0 && aar % 100) || (aar % 400) === 0) {
+                        dagerIMaaned[1] = 29;
+                    }
+                    return dag <= dagerIMaaned[--maaned];
+                }
+                return false;
+            },
+            reverseNorwegianDateFormat: function(dateString) {
+                if (this.hasCorrectDateFormat(dateString) && this.isValidDate(dateString)) {
+                    return dateString.split('.').reverse().join('-');
+                } else {
+                    return '';
+                }
+            },
+            hasCorrectDateFormat: function(date) {
+                return dateFormatRegExp.test(date);
+            },
+            isFutureDate: function (dateString) {
+                var dateArray = dateString.split(".");
+
+                var date = new Date();
+                date.setDate(dateArray[0]);
+                date.setMonth(dateArray[1] - 1);
+                date.setFullYear(dateArray[2]);
+                date.setHours(0);
+                date.setMilliseconds(0);
+                date.setSeconds(0);
+                date.setMinutes(0);
+
+                var today = new Date();
+                var temp = new Date();
+                temp.setMonth(today.getMonth());
+                temp.setDate(today.getDate());
+                temp.setFullYear(today.getFullYear());
+                temp.setHours(0);
+                temp.setMilliseconds(0);
+                temp.setSeconds(0);
+                temp.setMinutes(0);
+
+                var morgenDagensDatoMillis = temp.setTime(temp.getTime() + 86400000);
+
+                return date.getTime() >= morgenDagensDatoMillis;
+            }
+        };
     });
