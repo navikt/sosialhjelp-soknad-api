7bc3fd6934c18db926bfe7e57e02996daf089965
Ingvild Indrebø <ingvild.indrebo@nav.no>
2014-12-03 10:46:27 +0100

    [PK-15249] fikset litt styling og lagt til infoboks for turnusblokk

--- frontend/js/common/directives/markup/navinfoboksModule.js
+++ frontend/js/common/directives/markup/navinfoboksModule.js
@@ -1,21 +1,25 @@
 angular.module('nav.markup.navinfoboks', [])
-	.directive('navinfoboks', function ($parse) {
-		return {
-			restrict   : 'A',
-			replace    : true,
-			templateUrl: '../js/common/directives/markup/navinfoboksTemplate.html',
-            link: function(scope, el, attrs) {
-                scope.infoTekster = $parse(attrs.infotekster)(scope);
+    .directive('navinfoboks', function ($parse) {
+        return {
+            restrict: 'A',
+            replace: true,
+            templateUrl: '../js/common/directives/markup/navinfoboksTemplate.html',
+            link: function (scope, el, attrs) {
+                var infotekster = $parse(attrs.infotekster)(scope);
+                scope.infoTekster = [];
+                typeof infotekster === 'string' ? scope.infoTekster.push(infotekster) : scope.infoTekster = infotekster;
             }
-		};
-	})
-	.directive('vedlegginfoboks', function ($parse) {
-		return {
-			restrict   : 'A',
-			replace    : true,
-			templateUrl: '../js/common/directives/markup/vedlegginfoboksTemplate.html',
-            link: function(scope, el, attrs) {
-                scope.vedleggTekster = $parse(attrs.vedleggtekster)(scope);
+        };
+    })
+    .directive('vedlegginfoboks', function ($parse) {
+        return {
+            restrict: 'A',
+            replace: true,
+            templateUrl: '../js/common/directives/markup/vedlegginfoboksTemplate.html',
+            link: function (scope, el, attrs) {
+                var vedleggtekster = $parse(attrs.vedleggtekster)(scope);
+                scope.vedleggTekster = [];
+                typeof vedleggtekster === 'string' ? scope.vedleggTekster.push(vedleggtekster) : scope.vedleggTekster = vedleggtekster;
             }
-		};
-	});
+        };
+    });
