4ed50b2c8472b78ebbbfdb00930d5a3fc1902373
Aleksander Hindenes <aleksander.hindenes@nav.no>
2014-12-03 08:28:44 +0100

    [PK-15248] La til en test av nytt tilfelle hvor det skulle rutes til dagpenger eøs

--- business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/util/WebSoknadUtils.java
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/util/WebSoknadUtils.java
@@ -97,7 +97,6 @@ public class WebSoknadUtils {
         return nyeArbeidsforhold.getValue().equals("true");
     }
 
-
     public static String getSkjemanummer(WebSoknad soknad) {
         if (soknad.erEttersending()) {
             return soknad.getskjemaNummer();
