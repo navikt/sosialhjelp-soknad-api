9187d313ea23868c2b47f14859b4be1523889617
Steffen Hageland <steffen.hageland@nav.no>
2014-12-04 14:35:23 +0100

    [Testdekning] Fjernet ubrukt kode

--- frontend/js/utslagskriterier/dagpenger/directives.js
+++ frontend/js/utslagskriterier/dagpenger/directives.js
@@ -1,6 +1,5 @@
 angular.module('utslagskriterierDagpenger.directives', [
     'nav.booleanradio',
-    'nav.booleanradioUtenfaktum',
     'nav.navfaktum',
     'nav.routingForGjenopptakModule',
     'nav.feilmeldinger',
