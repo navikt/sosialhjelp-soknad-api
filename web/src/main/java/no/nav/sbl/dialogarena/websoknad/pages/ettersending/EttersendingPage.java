062927e4bf61977c2101380f25d41555b1211847
Jarle Svendsrud <jarle.svendsrud@nav.no>
2014-12-09 09:24:10 +0100

    [kaizen] Fjernet utkommentert kode

--- web/src/main/java/no/nav/sbl/dialogarena/websoknad/pages/ettersending/EttersendingPage.java
+++ web/src/main/java/no/nav/sbl/dialogarena/websoknad/pages/ettersending/EttersendingPage.java
@@ -7,12 +7,5 @@ public class EttersendingPage extends EttersendingBasePage {
     public EttersendingPage(PageParameters parameters) {
         super(parameters);
 
-//        WebSoknad soknad = ettersendingService.hentEttersendingForBehandlingskjedeId(brukerbehandlingId.toString());
-//        if (soknad != null) {
-//            new CookieUtils().remove("XSRF-TOKEN");
-//            new CookieUtils().save("XSRF-TOKEN", XsrfGenerator.generateXsrfToken(soknad.getBehandlingskjedeId()));
-//        } else {
-//            setResponsePage(StartEttersendingPage.class, parameters);
-//        }
     }
 }
