7726be542b6333031aa4854ab7f1e2430dde5c2e
Jarle Svendsrud <jarle.svendsrud@nav.no>
2014-12-10 16:29:53 +0100

    [kaizen] Fjernet javadoc og utkommentert kode

--- web/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/sikkerhet/HeaderFilter.java
+++ web/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/sikkerhet/HeaderFilter.java
@@ -9,19 +9,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/**
- * klaaasse som legger på sikkerhets headere
- */
 public class HeaderFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         response.setHeader("X-Frame-Options", "SAME");
         response.setHeader("X-Content-Type-Options", "nosniff");
         response.setHeader("X-XSS-Protection", "1; mode=block");
-        //String scp = "default-src: 'self'; script-src 'self' https://ssl.google-analytics.com http://www.google-analytics.com; media-src 'self'";
-        //response.setHeader("Content-Security-Policy", scp);
-        //response.setHeader("X-Content-Security-Policy", scp);
-        //response.setHeader("X-WebKit-SCP", scp);
         filterChain.doFilter(request, response);
     }
 }
