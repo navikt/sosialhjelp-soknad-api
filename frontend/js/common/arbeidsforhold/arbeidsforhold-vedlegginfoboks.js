944be67279db2dd6e8780df3c663039bf33edc47
Steffen Hageland <steffen.hageland@nav.no>
2014-12-12 12:54:46 +0100

    [PK-15249] Endret til vedlegg M6 Timelister for "rotasjon, skift, turnus" under arbeidsforhold

--- frontend/js/common/arbeidsforhold/arbeidsforhold-vedlegginfoboks.js
+++ frontend/js/common/arbeidsforhold/arbeidsforhold-vedlegginfoboks.js
@@ -1,12 +1,26 @@
 angular.module('nav.arbeidsforhold.vedlegginfoboks', [])
-    .directive('arbeidsforholdVedleggInfoboks', function () {
-    return {
-        restrict   : 'A',
-        replace    : true,
-        scope      : {
-            vedleggtekster : "=",
-            infotekster    : "="
-        },
-        templateUrl: '../js/common/arbeidsforhold/templates/arbeidsforholdVedleggInfoboksTemplate.html'
-    };
-});
+    .directive('arbeidsforholdVedleggInfoboks', function (cms) {
+        return {
+            restrict: 'A',
+            replace: true,
+            scope: {
+                vedleggtekster: "=",
+                infotekster: "=",
+                rotasjonVedlegg: "=?",
+                arbeidsforhold: "="
+            },
+            templateUrl: '../js/common/arbeidsforhold/templates/arbeidsforholdVedleggInfoboksTemplate.html',
+
+            link: {
+                pre: function (scope) {
+                    scope.alleVedleggtekster = scope.vedleggtekster;
+                    if (isNotNullOrUndefined(scope.rotasjonVedlegg)) {
+                        var rotasjonTekst = cms.tekster['arbeidsforhold.rotasjonskiftturnus.sporsmaal.alternativ.jarotasjon'];
+                        if (scope.arbeidsforhold.properties.rotasjonskiftturnus == rotasjonTekst) {
+                            scope.alleVedleggtekster = scope.vedleggtekster.concat("Dagpenger.vedlegg.M6.header");
+                        }
+                    }
+                }
+            }
+        };
+    });
