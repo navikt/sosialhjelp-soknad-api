2b136f32e60d72826c0f9c67c2270c16a198e692
Aleksander Hindenes <aleksander.hindenes@nav.no>
2014-12-12 08:38:11 +0100

    [kaizen] fjernet en console.log()

--- frontend/js/modules/datepicker/datoDirective.js
+++ frontend/js/modules/datepicker/datoDirective.js
@@ -135,7 +135,6 @@ angular.module('nav.datepicker.dato', [])
                     };
 
                     function harFeilMedNavDatepicker() {
-                        console.log(scope.disabled);
                         return scope.harRequiredFeil() || scope.harFormatteringsFeil() || scope.harTilDatoFeil() || scope.erIkkeGyldigDato()
                             || scope.erUloveligFremtidigDato() && (scope.disabled === undefined || scope.disabled === false);
                     }
