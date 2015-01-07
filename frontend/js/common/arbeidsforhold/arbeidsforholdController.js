944bb41ad37e514f9d60c34ac435960c18827090
Steffen Hageland <steffen.hageland@nav.no>
2015-01-07 16:06:43 +0100

    [kaizen] Fjernet et problem hvor feilmeldinger ble repetert flere ganger (1 gang for hver knapp som var knyttet til samme faktum)

--- frontend/js/common/arbeidsforhold/arbeidsforholdController.js
+++ frontend/js/common/arbeidsforhold/arbeidsforholdController.js
@@ -89,7 +89,6 @@ angular.module('nav.arbeidsforhold.controller', [])
 
         $scope.valider = function (skalScrolle) {
             var valid = $scope.runValidation(skalScrolle);
-            console.log("run validation!");
             if (valid) {
                 $scope.lukkTab('arbeidsforhold');
                 $scope.settValidert('arbeidsforhold');
