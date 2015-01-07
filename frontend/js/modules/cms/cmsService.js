1e03f817e7be57b49bac0e9c5bc97d6ea10c7281
Steffen Hageland <steffen.hageland@nav.no>
2015-01-07 16:05:56 +0100

    [kaizen] La til metode for å hente ut tekst fra CMS som plaintekst og ikke HTML. Fjerner en console-log

--- frontend/js/modules/cms/cmsService.js
+++ frontend/js/modules/cms/cmsService.js
@@ -49,6 +49,23 @@ angular.module('nav.cms.service', [])
                 }
                 return tekst === undefined ? undefined : $sce.trustAsHtml(tekst);
             },
+            getTextSafe: function(nokkel, args) {
+                var key = resolveKeyService.getKey(nokkel);
+                var tekst = cms.tekster[key];
+
+                if (args instanceof Array) {
+                    args.forEach(function (argTekst, idx) {
+                        tekst = tekst.replace('{' + idx + '}', argTekst);
+                    });
+                } else if (args) {
+                    tekst = tekst.replace('{0}', args);
+                }
+
+                if ($rootScope.visCmsnokkler) {
+                    tekst += ' [' + key + ']';
+                }
+                return tekst;
+            },
             getTrustedHtml: function(nokkel, args) {
                 return $sce.getTrustedHtml(this.getText(nokkel, args));
             }
