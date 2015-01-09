6980933d1b0e0aa4a4a6ce8a7dc49c1c12fafbab
Steffen Hageland <steffen.hageland@nav.no>
2015-01-09 10:26:05 +0100

    [kaizen] Endret til varchar(600) for value i søknadbrukerdata i stede for vachar(500), fjorde migreringsscriptet mer robust.

--- config/src/main/resources/db/migration/soknadInnsendingDataSource/V1_28__endret_value_for_faktum_fra_clob_til_varchar.sql
+++ config/src/main/resources/db/migration/soknadInnsendingDataSource/V1_28__endret_value_for_faktum_fra_clob_til_varchar.sql
@@ -1,4 +1,4 @@
-ALTER TABLE SoknadBrukerData ADD (foo VARCHAR(500));
-UPDATE SoknadBrukerData SET foo = value;
+ALTER TABLE SoknadBrukerData ADD (foo VARCHAR(600));
+UPDATE SoknadBrukerData SET foo = SUBSTR(value, 1, 600);
 ALTER TABLE SoknadBrukerData drop column value;
 ALTER TABLE SoknadBrukerData RENAME column foo to value;
