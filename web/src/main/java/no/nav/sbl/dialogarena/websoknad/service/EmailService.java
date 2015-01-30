0147cc414de08eff38f642b04f589de691183817
Steffen Hageland <steffen.hageland@nav.no>
2015-01-30 12:36:15 +0100

    [kaizen] unused import

--- web/src/main/java/no/nav/sbl/dialogarena/websoknad/service/EmailService.java
+++ web/src/main/java/no/nav/sbl/dialogarena/websoknad/service/EmailService.java
@@ -5,7 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 
