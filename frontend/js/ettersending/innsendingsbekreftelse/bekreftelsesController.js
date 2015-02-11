b35000208385db8aa3227cc0c1e13c039505f521
Aleksander Hindenes <aleksander.hindenes@nav.no>
2015-02-11 11:43:37 +0100

    [kaizen] redirect til saksoversikt fungerte ikke etter innsendt søknad og ettersendelse

--- frontend/js/ettersending/innsendingsbekreftelse/bekreftelsesController.js
+++ frontend/js/ettersending/innsendingsbekreftelse/bekreftelsesController.js
@@ -2,7 +2,7 @@ angular.module('ettersending.bekreftelse', [])
     /* Er avhengig av at søknadsoppsett er hentet før vi kommer til denne kontrolleren, da temakode blir hentet sånn
      Henter ikke for bekreftelsessiden siden man bare skal videresendes hit, og siden det blir krøll på backend
      */
-    .controller('BekreftelseEttersendingCtrl', function ($scope, $window, $timeout, $routeParams, $rootElement, data, bekreftelseEpostService) {
+    .controller('BekreftelseEttersendingCtrl', function ($scope, $window, $timeout, $rootElement, data, bekreftelseEpostService) {
         var appName = $rootElement.attr('data-ng-app');
         $scope.cmsprefix = {value: appName};
 
@@ -12,13 +12,14 @@ angular.module('ettersending.bekreftelse', [])
         }
 
         $scope.erEttersendelse = {value: true};
-
         $scope.fremdriftsindikator = {laster: true};
         $scope.temaKode = {value: data.soknadOppsett.temaKode};
-        new bekreftelseEpostService({epost: $scope.epost.value, temaKode: $scope.temaKode.value, erEttersendelse: $scope.erEttersendelse.value}).$send({behandlingId: $routeParams.behandlingsId}).then(function () {
+        var brukerBehandlingId = data.soknad.brukerBehandlingId;
+
+        new bekreftelseEpostService({epost: $scope.epost.value, temaKode: $scope.temaKode.value, erEttersendelse: $scope.erEttersendelse.value}).$send({behandlingId: brukerBehandlingId}).then(function () {
             $timeout(function() {
                 var saksoversiktBaseUrl = data.config['saksoversikt.link.url'];
-                redirectTilUrl(saksoversiktBaseUrl + '/detaljer/' + $scope.temaKode.value + '/' + $routeParams.behandlingsId);
+                redirectTilUrl(saksoversiktBaseUrl + '/detaljer/' + $scope.temaKode.value + '/' + brukerBehandlingId);
             }, 3000);
         });
     });
