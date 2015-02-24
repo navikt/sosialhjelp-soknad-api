9b3360036a9679db572ee9ad6c2221eeafbd2ab7
Jarle Svendsrud <jarle.svendsrud@nav.no>
2015-02-24 14:36:43 +0100

    [QA] Selftestoppsett

--- business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/service/SelfTestBase.java
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/service/SelfTestBase.java
@@ -1,51 +1,43 @@
 package no.nav.sbl.dialogarena.soknadinnsending.business.service;
 
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 
 import java.io.Serializable;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Iterator;
 
+import static org.apache.commons.lang3.StringUtils.join;
 import static org.slf4j.LoggerFactory.getLogger;
 
 public abstract class SelfTestBase {
-    private static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
     private static final Logger logger = getLogger(SelfTestBase.class);
-    public static final String STATUS_OK = "OK";
-    public static final String STATUS_ERROR = "ERROR";
-    protected String version = this.getApplicationVersion();
+    protected static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    protected static final String STATUS_OK = "OK";
+    protected static final String STATUS_ERROR = "ERROR";
+
+    protected String version = getApplicationVersion();
     protected String host = this.getHost();
-    protected String status;
+    protected String oppsummertStatus;
     protected String message;
-    protected SelfTestHTML html;
-    protected String applikasjonsNavn;
-    protected ArrayList statusList;
+    protected ArrayList<AvhengighetStatus> statusList = new ArrayList();
 
-    protected SelfTestBase(String applikasjonsNavn) {
-        this.applikasjonsNavn = applikasjonsNavn;
-        this.statusList = new ArrayList();
-        this.status = "OK";
-
-        ArrayList failed = new ArrayList();
-        Iterator req = statusList.iterator();
-
-        while(req.hasNext()) {
-            SelfTestBase.AvhengighetStatus serviceStatus = (SelfTestBase.AvhengighetStatus)req.next();
-            if(!"OK".equals(serviceStatus.getStatus())) {
-                this.status = "ERROR";
-                failed.add(serviceStatus.navn);
+    public void sjekkOppsummertStatus() {
+        ArrayList<String> failed = new ArrayList<>();
+        oppsummertStatus = STATUS_OK;
+        for (AvhengighetStatus avhengighetStatus : statusList) {
+            if (!STATUS_OK.equals(avhengighetStatus.getStatus())) {
+                oppsummertStatus = STATUS_ERROR;
+                failed.add(avhengighetStatus.getName());
             }
         }
-        this.message = StringUtils.join(failed, ',');
-
-        if (this.status.equals("ERROR")) {
-            logger.info("Selftest rapporterer melding: " + this.message);
+        message = join(failed, ',');
+        if (STATUS_ERROR.equals(oppsummertStatus)) {
+            logger.error("Selftest rapporterer melding: " + message);
         }
     }
-
-    protected abstract void replaceStatusList(List<SelfTestBase.AvhengighetStatus> var1);
 
     public class SelfTestHTML {
 
@@ -53,13 +45,13 @@ public abstract class SelfTestBase {
         private String newLine = System.getProperty("line.separator");
         private StringBuilder header = new StringBuilder();
         private StringBuilder style = new StringBuilder();
-        private StringBuilder body =  new StringBuilder();
+        private StringBuilder body = new StringBuilder();
 
-        public SelfTestHTML(String title){
+        public SelfTestHTML(String title) {
             this.title = title;
         }
 
-        public String buildPage(){
+        public String buildPage() {
             String header = buildHeader();
             String body = buildBody();
             String footer = buildFooter();
@@ -87,8 +79,8 @@ public abstract class SelfTestBase {
             header.append("<!DOCTYPE html>" + newLine);
             header.append("<html lang=\"no\">" + newLine);
             header.append("<head>" + newLine);
-            header.append("<title>"+ title +"</title>" + newLine );
-            header.append( buildStyle() + newLine + "</head>" + newLine);
+            header.append("<title>" + title + "</title>" + newLine);
+            header.append(buildStyle() + newLine + "</head>" + newLine);
             return header.toString();
         }
 
@@ -112,20 +104,19 @@ public abstract class SelfTestBase {
 
         public void appendToBody(ArrayList list) {
 
-            //TODO: introspect and create table headers based on getters
-            body.append("<table>" +  newLine);
-            body.append("<table>" +  newLine);
+            body.append("<table>" + newLine);
+            body.append("<table>" + newLine);
             body.append("<tr>" + newLine);
-            body.append("<th>Status</th>" +  newLine);
-            body.append("<th>Navn</th>" +  newLine );
-            body.append("<th>Responstid ms</th>" +  newLine);
-            body.append("<th>Beskrivelse</th>" +  newLine);
-            body.append("</tr>" +  newLine);
+            body.append("<th>Status</th>" + newLine);
+            body.append("<th>Navn</th>" + newLine);
+            body.append("<th>Responstid ms</th>" + newLine);
+            body.append("<th>Beskrivelse</th>" + newLine);
+            body.append("</tr>" + newLine);
 
             Iterator iterator = list.iterator();
 
-            while(iterator.hasNext()) {
-                SelfTestBase.AvhengighetStatus serviceStatus = (SelfTestBase.AvhengighetStatus)iterator.next();
+            while (iterator.hasNext()) {
+                SelfTestBase.AvhengighetStatus serviceStatus = (SelfTestBase.AvhengighetStatus) iterator.next();
                 body.append("<tr>" + newLine);
                 body.append("<td>" + serviceStatus.getStatus() + "</td>" + newLine);
                 body.append("<td>" + serviceStatus.getName() + "</td>" + newLine);
@@ -133,14 +124,14 @@ public abstract class SelfTestBase {
                 body.append("<td>" + serviceStatus.getBeskrivelse() + "</td>" + newLine);
                 body.append("</tr>" + newLine);
             }
-            body.append("</table>" +  newLine);
+            body.append("</table>" + newLine);
 
         }
 
     }
 
-    private String getApplicationVersion() {
-      return "Unknown";
+    protected String getApplicationVersion() {
+        return getClass().getPackage().getImplementationVersion();
     }
 
     private String getHost() {
@@ -169,14 +160,6 @@ public abstract class SelfTestBase {
             this(name, status, durationMilis, "");
         }
 
-        public AvhengighetStatus(String name, boolean isOK, long durationMilis, String beskrivelse) {
-            this(name, isOK?"OK":"ERROR", durationMilis, beskrivelse);
-        }
-
-        public AvhengighetStatus(String name, boolean isOK, long durationMilis) {
-            this(name, isOK, durationMilis, "");
-        }
-
         public String getName() {
             return this.navn;
         }
@@ -193,6 +176,5 @@ public abstract class SelfTestBase {
             return this.beskrivelse;
         }
     }
-
 
 }
