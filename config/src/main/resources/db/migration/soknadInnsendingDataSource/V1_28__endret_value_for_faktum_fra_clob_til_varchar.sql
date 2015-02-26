e8a2dfa3efa6f283bf527e2d7f7bf8a658e23084
Jarle Svendsrud <jarle.svendsrud@nav.no>
2015-02-26 23:04:45 +0100

    [prodfix] Endret ny kolonnetype til å ha opp til 650 karakterer i stedet for 600

--- config/src/main/resources/db/migration/soknadInnsendingDataSource/V1_28__endret_value_for_faktum_fra_clob_til_varchar.sql
+++ config/src/main/resources/db/migration/soknadInnsendingDataSource/V1_28__endret_value_for_faktum_fra_clob_til_varchar.sql
@@ -1,4 +1,4 @@
-ALTER TABLE SoknadBrukerData ADD (foo VARCHAR(600));
-UPDATE SoknadBrukerData SET foo = SUBSTR(value, 1, 600);
+ALTER TABLE SoknadBrukerData ADD (foo VARCHAR(650));
+UPDATE SoknadBrukerData SET foo = SUBSTR(value, 1, 650);
 ALTER TABLE SoknadBrukerData drop column value;
 ALTER TABLE SoknadBrukerData RENAME column foo to value;
