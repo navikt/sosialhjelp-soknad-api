e635e420f45b4da22a9eb1743aeee2dde7b22ae0
Borge Bardo <borge.bardo@nav.no>
2015-03-12 08:20:46 +0100

    [PK-19086] slår av sikkerhet lokalt inntil videre

--- web/src/main/java/no/nav/sbl/dialogarena/websoknad/config/ApplicationConfig.java
+++ web/src/main/java/no/nav/sbl/dialogarena/websoknad/config/ApplicationConfig.java
@@ -4,10 +4,8 @@ import no.nav.sbl.dialogarena.print.HandleBarKjoerer;
 import no.nav.sbl.dialogarena.print.HtmlGenerator;
 import no.nav.sbl.dialogarena.selftest.SelfTest;
 import no.nav.sbl.dialogarena.soknadinnsending.business.selftest.SelfTestService;
-import no.nav.sbl.dialogarena.soknadinnsending.sikkerhet.SikkerhetsAspect;
-import no.nav.sbl.dialogarena.soknadinnsending.sikkerhet.Tilgangskontroll;
-import no.nav.sbl.dialogarena.websoknad.service.EmailService;
 import no.nav.sbl.dialogarena.utils.InnloggetBruker;
+import no.nav.sbl.dialogarena.websoknad.service.EmailService;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -41,16 +39,6 @@ public class ApplicationConfig {
         PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
         propertySourcesPlaceholderConfigurer.setLocalOverride(true);
         return propertySourcesPlaceholderConfigurer;
-    }
-
-    @Bean
-    public SikkerhetsAspect sikkerhet() {
-        return new SikkerhetsAspect();
-    }
-
-    @Bean
-    public Tilgangskontroll tilgangskontroll() {
-        return new Tilgangskontroll();
     }
 
     @Bean
