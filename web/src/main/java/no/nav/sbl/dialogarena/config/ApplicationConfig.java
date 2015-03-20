ea907c902ce203593ded542b197ea53bf1efbd84
Borge Bardo <borge.bardo@nav.no>
2015-03-20 10:18:48 +0100

    fjernet ubrukt logger, del 2. Nå har også Intellij fjernet unused imports.¨

--- web/src/main/java/no/nav/sbl/dialogarena/config/ApplicationConfig.java
+++ web/src/main/java/no/nav/sbl/dialogarena/config/ApplicationConfig.java
@@ -1,12 +1,11 @@
 package no.nav.sbl.dialogarena.config;
 
+import no.nav.sbl.dialogarena.selftest.SelfTest;
+import no.nav.sbl.dialogarena.service.EmailService;
 import no.nav.sbl.dialogarena.service.HandleBarKjoerer;
 import no.nav.sbl.dialogarena.service.HtmlGenerator;
-import no.nav.sbl.dialogarena.selftest.SelfTest;
 import no.nav.sbl.dialogarena.soknadinnsending.business.selftest.SelfTestService;
 import no.nav.sbl.dialogarena.utils.InnloggetBruker;
-import no.nav.sbl.dialogarena.service.EmailService;
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,8 +16,6 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-
-import static org.slf4j.LoggerFactory.getLogger;
 
 /**
  * Applikasjonskontekst for ear-modulen.
