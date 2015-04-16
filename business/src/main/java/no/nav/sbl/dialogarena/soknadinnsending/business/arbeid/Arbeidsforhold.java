7b5da79d4a85868dde19f1752feecbe1cc43f6f8
Eirik L. Wang <eirik.wang@nav.no>
2015-04-16 13:29:14 +0200

    PK-19176: Henter ut arbeidsforhold fra AAReg. Henter kun ut orgnr og periode.

---
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/arbeid/Arbeidsforhold.java
@@ -0,0 +1,13 @@
+package no.nav.sbl.dialogarena.soknadinnsending.business.arbeid;
+
+import no.nav.sbl.dialogarena.soknadinnsending.business.domain.dto.Land;
+import org.joda.time.Interval;
+
+public class Arbeidsforhold {
+    public String orgnr;
+    public String arbridsgiverNavn;
+    public Land land;
+    public Interval periode;
+
+
+}
