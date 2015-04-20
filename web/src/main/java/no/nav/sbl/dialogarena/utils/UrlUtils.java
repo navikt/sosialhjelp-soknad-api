0f1fb8bc02e64bdf2348e7d2233c10bb88acb1bf
Steffen Hageland <steffen.hageland@nav.no>
2015-04-20 10:07:50 +0200

    [renaming] Endret URLer fra "dagpenger" til "soknaddagpenger"

--- web/src/main/java/no/nav/sbl/dialogarena/utils/UrlUtils.java
+++ web/src/main/java/no/nav/sbl/dialogarena/utils/UrlUtils.java
@@ -9,15 +9,16 @@ import static java.lang.System.getProperty;
 public class UrlUtils {
     public static final String HOSTNAME_REGEX = "(^http.://.*?)/";
 
-    private static final String FORTSETT_PATH = "/dagpenger/utslagskriterier";
+    private static final String FORTSETT_PATH = "/soknaddagpenger/utslagskriterier";
 
     //todo: denne bør egentlig gå til dialoginnsending slik at den er generell for alle typer søknader
+    //todo: ^ Dette er egentlig fikset i foreldrepenge-soknaden (må sørge for at det blir riktig ved merge)
     public static String getFortsettUrl(String requestUrl, String behandlingId) {
         return getBaseUrl(requestUrl) + FORTSETT_PATH + "/" + behandlingId + "?utm_source=web&utm_medium=email&utm_campaign=2";
     }
 
     public static String getEttersendelseUrl(String requestUrl, String behandlingId) {
-        return getBaseUrl(requestUrl) + getProperty("dialoginnsending.ettersending.path") + "/" + behandlingId;
+        return getBaseUrl(requestUrl) + getProperty("soknadinnsending.ettersending.path") + "/" + behandlingId;
     }
 
     private static String getBaseUrl(String requestUrl) {
