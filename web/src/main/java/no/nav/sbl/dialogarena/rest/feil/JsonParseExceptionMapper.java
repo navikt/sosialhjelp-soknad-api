c709ea18ffa91df0b8d35fabf6056a99f100f80f
Christoffer Karlsson <christoffer.karlsson@nav.no>
2015-05-18 13:40:12 +0200

    [Bug] Fikset slik at stack traces ikke vil bli sendt til klienten

---
+++ web/src/main/java/no/nav/sbl/dialogarena/rest/feil/JsonParseExceptionMapper.java
@@ -0,0 +1,25 @@
+package no.nav.sbl.dialogarena.rest.feil;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.Response.serverError;
+import static no.nav.sbl.dialogarena.rest.feil.Feilmelding.NO_BIGIP_5XX_REDIRECT;
+
+@Provider
+public class JsonParseExceptionMapper implements ExceptionMapper<JsonParseException> {
+    private static final Logger logger = LoggerFactory.getLogger(JsonParseExceptionMapper.class);
+
+    @Override
+    public Response toResponse(JsonParseException e) {
+        logger.error("Noe uventet feilet", e);
+        return serverError().header(NO_BIGIP_5XX_REDIRECT, true).type(APPLICATION_JSON).entity(new Feilmelding("unexpected_error", "Noe uventet feilet")).build();
+
+    }
+}
