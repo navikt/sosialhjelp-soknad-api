18ccf47ca9eea07fb7187f49408b81401701d7e9
Christian H. Mosveen <christian.mosveen@nav.no>
2015-05-19 15:40:03 +0200

    [Test] Lagde noen få tester for delsteg og journalførende enhet i SoknadRessurs

--- web/src/test/java/no/nav/sbl/dialogarena/rest/ressurser/SoknadRessursTest.java
+++ web/src/test/java/no/nav/sbl/dialogarena/rest/ressurser/SoknadRessursTest.java
@@ -1,13 +1,30 @@
 package no.nav.sbl.dialogarena.rest.ressurser;
 
+import no.nav.sbl.dialogarena.soknadinnsending.business.domain.DelstegStatus;
+import no.nav.sbl.dialogarena.soknadinnsending.business.service.SoknadService;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import javax.ws.rs.BadRequestException;
 import java.lang.reflect.Method;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
 
+@RunWith(MockitoJUnitRunner.class)
 public class SoknadRessursTest {
+
+    public static final String BEHANDLINGSID = "123";
+
+    @Mock
+    SoknadService soknadService;
+
+    @InjectMocks
+    SoknadRessurs ressurs;
 
     @Test
     public void endepunkterSkalHaSikkerhet() {
@@ -16,4 +33,38 @@ public class SoknadRessursTest {
         assertThat("some", is("some"));
     }
 
+    @Test(expected = BadRequestException.class)
+    public void oppdaterSoknadUtenParametreSkalKasteException() {
+        ressurs.oppdaterSoknad(BEHANDLINGSID, null, null);
+    }
+
+    @Test
+    public void oppdaterSoknadMedDelstegUtfyllingSkalSetteRiktigDelstegStatus() {
+        ressurs.oppdaterSoknad(BEHANDLINGSID, "utfylling", null);
+        verify(soknadService).settDelsteg(BEHANDLINGSID, DelstegStatus.UTFYLLING);
+    }
+
+    @Test
+    public void oppdaterSoknadMedDelstegOpprettetSkalSetteRiktigDelstegStatus() {
+        ressurs.oppdaterSoknad(BEHANDLINGSID, "opprettet", null);
+        verify(soknadService).settDelsteg(BEHANDLINGSID, DelstegStatus.OPPRETTET);
+    }
+
+    @Test
+    public void oppdaterSoknadMedDelstegVedleggSkalSetteRiktigDelstegStatus() {
+        ressurs.oppdaterSoknad(BEHANDLINGSID, "vedlegg", null);
+        verify(soknadService).settDelsteg(BEHANDLINGSID, DelstegStatus.SKJEMA_VALIDERT);
+    }
+
+    @Test
+    public void oppdaterSoknadMedDelstegOppsummeringSkalSetteRiktigDelstegStatus() {
+        ressurs.oppdaterSoknad(BEHANDLINGSID, "oppsummering", null);
+        verify(soknadService).settDelsteg(BEHANDLINGSID, DelstegStatus.VEDLEGG_VALIDERT);
+    }
+
+    @Test
+    public void oppdaterSoknadMedJournalforendeenhetSkalSetteJournalforendeEnhet() {
+        ressurs.oppdaterSoknad(BEHANDLINGSID, null, "NAV UTLAND");
+        verify(soknadService).settJournalforendeEnhet(BEHANDLINGSID, "NAV UTLAND");
+    }
 }
