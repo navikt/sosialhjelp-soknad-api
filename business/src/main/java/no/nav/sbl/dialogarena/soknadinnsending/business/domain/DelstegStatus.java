a5856cac2dcb6210e964580c067463ac4ff693e4
Christian H. Mosveen <christian.mosveen@nav.no>
2015-05-21 13:23:49 +0200

    [Sonar] Test for lagring av faktum

--- business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/domain/DelstegStatus.java
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/domain/DelstegStatus.java
@@ -6,7 +6,7 @@ import java.util.List;
 public enum DelstegStatus {
     OPPRETTET, UTFYLLING, SKJEMA_VALIDERT, VEDLEGG_VALIDERT, SAMTYKKET, ETTERSENDING_OPPRETTET, ETTERSENDING_UTFYLLING;
 
-    private static List<DelstegStatus> ETTERSENDING = Arrays.asList(ETTERSENDING_OPPRETTET, ETTERSENDING_UTFYLLING);
+    private static final List<DelstegStatus> ETTERSENDING = Arrays.asList(ETTERSENDING_OPPRETTET, ETTERSENDING_UTFYLLING);
     public boolean erEttersending() {
         return ETTERSENDING.contains(this);
     }
