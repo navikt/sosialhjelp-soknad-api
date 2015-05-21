fa3ff33b7219717200f630a89c3c3c7a9eb61b2a
Eirik L. Wang <eirik.wang@nav.no>
2015-05-21 12:52:30 +0200

    [Bug] propper fix for at en ikke henter ut arbeidsforhold om det er overføring av fedrekvote.

--- business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/kravdialoginformasjon/KravdialogInformasjon.java
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/kravdialoginformasjon/KravdialogInformasjon.java
@@ -1,5 +1,7 @@
 package no.nav.sbl.dialogarena.soknadinnsending.business.kravdialoginformasjon;
 
+import no.nav.sbl.dialogarena.soknadinnsending.business.domain.WebSoknad;
+
 import java.util.List;
 
 public interface KravdialogInformasjon {
@@ -9,10 +11,15 @@ public interface KravdialogInformasjon {
     String BOLK_ARBEIDSFORHOLD = "Arbeidsforhold";
 
     String getSoknadTypePrefix();
+
     String getSoknadUrlKey();
+
     String getFortsettSoknadUrlKey();
-    List<String> getSoknadBolker();
+
+    List<String> getSoknadBolker(WebSoknad soknad);
+
     String getStrukturFilnavn();
+
     List<String> getSkjemanummer();
 
 }
