bd7183880205e2b750734524e99ceb79ad001e7d
Christoffer Karlsson <christoffer.karlsson@nav.no>
2015-05-21 12:51:10 +0200

    [Bug] Fikset slik at egne feilmeldinger vises og ikke noe standard fra Jackson

--- web/src/main/java/no/nav/sbl/dialogarena/rest/SoknadApplication.java
+++ web/src/main/java/no/nav/sbl/dialogarena/rest/SoknadApplication.java
@@ -1,5 +1,6 @@
 package no.nav.sbl.dialogarena.rest;
 
+import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger;
@@ -13,7 +14,11 @@ public class SoknadApplication extends ResourceConfig {
     private static final Logger logger = LoggerFactory.getLogger(SoknadApplication.class);
 
     public SoknadApplication() {
+        // JacksonJaxbJsonProvider registreres manuelt for å unngå å dra inn Jacksons egne ExceptionMappers, som
+        // returnerer litt for mye informasjon i sine feilmeldinger. Desse ExceptionMappers har @Provider-annotationer
+        // og blir automatisk trukket inn hvis du tar tar inn hele Jackson-pakken for JSON.
         packages("no.nav.sbl.dialogarena.rest");
+        register(JacksonJaxbJsonProvider.class);
         register(MultiPartFeature.class);
 
         logger.info("Starter Jersey");
