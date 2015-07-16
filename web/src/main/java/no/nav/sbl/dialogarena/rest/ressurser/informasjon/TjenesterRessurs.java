4f4c550152e31f944af03fccbaa67c078904c29e
Tine Kleivane <tine.kleivane@nav.no>
2015-07-16 08:39:07 +0200

    [PK-23238] Eksponer AktivitetsRessurs via endepunkt /informasjon/tjenester/aktiviteter

--- web/src/main/java/no/nav/sbl/dialogarena/rest/ressurser/informasjon/TjenesterRessurs.java
+++ web/src/main/java/no/nav/sbl/dialogarena/rest/ressurser/informasjon/TjenesterRessurs.java
@@ -1,8 +1,12 @@
 package no.nav.sbl.dialogarena.rest.ressurser.informasjon;
 
 import no.nav.sbl.dialogarena.soknadinnsending.business.domain.Faktum;
+import no.nav.sbl.dialogarena.soknadinnsending.business.service.AktiviteterService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -10,19 +14,20 @@ import java.util.Arrays;
 import java.util.List;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static no.nav.modig.core.context.SubjectHandler.getSubjectHandler;
 
 @Controller
 @Path("/")
 @Produces(APPLICATION_JSON)
 public class TjenesterRessurs {
 
+    @Inject
+    AktiviteterService aktiviteterService;
+
     @GET
     @Path("/aktiviteter")
     public List<Faktum> hentAktiviteter() {
-        //TODO Fjernes når arenatjenesten er klar
-        return Arrays.asList(
-                new Faktum().medKey("aktivitet").medProperty("fom", "2015-01-15").medProperty("tom", "2015-02-15").medProperty("id", "9999").medProperty("navn", "Arbeidspraksis i ordinær virksomhet"),
-                new Faktum().medKey("aktivitet").medProperty("fom", "2015-02-31").medProperty("tom", "").medProperty("id", "8888").medProperty("navn", "Arbeid med bistand"));
+        return aktiviteterService.hentAktiviteter(getSubjectHandler().getUid());
     }
 
     @GET
--- web/src/test/java/no/nav/sbl/dialogarena/rest/ressurser/informasjon/TjenesterRessursTest.java
+++ web/src/test/java/no/nav/sbl/dialogarena/rest/ressurser/informasjon/TjenesterRessursTest.java
@@ -1,23 +1,42 @@
 package no.nav.sbl.dialogarena.rest.ressurser.informasjon;
 
 
+import no.nav.modig.core.context.StaticSubjectHandler;
+import no.nav.sbl.dialogarena.soknadinnsending.business.service.AktiviteterService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import static java.lang.System.setProperty;
+import static no.nav.modig.core.context.SubjectHandler.SUBJECTHANDLER_KEY;
 import static org.junit.Assert.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TjenesterRessursTest {
 
+    private String fodselsnummer;
     @InjectMocks
     TjenesterRessurs ressurs;
 
+    @Mock
+    AktiviteterService aktiviteterService;
+
+    @Before
+    public void setUp() throws Exception {
+        setProperty(SUBJECTHANDLER_KEY, StaticSubjectHandler.class.getName());
+        fodselsnummer = StaticSubjectHandler.getSubjectHandler().getUid();
+    }
+
     @Test
     public void skalHenteAktiviteter() throws Exception {
-        assertThat(ressurs.hentAktiviteter()).hasSize(2);
+        ressurs.hentAktiviteter();
+        verify(aktiviteterService).hentAktiviteter(fodselsnummer);
     }
 
     @Test
