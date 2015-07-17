66754d7649af8c9cc5e2569c7a7f3418164cfee7
Tine Kleivane <tine.kleivane@nav.no>
2015-07-17 10:22:29 +0200

    [kaizen QA] Beskriv interfacet HandlebarRegistry

--- web/src/main/java/no/nav/sbl/dialogarena/service/HandlebarRegistry.java
+++ web/src/main/java/no/nav/sbl/dialogarena/service/HandlebarRegistry.java
@@ -2,6 +2,10 @@ package no.nav.sbl.dialogarena.service;
 
 import com.github.jknack.handlebars.Helper;
 
+/*
+* Interface som skal registere Handlebars-helpers på en Handlebars-instanse
+* */
+
 public interface HandlebarRegistry {
 
     void registrerHelper(String name, Helper helper);
