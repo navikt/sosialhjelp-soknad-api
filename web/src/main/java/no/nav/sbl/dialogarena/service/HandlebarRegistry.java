448da0ccb414b5529801c2621d11f0bdc67eb836
Tine Kleivane <tine.kleivane@nav.no>
2015-07-17 15:25:32 +0200

    [kaizen QA] Forbedret kommentarer

--- web/src/main/java/no/nav/sbl/dialogarena/service/HandlebarRegistry.java
+++ web/src/main/java/no/nav/sbl/dialogarena/service/HandlebarRegistry.java
@@ -3,7 +3,12 @@ package no.nav.sbl.dialogarena.service;
 import com.github.jknack.handlebars.Helper;
 
 /*
-* Interface som skal registere Handlebars-helpers på en Handlebars-instanse
+* Interface som brukes av Handlebar-helpers for å registerer seg meg navn og implementasjon.
+* Mest vanlige bruksomraade er en spring-bean som extender RegistryAwareHelper som
+* bruker denne metoden for å tilgjengeliggjøre helpers for Handlebars senere.
+*
+* @See no.nav.sbl.dialogarena.service.helpers.RegistryAwareHelper
+*
 * */
 
 public interface HandlebarRegistry {
