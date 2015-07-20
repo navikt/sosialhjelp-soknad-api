a7b8921e5f26ee40480110a2acde6bbe952856d3
Tine Kleivane <tine.kleivane@nav.no>
2015-07-20 15:01:53 +0200

    [PK-23238] Returner liste av fakta for målgruppe service

--- business/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/business/service/MaalgrupperServiceTest.java
+++ business/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/business/service/MaalgrupperServiceTest.java
@@ -1,10 +1,15 @@
 package no.nav.sbl.dialogarena.soknadinnsending.business.service;
 
+import no.nav.sbl.dialogarena.soknadinnsending.business.domain.Faktum;
 import no.nav.tjeneste.virksomhet.maalgruppeinformasjon.v1.FinnMaalgruppeinformasjonListePersonIkkeFunnet;
 import no.nav.tjeneste.virksomhet.maalgruppeinformasjon.v1.FinnMaalgruppeinformasjonListeSikkerhetsbegrensning;
 import no.nav.tjeneste.virksomhet.maalgruppeinformasjon.v1.MaalgruppeinformasjonV1;
+import no.nav.tjeneste.virksomhet.maalgruppeinformasjon.v1.informasjon.WSMaalgruppe;
+import no.nav.tjeneste.virksomhet.maalgruppeinformasjon.v1.informasjon.WSPeriode;
 import no.nav.tjeneste.virksomhet.maalgruppeinformasjon.v1.meldinger.WSFinnMaalgruppeinformasjonListeRequest;
+import no.nav.tjeneste.virksomhet.maalgruppeinformasjon.v1.meldinger.WSFinnMaalgruppeinformasjonListeResponse;
 import org.joda.time.LocalDate;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -12,6 +17,8 @@ import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.fail;
@@ -30,13 +37,19 @@ public class MaalgrupperServiceTest {
 
     @Captor
     private ArgumentCaptor<WSFinnMaalgruppeinformasjonListeRequest> argument;
+    public static final String FODSELSNUMMER = "00000000000";
+
+    @Before
+    public void setUp() throws Exception {
+        when(webservice.finnMaalgruppeinformasjonListe(any(WSFinnMaalgruppeinformasjonListeRequest.class)))
+                .thenReturn(new WSFinnMaalgruppeinformasjonListeResponse());
+    }
 
     @Test
     public void finnMaalgruppeinformasjonListeKallesMedFodselsnummer() throws FinnMaalgruppeinformasjonListePersonIkkeFunnet, FinnMaalgruppeinformasjonListeSikkerhetsbegrensning {
-        String fodselsnummer = "00000000000";
-        maalgrupperService.hentMaalgrupper(fodselsnummer);
+        maalgrupperService.hentMaalgrupper(FODSELSNUMMER);
         verify(webservice).finnMaalgruppeinformasjonListe(argument.capture());
-        assertThat(argument.getValue().getPersonident()).isEqualTo(fodselsnummer);
+        assertThat(argument.getValue().getPersonident()).isEqualTo(FODSELSNUMMER);
     }
 
     @Test(expected = RuntimeException.class)
@@ -61,9 +74,51 @@ public class MaalgrupperServiceTest {
 
     @Test
     public void finnMaalgruppeinformasjonListeKallesMedPeriode() throws Exception {
-        String fodselsnummer = "00000000000";
-        maalgrupperService.hentMaalgrupper(fodselsnummer);
+        maalgrupperService.hentMaalgrupper(FODSELSNUMMER);
         verify(webservice).finnMaalgruppeinformasjonListe(argument.capture());
         assertThat(argument.getValue().getPeriode().getFom()).isEqualTo(new LocalDate("2015-01-01"));
+    }
+
+    @Test
+    public void skalReturnereListeAvMaalgruppeFakta() throws Exception {
+        WSMaalgruppe maalgruppe = lagMaalgruppe("", new LocalDate());
+        WSMaalgruppe maalgruppe2 = lagMaalgruppe("", new LocalDate());
+        when(webservice.finnMaalgruppeinformasjonListe(any(WSFinnMaalgruppeinformasjonListeRequest.class)))
+                .thenReturn(lagMaalgruppeRequest(maalgruppe, maalgruppe2));
+
+        List<Faktum> fakta = maalgrupperService.hentMaalgrupper(FODSELSNUMMER);
+        assertThat(fakta).hasSize(2);
+        for (Faktum faktum : fakta) {
+            assertThat(faktum.getKey()).isEqualTo("maalgruppe");
+        }
+    }
+
+    @Test
+    public void skalReturnereListeAvFaktumMedRettInnhold() throws Exception {
+        String arbeidssoker = "Arbeidssøker";
+        String ensligForsorger = "Enslig forsørger arbeidssøker";
+        WSMaalgruppe maalgruppe = lagMaalgruppe(arbeidssoker, new LocalDate("2015-02-02"));
+        WSMaalgruppe maalgruppe2 = lagMaalgruppe(ensligForsorger, new LocalDate("2015-03-02"));
+
+        when(webservice.finnMaalgruppeinformasjonListe(any(WSFinnMaalgruppeinformasjonListeRequest.class)))
+                .thenReturn(lagMaalgruppeRequest(maalgruppe, maalgruppe2));
+
+        List<Faktum> fakta = maalgrupperService.hentMaalgrupper(FODSELSNUMMER);
+        assertThat(fakta.get(0).getProperties()).containsEntry("navn", arbeidssoker);
+        assertThat(fakta.get(0).getProperties()).containsEntry("fom", "2015-02-02");
+
+        assertThat(fakta.get(1).getProperties()).containsEntry("navn", ensligForsorger);
+        assertThat(fakta.get(1).getProperties()).containsEntry("fom", "2015-03-02");
+    }
+
+    private WSFinnMaalgruppeinformasjonListeResponse lagMaalgruppeRequest(WSMaalgruppe... maalgrupper) {
+        return new WSFinnMaalgruppeinformasjonListeResponse()
+                .withMaalgruppeListe(maalgrupper);
+    }
+
+    private WSMaalgruppe lagMaalgruppe(String maalgruppenavn, LocalDate fom) {
+        return new WSMaalgruppe()
+                .withMaalgruppenavn(maalgruppenavn)
+                .withGyldighetsperiode(new WSPeriode().withFom(fom));
     }
 }
