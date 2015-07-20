657b658635236edb5c0dcae11300ddea1014d913
Tine Kleivane <tine.kleivane@nav.no>
2015-07-20 16:37:04 +0200

    [PK-23238] Spring wiring med mockdata

---
+++ consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/wsconfig/MaalgruppeinformasjonWSConfig.java
@@ -0,0 +1,17 @@
+package no.nav.sbl.dialogarena.soknadinnsending.consumer.wsconfig;
+
+import no.nav.sbl.dialogarena.sendsoknad.mockmodul.tjenester.MaalgrupperMock;
+import no.nav.tjeneste.virksomhet.maalgruppeinformasjon.v1.MaalgruppeinformasjonV1;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.mockito.Mockito.mock;
+
+@Configuration
+public class MaalgruppeinformasjonWSConfig {
+
+    @Bean
+    public MaalgruppeinformasjonV1 maalgruppeinformasjonEndpoint() {
+        return MaalgrupperMock.maalgruppeinformasjonV1();
+    }
+}
