8a16cdc1bc8efbe124e553439418201bbf070d44
Ingvild Indrebø <ingvild.indrebo@nav.no>
2015-07-30 16:44:45 +0200

    [PK-23238] Utvid likeVedlegg til å sjekke på vedleggslister
    *likeVedlegg kan ta inn liste(r) med gamle og nye vedlegg for å sjekke at listene inneholder eksakt de samme vedleggene

--- business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/util/VedleggsgenereringUtil.java
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/util/VedleggsgenereringUtil.java
@@ -7,11 +7,14 @@ import org.apache.commons.lang3.builder.EqualsBuilder;
 import java.util.Arrays;
 import java.util.List;
 
-import static no.nav.sbl.dialogarena.soknadinnsending.business.domain.Vedlegg.Status.*;
+import static no.nav.sbl.dialogarena.soknadinnsending.business.domain.Vedlegg.Status.LastetOpp;
+import static no.nav.sbl.dialogarena.soknadinnsending.business.domain.Vedlegg.Status.SendesIkke;
+import static no.nav.sbl.dialogarena.soknadinnsending.business.domain.Vedlegg.Status.SendesSenere;
+import static no.nav.sbl.dialogarena.soknadinnsending.business.domain.Vedlegg.Status.VedleggAlleredeSendt;
+import static no.nav.sbl.dialogarena.soknadinnsending.business.domain.Vedlegg.Status.VedleggKreves;
+import static no.nav.sbl.dialogarena.soknadinnsending.business.domain.Vedlegg.Status.VedleggSendesAvAndre;
 
 public class VedleggsgenereringUtil {
-
-
     public static boolean likeVedlegg(Vedlegg gammeltVedlegg, Vedlegg nyttVedlegg) {
         boolean vedleggErLike = new EqualsBuilder()
                 .append(nyttVedlegg.getSoknadId(), gammeltVedlegg.getSoknadId())
@@ -20,12 +23,11 @@ public class VedleggsgenereringUtil {
                 .append(nyttVedlegg.getSkjemanummerTillegg(), gammeltVedlegg.getSkjemanummerTillegg())
                 .isEquals();
 
-        if(gammeltVedlegg.getInnsendingsvalg() == nyttVedlegg.getInnsendingsvalg()){
+        if (gammeltVedlegg.getInnsendingsvalg() == nyttVedlegg.getInnsendingsvalg()) {
             return vedleggErLike;
         }
-        return erInnsendingsvalgPaakrevd(gammeltVedlegg, nyttVedlegg) &&
-                vedleggErLike;
 
+        return erInnsendingsvalgPaakrevd(gammeltVedlegg, nyttVedlegg) && vedleggErLike;
     }
 
     private static boolean erInnsendingsvalgPaakrevd(Vedlegg gammeltVedlegg, Vedlegg nyttVedlegg) {
@@ -39,5 +41,32 @@ public class VedleggsgenereringUtil {
 
         return paakrevdeVedleggsstatuser.contains(gammeltVedlegg.getInnsendingsvalg()) &&
                 paakrevdeVedleggsstatuser.contains(nyttVedlegg.getInnsendingsvalg());
+    }
+
+    public static boolean likeVedlegg(List<Vedlegg> gamleVedlegg, Vedlegg nyttVedlegg) {
+        boolean erLikeVedlegg = false;
+
+        int i = 0;
+        while (!erLikeVedlegg && i < gamleVedlegg.size()) {
+            erLikeVedlegg = likeVedlegg(gamleVedlegg.get(i), nyttVedlegg);
+            i++;
+        }
+
+        return erLikeVedlegg;
+    }
+
+    public static boolean likeVedlegg(List<Vedlegg> gamleVedlegg, List<Vedlegg> nyeVedlegg) {
+        if(gamleVedlegg.size() == nyeVedlegg.size()) {
+            boolean erLikeVedlegg = true;
+
+            int i = 0;
+            while (erLikeVedlegg && i < gamleVedlegg.size()) {
+                erLikeVedlegg = likeVedlegg(gamleVedlegg, nyeVedlegg.get(i));
+                i++;
+            }
+
+            return erLikeVedlegg;
+        }
+        return false;
     }
 }
