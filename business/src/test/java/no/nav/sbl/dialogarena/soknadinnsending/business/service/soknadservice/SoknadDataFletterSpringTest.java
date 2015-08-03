935433ba74aba0d481f52c1be57e1802b4f7a790
Jarle Svendsrud <jarle.svendsrud@nav.no>
2015-08-03 15:44:10 +0200

    [kaizen] Legg på springtest for datafletter

---
+++ business/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/business/service/soknadservice/SoknadDataFletterSpringTest.java
@@ -0,0 +1,53 @@
+package no.nav.sbl.dialogarena.soknadinnsending.business.service.soknadservice;
+
+import no.nav.sbl.dialogarena.soknadinnsending.business.SoknadDataFletterIntegrationTestContext;
+import no.nav.sbl.dialogarena.soknadinnsending.business.db.soknad.SoknadRepository;
+import no.nav.sbl.dialogarena.soknadinnsending.business.domain.DelstegStatus;
+import no.nav.sbl.dialogarena.soknadinnsending.business.domain.WebSoknad;
+import no.nav.sbl.dialogarena.soknadinnsending.business.domain.oppsett.SoknadStruktur;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.inject.Inject;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.joda.time.DateTime.now;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SoknadDataFletterIntegrationTestContext.class)
+public class SoknadDataFletterSpringTest {
+    WebSoknad soknad;
+    String uuid = "uid";
+    String skjemaNummer = "";
+    long soknadId;
+
+    @Inject
+    private SoknadRepository lokalDb;
+
+    @Inject
+    private SoknadDataFletter soknadDataFletter;
+
+    @Test
+    public void henterTemakode_FOR_forForeldrepenger() {
+        skjemaNummer = "NAV 14-05.06";
+        opprettOgPersisterSoknad("behId", "aktor");
+        SoknadStruktur soknadStruktur = soknadDataFletter.hentSoknadStruktur(soknadId);
+        assertThat(soknadStruktur.getTemaKode(), equalTo("FOR"));
+    }
+
+    private Long opprettOgPersisterSoknad(String behId, String aktor) {
+        soknad = WebSoknad.startSoknad()
+                .medUuid(uuid)
+                .medAktorId(aktor)
+                .medBehandlingId(behId)
+                .medDelstegStatus(DelstegStatus.OPPRETTET)
+                .medskjemaNummer(skjemaNummer).medOppretteDato(now());
+        soknadId = lokalDb.opprettSoknad(soknad);
+        soknad.setSoknadId(soknadId);
+        return soknadId;
+    }
+
+}
