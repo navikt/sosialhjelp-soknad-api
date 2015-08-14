3810697ac3b7f5e3f6152c8add26adf72433bca5
Eirik L. Wang <eirik.wang@nav.no>
2015-08-14 08:36:41 +0200

    [PK-23238] Lagt til reell implementasjon av din aktivitet.
    Conflicts:
    	consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/wsconfig/SakOgAktivitetInformasjonWSConfig.java

--- consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/wsconfig/SakOgAktivitetInformasjonWSConfig.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/wsconfig/SakOgAktivitetInformasjonWSConfig.java
@@ -1,15 +1,46 @@
 package no.nav.sbl.dialogarena.soknadinnsending.consumer.wsconfig;
 
 import no.nav.sbl.dialogarena.sendsoknad.mockmodul.tjenester.AktiviteterMock;
-import no.nav.tjeneste.virksomhet.sakogaktivitet.v1.SakOgAktivitetV1;
+import no.nav.sbl.dialogarena.soknadinnsending.consumer.ServiceBuilder;
+import no.nav.tjeneste.virksomhet.sakogaktivitetinformasjon.v1.SakOgAktivitetInformasjonV1;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.xml.namespace.QName;
+
+import static no.nav.sbl.dialogarena.soknadinnsending.consumer.util.InstanceSwitcher.createSwitcher;
+
 @Configuration
 public class SakOgAktivitetInformasjonWSConfig {
+    public static final String SAKOGAKTIVITET_KEY = "start.sakogaktivitet.withmock";
+    @Value("${soknad.webservice.arena.sakogaktivitet.url}")
+    private String sakOgAktivitetEndpoint;
 
     @Bean
-    public SakOgAktivitetV1 sakOgAktivitetInformasjonEndpoint() {
+    public SakOgAktivitetInformasjonV1 sakOgAktivitetInformasjonEndpoint() {
+        SakOgAktivitetInformasjonV1 mock = sakOgAktivitetInformasjonEndpointMock();
+        SakOgAktivitetInformasjonV1 prod = sakOgAktivitetInformasjonEndpointWS();
+        return createSwitcher(prod, mock, SAKOGAKTIVITET_KEY, SakOgAktivitetInformasjonV1.class);
+    }
+    private ServiceBuilder<SakOgAktivitetInformasjonV1>.PortTypeBuilder<SakOgAktivitetInformasjonV1> factory() {
+        return new ServiceBuilder<>(SakOgAktivitetInformasjonV1.class)
+                .asStandardService()
+                .withAddress(sakOgAktivitetEndpoint)
+                .withWsdl("classpath:/no/nav/tjeneste/virksomhet/sakOgAktivitetInformasjon/v1/Binding.wsdl")
+                .withServiceName(new QName("http://nav.no/tjeneste/virksomhet/sakOgAktivitetInformasjon/v1/Binding", "SakOgAktivitetInformasjon_v1"))
+                .withEndpointName(new QName("http://nav.no/tjeneste/virksomhet/sakOgAktivitetInformasjon/v1/Binding", "sakOgAktivitetInformasjon_v1Port"))
+                .build()
+                .withHttpsMock()
+                .withMDC();
+    }
+
+    @Bean
+    public SakOgAktivitetInformasjonV1 sakOgAktivitetInformasjonEndpointMock() {
         return new AktiviteterMock().sakOgAktivitetInformasjonV1Mock();
+    }
+    @Bean
+    public SakOgAktivitetInformasjonV1 sakOgAktivitetInformasjonEndpointWS() {
+        return factory().withUserSecurity().get();
     }
 }
