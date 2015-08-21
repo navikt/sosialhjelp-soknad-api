614b2b59d6c55d732b03892fd8105aeac65aa3b5
June Henriksen <june.henriksen2@nav.no>
2015-08-21 08:58:29 +0200

    [TG] Navneendring: /SoknadsFelt/BrukerFaktum

--- business/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/business/service/FaktaServiceTest.java
+++ business/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/business/service/FaktaServiceTest.java
@@ -65,7 +65,7 @@ public class FaktaServiceTest {
         when(soknadRepository.hentSoknad(soknadId)).thenReturn(new WebSoknad().medId(soknadId).medDelstegStatus(DelstegStatus.UTFYLLING));
         when(soknadRepository.opprettFaktum(soknadId, faktum)).thenReturn(2L);
         when(soknadRepository.hentFaktum(2L)).thenReturn(faktum);
-        faktaService.opprettSoknadsFelt(behandlingsId, faktum);
+        faktaService.opprettBrukerFaktum(behandlingsId, faktum);
         verify(soknadRepository, never()).settDelstegstatus(anyLong(), any(DelstegStatus.class));
     }
 
@@ -77,7 +77,7 @@ public class FaktaServiceTest {
         when(soknadRepository.hentSoknad(behandlingsId)).thenReturn(new WebSoknad().medId(soknadId));
         when(soknadRepository.opprettFaktum(soknadId, faktum)).thenReturn(2L);
         when(soknadRepository.hentFaktum(2L)).thenReturn(faktum);
-        faktaService.opprettSoknadsFelt(behandlingsId, faktum);
+        faktaService.opprettBrukerFaktum(behandlingsId, faktum);
         verify(soknadRepository).settSistLagretTidspunkt(soknadId);
         when(soknadRepository.hentBarneFakta(soknadId, faktum.getFaktumId())).thenReturn(Collections.singletonList(new Faktum().medKey("subkey")));
 
