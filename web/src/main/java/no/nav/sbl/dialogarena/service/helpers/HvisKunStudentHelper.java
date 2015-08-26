a80a9f3eb3d3f8e741132c26f9a886bea308f301
Nygård, Geir Furland <Geir.Furland.Nygard@nav.no>
2015-08-26 16:23:21 +0200

    [kaizen] Handlebar helper til 'hvisKunStudent', som brukes i AAP

---
+++ web/src/main/java/no/nav/sbl/dialogarena/service/helpers/HvisKunStudentHelper.java
@@ -0,0 +1,53 @@
+package no.nav.sbl.dialogarena.service.helpers;
+
+import com.github.jknack.handlebars.Helper;
+import com.github.jknack.handlebars.Options;
+import no.nav.sbl.dialogarena.service.HandleBarKjoerer;
+import no.nav.sbl.dialogarena.soknadinnsending.business.domain.Faktum;
+import no.nav.sbl.dialogarena.soknadinnsending.business.domain.WebSoknad;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class HvisKunStudentHelper extends RegistryAwareHelper<Object> {
+
+    private static final HvisKunStudentHelper INSTANS = new HvisKunStudentHelper();
+    private static final String NAVN = "hvisKunStudent";
+
+    @Override
+    public String getNavn() {
+        return NAVN;
+    }
+
+    @Override
+    public Helper<Object> getHelper() {
+        return INSTANS;
+    }
+
+    @Override
+    public String getBeskrivelse() {
+        return "Sjekker om brukeren har en annen status enn student (f.eks sykmeldt, i arbeid osv.)";
+    }
+
+    @Override
+    public CharSequence apply(Object context, Options options) throws IOException {
+        WebSoknad soknad = HandleBarKjoerer.finnWebSoknad(options.context);
+
+        Faktum iArbeidFaktum = soknad.getFaktumMedKey("navaerendeSituasjon.iArbeid");
+        Faktum sykmeldtFaktum = soknad.getFaktumMedKey("navaerendeSituasjon.sykmeldt");
+        Faktum arbeidsledigFaktum = soknad.getFaktumMedKey("navaerendeSituasjon.arbeidsledig");
+        Faktum forstegangstjenesteFaktum = soknad.getFaktumMedKey("navaerendeSituasjon.forstegangstjeneste");
+        Faktum annetFaktum = soknad.getFaktumMedKey("navaerendeSituasjon.annet");
+
+        Faktum[] fakta = {iArbeidFaktum, sykmeldtFaktum, arbeidsledigFaktum, forstegangstjenesteFaktum, annetFaktum};
+
+        for (Faktum faktum : fakta) {
+            if (faktum != null && "true".equals(faktum.getValue())) {
+                return options.inverse(this);
+            }
+        }
+
+        return options.fn(this);
+    }
+}
