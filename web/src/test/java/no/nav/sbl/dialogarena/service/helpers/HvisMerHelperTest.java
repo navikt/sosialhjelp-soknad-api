676a5a9ae0d2ba8cea53882497aff5eb405f1d65
Tine Kleivane <tine.kleivane@nav.no>
2015-08-28 10:23:04 +0200

    [HELPER] Flere tester og dokumentasjon

--- web/src/test/java/no/nav/sbl/dialogarena/service/helpers/HvisMerHelperTest.java
+++ web/src/test/java/no/nav/sbl/dialogarena/service/helpers/HvisMerHelperTest.java
@@ -2,6 +2,7 @@ package no.nav.sbl.dialogarena.service.helpers;
 
 import com.github.jknack.handlebars.Context;
 import com.github.jknack.handlebars.Handlebars;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -11,15 +12,34 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class HvisMerHelperTest {
 
+    private Handlebars handlebars;
+    private HashMap<Object, Object> model;
+
+    @Before
+    public void setUp() throws Exception {
+        handlebars = new Handlebars();
+        handlebars.registerHelper(HvisMerHelper.NAVN, HvisMerHelper.INSTANS);
+        model = new HashMap<>();
+    }
+
     @Test
     public void skalViseStorreVedStorreVerdi() throws Exception {
-        Handlebars handlebars = new Handlebars();
-        handlebars.registerHelper(HvisMerHelper.NAVN, HvisMerHelper.INSTANS);
-
-        HashMap<Object, Object> model = new HashMap<>();
         model.put("verdi", "2");
         String compiles = handlebars.compileInline("{{#hvisMer verdi \"1\"}}Verdien er høyere{{/hvisMer}}").apply(Context.newContext(model));
         assertThat(compiles).isEqualTo("Verdien er høyere");
+    }
 
+    @Test
+    public void skalViseElseVedLikVerdi() throws Exception {
+        model.put("verdi", "1");
+        String compiles = handlebars.compileInline("{{#hvisMer verdi \"1\"}}Verdien er høyere{{else}}Verdien er lik eller mindre{{/hvisMer}}").apply(Context.newContext(model));
+        assertThat(compiles).isEqualTo("Verdien er lik eller mindre");
+    }
+
+    @Test
+    public void skalViseElseVedMindreVerdi() throws Exception {
+        model.put("verdi", "0");
+        String compiles = handlebars.compileInline("{{#hvisMer verdi \"1\"}}Verdien er høyere{{else}}Verdien er lik eller mindre{{/hvisMer}}").apply(Context.newContext(model));
+        assertThat(compiles).isEqualTo("Verdien er lik eller mindre");
     }
 }
