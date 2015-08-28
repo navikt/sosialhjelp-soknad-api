922989e7e6b620c2e978ed21ca7d19601bd5522b
Ingvild Indrebø <ingvild.indrebo@nav.no>
2015-08-28 12:43:30 +0200

    [PK-21803] Legg til adresse, avstand og alternativeTransportUtgifter for samlingReiseTransformering
    * Mangler å legge til samlingsdato da denne kun støtter en liste med en dato og ikke en liste med perioder

--- business/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/business/transformer/tilleggsstonader/SamlingReiseTilXmlTest.java
+++ business/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/business/transformer/tilleggsstonader/SamlingReiseTilXmlTest.java
@@ -5,6 +5,9 @@ import no.nav.sbl.dialogarena.soknadinnsending.business.domain.Faktum;
 import no.nav.sbl.dialogarena.soknadinnsending.business.domain.WebSoknad;
 import org.junit.Test;
 
+import java.math.BigInteger;
+
+import static no.nav.sbl.dialogarena.soknadinnsending.business.transformer.tilleggsstonader.StofoKodeverkVerdier.InnsendingsintervallerKodeverk.uke;
 import static no.nav.sbl.dialogarena.soknadinnsending.business.transformer.tilleggsstonader.StofoTestUtils.periodeMatcher;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -36,6 +39,7 @@ public class SamlingReiseTilXmlTest {
         assertThat(samling.getPeriode().getFom()).is(periodeMatcher(2015, 1, 1));
         assertThat(samling.getPeriode().getTom()).is(periodeMatcher(2015, 3, 2));
     }
+
     @Test
     public void skalKonvertereFaktumStrukturMedRiktigPeriodeForEnSamling() {
         WebSoknad soknad = new WebSoknad();
@@ -50,4 +54,36 @@ public class SamlingReiseTilXmlTest {
         assertThat(samling.getPeriode().getTom()).is(periodeMatcher(2016, 1, 2));
     }
 
+    @Test
+    public void skalKonvertereFaktumStruktur() {
+        WebSoknad soknad = new WebSoknad();
+        soknad.getFakta().add(new Faktum().medKey("reise.samling.fleresamlinger").medValue("en"));
+        soknad.getFakta().add(new Faktum().medKey("reise.samling.aktivitetsperiode").medProperty("fom", "2016-01-01").medProperty("tom", "2016-01-02"));
+
+        soknad.getFakta().add(new Faktum().medKey("reise.samling.reisemaal").medProperty("adresse", "En adresse").medProperty("postnr", "2233"));
+        soknad.getFakta().add(new Faktum().medKey("reise.samling.reiselengde").medValue("100"));
+
+        soknad.getFakta().add(new Faktum().medKey("reise.samling.offentligtransport.egenbil.parkering").medValue("false"));
+        soknad.getFakta().add(new Faktum().medKey("reise.samling.offentligtransport.egenbil.sendebekreftelse").medValue(uke.name()));
+        soknad.getFakta().add(new Faktum().medKey("reise.samling.offentligtransport.drosje").medValue("true"));
+        soknad.getFakta().add(new Faktum().medKey("reise.samling.offentligtransport.drosje.belop").medValue("50"));
+        soknad.getFakta().add(new Faktum().medKey("reise.samling.offentligtransport").medValue("false"));
+        soknad.getFakta().add(new Faktum().medKey("reise.samling.offentligtransport.egenbil").medValue("true"));
+        soknad.getFakta().add(new Faktum().medKey("reise.samling.offentligtransport.egenbil.kostnader.bompenger").medValue("1"));
+        soknad.getFakta().add(new Faktum().medKey("reise.samling.offentligtransport.egenbil.kostnader.parkering").medValue("1"));
+        soknad.getFakta().add(new Faktum().medKey("reise.samling.offentligtransport.egenbil.kostnader.piggdekk").medValue("2"));
+        soknad.getFakta().add(new Faktum().medKey("reise.samling.offentligtransport.egenbil.kostnader.ferge").medValue("3"));
+        soknad.getFakta().add(new Faktum().medKey("reise.samling.offentligtransport.egenbil.kostnader.annet").medValue("4"));
+
+
+        ReiseObligatoriskSamling xml = new SamlingReiseTilXml().transform(soknad);
+
+        assertThat(xml.getReiseadresser()).isEqualTo("En adresse, 2233");
+        assertThat(xml.getAvstand()).isEqualTo(new BigInteger("100"));
+
+        assertThat(xml.getAlternativeTransportutgifter().getDrosjeTransportutgifter().getBeloep().equals(new BigInteger("50")));
+        assertThat(xml.getAlternativeTransportutgifter().getEgenBilTransportutgifter().getSumAndreUtgifter()).isEqualTo(11d);
+        assertThat(xml.getAlternativeTransportutgifter().isKanEgenBilBrukes()).isEqualTo(true);
+        assertThat(xml.getAlternativeTransportutgifter().isKanOffentligTransportBrukes()).isEqualTo(false);
+    }
 }
