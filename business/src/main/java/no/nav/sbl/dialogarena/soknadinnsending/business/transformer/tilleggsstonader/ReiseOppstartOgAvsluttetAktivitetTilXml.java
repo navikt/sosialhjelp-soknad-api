dec1163f3a7b5ec1b091e9e31196995bbbe786b8
Eirik L. Wang <eirik.wang@nav.no>
2015-08-28 10:16:47 +0200

    [PK-25021] Gjort uthenting av kollektiv-utfigter felles for alle bolker.

--- business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/transformer/tilleggsstonader/ReiseOppstartOgAvsluttetAktivitetTilXml.java
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/transformer/tilleggsstonader/ReiseOppstartOgAvsluttetAktivitetTilXml.java
@@ -1,34 +1,31 @@
 package no.nav.sbl.dialogarena.soknadinnsending.business.transformer.tilleggsstonader;
 
-import no.nav.melding.virksomhet.soeknadsskjema.v1.soeknadsskjema.AlternativeTransportutgifter;
 import no.nav.melding.virksomhet.soeknadsskjema.v1.soeknadsskjema.BarnUnderAtten;
-import no.nav.melding.virksomhet.soeknadsskjema.v1.soeknadsskjema.DagligReise;
-import no.nav.melding.virksomhet.soeknadsskjema.v1.soeknadsskjema.DrosjeTransportutgifter;
-import no.nav.melding.virksomhet.soeknadsskjema.v1.soeknadsskjema.EgenBilTransportutgifter;
-import no.nav.melding.virksomhet.soeknadsskjema.v1.soeknadsskjema.Formaal;
-import no.nav.melding.virksomhet.soeknadsskjema.v1.soeknadsskjema.Innsendingsintervaller;
-import no.nav.melding.virksomhet.soeknadsskjema.v1.soeknadsskjema.KollektivTransportutgifter;
 import no.nav.melding.virksomhet.soeknadsskjema.v1.soeknadsskjema.ReiseOppstartOgAvsluttetAktivitet;
-import no.nav.sbl.dialogarena.soknadinnsending.business.domain.Faktum;
 import no.nav.sbl.dialogarena.soknadinnsending.business.domain.WebSoknad;
 import org.apache.commons.collections15.Transformer;
 
-import javax.xml.datatype.XMLGregorianCalendar;
 import java.math.BigInteger;
 
 import static no.nav.sbl.dialogarena.soknadinnsending.business.transformer.tilleggsstonader.StofoTransformers.extractValue;
 import static no.nav.sbl.dialogarena.soknadinnsending.business.transformer.tilleggsstonader.StofoTransformers.faktumTilPeriode;
 
 public class ReiseOppstartOgAvsluttetAktivitetTilXml implements Transformer<WebSoknad, ReiseOppstartOgAvsluttetAktivitet> {
+
+    private static final String FAKTUM_REISEMAAL = "reise.midlertidig.reisemaal";
+    private static final String FAKTUM_PERIODE = "reise.midlertidig.periode";
+    private static final String FAKTUM_AVSTAND = "reise.midlertidig.dagligreiseavstand";
+    private static final String FAKTUM_HJEMMEBOENDE = "reise.midlertidig.hjemmeboende";
+
     @Override
     public ReiseOppstartOgAvsluttetAktivitet transform(WebSoknad soknad) {
         ReiseOppstartOgAvsluttetAktivitet reise = new ReiseOppstartOgAvsluttetAktivitet();
-        reise.setPeriode(faktumTilPeriode(soknad.getFaktumMedKey("reise.midlertidig.periode")));
-        reise.setAvstand(extractValue(soknad.getFaktumMedKey("reise.midlertidig.dagligreiseavstand"), BigInteger.class));
-        reise.setAktivitetsstedAdresse(aktivitetsAdresse(soknad));
-        reise.setHarBarnUnderFemteklasse(extractValue(soknad.getFaktumMedKey("reise.midlertidig.hjemmeboende"), Boolean.class));
+        reise.setPeriode(faktumTilPeriode(soknad.getFaktumMedKey(FAKTUM_PERIODE)));
+        reise.setAvstand(extractValue(soknad.getFaktumMedKey(FAKTUM_AVSTAND), BigInteger.class));
+        reise.setAktivitetsstedAdresse(extractValue(soknad.getFaktumMedKey(FAKTUM_REISEMAAL), StofoKodeverkVerdier.SammensattAdresse.class).sammensattAdresse);
+        reise.setHarBarnUnderFemteklasse(extractValue(soknad.getFaktumMedKey(FAKTUM_HJEMMEBOENDE), Boolean.class));
         reise.setBarnUnderAtten(barnUnder18(soknad));
-        reise.setAlternativeTransportutgifter(alternativeTransportUtgifter(soknad));
+        reise.setAlternativeTransportutgifter(StofoUtils.alternativeTransportUtgifter(soknad, "midlertidig"));
         return reise;
     }
 
@@ -37,36 +34,5 @@ public class ReiseOppstartOgAvsluttetAktivitetTilXml implements Transformer<WebS
         return StofoTransformers.extractValue(soknad.getFaktumMedKey("barn"), BarnUnderAtten.class, "fno");
     }
 
-    private String aktivitetsAdresse(WebSoknad soknad) {
-        return String.format("%s, %s",
-                extractValue(soknad.getFaktumMedKey("reise.midlertidig.reisemaal"), String.class, "adresse"),
-                extractValue(soknad.getFaktumMedKey("reise.midlertidig.reisemaal"), String.class, "postnr"));
-    }
 
-    private AlternativeTransportutgifter alternativeTransportUtgifter(WebSoknad soknad) {
-        AlternativeTransportutgifter utgifter = new AlternativeTransportutgifter();
-        utgifter.setDrosjeTransportutgifter(extractValue(soknad.getFaktumMedKey("reise.midlertidig.offentligtransport.drosje.belop"), DrosjeTransportutgifter.class));
-
-        utgifter.setKanEgenBilBrukes(extractValue(soknad.getFaktumMedKey("reise.midlertidig.offentligtransport.egenbil"), Boolean.class));
-        if(utgifter.isKanEgenBilBrukes()) {
-            utgifter.setEgenBilTransportutgifter(egenBilUtgifter(soknad));
-        }
-
-        utgifter.setKanOffentligTransportBrukes(extractValue(soknad.getFaktumMedKey("reise.midlertidig.offentligtransport"), Boolean.class));
-        utgifter.setKollektivTransportutgifter(extractValue(soknad.getFaktumMedKey("reise.midlertidig.offentligtransport.utgift"), KollektivTransportutgifter.class));
-        return utgifter;
-    }
-
-    private EgenBilTransportutgifter egenBilUtgifter(WebSoknad soknad) {
-        EgenBilTransportutgifter utgifter = new EgenBilTransportutgifter();
-        utgifter.setSumAndreUtgifter(
-                StofoTransformers.sumDouble(
-                        soknad.getFaktumMedKey("reise.midlertidig.offentligtransport.egenbil.kostnader.bompenger"),
-                        soknad.getFaktumMedKey("reise.midlertidig.offentligtransport.egenbil.kostnader.parkering"),
-                        soknad.getFaktumMedKey("reise.midlertidig.offentligtransport.egenbil.kostnader.piggdekk"),
-                        soknad.getFaktumMedKey("reise.midlertidig.offentligtransport.egenbil.kostnader.ferge"),
-                        soknad.getFaktumMedKey("reise.midlertidig.offentligtransport.egenbil.kostnader.annet")
-                ));
-        return utgifter;
-    }
 }
--- business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/transformer/tilleggsstonader/StofoKodeverkVerdier.java
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/transformer/tilleggsstonader/StofoKodeverkVerdier.java
@@ -1,6 +1,12 @@
 package no.nav.sbl.dialogarena.soknadinnsending.business.transformer.tilleggsstonader;
 
 public class StofoKodeverkVerdier {
+    public static final class SammensattAdresse{
+        public final String sammensattAdresse;
+        public SammensattAdresse(String adresse, String postnr) {
+            sammensattAdresse = String.format("%s, %s", adresse, postnr);
+        }
+    }
     public enum FormaalKodeverk{
         oppfolging("OPPF"), jobbintervju("JOBB"), tiltraa("TILT");
         public  final String kodeverksverdi;
