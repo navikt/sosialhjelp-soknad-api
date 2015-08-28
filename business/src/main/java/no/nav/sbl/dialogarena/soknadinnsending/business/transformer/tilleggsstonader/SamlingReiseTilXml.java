df0486262986c6ecd4cb6ede65ea624e5240c349
Ingvild Indrebø <ingvild.indrebo@nav.no>
2015-08-28 12:43:30 +0200

    [PK-21803] Legg til adresse, avstand og alternativeTransportUtgifter for samlingReiseTransformering
    * Mangler å legge til samlingsdato da denne kun støtter en liste med en dato og ikke en liste med perioder

--- business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/transformer/tilleggsstonader/SamlingReiseTilXml.java
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/transformer/tilleggsstonader/SamlingReiseTilXml.java
@@ -4,20 +4,28 @@ import no.nav.melding.virksomhet.soeknadsskjema.v1.soeknadsskjema.Periode;
 import no.nav.melding.virksomhet.soeknadsskjema.v1.soeknadsskjema.ReiseObligatoriskSamling;
 import no.nav.sbl.dialogarena.soknadinnsending.business.domain.Faktum;
 import no.nav.sbl.dialogarena.soknadinnsending.business.domain.WebSoknad;
+
+import java.math.BigInteger;
 import java.util.List;
 
+import static no.nav.sbl.dialogarena.soknadinnsending.business.transformer.tilleggsstonader.StofoTransformers.extractValue;
 import static no.nav.sbl.dialogarena.soknadinnsending.business.transformer.tilleggsstonader.StofoTransformers.faktumTilPeriode;
 
 public class SamlingReiseTilXml {
     public ReiseObligatoriskSamling transform(WebSoknad soknad) {
         ReiseObligatoriskSamling reise = new ReiseObligatoriskSamling();
         reise.setPeriode(reiseSamlinger(soknad));
+        reise.setReiseadresser(StofoUtils.sammensattAdresse(soknad.getFaktumMedKey("reise.samling.reisemaal")));
+        reise.setAvstand(extractValue(soknad.getFaktumMedKey("reise.samling.reiselengde"), BigInteger.class)); //TODO har desimaler
+        reise.setAlternativeTransportutgifter(StofoUtils.alternativeTransportUtgifter(soknad, "samling"));
+        //TODO legge til samlingsdato når denne blir fikset
+
         return reise;
     }
 
     private Periode reiseSamlinger(WebSoknad soknad) {
         String samlingsfaktum = soknad.getFaktumMedKey("reise.samling.fleresamlinger").getValue();
-        if(samlingsfaktum.equals("en")) {
+        if(samlingsfaktum != null && samlingsfaktum.equals("en")) {
             return faktumTilPeriode(soknad.getFaktumMedKey("reise.samling.aktivitetsperiode"));
         }
         return faktumTilPeriode(lagPeriodeAvForsteOgSisteSamlingsperiode(soknad));
