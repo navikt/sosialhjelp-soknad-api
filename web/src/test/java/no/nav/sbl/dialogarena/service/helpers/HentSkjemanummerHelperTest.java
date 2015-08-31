fa7069e35f07da4e91182f369fa2e6e900fa1813
Mats Krüger Svensson <mats.kruger.svensson@nav.no>
2015-08-31 09:32:13 +0200

    [HELPER] tester om den traverserer context for å finne websoknad og om dagpengelogikk trigges

--- web/src/test/java/no/nav/sbl/dialogarena/service/helpers/HentSkjemanummerHelperTest.java
+++ web/src/test/java/no/nav/sbl/dialogarena/service/helpers/HentSkjemanummerHelperTest.java
@@ -2,6 +2,7 @@ package no.nav.sbl.dialogarena.service.helpers;
 
 import com.github.jknack.handlebars.Context;
 import com.github.jknack.handlebars.Handlebars;
+import no.nav.sbl.dialogarena.soknadinnsending.business.domain.Faktum;
 import no.nav.sbl.dialogarena.soknadinnsending.business.domain.WebSoknad;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,12 +10,9 @@ import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.io.IOException;
+import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.mockito.internal.verification.VerificationModeFactory.times;
 
 @RunWith(MockitoJUnitRunner.class)
 public class HentSkjemanummerHelperTest {
@@ -37,6 +35,37 @@ public class HentSkjemanummerHelperTest {
     }
 
 
+    @Test
+    public void viserSkjemaNummerSelvOmWebSoknadIParentContext() throws IOException {
+        WebSoknad webSoknad = new WebSoknad();
+        webSoknad.setSkjemaNummer("999999");
+
+        Context parentContext = Context.newContext(webSoknad);
+        Context childContext = Context.newContext(parentContext, "555555");
+
+        String compiled = handlebars.compileInline("Skjemanummer: {{ hentSkjemanummer }}").apply(childContext);
+        assertThat(compiled).isEqualTo("Skjemanummer: 999999");
+    }
+
+
+    @Test
+    public void viserDagpengerSkjemanummer() throws IOException {
+        WebSoknad dagPengeSoknad = lagDagpengeSoknad();
+
+        String compiled = handlebars.compileInline("Skjemanummer: {{ hentSkjemanummer }}").apply(dagPengeSoknad);
+        assertThat(compiled).isEqualTo("Skjemanummer: NAV 04-01.04");
+    }
+
+    private WebSoknad lagDagpengeSoknad() {
+        WebSoknad dagPengeSoknad = new WebSoknad();
+        dagPengeSoknad.setSkjemaNummer("NAV 04-01.03");
+
+        Faktum permitering = new Faktum().medSoknadId(1L).medFaktumId(1L).medKey("arbeidsforhold.permitteringsperiode");
+        permitering.medProperty("permiteringsperiodedatofra", "2000-01-01");
+        dagPengeSoknad.setFakta(Collections.singletonList(permitering));
+
+        return dagPengeSoknad;
+    }
 
 
 }
