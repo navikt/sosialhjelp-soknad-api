8fce39b48772412e2fc65c973f51f46ee079b38e
Mats Krüger Svensson <mats.kruger.svensson@nav.no>
2015-09-01 09:52:54 +0200

    [HELPER] hvis har ikke-innsendte dokumenter helper dratt ut i egen klasse og testet

---
+++ web/src/test/java/no/nav/sbl/dialogarena/service/helpers/HvisHarIkkeInnsendteDokumenterHelperTest.java
@@ -0,0 +1,60 @@
+package no.nav.sbl.dialogarena.service.helpers;
+
+import com.github.jknack.handlebars.Context;
+import com.github.jknack.handlebars.Handlebars;
+import no.nav.sbl.dialogarena.soknadinnsending.business.domain.Vedlegg;
+import no.nav.sbl.dialogarena.soknadinnsending.business.domain.WebSoknad;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HvisHarIkkeInnsendteDokumenterHelperTest {
+
+    private Handlebars handlebars;
+
+    @Before
+    public void setUp() throws Exception {
+        handlebars = new Handlebars();
+        handlebars.registerHelper(HvisHarIkkeInnsendteDokumenterHelper.NAVN, HvisHarIkkeInnsendteDokumenterHelper.INSTANS);
+    }
+
+    @Test
+    public void skalViseAtSoknadenIkkeHarInnsendteDokumenter() throws Exception {
+        WebSoknad webSoknad = lagSoknadMedVedleggStatus(Vedlegg.Status.VedleggKreves);
+
+        String compiled = handlebars.compileInline("{{#hvisHarIkkeInnsendteDokumenter}}har ikke-innsendte dokumenter{{/hvisHarIkkeInnsendteDokumenter}}").apply(Context.newContext(webSoknad));
+        assertThat(compiled).isEqualTo("har ikke-innsendte dokumenter");
+    }
+
+    @Test
+    public void skalViseAtSoknadenHarInnsendteDokumenter() throws Exception {
+        WebSoknad webSoknad = lagSoknadMedVedleggStatus(Vedlegg.Status.LastetOpp);
+
+        String compiled = handlebars.compileInline("{{#hvisHarIkkeInnsendteDokumenter}}har ikke-innsendte dokumenter{{else}}alt er innsendt{{/hvisHarIkkeInnsendteDokumenter}}").apply(Context.newContext(webSoknad));
+        assertThat(compiled).isEqualTo("alt er innsendt");
+    }
+
+
+    @Test
+    public void viserOmInnsendteDokumenterSelvOmWebSoknadIParentContext() throws Exception {
+        WebSoknad webSoknad = lagSoknadMedVedleggStatus(Vedlegg.Status.VedleggKreves);
+
+        Context parentContext = Context.newContext(webSoknad);
+        Context childContext = Context.newContext(parentContext, "random");
+
+        String compiled = handlebars.compileInline("{{#hvisHarIkkeInnsendteDokumenter}}har ikke-innsendte dokumenter{{/hvisHarIkkeInnsendteDokumenter}}").apply(childContext);
+        assertThat(compiled).isEqualTo("har ikke-innsendte dokumenter");
+    }
+
+    private WebSoknad lagSoknadMedVedleggStatus(Vedlegg.Status status) {
+        WebSoknad webSoknad = new WebSoknad();
+
+        Vedlegg vedlegg = new Vedlegg();
+        vedlegg.setInnsendingsvalg(status);
+        webSoknad.medVedlegg(vedlegg);
+
+        return webSoknad;
+    }
+
+}
