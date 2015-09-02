a4d03dbc7f40b4f293368473c84368731de3791f
Mats Krüger Svensson <mats.kruger.svensson@nav.no>
2015-09-02 12:52:28 +0200

    [HELPER] dratt ut innsendte vedlegg-hjelpere og testet dem

---
+++ web/src/main/java/no/nav/sbl/dialogarena/service/helpers/ForInnsendteVedleggHelper.java
@@ -0,0 +1,46 @@
+package no.nav.sbl.dialogarena.service.helpers;
+
+import com.github.jknack.handlebars.Helper;
+import com.github.jknack.handlebars.Options;
+import no.nav.sbl.dialogarena.soknadinnsending.business.domain.Vedlegg;
+import no.nav.sbl.dialogarena.soknadinnsending.business.domain.WebSoknad;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.List;
+
+import static no.nav.sbl.dialogarena.service.Hjelpemetoder.finnWebSoknad;
+import static no.nav.sbl.dialogarena.service.Hjelpemetoder.lagItererbarRespons;
+
+@Component
+public class ForInnsendteVedleggHelper extends RegistryAwareHelper<Object> {
+
+    public static final String NAVN = "forInnsendteVedlegg";
+    public static final ForInnsendteVedleggHelper INSTANS = new ForInnsendteVedleggHelper();
+
+    @Override
+    public String getNavn() {
+        return null;
+    }
+
+    @Override
+    public Helper<Object> getHelper() {
+        return null;
+    }
+
+    @Override
+    public String getBeskrivelse() {
+        return null;
+    }
+
+    @Override
+    public CharSequence apply(Object o, Options options) throws IOException {
+        WebSoknad soknad = finnWebSoknad(options.context);
+        List<Vedlegg> vedlegg = soknad.getInnsendteVedlegg();
+        if (vedlegg.isEmpty()) {
+            return options.inverse(this);
+        } else {
+            return lagItererbarRespons(options, vedlegg);
+        }
+    }
+}
