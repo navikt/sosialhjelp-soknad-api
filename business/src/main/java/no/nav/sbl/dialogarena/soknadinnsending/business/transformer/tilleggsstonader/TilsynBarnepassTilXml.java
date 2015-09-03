d7d7f5bb145dc83acc8222e16feb646ceb7cd197
Ingvild Indrebø <ingvild.indrebo@nav.no>
2015-09-03 10:28:47 +0200

    [PK-22494] Tilpass ny barnefaktumstruktur

--- business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/transformer/tilleggsstonader/TilsynBarnepassTilXml.java
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/transformer/tilleggsstonader/TilsynBarnepassTilXml.java
@@ -2,24 +2,26 @@ package no.nav.sbl.dialogarena.soknadinnsending.business.transformer.tilleggssto
 
 import no.nav.melding.virksomhet.soeknadsskjema.v1.soeknadsskjema.Barn;
 import no.nav.melding.virksomhet.soeknadsskjema.v1.soeknadsskjema.Periode;
-import no.nav.melding.virksomhet.soeknadsskjema.v1.soeknadsskjema.Tilsynskategorier;
 import no.nav.melding.virksomhet.soeknadsskjema.v1.soeknadsskjema.TilsynsutgifterBarn;
 import no.nav.sbl.dialogarena.soknadinnsending.business.domain.Faktum;
 import no.nav.sbl.dialogarena.soknadinnsending.business.domain.WebSoknad;
 import org.apache.commons.collections15.Transformer;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
 
+import static no.nav.modig.lang.collections.IterUtils.on;
+import static no.nav.sbl.dialogarena.soknadinnsending.business.domain.FaktumPredicates.harValue;
 import static no.nav.sbl.dialogarena.soknadinnsending.business.transformer.tilleggsstonader.StofoTransformers.extractValue;
 
 public class TilsynBarnepassTilXml implements Transformer<WebSoknad, TilsynsutgifterBarn> {
-    private  static final String PERIODE = "barnepass.periode";
-    private  static final String UTBETALINGSDATO = "barnepass.utbetalingsdato";
-    private  static final String BARN = "barn";
-    private  static final String ANDREFORELDER = "andreforelder";
-    private  static final String SOKERBARNEPASS = "sokerOmBarnepass";
-    private  TilsynsutgifterBarn barnepass = new TilsynsutgifterBarn();
+    private static final String PERIODE = "barnepass.periode";
+    private static final String UTBETALINGSDATO = "barnepass.utbetalingsdato";
+    private static final String BARN = "barn";
+    private static final String ANDREFORELDER = "andreforelder";
+    private static final String SOKERBARNEPASS = "barnepass.sokerbarnepass";
+    private TilsynsutgifterBarn barnepass = new TilsynsutgifterBarn();
 
     @Override
     public TilsynsutgifterBarn transform(WebSoknad soknad) {
@@ -33,20 +35,27 @@ public class TilsynBarnepassTilXml implements Transformer<WebSoknad, Tilsynsutgi
     private void utbetalingsdato(WebSoknad soknad) {
         Faktum utbetalingsdatoFaktum = soknad.getFaktumMedKey(UTBETALINGSDATO);
 
-        if(utbetalingsdatoFaktum != null && utbetalingsdatoFaktum.hasValue()) {
+        if (utbetalingsdatoFaktum != null && utbetalingsdatoFaktum.hasValue()) {
             barnepass.setOensketUtbetalingsdag(new BigInteger(utbetalingsdatoFaktum.getValue()));
         }
     }
 
     private void barnSomDetSokesBarnepassOm(WebSoknad soknad) {
-        List<Faktum> alleBarn = soknad.getFaktaMedKey(BARN);
+        List<Faktum> sokerBarnepassBarn = on(soknad.getFaktaMedKey(SOKERBARNEPASS)).filter(harValue("true")).collect();
 
-        for (Faktum barn : alleBarn) {
-            if("true".equals(barn.getProperties().get(SOKERBARNEPASS))) {
-                barnepass.getBarn().add(extractValue(barn, Barn.class));
-                barnepass.setAnnenForsoergerperson(extractValue(barn, String.class, ANDREFORELDER));//TODO - er ikke støtte for å sende inn per barn
-                barnepass.setTilsynskategori(extractValue(barn, Tilsynskategorier.class));  //TODO - er ikke støtte for å sende inn per barn
-            }
+        List<Faktum> barnDetSokerBarnepassFor = new ArrayList<>();
+
+        for (Faktum barnepass : sokerBarnepassBarn) {
+            barnDetSokerBarnepassFor.addAll(soknad.getFaktaMedKeyOgParentFaktum(BARN, barnepass.getParrentFaktum()));
         }
+
+        for (Faktum barn : barnDetSokerBarnepassFor) {
+            barnepass.getBarn().add(extractValue(barn, Barn.class));
+        }
+//            if("true".equals(barn)) {
+//
+//                barnepass.setAnnenForsoergerperson(extractValue(barn, String.class, ANDREFORELDER));//TODO - er ikke støtte for å sende inn per barn
+//                barnepass.setTilsynskategori(extractValue(barn, Tilsynskategorier.class));  //TODO - er ikke støtte for å sende inn per barn
+//            }
     }
 }
