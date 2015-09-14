59830786a8c9094d5e300f23c74d4cbeb851406c
Ingvild Indrebø <ingvild.indrebo@nav.no>
2015-09-14 13:18:37 +0200

    [PK-25769] QA

--- business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/domain/Steg.java
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/domain/Steg.java
@@ -1,10 +1,10 @@
 package no.nav.sbl.dialogarena.soknadinnsending.business.domain;
 
 public class Steg {
-    public static Steg VEILEDNING = new Steg("informasjon", "veiledning");
-    public static Steg SOKNAD = new Steg("soknad", "skjema");
-    public static Steg VEDLEGG = new Steg("vedlegg", "vedlegg");
-    public static Steg OPPSUMMERING = new Steg("oppsummering", "sendInn");
+    public final static Steg VEILEDNING = new Steg("informasjon", "veiledning");
+    public final static Steg SOKNAD = new Steg("soknad", "skjema");
+    public final static Steg VEDLEGG = new Steg("vedlegg", "vedlegg");
+    public final static Steg OPPSUMMERING = new Steg("oppsummering", "sendInn");
 
     public final String url;
     public final String cmstekst;
