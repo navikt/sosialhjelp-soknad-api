faa81843952754cc4b3b1f52d7d62224c22ab4ee
Ingvild Indrebø <ingvild.indrebo@nav.no>
2015-09-16 11:49:50 +0200

    [PK-21802] Eksponer ServiceUtilsfunksjoner så de kan gjenbrukes

--- business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/service/ServiceUtils.java
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/service/ServiceUtils.java
@@ -1,13 +1,21 @@
 package no.nav.sbl.dialogarena.soknadinnsending.business.service;
 
+import com.sun.org.apache.xerces.internal.jaxp.datatype.XMLGregorianCalendarImpl;
+import org.joda.time.DateTime;
 import org.joda.time.LocalDate;
 
+import javax.xml.datatype.XMLGregorianCalendar;
+
 public class ServiceUtils {
-    static String datoTilString(LocalDate date) {
+    public static String datoTilString(LocalDate date) {
         return date != null ? date.toString("yyyy-MM-dd") : "";
     }
 
-    static String nullToBlank(Object value) {
+    public static XMLGregorianCalendar stringTilXmldato(String dato) {
+        return new XMLGregorianCalendarImpl(DateTime.parse(dato).toGregorianCalendar());
+    }
+
+    public static String nullToBlank(Object value) {
         if (value != null) {
             return value.toString();
         }
