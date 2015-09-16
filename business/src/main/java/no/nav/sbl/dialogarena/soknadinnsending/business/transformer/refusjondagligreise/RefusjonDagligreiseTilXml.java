10371bd72b029113e2d90796f667f2e89e8b7aae
Ingvild Indrebø <ingvild.indrebo@nav.no>
2015-09-16 11:52:06 +0200

    [PK-21802] Xmltransformering av refusjon

--- business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/transformer/refusjondagligreise/RefusjonDagligreiseTilXml.java
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/transformer/refusjondagligreise/RefusjonDagligreiseTilXml.java
@@ -1,42 +1,86 @@
 package no.nav.sbl.dialogarena.soknadinnsending.business.transformer.refusjondagligreise;
 
 import meldingsmodell.no.nav.melding.virksomhet.paaloepteutgifter.v1.paaloepteutgifter.PaaloepteUtgifter;
+import meldingsmodell.no.nav.melding.virksomhet.paaloepteutgifter.v1.paaloepteutgifter.Utgiftsdager;
 import meldingsmodell.no.nav.melding.virksomhet.paaloepteutgifter.v1.paaloepteutgifter.Utgiftsperioder;
 import no.nav.sbl.dialogarena.soknadinnsending.business.domain.AlternativRepresentasjon;
 import no.nav.sbl.dialogarena.soknadinnsending.business.domain.Faktum;
 import no.nav.sbl.dialogarena.soknadinnsending.business.domain.WebSoknad;
 import org.apache.commons.collections15.Transformer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.joda.time.LocalDate;
 
 import javax.xml.bind.JAXB;
 import java.io.ByteArrayOutputStream;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import static no.nav.modig.lang.collections.IterUtils.on;
 import static no.nav.sbl.dialogarena.soknadinnsending.business.domain.FaktumPredicates.harPropertyMedValue;
+import static no.nav.sbl.dialogarena.soknadinnsending.business.service.ServiceUtils.datoTilString;
+import static no.nav.sbl.dialogarena.soknadinnsending.business.service.ServiceUtils.stringTilXmldato;
 
 public class RefusjonDagligreiseTilXml implements Transformer<WebSoknad, AlternativRepresentasjon>
 {
-    private static final Logger LOG = LoggerFactory.getLogger(RefusjonDagligreiseTilXml.class);
+    private static final class FAKTUM_TIL_UTGIFTSPERIODER implements Transformer<Faktum, Utgiftsperioder> {
+        private Boolean trengerParkering;
 
-    private static final Transformer<Faktum, Utgiftsperioder> FAKTUM_TIL_UTGIFTSPERIODER = new Transformer<Faktum, Utgiftsperioder>() {
+        public FAKTUM_TIL_UTGIFTSPERIODER(Boolean trengerParkering) {
+            this.trengerParkering = trengerParkering;
+        }
+
         @Override
         public Utgiftsperioder transform(Faktum faktum) {
             Utgiftsperioder utgiftsperioder = new Utgiftsperioder();
             utgiftsperioder.setBetalingsplanId(faktum.getProperties().get("id"));
-            return null;
-        }
-    };
 
-    private static PaaloepteUtgifter refusjonDagligreise(WebSoknad webSoknad) {
+            int totaltParkeringbeløp = 0;
+            int totaltAntallDager = 0;
+
+            LocalDate fom = new LocalDate(faktum.getProperties().get("fom"));
+            LocalDate tom = new LocalDate(faktum.getProperties().get("tom"));
+
+            List<Utgiftsdager> utgiftsdager = new ArrayList<>();
+
+            for(LocalDate date = fom; date.isBefore(tom.plusDays(1)) ; date = date.plusDays(1)) {
+                String datoString = datoTilString(date);
+                if(sokerForDag(datoString, faktum)) {
+                    totaltAntallDager++;
+                    String parkeringsUtgift = faktum.getProperties().get(datoString + ".parkering");
+                    Utgiftsdager utgiftsdag = new Utgiftsdager();
+                    utgiftsdag.setUtgiftsdag(stringTilXmldato(datoString));
+
+                    if(trengerParkering && parkeringsUtgift != null) {
+                        int utgift = Integer.parseInt(parkeringsUtgift);
+                        totaltParkeringbeløp += utgift;
+                        utgiftsdag.setParkeringsutgift(BigInteger.valueOf(utgift));
+                    }
+                    utgiftsdager.add(utgiftsdag);
+                }
+            }
+
+            utgiftsperioder.setTotaltParkeringsbeloep(BigInteger.valueOf(totaltParkeringbeløp));
+            utgiftsperioder.setTotaltAntallDagerKjoert(BigInteger.valueOf(totaltAntallDager));
+            utgiftsperioder.getUtgiftsdagerMedParkering().addAll(utgiftsdager);
+            return utgiftsperioder;
+        }
+
+        private boolean sokerForDag(String dato, Faktum betalingsplan) {
+            return "true".equals(betalingsplan.getProperties().get(dato + ".soker"));
+        }
+    }
+    public static PaaloepteUtgifter refusjonDagligreise(WebSoknad webSoknad) {
         PaaloepteUtgifter skjema = new PaaloepteUtgifter();
         Faktum vedtak = webSoknad.getFaktumMedKey("vedtak");
+        FAKTUM_TIL_UTGIFTSPERIODER faktumTransformer = new FAKTUM_TIL_UTGIFTSPERIODER("true".equals(vedtak.getProperties().get("trengerParkering")));
 
         skjema.setVedtaksId(vedtak.getProperties().get("id"));
-        skjema.getUtgiftsperioder().addAll(on(webSoknad.getFaktaMedKey("vedtak.betalingsplan")).filter(harPropertyMedValue("registrert", "true")).map(FAKTUM_TIL_UTGIFTSPERIODER).collect());
+        skjema.getUtgiftsperioder().addAll(on(webSoknad.getFaktaMedKey("vedtak.betalingsplan"))
+                .filter(harPropertyMedValue("registrert", "true"))
+                .map(faktumTransformer).collect());
 
-        return null;
+        return skjema;
     }
 
     @Override
---
+++ business/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/business/transformer/refusjondagligreise/RefusjonDagligreiseTilXmlTest.java
@@ -0,0 +1,98 @@
+package no.nav.sbl.dialogarena.soknadinnsending.business.transformer.refusjondagligreise;
+
+import meldingsmodell.no.nav.melding.virksomhet.paaloepteutgifter.v1.paaloepteutgifter.PaaloepteUtgifter;
+import meldingsmodell.no.nav.melding.virksomhet.paaloepteutgifter.v1.paaloepteutgifter.Utgiftsperioder;
+import no.nav.sbl.dialogarena.soknadinnsending.business.domain.AlternativRepresentasjon;
+import no.nav.sbl.dialogarena.soknadinnsending.business.domain.Faktum;
+import no.nav.sbl.dialogarena.soknadinnsending.business.domain.WebSoknad;
+import org.joda.time.LocalDate;
+import org.junit.Test;
+
+import java.math.BigInteger;
+
+import static no.nav.sbl.dialogarena.soknadinnsending.business.service.ServiceUtils.datoTilString;
+import static no.nav.sbl.dialogarena.soknadinnsending.business.service.ServiceUtils.stringTilXmldato;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RefusjonDagligreiseTilXmlTest {
+
+
+    @Test
+    public void skalKonvertereTilAlternativRepresentasjon() throws Exception {
+        Faktum vedtak = settOppVedtak();
+        Faktum betalingsplan = new Faktum()
+                .medKey("vedtak.betalingsplan")
+                .medProperty("registrert", "false");
+        WebSoknad soknad = new WebSoknad().medFaktum(vedtak).medFaktum(betalingsplan);
+
+        AlternativRepresentasjon representasjon = new RefusjonDagligreiseTilXml().transform(soknad);
+
+        assertThat(representasjon.getMimetype()).isEqualTo("application/xml");
+        assertThat(representasjon.getFilnavn()).isEqualTo("RefusjonDagligreise.xml");
+        assertThat(representasjon.getContent()).isNotNull();
+    }
+
+    @Test
+    public void skalLeggeTilDag() throws Exception {
+        Boolean trengerParkering = false;
+        Faktum vedtak = settOppVedtak().medProperty("trengerParkering", trengerParkering.toString());
+        Faktum betalingsplan = lagBetalingsplan("2015-01-01", "2015-01-01", trengerParkering, "1");
+        WebSoknad soknad = new WebSoknad().medFaktum(vedtak).medFaktum(betalingsplan);
+
+        PaaloepteUtgifter paaloepteUtgifter = RefusjonDagligreiseTilXml.refusjonDagligreise(soknad);
+
+        assertThat(paaloepteUtgifter.getVedtaksId()).isEqualTo("1234");
+        assertThat(paaloepteUtgifter.getUtgiftsperioder().size()).isEqualTo(1);
+
+        Utgiftsperioder utgiftsperiode = paaloepteUtgifter.getUtgiftsperioder().get(0);
+        assertThat(utgiftsperiode.getBetalingsplanId()).isEqualTo("1");
+        assertThat(utgiftsperiode.getTotaltAntallDagerKjoert()).isEqualTo(BigInteger.valueOf(1));
+        assertThat(utgiftsperiode.getUtgiftsdagerMedParkering().size()).isEqualTo(1);
+        assertThat(utgiftsperiode.getUtgiftsdagerMedParkering().get(0).getUtgiftsdag()).isEqualTo(stringTilXmldato("2015-01-01"));
+    }
+
+    @Test
+    public void skalRegneUtParkeringsutgifterOgAntallDagerKjort() throws Exception {
+        Boolean trengerParkering = true;
+        Faktum vedtak = settOppVedtak().medProperty("trengerParkering", trengerParkering.toString());
+        Faktum betalingsplan = lagBetalingsplan("2015-01-01", "2015-01-03", trengerParkering, "1");
+        betalingsplan.medProperty("2015-01-02.soker", "false");
+
+        WebSoknad soknad = new WebSoknad().medFaktum(vedtak).medFaktum(betalingsplan);
+        PaaloepteUtgifter paaloepteUtgifter = RefusjonDagligreiseTilXml.refusjonDagligreise(soknad);
+        Utgiftsperioder utgiftsperiode = paaloepteUtgifter.getUtgiftsperioder().get(0);
+
+        assertThat(utgiftsperiode.getTotaltAntallDagerKjoert()).isEqualTo(BigInteger.valueOf(2L));
+        assertThat(utgiftsperiode.getTotaltParkeringsbeloep()).isEqualTo(BigInteger.valueOf(200L));
+    }
+
+    private Faktum lagBetalingsplan(String fom, String tom, Boolean trengerParkering, String id) {
+        Faktum betalingsplan = new Faktum().medKey("vedtak.betalingsplan")
+                .medProperty("registrert", "true")
+                .medProperty("id", id)
+                .medProperty("fom", fom)
+                .medProperty("tom", tom);
+
+        leggTilDager(betalingsplan, fom, tom, trengerParkering);
+        return betalingsplan;
+    }
+
+    private void leggTilDager(Faktum faktum, String fom, String tom, Boolean trengerParkering) {
+        LocalDate fomDato = new LocalDate(fom);
+        LocalDate tomDato = new LocalDate(tom);
+
+        for (LocalDate date = fomDato; date.isBefore(tomDato.plusDays(1)); date = date.plusDays(1)) {
+            faktum.medProperty(datoTilString(date) + ".soker", "true");
+            if(trengerParkering) {
+                faktum.medProperty(datoTilString(date) + ".parkering", "100");
+            }
+        }
+    }
+
+    private Faktum settOppVedtak() {
+        return new Faktum()
+                .medKey("vedtak")
+                .medProperty("id", "1234")
+                .medProperty("trengerParkering", "false");
+    }
+}
