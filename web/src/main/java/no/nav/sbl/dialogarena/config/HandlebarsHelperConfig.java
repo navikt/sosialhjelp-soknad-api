c98de2341e99b639375b29bd57936185253e4b6b
Mats Krüger Svensson <mats.kruger.svensson@nav.no>
2015-09-23 15:58:49 +0200

    [HELPER] fikset appconfig og tester

--- web/src/main/java/no/nav/sbl/dialogarena/config/HandlebarsHelperConfig.java
+++ web/src/main/java/no/nav/sbl/dialogarena/config/HandlebarsHelperConfig.java
@@ -1,11 +1,11 @@
 package no.nav.sbl.dialogarena.config;
 
-import no.nav.sbl.dialogarena.service.helpers.RegistryAwareHelper;
+import no.nav.sbl.dialogarena.service.HandleBarKjoerer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackageClasses = RegistryAwareHelper.class)
+@ComponentScan(basePackageClasses = HandleBarKjoerer.class)
 public class HandlebarsHelperConfig {
 
 }
