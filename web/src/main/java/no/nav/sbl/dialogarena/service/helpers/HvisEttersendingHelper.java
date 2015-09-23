0b19333813d2dd85b238c255aeb5ffd2ad295ef1
June Henriksen <june.henriksen2@nav.no>
2015-09-23 09:04:25 +0200

    [Helpers] Flytt hvisEttersending til egen Helper-klasse

---
+++ web/src/main/java/no/nav/sbl/dialogarena/service/helpers/HvisEttersendingHelper.java
@@ -0,0 +1,33 @@
+package no.nav.sbl.dialogarena.service.helpers;
+
+import com.github.jknack.handlebars.Options;
+import no.nav.sbl.dialogarena.soknadinnsending.business.domain.WebSoknad;
+
+import java.io.IOException;
+
+import static no.nav.sbl.dialogarena.service.HandlebarsUtils.finnWebSoknad;
+
+public class HvisEttersendingHelper extends RegistryAwareHelper<Object>{
+    public static final String NAVN = "hvisEttersending";
+
+    @Override
+    public String getNavn() {
+        return NAVN;
+    }
+
+    @Override
+    public String getBeskrivelse() {
+        return "Sjekk om søknad er av typen ettersending.";
+    }
+
+    @Override
+    public CharSequence apply(Object context, Options options) throws IOException {
+        WebSoknad soknad = finnWebSoknad(options.context);
+        if (soknad.erEttersending()) {
+            return options.fn(this);
+        } else {
+            return options.inverse(this);
+        }    }
+
+
+}
