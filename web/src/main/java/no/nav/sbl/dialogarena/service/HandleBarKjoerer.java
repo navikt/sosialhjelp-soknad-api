813465101a07a41e1f22c24b630598facafd4351
June Henriksen <june.henriksen2@nav.no>
2015-09-23 09:04:25 +0200

    [Helpers] Flytt hvisEttersending til egen Helper-klasse

--- web/src/main/java/no/nav/sbl/dialogarena/service/HandleBarKjoerer.java
+++ web/src/main/java/no/nav/sbl/dialogarena/service/HandleBarKjoerer.java
@@ -64,7 +64,6 @@ public class HandleBarKjoerer implements HtmlGenerator, HandlebarRegistry {
         handlebars.registerHelper("forBarnefakta", generateForBarnefaktaHelper());
         handlebars.registerHelper("formatterFodelsDato", generateFormatterFodselsdatoHelper());
         handlebars.registerHelper("formatterLangDato", generateFormatterLangDatoHelper());
-        handlebars.registerHelper("hvisEttersending", generateHvisEttersendingHelper());
         handlebars.registerHelper("hentTekst", generateHentTekstHelper());
         handlebars.registerHelper("hentTekstMedFaktumParameter", generateHentTekstMedFaktumParameterHelper());
         handlebars.registerHelper("hentLand", generateHentLandHelper());
@@ -159,20 +158,6 @@ public class HandleBarKjoerer implements HtmlGenerator, HandlebarRegistry {
                 return String.format("KEY MANGLER: [%s]", key);
             }
         }
-    }
-
-    private Helper<Object> generateHvisEttersendingHelper() {
-        return new Helper<Object>() {
-            @Override
-            public CharSequence apply(Object o, Options options) throws IOException {
-                WebSoknad soknad = finnWebSoknad(options.context);
-                if (soknad.erEttersending()) {
-                    return options.fn(this);
-                } else {
-                    return options.inverse(this);
-                }
-            }
-        };
     }
 
     private Helper<String> generateFormatterLangDatoHelper() {
