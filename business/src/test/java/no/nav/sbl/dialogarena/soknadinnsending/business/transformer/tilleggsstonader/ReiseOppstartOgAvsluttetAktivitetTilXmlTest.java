1ab9cded32ff7f5b46af2950ea6a0d55a697c768
Mats Krüger Svensson <mats.kruger.svensson@nav.no>
2015-10-15 11:47:26 +0200

    [PK-26876] henter ned nyeste xmlting og setter antall reiser

--- business/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/business/transformer/tilleggsstonader/ReiseOppstartOgAvsluttetAktivitetTilXmlTest.java
+++ business/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/business/transformer/tilleggsstonader/ReiseOppstartOgAvsluttetAktivitetTilXmlTest.java
@@ -1,7 +1,5 @@
 package no.nav.sbl.dialogarena.soknadinnsending.business.transformer.tilleggsstonader;
 
-import no.nav.melding.virksomhet.soeknadsskjema.v1.soeknadsskjema.DrosjeTransportutgifter;
-import no.nav.melding.virksomhet.soeknadsskjema.v1.soeknadsskjema.EgenBilTransportutgifter;
 import no.nav.melding.virksomhet.soeknadsskjema.v1.soeknadsskjema.ReiseOppstartOgAvsluttetAktivitet;
 import no.nav.sbl.dialogarena.soknadinnsending.business.domain.Faktum;
 import no.nav.sbl.dialogarena.soknadinnsending.business.domain.WebSoknad;
@@ -17,50 +15,39 @@ public class ReiseOppstartOgAvsluttetAktivitetTilXmlTest {
     @Test
     public void skalKonvertereFaktumStruktur() {
         WebSoknad soknad = new WebSoknad();
-        soknad.getFakta().add(new Faktum().medKey("reise.midlertidig.periode").medProperty("fom", "2015-01-02").medProperty("tom", "2015-03-04"));
-        soknad.getFakta().add(new Faktum().medKey("reise.midlertidig.reiselengde").medValue("123"));
-        soknad.getFakta().add(new Faktum().medKey("reise.midlertidig.reisemaal").medProperty("adresse", "adresse").medProperty("postnr", "1256"));
+        soknad.medFaktum(new Faktum().medKey("reise.midlertidig.periode").medProperty("fom", "2015-01-02").medProperty("tom", "2015-03-04"));
+        soknad.medFaktum(new Faktum().medKey("reise.midlertidig.reiselengde").medValue("123"));
+        soknad.medFaktum(new Faktum().medKey("reise.midlertidig.antallreiser").medValue("77"));
+        soknad.medFaktum(new Faktum().medKey("reise.midlertidig.reisemaal").medProperty("adresse", "adresse").medProperty("postnr", "1256"));
 
-        soknad.getFakta().add(new Faktum().medKey("reise.midlertidig.hjemmeboende").medValue("true"));
-        soknad.getFakta().add(new Faktum().medKey("barn").medProperty("skalFlytteMed", "true"));
+        soknad.medFaktum(new Faktum().medKey("reise.midlertidig.hjemmeboende").medValue("true"));
+        soknad.medFaktum(new Faktum().medKey("barn").medProperty("skalFlytteMed", "true"));
 
-        soknad.getFakta().add(new Faktum().medKey("reise.midlertidig.offentligtransport.drosje").medValue("true"));
-        soknad.getFakta().add(new Faktum().medKey("reise.midlertidig.offentligtransport.drosje.belop").medValue("50"));
+        soknad.medFaktum(new Faktum().medKey("reise.midlertidig.offentligtransport.drosje").medValue("true"));
+        soknad.medFaktum(new Faktum().medKey("reise.midlertidig.offentligtransport.drosje.belop").medValue("50"));
 
-        soknad.getFakta().add(new Faktum().medKey("reise.midlertidig.offentligtransport").medValue("false"));
-        soknad.getFakta().add(new Faktum().medKey("reise.midlertidig.offentligtransport.egenbil").medValue("true"));
-        soknad.getFakta().add(new Faktum().medKey("reise.midlertidig.offentligtransport.egenbil.kostnader.bompenger").medValue("1"));
-        soknad.getFakta().add(new Faktum().medKey("reise.midlertidig.offentligtransport.egenbil.kostnader.parkering").medValue("1"));
-        soknad.getFakta().add(new Faktum().medKey("reise.midlertidig.offentligtransport.egenbil.kostnader.piggdekk").medValue("2"));
-        soknad.getFakta().add(new Faktum().medKey("reise.midlertidig.offentligtransport.egenbil.kostnader.ferge").medValue("3"));
-        soknad.getFakta().add(new Faktum().medKey("reise.midlertidig.offentligtransport.egenbil.kostnader.annet").medValue("4"));
+        soknad.medFaktum(new Faktum().medKey("reise.midlertidig.offentligtransport").medValue("false"));
+        soknad.medFaktum(new Faktum().medKey("reise.midlertidig.offentligtransport.egenbil").medValue("true"));
+        soknad.medFaktum(new Faktum().medKey("reise.midlertidig.offentligtransport.egenbil.kostnader.bompenger").medValue("1"));
+        soknad.medFaktum(new Faktum().medKey("reise.midlertidig.offentligtransport.egenbil.kostnader.parkering").medValue("1"));
+        soknad.medFaktum(new Faktum().medKey("reise.midlertidig.offentligtransport.egenbil.kostnader.piggdekk").medValue("2"));
+        soknad.medFaktum(new Faktum().medKey("reise.midlertidig.offentligtransport.egenbil.kostnader.ferge").medValue("3"));
+        soknad.medFaktum(new Faktum().medKey("reise.midlertidig.offentligtransport.egenbil.kostnader.annet").medValue("4"));
 
-        soknad.getFakta().add(new Faktum().medKey("reise.midlertidig.offentligtransport.egenbil.parkering.belop").medValue("30"));
+        soknad.medFaktum(new Faktum().medKey("reise.midlertidig.offentligtransport.egenbil.parkering.belop").medValue("30"));
 
         ReiseOppstartOgAvsluttetAktivitet result = new ReiseOppstartOgAvsluttetAktivitetTilXml().transform(soknad);
 
         assertThat(result.getAktivitetsstedAdresse()).isEqualTo("adresse, 1256");
         assertThat(result.getAvstand()).isEqualTo(new BigInteger("123"));
+        assertThat(result.getAntallReiser()).isEqualTo(new BigInteger("77"));
         assertThat(result.isHarBarnUnderAtten()).isEqualTo(true);
         assertThat(result.getPeriode().getFom()).is(new PeriodMatcher(2015, 1, 2));
         assertThat(result.getPeriode().getTom()).is(new PeriodMatcher(2015, 3, 4));
         assertThat(result.isHarBarnUnderFemteklasse()).isEqualTo(true);
 
-
-        assertThat(result.getAlternativeTransportutgifter().getDrosjeTransportutgifter()).is(new Condition<DrosjeTransportutgifter>() {
-            @Override
-            public boolean matches(DrosjeTransportutgifter value) {
-                return value.getBeloep().equals(new BigInteger("50"));
-            }
-        });
-
-        assertThat(result.getAlternativeTransportutgifter().getEgenBilTransportutgifter()).is(new Condition<EgenBilTransportutgifter>() {
-            @Override
-            public boolean matches(EgenBilTransportutgifter value) {
-                return value.getSumAndreUtgifter().equals(11d);
-            }
-        });
-
+        assertThat(result.getAlternativeTransportutgifter().getDrosjeTransportutgifter().getBeloep()).isEqualTo(new BigInteger("50"));
+        assertThat(result.getAlternativeTransportutgifter().getEgenBilTransportutgifter().getSumAndreUtgifter()).isEqualTo(11d);
         assertThat(result.getAlternativeTransportutgifter().isKanEgenBilBrukes()).isEqualTo(true);
         assertThat(result.getAlternativeTransportutgifter().isKanOffentligTransportBrukes()).isEqualTo(false);
 
