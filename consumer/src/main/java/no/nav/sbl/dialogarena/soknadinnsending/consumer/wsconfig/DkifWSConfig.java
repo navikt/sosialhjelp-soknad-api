4c3c711aefedc1f7cc2ee2d7f8d3f993082cf4c2
Ingrid Guren <ingrid.guren@nav.no>
2015-11-16 12:50:49 +0100

    PKULV-1033: Laget dkifmock

--- consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/wsconfig/DkifWSConfig.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/wsconfig/DkifWSConfig.java
@@ -1,27 +1,32 @@
 package no.nav.sbl.dialogarena.soknadinnsending.consumer.wsconfig;
 
+import no.nav.sbl.dialogarena.sendsoknad.mockmodul.dkif.DkifMock;
 import no.nav.sbl.dialogarena.soknadinnsending.consumer.ServiceBuilder;
 import no.nav.sbl.dialogarena.types.Pingable;
-import no.nav.tjeneste.virksomhet.digitalkontaktinformasjon.v1.DigitalKontaktinformasjonV1;
+import no.nav.tjeneste.virksomhet.digitalkontaktinformasjon.v1.DigitalKontaktinformasjon_v1PortType;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import static no.nav.sbl.dialogarena.soknadinnsending.consumer.util.InstanceSwitcher.createSwitcher;
+
 @Configuration
 public class DkifWSConfig {
+
+    public static final String DKIF_KEY = "start.dkif.withmock";
 
     @Value("${dkif.webservice.url}")
     private String dkifEndpoint;
 
-    private static final String DKIF_WSDL= "classpath:dkif/dkif/no/nav/tjeneste/virksomhet/digitalKontaktinformasjon/v1/DigitalKontaktinformasjon.wsdl";
-
     @Bean
-    public DigitalKontaktinformasjonV1 dkifService() {
-        return factory().withMDC().withUserSecurity().get();
+    public DigitalKontaktinformasjon_v1PortType dkifService() {
+        DigitalKontaktinformasjon_v1PortType mock = new DkifMock().dkifMock();
+        DigitalKontaktinformasjon_v1PortType prod = factory().withMDC().withUserSecurity().get();
+        return createSwitcher(prod, mock, DKIF_KEY, DigitalKontaktinformasjon_v1PortType.class);
     }
 
     @Bean
-    public DigitalKontaktinformasjonV1 dkifServiceSelftest() {
+    public DigitalKontaktinformasjon_v1PortType dkifServiceSelftest() {
         return factory().withSystemSecurity().get();
     }
 
@@ -40,11 +45,10 @@ public class DkifWSConfig {
         };
     }
 
-    private ServiceBuilder<DigitalKontaktinformasjonV1>.PortTypeBuilder<DigitalKontaktinformasjonV1> factory() {
-        return new ServiceBuilder<>(DigitalKontaktinformasjonV1.class)
+    private ServiceBuilder<DigitalKontaktinformasjon_v1PortType>.PortTypeBuilder<DigitalKontaktinformasjon_v1PortType> factory() {
+        return new ServiceBuilder<>(DigitalKontaktinformasjon_v1PortType.class)
                 .asStandardService()
                 .withAddress(dkifEndpoint)
-                .withWsdl("classpath:dkif/dkif/no/nav/tjeneste/virksomhet/digitalKontaktinformasjon/v1/DigitalKontaktinformasjon.wsdl")
                 .build()
                 .withHttpsMock()
                 .withMDC();
