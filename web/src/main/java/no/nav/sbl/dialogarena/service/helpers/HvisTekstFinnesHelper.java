8e5d7594aeaba12490bae98e37156ddbdfcf9d6f
Eirik L. Wang <eirik.wang@nav.no>
2015-12-01 15:18:17 +0100

    PK-27434: Fiks etter merge

--- web/src/main/java/no/nav/sbl/dialogarena/service/helpers/HvisTekstFinnesHelper.java
+++ web/src/main/java/no/nav/sbl/dialogarena/service/helpers/HvisTekstFinnesHelper.java
@@ -3,10 +3,14 @@ package no.nav.sbl.dialogarena.service.helpers;
 import com.github.jknack.handlebars.Options;
 import no.nav.sbl.dialogarena.service.CmsTekst;
 import no.nav.sbl.dialogarena.service.HandlebarsUtils;
+import no.nav.sbl.dialogarena.soknadinnsending.business.domain.Faktum;
+import no.nav.sbl.dialogarena.soknadinnsending.business.domain.WebSoknad;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
 import java.io.IOException;
+
+import static org.apache.commons.lang3.LocaleUtils.toLocale;
 
 @Component
 public class HvisTekstFinnesHelper extends RegistryAwareHelper<String> {
@@ -26,8 +30,12 @@ public class HvisTekstFinnesHelper extends RegistryAwareHelper<String> {
 
     @Override
     public CharSequence apply(String key, Options options) throws IOException {
-        String soknadPrefix = HandlebarsUtils.finnWebSoknad(options.context).getSoknadPrefix();
-        if(cmsTekst.finnesTekst(key, soknadPrefix)){
+        WebSoknad soknad = HandlebarsUtils.finnWebSoknad(options.context);
+        String soknadPrefix = soknad.getSoknadPrefix();
+        Faktum sprakFaktum = soknad.getFaktumMedKey("skjema.sprak");
+        String sprak = sprakFaktum == null ? "nb_NO" : sprakFaktum.getValue();
+
+        if(cmsTekst.finnesTekst(key, soknadPrefix, toLocale(sprak))){
             return options.fn();
         } else {
             return options.inverse();
