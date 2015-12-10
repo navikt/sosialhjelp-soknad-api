3b560763dfbad6ae4cb4a07920ee2216be034a00
Mats Krüger Svensson <mats.kruger.svensson@nav.no>
2015-12-10 14:45:02 +0100

    PK-27434: xsd, fikser slik at alle xmlene validerer. Fjerner ugyldige elementer og skjemanummer, riktig rekkefølge, riktig xmlns. Foreldresoknad hadde enkelte plasser doble skjemanummertillegg

--- business/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/business/domain/oppsett/SoknadXmlValiderer.java
+++ business/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/business/domain/oppsett/SoknadXmlValiderer.java
@@ -1,8 +1,6 @@
 package no.nav.sbl.dialogarena.soknadinnsending.business.domain.oppsett;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.xml.sax.SAXException;
 
 import javax.xml.XMLConstants;
 import javax.xml.transform.stream.StreamSource;
@@ -10,30 +8,56 @@ import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 import java.io.File;
-import java.io.IOException;
 
 public class SoknadXmlValiderer {
 
     private Validator validator;
 
-    @Before
-    public void setup() throws SAXException {
-        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-        Schema schema = schemaFactory.newSchema(new File("src/main/resources/soknader/soknadstruktur.xsd"));
-        validator = schema.newValidator();
-    }
-
     @Test
-    public void testDagpengerXml() throws IOException, SAXException {
+    public void testDagpengerXml() throws Exception {
         test("dagpenger_ordinaer.xml");
     }
 
     @Test
-    public void testForeldrepengerXml() throws IOException, SAXException {
+    public void testDagpengerGjenopptakXml() throws Exception {
+        test("soknadtilleggsstonader.xml");
+    }
+
+    @Test
+    public void testForeldrepengerXml() throws Exception {
         test("foreldresoknad.xml");
     }
 
-    private void test(String xmlFilNavn) throws IOException, SAXException {
+    @Test
+    public void testTilleggStonaderXml() throws Exception {
+        test("soknadtilleggsstonader.xml");
+    }
+
+    @Test
+    public void testRefusjonXml() throws Exception {
+        test("refusjondagligreise.xml");
+    }
+
+
+    @Test
+    public void testTiltakspengerXml() throws Exception {
+        test("tiltakspenger.xml");
+    }
+
+    @Test
+    public void testBilstonadXml() throws Exception {
+        test("bilstonad.xml");
+    }
+
+    @Test
+    public void testAapXml() throws Exception {
+        test("aap_ordinaer.xml");
+    }
+
+    private void test(String xmlFilNavn) throws Exception {
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        Schema schema = schemaFactory.newSchema(new File("src/main/resources/soknader/soknadstruktur.xsd"));
+        validator = schema.newValidator();
         String path = "src/main/resources/soknader/";
         validator.validate(new StreamSource(path + xmlFilNavn));
     }
