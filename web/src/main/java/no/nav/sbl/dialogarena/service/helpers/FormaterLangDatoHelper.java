00a3ef5e1ad746579c16396c7cf5329400c4f0aa
June Henriksen <june.henriksen2@nav.no>
2016-01-08 14:30:19 +0100

    [HPALM-1104] Støtt engelsk på lange datoer i oppsummeringen
    Bruk engelske navn på måneder når valgt språk er engelsk.
    Datoformatteringen er norsk, dvs: 31. january 2015

--- web/src/main/java/no/nav/sbl/dialogarena/service/helpers/FormaterLangDatoHelper.java
+++ web/src/main/java/no/nav/sbl/dialogarena/service/helpers/FormaterLangDatoHelper.java
@@ -1,6 +1,9 @@
 package no.nav.sbl.dialogarena.service.helpers;
 
 import com.github.jknack.handlebars.Options;
+import no.nav.sbl.dialogarena.service.HandlebarsUtils;
+import no.nav.sbl.dialogarena.soknadinnsending.business.domain.Faktum;
+import no.nav.sbl.dialogarena.soknadinnsending.business.domain.WebSoknad;
 import org.apache.commons.lang3.StringUtils;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
@@ -10,6 +13,7 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 import static no.nav.sbl.dialogarena.service.HandlebarsUtils.NO_LOCALE;
+import static org.apache.commons.lang3.LocaleUtils.toLocale;
 
 @Component
 public class FormaterLangDatoHelper extends RegistryAwareHelper<String> {
@@ -30,6 +34,10 @@ public class FormaterLangDatoHelper extends RegistryAwareHelper<String> {
     @Override
     public CharSequence apply(String dato, Options options) throws IOException {
         if (StringUtils.isNotEmpty(dato)) {
+            WebSoknad soknad = HandlebarsUtils.finnWebSoknad(options.context);
+            Faktum sprakFaktum = soknad.getFaktumMedKey("skjema.sprak");
+            String sprak = sprakFaktum == null ? "nb_NO" : sprakFaktum.getValue();
+            langDatoformat = DateTimeFormat.forPattern("d. MMMM yyyy").withLocale(toLocale(sprak));
             return langDatoformat.print(DateTime.parse(dato));
         }
         return "";
