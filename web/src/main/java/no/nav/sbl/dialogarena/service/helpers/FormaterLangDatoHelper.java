777469475271a855e8d8f88b969d4eb5f8bf3097
June Henriksen <june.henriksen2@nav.no>
2016-01-11 15:18:14 +0100

    [HPALM-1104] Bruk getter på soknad for språk

--- web/src/main/java/no/nav/sbl/dialogarena/service/helpers/FormaterLangDatoHelper.java
+++ web/src/main/java/no/nav/sbl/dialogarena/service/helpers/FormaterLangDatoHelper.java
@@ -2,7 +2,6 @@ package no.nav.sbl.dialogarena.service.helpers;
 
 import com.github.jknack.handlebars.Options;
 import no.nav.sbl.dialogarena.service.HandlebarsUtils;
-import no.nav.sbl.dialogarena.soknadinnsending.business.domain.Faktum;
 import no.nav.sbl.dialogarena.soknadinnsending.business.domain.WebSoknad;
 import org.apache.commons.lang3.StringUtils;
 import org.joda.time.DateTime;
@@ -13,7 +12,6 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 import static no.nav.sbl.dialogarena.service.HandlebarsUtils.NO_LOCALE;
-import static org.apache.commons.lang3.LocaleUtils.toLocale;
 
 @Component
 public class FormaterLangDatoHelper extends RegistryAwareHelper<String> {
@@ -35,9 +33,7 @@ public class FormaterLangDatoHelper extends RegistryAwareHelper<String> {
     public CharSequence apply(String dato, Options options) throws IOException {
         if (StringUtils.isNotEmpty(dato)) {
             WebSoknad soknad = HandlebarsUtils.finnWebSoknad(options.context);
-            Faktum sprakFaktum = soknad.getFaktumMedKey("skjema.sprak");
-            String sprak = sprakFaktum == null ? "nb_NO" : sprakFaktum.getValue();
-            langDatoformat = DateTimeFormat.forPattern("d. MMMM yyyy").withLocale(toLocale(sprak));
+            langDatoformat = DateTimeFormat.forPattern("d. MMMM yyyy").withLocale(soknad.getSprak());
             return langDatoformat.print(DateTime.parse(dato));
         }
         return "";
