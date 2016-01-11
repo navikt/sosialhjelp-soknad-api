69fd3939536cf558337bdb95fcb639113acb23db
Hageland, Steffen <steffen.hageland@nav.no>
2016-01-11 14:40:04 +0100

    [PK-27642] fikser tester for ArbeidssokerInfoService

--- consumer/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/arbeid/ArbeidssokerInfoServiceTest.java
+++ consumer/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/arbeid/ArbeidssokerInfoServiceTest.java
@@ -1,11 +1,9 @@
 package no.nav.sbl.dialogarena.soknadinnsending.consumer.arbeid;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.ProtocolVersion;
-import org.apache.http.client.HttpClient;
+import org.apache.http.*;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.entity.BasicHttpEntity;
-import org.apache.http.message.BasicHttpResponse;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.message.BasicStatusLine;
 import org.junit.Before;
 import org.junit.Test;
@@ -20,6 +18,7 @@ import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -28,7 +27,7 @@ public class ArbeidssokerInfoServiceTest {
     ArbeidssokerInfoService arbeidssokerInfoService;
 
     @Mock
-    private HttpClient httpclient;
+    private CloseableHttpClient httpclient;
 
     @Before
     public void setup(){
@@ -37,13 +36,15 @@ public class ArbeidssokerInfoServiceTest {
 
     @Test
     public void getArbeidssokerArenaStatusSkalReturnereBrukerStatus() throws Exception {
-        when(httpclient.execute(any(HttpUriRequest.class))).thenReturn(buildResponse("{\"arenaStatusKode\": \"PARBS\"}"));
+        CloseableHttpResponse response = buildResponse("{\"arenaStatusKode\": \"PARBS\"}");
+        when(httpclient.execute(any(HttpUriRequest.class))).thenReturn(response);
         assertThat(arbeidssokerInfoService.getArbeidssokerArenaStatus("11111111111")).isEqualTo("PARBS");
     }
 
     @Test
     public void getArbeidssokerStatusSkalReturnereUkjentForTomRespons() throws Exception {
-        when(httpclient.execute(any(HttpUriRequest.class))).thenReturn(buildResponse("{}"));
+        CloseableHttpResponse response = buildResponse("{}");
+        when(httpclient.execute(any(HttpUriRequest.class))).thenReturn(response);
         assertThat(arbeidssokerInfoService.getArbeidssokerArenaStatus("11111111111")).isEqualTo("UKJENT");
     }
 
@@ -53,12 +54,13 @@ public class ArbeidssokerInfoServiceTest {
         assertThat(arbeidssokerInfoService.getArbeidssokerArenaStatus("11111111111")).isEqualTo("UKJENT");
     }
 
-    public HttpResponse buildResponse(String content) {
-        HttpResponse response = new BasicHttpResponse(new BasicStatusLine(new ProtocolVersion("HTTP", 1, 1), 200, "OK"));
-        BasicHttpEntity body = new BasicHttpEntity();
-        body.setContent(new ByteArrayInputStream(content.getBytes()));
-        body.setContentLength(content.length());
-        response.setEntity(body);
+    public CloseableHttpResponse buildResponse(String content) throws IOException {
+        HttpEntity body = mock(HttpEntity.class);
+        when(body.getContent()).thenReturn(new ByteArrayInputStream(content.getBytes()));
+
+        CloseableHttpResponse response = mock(CloseableHttpResponse.class);
+        when(response.getStatusLine()).thenReturn(new BasicStatusLine(HttpVersion.HTTP_1_1, HttpStatus.SC_OK, "OK"));
+        when(response.getEntity()).thenReturn(body);
         return response;
     }
 }
