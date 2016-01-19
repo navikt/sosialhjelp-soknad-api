295931b747cd8188da251b25b4fa1672ef3ffdeb
Jarle Svendsrud <jarle.svendsrud@nav.no>
2016-01-19 10:14:23 +0100

    [PK-29389] June fikser testfeil. Renamet og flyttet integrasjonstest for vedleggservice. Laget egen pakke.

--- business/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/business/service/vedleggservice/VedleggServiceIntegrationMockContext.java
+++ business/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/business/service/vedleggservice/VedleggServiceIntegrationMockContext.java
@@ -1,4 +1,4 @@
-package no.nav.sbl.dialogarena.soknadinnsending.business.service.soknadservice;
+package no.nav.sbl.dialogarena.soknadinnsending.business.service.vedleggservice;
 
 
 import no.nav.sbl.dialogarena.common.kodeverk.*;
@@ -7,118 +7,128 @@ import no.nav.sbl.dialogarena.sendsoknad.domain.kravdialoginformasjon.*;
 import no.nav.sbl.dialogarena.sendsoknad.domain.message.*;
 import no.nav.sbl.dialogarena.soknadinnsending.business.*;
 import no.nav.sbl.dialogarena.soknadinnsending.business.db.soknad.*;
+import no.nav.sbl.dialogarena.soknadinnsending.business.db.vedlegg.*;
 import no.nav.sbl.dialogarena.soknadinnsending.business.service.*;
+import no.nav.sbl.dialogarena.soknadinnsending.business.service.soknadservice.*;
 import no.nav.sbl.dialogarena.soknadinnsending.consumer.fillager.*;
 import no.nav.sbl.dialogarena.soknadinnsending.consumer.henvendelse.*;
 import no.nav.tjeneste.domene.brukerdialog.fillager.v1.*;
 import no.nav.tjeneste.domene.brukerdialog.sendsoknad.v1.*;
-import org.mockito.*;
 import org.springframework.context.*;
 import org.springframework.context.annotation.*;
 
 import javax.inject.*;
 import javax.sql.*;
 
+import static org.mockito.Mockito.*;
 
 @Configuration
-public class BatmansDummyConfig {
+public class VedleggServiceIntegrationMockContext {
 
     @Bean
     public HenvendelseService hendvendelseService() {
-        return Mockito.mock(HenvendelseService.class);
+        return mock(HenvendelseService.class);
     }
 
     @Bean
     public FillagerService fillagerService() {
-        return Mockito.mock(FillagerService.class);
+        return mock(FillagerService.class);
     }
 
     @Bean
     public Kodeverk kodeverk() {
-        return Mockito.mock(Kodeverk.class);
+        return mock(Kodeverk.class);
     }
 
     @Bean
     public FaktaService faktaService() {
-        return Mockito.mock(FaktaService.class);
+        return mock(FaktaService.class);
     }
 
     @Bean
     @Named("soknadInnsendingRepository")
     public SoknadRepository lokalDb() {
-        return Mockito.mock(SoknadRepository.class);
+        return mock(SoknadRepository.class);
     }
 
     @Bean
     public WebSoknadConfig config() {
-        return Mockito.mock(WebSoknadConfig.class);
+        return mock(WebSoknadConfig.class);
     }
 
     @Bean
     public ApplicationContext applicationContext() {
-        return Mockito.mock(ApplicationContext.class);
+        return mock(ApplicationContext.class);
     }
 
     @Bean
     public KravdialogInformasjonHolder kravdialogInformasjonHolder() {
-        return Mockito.mock(KravdialogInformasjonHolder.class);
+        return mock(KravdialogInformasjonHolder.class);
     }
 
     @Bean
     public StartDatoService startDatoService() {
-        return Mockito.mock(StartDatoService.class);
+        return mock(StartDatoService.class);
     }
 
     @Bean
     public NavMessageSource messageSource() {
-        return Mockito.mock(NavMessageSource.class);
+        return mock(NavMessageSource.class);
     }
 
     @Bean
     public DataSource dataSource() {
-        return Mockito.mock(DataSource.class);
+        return mock(DataSource.class);
     }
 
     @Bean
     public XmlService xmlService() {
-        return Mockito.mock(XmlService.class);
+        return mock(XmlService.class);
     }
 
     @Bean
     @Named("fillagerEndpoint")
     public FilLagerPortType filLagerEndpoint() {
-        return Mockito.mock(FilLagerPortType.class);
-
+        return mock(FilLagerPortType.class);
     }
 
     @Bean
     @Named("fillagerSelftestEndpoint")
     public FilLagerPortType filLagerSelftestEndpoint() {
-        return Mockito.mock(FilLagerPortType.class);
+        return mock(FilLagerPortType.class);
     }
 
     @Bean
     @Named("sendSoknadEndpoint")
     public SendSoknadPortType sendSoknadEndpoint() {
-        return Mockito.mock(SendSoknadPortType.class);
+        return mock(SendSoknadPortType.class);
+    }
 
+    @Bean
+    public SoknadDataFletter soknadDataFletter() {
+        return mock(SoknadDataFletter.class);
     }
 
     @Bean
     @Named("sendSoknadSelftestEndpoint")
     public SendSoknadPortType sendSoknadSelftestEndpoint() {
-        return Mockito.mock(SendSoknadPortType.class);
+        return mock(SendSoknadPortType.class);
     }
 
     @Bean
     public SoknadService soknadService(){
-        return Mockito.mock(SoknadService.class);
+        return mock(SoknadService.class);
     }
 
     @Bean
     public EttersendingService ettersendingService(){
-        return Mockito.mock(EttersendingService.class);
+        return mock(EttersendingService.class);
+    }
 
+    @Bean
+    @Named("vedleggRepository")
+    public VedleggRepository vedleggRepository() {
+        return mock(VedleggRepositoryJdbc.class);
     }
 
 }
