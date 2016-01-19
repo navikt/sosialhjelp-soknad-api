77871f75d6a5ad54546b06d2970eabccae892b38
Hageland, Steffen <steffen.hageland@nav.no>
2016-01-19 09:08:00 +0100

    [PK-27701] kan nå velge statsborgerskap (mock) fra dropdown

--- web/src/main/java/no/nav/sbl/dialogarena/rest/utils/MockdataUtils.java
+++ web/src/main/java/no/nav/sbl/dialogarena/rest/utils/MockdataUtils.java
@@ -3,7 +3,6 @@ package no.nav.sbl.dialogarena.rest.utils;
 import no.nav.sbl.dialogarena.sendsoknad.mockmodul.person.PersonPortTypeMock;
 import no.nav.tjeneste.virksomhet.person.v1.informasjon.Diskresjonskoder;
 import no.nav.tjeneste.virksomhet.person.v1.informasjon.Person;
-import no.nav.tjeneste.virksomhet.person.v1.informasjon.Statsborgerskap;
 
 
 public class MockdataUtils {
@@ -13,24 +12,20 @@ public class MockdataUtils {
         return diskresjonskoder;
     }
 
-    public static void setLandPaaStatsborgerskap(Statsborgerskap statsborgerskap, String landkode) {
-        statsborgerskap.getLand().setValue(landkode);
-    }
-
     public static MockdataFields getMockdataFields(PersonPortTypeMock portTypeMock) {
         Person person = portTypeMock.getPerson();
 
-        Boolean erNorskStatsborger = person.getStatsborgerskap().getLand().getValue().equalsIgnoreCase("NOR");
+        String statsborgerskap = person.getStatsborgerskap().getLand().getValue().toUpperCase();
 
         MockdataFields fields = new MockdataFields();
         fields.setKode6(person.getDiskresjonskode() != null);
-        fields.setUtenlandskStatsborger(!erNorskStatsborger);
+        fields.setStatsborgerskap(statsborgerskap);
         return fields;
     }
 
     public static class MockdataFields {
         private Boolean kode6;
-        private Boolean utenlandskStatsborger;
+        private String statsborgerskap;
 
         public Boolean getKode6() {
             return kode6;
@@ -41,12 +36,12 @@ public class MockdataUtils {
         }
 
 
-        public Boolean getUtenlandskStatsborger() {
-            return utenlandskStatsborger;
+        public String getStatsborgerskap() {
+            return statsborgerskap;
         }
 
-        public void setUtenlandskStatsborger(Boolean utenlandskStatsborger) {
-            this.utenlandskStatsborger = utenlandskStatsborger;
+        public void setStatsborgerskap(String statsborgerskap) {
+            this.statsborgerskap = statsborgerskap;
         }
 
 
