3c7a7992d39c105c4cc6afbfe6fd42d36cb80cac
Jarle Svendsrud <jarle.svendsrud@nav.no>
2016-01-20 14:49:30 +0100

    [kaizen] Flytting av transformasjon fra business -> consumer

--- business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/person/BarnBolk.java
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/person/BarnBolk.java
@@ -1,28 +1,18 @@
 package no.nav.sbl.dialogarena.soknadinnsending.business.person;
 
-import no.nav.sbl.dialogarena.sendsoknad.domain.Barn;
-import no.nav.sbl.dialogarena.sendsoknad.domain.Faktum;
-import no.nav.sbl.dialogarena.soknadinnsending.consumer.person.FamilierelasjonTransform;
-import no.nav.sbl.dialogarena.soknadinnsending.consumer.exceptions.IkkeFunnetException;
-import no.nav.sbl.dialogarena.soknadinnsending.consumer.person.PersonService;
-import no.nav.tjeneste.virksomhet.person.v1.meldinger.HentKjerneinformasjonRequest;
-import no.nav.tjeneste.virksomhet.person.v1.meldinger.HentKjerneinformasjonResponse;
-import org.apache.commons.collections15.Transformer;
-import org.slf4j.Logger;
-import org.springframework.stereotype.Service;
+import no.nav.sbl.dialogarena.sendsoknad.domain.*;
+import no.nav.sbl.dialogarena.soknadinnsending.consumer.person.*;
+import org.apache.commons.collections15.*;
+import org.springframework.stereotype.*;
 
-import javax.inject.Inject;
-import javax.xml.ws.WebServiceException;
-import java.util.ArrayList;
-import java.util.List;
+import javax.inject.*;
+import java.util.*;
 
-import static no.nav.modig.lang.collections.IterUtils.on;
-import static org.slf4j.LoggerFactory.getLogger;
+import static no.nav.modig.lang.collections.IterUtils.*;
+import static no.nav.sbl.dialogarena.sendsoknad.domain.Faktum.FaktumType.SYSTEMREGISTRERT;
 
 @Service
 public class BarnBolk implements BolkService {
-
-    private static final Logger logger = getLogger(BarnBolk.class);
 
     @Inject
     private PersonService personService;
@@ -33,27 +23,12 @@ public class BarnBolk implements BolkService {
     }
 
     @Override
-    public List<Faktum> genererSystemFakta(String fodselsnummer, Long soknadId) {
-        HentKjerneinformasjonResponse kjerneinformasjonResponse;
-
-        try {
-            kjerneinformasjonResponse = personService.hentKjerneinformasjon(lagXMLRequestKjerneinformasjon(fodselsnummer));
-            List<Barn> barn = FamilierelasjonTransform.mapFamilierelasjon(kjerneinformasjonResponse);
-            return genererBarnFakta(soknadId, barn);
-
-        } catch (IkkeFunnetException e) {
-            logger.warn("Ikke funnet person i TPS");
-        } catch (WebServiceException e) {
-            logger.error("Ingen kontakt med TPS.", e);
-        }
-        return new ArrayList<>();
-    }
-
-    private List<Faktum> genererBarnFakta(final Long soknadId, List<Barn> barneliste) {
-        return on(barneliste).map(new Transformer<Barn, Faktum>() {
+    public List<Faktum> genererSystemFakta(String fodselsnummer, final Long soknadId) {
+        List<Barn> listeAvBarn = personService.hentBarn(fodselsnummer);
+        return on(listeAvBarn).map(new Transformer<Barn, Faktum>() {
             @Override
             public Faktum transform(Barn barn) {
-                return new Faktum().medSoknadId(soknadId).medKey("barn").medType(Faktum.FaktumType.SYSTEMREGISTRERT)
+                return new Faktum().medSoknadId(soknadId).medKey("barn").medType(SYSTEMREGISTRERT)
                         .medSystemProperty("fornavn", barn.getFornavn())
                         .medSystemProperty("mellomnavn", barn.getMellomnavn())
                         .medSystemProperty("etternavn", barn.getEtternavn())
@@ -67,9 +42,4 @@ public class BarnBolk implements BolkService {
         }).collect();
     }
 
-    private HentKjerneinformasjonRequest lagXMLRequestKjerneinformasjon(String ident) {
-        HentKjerneinformasjonRequest request = new HentKjerneinformasjonRequest();
-        request.setIdent(ident);
-        return request;
-    }
 }
