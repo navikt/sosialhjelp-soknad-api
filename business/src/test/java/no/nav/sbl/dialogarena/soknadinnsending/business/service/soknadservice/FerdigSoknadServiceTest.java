614b084fdf07e2a2ff3c1dfd9cd463bcaeaa20b2
Tine Kleivane <tine.kleivane@nav.no>
2016-01-28 14:11:40 +0100

    [PK-28239] Test på bortfiltrering av kvitteringen

---
+++ business/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/business/service/soknadservice/FerdigSoknadServiceTest.java
@@ -0,0 +1,63 @@
+package no.nav.sbl.dialogarena.soknadinnsending.business.service.soknadservice;
+
+import no.nav.melding.domene.brukerdialog.behandlingsinformasjon.v1.XMLHenvendelse;
+import no.nav.melding.domene.brukerdialog.behandlingsinformasjon.v1.XMLMetadataListe;
+import no.nav.melding.domene.brukerdialog.behandlingsinformasjon.v1.XMLVedlegg;
+import no.nav.sbl.dialogarena.soknadinnsending.business.domain.FerdigSoknad;
+import no.nav.sbl.dialogarena.soknadinnsending.business.domain.Vedlegg;
+import no.nav.sbl.dialogarena.soknadinnsending.business.service.VedleggService;
+import no.nav.sbl.dialogarena.soknadinnsending.consumer.henvendelse.HenvendelseService;
+import org.assertj.core.api.Condition;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FerdigSoknadServiceTest {
+
+    public static final String SKJEMANUMMER_KVITTERING = FerdigSoknadService.SKJEMANUMMER_KVITTERING;
+
+    public static final XMLVedlegg KVITTERING = new XMLVedlegg()
+            .withInnsendingsvalg("LASTET_OPP")
+            .withSkjemanummer(SKJEMANUMMER_KVITTERING);
+    @Mock
+    private HenvendelseService henvendelseService;
+
+    @Mock
+    private VedleggService vedleggService;
+
+    @InjectMocks
+    private FerdigSoknadService service;
+
+    @Test
+    public void skalFjerneKvitteringerFraVedleggene() throws Exception {
+        when(henvendelseService.hentInformasjonOmAvsluttetSoknad(anyString())).thenReturn(
+                new XMLHenvendelse()
+                        .withMetadataListe(
+                                new XMLMetadataListe()
+                                        .withMetadata(
+                                                KVITTERING)));
+
+        FerdigSoknad soknad = service.hentFerdigSoknad("ID01");
+        assertThat(soknad.getIkkeInnsendteVedlegg()).areNot(new Condition<Vedlegg>() {
+            @Override
+            public boolean matches(Vedlegg vedlegg) {
+                return SKJEMANUMMER_KVITTERING.equals(vedlegg.getSkjemaNummer());
+            }
+        });
+        assertThat(soknad.getInnsendteVedlegg()).areNot(new Condition<Vedlegg>() {
+            @Override
+            public boolean matches(Vedlegg vedlegg) {
+                return SKJEMANUMMER_KVITTERING.equals(vedlegg.getSkjemaNummer());
+            }
+        });
+
+
+    }
+}
