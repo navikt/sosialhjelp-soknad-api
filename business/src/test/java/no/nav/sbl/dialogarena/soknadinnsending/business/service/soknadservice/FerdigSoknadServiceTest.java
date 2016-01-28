dc2ac98769ae6e62054f244fe3a30d4702b60dc7
Tine Kleivane <tine.kleivane@nav.no>
2016-01-28 14:18:23 +0100

    [PK-28239] Trekk ut i setupmetode og nye conditions opprettes i egen metode

--- business/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/business/service/soknadservice/FerdigSoknadServiceTest.java
+++ business/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/business/service/soknadservice/FerdigSoknadServiceTest.java
@@ -8,6 +8,7 @@ import no.nav.sbl.dialogarena.soknadinnsending.business.domain.Vedlegg;
 import no.nav.sbl.dialogarena.soknadinnsending.business.service.VedleggService;
 import no.nav.sbl.dialogarena.soknadinnsending.consumer.henvendelse.HenvendelseService;
 import org.assertj.core.api.Condition;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -23,9 +24,6 @@ public class FerdigSoknadServiceTest {
 
     public static final String SKJEMANUMMER_KVITTERING = FerdigSoknadService.SKJEMANUMMER_KVITTERING;
 
-    public static final XMLVedlegg KVITTERING = new XMLVedlegg()
-            .withInnsendingsvalg("LASTET_OPP")
-            .withSkjemanummer(SKJEMANUMMER_KVITTERING);
     @Mock
     private HenvendelseService henvendelseService;
 
@@ -34,52 +32,47 @@ public class FerdigSoknadServiceTest {
 
     @InjectMocks
     private FerdigSoknadService service;
+    private XMLMetadataListe xmlMetadataListe;
+
+    @Before
+    public void setUp() throws Exception {
+        XMLHenvendelse xmlHenvendelse = new XMLHenvendelse();
+        xmlMetadataListe = new XMLMetadataListe();
+        when(henvendelseService.hentInformasjonOmAvsluttetSoknad(anyString())).thenReturn(
+                xmlHenvendelse.withMetadataListe(xmlMetadataListe));
+    }
 
     @Test
     public void skalFjerneKvitteringerFraVedleggene() throws Exception {
-        when(henvendelseService.hentInformasjonOmAvsluttetSoknad(anyString())).thenReturn(
-                new XMLHenvendelse()
-                        .withMetadataListe(
-                                new XMLMetadataListe()
-                                        .withMetadata(
-                                                KVITTERING)));
+        xmlMetadataListe.withMetadata(new XMLVedlegg()
+                .withInnsendingsvalg("LASTET_OPP")
+                .withSkjemanummer(SKJEMANUMMER_KVITTERING));
 
         FerdigSoknad soknad = service.hentFerdigSoknad("ID01");
-        assertThat(soknad.getIkkeInnsendteVedlegg()).areNot(new Condition<Vedlegg>() {
-            @Override
-            public boolean matches(Vedlegg vedlegg) {
-                return SKJEMANUMMER_KVITTERING.equals(vedlegg.getSkjemaNummer());
-            }
-        });
-        assertThat(soknad.getInnsendteVedlegg()).areNot(new Condition<Vedlegg>() {
-            @Override
-            public boolean matches(Vedlegg vedlegg) {
-                return SKJEMANUMMER_KVITTERING.equals(vedlegg.getSkjemaNummer());
-            }
-        });
-
-
+        assertThat(soknad.getIkkeInnsendteVedlegg()).areNot(liktSkjemanummer(SKJEMANUMMER_KVITTERING));
+        assertThat(soknad.getInnsendteVedlegg()).areNot(liktSkjemanummer(SKJEMANUMMER_KVITTERING));
     }
 
     @Test
     public void skalPlassereOpplastetVedleggUnderInnsendteVedlegg() throws Exception {
-        when(henvendelseService.hentInformasjonOmAvsluttetSoknad(anyString())).thenReturn(
-                new XMLHenvendelse()
-                        .withMetadataListe(
-                                new XMLMetadataListe()
-                                        .withMetadata(
-                                                new XMLVedlegg()
-                                                        .withInnsendingsvalg("LASTET_OPP")
-                                                        .withSkjemanummer("NAV 10-07.40"))));
+        String skjemanummer = "NAV 10-07.40";
+        xmlMetadataListe.withMetadata(
+                new XMLVedlegg()
+                        .withInnsendingsvalg("LASTET_OPP")
+                        .withSkjemanummer(skjemanummer));
 
         FerdigSoknad soknad = service.hentFerdigSoknad("ID01");
-        assertThat(soknad.getInnsendteVedlegg()).are(new Condition<Vedlegg>() {
-            @Override
-            public boolean matches(Vedlegg vedlegg) {
-                return "NAV 10-07.40".equals(vedlegg.getSkjemaNummer());
-            }
-        });
+        assertThat(soknad.getInnsendteVedlegg()).are(liktSkjemanummer(skjemanummer));
         assertThat(soknad.getIkkeInnsendteVedlegg()).hasSize(0);
 
+    }
+
+    private Condition<Vedlegg> liktSkjemanummer(final String skjemanummer) {
+        return new Condition<Vedlegg>() {
+            @Override
+            public boolean matches(Vedlegg vedlegg) {
+                return skjemanummer.equals(vedlegg.getSkjemaNummer());
+            }
+        };
     }
 }
