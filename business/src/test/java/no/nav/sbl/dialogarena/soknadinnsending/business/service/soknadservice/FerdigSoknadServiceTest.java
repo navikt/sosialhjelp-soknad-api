d502aaea7d8e4004ca4e4a55f7cc027e9e5befa5
Tine Kleivane <tine.kleivane@nav.no>
2016-02-01 09:03:31 +0100

    [PK-28239 QA] Legg ved soknadPrefix på en ferdig soknad slik at vi kan mappe tekster per soknad

--- business/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/business/service/soknadservice/FerdigSoknadServiceTest.java
+++ business/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/business/service/soknadservice/FerdigSoknadServiceTest.java
@@ -1,9 +1,9 @@
 package no.nav.sbl.dialogarena.soknadinnsending.business.service.soknadservice;
 
-import no.nav.melding.domene.brukerdialog.behandlingsinformasjon.v1.XMLHenvendelse;
-import no.nav.melding.domene.brukerdialog.behandlingsinformasjon.v1.XMLMetadataListe;
-import no.nav.melding.domene.brukerdialog.behandlingsinformasjon.v1.XMLVedlegg;
+import no.nav.melding.domene.brukerdialog.behandlingsinformasjon.v1.*;
 import no.nav.sbl.dialogarena.sendsoknad.domain.Vedlegg;
+import no.nav.sbl.dialogarena.sendsoknad.domain.kravdialoginformasjon.KravdialogInformasjon;
+import no.nav.sbl.dialogarena.sendsoknad.domain.kravdialoginformasjon.KravdialogInformasjonHolder;
 import no.nav.sbl.dialogarena.soknadinnsending.business.domain.FerdigSoknad;
 import no.nav.sbl.dialogarena.soknadinnsending.business.service.VedleggService;
 import no.nav.sbl.dialogarena.soknadinnsending.consumer.henvendelse.HenvendelseService;
@@ -16,6 +16,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.when;
@@ -24,6 +27,10 @@ import static org.mockito.Mockito.when;
 public class FerdigSoknadServiceTest {
 
     public static final String SKJEMANUMMER_KVITTERING = FerdigSoknadService.SKJEMANUMMER_KVITTERING;
+    public static final String SOKNAD_PREFIX = "kravdialog.prefix";
+    public static final XMLHovedskjema HOVEDSKJEMA = new XMLHovedskjema()
+            .withSkjemanummer("NAV 11-12.12")
+            .withInnsendingsvalg("LASTET_OPP");
 
     private XMLMetadataListe xmlMetadataListe;
     private XMLHenvendelse xmlHenvendelse;
@@ -34,11 +41,20 @@ public class FerdigSoknadServiceTest {
     @Mock
     private VedleggService vedleggService;
 
+    @Mock
+    private KravdialogInformasjonHolder kravdialogInformasjonHolder;
+
+    @Mock
+    private KravdialogInformasjon kravdialogInformasjon;
+
     @InjectMocks
     private FerdigSoknadService service;
 
     @Before
     public void setUp() throws Exception {
+
+        when(kravdialogInformasjonHolder.hentKonfigurasjon(HOVEDSKJEMA.getSkjemanummer())).thenReturn(kravdialogInformasjon);
+        when(kravdialogInformasjon.getSoknadTypePrefix()).thenReturn(SOKNAD_PREFIX);
         xmlHenvendelse = new XMLHenvendelse();
         xmlMetadataListe = new XMLMetadataListe();
         when(henvendelseService.hentInformasjonOmAvsluttetSoknad(anyString())).thenReturn(
@@ -47,7 +63,9 @@ public class FerdigSoknadServiceTest {
 
     @Test
     public void skalFjerneKvitteringerFraVedleggene() throws Exception {
-        xmlMetadataListe.withMetadata(new XMLVedlegg()
+        xmlMetadataListe.withMetadata(
+                HOVEDSKJEMA,
+                new XMLVedlegg()
                 .withInnsendingsvalg("LASTET_OPP")
                 .withSkjemanummer(SKJEMANUMMER_KVITTERING));
 
@@ -58,19 +76,15 @@ public class FerdigSoknadServiceTest {
 
     @Test
     public void skalPlassereOpplastetVedleggUnderInnsendteVedlegg() throws Exception {
-        String skjemanummer = "NAV 10-07.40";
-        xmlMetadataListe.withMetadata(
-                new XMLVedlegg()
-                        .withInnsendingsvalg("LASTET_OPP")
-                        .withSkjemanummer(skjemanummer));
-
+        xmlMetadataListe.withMetadata(HOVEDSKJEMA);
         FerdigSoknad soknad = service.hentFerdigSoknad("ID01");
-        assertThat(soknad.getInnsendteVedlegg()).are(liktSkjemanummer(skjemanummer));
+        assertThat(soknad.getInnsendteVedlegg()).are(liktSkjemanummer(HOVEDSKJEMA.getSkjemanummer()));
         assertThat(soknad.getIkkeInnsendteVedlegg()).hasSize(0);
     }
 
     @Test
     public void skalMappeDetaljerFraHenvendelse() throws Exception {
+        xmlMetadataListe.withMetadata(HOVEDSKJEMA);
         xmlHenvendelse
                 .withAvsluttetDato(new DateTime(2016, 01, 01, 12, 00))
                 .withTema("TSO");
@@ -81,16 +95,28 @@ public class FerdigSoknadServiceTest {
     }
 
     @Test
-    public void skalPlassereIkkeOpplastetVedleggUnderIkkeInnsendteVedlegg() throws Exception {
+    public void skalHenteUtSoknadprefixFraHovedskjema() throws Exception {
         xmlMetadataListe.withMetadata(
-                new XMLVedlegg().withInnsendingsvalg("VEDLEGG_SENDES_AV_ANDRE"),
+                HOVEDSKJEMA,
+                new XMLVedlegg().withInnsendingsvalg("SEND_SENERE").withSkjemanummer(SKJEMANUMMER_KVITTERING));
+
+        FerdigSoknad soknad = service.hentFerdigSoknad("ID01");
+        assertThat(soknad.getSoknadPrefix()).isEqualToIgnoringCase(SOKNAD_PREFIX);
+    }
+
+    @Test
+    public void skalPlassereIkkeOpplastetVedleggUnderIkkeInnsendteVedlegg() throws Exception {
+        Collection<XMLMetadata> ikkeInnsendteVedlegg = Arrays.asList(
+                (XMLMetadata) new XMLVedlegg().withInnsendingsvalg("VEDLEGG_SENDES_AV_ANDRE"),
                 new XMLVedlegg().withInnsendingsvalg("SEND_SENERE"),
                 new XMLVedlegg().withInnsendingsvalg("VEDLEGG_ALLEREDE_SENDT"),
                 new XMLVedlegg().withInnsendingsvalg("VEDLEGG_SENDES_IKKE"));
+        xmlMetadataListe.withMetadata(HOVEDSKJEMA);
+        xmlMetadataListe.withMetadata(ikkeInnsendteVedlegg);
 
         FerdigSoknad soknad = service.hentFerdigSoknad("ID01");
-        assertThat(soknad.getInnsendteVedlegg()).hasSize(0);
-        assertThat(soknad.getIkkeInnsendteVedlegg()).hasSameSizeAs(xmlMetadataListe.getMetadata());
+        assertThat(soknad.getInnsendteVedlegg()).hasSize(1);
+        assertThat(soknad.getIkkeInnsendteVedlegg()).hasSameSizeAs(ikkeInnsendteVedlegg);
 
     }
 
