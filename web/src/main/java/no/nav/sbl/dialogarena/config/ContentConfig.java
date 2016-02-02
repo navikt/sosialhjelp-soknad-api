58f42f548d2d1c8a3afc98af5d1f6924c0bfd02b
Holger Ludvigsen <holger.ludvigsen@nav.no>
2016-02-02 11:59:11 +0100

    Fjerner url-parameter som var lagt til for å unngå cachen

--- web/src/main/java/no/nav/sbl/dialogarena/config/ContentConfig.java
+++ web/src/main/java/no/nav/sbl/dialogarena/config/ContentConfig.java
@@ -77,7 +77,7 @@ public class ContentConfig {
         try {
             saveLocal("enonic/sendsoknad_nb_NO.properties", new URI(cmsBaseUrl + "/app/sendsoknad/nb_NO/tekster"));
             saveLocal("enonic/sendsoknad_en.properties", new URI(cmsBaseUrl + "/app/sendsoknad/en/tekster"));
-            saveLocal("enonic/dagpenger_nb_NO.properties", new URI(cmsBaseUrl + "/app/dagpenger/nb_NO/tekster?dfg"));
+            saveLocal("enonic/dagpenger_nb_NO.properties", new URI(cmsBaseUrl + "/app/dagpenger/nb_NO/tekster"));
             saveLocal("enonic/dagpenger_en.properties", new URI(cmsBaseUrl + "/app/dagpenger/en/tekster"));
             saveLocal("enonic/foreldrepenger_nb_NO.properties", new URI(cmsBaseUrl + "/app/foreldrepenger/nb_NO/tekster"));
             saveLocal("enonic/aap_nb_NO.properties", new URI(cmsBaseUrl + "/app/AAP/nb_NO/tekster"));
