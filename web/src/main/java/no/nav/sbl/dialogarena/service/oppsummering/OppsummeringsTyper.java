9209242bf65a4aa41457a49c140e21047bcb306c
Eirik L. Wang <eirik.wang@nav.no>
2016-08-17 10:50:52 +0200

    Lagt til tekster før søknaden som faktum som vises i utvidet oppsummering. Lagt til støtte for å si at et faktum kun skal vises på utvidet soknad.

--- web/src/main/java/no/nav/sbl/dialogarena/service/oppsummering/OppsummeringsTyper.java
+++ web/src/main/java/no/nav/sbl/dialogarena/service/oppsummering/OppsummeringsTyper.java
@@ -1,6 +1,7 @@
 package no.nav.sbl.dialogarena.service.oppsummering;
 
 import org.slf4j.Logger;
+
 import static org.slf4j.LoggerFactory.getLogger;
 
 public enum OppsummeringsTyper {
@@ -14,7 +15,8 @@ public enum OppsummeringsTyper {
     hidden("skjema/generisk/hidden"),
     inputgroup("skjema/generisk/inputgroup"),
     infotekst("skjema/generisk/infotekst"),
-    radio("skjema/generisk/default");
+    radio("skjema/generisk/default"),
+    tekster("skjema/generisk/tekster");
 
     public final String template;
     private static final Logger logger = getLogger(OppsummeringsTyper.class);
