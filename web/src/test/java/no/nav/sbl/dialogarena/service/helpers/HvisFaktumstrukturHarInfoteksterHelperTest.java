2f38322b4acbcf4075fe219d24d8417b4a4c8b27
Hageland, Steffen <steffen.hageland@nav.no>
2016-08-29 17:14:26 +0200

    [PK-30788] Tester for HvisFaktukstrukturHarInfoteksterHelper

--- web/src/test/java/no/nav/sbl/dialogarena/service/helpers/HvisFaktumstrukturHarInfoteksterHelperTest.java
+++ web/src/test/java/no/nav/sbl/dialogarena/service/helpers/HvisFaktumstrukturHarInfoteksterHelperTest.java
@@ -4,11 +4,15 @@ import com.github.jknack.handlebars.Context;
 import com.github.jknack.handlebars.Handlebars;
 import no.nav.sbl.dialogarena.sendsoknad.domain.Faktum;
 import no.nav.sbl.dialogarena.sendsoknad.domain.WebSoknad;
-import no.nav.sbl.dialogarena.service.helpers.faktum.ForFaktumHelper;
+import no.nav.sbl.dialogarena.sendsoknad.domain.oppsett.Constraint;
+import no.nav.sbl.dialogarena.sendsoknad.domain.oppsett.FaktumStruktur;
+import no.nav.sbl.dialogarena.sendsoknad.domain.oppsett.TekstStruktur;
+import no.nav.sbl.dialogarena.service.oppsummering.OppsummeringsFaktum;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,6 +25,51 @@ public class HvisFaktumstrukturHarInfoteksterHelperTest {
         handlebars = new Handlebars();
         HvisFaktumstrukturHarInfoteksterHelper helper = new HvisFaktumstrukturHarInfoteksterHelper();
         handlebars.registerHelper(helper.getNavn(), helper);
+    }
 
+    @Test
+    public void skalViseBlokkOmFaktumHarInfoteksterDefinertIStrukturMedConstraintsSomOppfylles() throws IOException {
+        Faktum contrainedFaktum = new Faktum().medKey("etfaktum").medProperty("prop", "11");
+        FaktumStruktur faktumStruktur = new FaktumStruktur();
+        TekstStruktur tekstStruktur = new TekstStruktur();
+        Constraint constraint = new Constraint("etfaktum", "properties['prop'] > 10");
+        WebSoknad webSoknad = new WebSoknad().medFaktum(contrainedFaktum);
+
+        tekstStruktur.setKey("enkey");
+        tekstStruktur.setType("infotekst");
+        tekstStruktur.setConstraints(Arrays.asList(constraint));
+
+        faktumStruktur.setTekster(Arrays.asList(tekstStruktur));
+        Faktum faktum = new Faktum().medKey("hovedfaktum");
+        OppsummeringsFaktum oppsummeringsFaktum = new OppsummeringsFaktum(webSoknad, faktumStruktur, faktum, null, null);
+
+        Context parentContext = Context.newContext(webSoknad);
+        Context childContext = Context.newContext(parentContext, oppsummeringsFaktum);
+
+        String compiled = handlebars.compileInline("{{#hvisFaktumstrukturHarInfotekster}}harInfoTekster{{/hvisFaktumstrukturHarInfotekster}}").apply(childContext);
+        assertThat(compiled).isEqualTo("harInfoTekster");
+    }
+
+    @Test
+    public void skalIkkeViseBlokkOmFaktumHarInfoteksterDefinertIStrukturMedConstraintsSomIkkeOppfylles() throws IOException {
+        Faktum contrainedFaktum = new Faktum().medKey("etfaktum").medProperty("prop", "5");
+        FaktumStruktur faktumStruktur = new FaktumStruktur();
+        TekstStruktur tekstStruktur = new TekstStruktur();
+        Constraint constraint = new Constraint("etfaktum", "properties['prop'] > 10");
+        WebSoknad webSoknad = new WebSoknad().medFaktum(contrainedFaktum);
+
+        tekstStruktur.setKey("enkey");
+        tekstStruktur.setType("infotekst");
+        tekstStruktur.setConstraints(Arrays.asList(constraint));
+
+        faktumStruktur.setTekster(Arrays.asList(tekstStruktur));
+        Faktum faktum = new Faktum().medKey("hovedfaktum");
+        OppsummeringsFaktum oppsummeringsFaktum = new OppsummeringsFaktum(webSoknad, faktumStruktur, faktum, null, null);
+
+        Context parentContext = Context.newContext(webSoknad);
+        Context childContext = Context.newContext(parentContext, oppsummeringsFaktum);
+
+        String compiled = handlebars.compileInline("{{#hvisFaktumstrukturHarInfotekster}}harInfoTekster{{else}}ikkeInfoTekst{{/hvisFaktumstrukturHarInfotekster}}").apply(childContext);
+        assertThat(compiled).isEqualTo("ikkeInfoTekst");
     }
 }
