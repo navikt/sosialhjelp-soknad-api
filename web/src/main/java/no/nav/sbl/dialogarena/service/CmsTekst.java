892a8834361db134cd36d3e094cc576455224802
Eckbo <E148657@ADEO.NO>
2016-08-30 13:24:55 +0200

    PK-30788 Knytte tekstoppsummeringen til en type søknad.

--- web/src/main/java/no/nav/sbl/dialogarena/service/CmsTekst.java
+++ web/src/main/java/no/nav/sbl/dialogarena/service/CmsTekst.java
@@ -1,5 +1,7 @@
 package no.nav.sbl.dialogarena.service;
 
+import no.nav.sbl.dialogarena.config.ContentConfig;
+import no.nav.sbl.dialogarena.sendsoknad.domain.message.NavMessageSource;
 import org.springframework.context.MessageSource;
 import org.springframework.context.NoSuchMessageException;
 import org.springframework.stereotype.Component;
@@ -7,20 +9,21 @@ import org.springframework.stereotype.Component;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.util.Locale;
+import java.util.Map;
 
 @Component
 public class CmsTekst {
 
     @Inject
-    @Named("navMessageSource")
-    private MessageSource navMessageSource;
+    @Named("navMessageBundles")
+    private ContentConfig.NavMessageWrapper navMessageBundles;
 
     public String getCmsTekst(String key, Object[] parameters, String soknadTypePrefix, Locale locale) {
         try {
-            return navMessageSource.getMessage(soknadTypePrefix + "." + key, parameters, locale);
+            return navMessageBundles.get(soknadTypePrefix).getMessage(soknadTypePrefix + "." + key, parameters, locale);
         } catch (NoSuchMessageException e) {
             try {
-                return navMessageSource.getMessage(key, parameters, locale);
+                return navMessageBundles.get(soknadTypePrefix).getMessage(key, parameters, locale);
             } catch (NoSuchMessageException e2) {
                 return null;
             }
