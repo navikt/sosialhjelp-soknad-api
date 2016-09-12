b8dc2d9b2eda581707b8375fb9d89b1d6d44ccf5
Tine Kleivane <tine.kleivane@nav.no>
2016-09-12 14:36:37 +0200

    [PK-35004] Ny helper dersom man har innsendte vedlegg

---
+++ web/src/main/java/no/nav/sbl/dialogarena/service/helpers/HvisHarInnsendteDokumenterHelper.java
@@ -0,0 +1,38 @@
+package no.nav.sbl.dialogarena.service.helpers;
+
+import com.github.jknack.handlebars.Options;
+import no.nav.sbl.dialogarena.sendsoknad.domain.Vedlegg;
+import no.nav.sbl.dialogarena.sendsoknad.domain.WebSoknad;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.List;
+
+import static no.nav.sbl.dialogarena.service.HandlebarsUtils.finnWebSoknad;
+
+@Component
+public class HvisHarInnsendteDokumenterHelper extends RegistryAwareHelper<Object> {
+
+    public static final String NAVN = "hvisHarInnsendteDokumenter";
+
+    @Override
+    public String getNavn() {
+        return NAVN;
+    }
+
+    @Override
+    public String getBeskrivelse() {
+        return "Sjekker om søknaden har ett eller flere innsendte dokumenter";
+    }
+
+    @Override
+    public CharSequence apply(Object o, Options options) throws IOException {
+        WebSoknad soknad = finnWebSoknad(options.context);
+        List<Vedlegg> vedlegg = soknad.getInnsendteVedlegg();
+        if (vedlegg.isEmpty()) {
+            return options.inverse(this);
+        } else {
+            return options.fn(this);
+        }
+    }
+}
