14358132264e9368beb88ffe08604f2ebb9b1b9e
Mats Krüger Svensson <mats.kruger.svensson@nav.no>
2016-10-28 10:47:38 +0200

    [sikkerhet] Fjerner X-Frame-Options. Blir lagt på av lastbalanserer med riktig verdi (SAMEORIGIN)

--- web/src/main/java/no/nav/sbl/dialogarena/sikkerhet/HeaderFilter.java
+++ web/src/main/java/no/nav/sbl/dialogarena/sikkerhet/HeaderFilter.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 public class HeaderFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        response.setHeader("X-Frame-Options", "SAME");
         response.setHeader("X-Content-Type-Options", "nosniff");
         response.setHeader("X-XSS-Protection", "1; mode=block");
         filterChain.doFilter(request, response);
