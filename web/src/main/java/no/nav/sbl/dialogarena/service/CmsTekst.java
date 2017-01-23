f607758f779e04199764c7f1520411b69d50ebea
Ingvild Indrebø <ingvild.indrebo@nav.no>
2017-01-23 10:53:10 +0100

    [PK-38068] Endre oppsummering til å hente tekster på samme måte som i frontend

--- web/src/main/java/no/nav/sbl/dialogarena/service/CmsTekst.java
+++ web/src/main/java/no/nav/sbl/dialogarena/service/CmsTekst.java
@@ -1,29 +1,26 @@
 package no.nav.sbl.dialogarena.service;
 
-import no.nav.sbl.dialogarena.config.ContentConfig;
-import org.springframework.context.NoSuchMessageException;
+import no.nav.sbl.dialogarena.sendsoknad.domain.message.NavMessageSource;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
-import javax.inject.Named;
 import java.util.Locale;
+import java.util.Properties;
 
 @Component
 public class CmsTekst {
-
     @Inject
-    @Named("navMessageBundles")
-    private ContentConfig.NavMessageWrapper navMessageBundles;
+    private NavMessageSource messageSource;
 
-    public String getCmsTekst(String key, Object[] parameters, String soknadTypePrefix, Locale locale) {
-        try {
-            return navMessageBundles.get(soknadTypePrefix).getMessage(soknadTypePrefix + "." + key, parameters, locale);
-        } catch (NoSuchMessageException e) {
-            try {
-                return navMessageBundles.get(soknadTypePrefix).getMessage(key, parameters, locale);
-            } catch (NoSuchMessageException e2) {
-                return null;
-            }
+    public String getCmsTekst(String key, Object[] parameters, String soknadTypePrefix, String bundleName, Locale locale) {
+        Properties bundle = messageSource.getBundleFor(bundleName, locale);
+
+        String tekst = bundle.getProperty(soknadTypePrefix + "." + key);
+
+        if (tekst == null) {
+            tekst = bundle.getProperty(key);
         }
+
+        return tekst;
     }
 }
