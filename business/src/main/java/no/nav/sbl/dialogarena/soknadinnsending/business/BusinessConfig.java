6b8a0f5b06c71b4ad2073f3150187fd49d360e42
Mats Krüger Svensson <mats.kruger.svensson@nav.no>
2017-03-02 14:08:46 +0100

    Fjerner kode for sletting av søknader som lå feillagret i databasen

--- business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/BusinessConfig.java
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/BusinessConfig.java
@@ -4,7 +4,6 @@ import no.nav.sbl.dialogarena.sendsoknad.domain.kravdialoginformasjon.Kravdialog
 import no.nav.sbl.dialogarena.soknadinnsending.business.aktivitetbetalingsplan.AktivitetBetalingsplanBolk;
 import no.nav.sbl.dialogarena.soknadinnsending.business.arbeid.ArbeidsforholdBolk;
 import no.nav.sbl.dialogarena.soknadinnsending.business.batch.LagringsScheduler;
-import no.nav.sbl.dialogarena.soknadinnsending.business.batch.SlettFeilaktigeGamleSoknaderScheduler;
 import no.nav.sbl.dialogarena.soknadinnsending.business.db.DbConfig;
 import no.nav.sbl.dialogarena.soknadinnsending.business.person.BarnBolk;
 import no.nav.sbl.dialogarena.soknadinnsending.business.person.PersonaliaBolk;
@@ -16,7 +15,6 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @Import({
         LagringsScheduler.class,
-        SlettFeilaktigeGamleSoknaderScheduler.class,
         DbConfig.class,
         AktivitetService.class,
         MaalgrupperService.class,
