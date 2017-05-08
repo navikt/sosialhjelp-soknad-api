75924729fff0254a4e7d16882a1fcfa1eb8ac60c
Nemanja Aksic <nemanja.aksic@nav.no>
2017-05-08 13:22:12 +0200

    [PK-41557] Fjerner link mot enonic tekstene

--- web/src/main/java/no/nav/sbl/dialogarena/config/ContentConfig.java
+++ web/src/main/java/no/nav/sbl/dialogarena/config/ContentConfig.java
@@ -96,8 +96,6 @@ public class ContentConfig {
         try {
             saveLocal("enonic/sendsoknad_nb_NO.properties", new URI(cmsBaseUrl + "/app/sendsoknad/nb_NO/tekster"));
             saveLocal("enonic/sendsoknad_en.properties", new URI(cmsBaseUrl + "/app/sendsoknad/en/tekster"));
-            saveLocal("enonic/dagpenger_nb_NO.properties", new URI(cmsBaseUrl + "/app/dagpenger/nb_NO/tekster"));
-            saveLocal("enonic/dagpenger_en.properties", new URI(cmsBaseUrl + "/app/dagpenger/en/tekster"));
             saveLocal("enonic/bilstonad_nb_NO.properties", new URI(cmsBaseUrl + "/app/bilstonad/nb_NO/tekster"));
             saveLocal("enonic/tilleggsstonader_nb_NO.properties", new URI(cmsBaseUrl + "/app/tilleggsstonader/nb_NO/tekster"));
             saveLocal("enonic/tiltakspenger_nb_NO.properties", new URI(cmsBaseUrl + "/app/tiltakspenger/nb_NO/tekster"));
