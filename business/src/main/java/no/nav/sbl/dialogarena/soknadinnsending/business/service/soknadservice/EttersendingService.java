5480124433901eccb2fd8219d59e3b5279e735b0
Øyvind Møller Asbjørnsen <oyvind.moller.asbjornsen@nav.no>
2017-05-15 16:17:37 +0200

    PK-40099 jdbc, ettersending, lagringscheduler m.m. - Byttet modig.lang med java8-features

--- business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/service/soknadservice/EttersendingService.java
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/service/soknadservice/EttersendingService.java
@@ -4,9 +4,8 @@ import no.nav.melding.domene.brukerdialog.behandlingsinformasjon.v1.XMLHovedskje
 import no.nav.melding.domene.brukerdialog.behandlingsinformasjon.v1.XMLMetadata;
 import no.nav.melding.domene.brukerdialog.behandlingsinformasjon.v1.XMLMetadataListe;
 import no.nav.modig.core.exception.ApplicationException;
-import no.nav.modig.lang.collections.predicate.InstanceOf;
-import no.nav.modig.lang.collections.transform.Cast;
 import no.nav.sbl.dialogarena.sendsoknad.domain.Faktum;
+import no.nav.sbl.dialogarena.sendsoknad.domain.SoknadInnsendingStatus;
 import no.nav.sbl.dialogarena.sendsoknad.domain.WebSoknad;
 import no.nav.sbl.dialogarena.soknadinnsending.business.db.soknad.SoknadRepository;
 import no.nav.sbl.dialogarena.soknadinnsending.business.service.FaktaService;
@@ -21,12 +20,13 @@ import org.springframework.stereotype.Component;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import static java.util.UUID.randomUUID;
+import static java.util.stream.Collectors.toList;
 import static no.nav.modig.core.context.SubjectHandler.getSubjectHandler;
-import static no.nav.modig.lang.collections.IterUtils.on;
-import static no.nav.modig.lang.collections.PredicateUtils.*;
-import static no.nav.modig.lang.option.Optional.optional;
 import static no.nav.sbl.dialogarena.sendsoknad.domain.Faktum.FaktumType.SYSTEMREGISTRERT;
 import static no.nav.sbl.dialogarena.sendsoknad.domain.SoknadInnsendingStatus.AVBRUTT_AV_BRUKER;
 import static no.nav.sbl.dialogarena.soknadinnsending.business.service.soknadservice.StaticMetoder.*;
@@ -57,10 +57,10 @@ public class EttersendingService {
         List<WSBehandlingskjedeElement> behandlingskjede = henvendelseService.hentBehandlingskjede(behandlingsIdDetEttersendesPaa);
         WSHentSoknadResponse nyesteSoknad = hentNyesteSoknadFraHenvendelse(behandlingskjede);
 
-        optional(nyesteSoknad.getInnsendtDato()).getOrThrow(new ApplicationException("Kan ikke starte ettersending på en ikke fullfort soknad"));
+        Optional.ofNullable(nyesteSoknad.getInnsendtDato()).orElseThrow(() -> new ApplicationException("Kan ikke starte ettersending på en ikke fullfort soknad"));
 
         String nyBehandlingsId = henvendelseService.startEttersending(nyesteSoknad);
-        String behandlingskjedeId = optional(nyesteSoknad.getBehandlingskjedeId()).getOrElse(nyesteSoknad.getBehandlingsId());
+        String behandlingskjedeId = Optional.ofNullable(nyesteSoknad.getBehandlingskjedeId()).orElse(nyesteSoknad.getBehandlingsId());
         WebSoknad ettersending = lagreEttersendingTilLokalDb(behandlingsIdDetEttersendesPaa, behandlingskjede, behandlingskjedeId, nyBehandlingsId);
 
         soknadMetricsService.startetSoknad(ettersending.getskjemaNummer(), true);
@@ -69,18 +69,19 @@ public class EttersendingService {
     }
 
     private WSHentSoknadResponse hentNyesteSoknadFraHenvendelse(List<WSBehandlingskjedeElement> behandlingskjede) {
-        List<WSBehandlingskjedeElement> nyesteForstBehandlinger = on(behandlingskjede)
-                .filter(where(STATUS, not(equalTo(AVBRUTT_AV_BRUKER))))
-                .collect(NYESTE_FORST);
+        List<WSBehandlingskjedeElement> nyesteForstBehandlinger = behandlingskjede.stream()
+                .filter(element -> AVBRUTT_AV_BRUKER != SoknadInnsendingStatus.valueOf(element.getStatus()))
+                .sorted(NYESTE_FORST)
+                .collect(toList());
 
         return henvendelseService.hentSoknad(nyesteForstBehandlinger.get(0).getBehandlingsId());
     }
 
     private WebSoknad lagreEttersendingTilLokalDb(String originalBehandlingsId, List<WSBehandlingskjedeElement> behandlingskjede, String behandlingskjedeId, String ettersendingsBehandlingId) {
         List<XMLMetadata> alleVedlegg = ((XMLMetadataListe) henvendelseService.hentSoknad(ettersendingsBehandlingId).getAny()).getMetadata();
-        List<XMLMetadata> vedleggBortsettFraKvittering = on(alleVedlegg).filter(not(kvittering())).collect();
+        List<XMLMetadata> vedleggBortsettFraKvittering = alleVedlegg.stream().filter(IKKE_KVITTERING).collect(toList());
 
-        WebSoknad ettersending = lagSoknad(ettersendingsBehandlingId, behandlingskjedeId, finnHovedskjema(vedleggBortsettFraKvittering));
+                WebSoknad ettersending = lagSoknad(ettersendingsBehandlingId, behandlingskjedeId, finnHovedskjema(vedleggBortsettFraKvittering));
 
         Long soknadId = lokalDb.opprettSoknad(ettersending);
         ettersending.setSoknadId(soknadId);
@@ -110,10 +111,11 @@ public class EttersendingService {
     }
 
     private XMLHovedskjema finnHovedskjema(List<XMLMetadata> vedleggBortsettFraKvittering) {
-        return on(vedleggBortsettFraKvittering)
-                .filter(new InstanceOf<XMLMetadata>(XMLHovedskjema.class))
-                .map(new Cast<>(XMLHovedskjema.class))
-                .head()
-                .getOrThrow(new ApplicationException("Kunne ikke hente opp hovedskjema for søknad"));
+
+        return vedleggBortsettFraKvittering.stream()
+                .filter(xmlMetadata -> xmlMetadata instanceof XMLHovedskjema)
+                .map(xmlMetadata -> (XMLHovedskjema) xmlMetadata)
+                .findFirst()
+                .orElseThrow(()->new ApplicationException("Kunne ikke hente opp hovedskjema for søknad"));
     }
 }
