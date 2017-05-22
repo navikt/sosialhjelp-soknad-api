85a7ceebdb0fe3d3f7d5332e6dcbcec86389be2e
Martine Enger <martine.enger@nav.no>
2017-05-22 13:40:41 +0200

    PK-37770: Laget tester for barnetillegg-vedlegg

---
+++ web/src/test/java/no/nav/sbl/dialogarena/integration/DagpengerBarneTilleggVedleggIT.java
@@ -0,0 +1,23 @@
+package no.nav.sbl.dialogarena.integration;
+
+import no.nav.sbl.dialogarena.sendsoknad.domain.kravdialoginformasjon.DagpengerOrdinaerInformasjon;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DagpengerBarneTilleggVedleggIT extends AbstractIT {
+
+    private String dagpengerSkjemaNummer = new DagpengerOrdinaerInformasjon().getSkjemanummer().get(0);
+
+    @Before
+    public void setup() throws Exception {
+        EndpointDataMocking.setupMockWsEndpointData();
+    }
+
+    @Test
+    public void skalIkkeKreveNoenVedleggVedStart() {
+        soknadMedDelstegstatusOpprettet(dagpengerSkjemaNummer)
+                .hentPaakrevdeVedlegg()
+                .skalIkkeKreveNoenVedlegg();
+    }
+
+}
