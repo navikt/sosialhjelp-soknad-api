e373cdb5fa9b74ed79e2f8a186255dec7880bdc6
Tine Kleivane <tine.kleivane@nav.no>
2017-05-29 13:22:40 +0200

    KAIZEN Singelton list

--- domain/src/main/java/no/nav/sbl/dialogarena/sendsoknad/domain/kravdialoginformasjon/ForeldrepengerInformasjon.java
+++ domain/src/main/java/no/nav/sbl/dialogarena/sendsoknad/domain/kravdialoginformasjon/ForeldrepengerInformasjon.java
@@ -10,9 +10,11 @@ import org.apache.commons.collections15.Transformer;
 import org.springframework.context.MessageSource;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static java.util.Arrays.asList;
+import static java.util.Collections.*;
 
 
 public class ForeldrepengerInformasjon extends KravdialogInformasjon.DefaultOppsett {
@@ -38,10 +40,11 @@ public class ForeldrepengerInformasjon extends KravdialogInformasjon.DefaultOpps
     public List<String> getSkjemanummer() {
         return Arrays.asList("NAV 14-05.06", "NAV 14-05.07", "NAV 14-05.08", "NAV 14-05.09", "NAV 14-05.10");
     }
+
     @Override
     public List<AlternativRepresentasjonTransformer> getTransformers(MessageSource messageSource) {
         AlternativRepresentasjonTransformer engangsstonadTilXml = new ForeldrepengerEngangsstonadTilXml(messageSource);
-        return asList(engangsstonadTilXml);
+        return singletonList(engangsstonadTilXml);
     }
 
     @Override
