19e57c011c444b1eaac70384c68ef224feffde71
Tine Kleivane <tine.kleivane@nav.no>
2017-05-30 17:02:13 +0200

    KAIZEN Sett lognivå til debug på ting som skjer ekstremt ofte og flooder loggene

--- web/src/main/java/no/nav/sbl/dialogarena/sikkerhet/SikkerhetsAspect.java
+++ web/src/main/java/no/nav/sbl/dialogarena/sikkerhet/SikkerhetsAspect.java
@@ -59,7 +59,7 @@ public class SikkerhetsAspect {
             throw new NotFoundException("Fant ikke ressurs.");
         }
 
-        logger.info("Sjekker tilgang til ressurs med behandlingsId {} og type {}", behandlingsId, tilgang.type());
+        logger.debug("Sjekker tilgang til ressurs med behandlingsId {} og type {}", behandlingsId, tilgang.type());
         if (tilgang.sjekkXsrf() && skrivOperasjon(request)) {
             sjekkXsrfToken(request.getHeader("X-XSRF-TOKEN"), behandlingsId);
         }
