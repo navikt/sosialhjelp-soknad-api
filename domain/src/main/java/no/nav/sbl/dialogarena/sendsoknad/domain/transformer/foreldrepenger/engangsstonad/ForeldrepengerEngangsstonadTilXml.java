2d3756a231ed20b7a8188c1a893437a12d78be3d
Martine Enger <martine.enger@nav.no>
2017-06-01 13:44:36 +0200

    PK-38225: Valideringsmetode for engangsstønad-skjema xsd

--- domain/src/main/java/no/nav/sbl/dialogarena/sendsoknad/domain/transformer/foreldrepenger/engangsstonad/ForeldrepengerEngangsstonadTilXml.java
+++ domain/src/main/java/no/nav/sbl/dialogarena/sendsoknad/domain/transformer/foreldrepenger/engangsstonad/ForeldrepengerEngangsstonadTilXml.java
@@ -5,15 +5,30 @@ import no.nav.sbl.dialogarena.sendsoknad.domain.AlternativRepresentasjon;
 import no.nav.sbl.dialogarena.sendsoknad.domain.WebSoknad;
 import no.nav.sbl.dialogarena.sendsoknad.domain.transformer.AlternativRepresentasjonTransformer;
 import no.nav.sbl.dialogarena.sendsoknad.domain.transformer.AlternativRepresentasjonType;
+import org.slf4j.Logger;
 import org.springframework.context.MessageSource;
+import org.xml.sax.SAXException;
 
+import javax.xml.XMLConstants;
 import javax.xml.bind.JAXB;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.util.JAXBSource;
+import javax.xml.namespace.QName;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
 import java.io.ByteArrayOutputStream;
 import java.util.UUID;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 public class ForeldrepengerEngangsstonadTilXml implements AlternativRepresentasjonTransformer {
 
     private final MessageSource messageSource;
+    private static final Logger logger = getLogger(ForeldrepengerEngangsstonadTilXml.class);
 
     public ForeldrepengerEngangsstonadTilXml(MessageSource messageSource) {
         this.messageSource = messageSource;
@@ -21,6 +36,7 @@ public class ForeldrepengerEngangsstonadTilXml implements AlternativRepresentasj
 
     public AlternativRepresentasjon transform(WebSoknad webSoknad) {
         SoeknadsskjemaEngangsstoenad engangsstonad = tilSoeknadsskjemaEngangsstoenad(webSoknad, messageSource);
+        validerSkjema(engangsstonad);
         ByteArrayOutputStream xml = new ByteArrayOutputStream();
         JAXB.marshal(engangsstonad, xml);
         return new AlternativRepresentasjon()
@@ -45,5 +61,24 @@ public class ForeldrepengerEngangsstonadTilXml implements AlternativRepresentasj
         return transform(webSoknad);
     }
 
+    public void validerSkjema(SoeknadsskjemaEngangsstoenad engangsstoenadSkjema) {
+        QName qname = new QName("http://nav.no/melding/virksomhet/soeknadsskjemaEngangsstoenad/v1", "soeknadsskjemaengangsstoenad");
+        JAXBElement<SoeknadsskjemaEngangsstoenad> skjema = new JAXBElement<>(qname, SoeknadsskjemaEngangsstoenad.class, engangsstoenadSkjema);
+        try {
+            SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = factory.newSchema(new StreamSource(ForeldrepengerEngangsstonadTilXml.class.getResourceAsStream("/tjenestespesifikasjon/no/nav/melding/virksomhet/soeknadsskjemaEngangsstoenad/v1/v1.xsd")));
+            Marshaller marshaller = JAXBContext.newInstance(SoeknadsskjemaEngangsstoenad.class).createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            marshaller.setSchema(schema);
+            Validator validator = schema.newValidator();
+            JAXBSource source = new JAXBSource(marshaller, skjema);
+            validator.validate(source);
+        } catch (Exception e) {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            JAXB.marshal(skjema, baos);
+            logger.warn("Validering av skjema feilet: " + e + ". Xml: " + baos.toString(), e);
+        }
+
+    }
 
 }
