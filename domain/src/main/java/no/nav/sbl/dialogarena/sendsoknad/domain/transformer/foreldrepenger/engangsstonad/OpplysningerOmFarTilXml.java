de4038a6a831f62df9fd0408500f9f39b85b2a00
Nemanja Aksic <nemanja.aksic@nav.no>
2017-06-06 11:03:36 +0200

    [PK-38225] Oppretter en transformer for Opplysninger om far - bolken og mapper XML-representasjonen til faktaene.

---
+++ domain/src/main/java/no/nav/sbl/dialogarena/sendsoknad/domain/transformer/foreldrepenger/engangsstonad/OpplysningerOmFarTilXml.java
@@ -0,0 +1,51 @@
+package no.nav.sbl.dialogarena.sendsoknad.domain.transformer.foreldrepenger.engangsstonad;
+
+import no.nav.melding.virksomhet.soeknadsskjemaengangsstoenad.v1.*;
+import no.nav.sbl.dialogarena.sendsoknad.domain.WebSoknad;
+import java.util.function.Function;
+
+public class OpplysningerOmFarTilXml implements Function<WebSoknad, OpplysningerOmFar> {
+    @Override
+    public OpplysningerOmFar apply(WebSoknad webSoknad) {
+
+        OpplysningerOmFar opplysningerOmFar = new OpplysningerOmFar();
+
+        String fornavn = webSoknad.getValueForFaktum("infofar.opplysninger.fornavn");
+        String etternavn = webSoknad.getValueForFaktum("infofar.opplysninger.etternavn");
+        String personidentifikator = webSoknad
+                .getFaktumMedKey("infofar.opplysninger.personinfo")
+                .finnEgenskap("personnummer")
+                .getValue();
+
+        Boolean kanIkkeOppgi = Boolean.valueOf(webSoknad.getValueForFaktum("infofar.opplysninger.kanIkkeOppgi"));
+        String kanIkkeOppgiArsak = webSoknad.getValueForFaktum("infofar.opplysninger.kanIkkeOppgi.true.arsak");
+
+        if(kanIkkeOppgi){
+            KanIkkeOppgiFar kanIkkeOppgiFar = new KanIkkeOppgiFar();
+            kanIkkeOppgiFar.setAarsak(kanIkkeOppgiArsak);
+            if(kanIkkeOppgiArsak.equals("utenlandsk")){
+                opplysningerOmFar.setFornavn(fornavn);
+                opplysningerOmFar.setEtternavn(etternavn);
+
+                String utenlandskPersonnummer = webSoknad
+                        .getValueForFaktum("infofar.opplysninger.kanIkkeOppgi.true.arsak.utenlandsk.fodselsnummer");
+                kanIkkeOppgiFar.setUtenlandskfnr(utenlandskPersonnummer);
+
+                String landkodeForPersonnummer = webSoknad.
+                        getFaktumMedKey("infofar.opplysninger.kanIkkeOppgi.true.arsak")
+                        .finnEgenskap("land")
+                        .getValue();
+                kanIkkeOppgiFar.setUtenlandskfnrLand(new Landkoder().withKode(landkodeForPersonnummer));
+            }
+
+            opplysningerOmFar.setKanIkkeOppgiFar(kanIkkeOppgiFar);
+        }
+        else{
+            opplysningerOmFar.setFornavn(fornavn);
+            opplysningerOmFar.setEtternavn(etternavn);
+            opplysningerOmFar.setPersonidentifikator(personidentifikator);
+        }
+
+        return opplysningerOmFar;
+    }
+}
