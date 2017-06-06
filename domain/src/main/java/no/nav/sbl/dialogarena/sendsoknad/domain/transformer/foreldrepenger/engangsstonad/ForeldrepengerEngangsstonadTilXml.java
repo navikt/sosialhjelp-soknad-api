3f76c876c67bc067fcdedf9cc370bc2210fc4c5b
Nemanja Aksic <nemanja.aksic@nav.no>
2017-06-06 11:04:49 +0200

    [PK-38225] Legger til Opplyninger om far transformeren til engangsstønadskjema-objektet

--- domain/src/main/java/no/nav/sbl/dialogarena/sendsoknad/domain/transformer/foreldrepenger/engangsstonad/ForeldrepengerEngangsstonadTilXml.java
+++ domain/src/main/java/no/nav/sbl/dialogarena/sendsoknad/domain/transformer/foreldrepenger/engangsstonad/ForeldrepengerEngangsstonadTilXml.java
@@ -34,7 +34,8 @@ public class ForeldrepengerEngangsstonadTilXml implements AlternativRepresentasj
     private SoeknadsskjemaEngangsstoenad tilSoeknadsskjemaEngangsstoenad(WebSoknad webSoknad, MessageSource messageSource) {
         return new SoeknadsskjemaEngangsstoenad()
                 .withRettigheter(new RettigheterTilXml().apply(webSoknad))
-                .withTilknytningNorge(new TilknytningTilXml().apply(webSoknad));
+                .withTilknytningNorge(new TilknytningTilXml().apply(webSoknad))
+                .withOpplysningerOmFar(new OpplysningerOmFarTilXml().apply(webSoknad));
     }
 
     @Override
