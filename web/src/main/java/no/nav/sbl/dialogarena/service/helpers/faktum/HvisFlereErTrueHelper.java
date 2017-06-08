f54d569744b26106b5837ba1cc317173557035bd
Tessem <T150621@ADEO.NO>
2017-06-08 14:25:12 +0200

    [PK-43816] Fikset lambda og fjernet ubrukte imports i HvisFlereErTrueHjelper

--- web/src/main/java/no/nav/sbl/dialogarena/service/helpers/faktum/HvisFlereErTrueHelper.java
+++ web/src/main/java/no/nav/sbl/dialogarena/service/helpers/faktum/HvisFlereErTrueHelper.java
@@ -1,16 +1,11 @@
 package no.nav.sbl.dialogarena.service.helpers.faktum;
 
 import com.github.jknack.handlebars.Options;
-import no.nav.sbl.dialogarena.sendsoknad.domain.Faktum;
-import no.nav.sbl.dialogarena.sendsoknad.domain.WebSoknad;
 import no.nav.sbl.dialogarena.service.helpers.RegistryAwareHelper;
-import org.apache.commons.collections15.Predicate;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.List;
 
-import static no.nav.modig.lang.collections.IterUtils.on;
 import static no.nav.sbl.dialogarena.service.HandlebarsUtils.finnWebSoknad;
 
 @Component
@@ -30,17 +25,11 @@ public class HvisFlereErTrueHelper extends RegistryAwareHelper<String> {
     public CharSequence apply(String key, Options options) throws IOException {
         Integer grense = Integer.parseInt((String) options.param(0));
 
-        WebSoknad soknad = finnWebSoknad(options.context);
-        List<Faktum> fakta = soknad.getFaktaSomStarterMed(key);
-
-        int size = on(fakta).filter(new Predicate<Faktum>() {
-            @Override
-            public boolean evaluate(Faktum faktum) {
-                String value = faktum.getValue();
-                return value != null && value.equals("true");
-            }
-        }).collect().size();
-
+        int size = (int)finnWebSoknad(options.context).getFaktaSomStarterMed(key).stream()
+                .map(f -> f.getValue())
+                .filter(f -> f != null)
+                .filter(f -> f.equals("true"))
+                .count();
 
         if (size > grense) {
             return options.fn();
