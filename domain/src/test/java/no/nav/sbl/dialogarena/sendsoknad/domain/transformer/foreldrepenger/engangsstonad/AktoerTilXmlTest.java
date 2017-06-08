f316298d24353aefcaa831853d90daa45681a0ad
Nemanja Aksic <nemanja.aksic@nav.no>
2017-06-08 11:47:58 +0200

    [kaizen] Fjerner unødvendig fakta fra test

--- domain/src/test/java/no/nav/sbl/dialogarena/sendsoknad/domain/transformer/foreldrepenger/engangsstonad/AktoerTilXmlTest.java
+++ domain/src/test/java/no/nav/sbl/dialogarena/sendsoknad/domain/transformer/foreldrepenger/engangsstonad/AktoerTilXmlTest.java
@@ -15,8 +15,6 @@ public class AktoerTilXmlTest {
         WebSoknad soknad = new WebSoknad();
 
         soknad.medAktorId("010101010101");
-        soknad.getFakta().add(new Faktum().medKey("soknadsvalg.stonadstype").medValue("engangsstonadMor"));
-        soknad.getFakta().add(new Faktum().medKey("soknadsvalg.fodselelleradopsjon").medValue("fodsel"));
 
         AktoerId resultat = (AktoerId) new AktoerTilXml().apply(soknad);
         assertThat(resultat.getAktoerId()).isEqualTo("010101010101");
