cc6dd6d1a4178a226a606759646e41b1752fe763
Tine Kleivane <tine.kleivane@nav.no>
2017-06-23 16:07:19 +0200

    Revert "Rename og deprecate av aktorid i WebSoknad"
    This reverts commit 6cd75c97c689a1c0c5c20b69ce780b4cfacb77c7.
    
    Denne committen ble reversert etter å ha trigget en feil i produksjon. Den korte versjonen er at WebSoknad-objektet er lagret serialisert i henvendelse, som gjør at søknader som er
    startet tidligere og lagret i henvendelse må være serialiserbare tilbake ny versjon av dette objektet. Det var ikke mulig etter disse endringene. For brukerne gikk det bra med helt nye søknader,
    men søknader som ble hentet opp fra henvendelse kunne ikke fortsettes på.
    
    Fullversjonen av postmortem "22.06.17 - Ikke mulig å hente opp tidligere påbegynte søknader" kan lese her https://confluence.adeo.no/pages/viewpage.action?pageId=224319754

--- domain/src/test/java/no/nav/sbl/dialogarena/sendsoknad/domain/transformer/foreldrepenger/engangsstonad/BrukerTilXmlTest.java
+++ domain/src/test/java/no/nav/sbl/dialogarena/sendsoknad/domain/transformer/foreldrepenger/engangsstonad/BrukerTilXmlTest.java
@@ -12,7 +12,7 @@ public class BrukerTilXmlTest {
     public void medPersonnummerTest() {
         WebSoknad soknad = new WebSoknad();
 
-        soknad.medFodselsnummer("010101010101");
+        soknad.medAktorId("010101010101");
 
         Bruker resultat = (Bruker) new BrukerTilXml().apply(soknad);
         assertThat(resultat.getPersonidentifikator()).isEqualTo("010101010101");
