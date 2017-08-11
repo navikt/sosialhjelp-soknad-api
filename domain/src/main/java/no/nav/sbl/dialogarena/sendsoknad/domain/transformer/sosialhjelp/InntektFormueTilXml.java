7de763b48c407f0f02a329fb7960e1233e807134
Ingvild Indrebø <ingvild.indrebo@nav.no>
2017-08-11 16:29:36 +0200

    legg til eiendelspørsmål

--- domain/src/main/java/no/nav/sbl/dialogarena/sendsoknad/domain/transformer/sosialhjelp/InntektFormueTilXml.java
+++ domain/src/main/java/no/nav/sbl/dialogarena/sendsoknad/domain/transformer/sosialhjelp/InntektFormueTilXml.java
@@ -1,10 +1,8 @@
 package no.nav.sbl.dialogarena.sendsoknad.domain.transformer.sosialhjelp;
 
 import no.nav.melding.domene.brukerdialog.soeknadsskjemasosialhjelp.v1.XMLBoolean;
-import no.nav.melding.domene.brukerdialog.soeknadsskjemasosialhjelp.v1.XMLFamiliesituasjon;
-import no.nav.melding.domene.brukerdialog.soeknadsskjemasosialhjelp.v1.XMLFamiliesituasjon.Sivilstatus;
 import no.nav.melding.domene.brukerdialog.soeknadsskjemasosialhjelp.v1.XMLInntektFormue;
-import no.nav.melding.domene.brukerdialog.soeknadsskjemasosialhjelp.v1.XMLSivilstatus;
+import no.nav.melding.domene.brukerdialog.soeknadsskjemasosialhjelp.v1.XMLString;
 import no.nav.sbl.dialogarena.sendsoknad.domain.WebSoknad;
 
 import java.util.function.Function;
@@ -25,30 +23,68 @@ public class InntektFormueTilXml implements Function<WebSoknad, XMLInntektFormue
                 .withKilde(BRUKER)
                 .withValue(Boolean.valueOf(webSoknad.getValueForFaktum("inntekt.soktytelser")));
 
-        Boolean mottatBostotte = Boolean.valueOf(webSoknad.getValueForFaktum("inntekt.bostotte"));
+        String mottattBostotte = webSoknad.getValueForFaktum("inntekt.bostotte");
         XMLBoolean mottarBostotte = new XMLBoolean()
                 .withKilde(BRUKER)
-                .withValue(mottatBostotte);
+                .withValue(Boolean.valueOf(mottattBostotte));
 
         XMLInntektFormue xmlInntektFormue = new XMLInntektFormue();
 
         //forvirrende på grunn av booleanradio som forventer at nei verdien er det som skal gi oppfølgingspørsmål
-        if (mottatBostotte == false) {
-            XMLBoolean husbanken = new XMLBoolean()
+        if (mottattBostotte.equals("false")) {
+            xmlInntektFormue.withHusbanken(new XMLBoolean()
                     .withValue(Boolean.valueOf(webSoknad.getValueForFaktum("inntekt.bostotte.type.husbanken")))
-                    .withKilde(BRUKER);
-            XMLBoolean kommunal = new XMLBoolean()
+                    .withKilde(BRUKER));
+            xmlInntektFormue.withKommunal(new XMLBoolean()
                     .withValue(Boolean.valueOf(webSoknad.getValueForFaktum("inntekt.bostotte.type.kommunal")))
-                    .withKilde(BRUKER);
+                    .withKilde(BRUKER));
+        }
 
-            xmlInntektFormue
-                    .withHusbanken(husbanken)
-                    .withKommunal(kommunal);
+        String eierandelerVerdi = webSoknad.getValueForFaktum("inntekt.eierandeler");
+        XMLBoolean eierandeler = new XMLBoolean().withKilde(BRUKER).withValue(Boolean.valueOf(eierandelerVerdi));
+
+        if (eierandelerVerdi.equals("false")) {
+            xmlInntektFormue.withBolig(new XMLBoolean()
+                    .withValue(Boolean.valueOf(webSoknad.getValueForFaktum(
+                            "inntekt.eierandeler.type.bolig"
+                    )))
+                    .withKilde(BRUKER));
+
+            xmlInntektFormue.withKjoretoy(new XMLBoolean()
+                    .withValue(Boolean.valueOf(webSoknad.getValueForFaktum(
+                            "inntekt.eierandeler.type.kjoretoy"
+                    )))
+                    .withKilde(BRUKER));
+
+            xmlInntektFormue.withCampingvogn(new XMLBoolean()
+                    .withValue(Boolean.valueOf(webSoknad.getValueForFaktum(
+                            "inntekt.eierandeler.type.campingvogn"
+                    )))
+                    .withKilde(BRUKER));
+
+            xmlInntektFormue.withFritidseiendom(new XMLBoolean()
+                    .withValue(Boolean.valueOf(webSoknad.getValueForFaktum(
+                            "inntekt.eierandeler.type.fritidseiendom"
+                    )))
+                    .withKilde(BRUKER));
+
+            Boolean annetVerdi = Boolean.valueOf(webSoknad.getValueForFaktum(
+                    "inntekt.eierandeler.type.annet"
+            ));
+            xmlInntektFormue.withAnnet(new XMLBoolean()
+                    .withValue(annetVerdi)
+                    .withKilde(BRUKER));
+            if(annetVerdi) {
+                xmlInntektFormue.withAnnetBeskrivelse(new XMLString()
+                        .withValue(webSoknad.getValueForFaktum("inntekt.eierandeler.type.annet.true.beskrivelse"))
+                        .withKilde(BRUKER));
+            }
         }
 
         return xmlInntektFormue
                 .withMottarYtelser(mottarYtelser)
                 .withSoktYtelser(soktYtelser)
-                .withMottarBostotte(mottarBostotte);
+                .withMottarBostotte(mottarBostotte)
+                .withEierandeler(eierandeler);
     }
 }
