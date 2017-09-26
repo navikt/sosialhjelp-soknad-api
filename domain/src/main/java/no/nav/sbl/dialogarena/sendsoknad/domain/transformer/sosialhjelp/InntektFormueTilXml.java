5c4d170973c92ec295ebd5ad6f0774fae18532f7
Mats Krüger Svensson <mats.kruger.svensson@nav.no>
2017-09-26 19:00:31 +0200

    Mapper inntekter til xml-lister. Trenger ikke sjekke på ja/nei da usynlige fakta blir fjernet før transformeren. Fikser småissues i xmlen for inntekter

--- domain/src/main/java/no/nav/sbl/dialogarena/sendsoknad/domain/transformer/sosialhjelp/InntektFormueTilXml.java
+++ domain/src/main/java/no/nav/sbl/dialogarena/sendsoknad/domain/transformer/sosialhjelp/InntektFormueTilXml.java
@@ -1,92 +1,103 @@
 package no.nav.sbl.dialogarena.sendsoknad.domain.transformer.sosialhjelp;
 
 import no.nav.melding.domene.brukerdialog.soeknadsskjemasosialhjelp.v1.XMLInntektFormue;
+import no.nav.melding.domene.brukerdialog.soeknadsskjemasosialhjelp.v1.XMLInntektFormue.*;
+import no.nav.melding.domene.brukerdialog.soeknadsskjemasosialhjelp.v1.XMLInntektFormue.XMLBankinnskudd.XMLBankinnskuddliste;
+import no.nav.melding.domene.brukerdialog.soeknadsskjemasosialhjelp.v1.XMLInntektFormue.XMLBostotte.XMLBostotteliste;
+import no.nav.melding.domene.brukerdialog.soeknadsskjemasosialhjelp.v1.XMLInntektFormue.XMLUtbetalinger.XMLUtbetalingerliste;
+import no.nav.melding.domene.brukerdialog.soeknadsskjemasosialhjelp.v1.XMLInntektFormue.XMLVerdier.XMLVerdierliste;
+import no.nav.melding.domene.brukerdialog.soeknadsskjemasosialhjelp.v1.XMLKildeString;
+import no.nav.melding.domene.brukerdialog.soeknadsskjemasosialhjelp.v1.kodeverk.XMLUtbetaling;
+import no.nav.melding.domene.brukerdialog.soeknadsskjemasosialhjelp.v1.kodeverk.XMLVerdi;
 import no.nav.sbl.dialogarena.sendsoknad.domain.WebSoknad;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
+import static no.nav.melding.domene.brukerdialog.soeknadsskjemasosialhjelp.v1.kodeverk.XMLBankinnskudd.*;
+import static no.nav.melding.domene.brukerdialog.soeknadsskjemasosialhjelp.v1.kodeverk.XMLBankinnskudd.ANNET;
+import static no.nav.melding.domene.brukerdialog.soeknadsskjemasosialhjelp.v1.kodeverk.XMLBostotte.HUSBANKEN;
+import static no.nav.melding.domene.brukerdialog.soeknadsskjemasosialhjelp.v1.kodeverk.XMLBostotte.KOMMUNAL;
+import static no.nav.melding.domene.brukerdialog.soeknadsskjemasosialhjelp.v1.kodeverk.XMLUtbetaling.FORSIKRINGSUTBETALING;
+import static no.nav.melding.domene.brukerdialog.soeknadsskjemasosialhjelp.v1.kodeverk.XMLUtbetaling.SALG;
+import static no.nav.melding.domene.brukerdialog.soeknadsskjemasosialhjelp.v1.kodeverk.XMLUtbetaling.UTBYTTE;
+import static no.nav.melding.domene.brukerdialog.soeknadsskjemasosialhjelp.v1.kodeverk.XMLVerdi.*;
+import static no.nav.sbl.dialogarena.sendsoknad.domain.transformer.sosialhjelp.SoknadSosialhjelpUtils.tilBoolean;
 import static no.nav.sbl.dialogarena.sendsoknad.domain.transformer.sosialhjelp.SoknadSosialhjelpUtils.tilString;
 
 
 public class InntektFormueTilXml implements Function<WebSoknad, XMLInntektFormue> {
 
+    private static final Map<String, String> BOSTOTTE_MAP = lagBostotteMap();
+    private static final Map<String, String> VERDI_MAP = lagVerdiMap();
+    private static final Map<String, String> BANKINNSKUDD_MAP = lagBankinnskuddMap();
+    private static final Map<String, String> UTBETALINGER_MAP = lagUtbetalingerMap();
+
+    private static Map<String, String> lagBostotteMap() {
+        Map<String, String> map = new HashMap<>();
+        map.put("inntekt.bostotte.true.type.husbanken", HUSBANKEN.value());
+        map.put("inntekt.bostotte.true.type.kommunal", KOMMUNAL.value());
+        return map;
+    }
+
+    private static Map<String, String> lagVerdiMap() {
+        Map<String, String> map = new HashMap<>();
+        map.put("inntekt.eierandeler.true.type.bolig", BOLIG.value());
+        map.put("inntekt.eierandeler.true.type.kjoretoy", KJORETOY.value());
+        map.put("inntekt.eierandeler.true.type.campingvogn", CAMPINGVOGN.value());
+        map.put("inntekt.eierandeler.true.type.fritidseiendom", FRITIDSEIENDOM.value());
+        map.put("inntekt.eierandeler.true.type.annet", XMLVerdi.ANNET.value());
+        return map;
+    }
+
+    private static Map<String, String> lagBankinnskuddMap() {
+        Map<String, String> map = new HashMap<>();
+        map.put("inntekt.bankinnskudd.true.type.sparekonto", SPAREKONTO.value());
+        map.put("inntekt.bankinnskudd.true.type.brukskonto", BRUKSKONTO.value());
+        map.put("inntekt.bankinnskudd.true.type.livsforsikring", LIVSFORSIKRING.value());
+        map.put("inntekt.bankinnskudd.true.type.aksjer", AKSJER.value());
+        map.put("inntekt.bankinnskudd.true.type.annet", ANNET.value());
+        return map;
+    }
+
+    private static Map<String, String> lagUtbetalingerMap() {
+        Map<String, String> map = new HashMap<>();
+        map.put("inntekt.inntekter.true.type.utbytte", UTBYTTE.value());
+        map.put("inntekt.inntekter.true.type.salg", SALG.value());
+        map.put("inntekt.inntekter.true.type.forsikringsutbetalinger", FORSIKRINGSUTBETALING.value());
+        map.put("inntekt.inntekter.true.type.annet", XMLUtbetaling.ANNET.value());
+        return map;
+    }
+
+    private List<XMLKildeString> lagListeFraFakta(WebSoknad webSoknad, Map<String, String> keyTilEnum) {
+        return keyTilEnum.entrySet().stream()
+                .map((e) -> webSoknad.getValueForFaktum(e.getKey()).equals("true") ? e.getValue() : null)
+                .filter(Objects::nonNull)
+                .map(SoknadSosialhjelpUtils::tilString)
+                .collect(Collectors.toList());
+    }
+
     @Override
     public XMLInntektFormue apply(WebSoknad webSoknad) {
-        XMLInntektFormue xmlInntektFormue = new XMLInntektFormue();
-/*
-        String mottattBostotte = webSoknad.getValueForFaktum("inntekt.bostotte");
-
-        xmlInntektFormue
-                .withMottarBostotte(SoknadSosialhjelpUtils.tilBoolean(Boolean.valueOf(mottattBostotte)))
-                .withInntekter(SoknadSosialhjelpUtils.tilBoolean(webSoknad, "inntekt.mottarytelser"))
-                .withSoktYtelser(SoknadSosialhjelpUtils.tilBoolean(webSoknad, "inntekt.soktytelser"));
-
-
-        //forvirrende på grunn av booleanradio som forventer at nei verdien er det som skal gi oppfølgingspørsmål
-        if (mottattBostotte.equals("false")) {
-            xmlInntektFormue
-                    .withHusbanken(SoknadSosialhjelpUtils.tilBoolean(webSoknad, "inntekt.bostotte.type.husbanken"))
-                    .withKommunal(SoknadSosialhjelpUtils.tilBoolean(webSoknad, "inntekt.bostotte.type.kommunal"));
-        }
-
-        String eierandelerVerdi = webSoknad.getValueForFaktum("inntekt.eierandeler");
-        xmlInntektFormue.withEierandeler(SoknadSosialhjelpUtils.tilBoolean(Boolean.valueOf(eierandelerVerdi)));
-
-        if (eierandelerVerdi.equals("false")) {
-            xmlInntektFormue
-                    .withBolig(SoknadSosialhjelpUtils.tilBoolean(webSoknad, "inntekt.eierandeler.false.type.bolig"))
-                    .withKjoretoy(SoknadSosialhjelpUtils.tilBoolean(webSoknad, "inntekt.eierandeler.false.type.kjoretoy"))
-                    .withCampingvogn(SoknadSosialhjelpUtils.tilBoolean(webSoknad, "inntekt.eierandeler.false.type.campingvogn"))
-                    .withFritidseiendom(SoknadSosialhjelpUtils.tilBoolean(webSoknad, "inntekt.eierandeler.false.type.fritidseiendom"));
-
-            Boolean annetVerdi = Boolean.valueOf(webSoknad.getValueForFaktum("inntekt.eierandeler.false.type.annet"));
-            xmlInntektFormue.withAnnet(SoknadSosialhjelpUtils.tilBoolean(annetVerdi));
-
-            if (annetVerdi) {
-                xmlInntektFormue.withAnnetBeskrivelse(tilXMLKildeString(webSoknad, "inntekt.eierandeler.false.type.annet.true.beskrivelse"));
-            }
-        }
-
-        String bankinnskuddVerdi = webSoknad.getValueForFaktum("inntekt.bankinnskudd");
-        xmlInntektFormue.withBankinnskudd(SoknadSosialhjelpUtils.tilBoolean(Boolean.valueOf(bankinnskuddVerdi)));
-
-        if (bankinnskuddVerdi.equals("false")) {
-            xmlInntektFormue
-                    .withSparekonto(SoknadSosialhjelpUtils.tilBoolean(webSoknad, "inntekt.bankinnskudd.false.type.sparekonto"))
-                    .withBrukskonto(SoknadSosialhjelpUtils.tilBoolean(webSoknad, "inntekt.bankinnskudd.false.type.brukskonto"))
-                    .withLivsforsikring(SoknadSosialhjelpUtils.tilBoolean(webSoknad, "inntekt.bankinnskudd.false.type.livsforsikring"))
-                    .withAksjer(SoknadSosialhjelpUtils.tilBoolean(webSoknad, "inntekt.bankinnskudd.false.type.aksjer"));
-
-            Boolean annetVerdi = Boolean.valueOf(webSoknad.getValueForFaktum(
-                    "inntekt.bankinnskudd.false.type.annet"
-            ));
-
-            xmlInntektFormue.withBankinnskuddAnnet(SoknadSosialhjelpUtils.tilBoolean(annetVerdi));
-
-            if (annetVerdi) {
-                xmlInntektFormue.withBankinnskuddAnnetBeskrivelse(tilXMLKildeString(webSoknad, "inntekt.bankinnskudd.false.type.annet.true.beskrivelse"));
-            }
-        }
-
-        String inntekterVerdi = webSoknad.getValueForFaktum("inntekt.inntekter");
-        xmlInntektFormue.withInntekter(SoknadSosialhjelpUtils.tilBoolean(Boolean.valueOf(inntekterVerdi)));
-
-        if (inntekterVerdi.equals("false")) {
-            xmlInntektFormue.withUtbytte(SoknadSosialhjelpUtils.tilBoolean(webSoknad, "inntekt.inntekter.false.type.utbytte"))
-                    .withSalg(SoknadSosialhjelpUtils.tilBoolean(webSoknad, "inntekt.inntekter.false.type.salg"))
-                    .withLeieinntekter(SoknadSosialhjelpUtils.tilBoolean(webSoknad, "inntekt.inntekter.false.type.leieinntekter"))
-                    .withForsikringsutbetaling(SoknadSosialhjelpUtils.tilBoolean(webSoknad, "inntekt.inntekter.false.type.forsikringsutbetalinger"));
-
-            Boolean annetVerdi = Boolean.valueOf(webSoknad.getValueForFaktum(
-                    "inntekt.inntekter.false.type.annet"
-            ));
-            xmlInntektFormue.withAndreInntekter(SoknadSosialhjelpUtils.tilBoolean(annetVerdi));
-
-            if (annetVerdi) {
-                xmlInntektFormue.withAndreInntekterBeskrivelse(tilXMLKildeString(webSoknad, "inntekt.inntekter.false.type.annet.true.beskrivelse"));
-            }
-        }
-*/
-        return xmlInntektFormue;
+        return new XMLInntektFormue()
+                .withNavYtelser(new XMLNavYtelser()
+                        .withMottarYtelser(tilBoolean(webSoknad, "inntekt.bostotte"))
+                        .withIkkeFerdigbehandledeYtelser(tilBoolean(webSoknad, "inntekt.soktytelser")))
+                .withBostotte(new XMLBostotte()
+                        // TODO sette true/false siden listen ikke er påkrevd?
+                        .withBostotteliste(new XMLBostotteliste(lagListeFraFakta(webSoknad, BOSTOTTE_MAP))))
+                .withVerdier(new XMLVerdier()
+                        .withVerdierliste(new XMLVerdierliste(lagListeFraFakta(webSoknad, VERDI_MAP)))
+                        .withAnnetBeskrivelse(tilString(webSoknad, "inntekt.eierandeler.true.type.annet.true.beskrivelse")))
+                .withBankinnskudd(new XMLBankinnskudd()
+                        .withBankinnskuddliste(new XMLBankinnskuddliste(lagListeFraFakta(webSoknad, BANKINNSKUDD_MAP)))
+                        .withAnnetBeskrivelse(tilString(webSoknad, "inntekt.bankinnskudd.true.type.annet.true.beskrivelse")))
+                .withUtbetalinger(new XMLUtbetalinger()
+                        .withUtbetalingerliste(new XMLUtbetalingerliste(lagListeFraFakta(webSoknad, UTBETALINGER_MAP)))
+                        .withAnnetBeskrivelse(tilString(webSoknad, "inntekt.inntekter.true.type.annet.true.beskrivelse")));
     }
 }
