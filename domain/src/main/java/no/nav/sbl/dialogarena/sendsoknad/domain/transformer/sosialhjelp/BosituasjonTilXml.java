28f82ee06c465cbff59d75a664b5d0271272242e
Mats Krüger Svensson <mats.kruger.svensson@nav.no>
2017-09-29 15:16:06 +0200

    Bosituasjon setter nå botyper og antall personer i husstand. Frontend bruker kodeverksverdier så kan settes direkte. Oppdatert faktum-xml til å matche ny frontend, og dratt inn ny xsd.

--- domain/src/main/java/no/nav/sbl/dialogarena/sendsoknad/domain/transformer/sosialhjelp/BosituasjonTilXml.java
+++ domain/src/main/java/no/nav/sbl/dialogarena/sendsoknad/domain/transformer/sosialhjelp/BosituasjonTilXml.java
@@ -1,45 +1,21 @@
 package no.nav.sbl.dialogarena.sendsoknad.domain.transformer.sosialhjelp;
 
 import no.nav.melding.domene.brukerdialog.soeknadsskjemasosialhjelp.v1.XMLBosituasjon;
-import no.nav.melding.domene.brukerdialog.soeknadsskjemasosialhjelp.v1.kodeverk.XMLBotype;
 import no.nav.sbl.dialogarena.sendsoknad.domain.WebSoknad;
 
 import java.util.function.Function;
 
-import static no.nav.sbl.dialogarena.sendsoknad.domain.transformer.sosialhjelp.SoknadSosialhjelpUtils.*;
+import static no.nav.sbl.dialogarena.sendsoknad.domain.transformer.sosialhjelp.SoknadSosialhjelpUtils.tilInteger;
+import static no.nav.sbl.dialogarena.sendsoknad.domain.transformer.sosialhjelp.SoknadSosialhjelpUtils.tilString;
 
 
 public class BosituasjonTilXml implements Function<WebSoknad, XMLBosituasjon> {
 
     @Override
     public XMLBosituasjon apply(WebSoknad webSoknad) {
-
-        XMLBosituasjon xmlBosituasjon = new XMLBosituasjon();
-
-        String botypeVerdi = webSoknad.getValueForFaktum("bosituasjon");
-        switch (botypeVerdi) {
-            case "eier":
-                xmlBosituasjon.withBoType(tilString(XMLBotype.EIER.value()));
-            case "leierpivat":
-                xmlBosituasjon.withBoType(tilString(XMLBotype.LEIER.value()));
-            case "leierkommunalt":
-                xmlBosituasjon.withBoType(tilString(XMLBotype.KOMMUNAL.value()));
-                case "ingen":
-                xmlBosituasjon.withBoType(tilString(XMLBotype.INGEN.value()));
-            case "annet":
-                xmlBosituasjon.withBoType(tilString(XMLBotype.ANNET.value()));
-        }
-
-
-        if(botypeVerdi.equals("annet")) {
-//            xmlBosituasjon.withInstitusjon(SoknadSosialhjelpUtils.tilBoolean(webSoknad, "bosituasjon.annen.botype.institusjon"));
-//            xmlBosituasjon.withFengsel(SoknadSosialhjelpUtils.tilBoolean(webSoknad, "bosituasjon.annen.botype.fengsel"));
-//            xmlBosituasjon.withForeldre(SoknadSosialhjelpUtils.tilBoolean(webSoknad, "bosituasjon.annen.botype.foreldre"));
-//            xmlBosituasjon.withKrisesenter(SoknadSosialhjelpUtils.tilBoolean(webSoknad, "bosituasjon.annen.botype.krisesenter"));
-//            xmlBosituasjon.withVenner(SoknadSosialhjelpUtils.tilBoolean(webSoknad, "bosituasjon.annen.botype.venner"));
-//            xmlBosituasjon.withFamilie(SoknadSosialhjelpUtils.tilBoolean(webSoknad, "bosituasjon.annen.botype.familie"));
-        }
-
-        return xmlBosituasjon;
+        return new XMLBosituasjon()
+                .withBoType(tilString(webSoknad, "bosituasjon"))
+                .withAnnenSituasjon(tilString(webSoknad, "bosituasjon.annet.botype"))
+                .withAntallPersoner(tilInteger(webSoknad, "bosituasjon.antallpersoner"));
     }
 }
