ece2dfb0fcc850302bbb61bc886cb5afbece2f47
Mats Krüger Svensson <mats.kruger.svensson@nav.no>
2017-10-04 16:48:34 +0200

    XML personalia digisos

--- domain/src/main/java/no/nav/sbl/dialogarena/sendsoknad/domain/transformer/sosialhjelp/BrukerTilXml.java
+++ domain/src/main/java/no/nav/sbl/dialogarena/sendsoknad/domain/transformer/sosialhjelp/BrukerTilXml.java
@@ -7,7 +7,8 @@ import java.util.Map;
 import java.util.function.Function;
 
 import static no.nav.melding.domene.brukerdialog.soeknadsskjemasosialhjelp.v1.XMLKilde.SYSTEM;
-import static no.nav.sbl.dialogarena.sendsoknad.domain.transformer.sosialhjelp.SoknadSosialhjelpUtils.*;
+import static no.nav.sbl.dialogarena.sendsoknad.domain.personalia.Personalia.*;
+import static no.nav.sbl.dialogarena.sendsoknad.domain.transformer.sosialhjelp.SoknadSosialhjelpUtils.tilString;
 
 
 public class BrukerTilXml implements Function<WebSoknad, XMLPersonalia> {
@@ -19,9 +20,9 @@ public class BrukerTilXml implements Function<WebSoknad, XMLPersonalia> {
 
         return new XMLPersonalia()
                 .withPersonIdentifikator(tilString(personnummer, SYSTEM))
-//                .withNavn(tilXMLKildeString(personaliaProperties.get("navn"), SYSTEM))
-                .withStatsborgerskap(tilString(personaliaProperties.get("statsborgerskap"), SYSTEM));
-//                .withEpostAdresse(tilXMLKildeString(personaliaProperties.get("epost") == null ? "ingen epost" : personaliaProperties.get("epost"), SYSTEM))
-//                .withAdresse(tilXMLKildeString(personaliaProperties.get("gjeldendeAdresse"), SYSTEM))
+                .withStatsborgerskap(tilString(personaliaProperties.get(STATSBORGERSKAP_KEY), SYSTEM))
+                .withFornavn(tilString(personaliaProperties.get(FORNAVN_KEY), SYSTEM))
+                .withMellomnavn(tilString(personaliaProperties.get(MELLOMNAVN_KEY), SYSTEM))
+                .withEtternavn(tilString(personaliaProperties.get(ETTERNAVN_KEY), SYSTEM));
     }
 }
