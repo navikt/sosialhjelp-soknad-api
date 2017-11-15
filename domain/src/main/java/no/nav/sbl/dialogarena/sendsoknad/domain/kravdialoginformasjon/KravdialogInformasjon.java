76f1ba3b314008d86776ffc94fed483489690018
Nemanja Aksic <nemanja.aksic@nav.no>
2017-11-15 13:28:16 +0100

    [PK-42720] Legger til getSkjemaVersjon som default på 1 for alle skjema i utgangspunktet.

--- domain/src/main/java/no/nav/sbl/dialogarena/sendsoknad/domain/kravdialoginformasjon/KravdialogInformasjon.java
+++ domain/src/main/java/no/nav/sbl/dialogarena/sendsoknad/domain/kravdialoginformasjon/KravdialogInformasjon.java
@@ -1,10 +1,8 @@
 package no.nav.sbl.dialogarena.sendsoknad.domain.kravdialoginformasjon;
 
-import no.nav.sbl.dialogarena.sendsoknad.domain.AlternativRepresentasjon;
 import no.nav.sbl.dialogarena.sendsoknad.domain.Steg;
 import no.nav.sbl.dialogarena.sendsoknad.domain.WebSoknad;
 import no.nav.sbl.dialogarena.sendsoknad.domain.transformer.AlternativRepresentasjonTransformer;
-import org.apache.commons.collections15.Transformer;
 import org.springframework.context.MessageSource;
 
 import java.util.ArrayList;
@@ -29,11 +27,11 @@ public interface KravdialogInformasjon {
 
     String getFortsettSoknadUrlKey();
 
-    //TODO: Legge på getSoknadStrukturVersjon f.eks med implementasjon på hardkodet versjon 1 for alle soknader.
-
     List<String> getSoknadBolker(WebSoknad soknad);
 
     String getStrukturFilnavn();
+
+    static int getSkjemaVersjon() { return 1; }
 
     List<String> getSkjemanummer();
 
