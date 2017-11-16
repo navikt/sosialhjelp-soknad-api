466484d8989fb9afa42a2e447d285ca614840b10
Nemanja Aksic <nemanja.aksic@nav.no>
2017-11-16 13:13:13 +0100

    [PK-42720] Oppretter en håndterer for alle migrasjonene, som vil opprettholde en liste med migrasjoner pr skjemanummer. Legger til en FakeMigrasjon som eksempel på hvordan en migrasjon kan se ut på enklest mulig vis.

--- business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/service/migrasjon/Migrasjon.java
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/service/migrasjon/Migrasjon.java
@@ -5,8 +5,14 @@ import no.nav.sbl.dialogarena.sendsoknad.domain.WebSoknad;
 public abstract class Migrasjon {
     static int minVersjon;
     static String migrasjonSkjemanummer;
-    static boolean skalMigrere(String skjemanummer, int versjon) {
+
+    public Migrasjon(int versjon, String skjemanummer) {
+        minVersjon = versjon;
+        migrasjonSkjemanummer = skjemanummer;
+    }
+
+    public static boolean skalMigrere(String skjemanummer, int versjon) {
         return migrasjonSkjemanummer.equalsIgnoreCase(skjemanummer) && minVersjon-versjon == 1;
     }
-    abstract WebSoknad migreer(WebSoknad soknad, int versjon);
+    public abstract WebSoknad migreer(WebSoknad soknad, int versjon);
 }
