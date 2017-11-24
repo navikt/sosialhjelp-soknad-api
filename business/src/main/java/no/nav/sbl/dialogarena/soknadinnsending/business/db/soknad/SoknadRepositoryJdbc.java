741e61297c3974a71885c0eb1d56a5a821d3d604
Torbjørn Tessem <torbjorn.tessem@nav.no>
2017-11-24 13:27:12 +0100

    [PK-42720] Ny hendelsetype - SOKNAD_SLETTET

--- business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/db/soknad/SoknadRepositoryJdbc.java
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/db/soknad/SoknadRepositoryJdbc.java
@@ -79,7 +79,11 @@ public class SoknadRepositoryJdbc extends NamedParameterJdbcDaoSupport implement
                         soknad.getJournalforendeEnhet());
     }
 
-    public void insertHendelse(String behandlingsid, String hendelse_type, int versjon, String skjemanummer){
+    public void insertHendelse(String behandlingsid, String hendelse_type){
+        insertHendelse(behandlingsid,hendelse_type,0,null);
+    }
+
+    private void insertHendelse(String behandlingsid, String hendelse_type, int versjon, String skjemanummer){
         getJdbcTemplate()
                 .update("insert into hendelse (BEHANDLINGSID, HENDELSE_TYPE, HENDELSE_TIDSPUNKT, VERSJON, SKJEMANUMMER)" +
                             " values (?,?,CURRENT_TIMESTAMP,?,?)",
