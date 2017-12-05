508b5874727d833305a1c5d25065d1d714986e59
Nemanja Aksic <nemanja.aksic@nav.no>
2017-12-05 09:01:15 +0100

    [PK-42720] Mocker Soknadrepository for å fikse test som feiler

--- business/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/business/service/MigrasjonHandtererTest.java
+++ business/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/business/service/MigrasjonHandtererTest.java
@@ -2,13 +2,17 @@ package no.nav.sbl.dialogarena.soknadinnsending.business.service;
 
 import no.nav.sbl.dialogarena.sendsoknad.domain.DelstegStatus;
 import no.nav.sbl.dialogarena.sendsoknad.domain.WebSoknad;
+import no.nav.sbl.dialogarena.soknadinnsending.business.db.soknad.SoknadRepository;
 import no.nav.sbl.dialogarena.soknadinnsending.business.service.migrasjon.FakeMigrasjon;
 import no.nav.sbl.dialogarena.soknadinnsending.business.service.migrasjon.Migrasjon;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import javax.inject.Inject;
+import javax.inject.Named;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,6 +21,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(MockitoJUnitRunner.class)
 public class MigrasjonHandtererTest {
 
+    @Mock(name = "lokalDb")
+    private SoknadRepository lokalDb;
+
     MigrasjonHandterer handterer;
     WebSoknad innsendtSoknad;
 
@@ -24,7 +31,7 @@ public class MigrasjonHandtererTest {
     public void setup() {
         List<Migrasjon> testMigrasjoner = new ArrayList<>();
         testMigrasjoner.add(new FakeMigrasjon());
-        handterer = new MigrasjonHandterer(testMigrasjoner);
+        handterer = new MigrasjonHandterer(testMigrasjoner, lokalDb);
         innsendtSoknad = new WebSoknad().medId(1L).medskjemaNummer("NAV 11-13.05").medVersjon(1);
     }
 
