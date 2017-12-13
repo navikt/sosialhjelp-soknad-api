43de6e0e1c0691d44004c24c5333a43b281113e0
Mats Krüger Svensson <mats.kruger.svensson@nav.no>
2017-12-13 15:42:21 +0100

    Flyttet kode fra ressurs til service

--- web/src/main/java/no/nav/sbl/dialogarena/rest/ressurser/SosialhjelpVedleggRessurs.java
+++ web/src/main/java/no/nav/sbl/dialogarena/rest/ressurser/SosialhjelpVedleggRessurs.java
@@ -1,41 +1,29 @@
 package no.nav.sbl.dialogarena.rest.ressurser;
 
 import no.nav.metrics.aspects.Timed;
-import no.nav.sbl.dialogarena.sendsoknad.domain.Faktum;
 import no.nav.sbl.dialogarena.sendsoknad.domain.Vedlegg;
 import no.nav.sbl.dialogarena.sendsoknad.domain.WebSoknad;
 import no.nav.sbl.dialogarena.sendsoknad.domain.exception.OpplastingException;
-import no.nav.sbl.dialogarena.sendsoknad.domain.oppsett.FaktumStruktur;
 import no.nav.sbl.dialogarena.sikkerhet.SjekkTilgangTilSoknad;
 import no.nav.sbl.dialogarena.sikkerhet.SjekkTilgangTilSoknad.Type;
-import no.nav.sbl.dialogarena.soknadinnsending.business.db.soknad.SoknadRepository;
 import no.nav.sbl.dialogarena.soknadinnsending.business.service.VedleggOriginalFilerService;
 import no.nav.sbl.dialogarena.soknadinnsending.business.service.VedleggService;
-import no.nav.sbl.dialogarena.soknadinnsending.business.service.soknadservice.SoknadService;
 import org.apache.commons.io.IOUtils;
 import org.glassfish.jersey.media.multipart.FormDataBodyPart;
 import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.springframework.stereotype.Controller;
 
 import javax.inject.Inject;
-import javax.inject.Named;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.stream.Collectors;
 
-import static java.util.Collections.sort;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static javax.ws.rs.core.MediaType.MULTIPART_FORM_DATA;
 import static no.nav.sbl.dialogarena.rest.ressurser.VedleggRessurs.MAKS_TOTAL_FILSTORRELSE;
-import static no.nav.sbl.dialogarena.sendsoknad.domain.Faktum.FaktumType.BRUKERREGISTRERT;
-import static no.nav.sbl.dialogarena.sendsoknad.domain.oppsett.FaktumStruktur.sammenlignEtterDependOn;
 import static no.nav.sbl.dialogarena.sikkerhet.SjekkTilgangTilSoknad.Type.Vedlegg;
 
 
@@ -51,59 +39,11 @@ public class SosialhjelpVedleggRessurs {
     @Inject
     private VedleggOriginalFilerService vedleggOriginalFilerService;
 
-    @Inject
-    private SoknadService soknadService;
-
-
-    @Inject
-    @Named("soknadInnsendingRepository")
-    private SoknadRepository lokalDb;
-
     @GET
     @Path("/oppdaterVedlegg/{behandlingsId}")
     @SjekkTilgangTilSoknad
     public WebSoknad oppdaterVedleggFaktum(@PathParam("behandlingsId") String behandlingsId) {
-        WebSoknad soknad = soknadService.hentSoknad(behandlingsId, true, true);
-
-        List<FaktumStruktur> faktaStruktur = soknadService.hentSoknadStruktur(soknad.getskjemaNummer()).getFakta();
-        sort(faktaStruktur, sammenlignEtterDependOn());
-
-        List<FaktumStruktur> opplysninger = faktaStruktur.stream()
-                .filter(struktur -> struktur.getId().startsWith("opplysninger."))
-                .filter(struktur -> soknad.getFaktaMedKey(struktur.getId()).isEmpty())
-                .collect(Collectors.toList());
-
-        Iterator<Long> faktumIder = lokalDb.hentLedigeFaktumIder(opplysninger.size()).iterator();
-
-        List<Faktum> nyeFakta = new ArrayList<>();
-
-        for (FaktumStruktur struktur : opplysninger) {
-            Long parentFaktumId = null;
-
-            if (struktur.getDependOn() != null) {
-                if (soknad.getFaktumMedKey(struktur.getDependOn().getId()) != null) {
-                    parentFaktumId = soknad.getFaktumMedKey(struktur.getDependOn().getId()).getFaktumId();
-                } else {
-                    parentFaktumId = nyeFakta.stream()
-                            .filter(f -> f.getKey().equals(struktur.getDependOn().getId()))
-                            .findFirst()
-                            .map(Faktum::getFaktumId)
-                            .orElse(null);
-                }
-            }
-
-            nyeFakta.add(new Faktum()
-                    .medFaktumId(faktumIder.next())
-                    .medParrentFaktumId(parentFaktumId)
-                    .medKey(struktur.getId())
-                    .medType(BRUKERREGISTRERT)
-                    .medSoknadId(soknad.getSoknadId()));
-        }
-
-        lokalDb.batchOpprettTommeFakta(nyeFakta);
-
-        WebSoknad soknadOppdatert = soknadService.hentSoknad(behandlingsId, true, true);
-        return soknadOppdatert;
+        return vedleggOriginalFilerService.oppdaterVedleggOgBelopFaktum(behandlingsId);
     }
 
     @POST
