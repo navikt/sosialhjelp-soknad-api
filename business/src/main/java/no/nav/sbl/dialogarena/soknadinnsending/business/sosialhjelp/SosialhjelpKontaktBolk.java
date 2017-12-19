5181bc531a77948ee6fd8e92dcab765dbff8f258
Stian Grenborgen <grenborgen.stian@nav.no>
2017-12-19 12:52:17 +0100

    [DIGISOS-200] Legger til håndtering av adresse fra TPS.

--- business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/sosialhjelp/SosialhjelpKontaktBolk.java
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/sosialhjelp/SosialhjelpKontaktBolk.java
@@ -7,6 +7,10 @@ import javax.inject.Inject;
 
 import org.springframework.stereotype.Service;
 
+import no.nav.sbl.dialogarena.sendsoknad.domain.Adresse;
+import no.nav.sbl.dialogarena.sendsoknad.domain.Adresse.Gateadresse;
+import no.nav.sbl.dialogarena.sendsoknad.domain.Adresse.MatrikkelAdresse;
+import no.nav.sbl.dialogarena.sendsoknad.domain.Adresse.StrukturertAdresse;
 import no.nav.sbl.dialogarena.sendsoknad.domain.Faktum;
 import no.nav.sbl.dialogarena.sendsoknad.domain.personalia.Personalia;
 import no.nav.sbl.dialogarena.soknadinnsending.business.service.BolkService;
@@ -37,7 +41,8 @@ public class SosialhjelpKontaktBolk implements BolkService {
     private List<Faktum> genererPersonaliaFaktum(Long soknadId, Personalia personalia) {
         return Arrays.asList(
                 new Faktum().medSoknadId(soknadId).medKey("kontakt.system.kontonummer").medValue(norskKontonummer(personalia)),
-                new Faktum().medSoknadId(soknadId).medKey("kontakt.system.telefon").medValue(personalia.getMobiltelefonnummer())
+                new Faktum().medSoknadId(soknadId).medKey("kontakt.system.telefon").medValue(personalia.getMobiltelefonnummer()),
+                genererAdresseFaktum(soknadId, personalia)
         );
     }
 
@@ -47,5 +52,77 @@ public class SosialhjelpKontaktBolk implements BolkService {
         } else {
             return personalia.getKontonummer();
         }
+    }
+
+    private Faktum genererAdresseFaktum(Long soknadId, Personalia personalia) {
+        final Faktum adresseFaktum = new Faktum().medSoknadId(soknadId).medKey("kontakt.system.adresse");
+        final Adresse gjeldendeAdresse = personalia.getGjeldendeAdresse();
+        
+        if (gjeldendeAdresse == null || isUtenlandskAdresse(gjeldendeAdresse)) {
+            return adresseFaktum;
+        }
+        
+        final StrukturertAdresse strukturertAdresse = gjeldendeAdresse.getStrukturertAdresse();
+        
+        if (strukturertAdresse == null) {
+            populerUstrukturertAdresse(adresseFaktum, gjeldendeAdresse);
+        } else {
+            populerStrukturertAdresse(adresseFaktum, strukturertAdresse);
+        }
+        
+        return adresseFaktum;
+        
+    }
+
+    private boolean isUtenlandskAdresse(final Adresse gjeldendeAdresse) {
+        return gjeldendeAdresse.getLandkode() != null && !gjeldendeAdresse.getLandkode().equals("NOR");
+    }
+
+    private void populerUstrukturertAdresse(Faktum adresseFaktum, Adresse adresse) {
+        adresseFaktum
+                .medSystemProperty("adresse", adresse.getAdresse())
+                .medSystemProperty("type", "ustrukturert");
+    }
+
+    private void populerStrukturertAdresse(final Faktum adresseFaktum, final StrukturertAdresse adresse) {
+        adresseFaktum
+                .medSystemProperty("type", adresse.type)
+                .medSystemProperty("kommunenummer", adresse.kommunenummer)
+                .medSystemProperty("bolignummer", adresse.bolignummer)
+                .medSystemProperty("postnummer", adresse.postnummer)
+                .medSystemProperty("poststed", adresse.poststed)
+                ;
+        
+        if (adresse instanceof Gateadresse) {
+            populerGateadresse(adresseFaktum, adresse);
+        } else if (adresse instanceof MatrikkelAdresse) {
+            populerMatrikkeladresse(adresseFaktum, adresse);
+        } else {
+            throw new RuntimeException("Ukjent adressetype: " + adresse.getClass().getName());
+        }
+    }
+
+    private void populerGateadresse(final Faktum adresseFaktum, final StrukturertAdresse adresse) {
+        final Gateadresse gateadresse = (Gateadresse) adresse;
+        adresseFaktum.medSystemProperty("gatenavn", gateadresse.gatenavn);
+        adresseFaktum.medSystemProperty("husnummer", gateadresse.husnummer);
+        
+        /* 
+         * Kombinert gatenavn og husnummer etter ønske fra interaksjonsdesigner.
+         * Vises kun til bruker der adresse kan overstyres. Brukes ikke i oppsummering.
+         */
+        adresseFaktum.medSystemProperty("adresse", gateadresse.gatenavn + " " + gateadresse.husnummer); 
+    }
+    
+    private void populerMatrikkeladresse(final Faktum adresseFaktum, final StrukturertAdresse adresse) {
+        final MatrikkelAdresse matrikkeladresse = (MatrikkelAdresse) adresse;
+        adresseFaktum
+                .medSystemProperty("eiendomsnavn", matrikkeladresse.eiendomsnavn)
+                .medSystemProperty("gaardsnummer", matrikkeladresse.gaardsnummer)
+                .medSystemProperty("bruksnummer", matrikkeladresse.bruksnummer)
+                .medSystemProperty("festenummer", matrikkeladresse.festenummer)
+                .medSystemProperty("seksjonsnummer", matrikkeladresse.seksjonsnummer)
+                .medSystemProperty("undernummer", matrikkeladresse.undernummer)
+                ;
     }
 }
