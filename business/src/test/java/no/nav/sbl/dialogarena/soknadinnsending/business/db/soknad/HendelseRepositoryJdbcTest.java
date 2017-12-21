17ce4e887803e5d8f66eaa32c591c59e31aa7b62
Tine Kleivane <tine.kleivane@nav.no>
2017-12-21 09:30:09 +0100

    KAIZEN Tester på avbrutte hendelsestyper

--- business/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/business/db/soknad/HendelseRepositoryJdbcTest.java
+++ business/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/business/db/soknad/HendelseRepositoryJdbcTest.java
@@ -1,6 +1,7 @@
 package no.nav.sbl.dialogarena.soknadinnsending.business.db.soknad;
 
 import no.digipost.time.ControllableClock;
+import no.nav.sbl.dialogarena.sendsoknad.domain.HendelseType;
 import no.nav.sbl.dialogarena.sendsoknad.domain.WebSoknad;
 import no.nav.sbl.dialogarena.soknadinnsending.business.db.DbTestConfig;
 import org.junit.Before;
@@ -37,7 +38,7 @@ public class HendelseRepositoryJdbcTest {
     public void skalHenteForGammelUnderArbeidSak() {
         controllableClock.set(LocalDateTime.now().minusDays(56));
 
-        hendelseRepository.registrerOpprettetHendelse(new WebSoknad().medskjemaNummer("TEST_1").medBehandlingId("1").medVersjon(1));
+        hendelseRepository.registrerOpprettetHendelse(soknad());
 
         assertThat(hendelseRepository.hentSoknaderUnderArbeidEldreEnn(50)).hasSize(1);
     }
@@ -45,10 +46,55 @@ public class HendelseRepositoryJdbcTest {
     @Test
     public void skalIkkeHenteNyereUnderArbeidSak() {
         controllableClock.set(LocalDateTime.now().minusDays(1));
-        hendelseRepository.registrerOpprettetHendelse(new WebSoknad().medskjemaNummer("TEST_1").medBehandlingId("1").medVersjon(1));
+        hendelseRepository.registrerOpprettetHendelse(soknad());
 
         assertThat(hendelseRepository.hentSoknaderUnderArbeidEldreEnn(50)).isEmpty();
+    }
 
+    @Test
+    public void skalIkkeHenteSakHvorSisteHendelseErAvbruttAvBruker() {
+        controllableClock.set(LocalDateTime.now().minusDays(56));
+        hendelseRepository.registrerOpprettetHendelse(soknad().medBehandlingId("1"));
+        hendelseRepository.registrerHendelse(soknad().medBehandlingId("1"), HendelseType.AVBRUTT_AV_BRUKER);
+
+        controllableClock.set(LocalDateTime.now().minusDays(1));
+        hendelseRepository.registrerOpprettetHendelse(soknad().medBehandlingId("2"));
+        hendelseRepository.registrerHendelse(soknad().medBehandlingId("2"), HendelseType.AVBRUTT_AV_BRUKER);
+
+        assertThat(hendelseRepository.hentSoknaderUnderArbeidEldreEnn(50)).isEmpty();
+    }
+
+    @Test
+    public void skalIkkeHenteSakHvorSisteHendelseErAvbruttAutomatisk() {
+        controllableClock.set(LocalDateTime.now().minusDays(56));
+        hendelseRepository.registrerOpprettetHendelse(soknad().medBehandlingId("1"));
+        hendelseRepository.registrerHendelse(soknad().medBehandlingId("1"), HendelseType.LAGRET_I_HENVENDELSE);
+        hendelseRepository.registrerAutomatiskAvsluttetHendelse("1");
+
+        controllableClock.set(LocalDateTime.now().minusDays(1));
+        hendelseRepository.registrerOpprettetHendelse(soknad().medBehandlingId("2"));
+        hendelseRepository.registrerHendelse(soknad().medBehandlingId("2"), HendelseType.LAGRET_I_HENVENDELSE);
+        hendelseRepository.registrerAutomatiskAvsluttetHendelse("2");
+
+        assertThat(hendelseRepository.hentSoknaderUnderArbeidEldreEnn(50)).isEmpty();
+    }
+
+    @Test
+    public void skalIkkeHenteSakHvorSisteHendelseErInnsendt() {
+        controllableClock.set(LocalDateTime.now().minusDays(56));
+        hendelseRepository.registrerOpprettetHendelse(soknad().medBehandlingId("1"));
+        hendelseRepository.registrerHendelse(soknad().medBehandlingId("1"), HendelseType.INNSENDT);
+
+        controllableClock.set(LocalDateTime.now().minusDays(1));
+        hendelseRepository.registrerOpprettetHendelse(soknad().medBehandlingId("2"));
+        hendelseRepository.registrerHendelse(soknad().medBehandlingId("2"), HendelseType.INNSENDT);
+
+        assertThat(hendelseRepository.hentSoknaderUnderArbeidEldreEnn(50)).isEmpty();
+    }
+
+
+    private WebSoknad soknad() {
+        return new WebSoknad().medskjemaNummer("NAV-01").medBehandlingId("1").medVersjon(1);
     }
 
 
