720df9435ef5c61890c09b8fbd48a46b3384b6a0
Stian Grenborgen <grenborgen.stian@nav.no>
2018-01-11 18:17:42 +0100

    [DIGISOS-216] Retter oppsett slik at bruker blir spurt om lønnsinntekter og å legge ved lønnslipp.

--- business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/arbeid/SosialhjelpArbeidsforholdBolk.java
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/arbeid/SosialhjelpArbeidsforholdBolk.java
@@ -22,6 +22,8 @@ public class SosialhjelpArbeidsforholdBolk extends ArbeidsforholdBolk {
 
     @Override
     protected void afterGenererArbeidsforhold(List<Faktum> arbeidsforholdFakta, Long soknadId) {
-        // Ignore.
+        final boolean harArbeid = !arbeidsforholdFakta.isEmpty();
+        final Faktum dinSituasjonJobb = new Faktum().medSoknadId(soknadId).medKey("dinsituasjon.jobb").medValue(Boolean.toString(harArbeid));
+        arbeidsforholdFakta.add(dinSituasjonJobb);
     }
 }
