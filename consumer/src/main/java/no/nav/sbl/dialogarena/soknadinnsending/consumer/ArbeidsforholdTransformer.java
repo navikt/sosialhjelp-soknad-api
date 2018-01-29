544889efecbda4d89b7718fd8cf3cdc34bc3a13b
Mats Krüger Svensson <mats.kruger.svensson@nav.no>
2018-01-29 15:26:22 +0100

    [kaizen] logge hvorfor arbeidsgiver ikke får navn

--- consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/ArbeidsforholdTransformer.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/ArbeidsforholdTransformer.java
@@ -111,6 +111,7 @@ public class ArbeidsforholdTransformer implements Transformer<no.nav.tjeneste.vi
             try {
                 hentKjerneinformasjonResponse = personService.hentKjerneinformasjon(fnr);
             } catch (Exception e) {
+                LOGGER.warn("Kunne ikke sette navn på arbeidsgiver", e);
                 return "";
             }
 
