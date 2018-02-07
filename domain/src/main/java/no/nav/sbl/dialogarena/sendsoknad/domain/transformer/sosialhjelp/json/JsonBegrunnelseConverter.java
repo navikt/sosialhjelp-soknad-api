103831f1da987d799c8179501fce277ab37ed10c
Mats Krüger Svensson <mats.kruger.svensson@nav.no>
2018-02-07 16:15:49 +0100

    [DIGISOS-387] Ser ut som feltene var required, så gjør den lettere

--- domain/src/main/java/no/nav/sbl/dialogarena/sendsoknad/domain/transformer/sosialhjelp/json/JsonBegrunnelseConverter.java
+++ domain/src/main/java/no/nav/sbl/dialogarena/sendsoknad/domain/transformer/sosialhjelp/json/JsonBegrunnelseConverter.java
@@ -5,7 +5,6 @@ import no.nav.sbl.soknadsosialhjelp.soknad.begrunnelse.JsonBegrunnelse;
 import no.nav.sbl.soknadsosialhjelp.soknad.common.JsonKildeBruker;
 import org.slf4j.Logger;
 
-import static no.nav.sbl.dialogarena.sendsoknad.domain.transformer.sosialhjelp.json.JsonUtils.erIkkeTom;
 import static org.slf4j.LoggerFactory.getLogger;
 
 public final class JsonBegrunnelseConverter {
@@ -19,14 +18,9 @@ public final class JsonBegrunnelseConverter {
     public static JsonBegrunnelse tilBegrunnelse(WebSoknad webSoknad) {
         String hvorfor = webSoknad.getValueForFaktum("begrunnelse.hvorfor");
         String hva = webSoknad.getValueForFaktum("begrunnelse.hva");
-
-        if (erIkkeTom(hvorfor) || erIkkeTom(hva)) {
-            return new JsonBegrunnelse()
-                    .withKilde(JsonKildeBruker.BRUKER)
-                    .withHvorforSoke(erIkkeTom(hvorfor) ? hvorfor : null)
-                    .withHvaSokesOm(erIkkeTom(hva) ? hva : null);
-        }
-
-        return null;
+        return new JsonBegrunnelse()
+                .withKilde(JsonKildeBruker.BRUKER)
+                .withHvorforSoke(hvorfor)
+                .withHvaSokesOm(hva);
     }
 }
