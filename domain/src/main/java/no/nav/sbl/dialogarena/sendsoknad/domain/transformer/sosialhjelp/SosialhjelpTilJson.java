ab3dc9db7701edd2c4b1061f1fd2b7bf83f5e0b2
Stian Grenborgen <grenborgen.stian@nav.no>
2018-02-23 10:33:14 +0100

    [DIGISOS-443] Legger på skjemavalidering av soknad og vedlegg før disse sendes ut.

--- domain/src/main/java/no/nav/sbl/dialogarena/sendsoknad/domain/transformer/sosialhjelp/SosialhjelpTilJson.java
+++ domain/src/main/java/no/nav/sbl/dialogarena/sendsoknad/domain/transformer/sosialhjelp/SosialhjelpTilJson.java
@@ -1,19 +1,23 @@
 package no.nav.sbl.dialogarena.sendsoknad.domain.transformer.sosialhjelp;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
+import java.util.UUID;
+
+import org.slf4j.Logger;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import no.nav.sbl.dialogarena.sendsoknad.domain.AlternativRepresentasjon;
 import no.nav.sbl.dialogarena.sendsoknad.domain.WebSoknad;
 import no.nav.sbl.dialogarena.sendsoknad.domain.exception.AlleredeHandtertException;
 import no.nav.sbl.dialogarena.sendsoknad.domain.transformer.AlternativRepresentasjonTransformer;
 import no.nav.sbl.dialogarena.sendsoknad.domain.transformer.AlternativRepresentasjonType;
 import no.nav.sbl.dialogarena.sendsoknad.domain.transformer.sosialhjelp.json.JsonSoknadConverter;
+import no.nav.sbl.soknadsosialhjelp.json.JsonSosialhjelpValidationException;
+import no.nav.sbl.soknadsosialhjelp.json.JsonSosialhjelpValidator;
 import no.nav.sbl.soknadsosialhjelp.soknad.JsonSoknad;
-import org.slf4j.Logger;
-
-import java.util.UUID;
-
-import static org.slf4j.LoggerFactory.getLogger;
 
 public class SosialhjelpTilJson implements AlternativRepresentasjonTransformer {
 
@@ -34,7 +38,8 @@ public class SosialhjelpTilJson implements AlternativRepresentasjonTransformer {
         try {
             final JsonSoknad jsonSoknad = JsonSoknadConverter.tilJsonSoknad(webSoknad);
             json = new ObjectMapper().writeValueAsString(jsonSoknad);
-        } catch (JsonProcessingException e) {
+            JsonSosialhjelpValidator.ensureValidSoknad(json);
+        } catch (JsonSosialhjelpValidationException|JsonProcessingException e) {
             logger.error("Kunne ikke generere soknads-JSON for {}", webSoknad.getBrukerBehandlingId(), e);
             throw new AlleredeHandtertException();
         }
