aa92b1f09d8423f524d5218ab2eb96a57081197b
Frederik Rønnevig <Frederik.Ronnevig@nav.no>
2018-02-27 15:38:12 +0100

    [DIGISOS-443] Implementere JSON-format for økonomiopplysninger

--- domain/src/main/java/no/nav/sbl/dialogarena/sendsoknad/domain/transformer/sosialhjelp/json/JsonOkonomiConverter.java
+++ domain/src/main/java/no/nav/sbl/dialogarena/sendsoknad/domain/transformer/sosialhjelp/json/JsonOkonomiConverter.java
@@ -3,6 +3,7 @@ package no.nav.sbl.dialogarena.sendsoknad.domain.transformer.sosialhjelp.json;
 import no.nav.sbl.dialogarena.sendsoknad.domain.Faktum;
 import no.nav.sbl.dialogarena.sendsoknad.domain.WebSoknad;
 import no.nav.sbl.soknadsosialhjelp.soknad.common.JsonKilde;
+import no.nav.sbl.soknadsosialhjelp.soknad.common.JsonKildeBruker;
 import no.nav.sbl.soknadsosialhjelp.soknad.okonomi.JsonOkonomi;
 import no.nav.sbl.soknadsosialhjelp.soknad.okonomi.JsonOkonomiopplysninger;
 import no.nav.sbl.soknadsosialhjelp.soknad.okonomi.JsonOkonomioversikt;
@@ -32,6 +33,7 @@ public class JsonOkonomiConverter {
     }
 
     private static JsonOkonomioversikt tilJsonOversikt(WebSoknad webSoknad) {
+
         return new JsonOkonomioversikt()
 
                 .withInntekt(tilJsonOkonomioversiktInntekt(webSoknad))
@@ -39,7 +41,6 @@ public class JsonOkonomiConverter {
                 .withFormue(tilJsonOkonomioversiktFormue(webSoknad))
                 ;
     }
-
 
     private static JsonOkonomiopplysninger tilJsonOpplysninger(WebSoknad webSoknad) {
 
@@ -49,7 +50,6 @@ public class JsonOkonomiConverter {
                 .withBekreftelse(tilJsonOkonomiopplysningerBekreftelse(webSoknad))
                 .withBeskrivelseAvAnnet(tilJsonOkonomiopplysningerBeskrivelseAvAnnet(webSoknad))
                 ;
-
     }
 
     private static List<JsonOkonomioversiktUtgift> tilJsonOkonomioversiktUtgift(WebSoknad webSoknad) {
@@ -84,22 +84,46 @@ public class JsonOkonomiConverter {
         return result.stream().filter(r -> r != null).collect(Collectors.toList());
     }
 
-
     private static JsonOkonomibeskrivelserAvAnnet tilJsonOkonomiopplysningerBeskrivelseAvAnnet(WebSoknad webSoknad) {
-        return null;
 
+        final JsonOkonomibeskrivelserAvAnnet jsonOkonomibeskrivelserAvAnnet = new JsonOkonomibeskrivelserAvAnnet().withKilde(JsonKildeBruker.BRUKER);
+
+        // 6 - Eier du noe med økonomisk verdi? Annet
+        jsonOkonomibeskrivelserAvAnnet.setVerdi(webSoknad.getValueForFaktum("inntekt.eierandeler.true.type.annet.true.beskrivelse"));
+
+        // 6 - Hva har du av innskudd og/eller sparing? Annet
+        jsonOkonomibeskrivelserAvAnnet.setSparing(webSoknad.getValueForFaktum( "inntekt.bankinnskudd.true.type.annet.true.beskrivelse"));
+
+        // 6 - Har du de siste tre månedene fått utbetalt penger som ikke er lønn og/eller penger fra NAV? Annet
+        jsonOkonomibeskrivelserAvAnnet.setUtbetaling(webSoknad.getValueForFaktum( "inntekt.inntekter.true.type.annet.true.beskrivelse"));
+
+        // 7 - Har du boutgifter? Andre utgifter
+        jsonOkonomibeskrivelserAvAnnet.setBoutgifter(webSoknad.getValueForFaktum("utgifter.boutgift.true.type.andreutgifter.true.beskrivelse"));
+
+        //7 Har du utgifter til barn? Annet
+        jsonOkonomibeskrivelserAvAnnet.setBarneutgifter(webSoknad.getValueForFaktum("utgifter.barn.true.utgifter.annet.true.beskrivelse"));
+
+        return jsonOkonomibeskrivelserAvAnnet;
     }
 
     private static List<JsonOkonomibekreftelse> tilJsonOkonomiopplysningerBekreftelse(WebSoknad webSoknad) {
 
         final List<JsonOkonomibekreftelse> result = new ArrayList<>();
 
+        result.add(opplysningBekreftelse("bostotte", "Søkt eller mottatt bostøtte fra Husbanken.", webSoknad.getFaktumMedKey("inntekt.bostotte")));
+
+        result.add(opplysningBekreftelse("verdi", "Eier noe av økonomisk verdi.", webSoknad.getFaktumMedKey("inntekt.eierandeler")));
+        result.add(opplysningBekreftelse("sparing", "Bankinnskudd eller annen sparing.", webSoknad.getFaktumMedKey("inntekt.bankinnskudd")));
+
+        result.add(opplysningBekreftelse("utbetaling", "Annen utbetaling", webSoknad.getFaktumMedKey("inntekt.inntekter")));
+
+        result.add(opplysningBekreftelse("boutgifter", "Boutgifter", webSoknad.getFaktumMedKey("utgifter.boutgift")));
+
+        result.add(opplysningBekreftelse("barneutgifter", "Utgifter til barn", webSoknad.getFaktumMedKey("utgifter.barn")));
+
         return result.stream().filter(r -> r != null).collect(Collectors.toList());
 
     }
-
-
-
 
     private static List<JsonOkonomiOpplysningUtbetaling> tilJsonOkonomiopplysningerUtbetaling(WebSoknad webSoknad) {
 
@@ -124,11 +148,9 @@ public class JsonOkonomiConverter {
                 webSoknad.getFaktaMedKey("opplysninger.inntekt.inntekter.annet"),
                 "sum"));
 
-
         return result.stream().filter(r -> r != null).collect(Collectors.toList());
 
     }
-
 
     private static List<JsonOkonomioversiktFormue> tilJsonOkonomioversiktFormue(WebSoknad webSoknad) {
 
@@ -163,13 +185,11 @@ public class JsonOkonomiConverter {
                 webSoknad.getFaktaMedKey("opplysninger.inntekt.bankinnskudd.annet"),
                 "saldo"));
 
-
         return result.stream().filter(r -> r != null).collect(Collectors.toList());
     }
 
     private static List<JsonOkonomiOpplysningUtgift> tilJsonOkonomiopplysningerUtgift(WebSoknad webSoknad) {
         final List<JsonOkonomiOpplysningUtgift> result = new ArrayList<>();
-
 
         result.addAll(opplysningUtgift("strom",
                 "Strøm (siste regning)",
@@ -211,10 +231,8 @@ public class JsonOkonomiConverter {
                 webSoknad.getFaktaMedKey("opplysninger.ekstrainfo.utgifter"),
                 "utgift", "beskrivelse"));
 
-
         return result.stream().filter(r -> r != null).collect(Collectors.toList());
     }
-
 
     private static final <E> void addIfNotNull(List<? super E> liste, E... elementer) {
         for (E e : elementer) {
@@ -231,29 +249,23 @@ public class JsonOkonomiConverter {
                 "Bostøtte",
                 webSoknad.getFaktaMedKey("opplysninger.inntekt.bostotte"),
                 "utbetaling"));
-
         result.addAll(oversiktInntekt("jobb",
                 "Lønnsinntekt",
                 webSoknad.getFaktaMedKey("opplysninger.arbeid.jobb"),
                 "bruttolonn",
                 "nettolonn"
         ));
-
         result.addAll(oversiktInntekt("studielanOgStipend",
                 "Studielån og -stipend",
                 webSoknad.getFaktaMedKey("opplysninger.arbeid.student"),
                 "utbetaling"
-
         ));
-
         result.addAll(oversiktInntekt("barnebidrag",
                 "Barnebidrag",
                 webSoknad.getFaktaMedKey("opplysninger.familiesituasjon.barnebidrag.betaler"),
                 "betaler"
 
         ));
-
-
         return result.stream().filter(r -> r != null).collect(Collectors.toList());
     }
 
@@ -296,13 +308,12 @@ public class JsonOkonomiConverter {
                     .withBelop(JsonUtils.tilInteger(properties.get(belopNavn)))
                     .withOverstyrtAvBruker(false);
         }).collect(Collectors.toList());
-
-
     }
 
     private static Collection<? extends JsonOkonomiOpplysningUtgift> opplysningUtgift(String type, String tittel, List<Faktum> fakta, String belopNavn) {
         return opplysningUtgift(type, tittel, fakta, belopNavn, null);
     }
+
     private static Collection<? extends JsonOkonomiOpplysningUtgift> opplysningUtgift(String type, String tittel, List<Faktum> fakta, String belopNavn, String tittelDelNavn) {
 
         return fakta.stream().filter(f -> f != null).map(faktum -> {
@@ -321,8 +332,6 @@ public class JsonOkonomiConverter {
                     .withBelop(JsonUtils.tilInteger(properties.get(belopNavn)))
                     .withOverstyrtAvBruker(false);
         }).collect(Collectors.toList());
-
-
     }
 
     private static Collection<? extends JsonOkonomioversiktUtgift> oversiktUtgift(String type, String tittel, List<Faktum> fakta, String belopnavn) {
@@ -335,7 +344,18 @@ public class JsonOkonomiConverter {
                     .withBelop(JsonUtils.tilInteger(properties.get(belopnavn)))
                     .withOverstyrtAvBruker(false);
         }).collect(Collectors.toList());
+    }
 
+
+    private static JsonOkonomibekreftelse opplysningBekreftelse(String type, String tittel, Faktum faktum) {
+        if (faktum == null || faktum.getValue() == null) {
+            return null;
+        }
+        return new JsonOkonomibekreftelse()
+                .withKilde(JsonKilde.BRUKER)
+                .withTittel(tittel)
+                .withType(type)
+                .withVerdi(Boolean.parseBoolean(faktum.getValue()));
     }
 }
 
