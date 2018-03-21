9d11a96a9dc58662269e039571d0b33188169ccc
Mats Krüger Svensson <mats.kruger.svensson@nav.no>
2018-03-21 13:04:55 +0100

    [DIGISOS-488] Dro ut lik kode for unmarshalling/marshalling

--- business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/batch/oppgave/Oppgave.java
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/batch/oppgave/Oppgave.java
@@ -2,15 +2,8 @@ package no.nav.sbl.dialogarena.soknadinnsending.business.batch.oppgave;
 
 import no.nav.sbl.dialogarena.soknadinnsending.business.batch.oppgave.fiks.FiksData;
 import no.nav.sbl.dialogarena.soknadinnsending.business.batch.oppgave.fiks.FiksResultat;
+import no.nav.sbl.dialogarena.soknadinnsending.business.util.JAXBHelper;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBElement;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
-import java.io.StringReader;
-import java.io.StringWriter;
 import java.time.LocalDateTime;
 
 public class Oppgave {
@@ -39,41 +32,10 @@ public class Oppgave {
         KLAR, UNDER_ARBEID, FERDIG, FEILET
     }
 
-    public static class JAXB {
+    public final static JAXBHelper JAXB = new JAXBHelper(
+            FiksData.class,
+            FiksResultat.class
+    );
 
-        public static final JAXBContext CONTEXT;
 
-        static { // TODO dra ut
-            try {
-                CONTEXT = JAXBContext.newInstance(
-                        FiksData.class,
-                        FiksResultat.class
-                );
-            } catch (JAXBException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        public static String marshal(Object jaxbelement) {
-            try {
-                StringWriter writer = new StringWriter();
-                Marshaller marshaller = CONTEXT.createMarshaller();
-                marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-                marshaller.setProperty(Marshaller.JAXB_FRAGMENT, true);
-                marshaller.marshal(jaxbelement, new StreamResult(writer));
-                return writer.toString();
-            } catch (JAXBException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        public static <T> T unmarshal(String melding, Class<T> elementClass) {
-            try {
-                JAXBElement<T> value = CONTEXT.createUnmarshaller().unmarshal(new StreamSource(new StringReader(melding)), elementClass);
-                return value.getValue();
-            } catch (JAXBException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
 }
