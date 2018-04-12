53dcee387292e1e85c58960d0ece8fb55c67c1b4
Martine Enger <martine.enger@nav.no>
2018-04-12 10:41:59 +0200

    SD-31: Bruker nytt format på selftest etter oppdatering av common-web

--- consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/wsconfig/EmailConfig.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/wsconfig/EmailConfig.java
@@ -1,6 +1,7 @@
 package no.nav.sbl.dialogarena.soknadinnsending.consumer.wsconfig;
 
 import no.nav.sbl.dialogarena.types.Pingable;
+import no.nav.sbl.dialogarena.types.Pingable.Ping.PingMetadata;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,9 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketAddress;
+
+import static no.nav.sbl.dialogarena.types.Pingable.Ping.feilet;
+import static no.nav.sbl.dialogarena.types.Pingable.Ping.lyktes;
 
 @Configuration
 public class EmailConfig {
@@ -31,17 +35,18 @@ public class EmailConfig {
                 SocketAddress socketAddress = new InetSocketAddress(host, port);
                 Socket socket = new Socket();
                 Ping pingResultat;
+                PingMetadata metadata = new PingMetadata(host, "SMTP-server for mail-utsending", false);
 
                 try {
                     socket.connect(socketAddress, SMTP_TIMEOUT);
-                    pingResultat = Ping.lyktes("SMTP-server for mail-utsending");
+                    pingResultat = lyktes(metadata);
                 } catch (IOException e) {
-                    pingResultat = Ping.feilet("SMTP-server for mail-utsending", e);
+                    pingResultat = feilet(metadata, e);
                 } finally {
                     try {
                         socket.close();
                     } catch (IOException e) {
-                        pingResultat = Ping.feilet("SMTP-server for mail-utsending", e);
+                        pingResultat = feilet(metadata, e);
                     }
                 }
                 return pingResultat;
