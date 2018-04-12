1274e69f583c2ea0bf6d6dfe163b7fc5d6d95492
Mats Krüger Svensson <mats.kruger.svensson@nav.no>
2018-04-12 12:54:03 +0200

    [kaizen] Mock av fiks for lokal bruk

--- mock/src/main/java/no/nav/sbl/dialogarena/sendsoknad/mockmodul/fiks/ForsendelseServiceMock.java
+++ mock/src/main/java/no/nav/sbl/dialogarena/sendsoknad/mockmodul/fiks/ForsendelseServiceMock.java
@@ -1,17 +1,30 @@
 package no.nav.sbl.dialogarena.sendsoknad.mockmodul.fiks;
 
+import no.ks.svarut.servicesv9.Forsendelse;
 import no.ks.svarut.servicesv9.ForsendelsesServiceV9;
+import org.slf4j.Logger;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.slf4j.LoggerFactory.getLogger;
 
 public class ForsendelseServiceMock {
+    private static final Logger logger = getLogger(ForsendelseServiceMock.class);
+
+
     public ForsendelsesServiceV9 forsendelseMock() {
         ForsendelsesServiceV9 amock = mock(ForsendelsesServiceV9.class);
 
         try {
-            when(amock.sendForsendelse(any())).thenReturn("fake-fiksid-1234567890");
+            when(amock.sendForsendelse(any())).then(invocation -> {
+                Forsendelse forsendelse = invocation.getArgumentAt(0, Forsendelse.class);
+                String behId = forsendelse.getEksternref();
+
+                String fiksId = "fake-fiksid-" + behId;
+                logger.info("Mocker kall til Fiks, returnerer fake-fiksid: {}", fiksId);
+                return fiksId;
+            });
         } catch (Exception ignored) {
         }
         return amock;
