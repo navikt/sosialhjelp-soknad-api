a8fd35de11d62ff9aab2b4ce2d56f257cf0d661f
Tine Kleivane <tine.kleivane@nav.no>
2018-04-23 13:46:36 +0200

    SD-229 Ta med tidspunkt i testene

--- web/src/test/java/no/nav/sbl/dialogarena/service/helpers/VisInformasjonOmPerioderHelperTest.java
+++ web/src/test/java/no/nav/sbl/dialogarena/service/helpers/VisInformasjonOmPerioderHelperTest.java
@@ -4,6 +4,8 @@ import com.github.jknack.handlebars.Handlebars;
 import no.nav.sbl.dialogarena.sendsoknad.domain.WebSoknad;
 import no.nav.sbl.dialogarena.sendsoknad.domain.kravdialoginformasjon.ForeldrepengerInformasjon;
 import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -39,14 +41,29 @@ public class VisInformasjonOmPerioderHelperTest {
     @Test
     public void viserIkkeInnholdNarSoknadHarOpprettetDatoFørFeilretting() throws IOException {
         String compiled = handlebars.compileInline("{{#visInformasjonOmPerioder}}JA{{/visInformasjonOmPerioder}}").apply(
-                opprettSoknad(ForeldrepengerInformasjon.FORSTEGANGSSOKNADER.get(0), "2018-04-17"));
+                opprettSoknad(ForeldrepengerInformasjon.FORSTEGANGSSOKNADER.get(0), "2018-04-17 00:00"));
         assertThat(compiled).isEqualTo("JA");
     }
 
     @Test
+    public void viserIkkeInnholdNarSoknadHarOpprettetTidspunktFørFeilretting() throws IOException {
+        String compiled = handlebars.compileInline("{{#visInformasjonOmPerioder}}JA{{/visInformasjonOmPerioder}}").apply(
+                opprettSoknad(ForeldrepengerInformasjon.FORSTEGANGSSOKNADER.get(0), "2018-04-17 15:59"));
+        assertThat(compiled).isEqualTo("JA");
+    }
+
+    @Test
+    public void viserIkkeInnholdNarSoknadHarOpprettetTidspunktRettEtterFeilretting() throws IOException {
+        String compiled = handlebars.compileInline("{{#visInformasjonOmPerioder}}JA{{/visInformasjonOmPerioder}}").apply(
+                opprettSoknad(ForeldrepengerInformasjon.FORSTEGANGSSOKNADER.get(0), "2018-04-17 16:01"));
+        assertThat(compiled).isEmpty();
+    }
+
+
+    @Test
     public void viserIkkeInnholdNarSoknadHarSenOpprettetDato() throws IOException {
         String compiled = handlebars.compileInline("{{#visInformasjonOmPerioder}}JA{{/visInformasjonOmPerioder}}").apply(
-                opprettSoknad(ForeldrepengerInformasjon.FORSTEGANGSSOKNADER.get(0), "2018-04-18"));
+                opprettSoknad(ForeldrepengerInformasjon.FORSTEGANGSSOKNADER.get(0), "2018-04-18 00:00"));
         assertThat(compiled).isEmpty();
     }
 
@@ -70,12 +87,12 @@ public class VisInformasjonOmPerioderHelperTest {
     private WebSoknad opprettSoknad(String skjemaNummer, String opprettetDato) {
         WebSoknad soknad = new WebSoknad();
         soknad.setSkjemaNummer(skjemaNummer);
-        soknad.medOppretteDato(DateTime.parse(opprettetDato));
+        soknad.medOppretteDato(DateTime.parse(opprettetDato, DateTimeFormat.forPattern("y-M-d H:m")));
         return soknad;
     }
 
     private WebSoknad opprettSoknad(String skjemaNummer) {
-        WebSoknad soknad = opprettSoknad(skjemaNummer, "2018-04-01");
+        WebSoknad soknad = opprettSoknad(skjemaNummer, "2018-04-01 00:00");
         return soknad;
     }
 }
