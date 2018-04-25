93215097bd121a06a647e8d028c1f938513f2cd6
Frederik Rønnevig <Frederik.Ronnevig@nav.no>
2018-04-25 12:44:18 +0200

    [DIGISOS-562] Endringer etter QA

--- web/src/main/java/no/nav/sbl/dialogarena/service/helpers/EttersendteVedleggHelper.java
+++ web/src/main/java/no/nav/sbl/dialogarena/service/helpers/EttersendteVedleggHelper.java
@@ -1,12 +1,14 @@
 package no.nav.sbl.dialogarena.service.helpers;
 
 import com.github.jknack.handlebars.Options;
+import no.nav.sbl.dialogarena.sendsoknad.domain.Vedlegg;
 import no.nav.sbl.dialogarena.sendsoknad.domain.WebSoknad;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 import static no.nav.sbl.dialogarena.service.HandlebarsUtils.finnWebSoknad;
 import static no.nav.sbl.dialogarena.service.HandlebarsUtils.lagItererbarRespons;
@@ -23,10 +25,32 @@ public class EttersendteVedleggHelper extends RegistryAwareHelper<Object> {
 
         WebSoknad soknad = finnWebSoknad(options.context);
 
-        List<String> filnavnene = soknad.getVedlegg().stream().map(
-                vedlegg -> vedlegg.getFilnavn()).filter(vedlegg -> vedlegg != null).collect(Collectors.toList());
+        //    List<String> filnavnene = soknad.getVedlegg().stream().map(
+        //          vedlegg -> vedlegg.getFilnavn()).filter(vedlegg -> vedlegg != null).collect(Collectors.toList());
 
-        return lagItererbarRespons(options, filnavnene);
+        List<Vedlegg> alleVedlegg = soknad.getVedlegg();
+
+        Map<String, Vedlegg> vedleggMap = new HashMap<>();
+
+        for (Vedlegg vedlegg : alleVedlegg) {
+          /*  if (vedlegg.getInnsendingsvalg().erIkke(LastetOpp)) {
+                continue;
+            }*/
+
+            String sammensattNavn = vedlegg.getSkjemaNummer() + "|" + vedlegg.getSkjemanummerTillegg();
+            Vedlegg vedlegget = vedleggMap.get(sammensattNavn);
+
+            if (vedlegget == null) {
+                vedlegget = new Vedlegg();
+                vedlegget.setSkjemaNummer(vedlegg.getSkjemaNummer());
+                vedlegget.setSkjemanummerTillegg(vedlegg.getSkjemanummerTillegg());
+                vedlegget.setFilnavn(vedlegg.getFilnavn());
+                vedleggMap.put(sammensattNavn, vedlegget);
+            }
+
+        }
+
+        return lagItererbarRespons(options, vedleggMap.values());
     }
 
     @Override
