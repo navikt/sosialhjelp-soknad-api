505cd52a10dadcda58a3475aef832c2bdb494b9b
Frederik Rønnevig <Frederik.Ronnevig@nav.no>
2018-04-25 14:11:15 +0200

    [DIGISOS-562] Endringer etter QA

--- web/src/main/java/no/nav/sbl/dialogarena/service/helpers/EttersendteVedleggHelper.java
+++ web/src/main/java/no/nav/sbl/dialogarena/service/helpers/EttersendteVedleggHelper.java
@@ -6,10 +6,12 @@ import no.nav.sbl.dialogarena.sendsoknad.domain.WebSoknad;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static no.nav.sbl.dialogarena.sendsoknad.domain.Vedlegg.Status.LastetOpp;
 import static no.nav.sbl.dialogarena.service.HandlebarsUtils.finnWebSoknad;
 import static no.nav.sbl.dialogarena.service.HandlebarsUtils.lagItererbarRespons;
 
@@ -21,27 +23,29 @@ public class EttersendteVedleggHelper extends RegistryAwareHelper<Object> {
     @Override
     public CharSequence apply(Object key, Options options) throws IOException {
 
-        System.out.println(NAVN + " : " + getBeskrivelse());
-        
         WebSoknad soknad = finnWebSoknad(options.context);
 
         List<Vedlegg> alleVedlegg = soknad.getVedlegg();
 
-        Map<String, Vedlegg> vedleggMap = new HashMap<>();
+        Map<String, List<Vedlegg>> vedleggMap = new HashMap<>();
 
         for (Vedlegg vedlegg : alleVedlegg) {
-
-            String sammensattNavn = vedlegg.getSkjemaNummer() + "|" + vedlegg.getSkjemanummerTillegg();
-            Vedlegg vedlegget = vedleggMap.get(sammensattNavn);
-
-            if (vedlegget == null) {
-                vedlegget = new Vedlegg();
-                vedlegget.setSkjemaNummer(vedlegg.getSkjemaNummer());
-                vedlegget.setSkjemanummerTillegg(vedlegg.getSkjemanummerTillegg());
-                vedlegget.setFilnavn(vedlegg.getFilnavn());
-                vedleggMap.put(sammensattNavn, vedlegget);
+            if (vedlegg.getInnsendingsvalg().erIkke(LastetOpp)) {
+                continue;
             }
 
+            String sammensattNavn = vedlegg.getSkjemaNummer() + "|" + vedlegg.getSkjemanummerTillegg();
+            List<Vedlegg> vedleggsGruppe = vedleggMap.get(sammensattNavn);
+
+            if (vedleggsGruppe == null) {
+                vedleggsGruppe = new ArrayList<>();
+                vedleggMap.put(sammensattNavn, vedleggsGruppe);
+            }
+            Vedlegg v = new Vedlegg();
+            v.setSkjemaNummer(vedlegg.getSkjemaNummer());
+            v.setSkjemanummerTillegg(vedlegg.getSkjemanummerTillegg());
+            v.setFilnavn(vedlegg.getFilnavn());
+            vedleggsGruppe.add(v);
         }
 
         return lagItererbarRespons(options, vedleggMap.values());
