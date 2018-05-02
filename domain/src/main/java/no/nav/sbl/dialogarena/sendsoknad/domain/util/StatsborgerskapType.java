240a1819056e3face8a414390060650dc1ab328f
André Roaldseth <andre.roaldseth@nav.no>
2018-05-02 13:29:09 +0200

    DIGIDAG-86 Bruk egen liste for land som ikke skal ha T4
    Den opprinnelige løsningen med å utvide statsborgerskapsType ble kronglete fordi
    definisjonen av EØS ikke er korrekt i den, og den treffer sannsynligvis flere
    søknader.

--- domain/src/main/java/no/nav/sbl/dialogarena/sendsoknad/domain/util/StatsborgerskapType.java
+++ domain/src/main/java/no/nav/sbl/dialogarena/sendsoknad/domain/util/StatsborgerskapType.java
@@ -1,24 +1,16 @@
 package no.nav.sbl.dialogarena.sendsoknad.domain.util;
 
 import static no.nav.sbl.dialogarena.sendsoknad.domain.util.LandListe.EOS_LAND;
-import static no.nav.sbl.dialogarena.sendsoknad.domain.util.LandListe.NORDEN_LAND;
 
 public class StatsborgerskapType {
 
-    public static final String TYPE_NORSK = "norsk";
-    public static final String TYPE_EOS = "eos";
-    public static final String TYPE_NORDEN = "norden";
-    public static final String TYPE_IKKE_EOS = "ikkeEos";
-
     public static String get(String landkode) {
         if ("NOR".equals(landkode)) {
-            return TYPE_NORSK;
+            return "norsk";
         } else if (EOS_LAND.contains(landkode)) {
-            return TYPE_EOS;
-        } else if (NORDEN_LAND.contains(landkode)) {
-            return TYPE_NORDEN;
+            return "eos";
         } else {
-            return TYPE_IKKE_EOS;
+            return "ikkeEos";
         }
     }
 }
