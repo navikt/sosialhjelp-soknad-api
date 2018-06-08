7f078eaeea6044b895fededf1ae92a28797c7705
Frederik Rønnevig <Frederik.Ronnevig@nav.no>
2018-06-08 13:25:56 +0200

    [DIGISOS-676] Legger til støtte for å lagre sha

--- business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/service/FillagerService.java
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/service/FillagerService.java
@@ -5,6 +5,8 @@ import no.nav.sbl.dialogarena.sendsoknad.domain.exception.AlleredeHandtertExcept
 import no.nav.sbl.dialogarena.soknadinnsending.business.db.fillager.FillagerRepository;
 import no.nav.sbl.dialogarena.soknadinnsending.business.db.fillager.FillagerRepository.Fil;
 import org.apache.commons.io.IOUtils;
+import org.bouncycastle.jcajce.provider.digest.SHA512;
+import org.bouncycastle.util.encoders.Hex;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +15,7 @@ import java.io.InputStream;
 import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
+
 
 @Component
 public class FillagerService {
@@ -24,8 +27,16 @@ public class FillagerService {
 
     public void lagreFil(String behandlingsId, String uid, String fnr, InputStream fil) {
         logger.info("Skal lagre fil til fillager for behandlingsId {}. UUID: {}", behandlingsId, uid);
+        SHA512.Digest digest = new SHA512.Digest();
+
+
         try {
-            fillagerRepository.lagreFil(new Fil(behandlingsId, uid, IOUtils.toByteArray(fil), fnr));
+
+            byte[] bytes = IOUtils.toByteArray(fil);
+            digest.update(bytes);
+            String sha = Hex.toHexString(digest.digest());
+
+            fillagerRepository.lagreFil(new Fil(behandlingsId, uid, bytes, fnr, sha));
         } catch (Exception e) {
             logger.error("Kunne ikke lagre fil med uuid {}", uid, e);
             throw new AlleredeHandtertException();
