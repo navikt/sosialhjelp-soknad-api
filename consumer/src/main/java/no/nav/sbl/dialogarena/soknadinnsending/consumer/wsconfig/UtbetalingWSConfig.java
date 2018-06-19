680289e7d7eef4f07f805da3ea5f46eaa632c1bf
Tia Firing <tia.firing@nav.no>
2018-06-19 09:44:13 +0200

    DIGISOS-217 legger til utbetalinger fra nav i json

--- consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/wsconfig/UtbetalingWSConfig.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/wsconfig/UtbetalingWSConfig.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.xml.namespace.QName;
+
 import static no.nav.sbl.dialogarena.common.cxf.InstanceSwitcher.createMetricsProxyWithInstanceSwitcher;
 import static no.nav.sbl.dialogarena.types.Pingable.Ping.feilet;
 import static no.nav.sbl.dialogarena.types.Pingable.Ping.lyktes;
@@ -17,16 +19,19 @@ public class UtbetalingWSConfig {
 
     public static final String UTBETALING_KEY = "start.utbetaling.withmock";
 
-    @Value("${utbetaling.url}")
+    @Value("${Utbetaling_v1.url}")
     private String utbetalingEndpoint;
 
     private ServiceBuilder<UtbetalingV1>.PortTypeBuilder<UtbetalingV1> factory() {
         return new ServiceBuilder<>(UtbetalingV1.class)
                 .asStandardService()
                 .withAddress(utbetalingEndpoint)
-//                .withWsdl("classpath:/utbetaling/no/nav/tjeneste/virksomhet/utbetaling/v1/utbetaling.wsdl")
+                .withWsdl("classpath:/utbetaling/no/nav/tjeneste/virksomhet/utbetaling/v1/Binding.wsdl")
+                .withServiceName(new QName("http://nav.no/tjeneste/virksomhet/utbetaling/v1/Binding", "Utbetaling_v1"))
+                .withEndpointName(new QName("http://nav.no/tjeneste/virksomhet/utbetaling/v1/Binding", "Utbetaling_v1Port"))
                 .build()
-                .withHttpsMock();
+                .withHttpsMock()
+                .withMDC();
     }
 
     @Bean
