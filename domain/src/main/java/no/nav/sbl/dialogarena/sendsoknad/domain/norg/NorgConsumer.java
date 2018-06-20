b133bb3720ac82f4c3fc7356dd5160a78dcf00ee
Tia Firing <tia.firing@nav.no>
2018-06-20 14:27:36 +0200

    DIGISOS-726 fjerner integrasjonen mot norg midlertidig og legger til enheter i mock i stedet

--- domain/src/main/java/no/nav/sbl/dialogarena/sendsoknad/domain/norg/NorgConsumer.java
+++ domain/src/main/java/no/nav/sbl/dialogarena/sendsoknad/domain/norg/NorgConsumer.java
@@ -19,6 +19,26 @@ public interface NorgConsumer {
         public String organisasjonsnummer;
         public String sosialeTjenester;
         public String orgNrTilKommunaltNavKontor;
+
+        public RsNorgEnhet withEnhetId(long enhetId) {
+            this.enhetId = enhetId;
+            return this;
+        }
+
+        public RsNorgEnhet withNavn(String navn) {
+            this.navn = navn;
+            return this;
+        }
+
+        public RsNorgEnhet withEnhetNr(String enhetNr) {
+            this.enhetNr = enhetNr;
+            return this;
+        }
+
+        public RsNorgEnhet withOrgNrTilKommunaltNavKontor(String orgNrTilKommunaltNavKontor) {
+            this.orgNrTilKommunaltNavKontor = orgNrTilKommunaltNavKontor;
+            return this;
+        }
     }
 
     class RsKontaktinformasjon {
