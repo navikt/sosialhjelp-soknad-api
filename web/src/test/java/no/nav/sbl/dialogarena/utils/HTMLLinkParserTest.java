2ff023625e3f958f7e44f437c156aab4b252678f
Frederik Rønnevig <Frederik.Ronnevig@nav.no>
2018-06-21 14:23:59 +0200

    [DIGISOS-715] Legger på vasking av tekst for PDF-validering av juridisk logg

--- web/src/test/java/no/nav/sbl/dialogarena/utils/HTMLLinkParserTest.java
+++ web/src/test/java/no/nav/sbl/dialogarena/utils/HTMLLinkParserTest.java
@@ -1,53 +1,42 @@
 package no.nav.sbl.dialogarena.utils;
 
-import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.ArrayList;
 
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+
 
 public class HTMLLinkParserTest {
 
-    private static HTMLLinkParser htmlLinkExtractor;
 
     private static String TEST_LINK = "http://www.nav.no";
-
-    @BeforeClass
-    public void setup() {
-
-        htmlLinkExtractor = new HTMLLinkParser();
-    }
 
 
     @Test
     public void testValidHTML() {
 
-        String html = HTMLContentProvider().toString();
+        HTMLLinkParser htmlLinkExtractor = new HTMLLinkParser();
 
-        ArrayList<HTMLLinkParser.HtmlLink> links = htmlLinkExtractor.extractHTMLLinks(html);
+        String html = "abc hmmmm <a href='" + TEST_LINK + "'>NAV</a>" +
+                "\n" + "abc hmmmm <a HREF='" + TEST_LINK + "'>NAV</a>" +
+                "\n" + "abc hmmmm <A HREF='" + TEST_LINK + "'>NAV</A> abc hmmmm <A HREF='" + TEST_LINK + "' + target='_blank'>NAV</A>" +
+                "\n" + "abc hmmmm <A HREF='" + TEST_LINK + "' target='_blank'>NAV</A>" +
+                "\n" + "abc hmmmm <A target='_blank' HREF='" + TEST_LINK + "'>NAV</A>" +
+                "\n" + "abc hmmmm <A target='_blank' HREF=\"" + TEST_LINK + "\">NAV</A>" +
+                "\n" + "abc hmmmm <a HREF=" + TEST_LINK + ">NAV</a>";
 
-        Assert.assertTrue(links.size() != 0);
+
+
+        ArrayList<HTMLLinkParser.HtmlLink> links = htmlLinkExtractor.getLinks(html);
+
+        assertTrue(links.size() != 0);
 
         for (int i = 0; i < links.size(); i++) {
             HTMLLinkParser.HtmlLink htmlLinks = links.get(i);
 
-            Assert.assertEquals(htmlLinks.getLink(), TEST_LINK);
+            assertEquals(htmlLinks.getLink(), TEST_LINK);
         }
-
-    }
-
-    private Object[][] HTMLContentProvider() {
-        return new Object[][]{
-                new Object[]{"abc hmmmm <a href='" + TEST_LINK + "'>NAV</a>"},
-                new Object[]{"abc hmmmm <a HREF='" + TEST_LINK + "'>NAV</a>"},
-
-                new Object[]{"abc hmmmm <A HREF='" + TEST_LINK + "'>NAV</A> , "
-                        + "abc hmmmm <A HREF='" + TEST_LINK + "' target='_blank'>NAV</A>"},
-
-                new Object[]{"abc hmmmm <A HREF='" + TEST_LINK + "' target='_blank'>NAV</A>"},
-                new Object[]{"abc hmmmm <A target='_blank' HREF='" + TEST_LINK + "'>NAV</A>"},
-                new Object[]{"abc hmmmm <A target='_blank' HREF=\"" + TEST_LINK + "\">NAV</A>"},
-                new Object[]{"abc hmmmm <a HREF=" + TEST_LINK + ">NAV</a>"},};
     }
 }
