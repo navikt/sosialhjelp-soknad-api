8b924b6ae40c6a9e619ca0cff676d8484eb4190d
Tia Firing <tia.firing@nav.no>
2018-07-06 14:07:20 +0200

    DIGISOS-190 første del av oppsett for å legge til systemregistrert sivilstatus i json

---
+++ domain/src/test/java/no/nav/sbl/dialogarena/sendsoknad/domain/transformer/sosialhjelp/json/JsonFamilieConverterTest.java
@@ -0,0 +1,54 @@
+package no.nav.sbl.dialogarena.sendsoknad.domain.transformer.sosialhjelp.json;
+
+import no.nav.sbl.dialogarena.sendsoknad.domain.Faktum;
+import no.nav.sbl.dialogarena.sendsoknad.domain.WebSoknad;
+import no.nav.sbl.soknadsosialhjelp.soknad.familie.JsonSivilstatus;
+import org.junit.Test;
+
+import static no.nav.sbl.soknadsosialhjelp.soknad.common.JsonKilde.BRUKER;
+import static no.nav.sbl.soknadsosialhjelp.soknad.common.JsonKilde.SYSTEM;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.nullValue;
+import static org.junit.Assert.assertThat;
+
+public class JsonFamilieConverterTest {
+
+    @Test
+    public void tilJsonSivilstatusReturnererNullHvisBrukerregistrertOgSystemregistrertSivilstatusMangler() {
+        final WebSoknad webSoknad = new WebSoknad().medFaktum(new Faktum().medKey("ikke.sivilstatus").medValue("Noe annet"));
+
+        JsonSivilstatus jsonSivilstatus = JsonFamilieConverter.tilJsonSivilstatus(webSoknad);
+
+        assertThat(jsonSivilstatus, nullValue());
+    }
+
+    @Test
+    public void tilJsonSivilstatusBrukerSystemregistrertSivilstatusHvisBrukerregistrertMangler() {
+        final WebSoknad webSoknad = new WebSoknad().medFaktum(new Faktum().medKey("system.familie.sivilstatus").medValue("gift"));
+
+        JsonSivilstatus jsonSivilstatus = JsonFamilieConverter.tilJsonSivilstatus(webSoknad);
+
+        assertThat(jsonSivilstatus.getKilde(), is(SYSTEM));
+    }
+
+    @Test
+    public void tilJsonSivilstatusBrukerBrukerregistrertSivilstatusHvisSystemregistrertManglerOgBrukerregistrertFinnes() {
+        final WebSoknad webSoknad = new WebSoknad().medFaktum(new Faktum().medKey("familie.sivilstatus").medValue("gift"));
+
+        JsonSivilstatus jsonSivilstatus = JsonFamilieConverter.tilJsonSivilstatus(webSoknad);
+
+        assertThat(jsonSivilstatus.getKilde(), is(BRUKER));
+    }
+
+    @Test
+    public void tilJsonSivilstatusBrukerBrukerregistrertSivilstatusHvisBrukeregistrertOgSystemregistrertSivilstatusFinnes() {
+        final WebSoknad webSoknad = new WebSoknad()
+                .medFaktum(new Faktum().medKey("familie.sivilstatus").medValue("gift"))
+                .medFaktum(new Faktum().medKey("system.familie.sivilstatus").medValue("ugift"));
+
+        JsonSivilstatus jsonSivilstatus = JsonFamilieConverter.tilJsonSivilstatus(webSoknad);
+
+        assertThat(jsonSivilstatus.getKilde(), is(BRUKER));
+    }
+
+}
