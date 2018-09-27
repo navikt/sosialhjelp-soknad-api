2e47944ccc92ad7669c241b1445f388a0308df2f
Tia Firing <tia.firing@nav.no>
2018-09-27 14:51:26 +0200

    DIGISOS-192 legger på sortering av barn basert på alder samt id slik at barna ikke ble flere i løpet av søknadsinnsendingen

--- business/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/business/person/BarnBolkTest.java
+++ business/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/business/person/BarnBolkTest.java
@@ -13,6 +13,7 @@ import org.mockito.runners.MockitoJUnitRunner;
 import java.util.*;
 
 import static no.nav.sbl.dialogarena.sendsoknad.domain.Faktum.FaktumType.SYSTEMREGISTRERT;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
@@ -26,14 +27,14 @@ public class BarnBolkTest {
     private static final String BARN_FORNAVN = "Bjarne";
     private static final String BARN_MELLOMNAVN = "Navn";
     private static final String BARN_ETTERNAVN = "Barnet";
-    private static final LocalDate BARN_FODSELSDATO = new LocalDate(2000, 1, 1);
-    private static final String BARN_FODSELSDATO_TEKST = "2000-01-01";
+    private static final LocalDate BARN_FODSELSDATO = new LocalDate(2008, 7, 23);
+    private static final String BARN_FODSELSDATO_TEKST = "2008-07-23";
 
     private static final String BARN2_IDENT = "***REMOVED***";
     private static final String BARN2_FORNAVN = "Per";
     private static final String BARN2_ETTERNAVN = "Barnet";
-    private static final LocalDate BARN2_FODSELSDATO = new LocalDate(2008, 7, 23);
-    private static final String BARN2_FODSELSDATO_TEKST = "2008-07-23";
+    private static final LocalDate BARN2_FODSELSDATO = new LocalDate(2006, 1, 1);
+    private static final String BARN2_FODSELSDATO_TEKST = "2006-01-01";
     private static final Long SOKNAD_ID = 21L;
 
     @InjectMocks
@@ -78,7 +79,8 @@ public class BarnBolkTest {
 
         assertThat(fakta.size(), is(2));
         assertThat(fakta.get(0).getKey(), is("system.familie.barn.true.barn"));
-        assertThat(fakta.get(0).getUnikProperty(), is("fnr"));
+        assertThat(fakta.get(0).getUnikProperty(), is("id"));
+        assertThat(barn.get("id"), is("1"));
         assertThat(barn.get("fnr"), is(BARN_IDENT));
         assertThat(barn.get("fornavn"), is(BARN_FORNAVN));
         assertThat(barn.get("mellomnavn"), is(BARN_MELLOMNAVN));
@@ -88,7 +90,8 @@ public class BarnBolkTest {
         assertThat(barn.get("folkeregistrertsammen"), is("true"));
 
         assertThat(fakta.get(1).getKey(), is("system.familie.barn.true.barn"));
-        assertThat(fakta.get(1).getUnikProperty(), is("fnr"));
+        assertThat(fakta.get(1).getUnikProperty(), is("id"));
+        assertThat(barn2.get("id"), is("2"));
         assertThat(barn2.get("fnr"), is(BARN2_IDENT));
         assertThat(barn2.get("fornavn"), is(BARN2_FORNAVN));
         assertThat(barn2.get("mellomnavn"), nullValue());
@@ -114,6 +117,7 @@ public class BarnBolkTest {
         Map<String, String> barn = fakta.get(0).getProperties();
 
         assertThat(fakta.size(), is(1));
+        assertThat(barn.get("id"), is("1"));
         assertThat(barn.get("fnr"), nullValue());
         assertThat(barn.get("fornavn"), nullValue());
         assertThat(barn.get("mellomnavn"), nullValue());
@@ -125,20 +129,20 @@ public class BarnBolkTest {
 
     private List<Barn> lagListeMedBarn(int antallBarn) {
         List<Barn> barn = new ArrayList<>();
-        barn.add(new Barn().withFnr(BARN_IDENT)
-                .withFornavn(BARN_FORNAVN)
-                .withMellomnavn(BARN_MELLOMNAVN)
-                .withEtternavn(BARN_ETTERNAVN)
-                .withFodselsdato(BARN_FODSELSDATO)
-                .withFolkeregistrertsammen(true)
+        barn.add(new Barn().withFnr(BARN2_IDENT)
+                .withFornavn(BARN2_FORNAVN)
+                .withEtternavn(BARN2_ETTERNAVN)
+                .withFodselsdato(BARN2_FODSELSDATO)
+                .withFolkeregistrertsammen(false)
                 .withIkkeTilgang(false));
 
         if (antallBarn == 2) {
-            barn.add(new Barn().withFnr(BARN2_IDENT)
-                    .withFornavn(BARN2_FORNAVN)
-                    .withEtternavn(BARN2_ETTERNAVN)
-                    .withFodselsdato(BARN2_FODSELSDATO)
-                    .withFolkeregistrertsammen(false)
+            barn.add(new Barn().withFnr(BARN_IDENT)
+                    .withFornavn(BARN_FORNAVN)
+                    .withMellomnavn(BARN_MELLOMNAVN)
+                    .withEtternavn(BARN_ETTERNAVN)
+                    .withFodselsdato(BARN_FODSELSDATO)
+                    .withFolkeregistrertsammen(true)
                     .withIkkeTilgang(false));
         }
         return barn;
