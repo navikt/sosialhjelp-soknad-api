e8fc771a186ccbe50bcdcf9170e02fa41fc2ad8b
Tia Firing <tia.firing@nav.no>
2018-09-27 14:51:26 +0200

    DIGISOS-192 legger på sortering av barn basert på alder samt id slik at barna ikke ble flere i løpet av søknadsinnsendingen

--- business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/person/BarnBolk.java
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/person/BarnBolk.java
@@ -8,8 +8,11 @@ import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 
+import static java.lang.String.valueOf;
 import static no.nav.sbl.dialogarena.sendsoknad.domain.Faktum.FaktumType.SYSTEMREGISTRERT;
 
 @Service
@@ -42,11 +45,14 @@ public class BarnBolk implements BolkService {
     }
 
     List<Faktum> genererSystemFaktaForBarn(List<Barn> barnListe, final Long soknadId) {
+        AtomicLong lopenummer = new AtomicLong(1);
         return barnListe.stream()
                 .filter(Objects::nonNull)
+                .sorted(Comparator.comparing(Barn::getFodselsdato, Comparator.nullsLast(Comparator.reverseOrder())))
                 .map(barn ->
                         new Faktum().medSoknadId(soknadId).medKey("system.familie.barn.true.barn").medType(SYSTEMREGISTRERT)
-                                .medUnikProperty("fnr")
+                                .medUnikProperty("id")
+                                .medSystemProperty("id", valueOf(lopenummer.getAndIncrement()))
                                 .medSystemProperty("fnr", barn.getFnr())
                                 .medSystemProperty("fornavn", barn.getFornavn())
                                 .medSystemProperty("mellomnavn", barn.getMellomnavn())
