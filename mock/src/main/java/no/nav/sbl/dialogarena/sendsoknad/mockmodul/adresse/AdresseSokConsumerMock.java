040c67983d4a48a4cc6d3ca2f4a8713ede8c573b
Stian Grenborgen <grenborgen.stian@nav.no>
2018-11-14 12:43:07 +0100

    [kaizen] Oppdaterer mock for å kunne kjøre juridisklogg-tester lokalt.

--- mock/src/main/java/no/nav/sbl/dialogarena/sendsoknad/mockmodul/adresse/AdresseSokConsumerMock.java
+++ mock/src/main/java/no/nav/sbl/dialogarena/sendsoknad/mockmodul/adresse/AdresseSokConsumerMock.java
@@ -1,16 +1,20 @@
 package no.nav.sbl.dialogarena.sendsoknad.mockmodul.adresse;
 
-import no.nav.sbl.dialogarena.sendsoknad.domain.adresse.AdresseSokConsumer;
-import no.nav.sbl.dialogarena.sendsoknad.domain.adresse.AdresseSokConsumer.AdresseData;
-import no.nav.sbl.dialogarena.sendsoknad.domain.adresse.AdresseSokConsumer.AdressesokRespons;
-import org.slf4j.Logger;
-
-import java.util.ArrayList;
-
 import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.slf4j.LoggerFactory.getLogger;
+
+import java.util.Arrays;
+
+import org.mockito.stubbing.Answer;
+import org.slf4j.Logger;
+
+import no.nav.sbl.dialogarena.sendsoknad.domain.adresse.AdresseSokConsumer;
+import no.nav.sbl.dialogarena.sendsoknad.domain.adresse.AdresseSokConsumer.AdresseData;
+import no.nav.sbl.dialogarena.sendsoknad.domain.adresse.AdresseSokConsumer.AdressesokRespons;
+import no.nav.sbl.dialogarena.sendsoknad.domain.adresse.AdresseSokConsumer.Sokedata;
 
 public class AdresseSokConsumerMock {
     private static final Logger logger = getLogger(AdresseSokConsumerMock.class);
@@ -18,29 +22,46 @@ public class AdresseSokConsumerMock {
     public AdresseSokConsumer adresseRestService() {
         AdresseSokConsumer mock = mock(AdresseSokConsumer.class);
 
-        when(mock.sokAdresse(anyString())).thenAnswer(invocation -> {
-            String param = invocation.getArgumentAt(0, String.class);
+        when(mock.sokAdresse(any(Sokedata.class))).thenAnswer(adressesokResponsMock());
+        when(mock.sokAdresse(anyString())).thenAnswer(adressesokResponsMock());
+
+        return mock;
+    }
+
+    private Answer<?> adressesokResponsMock() {
+        return invocation -> {
+            Object param = invocation.getArgumentAt(0, Object.class);
 
             logger.info("Mocker respons til adressesok for søk: {}", param);
 
             AdressesokRespons respons = new AdressesokRespons();
-            AdresseData a = new AdresseData();
-            a.kommunenummer = "1841";
-            a.kommunenavn = "Fauske";
-            a.adressenavn = "SANNERGATA";
-            a.husnummerFra = "0001";
-            a.husnummerTil = "9999";
-            a.postnummer = null;
-            a.poststed = null;
-            a.geografiskTilknytning = "0216";
-            a.gatekode = "02081";
-            a.bydel = null;
+            final AdresseData a1 = new AdresseData();
+            a1.kommunenummer = "1201";
+            a1.kommunenavn = "Bergen";
+            a1.adressenavn = "SANNERGATA";
+            a1.husnummerFra = "0001";
+            a1.husnummerTil = "0010";
+            a1.postnummer = "1337";
+            a1.poststed = "Leet";
+            a1.geografiskTilknytning = "120102";
+            a1.gatekode = "02081";
+            a1.bydel = "120102";
+            
+            final AdresseData a2 = new AdresseData();
+            a2.kommunenummer = "1201";
+            a2.kommunenavn = "Bergen";
+            a2.adressenavn = "SANNERGATA";
+            a2.husnummerFra = "0011";
+            a2.husnummerTil = "9999";
+            a2.postnummer = "1337";
+            a2.poststed = "Leet";
+            a2.geografiskTilknytning = "120107";
+            a2.gatekode = "02081";
+            a2.bydel = "120107";
 
-            respons.adresseDataList = new ArrayList<>();
-            respons.adresseDataList.add(a);
+            respons.adresseDataList = Arrays.asList(a1, a2);
+            
             return respons;
-        });
-
-        return mock;
+        };
     }
 }
