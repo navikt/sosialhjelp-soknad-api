61d178af84fe70c78c2dea7748144d210e780f58
Oskar Asplin <oskar.asplin@nav.no>
2018-11-19 17:35:59 +0100

    DIGISOS-831 Type formue valgt listes kun opp en gang per type

--- web/src/main/java/no/nav/sbl/dialogarena/service/HandlebarContext.java
+++ web/src/main/java/no/nav/sbl/dialogarena/service/HandlebarContext.java
@@ -1,12 +1,16 @@
 package no.nav.sbl.dialogarena.service;
 
-import java.util.Collections;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-import no.nav.sbl.dialogarena.sendsoknad.domain.Vedlegg;
+import no.nav.sbl.dialogarena.service.context.Formuetype;
 import no.nav.sbl.soknadsosialhjelp.soknad.JsonInternalSoknad;
 import no.nav.sbl.soknadsosialhjelp.soknad.JsonSoknad;
 import no.nav.sbl.soknadsosialhjelp.soknad.internal.JsonSoknadsmottaker;
+import no.nav.sbl.soknadsosialhjelp.soknad.okonomi.oversikt.JsonOkonomioversiktFormue;
 import no.nav.sbl.soknadsosialhjelp.vedlegg.JsonVedleggSpesifikasjon;
 
 public final class HandlebarContext {
@@ -33,5 +37,13 @@ public final class HandlebarContext {
 
     public boolean getUtvidetSoknad() {
         return utvidetSoknad;
+    }
+
+    public Collection<Formuetype> getFormuetyper() {
+        final List<JsonOkonomioversiktFormue> formue = internalSoknad.getSoknad().getData().getOkonomi().getOversikt().getFormue();
+        return formue.stream()
+            .map((f) -> new Formuetype(f.getType(), f.getTittel()))
+            .distinct()
+            .collect(Collectors.toSet());
     }
 }
