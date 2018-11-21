d6b9396e7be4ae7d030037efeaf83e2e803b63dd
Oskar Asplin <oskar.asplin@nav.no>
2018-11-21 10:07:00 +0100

    DIGISOS-831 Fjernet dobbel formaterDatoHelper

--- web/src/main/java/no/nav/sbl/dialogarena/service/helpers/FormaterDatoHelper.java
+++ web/src/main/java/no/nav/sbl/dialogarena/service/helpers/FormaterDatoHelper.java
@@ -1,40 +1,31 @@
 package no.nav.sbl.dialogarena.service.helpers;
 
 import com.github.jknack.handlebars.Options;
+import org.joda.time.LocalDate;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 @Component
-public class FormaterDatoHelper extends RegistryAwareHelper<Object>{
-
-    public static final String NAVN = "formaterDato";
-
-    @Override
-    public CharSequence apply(Object key, Options options) throws IOException {
-        if (key != null) {
-            try {
-                LocalDate datetime = LocalDate.parse(key.toString(), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-                return datetime.format(DateTimeFormatter.ofPattern("ddMMyyyy")).toString();
-            }
-            catch (java.time.format.DateTimeParseException e) {
-                return "";
-            }
-        }
-        else {
-            return "";
-        }
-    }
+public class FormaterDatoHelper extends RegistryAwareHelper<String>{
+    private final Locale locale = new Locale("nb", "NO");
 
     @Override
     public String getNavn() {
-        return NAVN;
+        return "formaterDato";
     }
 
     @Override
     public String getBeskrivelse() {
-        return "Returnerer personnr fra personIdentifikator";
+        return "Formaterer en innsendt dato på et gitt format som også sendes inn";
+    }
+
+    @Override
+    public CharSequence apply(String datoStreng, Options options) throws IOException {
+        LocalDate date = new LocalDate(datoStreng);
+        String format = options.param(0);
+
+        return date.toString(format, locale);
     }
 }
