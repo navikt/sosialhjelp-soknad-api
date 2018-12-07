a9d4730722bfdf051836303c0e8052da2c186062
Oskar Asplin <oskar.asplin@nav.no>
2018-12-07 10:14:38 +0100

    DIGISOS-1012 Automatiserte henting av typenavn i helperne hvisBarneutgift og hvisBoutgift

--- business/src/main/java/no/nav/sbl/sosialhjelp/pdf/helpers/HentSvaralternativerHelper.java
+++ business/src/main/java/no/nav/sbl/sosialhjelp/pdf/helpers/HentSvaralternativerHelper.java
@@ -1,25 +1,18 @@
 package no.nav.sbl.sosialhjelp.pdf.helpers;
 
-import static no.nav.sbl.sosialhjelp.pdf.HandlebarContext.SPRAK;
+import com.github.jknack.handlebars.Options;
+import no.nav.sbl.dialogarena.soknadsosialhjelp.message.NavMessageSource;
+import org.springframework.stereotype.Component;
 
+import javax.inject.Inject;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Locale;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Optional;
-import java.util.Properties;
-import java.util.Set;
 import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import javax.inject.Inject;
-
-import org.springframework.stereotype.Component;
-
-import com.github.jknack.handlebars.Options;
-
-import no.nav.sbl.dialogarena.soknadsosialhjelp.message.NavMessageSource;
+import static no.nav.sbl.sosialhjelp.pdf.HandlebarContext.SPRAK;
 
 @Component
 public class HentSvaralternativerHelper extends RegistryAwareHelper<String> {
@@ -53,19 +46,42 @@ public class HentSvaralternativerHelper extends RegistryAwareHelper<String> {
 
         final Pattern tekstfilNavnStruktur = directChildPattern(parentKey);
         for (Entry<String, String> tekstfil : allProperties(locale)) {
-            findMatchingSubKey(tekstfilNavnStruktur, tekstfil, HentSvaralternativerHelper::shouldIncludeSubKey)
+            findValueForMatchingSubKey(tekstfilNavnStruktur, tekstfil, HentSvaralternativerHelper::shouldIncludeSubKey)
                 .ifPresent((v) -> result.add(v));
         }
         
         return result;
     }
+
+    public Set<String> findChildPropertySubkeys(final String parentKey, final Locale locale) {
+        final Set<String> result = new HashSet<>();
+
+        final Pattern tekstfilNavnStruktur = directChildPattern(parentKey);
+        for (Entry<String, String> tekstfil : allProperties(locale)) {
+            findMatchingSubKey(tekstfilNavnStruktur, tekstfil, HentSvaralternativerHelper::shouldIncludeSubKey)
+                    .ifPresent((v) -> result.add(v));
+        }
+
+        return result;
+    }
     
-    private Optional<String> findMatchingSubKey(final Pattern tekstfilNavnStruktur, final Entry<String, String> tekstfil, final Predicate<String> shouldInclude) {
+    private Optional<String> findValueForMatchingSubKey(final Pattern tekstfilNavnStruktur, final Entry<String, String> tekstfil, final Predicate<String> shouldInclude) {
         final Matcher matcher = tekstfilNavnStruktur.matcher(tekstfil.getKey());
         if (matcher.matches()) {
             final String subKey = matcher.group(1);
             if (shouldInclude.test(subKey)) {
                 return Optional.of(tekstfil.getValue());
+            }
+        }
+        return Optional.empty();
+    }
+
+    private Optional<String> findMatchingSubKey(final Pattern tekstfilNavnStruktur, final Entry<String, String> tekstfil, final Predicate<String> shouldInclude) {
+        final Matcher matcher = tekstfilNavnStruktur.matcher(tekstfil.getKey());
+        if (matcher.matches()) {
+            final String subKey = matcher.group(1);
+            if (shouldInclude.test(subKey)) {
+                return Optional.of(subKey);
             }
         }
         return Optional.empty();
