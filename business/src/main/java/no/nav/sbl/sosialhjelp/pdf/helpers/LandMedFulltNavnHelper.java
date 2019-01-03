f9570257652988aa0ebd041bdfcf75c81ae53b1d
Oskar Asplin <oskar.asplin@nav.no>
2019-01-03 17:20:23 +0100

    DIGISOS-1042 La til visning av fullt navn for statsborgerskap

---
+++ business/src/main/java/no/nav/sbl/sosialhjelp/pdf/helpers/LandMedFulltNavnHelper.java
@@ -0,0 +1,34 @@
+package no.nav.sbl.sosialhjelp.pdf.helpers;
+
+import com.github.jknack.handlebars.Options;
+import no.nav.sbl.dialogarena.kodeverk.Adressekodeverk;
+import org.springframework.stereotype.Component;
+
+import javax.inject.Inject;
+import java.io.IOException;
+
+@Component
+public class LandMedFulltNavnHelper extends RegistryAwareHelper<String>{
+
+    @Inject
+    private Adressekodeverk adressekodeverk;
+
+    @Override
+    public String getNavn() {
+        return "landMedFulltNavn";
+    }
+
+    @Override
+    public String getBeskrivelse() {
+        return "Returnerer fullt navn på land som sendes inn med trebokstav-forkortelse ifølge ISO 3166";
+    }
+
+    @Override
+    public CharSequence apply(String landForkortelse, Options options) throws IOException {
+        if (landForkortelse == null) {
+            return "";
+        }
+
+        return adressekodeverk.getLand(landForkortelse);
+    }
+}
