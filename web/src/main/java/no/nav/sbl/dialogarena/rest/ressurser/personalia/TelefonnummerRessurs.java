797e150669428ef75bab86c8bcedf82753f99997
Oskar Asplin <oskar.asplin@nav.no>
2019-01-15 15:04:16 +0100

    DIGISOS-1049 Splittet ut innhenting av systemdata til telefonnummer i egen klasse

--- web/src/main/java/no/nav/sbl/dialogarena/rest/ressurser/personalia/TelefonnummerRessurs.java
+++ web/src/main/java/no/nav/sbl/dialogarena/rest/ressurser/personalia/TelefonnummerRessurs.java
@@ -1,33 +1,28 @@
 package no.nav.sbl.dialogarena.rest.ressurser.personalia;
 
-import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
-
-import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-
-import org.springframework.stereotype.Controller;
-
 import no.nav.metrics.aspects.Timed;
 import no.nav.modig.core.context.SubjectHandler;
 import no.nav.sbl.dialogarena.rest.ressurser.LegacyHelper;
 import no.nav.sbl.dialogarena.sendsoknad.domain.Faktum;
 import no.nav.sbl.dialogarena.sendsoknad.domain.WebSoknad;
-import no.nav.sbl.dialogarena.sendsoknad.domain.personalia.Personalia;
 import no.nav.sbl.dialogarena.sikkerhet.Tilgangskontroll;
 import no.nav.sbl.dialogarena.soknadinnsending.business.service.FaktaService;
 import no.nav.sbl.dialogarena.soknadinnsending.business.service.soknadservice.SoknadService;
-import no.nav.sbl.dialogarena.soknadinnsending.business.service.soknadservice.Systemdata;
-import no.nav.sbl.dialogarena.soknadinnsending.consumer.personalia.PersonaliaFletter;
+import no.nav.sbl.dialogarena.soknadinnsending.business.service.systemdata.TelefonnummerSystemdata;
 import no.nav.sbl.soknadsosialhjelp.soknad.JsonInternalSoknad;
 import no.nav.sbl.soknadsosialhjelp.soknad.common.JsonKilde;
+import no.nav.sbl.soknadsosialhjelp.soknad.personalia.JsonPersonalia;
 import no.nav.sbl.soknadsosialhjelp.soknad.personalia.JsonTelefonnummer;
 import no.nav.sbl.sosialhjelp.domain.SoknadUnderArbeid;
+import no.nav.sbl.sosialhjelp.soknadunderbehandling.SoknadUnderArbeidRepository;
+import org.springframework.stereotype.Controller;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 @Controller
 @Path("/soknader/{behandlingsId}/personalia/telefonnummer")
@@ -37,9 +32,6 @@ public class TelefonnummerRessurs {
 
     @Inject
     private SoknadService soknadService;
-        
-    @Inject
-    private PersonaliaFletter personaliaFletter;
     
     @Inject
     private FaktaService faktaService;
@@ -50,7 +42,13 @@ public class TelefonnummerRessurs {
     @Inject
     private LegacyHelper legacyHelper;
 
-    
+    @Inject
+    TelefonnummerSystemdata telefonnummerService;
+
+    @Inject
+    private SoknadUnderArbeidRepository soknadUnderArbeidRepository;
+
+
     @GET
     public TelefonnummerFrontend hentTelefonnummer(@PathParam("behandlingsId") String behandlingsId) {
         final String eier = SubjectHandler.getSubjectHandler().getUid();
@@ -58,7 +56,7 @@ public class TelefonnummerRessurs {
         final String personIdentifikator = soknad.getSoknad().getData().getPersonalia().getPersonIdentifikator().getVerdi();
         final JsonTelefonnummer telefonnummer = soknad.getSoknad().getData().getPersonalia().getTelefonnummer();
 
-        final String systemverdi = innhentSystemverdiTelefonnummer(personIdentifikator); 
+        final String systemverdi = telefonnummerService.innhentSystemverdiTelefonnummer(personIdentifikator);
         
         return new TelefonnummerFrontend()
                 .withBrukerdefinert(telefonnummer.getKilde() == JsonKilde.BRUKER)
@@ -69,8 +67,24 @@ public class TelefonnummerRessurs {
     @PUT
     public void endreTelefonnummer(@PathParam("behandlingsId") String behandlingsId, TelefonnummerFrontend telefonnummerFrontend) {
         tilgangskontroll.verifiserAtBrukerKanEndreSoknad(behandlingsId);
-
+        update(behandlingsId, telefonnummerFrontend);
         legacyUpdate(behandlingsId, telefonnummerFrontend);
+    }
+
+    private void update(String behandlingsId, TelefonnummerFrontend telefonnummerFrontend) {
+        final String eier = SubjectHandler.getSubjectHandler().getUid();
+        final SoknadUnderArbeid soknad = soknadUnderArbeidRepository.hentSoknad(behandlingsId, eier).get();
+        final JsonPersonalia personalia = soknad.getJsonInternalSoknad().getSoknad().getData().getPersonalia();
+        final JsonTelefonnummer telefonnummer = personalia.getTelefonnummer();
+        final String personIdentifikator = personalia.getPersonIdentifikator().getVerdi();
+        if (telefonnummerFrontend.brukerdefinert) {
+            telefonnummer.setKilde(JsonKilde.BRUKER);
+            telefonnummer.setVerdi(telefonnummerFrontend.verdi);
+        } else if (telefonnummer.getKilde() == JsonKilde.BRUKER) {
+            telefonnummer.setKilde(JsonKilde.SYSTEM);
+            telefonnummer.setVerdi(telefonnummerService.innhentSystemverdiTelefonnummer(personIdentifikator));
+        }
+        soknadUnderArbeidRepository.oppdaterSoknadsdata(soknad, eier);
     }
 
     private void legacyUpdate(String behandlingsId, TelefonnummerFrontend telefonnummerFrontend) {
@@ -84,27 +98,7 @@ public class TelefonnummerRessurs {
         telefon.setValue(telefonnummerFrontend.verdi);
         faktaService.lagreBrukerFaktum(telefon);
     }
-    
 
-    private String innhentSystemverdiTelefonnummer(final String personIdentifikator) {
-        final Personalia personalia = personaliaFletter.mapTilPersonalia(personIdentifikator);
-        final String systemVerdi = norskTelefonnummer(personalia.getMobiltelefonnummer());
-        return systemVerdi;
-    }
-    
-    static String norskTelefonnummer(String mobiltelefonnummer) {
-        if (mobiltelefonnummer == null) {
-            return null;
-        }
-        if (mobiltelefonnummer.length() == 8) {
-            return "+47" + mobiltelefonnummer;
-        }
-        if (mobiltelefonnummer.startsWith("+47") && mobiltelefonnummer.length() == 11) {
-            return mobiltelefonnummer;
-        }
-        return null;
-    }
-   
 
     @XmlAccessorType(XmlAccessType.FIELD)
     public static final class TelefonnummerFrontend {
