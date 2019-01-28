f2612b51071f458ac4fca976f344efc008d144a0
Oskar Asplin <oskar.asplin@nav.no>
2019-01-28 16:10:49 +0100

    DIGISOS-1049 Fullførte SivilstatusRessurs og la til tester

--- web/src/main/java/no/nav/sbl/dialogarena/rest/ressurser/familie/SivilstatusRessurs.java
+++ web/src/main/java/no/nav/sbl/dialogarena/rest/ressurser/familie/SivilstatusRessurs.java
@@ -4,20 +4,27 @@ import no.nav.metrics.aspects.Timed;
 import no.nav.modig.core.context.SubjectHandler;
 import no.nav.sbl.dialogarena.rest.ressurser.LegacyHelper;
 import no.nav.sbl.dialogarena.rest.ressurser.NavnFrontend;
+import no.nav.sbl.dialogarena.sendsoknad.domain.Faktum;
 import no.nav.sbl.dialogarena.sendsoknad.domain.WebSoknad;
 import no.nav.sbl.dialogarena.sikkerhet.Tilgangskontroll;
+import no.nav.sbl.dialogarena.soknadinnsending.business.service.FaktaService;
 import no.nav.sbl.dialogarena.soknadinnsending.business.service.soknadservice.SoknadService;
 import no.nav.sbl.soknadsosialhjelp.soknad.JsonInternalSoknad;
 import no.nav.sbl.soknadsosialhjelp.soknad.common.JsonKilde;
 import no.nav.sbl.soknadsosialhjelp.soknad.common.JsonNavn;
 import no.nav.sbl.soknadsosialhjelp.soknad.familie.JsonEktefelle;
+import no.nav.sbl.soknadsosialhjelp.soknad.familie.JsonFamilie;
 import no.nav.sbl.soknadsosialhjelp.soknad.familie.JsonSivilstatus;
+import no.nav.sbl.sosialhjelp.domain.SoknadUnderArbeid;
+import no.nav.sbl.sosialhjelp.soknadunderbehandling.SoknadUnderArbeidRepository;
 import org.springframework.stereotype.Controller;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import java.util.HashMap;
+import java.util.Map;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
@@ -27,9 +34,6 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 @Produces(APPLICATION_JSON)
 public class SivilstatusRessurs {
 
-    /*@Inject
-    FamilieSystemdata familieSystemdata;*/
-
     @Inject
     private LegacyHelper legacyHelper;
 
@@ -38,6 +42,12 @@ public class SivilstatusRessurs {
 
     @Inject
     private SoknadService soknadService;
+
+    @Inject
+    private FaktaService faktaService;
+
+    @Inject
+    private SoknadUnderArbeidRepository soknadUnderArbeidRepository;
 
     @GET
     public SivilstatusFrontend hentSivilstatus(@PathParam("behandlingsId") String behandlingsId){
@@ -59,24 +69,56 @@ public class SivilstatusRessurs {
         legacyUpdate(behandlingsId, sivilstatusFrontend);
     }
 
+    private void update(String behandlingsId, SivilstatusFrontend sivilstatusFrontend) {
+        final String eier = SubjectHandler.getSubjectHandler().getUid();
+        final SoknadUnderArbeid soknad = soknadUnderArbeidRepository.hentSoknad(behandlingsId, eier).get();
+        final JsonFamilie familie = soknad.getJsonInternalSoknad().getSoknad().getData().getFamilie();
+
+        if (familie.getSivilstatus() == null){
+            soknad.getJsonInternalSoknad().getSoknad().getData().getFamilie().setSivilstatus(new JsonSivilstatus());
+        }
+
+        final JsonSivilstatus sivilstatus = familie.getSivilstatus();
+        sivilstatus.setKilde(JsonKilde.BRUKER);
+        sivilstatus.setStatus(sivilstatusFrontend.sivilstatus);
+        sivilstatus.setEktefelle(mapToJsonEktefelle(sivilstatusFrontend.ektefelle));
+        sivilstatus.setBorSammenMed(sivilstatusFrontend.borSammenMed);
+
+        soknadUnderArbeidRepository.oppdaterSoknadsdata(soknad, eier);
+    }
+
     private void legacyUpdate(String behandlingsId, SivilstatusFrontend sivilstatusFrontend) {
         final WebSoknad webSoknad = soknadService.hentSoknad(behandlingsId, false, false);
 
-        final String sivilstatus = webSoknad.getValueForFaktum("system.familie.sivilstatus");
+        final Faktum sivilstatus = faktaService.hentFaktumMedKey(webSoknad.getSoknadId(), "familie.sivilstatus");
+        sivilstatus.setType(Faktum.FaktumType.BRUKERREGISTRERT);
+        sivilstatus.setValue(sivilstatusFrontend.sivilstatus.toString());
+        faktaService.lagreBrukerFaktum(sivilstatus);
+
+        final Faktum ektefelle = faktaService.hentFaktumMedKey(webSoknad.getSoknadId(), "familie.sivilstatus.gift.ektefelle");
+        sivilstatus.setType(Faktum.FaktumType.BRUKERREGISTRERT);
+
+        final Map<String, String> ektefelleProperties = getFaktumProperties(ektefelle);
+        EktefelleFrontend ektefelleFrontend = sivilstatusFrontend.ektefelle;
+        if (ektefelleFrontend != null){
+            ektefelleProperties.put("fornavn", ektefelleFrontend.navn.fornavn);
+            ektefelleProperties.put("mellomnavn", ektefelleFrontend.navn.mellomnavn);
+            ektefelleProperties.put("etternavn", ektefelleFrontend.navn.etternavn);
+            ektefelleProperties.put("fodselsdato", ektefelleFrontend.fodselsdato);
+            ektefelleProperties.put("fnr", ektefelleFrontend.personIdentifikator);
+        }
+        ektefelleProperties.put("borsammen", sivilstatusFrontend.borSammenMed != null ? sivilstatusFrontend.borSammenMed.toString() : null);
+        if (!ektefelleProperties.isEmpty()){
+            ektefelle.setProperties(ektefelleProperties);
+        }
+        faktaService.lagreBrukerFaktum(ektefelle);
     }
 
-    private void update(String behandlingsId, SivilstatusFrontend sivilstatusFrontend) {
-
-    }
-
-    private SivilstatusFrontend mapToSivilstatusFrontend(JsonSivilstatus jsonSivilstatus) {
-        return new SivilstatusFrontend()
-                .withSystemKilde(mapToSystemBoolean(jsonSivilstatus.getKilde()))
-                .withSivilstatus(jsonSivilstatus.getStatus())
-                .withEktefelle(jsonSivilstatus.getEktefelle() == null ? null : addEktefelleFrontend(jsonSivilstatus.getEktefelle()))
-                .withEktefelleHarDiskresjonskode(jsonSivilstatus.getEktefelleHarDiskresjonskode())
-                .withFolkeregistrertMedEktefelle(jsonSivilstatus.getFolkeregistrertMedEktefelle())
-                .withBorSammenMed(jsonSivilstatus.getBorSammenMed());
+    private static Map<String, String> getFaktumProperties(Faktum faktum) {
+        if (faktum == null) {
+            return new HashMap<>();
+        }
+        return faktum.getProperties();
     }
 
     private EktefelleFrontend addEktefelleFrontend(JsonEktefelle jsonEktefelle) {
@@ -85,6 +127,32 @@ public class SivilstatusRessurs {
                 .withNavn(new NavnFrontend(navn.getFornavn(), navn.getMellomnavn(), navn.getEtternavn()))
                 .withFodselsdato(jsonEktefelle.getFodselsdato())
                 .withPersonIdentifikator(jsonEktefelle.getPersonIdentifikator());
+    }
+
+    private JsonEktefelle mapToJsonEktefelle(EktefelleFrontend ektefelle) {
+        if(ektefelle == null){
+            return null;
+        }
+        return new JsonEktefelle().withNavn(mapToJsonNavn(ektefelle.navn))
+                .withFodselsdato(ektefelle.fodselsdato)
+                .withPersonIdentifikator(ektefelle.personIdentifikator);
+    }
+
+    private SivilstatusFrontend mapToSivilstatusFrontend(JsonSivilstatus jsonSivilstatus) {
+        return new SivilstatusFrontend()
+                .withKildeErSystem(mapToSystemBoolean(jsonSivilstatus.getKilde()))
+                .withSivilstatus(jsonSivilstatus.getStatus())
+                .withEktefelle(jsonSivilstatus.getEktefelle() == null ? null : addEktefelleFrontend(jsonSivilstatus.getEktefelle()))
+                .withEktefelleHarDiskresjonskode(jsonSivilstatus.getEktefelleHarDiskresjonskode())
+                .withFolkeregistrertMedEktefelle(jsonSivilstatus.getFolkeregistrertMedEktefelle())
+                .withBorSammenMed(jsonSivilstatus.getBorSammenMed());
+    }
+
+    private JsonNavn mapToJsonNavn(NavnFrontend navn) {
+        return new JsonNavn()
+                .withFornavn(navn.fornavn != null ? navn.fornavn : "")
+                .withMellomnavn(navn.mellomnavn != null ? navn.mellomnavn : "")
+                .withEtternavn(navn.etternavn != null ? navn.etternavn : "");
     }
 
     private Boolean mapToSystemBoolean(JsonKilde kilde) {
@@ -100,14 +168,15 @@ public class SivilstatusRessurs {
 
     @XmlAccessorType(XmlAccessType.FIELD)
     public static final class SivilstatusFrontend {
-        public Boolean systemKilde;
+        public Boolean kildeErSystem;
         public JsonSivilstatus.Status sivilstatus;
         public EktefelleFrontend ektefelle;
         public Boolean ektefelleHarDiskresjonskode;
         public Boolean folkeregistrertMedEktefelle;
         public Boolean borSammenMed;
-        public SivilstatusFrontend withSystemKilde(Boolean systemKilde) {
-            this.systemKilde = systemKilde;
+
+        public SivilstatusFrontend withKildeErSystem(Boolean kildeErSystem) {
+            this.kildeErSystem = kildeErSystem;
             return this;
         }
 
---
+++ web/src/test/java/no/nav/sbl/dialogarena/rest/ressurser/familie/SivilstatusRessursTest.java
@@ -0,0 +1,244 @@
+package no.nav.sbl.dialogarena.rest.ressurser.familie;
+
+import no.nav.modig.core.context.StaticSubjectHandler;
+import no.nav.sbl.dialogarena.rest.ressurser.LegacyHelper;
+import no.nav.sbl.dialogarena.rest.ressurser.NavnFrontend;
+import no.nav.sbl.dialogarena.rest.ressurser.familie.SivilstatusRessurs.EktefelleFrontend;
+import no.nav.sbl.dialogarena.rest.ressurser.familie.SivilstatusRessurs.SivilstatusFrontend;
+import no.nav.sbl.dialogarena.sendsoknad.domain.Faktum;
+import no.nav.sbl.dialogarena.sendsoknad.domain.WebSoknad;
+import no.nav.sbl.dialogarena.sikkerhet.Tilgangskontroll;
+import no.nav.sbl.dialogarena.soknadinnsending.business.service.FaktaService;
+import no.nav.sbl.dialogarena.soknadinnsending.business.service.soknadservice.SoknadService;
+import no.nav.sbl.soknadsosialhjelp.soknad.JsonData;
+import no.nav.sbl.soknadsosialhjelp.soknad.JsonInternalSoknad;
+import no.nav.sbl.soknadsosialhjelp.soknad.JsonSoknad;
+import no.nav.sbl.soknadsosialhjelp.soknad.common.JsonKilde;
+import no.nav.sbl.soknadsosialhjelp.soknad.common.JsonNavn;
+import no.nav.sbl.soknadsosialhjelp.soknad.familie.JsonEktefelle;
+import no.nav.sbl.soknadsosialhjelp.soknad.familie.JsonFamilie;
+import no.nav.sbl.soknadsosialhjelp.soknad.familie.JsonSivilstatus;
+import no.nav.sbl.sosialhjelp.domain.SoknadUnderArbeid;
+import no.nav.sbl.sosialhjelp.soknadunderbehandling.SoknadUnderArbeidRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.Optional;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SivilstatusRessursTest {
+
+    private static final String BEHANDLINGSID = "123";
+    private static final JsonEktefelle JSON_EKTEFELLE = new JsonEktefelle()
+            .withNavn(new JsonNavn()
+                    .withFornavn("Alfred")
+                    .withMellomnavn("Thaddeus Crane")
+                    .withEtternavn("Pennyworth"))
+            .withFodselsdato("1940-01-01")
+            .withPersonIdentifikator("01014012345");
+
+    private static final EktefelleFrontend EKTEFELLE_FRONTEND = new EktefelleFrontend()
+            .withNavn(new NavnFrontend("Alfred", "Thaddeus Crane", "Pennyworth"))
+            .withFodselsdato("1940-01-01")
+            .withPersonIdentifikator("01014012345");
+
+    @Mock
+    private LegacyHelper legacyHelper;
+
+    @InjectMocks
+    private SivilstatusRessurs sivilstatusRessurs;
+
+    @Mock
+    private Tilgangskontroll tilgangskontroll;
+
+    @Mock
+    private SoknadService soknadService;
+
+    @Mock
+    private FaktaService faktaService;
+
+    @Mock
+    private SoknadUnderArbeidRepository soknadUnderArbeidRepository;
+
+    @Before
+    public void setUp() {
+        System.setProperty("no.nav.modig.core.context.subjectHandlerImplementationClass", StaticSubjectHandler.class.getName());
+    }
+
+    @Test
+    public void getSivilstatusSkalReturnereNull(){
+        when(legacyHelper.hentSoknad(anyString(), anyString())).thenReturn(
+                createJsonInternalSoknadWithSivilstatus(null, null, null,
+                        null, null, null));
+
+        final SivilstatusFrontend sivilstatusFrontend = sivilstatusRessurs.hentSivilstatus(BEHANDLINGSID);
+
+        assertThat(sivilstatusFrontend, nullValue());
+    }
+
+    @Test
+    public void getSivilstatusSkalReturnereKunBrukerdefinertStatus(){
+        when(legacyHelper.hentSoknad(anyString(), anyString())).thenReturn(
+                createJsonInternalSoknadWithSivilstatus(true, JsonSivilstatus.Status.GIFT, null,
+                        null, null, null));
+
+        final SivilstatusFrontend sivilstatusFrontend = sivilstatusRessurs.hentSivilstatus(BEHANDLINGSID);
+
+        assertThat(sivilstatusFrontend.sivilstatus, is(JsonSivilstatus.Status.GIFT));
+        assertThat(sivilstatusFrontend.kildeErSystem, is(false));
+        assertThat(sivilstatusFrontend.ektefelle, nullValue());
+        assertThat(sivilstatusFrontend.ektefelleHarDiskresjonskode, nullValue());
+        assertThat(sivilstatusFrontend.folkeregistrertMedEktefelle, nullValue());
+        assertThat(sivilstatusFrontend.borSammenMed, nullValue());
+    }
+
+    @Test
+    public void getSivilstatusSkalReturnereBrukerdefinertEktefelleRiktigKonvertert(){
+        when(legacyHelper.hentSoknad(anyString(), anyString())).thenReturn(
+                createJsonInternalSoknadWithSivilstatus(true, JsonSivilstatus.Status.GIFT, JSON_EKTEFELLE,
+                        null, null, true));
+
+        final SivilstatusFrontend sivilstatusFrontend = sivilstatusRessurs.hentSivilstatus(BEHANDLINGSID);
+
+        assertThat(sivilstatusFrontend.kildeErSystem, is(false));
+        assertThat(sivilstatusFrontend.sivilstatus, is(JsonSivilstatus.Status.GIFT));
+        assertThatEktefelleIsCorrectlyConverted(sivilstatusFrontend.ektefelle, JSON_EKTEFELLE);
+        assertThat(sivilstatusFrontend.ektefelleHarDiskresjonskode, nullValue());
+        assertThat(sivilstatusFrontend.folkeregistrertMedEktefelle, nullValue());
+        assertThat(sivilstatusFrontend.borSammenMed, is(true));
+    }
+
+    @Test
+    public void getSivilstatusSkalReturnereSystemdefinertEktefelleRiktigKonvertert(){
+        when(legacyHelper.hentSoknad(anyString(), anyString())).thenReturn(
+                createJsonInternalSoknadWithSivilstatus(false, JsonSivilstatus.Status.GIFT, JSON_EKTEFELLE,
+                        false, true, null));
+
+        final SivilstatusFrontend sivilstatusFrontend = sivilstatusRessurs.hentSivilstatus(BEHANDLINGSID);
+
+        assertThat(sivilstatusFrontend.kildeErSystem, is(true));
+        assertThat(sivilstatusFrontend.sivilstatus, is(JsonSivilstatus.Status.GIFT));
+        assertThatEktefelleIsCorrectlyConverted(sivilstatusFrontend.ektefelle, JSON_EKTEFELLE);
+        assertThat(sivilstatusFrontend.ektefelleHarDiskresjonskode, is(false));
+        assertThat(sivilstatusFrontend.folkeregistrertMedEktefelle, is(true));
+        assertThat(sivilstatusFrontend.borSammenMed, nullValue());
+    }
+
+    @Test
+    public void getSivilstatusSkalReturnereSystemdefinertEktefelleMedDiskresjonskode(){
+        when(legacyHelper.hentSoknad(anyString(), anyString())).thenReturn(
+                createJsonInternalSoknadWithSivilstatus(false, JsonSivilstatus.Status.GIFT, JSON_EKTEFELLE,
+                        true, null, null));
+
+        final SivilstatusFrontend sivilstatusFrontend = sivilstatusRessurs.hentSivilstatus(BEHANDLINGSID);
+
+        assertThat(sivilstatusFrontend.kildeErSystem, is(true));
+        assertThat(sivilstatusFrontend.sivilstatus, is(JsonSivilstatus.Status.GIFT));
+        assertThat(sivilstatusFrontend.ektefelleHarDiskresjonskode, is(true));
+        assertThat(sivilstatusFrontend.folkeregistrertMedEktefelle, nullValue());
+        assertThat(sivilstatusFrontend.borSammenMed, nullValue());
+    }
+
+    @Test
+    public void putSivilstatusSkalKunneSetteAlleTyperSivilstatus(){
+        ignoreTilgangskontrollAndLegacyUpdate();
+        when(soknadUnderArbeidRepository.hentSoknad(anyString(), anyString())).thenReturn(
+                Optional.of(createJsonInternalSoknadWithSivilstatus(null, null, null,
+                        null, null, null)));
+
+        assertThatPutSivilstatusSetterRiktigStatus(JsonSivilstatus.Status.GIFT);
+        assertThatPutSivilstatusSetterRiktigStatus(JsonSivilstatus.Status.ENKE);
+        assertThatPutSivilstatusSetterRiktigStatus(JsonSivilstatus.Status.SAMBOER);
+        assertThatPutSivilstatusSetterRiktigStatus(JsonSivilstatus.Status.SEPARERT);
+        assertThatPutSivilstatusSetterRiktigStatus(JsonSivilstatus.Status.SKILT);
+        assertThatPutSivilstatusSetterRiktigStatus(JsonSivilstatus.Status.UGIFT);
+    }
+
+    @Test
+    public void putSivilstatusSkalSetteStatusGiftOgEktefelle(){
+        ignoreTilgangskontrollAndLegacyUpdate();
+        when(soknadUnderArbeidRepository.hentSoknad(anyString(), anyString())).thenReturn(
+                Optional.of(createJsonInternalSoknadWithSivilstatus(null, null, null,
+                        null, null, null)));
+
+        final SivilstatusFrontend sivilstatusFrontend = new SivilstatusFrontend()
+                .withKildeErSystem(false).withSivilstatus(JsonSivilstatus.Status.GIFT)
+                .withEktefelle(EKTEFELLE_FRONTEND);
+
+        sivilstatusRessurs.updateSivilstatus(BEHANDLINGSID, sivilstatusFrontend);
+
+        final SoknadUnderArbeid soknadUnderArbeid = catchSoknadUnderArbeidSentToOppdaterSoknadsdata();
+        final JsonSivilstatus sivilstatus = soknadUnderArbeid.getJsonInternalSoknad().getSoknad().getData().getFamilie().getSivilstatus();
+        assertThat(sivilstatus.getKilde(), is(JsonKilde.BRUKER));
+        assertThat(sivilstatus.getStatus(), is(JsonSivilstatus.Status.GIFT));
+        assertThatEktefelleIsCorrectlyConverted(EKTEFELLE_FRONTEND, sivilstatus.getEktefelle());
+    }
+
+    private void assertThatEktefelleIsCorrectlyConverted(EktefelleFrontend ektefelle, JsonEktefelle jsonEktefelle) {
+        assertThat("fodselsdato", ektefelle.fodselsdato, is(jsonEktefelle.getFodselsdato()));
+        assertThat("personIdentifikator", ektefelle.personIdentifikator, is(jsonEktefelle.getPersonIdentifikator()));
+        assertThat("fornavn", ektefelle.navn.fornavn, is(jsonEktefelle.getNavn().getFornavn()));
+        assertThat("mellomnavn", ektefelle.navn.mellomnavn, is(jsonEktefelle.getNavn().getMellomnavn()));
+        assertThat("etternavn", ektefelle.navn.etternavn, is(jsonEktefelle.getNavn().getEtternavn()));
+    }
+
+    private void assertThatPutSivilstatusSetterRiktigStatus(JsonSivilstatus.Status status) {
+        final SivilstatusFrontend sivilstatusFrontend = new SivilstatusFrontend()
+                .withKildeErSystem(false).withSivilstatus(status);
+
+        sivilstatusRessurs.updateSivilstatus(BEHANDLINGSID, sivilstatusFrontend);
+
+        final SoknadUnderArbeid soknadUnderArbeid = catchSoknadUnderArbeidSentToOppdaterSoknadsdata();
+        final JsonSivilstatus sivilstatus = soknadUnderArbeid.getJsonInternalSoknad().getSoknad().getData().getFamilie().getSivilstatus();
+        assertThat("kilde", sivilstatus.getKilde(), is(JsonKilde.BRUKER));
+        assertThat("status", sivilstatus.getStatus(), is(status));
+    }
+
+    private SoknadUnderArbeid catchSoknadUnderArbeidSentToOppdaterSoknadsdata() {
+        ArgumentCaptor<SoknadUnderArbeid> argument = ArgumentCaptor.forClass(SoknadUnderArbeid.class);
+        verify(soknadUnderArbeidRepository, atLeastOnce()).oppdaterSoknadsdata(argument.capture(), anyString());
+        return argument.getValue();
+    }
+
+    private void ignoreTilgangskontrollAndLegacyUpdate() {
+        doNothing().when(tilgangskontroll).verifiserAtBrukerKanEndreSoknad(anyString());
+        when(soknadService.hentSoknad(anyString(), anyBoolean(), anyBoolean())).thenReturn(new WebSoknad());
+        when(faktaService.hentFaktumMedKey(anyLong(), anyString())).thenReturn(new Faktum());
+        when(faktaService.lagreBrukerFaktum(any(Faktum.class))).thenReturn(new Faktum());
+    }
+
+    private SoknadUnderArbeid createJsonInternalSoknadWithSivilstatus(Boolean brukerutfylt, JsonSivilstatus.Status status,
+                                                                      JsonEktefelle ektefelle, Boolean harDiskresjonskode,
+                                                                      Boolean folkeregistrertMed, Boolean borSammen) {
+        return new SoknadUnderArbeid()
+                .withJsonInternalSoknad(new JsonInternalSoknad()
+                        .withSoknad(new JsonSoknad()
+                                .withData(new JsonData()
+                                        .withFamilie(new JsonFamilie()
+                                                .withSivilstatus(brukerutfylt == null ? null :
+                                                        new JsonSivilstatus()
+                                                                .withKilde(brukerutfylt ? JsonKilde.BRUKER : JsonKilde.SYSTEM)
+                                                                .withStatus(status)
+                                                                .withEktefelle(ektefelle)
+                                                                .withEktefelleHarDiskresjonskode(harDiskresjonskode)
+                                                                .withFolkeregistrertMedEktefelle(folkeregistrertMed)
+                                                                .withBorSammenMed(borSammen)
+                                                )
+                                        )
+                                )
+                        )
+                );
+    }
+
+}
