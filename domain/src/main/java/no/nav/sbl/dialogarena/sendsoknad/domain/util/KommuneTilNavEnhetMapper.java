191f5e39f397838b6019343986b195cc915cec18
André Wolden <andre.wolden@nav.no>
2019-02-04 11:03:04 +0100

    kobler paa Balsfjord Storfjord Vefsn og Klaebu

--- domain/src/main/java/no/nav/sbl/dialogarena/sendsoknad/domain/util/KommuneTilNavEnhetMapper.java
+++ domain/src/main/java/no/nav/sbl/dialogarena/sendsoknad/domain/util/KommuneTilNavEnhetMapper.java
@@ -51,7 +51,7 @@ public class KommuneTilNavEnhetMapper {
             .put("0334", "970145311")    // Vestre Aker, Oslo kommune
             .put("0321", "974778807")    // Østensjø, Oslo kommune
             .put("5701", "892284792")    // Falkenborg, Trondheim kommune
-            .put("5702", "992284838")    // Lerkendal, Trondheim kommune
+            .put("5702", "992284838")    // Lerkendal, Trondheim kommune & Klæbu
             .put("1161", "873864192")    // Eiganes og Tasta, Stavanger kommune
             .put("1164", "976670531")    // Hillevåg og Hinna, Stavanger kommune
             .put("1162", "973864181")    // Hundvåg og Storhaug, Stavanger kommune
@@ -73,6 +73,8 @@ public class KommuneTilNavEnhetMapper {
             .put("0532", "974596016")    // Jevnaker
             .put("1401", "974551918")    // Flora
             .put("1238", "944233199")    // Kvam
+            .put("1933", "921858361")    // Balsfjord & Storfjord
+            .put("1824", "991379657")    // Vefsn
 
             .build();
 
@@ -281,7 +283,11 @@ public class KommuneTilNavEnhetMapper {
                     "1134",
                     "0532",
                     "1401",
-                    "1238"
+                    "1238",
+                    "1933",
+                    "1939",
+                    "1824",
+                    "5030"
             ));
 
     private static final Map<String, String> TEST_KOMMUNER_MED_BYDELER = new ImmutableMap.Builder<String, String>()
