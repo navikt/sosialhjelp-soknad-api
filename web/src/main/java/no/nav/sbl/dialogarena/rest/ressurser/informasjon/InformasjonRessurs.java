43a7967fa1561126f2e796555fe94c8fdd09324a
Flemming, Thomas <thomas.flemming@gmail.com>
2019-03-11 14:59:27 +0100

    Refaktorisering etter kommentar av Silje

--- web/src/main/java/no/nav/sbl/dialogarena/rest/ressurser/informasjon/InformasjonRessurs.java
+++ web/src/main/java/no/nav/sbl/dialogarena/rest/ressurser/informasjon/InformasjonRessurs.java
@@ -4,8 +4,6 @@ import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.toList;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static no.nav.modig.core.context.SubjectHandler.getSubjectHandler;
-import static no.nav.sbl.dialogarena.sendsoknad.domain.util.KommuneTilNavEnhetMapper.PROD_ORGANISASJONSNUMMER;
-import static no.nav.sbl.dialogarena.sendsoknad.domain.util.KommuneTilNavEnhetMapper.TEST_ORGANISASJONSNUMMER;
 import static org.apache.commons.lang3.StringUtils.isNotEmpty;
 
 import java.util.ArrayList;
