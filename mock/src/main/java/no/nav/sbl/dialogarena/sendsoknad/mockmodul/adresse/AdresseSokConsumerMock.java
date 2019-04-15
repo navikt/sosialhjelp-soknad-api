86afae2cd1cad6a8731f812e8254d275287fac83
Tveter, Martin <Martin.Skjelsvik.Tveter@nav.no>
2019-04-15 12:35:30 +0200

    DIGISOS-1171: Lagt til flere adresser i AdresseSokMock

--- mock/src/main/java/no/nav/sbl/dialogarena/sendsoknad/mockmodul/adresse/AdresseSokConsumerMock.java
+++ mock/src/main/java/no/nav/sbl/dialogarena/sendsoknad/mockmodul/adresse/AdresseSokConsumerMock.java
@@ -1,67 +1,81 @@
 package no.nav.sbl.dialogarena.sendsoknad.mockmodul.adresse;
 
-import static org.mockito.Matchers.anyString;
+import static java.util.stream.Collectors.toList;
+import static no.nav.sbl.dialogarena.sendsoknad.mockmodul.adresse.AdresseSokMockHelper.adresseDataList;
+import static no.nav.sbl.dialogarena.sendsoknad.mockmodul.adresse.AdresseSokMockHelper.bergen;
+import static no.nav.sbl.dialogarena.sendsoknad.mockmodul.adresse.AdresseSokMockHelper.bergenLowercase;
+import static no.nav.sbl.dialogarena.sendsoknad.mockmodul.adresse.AdresseSokMockHelper.kristiansand;
+import static no.nav.sbl.dialogarena.sendsoknad.mockmodul.adresse.AdresseSokMockHelper.kristiansandLowercase;
+import static no.nav.sbl.dialogarena.sendsoknad.mockmodul.adresse.AdresseSokMockHelper.oslo;
+import static no.nav.sbl.dialogarena.sendsoknad.mockmodul.adresse.AdresseSokMockHelper.osloLowercase;
+import static no.nav.sbl.dialogarena.sendsoknad.mockmodul.adresse.AdresseSokMockHelper.svalbard;
+import static no.nav.sbl.dialogarena.sendsoknad.mockmodul.adresse.AdresseSokMockHelper.svalbardLowercase;
+import static no.nav.sbl.dialogarena.sendsoknad.mockmodul.adresse.AdresseSokMockHelper.tromso;
+import static no.nav.sbl.dialogarena.sendsoknad.mockmodul.adresse.AdresseSokMockHelper.tromsoLowercase;
+import static no.nav.sbl.dialogarena.sendsoknad.mockmodul.adresse.AdresseSokMockHelper.trondheim;
+import static no.nav.sbl.dialogarena.sendsoknad.mockmodul.adresse.AdresseSokMockHelper.trondheimLowercase;
+import static org.mockito.AdditionalMatchers.or;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.refEq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.slf4j.LoggerFactory.getLogger;
 
-import java.util.Arrays;
-
+import no.nav.sbl.dialogarena.sendsoknad.domain.adresse.AdresseSokConsumer;
+import no.nav.sbl.dialogarena.sendsoknad.domain.adresse.AdresseSokConsumer.AdressesokRespons;
+import no.nav.sbl.dialogarena.sendsoknad.domain.adresse.AdresseSokConsumer.Sokedata;
 import org.mockito.stubbing.Answer;
 import org.slf4j.Logger;
 
-import no.nav.sbl.dialogarena.sendsoknad.domain.adresse.AdresseSokConsumer;
-import no.nav.sbl.dialogarena.sendsoknad.domain.adresse.AdresseSokConsumer.AdresseData;
-import no.nav.sbl.dialogarena.sendsoknad.domain.adresse.AdresseSokConsumer.AdressesokRespons;
-import no.nav.sbl.dialogarena.sendsoknad.domain.adresse.AdresseSokConsumer.Sokedata;
 
 public class AdresseSokConsumerMock {
-    private static final Logger logger = getLogger(AdresseSokConsumerMock.class);
+	private static final Logger logger = getLogger(AdresseSokConsumerMock.class);
 
-    public AdresseSokConsumer adresseRestService() {
-        AdresseSokConsumer mock = mock(AdresseSokConsumer.class);
+	public AdresseSokConsumer adresseRestService() {
+		AdresseSokConsumer mock = mock(AdresseSokConsumer.class);
 
-        when(mock.sokAdresse(any(Sokedata.class))).thenAnswer(adressesokResponsMock());
-        when(mock.sokAdresse(anyString())).thenAnswer(adressesokResponsMock());
+		when(mock.sokAdresse(any(Sokedata.class))).thenAnswer(adressesokResponsMock());
 
-        return mock;
-    }
+		when(mock.sokAdresse(or(refEq(oslo), refEq(osloLowercase)))).thenAnswer(mockAnswer("Oslo"));
+		when(mock.sokAdresse(or(refEq(bergen), refEq(bergenLowercase)))).thenAnswer(mockAnswer("Bergen"));
+		when(mock.sokAdresse(or(refEq(trondheim), refEq(trondheimLowercase)))).thenAnswer(mockAnswer("Trondheim"));
+		when(mock.sokAdresse(or(refEq(tromso), refEq(tromsoLowercase)))).thenAnswer(mockAnswer("Tromsø"));
+		when(mock.sokAdresse(or(refEq(kristiansand), refEq(kristiansandLowercase)))).thenAnswer(mockAnswer("Kristiansand"));
+		when(mock.sokAdresse(or(refEq(svalbard), refEq(svalbardLowercase)))).thenAnswer(mockAnswer("Svalbard"));
 
-    private Answer<?> adressesokResponsMock() {
-        return invocation -> {
-            Object param = invocation.getArgumentAt(0, Object.class);
+		when(mock.sokAdresse(anyString())).thenAnswer(adressesokResponsMock());
 
-            logger.info("Mocker respons til adressesok for søk: {}", param);
+		return mock;
+	}
 
-            AdressesokRespons respons = new AdressesokRespons();
-            final AdresseData a1 = new AdresseData();
-            a1.kommunenummer = "1201";
-            a1.kommunenavn = "Bergen";
-            a1.adressenavn = "SANNERGATA";
-            a1.husnummerFra = "0001";
-            a1.husnummerTil = "0010";
-            a1.postnummer = "1337";
-            a1.poststed = "Leet";
-            a1.geografiskTilknytning = "120102";
-            a1.gatekode = "02081";
-            a1.bydel = "120102";
-            
-            final AdresseData a2 = new AdresseData();
-            a2.kommunenummer = "1201";
-            a2.kommunenavn = "Bergen";
-            a2.adressenavn = "SANNERGATA";
-            a2.husnummerFra = "0011";
-            a2.husnummerTil = "9999";
-            a2.postnummer = "1337";
-            a2.poststed = "Leet";
-            a2.geografiskTilknytning = "120107";
-            a2.gatekode = "02081";
-            a2.bydel = "120107";
+	private Answer<?> adressesokResponsMock() {
+		return invocation -> {
+			Object param = invocation.getArgumentAt(0, Object.class);
 
-            respons.adresseDataList = Arrays.asList(a1, a2);
-            
-            return respons;
-        };
-    }
+			logger.info("Mocker respons til adressesok for søk: {}", param);
+
+			AdressesokRespons respons = new AdressesokRespons();
+
+			respons.adresseDataList = adresseDataList;
+
+			return respons;
+		};
+	}
+
+	private Answer<?> mockAnswer(String adresse) {
+		return invocation -> {
+			Object param = invocation.getArgumentAt(0, Object.class);
+
+			logger.info("Mocker respons til adressesok for søk: {}", param);
+
+			AdressesokRespons respons = new AdressesokRespons();
+
+			respons.adresseDataList = adresseDataList.stream().filter(adresseData -> adresse.equals(adresseData.kommunenavn)).collect(toList());
+
+			return respons;
+		};
+	}
+
+
 }
