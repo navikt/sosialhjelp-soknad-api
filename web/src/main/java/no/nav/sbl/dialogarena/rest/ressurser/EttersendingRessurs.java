900a0d313a9803a545f1a246063fcf5292fafa02
Oskar Asplin <oskarasplin@gmail.com>
2019-05-07 13:53:22 +0200

    DIGISOS-1136 La på konvertering av vedlegg fra faktum dersom noe er lastet opp

--- web/src/main/java/no/nav/sbl/dialogarena/rest/ressurser/EttersendingRessurs.java
+++ web/src/main/java/no/nav/sbl/dialogarena/rest/ressurser/EttersendingRessurs.java
@@ -1,12 +1,15 @@
 package no.nav.sbl.dialogarena.rest.ressurser;
 
 import no.nav.metrics.aspects.Timed;
+import no.nav.sbl.dialogarena.sendsoknad.domain.WebSoknad;
 import no.nav.sbl.dialogarena.sendsoknad.domain.exception.OpplastingException;
 import no.nav.sbl.dialogarena.sikkerhet.SjekkTilgangTilSoknad;
 import no.nav.sbl.dialogarena.soknadinnsending.business.domain.BehandlingsKjede;
 import no.nav.sbl.dialogarena.soknadinnsending.business.service.EttersendelseVedleggService;
 import no.nav.sbl.dialogarena.soknadinnsending.business.service.EttersendelseVedleggService.EttersendelseVedlegg;
 import no.nav.sbl.dialogarena.soknadinnsending.business.service.soknadservice.InnsendtSoknadService;
+import no.nav.sbl.dialogarena.soknadinnsending.business.service.soknadservice.SoknadDataFletter;
+import no.nav.sbl.dialogarena.soknadinnsending.business.service.soknadservice.SoknadService;
 import no.nav.sbl.soknadsosialhjelp.vedlegg.JsonVedlegg;
 import no.nav.sbl.sosialhjelp.domain.OpplastetVedlegg;
 import no.nav.sbl.sosialhjelp.domain.SoknadUnderArbeid;
@@ -23,6 +26,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import java.util.Optional;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static javax.ws.rs.core.MediaType.MULTIPART_FORM_DATA;
@@ -51,6 +55,12 @@ public class EttersendingRessurs {
     @Inject
     private OpplastetVedleggRepository opplastetVedleggRepository;
 
+    @Inject
+    private SoknadDataFletter soknadDataFletter;
+
+    @Inject
+    private SoknadService soknadService;
+
 
     @GET
     @Path("/innsendte/{behandlingsId}")
@@ -69,10 +79,18 @@ public class EttersendingRessurs {
     @GET
     @Path("/ettersendteVedlegg/{behandlingsId}")
     public List<EttersendtVedlegg> hentVedlegg(@PathParam("behandlingsId") String behandlingsId) {
-        final String eier = getSubjectHandler().getUid();
-        final SoknadUnderArbeid soknadUnderArbeid = soknadUnderArbeidRepository.hentSoknad(behandlingsId, eier).get();
-        final List<OpplastetVedlegg> opplastedeVedlegg = opplastetVedleggRepository.hentVedleggForSoknad(soknadUnderArbeid.getSoknadId(), eier);
-        final List<JsonVedlegg> originaleVedlegg = soknadUnderArbeid.getJsonInternalSoknad().getVedlegg().getVedlegg();
+        String eier = getSubjectHandler().getUid();
+        SoknadUnderArbeid soknadUnderArbeid;
+        Optional<SoknadUnderArbeid> soknadUnderArbeidOptional = soknadUnderArbeidRepository.hentSoknad(behandlingsId, eier);
+        if (soknadUnderArbeidOptional.isPresent()){
+            soknadUnderArbeid = soknadUnderArbeidOptional.get();
+        } else {
+            WebSoknad webSoknad = soknadService.hentSoknad(behandlingsId, true, true);
+            soknadDataFletter.legacyKonverterVedleggOgOppdaterSoknadUnderArbeid(behandlingsId, eier, webSoknad);
+            soknadUnderArbeid = soknadUnderArbeidRepository.hentSoknad(behandlingsId, eier).get();
+        }
+        List<OpplastetVedlegg> opplastedeVedlegg = opplastetVedleggRepository.hentVedleggForSoknad(soknadUnderArbeid.getSoknadId(), eier);
+        List<JsonVedlegg> originaleVedlegg = soknadUnderArbeid.getJsonInternalSoknad().getVedlegg().getVedlegg();
 
         return mapVedleggToSortedListOfEttersendteVedlegg(opplastedeVedlegg, originaleVedlegg);
     }
