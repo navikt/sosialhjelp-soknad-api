861a5dab130ebc9463f83d4dff9bb9ccbf2f10ef
Oskar Asplin <oskarasplin@gmail.com>
2019-05-10 13:31:34 +0200

    DIGISOS-1136 Prøver å fjerne tilgangTilSoknad forespørsel for å fikse testmiljø-problem

--- web/src/main/java/no/nav/sbl/dialogarena/rest/ressurser/SoknadRessurs.java
+++ web/src/main/java/no/nav/sbl/dialogarena/rest/ressurser/SoknadRessurs.java
@@ -180,7 +180,6 @@ public class SoknadRessurs {
             opprettetBehandlingsId = soknadService.startSoknad(soknadType.getSoknadType());
         } else {
             final String eier = SubjectHandler.getSubjectHandler().getUid();
-            tilgangskontroll.verifiserAtBrukerKanEndreSoknad(behandlingsId);
             WebSoknad soknad = soknadService.hentEttersendingForBehandlingskjedeId(behandlingsId);
             if (soknad == null){
                 Optional<SoknadUnderArbeid> soknadUnderArbeid = soknadUnderArbeidRepository.hentEttersendingMedTilknyttetBehandlingsId(behandlingsId, eier);
