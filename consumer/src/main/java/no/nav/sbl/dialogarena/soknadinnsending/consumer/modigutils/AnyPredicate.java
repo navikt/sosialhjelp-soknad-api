640c0a35e2668ccb7b95fa5a6b2784bdf014ccb5
siljee <silje.hansen3@nav.no>
2019-05-27 14:15:41 +0200

    Fjernet alt som ble dratt inn pga optional. Og fjernet all modig fra comsumer-pom.xml Det er fortsatt flere ting som må fikses.

--- consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/modigutils/AnyPredicate.java
+++
@@ -1,4 +0,0 @@
-package no.nav.sbl.dialogarena.soknadinnsending.consumer.modigutils;
-
-public class AnyPredicate {
-}
--- consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/modigutils/ReduceFunction.java
+++
@@ -1,6 +0,0 @@
-package no.nav.sbl.dialogarena.soknadinnsending.consumer.modigutils;
-
-public interface ReduceFunction<T, U> {
-    U reduce(U accumulator, T newValue);
-    U identity();
-}
