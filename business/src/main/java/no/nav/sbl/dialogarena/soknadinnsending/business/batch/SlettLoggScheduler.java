e2778d44969719a8843cd1737ff91c322b6eefbb
Oskar Asplin <oskarasplin@gmail.com>
2019-08-13 12:51:34 +0200

    DIGISOS-749 Endrer tidspunkt for sletting av logger for testing

--- business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/batch/SlettLoggScheduler.java
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/batch/SlettLoggScheduler.java
@@ -25,7 +25,7 @@ public class SlettLoggScheduler {
 
     private static final Logger logger = getLogger(SlettLoggScheduler.class);
 
-    private static final String KLOKKEN_FEM_OM_NATTEN = "0 10 15 * * *";
+    private static final String KLOKKEN_FEM_OM_NATTEN = "0 5 13 * * *";
     private static final int SCHEDULE_INTERRUPT_S = 60 * 10;
     private static final int DAGER_GAMMELT = 365; // Ett år
 
