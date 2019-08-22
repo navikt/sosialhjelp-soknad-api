29149a7988277baa07209d46a291c60d9cc6a310
Eivind K. Nilsbakken <eivind.nilsbakken@nav.no>
2019-08-22 12:44:19 +0200

    Følg navnekonvensjon på init-scripts

--- web/nais/scripts/90-base64-decode-keystore.sh
+++ web/nais/scripts/90-base64-decode-keystore.sh
@@ -4,8 +4,8 @@ if test -d /var/run/secrets/nais.io/vault;
 then
     for FILE in /var/run/secrets/nais.io/vault/*.b64
     do
-        echo "base64-decoding $FILE"
         OUT=`echo $(dirname $FILE)/$(basename $FILE .b64)`
+        echo "Base64-decode $FILE to $OUT"
         base64 -d $FILE > $OUT
     done
 fi
