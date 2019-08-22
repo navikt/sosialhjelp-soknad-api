d6e1bfd69bb6dbb6cd519868f5c2568ac437a4cc
Eivind K. Nilsbakken <eivind.nilsbakken@nav.no>
2019-08-22 08:47:30 +0200

    Sett DB-URL i lokal config

--- web/nais/scripts/set-datasource.sh
+++ web/nais/scripts/set-datasource.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
 
-export SOKNADSOSIALHJELPDATASOURCE_URL=$(cat /var/run/secrets/datasource/url)
 export SOKNADSOSIALHJELPDATASOURCE_USERNAME=$(cat /var/run/secrets/datasource/username)
 export SOKNADSOSIALHJELPDATASOURCE_PASSWORD=$(cat /var/run/secrets/datasource/password)
