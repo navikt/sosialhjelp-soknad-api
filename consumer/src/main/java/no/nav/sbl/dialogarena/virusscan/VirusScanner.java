eb334fbe7c26cd59c78f73b7b93baf4f7cbe1b89
Eivind <eivind.nilsbakken@nav.no>
2019-09-19 12:28:27 +0200

    Feature/digisos 1220 virusscan vedlegg (#126)
    * DIGISOS-1220: Legg til virus-scanning
    
    * DIGISOS-1220: Legg til enhetstester for virus-scanning
    
    * DIGISOS-1220: Kast OpplastingException når mulig virus
    
    * DIGISOS-1220: Endre tekster++ etter QA
    
    * Fjerner dobbelt mellomrom i feilmelding, iht. QA

---
+++ consumer/src/main/java/no/nav/sbl/dialogarena/virusscan/VirusScanner.java
@@ -0,0 +1,10 @@
+package no.nav.sbl.dialogarena.virusscan;
+
+import no.nav.sbl.dialogarena.sendsoknad.domain.exception.OpplastingException;
+
+/**
+ * Integrasjonen er kopiert fra https://github.com/navikt/foreldrepengesoknad-api og modifisert til eget bruk
+ */
+public interface VirusScanner {
+    void scan(String filnavn, byte[] data) throws OpplastingException;
+}
