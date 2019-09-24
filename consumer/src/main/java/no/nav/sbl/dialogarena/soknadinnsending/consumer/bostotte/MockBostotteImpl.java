549562820e6e31675d79038769a8e1beac5247f5
Per-Christian Nielsen <per.christian.nielsen@nav.no>
2019-09-24 12:52:31 +0200

    Fiksa utviklings miljøvariabler og lagt til oversettinger.

--- consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/bostotte/MockBostotteImpl.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/bostotte/MockBostotteImpl.java
@@ -10,7 +10,7 @@ public class MockBostotteImpl implements Bostotte {
 
     @Override
     public BostotteDto hentBostotte(String personIdentifikator, LocalDate fra, LocalDate til) {
-        String mottaker = "Ola Normann";
+        String mottaker = "KOMMUNE";
         BigDecimal belop = BigDecimal.valueOf(10000);
         LocalDate utbetalingsDato = LocalDate.now();
         return new BostotteDto().withWithUtbetaling(new UtbetalingerDto().with(mottaker, belop, utbetalingsDato));
