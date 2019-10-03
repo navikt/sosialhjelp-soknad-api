f679580da2c0cf41c1535d679562baa04a4be02b
Jan Tore Bøe <jan@boee.org>
2019-10-03 12:47:41 +0200

    Feature/fiksdigisosklient (#152)
    * Lagt til fiksdigisosklient

---
+++ consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/digisosapi/DigisosApi.java
@@ -0,0 +1,11 @@
+package no.nav.sbl.dialogarena.soknadinnsending.consumer.digisosapi;
+
+import java.util.List;
+
+public interface DigisosApi {
+    void ping();
+
+    KommuneStatus kommuneInfo(String kommunenr);
+
+    void krypterOgLastOppFiler(String soknadJson, String vedleggJson, List<FilOpplasting> dokumenter, String kommunenr, String navEkseternRefId, String token);
+}
