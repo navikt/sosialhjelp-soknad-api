788362ddd652f49a9061d5b154629a04a39b3948
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2019-10-14 14:27:52 +0200

    endrer metrics-config. setter miljø = fasit_environment_name

--- web/src/main/java/no/nav/sbl/dialogarena/config/MetricsConfig.java
+++ web/src/main/java/no/nav/sbl/dialogarena/config/MetricsConfig.java
@@ -1,7 +1,11 @@
 package no.nav.sbl.dialogarena.config;
 
+import no.nav.metrics.MetricsClient;
 import no.nav.metrics.aspects.TimerAspect;
 import org.springframework.context.annotation.Bean;
+
+import static java.lang.Boolean.parseBoolean;
+import static java.lang.System.getProperty;
 
 public class MetricsConfig {
 
@@ -10,4 +14,10 @@ public class MetricsConfig {
         return new TimerAspect();
     }
 
+    public MetricsConfig() {
+        String miljo = System.getenv("FASIT_ENVIRONMENT_NAME");
+        if (!parseBoolean(getProperty("disable.metrics.report"))) {
+            MetricsClient.enableMetrics(no.nav.metrics.MetricsConfig.resolveNaisConfig().withEnvironment(miljo));
+        }
+    }
 }
