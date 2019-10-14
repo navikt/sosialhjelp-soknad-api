89047417eba71739c4dbb1abd08a44ab7dedc891
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2019-10-14 10:33:11 +0200

    prøver å enable metrikker manuelt

--- web/src/main/java/no/nav/sbl/dialogarena/config/MetricsConfig.java
+++ web/src/main/java/no/nav/sbl/dialogarena/config/MetricsConfig.java
@@ -1,12 +1,23 @@
 package no.nav.sbl.dialogarena.config;
 
+import no.nav.metrics.MetricsClient;
 import no.nav.metrics.aspects.TimerAspect;
 import org.springframework.context.annotation.Bean;
+
+import static java.lang.Boolean.parseBoolean;
+import static java.lang.System.getProperty;
 
 public class MetricsConfig {
 
     @Bean
     public TimerAspect timerAspect() {
         return new TimerAspect();
+    }
+
+    public MetricsConfig() {
+        String miljø = System.getenv("NAIS_CLUSTER_NAME");
+        if (!parseBoolean(getProperty("disable.metrics.report"))) {
+            MetricsClient.enableMetrics(no.nav.metrics.MetricsConfig.resolveNaisConfig().withEnvironment(miljø));
+        }
     }
 }
