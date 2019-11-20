4708005db45c8649176303ab36d845b349083231
siljee <silje.hansen3@nav.no>
2019-11-20 12:22:51 +0100

    Endre adressesøktype til LIK for i håp om at det søker på ufullstendige gateadresser uten å gjøre det fonetisk.

--- domain/src/main/java/no/nav/sbl/dialogarena/sendsoknad/domain/adresse/AdresseSokConsumer.java
+++ domain/src/main/java/no/nav/sbl/dialogarena/sendsoknad/domain/adresse/AdresseSokConsumer.java
@@ -37,8 +37,10 @@ public interface AdresseSokConsumer {
     
     enum Soketype {
         FONETISK("F"),
-        EKSAKT("E");
-        
+        EKSAKT("E"),
+        TILFELDIG("T"),
+        LIK("L");
+
         String tpsKode;
         
         Soketype(String tpsKode) {
@@ -58,7 +60,7 @@ public interface AdresseSokConsumer {
         public String poststed;
         
         public String kommunenummer;
-        public Soketype soketype = Soketype.EKSAKT;
+        public Soketype soketype = Soketype.LIK;
         
         public Sokedata withAdresse(String adresse) {
             this.adresse = adresse;
