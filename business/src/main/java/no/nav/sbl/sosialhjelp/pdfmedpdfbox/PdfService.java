109a58a4e210ae8dab015de2a8ee038e9d53686a
Andre Wolden <wolden.andre@gmail.com>
2019-12-03 14:18:07 +0100

    teste å lage pdf og lagre til disk med pdfbox

---
+++ business/src/main/java/no/nav/sbl/sosialhjelp/pdfmedpdfbox/PdfService.java
@@ -0,0 +1,12 @@
+package no.nav.sbl.sosialhjelp.pdfmedpdfbox;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PdfService {
+
+    public PdfService() {
+    }
+
+    public
+}
