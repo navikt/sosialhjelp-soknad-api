616f697a9e7f4a307c7243d170f8a709a5012a70
Jan Tore Bøe <jan.tore.boe@nav.no>
2019-12-03 10:17:57 +0100

    Initial person_v1 til v3

--- web/src/test/java/no/nav/sbl/dialogarena/integration/EndpointDataMocking.java
+++ web/src/test/java/no/nav/sbl/dialogarena/integration/EndpointDataMocking.java
@@ -8,16 +8,11 @@ import no.nav.tjeneste.virksomhet.digitalkontaktinformasjon.v1.DigitalKontaktinf
 import no.nav.tjeneste.virksomhet.digitalkontaktinformasjon.v1.informasjon.WSEpostadresse;
 import no.nav.tjeneste.virksomhet.digitalkontaktinformasjon.v1.informasjon.WSKontaktinformasjon;
 import no.nav.tjeneste.virksomhet.digitalkontaktinformasjon.v1.meldinger.WSHentDigitalKontaktinformasjonResponse;
-import no.nav.tjeneste.virksomhet.person.v1.PersonPortType;
-import no.nav.tjeneste.virksomhet.person.v1.informasjon.*;
-import no.nav.tjeneste.virksomhet.person.v1.meldinger.HentKjerneinformasjonResponse;
 import no.nav.tjeneste.virksomhet.person.v3.binding.HentPersonPersonIkkeFunnet;
 import no.nav.tjeneste.virksomhet.person.v3.binding.HentPersonSikkerhetsbegrensning;
 import no.nav.tjeneste.virksomhet.person.v3.binding.PersonV3;
 import no.nav.tjeneste.virksomhet.person.v3.meldinger.HentPersonRequest;
 import org.mockito.Mockito;
-
-import javax.xml.datatype.DatatypeFactory;
 
 import static no.nav.sbl.dialogarena.sendsoknad.mockmodul.person.PersonV3Mock.createPersonV3HentPersonRequest;
 import static org.mockito.Matchers.any;
@@ -28,38 +23,9 @@ public class EndpointDataMocking {
     private static int behandlingsIdCounter = 1;
 
     public static void setupMockWsEndpointData() throws Exception {
-        mockPersonEndpoint();
         mockPersonV3Endpoint();
         mockDkifService();
         mockArbeidsForholdService();
-    }
-
-    static void mockPersonEndpoint() throws Exception {
-        PersonPortType personEndpoint = IntegrationConfig.getMocked("personEndpoint");
-        HentKjerneinformasjonResponse hentKjerneinformasjonResponse = new HentKjerneinformasjonResponse();
-
-        Foedselsdato foedselsdato = new Foedselsdato();
-        foedselsdato.setFoedselsdato(DatatypeFactory.newInstance().newXMLGregorianCalendar());
-
-        Familierelasjoner familierelasjoner = new Familierelasjoner();
-        familierelasjoner.setValue("BARN");
-
-        Familierelasjon familierelasjon = new Familierelasjon();
-        familierelasjon.setTilRolle(familierelasjoner);
-
-        Person person = new Person();
-        Person barn = new Person();
-        NorskIdent norskIdent = new NorskIdent();
-        norskIdent.setIdent("04031659235");
-        barn.setIdent(norskIdent);
-        barn.setFoedselsdato(foedselsdato);
-        familierelasjon.setTilPerson(barn);
-        person.getHarFraRolleI().add(familierelasjon);
-
-        person.setFoedselsdato(foedselsdato);
-        hentKjerneinformasjonResponse.setPerson(person);
-
-        Mockito.when(personEndpoint.hentKjerneinformasjon(any())).thenReturn(hentKjerneinformasjonResponse);
     }
 
     static void mockPersonV3Endpoint() throws Exception {
