a1a6007765fb502ee7afa93a28e0693b4c38c02d
Andre Wolden <wolden.andre@gmail.com>
2019-12-05 12:15:39 +0100

    [WIP]

--- business/src/main/java/no/nav/sbl/sosialhjelp/pdfmedpdfbox/SosialhjelpPdfGenerator.java
+++ business/src/main/java/no/nav/sbl/sosialhjelp/pdfmedpdfbox/SosialhjelpPdfGenerator.java
@@ -3,65 +3,80 @@ package no.nav.sbl.sosialhjelp.pdfmedpdfbox;
 import no.nav.sbl.dialogarena.soknadsosialhjelp.message.NavMessageSource;
 import no.nav.sbl.soknadsosialhjelp.soknad.JsonInternalSoknad;
 import no.nav.sbl.soknadsosialhjelp.soknad.personalia.JsonPersonalia;
-import org.apache.pdfbox.exceptions.COSVisitorException;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.edit.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
+import sun.security.ssl.Debug;
 
 import javax.inject.Inject;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.Locale;
-import java.util.Properties;
+import java.util.ArrayList;
+import java.util.List;
+
+import static no.nav.sbl.sosialhjelp.pdfmedpdfbox.PdfGenerator.*;
 
 @Component
 public class SosialhjelpPdfGenerator {
 
+    private PdfGenerator pdfGen;
+
+    private static final float FONT_SIZE = 12;
+    public static final float PAGE_WIDTH = 400;
+    public static final float PAGE_HEIGHT = 500;
+
     @Inject
     private NavMessageSource navMessageSource;
-
-
-    private PdfGenerator pdfGen;
 
     @Inject
     public SosialhjelpPdfGenerator() {
         pdfGen = new PdfGenerator();
     }
 
-    public byte[] generate(JsonInternalSoknad jsonInternalSoknad){
+    public byte[] generate(JsonInternalSoknad jsonInternalSoknad) {
+        try {
+            PDDocument doc = new PDDocument();
+            final PDPage page1 = pdfGen.newPage();
+            PDPageContentStream cos1 = new PDPageContentStream(doc, page1);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
-        byte[] pdf;
-        final PDPage page = pdfGen.newPage();
-        try (
-                PDDocument doc = new PDDocument();
-                PDPageContentStream cos = new PDPageContentStream(doc, page);
-                ByteArrayOutputStream baos = new ByteArrayOutputStream()
-        ){
+
             float y = PdfGenerator.calculateStartY();
 
             // write stuff
-            y -= header(doc, cos, y);
-            y -= leggTilPersonalia(doc, cos, y, jsonInternalSoknad.getSoknad().getData().getPersonalia());
+            y -= header(doc, cos1, y);
 
+            PDRectangle mediaBox = page1.getMediaBox();
+            float marginY = 80;
+            float marginX = 60;
+            float width = mediaBox.getWidth() - 2 * marginX;
+            float startX = mediaBox.getLowerLeftX() + marginX;
+            float startY = mediaBox.getUpperRightY() - marginY;
+
+
+            cos1.beginText();
+            y -= addParagraph(cos1, width, startX, startY, "Første vanlige linje", PdfGenerator.FONT_PLAIN, PdfGenerator.FONT_PLAIN_SIZE, PdfGenerator.LEADING_PERCENTAGE);
+            cos1.endText();
+            y -= leggTilPersonalia(doc, cos1, y, jsonInternalSoknad.getSoknad().getData().getPersonalia());
 
 
 
             final PDPage page2 = pdfGen.newPage();
 
-
-
-            doc.addPage(page);
+            doc.addPage(page1);
             doc.addPage(page2);
 
-
-            cos.close();
+            cos1.close();
             doc.save(baos);
+            byte[] pdf;
             pdf = baos.toByteArray();
             return pdf;
 
-        } catch (IOException | COSVisitorException e) {
+        } catch (IOException e) {
             throw new RuntimeException("Error while creating pdf", e);
         }
     }
@@ -70,29 +85,99 @@ public class SosialhjelpPdfGenerator {
 
 //        final Properties tekstbundle = navMessageSource.getBundleFor("sendsoknad", new Locale("nb", "NO"));
 //        final String personaliabolkTittel = tekstbundle.getProperty("personaliabolk.tittel");
-        float startY = y;
-
-        cos.beginText();
-        cos.setFont(fontPlain, fontPLainSize);
-        cos.moveTextPositionByAmount(margin, startY);
-        cos.drawString(line);
-        cos.endText();
 
 
+        if (jsonPersonalia.getNavn() != null) {
 
 
+            final Debug debug = new Debug();
+            debug.println("y = " + y);
 
-        y -= pdfGen.addLineOfRegularText("Personalia", cos, y);
+            cos.beginText();
+            y -= addParagraph(cos, MEDIA_BOX.getWidth() - 2 * MARGIN, MARGIN, y, "Navn på person", FONT_BOLD, FONT_SIZE, LEADING_PERCENTAGE);
+            debug.println("y = " + y);
+            y -= addParagraph(cos, MEDIA_BOX.getWidth() - 2 * MARGIN, MARGIN, -y, jsonPersonalia.getNavn().getFornavn() + " " + jsonPersonalia.getNavn().getEtternavn(), FONT_PLAIN, FONT_SIZE, LEADING_PERCENTAGE);
+            debug.println("y = " + y);
+            y -= addParagraph(cos, MEDIA_BOX.getWidth() - 2 * MARGIN, MARGIN, -y, "Telefonnummer", FONT_BOLD, FONT_SIZE, LEADING_PERCENTAGE);
+            debug.println("y = " + y);
+            y -= addParagraph(cos, MEDIA_BOX.getWidth() - 2 * MARGIN, MARGIN, -y, "asdf " + jsonPersonalia.getTelefonnummer().getVerdi(), FONT_PLAIN, FONT_SIZE, LEADING_PERCENTAGE);
+            debug.println("y = " + y);
+            y -= addParagraph(cos, MEDIA_BOX.getWidth() - 2 * MARGIN, MARGIN, -y, "Bankkontonummer", FONT_BOLD, FONT_SIZE, LEADING_PERCENTAGE);
+            debug.println("y = " + y);
+            y -= addParagraph(cos, MEDIA_BOX.getWidth() - 2 * MARGIN, MARGIN, -y, "asdf " + jsonPersonalia.getKontonummer().getVerdi(), FONT_PLAIN, FONT_SIZE, LEADING_PERCENTAGE);
+            debug.println("y = " + y);
+            cos.endText();
+        }
 
 
-
-        return startY - y;
+        return y;
     }
 
     private float header(PDDocument doc, PDPageContentStream cos, float y) throws IOException {
         float startY = y;
-        y -= pdfGen.addCenteredHeading("Heart of the swarm", cos, y);
+        y -= pdfGen.addCenteredHeading("Heart of the swarm - med en veldig veldig veldig veldig veldig glhf glhf glhf glhf lang tittel.", cos, y);
         y -= pdfGen.addDividerLine(cos, y);
         return startY - y;
     }
+
+    public byte[] pracGenerate() {
+
+        try (final PDDocument doc = new PDDocument()) {
+
+            final PdfGenerator pdfGen = new PdfGenerator();
+
+            PDPage page = pdfGen.newPage();
+            PDPage page2 = pdfGen.newPage();
+            doc.addPage(page);
+            doc.addPage(page2);
+            PDPageContentStream contentStreamPage1 = new PDPageContentStream(doc, page);
+            PDPageContentStream contentStreamPage2 = new PDPageContentStream(doc, page2);
+
+
+            PDRectangle mediaBox = page.getMediaBox();
+            float marginY = 80;
+            float marginX = 60;
+            float width = mediaBox.getWidth() - 2 * marginX;
+            float startX = mediaBox.getLowerLeftX() + marginX;
+            float startY = mediaBox.getUpperRightY() - marginY;
+
+            String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt" +
+                    " ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco" +
+                    " laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in " +
+                    " ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco" +
+                    " laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in " +
+                    "voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat" +
+                    " non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+
+            contentStreamPage1.lineTo(0, 0);
+            contentStreamPage1.closeAndStroke();
+            contentStreamPage1.addLine(5, 5, PAGE_WIDTH, PAGE_HEIGHT);
+
+            contentStreamPage1.beginText();
+            addParagraph(contentStreamPage1, width, startX, startY, text, PdfGenerator.FONT_PLAIN, PdfGenerator.FONT_PLAIN_SIZE, PdfGenerator.LEADING_PERCENTAGE, true);
+            addParagraph(contentStreamPage1, width, startX, startY, text, PdfGenerator.FONT_PLAIN, PdfGenerator.FONT_PLAIN_SIZE, PdfGenerator.LEADING_PERCENTAGE);
+            addParagraph(contentStreamPage1, width, startX, startY, text, PdfGenerator.FONT_PLAIN, PdfGenerator.FONT_PLAIN_SIZE, PdfGenerator.LEADING_PERCENTAGE, false);
+            contentStreamPage1.endText();
+            contentStreamPage1.close();
+
+            contentStreamPage2.beginText();
+            addParagraph(contentStreamPage1, width, startX, startY, text, PdfGenerator.FONT_PLAIN, PdfGenerator.FONT_PLAIN_SIZE, PdfGenerator.LEADING_PERCENTAGE, true);
+            contentStreamPage2.endText();
+            contentStreamPage2.close();
+
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            doc.save(baos);
+            return baos.toByteArray();
+
+        } catch (IOException e) {
+            System.err.println("Exception while trying to create pdf document - " + e);
+        }
+
+        return null;
+
+    }
+
+
 }
--- web/src/main/java/no/nav/sbl/dialogarena/mock/TjenesteMockRessurs.java
+++ web/src/main/java/no/nav/sbl/dialogarena/mock/TjenesteMockRessurs.java
@@ -16,10 +16,17 @@ import no.nav.sbl.dialogarena.soknadinnsending.business.batch.oppgave.fiks.FiksS
 import no.nav.sbl.dialogarena.soknadinnsending.business.service.soknadservice.SoknadService;
 import no.nav.sbl.dialogarena.soknadinnsending.consumer.bostotte.MockBostotteImpl;
 import no.nav.sbl.dialogarena.soknadsosialhjelp.message.NavMessageSource;
+import no.nav.sbl.soknadsosialhjelp.soknad.JsonData;
+import no.nav.sbl.soknadsosialhjelp.soknad.JsonInternalSoknad;
+import no.nav.sbl.soknadsosialhjelp.soknad.JsonSoknad;
+import no.nav.sbl.soknadsosialhjelp.soknad.common.JsonNavn;
+import no.nav.sbl.soknadsosialhjelp.soknad.personalia.JsonPersonalia;
+import no.nav.sbl.soknadsosialhjelp.soknad.personalia.JsonSokernavn;
 import no.nav.sbl.soknadsosialhjelp.soknad.personalia.JsonTelefonnummer;
 import no.nav.sbl.sosialhjelp.InnsendingService;
 import no.nav.sbl.sosialhjelp.domain.SendtSoknad;
 import no.nav.sbl.sosialhjelp.pdfmedpdfbox.SosialhjelpPdfGenerator;
+import no.nav.sbl.sosialhjelp.soknadunderbehandling.SoknadUnderArbeidRepository;
 import no.nav.security.oidc.api.Unprotected;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
@@ -65,6 +72,8 @@ public class TjenesteMockRessurs {
     private NavMessageSource messageSource;
     @Inject
     private SoknadService soknadService;
+    @Inject
+    private SoknadUnderArbeidRepository soknadUnderArbeidRepository;
 
     private void clearCache() {
         for (String cacheName : cacheManager.getCacheNames()) {
@@ -75,8 +84,21 @@ public class TjenesteMockRessurs {
     @GET
     @Path("/pdfboxtest")
     public void generatePdfAndSaveToDisk(){
+
+        if (!isTillatMockRessurs()) {
+            throw new RuntimeException("Mocking har ikke blitt aktivert.");
+        }
+
+//        final String fnr = OidcFeatureToggleUtils.getUserId();
+//        final JsonInternalSoknad jsonInternalSoknad = soknadUnderArbeidRepository.hentSoknad(behandlingsId, fnr).getJsonInternalSoknad();
+
         SosialhjelpPdfGenerator sosialhjelpPdfGenerator = new SosialhjelpPdfGenerator();
-        byte[] bytes = sosialhjelpPdfGenerator.generate();
+
+        byte[] bytes = sosialhjelpPdfGenerator.generate(new JsonInternalSoknad().withSoknad(
+                new JsonSoknad().withData(new JsonData().withPersonalia(new JsonPersonalia().withNavn(
+                        new JsonSokernavn().withFornavn("Han").withEtternavn("Solo")
+                ))
+        )));
 
         try {
             FileOutputStream out = new FileOutputStream("starcraft.pdf");
