4ba40a3f9637e5024621a8479406ae35e3f256ee
siljee <silje.hansen3@nav.no>
2019-12-06 16:15:20 +0100

    Skru på sending til fiks-digisos-api. (#202)
    * Skru på sending til fiks-digisos-api.
    * Fjerne ekstra sikkerhetssjekk på prod.
    * Filtrere vekk metadata som er sendt inn via fiks-digisos-api slik at de ikke vises i saksoversikten.
    * Fjernet mock-sjekker fra serviceUtil og tok heller i bruk sjekkene fra MockUtil.
    (som var planen da de ble lagt inn i MockUtil)
    * Gjør q1 prodlik

--- web/src/main/java/no/nav/sbl/dialogarena/rest/actions/SoknadActions.java
+++ web/src/main/java/no/nav/sbl/dialogarena/rest/actions/SoknadActions.java
@@ -28,7 +28,7 @@ import javax.xml.bind.annotation.XmlAccessorType;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static no.nav.sbl.dialogarena.sendsoknad.domain.SoknadInnsendingStatus.SENDT_MED_DIGISOS_API;
-import static no.nav.sbl.dialogarena.sendsoknad.domain.util.ServiceUtils.isAlltidSendTilNavTestkommune;
+import static no.nav.sbl.dialogarena.sendsoknad.domain.mock.MockUtils.isAlltidSendTilNavTestkommune;
 import static no.nav.sbl.dialogarena.sendsoknad.domain.util.ServiceUtils.isSendingTilFiksEnabled;
 import static org.slf4j.LoggerFactory.getLogger;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
@@ -69,8 +69,7 @@ public class SoknadActions {
         String eier = OidcFeatureToggleUtils.getUserId();
         SoknadUnderArbeid soknadUnderArbeid = soknadUnderArbeidRepository.hentSoknad(behandlingsId, eier);
 
-        if (ServiceUtils.isRunningInProd()
-                || !isSendingTilFiksEnabled()
+        if (!isSendingTilFiksEnabled()
                 || soknadUnderArbeid == null
                 || isEttersendelsePaSoknadSendtViaSvarUt(soknadUnderArbeid)) {
             log.info("BehandlingsId {} sendes til SvarUt.", behandlingsId);
