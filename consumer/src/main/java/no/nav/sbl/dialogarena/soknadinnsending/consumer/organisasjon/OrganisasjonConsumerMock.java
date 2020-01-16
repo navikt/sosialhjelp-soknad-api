1a53e1549c43c467a3b17470836fcecbf01bf699
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2020-01-16 12:41:10 +0100

    lagt til default organisasjonNoekkelinfo response i mock (men brukes ikke pr nå)

--- consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/organisasjon/OrganisasjonConsumerMock.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/organisasjon/OrganisasjonConsumerMock.java
@@ -1,26 +1,30 @@
 package no.nav.sbl.dialogarena.soknadinnsending.consumer.organisasjon;
 
+import no.nav.sbl.dialogarena.soknadinnsending.consumer.organisasjon.dto.NavnDto;
 import no.nav.sbl.dialogarena.soknadinnsending.consumer.organisasjon.dto.OrganisasjonNoekkelinfoDto;
 import org.mockito.invocation.InvocationOnMock;
 
-import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class OrganisasjonConsumerMock {
 
+    private static final String orgnrMock = "123457890";
+
     public OrganisasjonConsumer organisasjonConsumerMock(){
         OrganisasjonConsumer mock = mock(OrganisasjonConsumer.class);
 
-        when(mock.hentOrganisasjonNoekkelinfo(any(String.class)))
-                .thenAnswer(OrganisasjonConsumerMock::getOrCreateOrganisasjonResponse);
+        when(mock.hentOrganisasjonNoekkelinfo(anyString()))
+                .thenAnswer(OrganisasjonConsumerMock::getDefaultOrganisasjonResponse);
 
         return mock;
     }
 
-
-    private static OrganisasjonNoekkelinfoDto getOrCreateOrganisasjonResponse(InvocationOnMock invocationOnMock) {
-        // todo implement
-        return null;
+    private static OrganisasjonNoekkelinfoDto getDefaultOrganisasjonResponse(InvocationOnMock invocationOnMock) {
+        return new OrganisasjonNoekkelinfoDto(
+                new NavnDto("NAV MOCK AS", null, null, null, null),
+                orgnrMock
+        );
     }
 }
