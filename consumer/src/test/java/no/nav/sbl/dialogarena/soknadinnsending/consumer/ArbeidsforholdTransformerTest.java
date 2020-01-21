1cca98b538e8ce09ee078b035ef273fbaf75f209
martintveter <41987225+martintveter@users.noreply.github.com>
2020-01-21 14:39:52 +0100

    Feature/organisasjon v4 til rest (#243)
    * Lagt til consumer for Ereg - OrganisasjonConsumer.
    Feature toggle styrer om gammel webservice eller ny rest-tjeneste benyttes i ArbeidsforholdTransformer.

--- consumer/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/ArbeidsforholdTransformerTest.java
+++ consumer/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/ArbeidsforholdTransformerTest.java
@@ -1,14 +1,8 @@
 package no.nav.sbl.dialogarena.soknadinnsending.consumer;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertThat;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.when;
-
-import java.math.BigDecimal;
-
-import javax.xml.datatype.DatatypeFactory;
-
+import no.nav.sbl.dialogarena.sendsoknad.domain.Arbeidsforhold;
+import no.nav.sbl.dialogarena.soknadinnsending.consumer.organisasjon.OrganisasjonService;
+import no.nav.tjeneste.virksomhet.arbeidsforhold.v3.informasjon.arbeidsforhold.*;
 import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Ignore;
@@ -18,45 +12,33 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import no.nav.sbl.dialogarena.sendsoknad.domain.Arbeidsforhold;
-import no.nav.tjeneste.virksomhet.arbeidsforhold.v3.informasjon.arbeidsforhold.Aktoer;
-import no.nav.tjeneste.virksomhet.arbeidsforhold.v3.informasjon.arbeidsforhold.AnsettelsesPeriode;
-import no.nav.tjeneste.virksomhet.arbeidsforhold.v3.informasjon.arbeidsforhold.Arbeidsavtale;
-import no.nav.tjeneste.virksomhet.arbeidsforhold.v3.informasjon.arbeidsforhold.Avloenningstyper;
-import no.nav.tjeneste.virksomhet.arbeidsforhold.v3.informasjon.arbeidsforhold.Gyldighetsperiode;
-import no.nav.tjeneste.virksomhet.arbeidsforhold.v3.informasjon.arbeidsforhold.Organisasjon;
-import no.nav.tjeneste.virksomhet.organisasjon.v4.binding.OrganisasjonV4;
-import no.nav.tjeneste.virksomhet.organisasjon.v4.informasjon.UstrukturertNavn;
-import no.nav.tjeneste.virksomhet.organisasjon.v4.meldinger.HentOrganisasjonRequest;
-import no.nav.tjeneste.virksomhet.organisasjon.v4.meldinger.HentOrganisasjonResponse;
+import javax.xml.datatype.DatatypeFactory;
+import java.math.BigDecimal;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ArbeidsforholdTransformerTest {
 
     public static final String ORGNUMMER = "1234567";
+
     @Mock
-    private OrganisasjonV4 organisasjon;
+    private OrganisasjonService organisasjonService;
 
     @InjectMocks
     private ArbeidsforholdTransformer arbeidsforholdTransformer;
 
     private DatatypeFactory datatypeFactory;
 
+    private String orgnavn = "Testesen A/S, andre linje";
+
     @Before
     public void setup() throws Exception {
         datatypeFactory = DatatypeFactory.newInstance();
-        when(organisasjon.hentOrganisasjon(any(HentOrganisasjonRequest.class))).thenReturn(createOrgResponse());
-    }
-
-    private HentOrganisasjonResponse createOrgResponse() {
-        HentOrganisasjonResponse response = new HentOrganisasjonResponse();
-        no.nav.tjeneste.virksomhet.organisasjon.v4.informasjon.Organisasjon org = new no.nav.tjeneste.virksomhet.organisasjon.v4.informasjon.Organisasjon();
-        UstrukturertNavn value = new UstrukturertNavn();
-        value.getNavnelinje().add("Testesen A/S");
-        value.getNavnelinje().add("andre linje");
-        org.setNavn(value);
-        response.setOrganisasjon(org);
-        return response;
+        when(organisasjonService.hentOrgNavn(anyString())).thenReturn(orgnavn);
     }
 
     @Test
@@ -66,20 +48,6 @@ public class ArbeidsforholdTransformerTest {
         assertThat(arbeidsforhold.fastStillingsprosent, equalTo(100L));
         assertThat(arbeidsforhold.fom, equalTo("2015-01-01"));
         assertThat(arbeidsforhold.tom, equalTo(null));
-        assertThat(arbeidsforhold.arbeidsgivernavn, equalTo("Testesen A/S, andre linje"));
-    }
-    
-    @Test
-    public void skalIgnorereNullVerdierIOrgNavn() throws Exception {
-        when(organisasjon.hentOrganisasjon(any(HentOrganisasjonRequest.class))).thenReturn(createOrgResponseWithNulls());
-        Arbeidsforhold arbeidsforhold = arbeidsforholdTransformer.transform(lagArbeidsforhold());
-        assertThat(arbeidsforhold.arbeidsgivernavn, equalTo("Testesen A/S, andre linje"));
-    }
-    
-    @Test
-    public void skalIgnorereTommeStrengerIOrgNavn() throws Exception {
-        when(organisasjon.hentOrganisasjon(any(HentOrganisasjonRequest.class))).thenReturn(createOrgResponseWithEmptyStrings());
-        Arbeidsforhold arbeidsforhold = arbeidsforholdTransformer.transform(lagArbeidsforhold());
         assertThat(arbeidsforhold.arbeidsgivernavn, equalTo("Testesen A/S, andre linje"));
     }
 
@@ -141,31 +109,4 @@ public class ArbeidsforholdTransformerTest {
         org.setOrgnummer(ORGNUMMER);
         return org;
     }
-    
-    private HentOrganisasjonResponse createOrgResponseWithNulls() {
-        HentOrganisasjonResponse response = new HentOrganisasjonResponse();
-        no.nav.tjeneste.virksomhet.organisasjon.v4.informasjon.Organisasjon org = new no.nav.tjeneste.virksomhet.organisasjon.v4.informasjon.Organisasjon();
-        UstrukturertNavn value = new UstrukturertNavn();
-        value.getNavnelinje().add("Testesen A/S");
-        value.getNavnelinje().add("andre linje");
-        value.getNavnelinje().add(null);
-        value.getNavnelinje().add(null);
-        org.setNavn(value);
-        response.setOrganisasjon(org);
-        return response;
-    }
-    
-    private HentOrganisasjonResponse createOrgResponseWithEmptyStrings() {
-        HentOrganisasjonResponse response = new HentOrganisasjonResponse();
-        no.nav.tjeneste.virksomhet.organisasjon.v4.informasjon.Organisasjon org = new no.nav.tjeneste.virksomhet.organisasjon.v4.informasjon.Organisasjon();
-        UstrukturertNavn value = new UstrukturertNavn();
-        value.getNavnelinje().add("Testesen A/S");
-        value.getNavnelinje().add("andre linje");
-        value.getNavnelinje().add("");
-        value.getNavnelinje().add("");
-        org.setNavn(value);
-        response.setOrganisasjon(org);
-        return response;
-    }
-
 }
