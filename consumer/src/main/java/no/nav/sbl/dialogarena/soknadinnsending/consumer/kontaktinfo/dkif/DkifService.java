62f90b09858502cf6477b651a9c7f36109f4f1b6
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2020-02-10 13:55:09 +0100

    null-safe håndtering av response fra Dkif

--- consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/kontaktinfo/dkif/DkifService.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/kontaktinfo/dkif/DkifService.java
@@ -2,11 +2,16 @@ package no.nav.sbl.dialogarena.soknadinnsending.consumer.kontaktinfo.dkif;
 
 import no.nav.sbl.dialogarena.sendsoknad.domain.DigitalKontaktinfo;
 import no.nav.sbl.dialogarena.soknadinnsending.consumer.kontaktinfo.dkif.dto.DigitalKontaktinfoBolk;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import static java.lang.System.getProperty;
 
 @Component
 public class DkifService {
 
+    private static final Logger log = LoggerFactory.getLogger(DkifService.class);
     private static final String DKIF_API_ENABLED = "dkif_api_enabled";
 
     private DkifConsumer consumer;
@@ -19,7 +24,7 @@ public class DkifService {
     }
 
     public boolean brukDkifApi() {
-        return Boolean.parseBoolean(System.getProperty(DKIF_API_ENABLED, "false"));
+        return Boolean.parseBoolean(getProperty(DKIF_API_ENABLED, "false"));
     }
 
     public String hentMobiltelefonnummer(String ident) {
@@ -28,6 +33,12 @@ public class DkifService {
 
     private String hentMobiltelefonnummerRest(String ident) {
         DigitalKontaktinfoBolk digitalKontaktinfoBolk = consumer.hentDigitalKontaktinfo(ident);
+        if (digitalKontaktinfoBolk == null || digitalKontaktinfoBolk.getKontaktinfo() == null || digitalKontaktinfoBolk.getKontaktinfo().getMobiltelefonnummer() == null) {
+            return null;
+        } else if ( digitalKontaktinfoBolk.getFeil() != null) {
+            log.warn("Dkif.api - response har feil - {}", digitalKontaktinfoBolk.getFeil().getMelding());
+            return null;
+        }
         return digitalKontaktinfoBolk.getKontaktinfo().getMobiltelefonnummer();
     }
 
--- consumer/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/kontaktinfo/dkif/DkifServiceTest.java
+++ consumer/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/kontaktinfo/dkif/DkifServiceTest.java
@@ -2,6 +2,7 @@ package no.nav.sbl.dialogarena.soknadinnsending.consumer.kontaktinfo.dkif;
 
 import no.nav.sbl.dialogarena.soknadinnsending.consumer.kontaktinfo.dkif.dto.DigitalKontaktinfo;
 import no.nav.sbl.dialogarena.soknadinnsending.consumer.kontaktinfo.dkif.dto.DigitalKontaktinfoBolk;
+import no.nav.sbl.dialogarena.soknadinnsending.consumer.kontaktinfo.dkif.dto.Feil;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +11,7 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.when;
 
@@ -36,6 +38,42 @@ public class DkifServiceTest {
         String response = service.hentMobiltelefonnummer("ident");
 
         assertEquals(mobiltelefonnummer, response);
+    }
+
+    @Test
+    public void skalReturnereNullHvis_DigitalKontaktinfoBolk_ErNull() {
+        when(dkifConsumer.hentDigitalKontaktinfo(anyString())).thenReturn(null);
+
+        String response = service.hentMobiltelefonnummer("ident");
+
+        assertNull(response);
+    }
+
+    @Test
+    public void skalReturnereNullHvis_DigitalKontaktinfoBolk_Kontaktinfo_ErNull() {
+        when(dkifConsumer.hentDigitalKontaktinfo(anyString())).thenReturn(new DigitalKontaktinfoBolk(null, null));
+
+        String response = service.hentMobiltelefonnummer("ident");
+
+        assertNull(response);
+    }
+
+    @Test
+    public void skalReturnereNullHvis_DigitalKontaktinfoBolk_Kontaktinfo_Mobiltelefonnummer_ErNull() {
+        when(dkifConsumer.hentDigitalKontaktinfo(anyString())).thenReturn(new DigitalKontaktinfoBolk(new DigitalKontaktinfo(null), null));
+
+        String response = service.hentMobiltelefonnummer("ident");
+
+        assertNull(response);
+    }
+
+    @Test
+    public void skalReturnereNullHvis_DigitalKontaktinfoBolk_Feil_ErSatt() {
+        when(dkifConsumer.hentDigitalKontaktinfo(anyString())).thenReturn(new DigitalKontaktinfoBolk(null, new Feil("feil feil feil")));
+
+        String response = service.hentMobiltelefonnummer("ident");
+
+        assertNull(response);
     }
 
     private DigitalKontaktinfoBolk createDigitalKontaktinfoBolk() {
