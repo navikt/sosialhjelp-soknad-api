cdb6945cec508ec2902dd620c481200b84a8d731
martintveter <41987225+martintveter@users.noreply.github.com>
2020-02-12 09:22:21 +0100

    Fix/rydding (#274)
    * #1 fjerner knytninger til "arbeid_rest_url" og "arbeidservice"-credentials. Kan ikke se at det er i bruk
    
    * #2 fjerner knytninger til "dokumentinnsending_smtpserver"
    
    * #3 fjerner "dagpengerbrosjyreurl" fra /miljovariabler (test)
    
    * #4 fjerner "skjemaveilederurl" fra /miljovariabler
    
    * #5 fjerner "reelarbeidsokerurl" fra /miljovariabler
    
    * #6 fjerner "dialogarena.navnolink.url" fra /miljovariabler
    
    * #7 fjerner "dialogarena.cms.url" fra /miljovariabler
    
    * #8 fjerner "soknad.alderspensjonurl" fra /miljovariabler
    
    * #9 fjerner "soknadtilleggsstonaderpath" fra /miljovariabler
    
    * #10 fjerner "dineutbetalingerlink" fra /miljovariabler
    
    * #11 fjerner "soknadinnsending.soknad.path" fra /miljovariabler og "soknadinnsending.ettersending.path" og SOKNADINNSENDING_LINK_URL fra properties
    
    * #12 fjerner "modia.url" fra /miljovariabler
    
    * #13 fjerner "saksoversikt.link.url" fra /miljovariabler

--- business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/service/InformasjonService.java
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/service/InformasjonService.java
@@ -9,48 +9,15 @@ import java.util.Map;
 @Component
 public class InformasjonService implements Miljovariabler {
 
-    @Value("${saksoversikt.link.url}")
-    private String saksoversiktUrl;
-    @Value("${dialogarena.navnolink.url}")
-    private String navNoUrl;
-    @Value("${soknad.skjemaveileder.url}")
-    private String skjemaveilederUrl;
-    @Value("${soknad.alderspensjon.url}")
-    private String alderspensjonUrl;
-    @Value("${soknad.reelarbeidsoker.url}")
-    private String reelarbeidsokerUrl;
-    @Value("${soknad.dagpengerbrosjyre.url}")
-    private String dagpengerBrosjyreUrl;
     @Value("${dittnav.link.url}")
     private String dittnavUrl;
     @Value("${soknad.ettersending.antalldager}")
     private String antallDager;
-    @Value("${dialogarena.cms.url}")
-    private String appresUrl;
-    @Value("${soknadinnsending.soknad.path}")
-    private String soknadinnsendingSoknadPath;
-    @Value("${soknadtilleggsstonader.path}")
-    private String soknadtilleggsstonaderPath;
-    @Value("${dineutbetalinger.link.url}")
-    private String dineUtbetalingerLink;
-
-    private String sporsmalsvarUrl = "";
 
     public Map<String,String> hentMiljovariabler() {
         Map<String, String> result = new HashMap<>();
 
-        result.put("saksoversikt.link.url", saksoversiktUrl);
         result.put("dittnav.link.url", dittnavUrl);
-        result.put("dialogarena.navnolink.url", navNoUrl);
-        result.put("soknad.skjemaveileder.url", skjemaveilederUrl);
-        result.put("soknad.alderspensjon.url", alderspensjonUrl);
-        result.put("soknad.reelarbeidsoker.url", reelarbeidsokerUrl);
-        result.put("soknad.dagpengerbrosjyre.url", dagpengerBrosjyreUrl);
-        result.put("dialogarena.cms.url", appresUrl);
-        result.put("soknadinnsending.soknad.path", soknadinnsendingSoknadPath);
-        result.put("modia.url", sporsmalsvarUrl);
-        result.put("soknadtilleggsstonader.url", soknadtilleggsstonaderPath);
-        result.put("dineutbetalinger.link.url", dineUtbetalingerLink);
         result.put("soknad.ettersending.antalldager", antallDager);
 
         result.putAll(getTestSpesifikkConfig());
