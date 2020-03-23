72a803c7c8c10befe5064e3aeea998db9a48a897
Halvor Bjørn <halvor.bjorn@nav.no>
2020-03-23 11:28:32 +0100

    Bytter til ny testkommune (#323)

--- domain/src/test/java/no/nav/sbl/dialogarena/sendsoknad/domain/digisosapi/DigisosApiImplTest.java
+++ domain/src/test/java/no/nav/sbl/dialogarena/sendsoknad/domain/digisosapi/DigisosApiImplTest.java
@@ -9,21 +9,21 @@ public class DigisosApiImplTest {
 
     @Test
     public void getDigisosId_whenFinnesAllerede_shouldReturnIdTest() {
-        String testresponse = "{\"timestamp\":1579253567738,\"status\":400,\"error\":\"Bad Request\",\"errorId\":\"afb2627d-1795-4ec1-a772-989d0a42a11a\",\"path\":\"/digisos/api/v1/soknader/2352/1100006QX\",\"originalPath\":null,\"message\":\"Soknad med tilhørende navEksternRefId 1100006QX finnes allerede i Fiks-Digisos med DigisosId a7b1c576-0851-455c-ad08-4f067be43629\",\"errorCode\":null,\"errorJson\":null}";
+        String testresponse = "{\"timestamp\":1579253567738,\"status\":400,\"error\":\"Bad Request\",\"errorId\":\"afb2627d-1795-4ec1-a772-989d0a42a11a\",\"path\":\"/digisos/api/v1/soknader/3002/1100006QX\",\"originalPath\":null,\"message\":\"Soknad med tilhørende navEksternRefId 1100006QX finnes allerede i Fiks-Digisos med DigisosId a7b1c576-0851-455c-ad08-4f067be43629\",\"errorCode\":null,\"errorJson\":null}";
         String digisosId = getDigisosIdFromResponse(testresponse, "1100006QX");
         assertEquals("a7b1c576-0851-455c-ad08-4f067be43629", digisosId);
     }
 
     @Test
     public void getDigisosId_whenOtherError_shouldNotReturnIdTest() {
-        String testresponse = "{\"timestamp\":1579253567738,\"status\":400,\"error\":\"Bad Request\",\"errorId\":\"afb2627d-1795-4ec1-a772-989d0a42a11a\",\"path\":\"/digisos/api/v1/soknader/2352/1100006QX\",\"originalPath\":null,\"message\":\"Det er skjedd en uventet feil. Her er en random id a7b1c576-0851-455c-ad08-4f067be43629\",\"errorCode\":null,\"errorJson\":null}";
+        String testresponse = "{\"timestamp\":1579253567738,\"status\":400,\"error\":\"Bad Request\",\"errorId\":\"afb2627d-1795-4ec1-a772-989d0a42a11a\",\"path\":\"/digisos/api/v1/soknader/3002/1100006QX\",\"originalPath\":null,\"message\":\"Det er skjedd en uventet feil. Her er en random id a7b1c576-0851-455c-ad08-4f067be43629\",\"errorCode\":null,\"errorJson\":null}";
         String digisosId = getDigisosIdFromResponse(testresponse, "1100006QX");
         assertNull(digisosId);
     }
 
     @Test
     public void getDigisosId_whenErrorForAnotherBehandlingsId_shouldNotReturnIdTest() {
-        String testresponse = "{\"timestamp\":1579253567738,\"status\":400,\"error\":\"Bad Request\",\"errorId\":\"afb2627d-1795-4ec1-a772-989d0a42a11a\",\"path\":\"/digisos/api/v1/soknader/2352/1100006QX\",\"originalPath\":null,\"message\":\"Det er skjedd en uventet feil. Her er en random id a7b1c576-0851-455c-ad08-4f067be43629\",\"errorCode\":null,\"errorJson\":null}";
+        String testresponse = "{\"timestamp\":1579253567738,\"status\":400,\"error\":\"Bad Request\",\"errorId\":\"afb2627d-1795-4ec1-a772-989d0a42a11a\",\"path\":\"/digisos/api/v1/soknader/3002/1100006QX\",\"originalPath\":null,\"message\":\"Det er skjedd en uventet feil. Her er en random id a7b1c576-0851-455c-ad08-4f067be43629\",\"errorCode\":null,\"errorJson\":null}";
         String digisosId = getDigisosIdFromResponse(testresponse, "110000001");
         assertNull(digisosId);
     }
