e0e73ff1723e09043d00d0bb4c47c9dc7229e3c9
martintveter <41987225+martintveter@users.noreply.github.com>
2020-04-16 11:54:20 +0200

    revert endringer fra #354 (#358)

--- business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/batch/oppgave/fiks/FiksSender.java
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/batch/oppgave/fiks/FiksSender.java
@@ -16,7 +16,6 @@ import org.springframework.stereotype.Service;
 import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -63,7 +62,7 @@ public class FiksSender {
 
         validerAtEttersendelseSinSoknadHarForsendelseId(sendtSoknad, svarPaForsendelseId);
 
-        Forsendelse forsendelse = new Forsendelse()
+        return new Forsendelse()
                 .withMottaker(new Adresse()
                         .withDigitalAdresse(
                                 new OrganisasjonDigitalAdresse().withOrgnr(sendtSoknad.getOrgnummer()))
@@ -82,13 +81,6 @@ public class FiksSender {
                         new NoarkMetadataFraAvleverendeSakssystem()
                                 .withDokumentetsDato(sendtSoknad.getBrukerFerdigDato())
                 );
-
-        if (Objects.equals(sendtSoknad.getBehandlingsId(), "11000L5XF")) {
-            return  forsendelse
-                    .withKunDigitalLevering(true);
-        }
-
-        return forsendelse;
     }
 
     private void validerAtEttersendelseSinSoknadHarForsendelseId(SendtSoknad sendtSoknad, String svarPaForsendelseId) {
