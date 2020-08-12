bbc7c79be3d465eb176775966e35f2e630876913
siljee <silje.hansen3@nav.no>
2020-08-12 12:26:03 +0200

    Slår OpenAmLoginFilter og LoginFilter.java sammen til ett filter og fjerner ubrukte klasser etter opprydding. Fjerner redirectUrl da den er null. Redirect er uansett noe saksoversikt bør håndtere om brukeren ikke er autentisert Fjerner modig-security-filter

--- web/src/main/java/no/nav/sbl/dialogarena/saml/OpenAmConfig.java
+++
@@ -1,8 +0,0 @@
-package no.nav.sbl.dialogarena.saml;
-
-import java.util.List;
-
-public class OpenAmConfig {
-    public List<String> additionalAttributes;
-    public OpenAMEventListener openAMEventListener = new OpenAMEventListener.DefaultOpenAMEventListener();
-}
--- web/src/main/java/no/nav/sbl/dialogarena/saml/OpenAmLoginFilter.java
+++ web/src/main/java/no/nav/sbl/dialogarena/saml/OpenAmLoginFilter.java
@@ -1,32 +1,60 @@
 package no.nav.sbl.dialogarena.saml;
 
+import lombok.extern.slf4j.Slf4j;
 import no.nav.common.auth.Subject;
+import no.nav.common.auth.SubjectHandler;
 import no.nav.sbl.util.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.*;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Optional;
 
+import static javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
 
-public class OpenAMLoginFilter implements LoginProvider {
-    private static final Logger log = LoggerFactory.getLogger(LoginProvider.class);
-
-    public static final String NAV_ESSO_COOKIE_NAVN = "nav-esso";
+@Slf4j
+public class OpenAmLoginFilter implements Filter {
+    private static final Logger log = LoggerFactory.getLogger(OpenAmLoginFilter.class);
 
     private final OpenAMUserInfoService userInfoService;
 
-    public OpenAMLoginFilter() {
-        this(new OpenAMUserInfoService());
-    }
+    public static final String NAV_ESSO_COOKIE_NAVN = "nav-esso";
 
-    OpenAMLoginFilter(OpenAMUserInfoService userInfoService) {
-        this.userInfoService = userInfoService;
+    public OpenAmLoginFilter() {
+        this.userInfoService = new OpenAMUserInfoService();
     }
 
     @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+
+        Subject subject = authenticate(httpServletRequest, httpServletResponse);
+        if (subject == null) {
+            unAuthenticated(httpServletRequest, httpServletResponse);
+        } else {
+            log.info("DEBUG - LoginFilter subject:  " + subject);
+            SubjectHandler.withSubject(subject, () -> chain.doFilter(request, response));
+        }
+    }
+
+    private void unAuthenticated(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException {
+        Optional<String> optionalRedirectUrl = Optional.empty();
+        if ("application/json".equals(httpServletRequest.getHeader("Accept")) || optionalRedirectUrl.isEmpty()) {
+            httpServletResponse.sendError(SC_UNAUTHORIZED);
+        } else {
+            httpServletResponse.sendRedirect(optionalRedirectUrl.get());
+        }
+    }
+
     public Subject authenticate(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
         String requestEksternSsoToken = getRequestEksternSsoToken(httpServletRequest);
         if (StringUtils.nullOrEmpty(requestEksternSsoToken)) {
@@ -43,11 +71,6 @@ public class OpenAMLoginFilter implements LoginProvider {
 
         log.info("DEBUG - OpenAMLoginFilter authenticate userInfo is present" + userInfo.get());
         return userInfo.get();
-    }
-
-    @Override
-    public Optional<String> redirectUrl(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
-        return Optional.empty();
     }
 
     private String getRequestEksternSsoToken(HttpServletRequest request) {
@@ -76,4 +99,8 @@ public class OpenAMLoginFilter implements LoginProvider {
         }
     }
 
+    @Override
+    public void destroy() {
+
+    }
 }
