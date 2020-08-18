3b6e29db9a82a41a28d66184ef672a749b7d96e9
siljee <silje.hansen3@nav.no>
2020-08-18 16:09:16 +0200

    Endre konstant for sts-url også

--- consumer/src/main/java/no/nav/sbl/dialogarena/sts/StsSecurityConstants.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/sts/StsSecurityConstants.java
@@ -3,7 +3,7 @@ package no.nav.sbl.dialogarena.sts;
 /* Originally from common-java-modules (no.nav.sbl.dialogarena.common.cxf) */
 
 public class StsSecurityConstants {
-    public static final String STS_URL_KEY = "no.nav.modig.security.sts.url";
+    public static final String STS_URL_KEY = "sts.url";
     public static final String SYSTEMUSER_USERNAME = "systemuser.username";
     public static final String SYSTEMUSER_PASSWORD = "systemuser.password";
 }
