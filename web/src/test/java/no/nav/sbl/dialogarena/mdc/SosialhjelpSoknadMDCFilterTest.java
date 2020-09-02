2761dc5abcade7c8eb79ad746804a92c6cb8ffdd
siljee <silje.hansen3@nav.no>
2020-09-02 08:44:30 +0200

    Reset subjecthandler etter tester og implisitt si at det er oidcSubjectHandler som skal testes i MDCTest

--- web/src/test/java/no/nav/sbl/dialogarena/mdc/SosialhjelpSoknadMDCFilterTest.java
+++ web/src/test/java/no/nav/sbl/dialogarena/mdc/SosialhjelpSoknadMDCFilterTest.java
@@ -1,5 +1,7 @@
 package no.nav.sbl.dialogarena.mdc;
 
+import no.nav.sbl.dialogarena.sendsoknad.domain.oidc.OidcSubjectHandlerService;
+import no.nav.sbl.dialogarena.sendsoknad.domain.oidc.SubjectHandler;
 import no.nav.sbl.dialogarena.sts.StsSecurityConstants;
 import org.hamcrest.collection.IsMapContaining;
 import org.junit.AfterClass;
@@ -22,11 +24,13 @@ public class SosialhjelpSoknadMDCFilterTest {
     @BeforeClass
     public static void setUp() {
         System.setProperty(StsSecurityConstants.SYSTEMUSER_USERNAME, "srvuser");
+        SubjectHandler.setSubjectHandlerService(new OidcSubjectHandlerService());
     }
 
     @AfterClass
     public static void cleanUp() {
         System.clearProperty(StsSecurityConstants.SYSTEMUSER_USERNAME);
+        SubjectHandler.resetOidcSubjectHandlerService();
     }
 
     @Test
