807cc9853124c4985bbcb93f4d160ee231cb8283
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2020-10-15 13:03:13 +0200

    Flytter Config for REST-consumere til restconfig-pakken

--- consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/restconfig/DigisosApiRestConfig.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/restconfig/DigisosApiRestConfig.java
@@ -2,14 +2,14 @@ package no.nav.sbl.dialogarena.soknadinnsending.consumer.restconfig;
 
 import no.nav.sbl.dialogarena.soknadinnsending.consumer.fiks.DigisosApi;
 import no.nav.sbl.dialogarena.soknadinnsending.consumer.fiks.DigisosApiImpl;
-import no.nav.sbl.dialogarena.soknadinnsending.consumer.fiks.KommuneInfoService;
 import no.nav.sbl.dialogarena.soknadinnsending.consumer.fiks.DigisosApiMock;
+import no.nav.sbl.dialogarena.soknadinnsending.consumer.fiks.KommuneInfoService;
 import no.nav.sbl.dialogarena.types.Pingable;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import static no.nav.sbl.dialogarena.common.cxf.InstanceSwitcher.createMetricsProxyWithInstanceSwitcher;
+import static no.nav.sbl.dialogarena.common.cxf.InstanceSwitcher.createSwitcher;
 import static no.nav.sbl.dialogarena.types.Pingable.Ping.feilet;
 import static no.nav.sbl.dialogarena.types.Pingable.Ping.lyktes;
 
@@ -24,7 +24,7 @@ public class DigisosApiRestConfig {
     public DigisosApi digisosApi() {
         DigisosApi mock = new DigisosApiMock().digisosApiMock();
         DigisosApi prod = new DigisosApiImpl();
-        return createMetricsProxyWithInstanceSwitcher("DigisosApi", prod, mock, DIGISOSAPI_WITHMOCK, DigisosApi.class);
+        return createSwitcher(prod, mock, DIGISOSAPI_WITHMOCK, DigisosApi.class);
     }
 
     @Bean
