bda159d7fcd3a2a273164757ee14553e1427a145
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2020-11-17 12:28:13 +0100

    logger mimetype for opplastet vedlegg

--- business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/util/FileDetectionUtils.java
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/util/FileDetectionUtils.java
@@ -1,8 +1,13 @@
 package no.nav.sbl.dialogarena.soknadinnsending.business.util;
 
 import org.apache.tika.Tika;
+import org.slf4j.Logger;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 public class FileDetectionUtils {
+
+    private static final Logger log = getLogger(FileDetectionUtils.class);
 
     public static String getMimeType(byte[] bytes) {
         return new Tika().detect(bytes);
@@ -10,6 +15,7 @@ public class FileDetectionUtils {
 
     public static boolean isImage(byte[] bytes) {
         String mimeType = new Tika().detect(bytes);
+        log.info("FileDetectionUtils.isImage - mimeType={}", mimeType);
         return (mimeType.equalsIgnoreCase("image/png") || mimeType.equalsIgnoreCase("image/jpeg"));
     }
 
