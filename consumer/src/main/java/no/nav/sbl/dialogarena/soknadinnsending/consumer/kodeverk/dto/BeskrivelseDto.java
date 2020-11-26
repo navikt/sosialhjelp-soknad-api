1099075b3fc2bd06fb14c2519868d62b70803629
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2020-11-26 15:06:36 +0100

    dto for kodeverk rest

---
+++ consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/kodeverk/dto/BeskrivelseDto.java
@@ -0,0 +1,29 @@
+package no.nav.sbl.dialogarena.soknadinnsending.consumer.kodeverk.dto;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class BeskrivelseDto {
+
+    private final String term;
+    private final String tekst;
+
+    @JsonCreator
+    public BeskrivelseDto(
+            @JsonProperty("term") String term,
+            @JsonProperty("tekst") String tekst
+    ) {
+        this.term = term;
+        this.tekst = tekst;
+    }
+
+    public String getTerm() {
+        return term;
+    }
+
+    public String getTekst() {
+        return tekst;
+    }
+}
