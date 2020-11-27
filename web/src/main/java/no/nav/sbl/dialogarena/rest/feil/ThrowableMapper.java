ea94c3dd7feb1c07b9032c63319c228f1a2cda70
Per-Christian Nielsen <per-christian.nielsen@nav.no>
2020-11-27 09:25:50 +0100

    Debug logging.

--- web/src/main/java/no/nav/sbl/dialogarena/rest/feil/ThrowableMapper.java
+++ web/src/main/java/no/nav/sbl/dialogarena/rest/feil/ThrowableMapper.java
@@ -29,10 +29,10 @@ public class ThrowableMapper implements ExceptionMapper<Throwable> {
 
             int status = exception.getResponse().getStatus();
             if (status == UNAUTHORIZED.getStatusCode()) {
-                logger.debug(e.getMessage(), e);
+                logger.warn(e.getMessage(), e);
                 return createUnauthorizedWithLoginLocationResponse("Autentiseringsfeil");
             } else if(status == FORBIDDEN.getStatusCode()) {
-                logger.debug(e.getMessage(), e);
+                logger.warn(e.getMessage(), e);
                 return createUnauthorizedWithLoginLocationResponse( "Autoriseringsfeil");
             } else if (e instanceof NotFoundException) {
                 logger.warn(e.getMessage(), e);
