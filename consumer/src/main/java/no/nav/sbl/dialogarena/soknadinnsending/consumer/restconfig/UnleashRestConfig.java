a77199c7457ad63e0cd2e9fc3eadd5552f321258
siljee <silje.hansen3@nav.no>
2020-12-04 08:26:46 +0100

    Bruke Unleash sitt java-bibliotek
    Bruker toggleFeatures for å sjekke status. Denne henter ut alle toggles, men gir oss også en status. Det er den de andre teamene i NAV, som har utviklet ping, bruker.

--- consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/restconfig/UnleashRestConfig.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/restconfig/UnleashRestConfig.java
@@ -1,46 +1,68 @@
 package no.nav.sbl.dialogarena.soknadinnsending.consumer.restconfig;
 
-import no.nav.sbl.dialogarena.soknadinnsending.consumer.unleash.UnleashConsumer;
-import no.nav.sbl.dialogarena.soknadinnsending.consumer.unleash.UnleashConsumerImpl;
-import no.nav.sbl.dialogarena.soknadinnsending.consumer.unleash.UnleashConsumerMock;
+import no.finn.unleash.DefaultUnleash;
+import no.finn.unleash.FakeUnleash;
+import no.finn.unleash.Unleash;
+import no.finn.unleash.repository.FeatureToggleResponse;
+import no.finn.unleash.repository.FeatureToggleResponse.Status;
+import no.finn.unleash.repository.HttpToggleFetcher;
+import no.finn.unleash.repository.ToggleFetcher;
+import no.finn.unleash.util.UnleashConfig;
 import no.nav.sbl.dialogarena.types.Pingable;
 import no.nav.sbl.dialogarena.types.Pingable.Ping.PingMetadata;
-import no.nav.sbl.rest.RestUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import javax.ws.rs.client.Client;
 
+import static no.finn.unleash.repository.FeatureToggleResponse.Status.CHANGED;
+import static no.finn.unleash.repository.FeatureToggleResponse.Status.NOT_CHANGED;
 import static no.nav.sbl.dialogarena.common.cxf.InstanceSwitcher.createSwitcher;
 import static no.nav.sbl.dialogarena.types.Pingable.Ping.feilet;
 import static no.nav.sbl.dialogarena.types.Pingable.Ping.lyktes;
 
 public class UnleashRestConfig {
     public static final String MOCK_KEY = "start.unleash.withmock";
+    public static final String APPLICATION_NAME = "sosialhjelp-soknad-api";
 
     @Value("${unleash_api_url}")
     private String endpoint;
 
+    @Value("${unleash_instance_id}")
+    private String instanceId;
+
     @Bean
-    public UnleashConsumer unleashConsumer() {
-        UnleashConsumer prod = new UnleashConsumerImpl(unleashClient(), endpoint);
-        UnleashConsumer mock = new UnleashConsumerMock().unleashConsumerMock();
-        return createSwitcher(prod, mock, MOCK_KEY, UnleashConsumer.class);
+    public Unleash unleashConsumer() {
+        Unleash prod = new DefaultUnleash(config());
+        Unleash mock = new FakeUnleash();
+        return createSwitcher(prod, mock, MOCK_KEY, Unleash.class);
     }
 
     @Bean
-    public Pingable unleashRestPing(UnleashConsumer unleashConsumer) {
+    public ToggleFetcher unleashToggleFetcher() {
+        return new HttpToggleFetcher(config());
+    }
+
+    @Bean
+    public Pingable unleashRestPing(ToggleFetcher unleashToggleFetcher) {
         return () -> {
             PingMetadata metadata = new PingMetadata("unleash_ping", endpoint, "Unleash", false);
             try {
-                unleashConsumer.ping();
-                return lyktes(metadata);
+                Status status = unleashToggleFetcher.fetchToggles().getStatus();
+                if (status == CHANGED || status == NOT_CHANGED) {
+                    return lyktes(metadata);
+                } else {
+                    return feilet(metadata, "Ping mot Unleash på " + endpoint + ". Ga status " + status);
+                }
             } catch (Exception e) {
                 return feilet(metadata, e);
             }
         };
     }
 
-    private Client unleashClient() {
-        return RestUtils.createClient();
+    private UnleashConfig config() {
+        return UnleashConfig.builder()
+                .appName(APPLICATION_NAME)
+                .instanceId(instanceId)
+                .unleashAPI(endpoint)
+                .build();
     }
 }
--- consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/unleash/UnleashConsumer.java
+++
@@ -1,6 +0,0 @@
-package no.nav.sbl.dialogarena.soknadinnsending.consumer.unleash;
-
-public interface UnleashConsumer {
-
-    void ping();
-}
