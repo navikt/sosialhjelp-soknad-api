79c1d78f245d7fb9f05b9434a372e20cd3ef072f
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-01-15 10:49:37 +0100

    Catcher RedisCommandTimeoutException. Ved redisStore.get håndteres de som at 'key' ikke finnes. Ved redisStore.set eller setex håndteres de som at key-value ikke blir lagret

--- consumer/src/main/java/no/nav/sbl/dialogarena/redis/RedisStore.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/redis/RedisStore.java
@@ -1,17 +1,23 @@
 package no.nav.sbl.dialogarena.redis;
 
 import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisCommandTimeoutException;
 import io.lettuce.core.api.StatefulRedisConnection;
 import io.lettuce.core.api.sync.RedisCommands;
 import io.lettuce.core.codec.ByteArrayCodec;
 import io.lettuce.core.codec.RedisCodec;
 import io.lettuce.core.codec.StringCodec;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 @Component
 public class RedisStore {
+
+    private static final Logger log = getLogger(RedisStore.class);
 
     private final RedisCommands<String, byte[]> commands;
 
@@ -22,14 +28,29 @@ public class RedisStore {
     }
 
     public byte[] get(String key) {
-        return commands.get(key);
+        try {
+            return commands.get(key);
+        } catch (RedisCommandTimeoutException e) {
+            log.warn("Redis timeout. Returnerer null.", e);
+            return null;
+        }
     }
 
     public String setex(String key, byte[] value, long timeToLiveSeconds) {
-        return commands.setex(key, timeToLiveSeconds, value);
+        try {
+            return commands.setex(key, timeToLiveSeconds, value);
+        } catch (RedisCommandTimeoutException e) {
+            log.warn("Redis timeout. Returnerer null.", e);
+            return null;
+        }
     }
 
     public String set(String key, byte[] value) {
-        return commands.set(key, value);
+        try {
+            return commands.set(key, value);
+        } catch (RedisCommandTimeoutException e) {
+            log.warn("Redis timeout. Returnerer null.", e);
+            return null;
+        }
     }
 }
