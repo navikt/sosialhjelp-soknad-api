7c8c9a577fb25acd5b020c8c2c222ebbd23165f4
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-02-08 14:38:02 +0100

    Fjerner vedleggskrav (gjenstående vedlegg) fra ettersendelsesiden hvis det er mer enn 30 dager siden søknaden ble sendt

--- web/src/main/java/no/nav/sbl/dialogarena/rest/mappers/VedleggMapper.java
+++ web/src/main/java/no/nav/sbl/dialogarena/rest/mappers/VedleggMapper.java
@@ -13,6 +13,7 @@ import no.nav.sbl.soknadsosialhjelp.vedlegg.JsonFiler;
 import no.nav.sbl.soknadsosialhjelp.vedlegg.JsonVedlegg;
 import no.nav.sbl.sosialhjelp.domain.OpplastetVedlegg;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -25,6 +26,7 @@ import static no.nav.sbl.dialogarena.rest.mappers.OkonomiskGruppeMapper.getGrupp
 import static no.nav.sbl.dialogarena.rest.mappers.VedleggTypeToSoknadTypeMapper.getSoknadPath;
 import static no.nav.sbl.dialogarena.rest.mappers.VedleggTypeToSoknadTypeMapper.isInSoknadJson;
 import static no.nav.sbl.dialogarena.rest.mappers.VedleggTypeToSoknadTypeMapper.vedleggTypeToSoknadType;
+import static no.nav.sbl.dialogarena.soknadinnsending.business.util.EttersendelseUtils.soknadSendtForMindreEnn30DagerSiden;
 import static no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.JOBB;
 import static no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.UTGIFTER_ANDRE_UTGIFTER;
 import static no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.UTGIFTER_ANNET_BARN;
@@ -33,7 +35,10 @@ import static no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.UTGIFTER_BARN_FR
 import static no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.UTGIFTER_BOLIGLAN_AVDRAG;
 import static no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.UTGIFTER_BOLIGLAN_RENTER;
 
-public class VedleggMapper {
+public final class VedleggMapper {
+
+    private VedleggMapper() {
+    }
 
     public static VedleggFrontend mapToVedleggFrontend(JsonVedlegg vedlegg, JsonOkonomi jsonOkonomi, List<OpplastetVedlegg> opplastedeVedlegg) {
         List<FilFrontend> filer = mapJsonFilerAndOpplastedeVedleggToFilerFrontend(vedlegg.getFiler(), opplastedeVedlegg);
@@ -56,11 +61,11 @@ public class VedleggMapper {
         String soknadPath = getSoknadPath(vedleggType);
 
         // Spesialtilfelle for avdrag og renter
-        if (soknadType.equals(UTGIFTER_BOLIGLAN_AVDRAG)){
+        if (soknadType.equals(UTGIFTER_BOLIGLAN_AVDRAG)) {
             return getRadListWithAvdragAndRenter(jsonOkonomi);
         }
 
-        switch (soknadPath){
+        switch (soknadPath) {
             case "utbetaling":
                 return getRadListFromUtbetaling(jsonOkonomi, soknadType);
             case "opplysningerUtgift":
@@ -80,8 +85,8 @@ public class VedleggMapper {
         List<VedleggRadFrontend> avdragRad = getRadListFromOversiktUtgift(jsonOkonomi, UTGIFTER_BOLIGLAN_AVDRAG);
         List<VedleggRadFrontend> renterRad = getRadListFromOversiktUtgift(jsonOkonomi, UTGIFTER_BOLIGLAN_RENTER);
 
-        if (avdragRad != null){
-            for (int i = 0; i < avdragRad.size(); i++){
+        if (avdragRad != null) {
+            for (int i = 0; i < avdragRad.size(); i++) {
                 avdragRad.get(i).withRenter(renterRad.get(i).renter);
             }
         }
@@ -101,7 +106,7 @@ public class VedleggMapper {
                 jsonOkonomi.getOpplysninger().getUtgift().stream()
                         .filter(utgift -> utgift.getType().equals(soknadType))
                         .map(utgift -> getRadFromOpplysningerUtgift(utgift, soknadType)).collect(Collectors.toList());
-        if (radList.isEmpty() && soknadType.equals(UTGIFTER_ANDRE_UTGIFTER)){
+        if (radList.isEmpty() && soknadType.equals(UTGIFTER_ANDRE_UTGIFTER)) {
             return Collections.singletonList(new VedleggRadFrontend());
         } else {
             return radList;
@@ -111,7 +116,7 @@ public class VedleggMapper {
     private static List<VedleggRadFrontend> getRadListFromInntekt(JsonOkonomi jsonOkonomi, String soknadType) {
         return jsonOkonomi.getOversikt().getInntekt().isEmpty() ? Collections.singletonList(new VedleggRadFrontend()) :
                 jsonOkonomi.getOversikt().getInntekt().stream()
-                        .filter(inntekt-> inntekt.getType().equals(soknadType))
+                        .filter(inntekt -> inntekt.getType().equals(soknadType))
                         .map(inntekt -> getRadFromInntekt(inntekt, soknadType)).collect(Collectors.toList());
     }
 
@@ -132,9 +137,9 @@ public class VedleggMapper {
     }
 
     private static VedleggRadFrontend getRadFromUtbetaling(JsonOkonomiOpplysningUtbetaling utbetaling) {
-        if (utbetaling.getBelop() != null){
+        if (utbetaling.getBelop() != null) {
             return new VedleggRadFrontend().withBelop(utbetaling.getBelop());
-        } else if (utbetaling.getBrutto() != null){
+        } else if (utbetaling.getBrutto() != null) {
             return new VedleggRadFrontend().withBelop(Integer.valueOf(String.valueOf(utbetaling.getBrutto())));
         } else if (utbetaling.getNetto() != null) {
             return new VedleggRadFrontend().withBelop(utbetaling.getNetto().intValue());
@@ -143,7 +148,7 @@ public class VedleggMapper {
     }
 
     private static VedleggRadFrontend getRadFromOpplysningerUtgift(JsonOkonomiOpplysningUtgift utgift, String soknadType) {
-        switch (soknadType){
+        switch (soknadType) {
             case UTGIFTER_ANDRE_UTGIFTER:
             case UTGIFTER_ANNET_BARN:
             case UTGIFTER_ANNET_BO:
@@ -156,12 +161,12 @@ public class VedleggMapper {
     }
 
     private static VedleggRadFrontend getRadFromInntekt(JsonOkonomioversiktInntekt inntekt, String soknadType) {
-        if (soknadType.equals(JOBB)){
+        if (soknadType.equals(JOBB)) {
             return new VedleggRadFrontend()
                     .withBrutto(inntekt.getBrutto())
                     .withNetto(inntekt.getNetto());
         }
-        if (inntekt.getBrutto() != null){
+        if (inntekt.getBrutto() != null) {
             return new VedleggRadFrontend().withBelop(inntekt.getBrutto());
         } else if (inntekt.getNetto() != null) {
             return new VedleggRadFrontend().withBelop(inntekt.getNetto());
@@ -170,9 +175,9 @@ public class VedleggMapper {
     }
 
     private static VedleggRadFrontend getRadFromOversiktUtgift(JsonOkonomioversiktUtgift utgift, String soknadType) {
-        if (soknadType.equals(UTGIFTER_BOLIGLAN_AVDRAG)){
+        if (soknadType.equals(UTGIFTER_BOLIGLAN_AVDRAG)) {
             return new VedleggRadFrontend().withAvdrag(utgift.getBelop());
-        } else if (soknadType.equals(UTGIFTER_BOLIGLAN_RENTER)){
+        } else if (soknadType.equals(UTGIFTER_BOLIGLAN_RENTER)) {
             return new VedleggRadFrontend().withRenter(utgift.getBelop());
         }
         return new VedleggRadFrontend().withBelop(utgift.getBelop());
@@ -187,23 +192,35 @@ public class VedleggMapper {
         }).collect(Collectors.toList());
     }
 
-    public static List<EttersendtVedlegg> mapVedleggToSortedListOfEttersendteVedlegg(List<OpplastetVedlegg> opplastedeVedlegg, List<JsonVedlegg> originaleVedlegg) {
+    public static List<EttersendtVedlegg> mapVedleggToSortedListOfEttersendteVedlegg(LocalDateTime innsendingstidspunkt, List<OpplastetVedlegg> opplastedeVedlegg, List<JsonVedlegg> originaleVedlegg) {
         SortedMap<String, EttersendtVedlegg> ettersendteVedlegg = new TreeMap<>(sortAlphabeticallyAndPutTypeAnnetLast());
 
         originaleVedlegg.forEach(vedlegg -> {
             String sammensattNavn = vedlegg.getType() + "|" + vedlegg.getTilleggsinfo();
 
             if (!ettersendteVedlegg.containsKey(sammensattNavn)) {
-                List<FilFrontend> filerFrontend = new ArrayList<>();
-                if (vedlegg.getStatus().equals("LastetOpp")){
-                    List<JsonFiler> filer = vedlegg.getFiler();
-                    filerFrontend = mapJsonFilerAndOpplastedeVedleggToFilerFrontend(filer, opplastedeVedlegg);
-                }
+                if (innsendingstidspunkt != null && soknadSendtForMindreEnn30DagerSiden(innsendingstidspunkt.toLocalDate())) {
+                    List<FilFrontend> filerFrontend = new ArrayList<>();
+                    if (vedlegg.getStatus().equals("LastetOpp")) {
+                        List<JsonFiler> filer = vedlegg.getFiler();
+                        filerFrontend = mapJsonFilerAndOpplastedeVedleggToFilerFrontend(filer, opplastedeVedlegg);
+                    }
 
-                ettersendteVedlegg.put(sammensattNavn, new EttersendtVedlegg()
-                        .withType(sammensattNavn)
-                        .withVedleggStatus(vedlegg.getStatus())
-                        .withFiler(filerFrontend));
+                    ettersendteVedlegg.put(sammensattNavn, new EttersendtVedlegg()
+                            .withType(sammensattNavn)
+                            .withVedleggStatus(vedlegg.getStatus())
+                            .withFiler(filerFrontend));
+                } else {
+                    if (vedlegg.getStatus().equals("LastetOpp")) {
+                        List<JsonFiler> filer = vedlegg.getFiler();
+                        var filerFrontend = mapJsonFilerAndOpplastedeVedleggToFilerFrontend(filer, opplastedeVedlegg);
+
+                        ettersendteVedlegg.put(sammensattNavn, new EttersendtVedlegg()
+                                .withType(sammensattNavn)
+                                .withVedleggStatus(vedlegg.getStatus())
+                                .withFiler(filerFrontend));
+                    }
+                }
             }
         });
 
