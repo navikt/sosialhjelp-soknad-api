c22ab2c3ea058e29006d675576390d3c5e2ac04d
siljee <silje.hansen3@nav.no>
2021-02-08 11:32:05 +0100

    Rund av til nærmeste heltall

--- business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/util/MetricsUtils.java
+++ business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/util/MetricsUtils.java
@@ -13,7 +13,7 @@ public class MetricsUtils {
 
     public static int getProsent(int partial, int total) {
         if (total == 0) return 0;
-
-        return (int) ((double) partial/total * 100); // Cast til double for å få desimaler i delingen. Cast til int fordi det er kun er nødvendig med prosent i heltall
+        double prosent = (double) partial/total * 100; // Cast til double for å få desimaler i delingen
+        return (int) Math.round(prosent);
     }
 }
--- business/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/business/util/MetricsUtilsTest.java
+++ business/src/test/java/no/nav/sbl/dialogarena/soknadinnsending/business/util/MetricsUtilsTest.java
@@ -9,8 +9,15 @@ public class MetricsUtilsTest {
 
     @Test
     public void getProsent_shouldWork() {
-        assertEquals(38, getProsent(7, 18), 0);
+        assertEquals(22, getProsent(4, 18), 0);
         assertEquals(99, getProsent(99, 100), 0);
+    }
+
+    @Test
+    public void getProsent_shouldRoundToNearestInteger() {
+        assertEquals(3, getProsent(3_499, 100_000), 0);
+        assertEquals(4, getProsent(3_500, 100_000), 0);
+        assertEquals(4, getProsent(3_599, 100_000), 0);
     }
 
     @Test
