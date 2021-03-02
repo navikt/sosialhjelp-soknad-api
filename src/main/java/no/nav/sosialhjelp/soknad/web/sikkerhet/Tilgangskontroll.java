ace6adf11d07eb6445b38bafdac34967563be927
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-03-02 10:58:39 +0100

    fnr -> eier

--- src/main/java/no/nav/sosialhjelp/soknad/web/sikkerhet/Tilgangskontroll.java
+++ src/main/java/no/nav/sosialhjelp/soknad/web/sikkerhet/Tilgangskontroll.java
@@ -44,25 +44,25 @@ public class Tilgangskontroll {
 
     public void verifiserBrukerHarTilgangTilSoknad(String behandlingsId) {
         Optional<SoknadUnderArbeid> soknadUnderArbeidOptional = soknadUnderArbeidRepository.hentSoknadOptional(behandlingsId, SubjectHandler.getUserId());
-        String fnr;
+        String eier;
         if (soknadUnderArbeidOptional.isPresent()) {
-            fnr = soknadUnderArbeidOptional.get().getEier();
+            eier = soknadUnderArbeidOptional.get().getEier();
         } else {
             throw new AuthorizationException("Bruker har ikke tilgang til søknaden.");
         }
 
-        verifiserAtInnloggetBrukerErEierAvSoknad(fnr);
+        verifiserAtInnloggetBrukerErEierAvSoknad(eier);
     }
 
     public void verifiserBrukerHarTilgangTilMetadata(String behandlingsId) {
-        String fnr = "undefined";
+        String eier = "undefined";
         try {
             SoknadMetadata metadata = soknadMetadataRepository.hent(behandlingsId);
-            fnr = metadata.fnr;
+            eier = metadata.fnr;
         } catch (Exception e) {
             logger.warn("Kunne ikke avgjøre hvem som eier søknad med behandlingsId {} -> Ikke tilgang.", behandlingsId, e);
         }
-        verifiserAtInnloggetBrukerErEierAvSoknad(fnr);
+        verifiserAtInnloggetBrukerErEierAvSoknad(eier);
     }
 
     private void verifiserAtInnloggetBrukerErEierAvSoknad(String eier) {
