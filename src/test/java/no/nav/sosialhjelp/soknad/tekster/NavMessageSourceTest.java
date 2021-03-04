525493dfb566fd5dfcda074184a673c450cd868a
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-03-04 13:34:51 +0100

    rydder i NavMessageSourceTest

--- src/test/java/no/nav/sosialhjelp/soknad/tekster/NavMessageSourceTest.java
+++ src/test/java/no/nav/sosialhjelp/soknad/tekster/NavMessageSourceTest.java
@@ -16,10 +16,6 @@ public class NavMessageSourceTest {
     {
         mockedCmsValues.put("classpath:sendsoknad_nb_NO", "felles.key=norsk felles fra minne");
         mockedCmsValues.put("classpath:sendsoknad_en_GB", "felles.key=engelsk felles fra minne");
-        mockedCmsValues.put("classpath:dagpenger_nb_NO", "dagpenger.key=norsk dagpenger fra minne;dagpenger.key.minne=norsk dagpenger kun i minne");
-        mockedCmsValues.put("classpath:dagpenger_en_GB", "dagpenger.key=engelsk dagpenger fra minne");
-        mockedCmsValues.put("classpath:foreldrepenger_nb_NO", "foreldrepenger.key=norsk foreldrepenger fra minne");
-        mockedCmsValues.put("classpath:foreldrepenger_en_GB", "foreldrepenger.key=engelsk foreldrepenger fra minne");
     }
 
     private NavMessageSource messageSource;
@@ -48,49 +44,32 @@ public class NavMessageSourceTest {
         };
 
         messageSource.setBasenames(
-                new NavMessageSource.Bundle("sendsoknad", "classpath:sendsoknad"),
-                new NavMessageSource.Bundle("dagpenger", "classpath:dagpenger"),
-                new NavMessageSource.Bundle("foreldrepenger", "classpath:foreldrepenger")
+                new NavMessageSource.Bundle("sendsoknad", "classpath:sendsoknad")
         );
     }
 
     @Test
     public void skalHenteSoknadensEgneTeksterOgFellesTeksterNorsk() {
-        Properties properties = messageSource.getBundleFor("dagpenger", new Locale("nb", "NO"));
+        Properties properties = messageSource.getBundleFor("sendsoknad", new Locale("nb", "NO"));
         assertEquals("norsk felles fra minne", properties.getProperty("felles.key"));
-        assertEquals("norsk dagpenger fra minne", properties.getProperty("dagpenger.key"));
-
-        properties = messageSource.getBundleFor("foreldrepenger", new Locale("nb", "NO"));
-        assertEquals("norsk felles fra minne", properties.getProperty("felles.key"));
-        assertEquals("norsk foreldrepenger fra minne", properties.getProperty("foreldrepenger.key"));
     }
 
     @Test
     public void skalHenteSoknadensEgneTeksterOgFellesTeksterEngelsk() {
-        Properties properties = messageSource.getBundleFor("dagpenger", new Locale("en", "GB"));
+        Properties properties = messageSource.getBundleFor("sendsoknad", new Locale("en", "GB"));
         assertEquals("engelsk felles fra minne", properties.getProperty("felles.key"));
-        assertEquals("engelsk dagpenger fra minne", properties.getProperty("dagpenger.key"));
-
-        properties = messageSource.getBundleFor("foreldrepenger", new Locale("en", "GB"));
-        assertEquals("engelsk felles fra minne", properties.getProperty("felles.key"));
-        assertEquals("engelsk foreldrepenger fra minne", properties.getProperty("foreldrepenger.key"));
     }
 
     @Test
     public void skalIkkeHenteAndreSoknadersTekster() {
-        Properties properties = messageSource.getBundleFor("dagpenger", new Locale("nb", "NO"));
+        Properties properties = messageSource.getBundleFor("sendsoknad", new Locale("nb", "NO"));
         assertFalse(properties.containsKey("foreldrepenger.key"));
-
-        properties = messageSource.getBundleFor("foreldrepenger", new Locale("nb", "NO"));
-        assertFalse(properties.containsKey("dagpenger.key"));
     }
 
     @Test
     public void skalHenteAlleTeksterHvisTypeMangler() {
         Properties properties = messageSource.getBundleFor(null, new Locale("nb", "NO"));
         assertEquals("norsk felles fra minne", properties.getProperty("felles.key"));
-        assertEquals("norsk dagpenger fra minne", properties.getProperty("dagpenger.key"));
-        assertEquals("norsk foreldrepenger fra minne", properties.getProperty("foreldrepenger.key"));
     }
 
 }
