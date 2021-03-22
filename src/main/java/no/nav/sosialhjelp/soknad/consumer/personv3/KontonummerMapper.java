f3099548c1d144111d0815ba9d339e70d5a7886a
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-03-22 11:19:22 +0100

    gammal kode import-fix

--- src/main/java/no/nav/sosialhjelp/soknad/consumer/personv3/KontonummerMapper.java
+++ src/main/java/no/nav/sosialhjelp/soknad/consumer/personv3/KontonummerMapper.java
@@ -1,6 +1,10 @@
 package no.nav.sosialhjelp.soknad.consumer.personv3;
 
 import no.nav.sosialhjelp.soknad.domain.model.Kontonummer;
+import no.nav.tjeneste.virksomhet.person.v3.informasjon.Bankkonto;
+import no.nav.tjeneste.virksomhet.person.v3.informasjon.BankkontoNorge;
+import no.nav.tjeneste.virksomhet.person.v3.informasjon.Bankkontonummer;
+import no.nav.tjeneste.virksomhet.person.v3.informasjon.Bruker;
 import no.nav.tjeneste.virksomhet.person.v3.informasjon.Person;
 
 import static java.util.Optional.ofNullable;
@@ -15,21 +19,19 @@ public final class KontonummerMapper {
                 .withKontonummer(kanskjeKontonummer(person));
     }
 
-    private static String kanskjeKontonummer(no.nav.tjeneste.virksomhet.person.v3.informasjon.Person person) {
-        if (person instanceof no.nav.tjeneste.virksomhet.person.v3.informasjon.Bruker) {
-            no.nav.tjeneste.virksomhet.person.v3.informasjon.Bankkonto bankkonto =
-                    ((no.nav.tjeneste.virksomhet.person.v3.informasjon.Bruker) person).getBankkonto();
+    private static String kanskjeKontonummer(Person person) {
+        if (person instanceof Bruker) {
+            Bankkonto bankkonto = ((Bruker) person).getBankkonto();
             return kanskjeKontonummer(bankkonto);
         }
         return null;
     }
 
-    private static String kanskjeKontonummer(no.nav.tjeneste.virksomhet.person.v3.informasjon.Bankkonto bankkonto) {
-        if (bankkonto instanceof no.nav.tjeneste.virksomhet.person.v3.informasjon.BankkontoNorge) {
-            no.nav.tjeneste.virksomhet.person.v3.informasjon.BankkontoNorge bankkontoNorge =
-                    (no.nav.tjeneste.virksomhet.person.v3.informasjon.BankkontoNorge) bankkonto;
+    private static String kanskjeKontonummer(Bankkonto bankkonto) {
+        if (bankkonto instanceof BankkontoNorge) {
+            BankkontoNorge bankkontoNorge = (BankkontoNorge) bankkonto;
             return ofNullable(bankkontoNorge.getBankkonto())
-                    .map(no.nav.tjeneste.virksomhet.person.v3.informasjon.Bankkontonummer::getBankkontonummer)
+                    .map(Bankkontonummer::getBankkontonummer)
                     .orElse(null);
         }
         return null;
