40c01584c4ddd4011cdecab094f02cd20ba54f9d
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-04-09 09:53:47 +0200

    * retryConfig med default params. * retry oppslag kontonummer

--- src/main/java/no/nav/sosialhjelp/soknad/oppslag/OppslagConsumerImpl.java
+++ src/main/java/no/nav/sosialhjelp/soknad/oppslag/OppslagConsumerImpl.java
@@ -1,5 +1,6 @@
 package no.nav.sosialhjelp.soknad.oppslag;
 
+import io.github.resilience4j.retry.Retry;
 import no.nav.sosialhjelp.soknad.consumer.exceptions.TjenesteUtilgjengeligException;
 import no.nav.sosialhjelp.soknad.consumer.mdc.MDCOperations;
 import no.nav.sosialhjelp.soknad.domain.model.oidc.SubjectHandler;
@@ -9,9 +10,12 @@ import org.slf4j.Logger;
 
 import javax.ws.rs.NotAuthorizedException;
 import javax.ws.rs.NotFoundException;
+import javax.ws.rs.ServerErrorException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.Invocation;
 
+import static no.nav.sosialhjelp.soknad.consumer.retry.RetryUtils.retryConfig;
+import static no.nav.sosialhjelp.soknad.consumer.retry.RetryUtils.withRetry;
 import static no.nav.sosialhjelp.soknad.domain.model.util.HeaderConstants.BEARER;
 import static no.nav.sosialhjelp.soknad.domain.model.util.HeaderConstants.HEADER_CALL_ID;
 import static no.nav.sosialhjelp.soknad.domain.model.util.HeaderConstants.HEADER_CONSUMER_ID;
@@ -19,14 +23,19 @@ import static org.slf4j.LoggerFactory.getLogger;
 
 public class OppslagConsumerImpl implements OppslagConsumer {
 
-    private static final Logger logger = getLogger(OppslagConsumerImpl.class);
+    private static final Logger log = getLogger(OppslagConsumerImpl.class);
 
     private final Client client;
     private final String endpoint;
+    private final Retry retry;
 
     public OppslagConsumerImpl(Client client, String endpoint) {
         this.client = client;
         this.endpoint = endpoint;
+        this.retry = retryConfig(
+                endpoint,
+                new Class[]{ServerErrorException.class},
+                log);
     }
 
     @Override
@@ -34,7 +43,7 @@ public class OppslagConsumerImpl implements OppslagConsumer {
         var request = client.target(endpoint + "ping").request();
         try (var response = request.get()) {
             if (response.getStatus() != 200) {
-                logger.warn("Ping feilet mot oppslag.kontonummer: {}", response.getStatus());
+                log.warn("Ping feilet mot oppslag.kontonummer: {}", response.getStatus());
             }
         }
     }
@@ -43,15 +52,15 @@ public class OppslagConsumerImpl implements OppslagConsumer {
     public KontonummerDto getKontonummer(String ident) {
         var request = lagRequest(endpoint + "kontonummer");
         try {
-            return request.get(KontonummerDto.class);
+            return withRetry(retry, () -> request.get(KontonummerDto.class));
         } catch (NotAuthorizedException e) {
-            logger.warn("oppslag.kontonummer - 401 Unauthorized - {}", e.getMessage());
+            log.warn("oppslag.kontonummer - 401 Unauthorized - {}", e.getMessage());
             return null;
         } catch (NotFoundException e) {
-            logger.warn("oppslag.kontonummer - 404 Not Found - {}", e.getMessage());
+            log.warn("oppslag.kontonummer - 404 Not Found - {}", e.getMessage());
             return null;
         } catch (Exception e) {
-            logger.warn("oppslag.kontonummer - Noe uventet feilet");
+            log.warn("oppslag.kontonummer - Noe uventet feilet");
             throw new TjenesteUtilgjengeligException("oppslag.kontonummer", e);
         }
     }
