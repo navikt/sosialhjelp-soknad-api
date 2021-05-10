7b6ee586baa0b891c9e26ecb51c155eeb26026b3
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-05-10 10:21:25 +0200

    ny tid

--- src/main/java/no/nav/sosialhjelp/soknad/business/batch/SlettSoknadUnderArbeidScheduler.java
+++ src/main/java/no/nav/sosialhjelp/soknad/business/batch/SlettSoknadUnderArbeidScheduler.java
@@ -17,7 +17,7 @@ public class SlettSoknadUnderArbeidScheduler {
 
     private static final Logger logger = getLogger(SlettSoknadUnderArbeidScheduler.class);
 
-    private static final String KLOKKEN_HALV_FEM_OM_NATTEN = "0 20 10 * * *";
+    private static final String KLOKKEN_HALV_FEM_OM_NATTEN = "0 35 10 * * *";
     private static final int SCHEDULE_INTERRUPT_S = 60 * 10;
 
     private LocalDateTime batchStartTime;
