c769847735a9ca96be03d8ec3c14686212e80550
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-05-26 16:32:13 +0200

    unødvendige greier for test

--- src/test/java/no/nav/sosialhjelp/soknad/web/config/TestSoknadApplication.java
+++ src/test/java/no/nav/sosialhjelp/soknad/web/config/TestSoknadApplication.java
@@ -1,11 +1,9 @@
 package no.nav.sosialhjelp.soknad.web.config;
 
-import no.nav.sosialhjelp.soknad.web.oidc.TestTokenGeneratorResource;
 import no.nav.sosialhjelp.soknad.web.rest.SoknadApplication;
 
 public class TestSoknadApplication extends SoknadApplication {
 
     public TestSoknadApplication() {
-        register(TestTokenGeneratorResource.class); // Legger på endepunkter for å hente ut test-cookie til lokal kjøring
     }
 }
