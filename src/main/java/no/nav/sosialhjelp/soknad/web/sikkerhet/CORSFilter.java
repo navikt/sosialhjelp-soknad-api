0a0d0237454db42c7ef6803c7935772ae12bc954
martintveter <41987225+martintveter@users.noreply.github.com>
2021-05-27 09:42:30 +0200

    Skriver om CORSFilter til ContainerResponseFilter, og trekker ut fra web.xml (#774)

--- src/main/java/no/nav/sosialhjelp/soknad/web/sikkerhet/CORSFilter.java
+++ src/main/java/no/nav/sosialhjelp/soknad/web/sikkerhet/CORSFilter.java
@@ -2,51 +2,29 @@ package no.nav.sosialhjelp.soknad.web.sikkerhet;
 
 import no.nav.sosialhjelp.soknad.domain.model.util.ServiceUtils;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
 import java.util.List;
+import java.util.Optional;
 
 import static java.util.Arrays.asList;
 
-public class CORSFilter implements Filter {
+@Provider
+public class CORSFilter implements ContainerResponseFilter {
     private static final List<String> ALLOWED_ORIGINS = asList(
             "https://tjenester.nav.no",
             "https://www.nav.no");
 
     @Override
-    public void init(FilterConfig filterConfig) {}
-
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-
-        String origin = "*";
-        if (servletRequest instanceof  HttpServletRequest) {
-            HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
-            origin = httpRequest.getHeader("Origin");
-        }
-
-        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
-        setCorsHeaders(httpResponse, origin);
-
-        filterChain.doFilter(servletRequest, httpResponse);
-    }
-
-    public void setCorsHeaders(HttpServletResponse httpResponse, String origin) {
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
+        var origin = Optional.ofNullable(requestContext.getHeaderString("Origin")).orElse("*");
         if (ServiceUtils.isNonProduction() || ALLOWED_ORIGINS.contains(origin)) {
-            httpResponse.setHeader("Access-Control-Allow-Origin", origin);
-            httpResponse.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, X-XSRF-TOKEN, Nav-Call-Id, Authorization");
-            httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-            httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
+            responseContext.getHeaders().add("Access-Control-Allow-Origin", origin);
+            responseContext.getHeaders().add("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, X-XSRF-TOKEN, Nav-Call-Id, Authorization");
+            responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
         }
     }
-
-    @Override
-    public void destroy() {}
 }
