00c588fb75a76ba33d0c4b012bca03fba577b9ae
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-05-27 09:22:31 +0200

    service

--- src/main/java/no/nav/sosialhjelp/soknad/business/service/ServiceConfig.java
+++ src/main/java/no/nav/sosialhjelp/soknad/business/service/ServiceConfig.java
@@ -3,6 +3,7 @@ package no.nav.sosialhjelp.soknad.business.service;
 import no.nav.sosialhjelp.soknad.business.InnsendingService;
 import no.nav.sosialhjelp.soknad.business.SoknadUnderArbeidService;
 import no.nav.sosialhjelp.soknad.business.service.digisosapi.DigisosApiService;
+import no.nav.sosialhjelp.soknad.business.service.minesaker.MineSakerMetadataService;
 import no.nav.sosialhjelp.soknad.business.service.soknadservice.EttersendingService;
 import no.nav.sosialhjelp.soknad.business.service.soknadservice.InnsendtSoknadService;
 import no.nav.sosialhjelp.soknad.business.service.soknadservice.SoknadMetricsService;
@@ -22,7 +23,8 @@ import org.springframework.context.annotation.Import;
         InnsendingService.class,
         SoknadUnderArbeidService.class,
         TextService.class,
-        OpplastetVedleggService.class
+        OpplastetVedleggService.class,
+        MineSakerMetadataService.class
 })
 public class ServiceConfig {
 }
