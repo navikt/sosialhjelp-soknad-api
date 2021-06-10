afe7b9414ec229fe4cc8b3f796967c71373d5635
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-06-10 09:47:24 +0200

    legger endepunkt bak featuretoggle, slik at branch kan dyttes ut til prod

--- src/main/java/no/nav/sosialhjelp/soknad/web/rest/ressurser/eksponerte/MineSakerMetadataRessurs.java
+++ src/main/java/no/nav/sosialhjelp/soknad/web/rest/ressurser/eksponerte/MineSakerMetadataRessurs.java
@@ -1,5 +1,6 @@
 package no.nav.sosialhjelp.soknad.web.rest.ressurser.eksponerte;
 
+import no.finn.unleash.Unleash;
 import no.nav.security.token.support.core.api.ProtectedWithClaims;
 import no.nav.security.token.support.core.api.Unprotected;
 import no.nav.sosialhjelp.metrics.aspects.Timed;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import java.util.Collections;
 import java.util.List;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
@@ -27,13 +29,17 @@ import static org.slf4j.LoggerFactory.getLogger;
 public class MineSakerMetadataRessurs {
 
     private static final Logger log = getLogger(MineSakerMetadataRessurs.class);
+    private static final String MINESAKER_INNSENDTE_ENDEPUNKT_ENABLED = "sosialhjelp.soknad.minesaker-innsendte-endepunkt-enabled";
 
     private final MineSakerMetadataService mineSakerMetadataService;
+    private final Unleash unleash;
 
     public MineSakerMetadataRessurs(
-            MineSakerMetadataService mineSakerMetadataService
+            MineSakerMetadataService mineSakerMetadataService,
+            Unleash unleash
     ) {
         this.mineSakerMetadataService = mineSakerMetadataService;
+        this.unleash = unleash;
     }
 
     /**
@@ -43,6 +49,11 @@ public class MineSakerMetadataRessurs {
     @GET
     @Path("/innsendte")
     public List<InnsendtSoknadDto> hentInnsendteSoknaderForBruker() {
+        if (unleash.isEnabled(MINESAKER_INNSENDTE_ENDEPUNKT_ENABLED, false)) {
+            log.info("Endepunkt for henting av innsendte søknader er ikke enabled. Returnerer tom liste.");
+            return Collections.emptyList();
+        }
+
         var fnr = SubjectHandler.getUserId();
         return mineSakerMetadataService.hentInnsendteSoknader(fnr);
     }
