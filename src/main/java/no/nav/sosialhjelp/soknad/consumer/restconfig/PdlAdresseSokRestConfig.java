32fafe2e5836d094f4e780bea5b6f94ae3416895
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-06-10 08:40:27 +0200

    fjern abstract config

--- src/main/java/no/nav/sosialhjelp/soknad/consumer/restconfig/PdlAdresseSokRestConfig.java
+++ src/main/java/no/nav/sosialhjelp/soknad/consumer/restconfig/PdlAdresseSokRestConfig.java
@@ -1,18 +1,32 @@
 package no.nav.sosialhjelp.soknad.consumer.restconfig;
 
+import no.nav.sosialhjelp.soknad.consumer.common.rest.RestUtils;
 import no.nav.sosialhjelp.soknad.consumer.pdl.adressesok.PdlAdresseSokConsumer;
 import no.nav.sosialhjelp.soknad.consumer.sts.apigw.STSConsumer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 
-public class PdlAdresseSokRestConfig extends PdlConfig {
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientRequestFilter;
+
+import static java.lang.System.getenv;
+import static no.nav.sosialhjelp.soknad.domain.model.util.HeaderConstants.HEADER_NAV_APIKEY;
+
+public class PdlAdresseSokRestConfig {
+
+    private static final String PDLAPI_APIKEY = "PDLAPI_APIKEY";
 
     @Value("${pdl_api_url}")
     private String endpoint;
 
     @Bean
     public PdlAdresseSokConsumer pdlAdresseSokConsumer(STSConsumer stsConsumer) {
-        return new PdlAdresseSokConsumer(pdlClient(), endpoint, stsConsumer);
+        return new PdlAdresseSokConsumer(pdlAdresseSokClient(), endpoint, stsConsumer);
     }
 
+    private Client pdlAdresseSokClient() {
+        final var apiKey = getenv(PDLAPI_APIKEY);
+        return RestUtils.createClient()
+                .register((ClientRequestFilter) requestContext -> requestContext.getHeaders().putSingle(HEADER_NAV_APIKEY, apiKey));
+    }
 }
--- src/main/java/no/nav/sosialhjelp/soknad/consumer/restconfig/PdlHentPersonRestConfig.java
+++ src/main/java/no/nav/sosialhjelp/soknad/consumer/restconfig/PdlHentPersonRestConfig.java
@@ -1,5 +1,6 @@
 package no.nav.sosialhjelp.soknad.consumer.restconfig;
 
+import no.nav.sosialhjelp.soknad.consumer.common.rest.RestUtils;
 import no.nav.sosialhjelp.soknad.consumer.pdl.person.PdlHentPersonConsumer;
 import no.nav.sosialhjelp.soknad.consumer.pdl.person.PdlHentPersonConsumerImpl;
 import no.nav.sosialhjelp.soknad.consumer.sts.apigw.STSConsumer;
@@ -8,17 +9,24 @@ import no.nav.sosialhjelp.soknad.web.selftest.Pingable.Ping.PingMetadata;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientRequestFilter;
+
+import static java.lang.System.getenv;
+import static no.nav.sosialhjelp.soknad.domain.model.util.HeaderConstants.HEADER_NAV_APIKEY;
 import static no.nav.sosialhjelp.soknad.web.selftest.Pingable.Ping.feilet;
 import static no.nav.sosialhjelp.soknad.web.selftest.Pingable.Ping.lyktes;
 
-public class PdlHentPersonRestConfig extends PdlConfig {
+public class PdlHentPersonRestConfig {
+
+    private static final String PDLAPI_APIKEY = "PDLAPI_APIKEY";
 
     @Value("${pdl_api_url}")
     private String endpoint;
 
     @Bean
     public PdlHentPersonConsumer pdlHentPersonConsumer(STSConsumer stsConsumer) {
-        return new PdlHentPersonConsumerImpl(pdlClient(), endpoint, stsConsumer);
+        return new PdlHentPersonConsumerImpl(pdlHentPersonClient(), endpoint, stsConsumer);
     }
 
     // Trenger kun en ping mot PDL
@@ -33,5 +41,11 @@ public class PdlHentPersonRestConfig extends PdlConfig {
                 return feilet(metadata, e);
             }
         };
+    }
+
+    private Client pdlHentPersonClient() {
+        final var apiKey = getenv(PDLAPI_APIKEY);
+        return RestUtils.createClient()
+                .register((ClientRequestFilter) requestContext -> requestContext.getHeaders().putSingle(HEADER_NAV_APIKEY, apiKey));
     }
 }
