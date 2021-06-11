67226538d6aac380ec382483bf0c01881b7ec6e1
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-06-11 12:12:33 +0200

    legger endepunkt bak featuretoggle. hvis disabled, returnerer tom liste.

--- src/main/java/no/nav/sosialhjelp/soknad/web/rest/ressurser/eksponerte/DittNavMetadataRessurs.java
+++ src/main/java/no/nav/sosialhjelp/soknad/web/rest/ressurser/eksponerte/DittNavMetadataRessurs.java
@@ -1,5 +1,6 @@
 package no.nav.sosialhjelp.soknad.web.rest.ressurser.eksponerte;
 
+import no.finn.unleash.Unleash;
 import no.nav.security.token.support.core.api.ProtectedWithClaims;
 import no.nav.security.token.support.core.api.Unprotected;
 import no.nav.sosialhjelp.metrics.aspects.Timed;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import java.util.Collections;
 import java.util.List;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
@@ -28,16 +30,23 @@ import static org.slf4j.LoggerFactory.getLogger;
 public class DittNavMetadataRessurs {
 
     private static final Logger log = getLogger(DittNavMetadataRessurs.class);
+    private static final String DITTNAV_PABEGYNTE_ENDEPUNKT_ENABLED = "sosialhjelp.soknad.dittnav-pabegynte-endepunkt-enabled";
 
     private final DittNavMetadataService dittNavMetadataService;
+    private final Unleash unleash;
 
-    public DittNavMetadataRessurs(DittNavMetadataService dittNavMetadataService) {
+    public DittNavMetadataRessurs(DittNavMetadataService dittNavMetadataService, Unleash unleash) {
         this.dittNavMetadataService = dittNavMetadataService;
+        this.unleash = unleash;
     }
 
     @GET
     @Path("/pabegynte")
     public List<PabegyntSoknadDto> hentPabegynteSoknaderForBruker() {
+        if (unleash.isEnabled(DITTNAV_PABEGYNTE_ENDEPUNKT_ENABLED, false)) {
+            log.info("Endepunkt for å hente info om påbegynte søknader for dittNav er ikke enabled. Returnerer tom liste.");
+            return Collections.emptyList();
+        }
         var fnr = SubjectHandler.getUserId();
         return dittNavMetadataService.hentPabegynteSoknader(fnr);
     }
