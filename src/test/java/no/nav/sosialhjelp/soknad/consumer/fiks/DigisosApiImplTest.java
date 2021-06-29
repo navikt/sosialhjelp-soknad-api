1572f3337b73be9fe8f13734a9dd61ee8ad9caf1
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-06-29 10:29:07 +0200

    hamcrest -> assertj i "consumer"-pakken og "domain"-pakken

--- src/test/java/no/nav/sosialhjelp/soknad/consumer/fiks/DigisosApiImplTest.java
+++ src/test/java/no/nav/sosialhjelp/soknad/consumer/fiks/DigisosApiImplTest.java
@@ -7,10 +7,7 @@ import no.nav.sosialhjelp.api.fiks.KommuneInfo;
 import org.junit.Test;
 
 import static no.nav.sosialhjelp.soknad.consumer.fiks.DigisosApiImpl.getDigisosIdFromResponse;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class DigisosApiImplTest {
 
@@ -18,21 +15,21 @@ public class DigisosApiImplTest {
     public void getDigisosId_whenFinnesAllerede_shouldReturnIdTest() {
         String testresponse = "{\"timestamp\":1579253567738,\"status\":400,\"error\":\"Bad Request\",\"errorId\":\"afb2627d-1795-4ec1-a772-989d0a42a11a\",\"path\":\"/digisos/api/v1/soknader/3002/1100006QX\",\"originalPath\":null,\"message\":\"Soknad med tilhørende navEksternRefId 1100006QX finnes allerede i Fiks-Digisos med DigisosId a7b1c576-0851-455c-ad08-4f067be43629\",\"errorCode\":null,\"errorJson\":null}";
         String digisosId = getDigisosIdFromResponse(testresponse, "1100006QX");
-        assertEquals("a7b1c576-0851-455c-ad08-4f067be43629", digisosId);
+        assertThat(digisosId).isEqualTo("a7b1c576-0851-455c-ad08-4f067be43629");
     }
 
     @Test
     public void getDigisosId_whenOtherError_shouldNotReturnIdTest() {
         String testresponse = "{\"timestamp\":1579253567738,\"status\":400,\"error\":\"Bad Request\",\"errorId\":\"afb2627d-1795-4ec1-a772-989d0a42a11a\",\"path\":\"/digisos/api/v1/soknader/3002/1100006QX\",\"originalPath\":null,\"message\":\"Det er skjedd en uventet feil. Her er en random id a7b1c576-0851-455c-ad08-4f067be43629\",\"errorCode\":null,\"errorJson\":null}";
         String digisosId = getDigisosIdFromResponse(testresponse, "1100006QX");
-        assertNull(digisosId);
+        assertThat(digisosId).isNull();
     }
 
     @Test
     public void getDigisosId_whenErrorForAnotherBehandlingsId_shouldNotReturnIdTest() {
         String testresponse = "{\"timestamp\":1579253567738,\"status\":400,\"error\":\"Bad Request\",\"errorId\":\"afb2627d-1795-4ec1-a772-989d0a42a11a\",\"path\":\"/digisos/api/v1/soknader/3002/1100006QX\",\"originalPath\":null,\"message\":\"Det er skjedd en uventet feil. Her er en random id a7b1c576-0851-455c-ad08-4f067be43629\",\"errorCode\":null,\"errorJson\":null}";
         String digisosId = getDigisosIdFromResponse(testresponse, "110000001");
-        assertNull(digisosId);
+        assertThat(digisosId).isNull();
     }
 
     @Test
@@ -58,6 +55,6 @@ public class DigisosApiImplTest {
         ObjectMapper objectMapper = new ObjectMapper().registerModule(new KotlinModule());
         KommuneInfo kommuneInfo = objectMapper.readValue(response, KommuneInfo.class);
 
-        assertThat(kommuneInfo.getKommunenummer(), is("4699"));
+        assertThat(kommuneInfo.getKommunenummer()).isEqualTo("4699");
     }
 }
