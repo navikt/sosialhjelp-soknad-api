43846ed7e6563d01c97e8da31fb50ca384d9fb35
martintveter <41987225+martintveter@users.noreply.github.com>
2021-07-09 13:06:47 +0200

    Post endepunkt dittnav, og deler opp i 2 GET-endepunkter (#799)
    POST-endepunkt for å kunne la brukere markere en "påbegynte søknad" som "lest" på DittNav.
    Deler opp i 2 GET-endepunkter, slik at DittNav kan hente aktive og inaktive "påbegynte søknader" i separate kall.

--- src/main/java/no/nav/sosialhjelp/soknad/business/service/dittnav/DittNavMetadataService.java
+++ src/main/java/no/nav/sosialhjelp/soknad/business/service/dittnav/DittNavMetadataService.java
@@ -2,6 +2,7 @@ package no.nav.sosialhjelp.soknad.business.service.dittnav;
 
 import no.nav.sosialhjelp.soknad.business.db.repositories.soknadmetadata.SoknadMetadataRepository;
 import no.nav.sosialhjelp.soknad.web.rest.ressurser.eksponerte.dto.PabegyntSoknadDto;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Component;
 
 import java.time.ZoneId;
@@ -9,9 +10,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static no.nav.sosialhjelp.soknad.business.util.TimeUtils.toUtc;
+import static org.slf4j.LoggerFactory.getLogger;
 
 @Component
 public class DittNavMetadataService {
+
+    private static final Logger log = getLogger(DittNavMetadataService.class);
 
     private static final String SOKNAD_TITTEL = "Søknad om økonomisk sosialhjelp";
     private static final int SIKKERHETSNIVAA_3 = 3;
@@ -23,21 +27,50 @@ public class DittNavMetadataService {
         this.soknadMetadataRepository = soknadMetadataRepository;
     }
 
-    public List<PabegyntSoknadDto> hentPabegynteSoknader(String fnr) {
-        var pabegynteSoknader = soknadMetadataRepository.hentPabegynteSoknaderForBruker(fnr);
+    public List<PabegyntSoknadDto> hentAktivePabegynteSoknader(String fnr) {
+        return hentPabegynteSoknader(fnr, true);
+    }
+
+    public List<PabegyntSoknadDto> hentInaktivePabegynteSoknader(String fnr) {
+        return hentPabegynteSoknader(fnr, false);
+    }
+
+    private List<PabegyntSoknadDto> hentPabegynteSoknader(String fnr, boolean aktiv) {
+        var pabegynteSoknader = soknadMetadataRepository.hentPabegynteSoknaderForBruker(fnr, !aktiv);
 
         return pabegynteSoknader.stream()
                 .map(soknadMetadata -> new PabegyntSoknadDto(
-                        toUtc(soknadMetadata.sistEndretDato, ZoneId.systemDefault()),
+                        toUtc(soknadMetadata.opprettetDato, ZoneId.systemDefault()),
+                        eventId(soknadMetadata.behandlingsId, aktiv),
                         soknadMetadata.behandlingsId,
                         SOKNAD_TITTEL,
                         lenkeTilPabegyntSoknad(soknadMetadata.behandlingsId),
                         SIKKERHETSNIVAA_3, // todo finn ut hvilken
-                        toUtc(soknadMetadata.opprettetDato.plusDays(14), ZoneId.systemDefault())
+                        toUtc(soknadMetadata.sistEndretDato, ZoneId.systemDefault()),
+                        aktiv
                 ))
                 .collect(Collectors.toList());
     }
 
+    public boolean oppdaterLestDittNavForPabegyntSoknad(String behandlingsId, String fnr) {
+        var soknadMetadata = soknadMetadataRepository.hent(behandlingsId);
+        if (soknadMetadata == null) {
+            log.warn("Fant ingen soknadMetadata med behandlingsId={}", behandlingsId);
+            return false;
+        }
+        soknadMetadata.lestDittNav = true;
+        try {
+            soknadMetadataRepository.oppdaterLestDittNav(soknadMetadata, fnr);
+            return true;
+        } catch (Exception e) {
+            log.warn("Noe feilet ved oppdatering av lestDittNav for soknadMetadata med behandlingsId={}", behandlingsId, e);
+            return false;
+        }
+    }
+
+    private String eventId(String behandlingsId, boolean aktiv) {
+        return behandlingsId + "_" + (aktiv ? "aktiv" : "inaktiv");
+    }
 
     private String lenkeTilPabegyntSoknad(String behandlingsId) {
         return lagContextLenke() + "skjema/" + behandlingsId + "/0";
---
+++ src/test/java/no/nav/sosialhjelp/soknad/business/service/dittnav/DittNavMetadataServiceTest.java
@@ -0,0 +1,96 @@
+package no.nav.sosialhjelp.soknad.business.service.dittnav;
+
+import no.nav.sosialhjelp.soknad.business.db.repositories.soknadmetadata.SoknadMetadataRepository;
+import no.nav.sosialhjelp.soknad.business.domain.SoknadMetadata;
+import no.nav.sosialhjelp.soknad.domain.SoknadMetadataInnsendingStatus;
+import no.nav.sosialhjelp.soknad.domain.model.kravdialoginformasjon.SoknadType;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DittNavMetadataServiceTest {
+
+    @Mock
+    private SoknadMetadataRepository soknadMetadataRepository;
+
+    @InjectMocks
+    private DittNavMetadataService dittNavMetadataService;
+
+    @Test
+    public void skalHenteAktivePabegynteSoknaderForBruker() {
+        var soknadMetadata = createSoknadMetadata(false);
+
+        when(soknadMetadataRepository.hentPabegynteSoknaderForBruker("12345", false))
+                .thenReturn(Collections.singletonList(soknadMetadata));
+
+        var dtos = dittNavMetadataService.hentAktivePabegynteSoknader("12345");
+
+        assertThat(dtos).hasSize(1);
+        assertThat(dtos.get(0).getEventId()).isEqualTo(soknadMetadata.behandlingsId + "_aktiv");
+        assertThat(dtos.get(0).getGrupperingsId()).isEqualTo(soknadMetadata.behandlingsId);
+        assertThat(dtos.get(0).isAktiv()).isTrue();
+    }
+
+    @Test
+    public void skalHenteInaktivePabegynteSoknaderForBruker() {
+        var soknadMetadata = createSoknadMetadata(true);
+
+        when(soknadMetadataRepository.hentPabegynteSoknaderForBruker("12345", true))
+                .thenReturn(Collections.singletonList(soknadMetadata));
+
+        var dtos = dittNavMetadataService.hentInaktivePabegynteSoknader("12345");
+
+        assertThat(dtos).hasSize(1);
+        assertThat(dtos.get(0).getEventId()).isEqualTo(soknadMetadata.behandlingsId + "_inaktiv");
+        assertThat(dtos.get(0).getGrupperingsId()).isEqualTo(soknadMetadata.behandlingsId);
+        assertThat(dtos.get(0).isAktiv()).isFalse();
+    }
+
+    @Test
+    public void markerPabegyntSoknadSomLest_skalGiFalse_hvisRepositoryReturnererNull() {
+        when(soknadMetadataRepository.hent(anyString()))
+                .thenReturn(null);
+
+        var markert = dittNavMetadataService.oppdaterLestDittNavForPabegyntSoknad("behandlingsId", "12345");
+
+        assertThat(markert).isFalse();
+    }
+
+    @Test
+    public void markerPabegyntSoknadSomLest_skalGiFalse_hvisNoeFeiler() {
+        var soknadMetadata = createSoknadMetadata(false);
+
+        when(soknadMetadataRepository.hent(anyString()))
+                .thenReturn(soknadMetadata);
+        doThrow(new RuntimeException("Noe feilet")).when(soknadMetadataRepository).oppdaterLestDittNav(any(SoknadMetadata.class), anyString());
+
+        var markert = dittNavMetadataService.oppdaterLestDittNavForPabegyntSoknad("behandlingsId", "12345");
+
+        assertThat(markert).isFalse();
+    }
+
+    private SoknadMetadata createSoknadMetadata(boolean lestDittNav) {
+        var soknadMetadata = new SoknadMetadata();
+        soknadMetadata.fnr = "12345";
+        soknadMetadata.behandlingsId = "beh123";
+        soknadMetadata.status = SoknadMetadataInnsendingStatus.UNDER_ARBEID;
+        soknadMetadata.type = SoknadType.SEND_SOKNAD_KOMMUNAL;
+        soknadMetadata.opprettetDato = LocalDateTime.now().minusDays(10);
+        soknadMetadata.innsendtDato = LocalDateTime.now().minusDays(2);
+        soknadMetadata.sistEndretDato = LocalDateTime.now().minusDays(2);
+        soknadMetadata.lestDittNav = lestDittNav;
+        return soknadMetadata;
+    }
+}
