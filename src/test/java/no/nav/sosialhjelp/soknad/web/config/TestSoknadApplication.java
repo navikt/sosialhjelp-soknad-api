ba9ec8c09d830dc298385b7b1a9dffb10e46ce9e
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-07-19 15:25:23 +0200

    fjern TestSoknadApplication

--- src/test/java/no/nav/sosialhjelp/soknad/web/config/TestSoknadApplication.java
+++
@@ -1,9 +0,0 @@
-package no.nav.sosialhjelp.soknad.web.config;
-
-import no.nav.sosialhjelp.soknad.web.rest.SoknadApplication;
-
-public class TestSoknadApplication extends SoknadApplication {
-
-    public TestSoknadApplication() {
-    }
-}
