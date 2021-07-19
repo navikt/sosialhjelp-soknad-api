b95979e91bc887b4b2e613f51228d600b1e9cea2
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-07-19 15:32:51 +0200

    fix

--- src/main/java/no/nav/sosialhjelp/soknad/web/rest/ressurser/InternalRessurs.java
+++ src/main/java/no/nav/sosialhjelp/soknad/web/rest/ressurser/InternalRessurs.java
@@ -1,12 +1,10 @@
 package no.nav.sosialhjelp.soknad.web.rest.ressurser;
 
 import no.nav.security.token.support.core.api.Unprotected;
-import no.nav.sosialhjelp.soknad.web.selftest.Pingable;
 import no.nav.sosialhjelp.soknad.web.selftest.SelftestService;
 import no.nav.sosialhjelp.soknad.web.selftest.generators.SelftestHtmlGenerator;
 import no.nav.sosialhjelp.soknad.web.selftest.generators.SelftestJsonGenerator;
 import org.slf4j.Logger;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 
 import javax.ws.rs.GET;
@@ -18,11 +16,7 @@ import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.Collection;
-import java.util.List;
-import java.util.function.Function;
 
-import static java.util.stream.Collectors.toList;
 import static org.slf4j.LoggerFactory.getLogger;
 import static org.springframework.http.HttpHeaders.ACCEPT;
 
@@ -33,14 +27,9 @@ public class InternalRessurs {
 
     private static final Logger log = getLogger(InternalRessurs.class);
 
-    private final ApplicationContext appContext;
     private final SelftestService selftestService;
 
-    protected List<Pingable.Ping> result;
-    private volatile long lastResultTime;
-
-    public InternalRessurs(ApplicationContext appContext, SelftestService selftestService) {
-        this.appContext = appContext;
+    public InternalRessurs(SelftestService selftestService) {
         this.selftestService = selftestService;
     }
 
@@ -55,7 +44,6 @@ public class InternalRessurs {
     @Path(value = "/selftest")
     @Produces({MediaType.TEXT_HTML})
     public Response getSelftest(@HeaderParam(value = ACCEPT) String accept) throws IOException {
-        doPing();
         var selftest = selftestService.lagSelftest();
         var response = Response.ok();
         if ("application/json".equalsIgnoreCase(accept)) {
@@ -64,22 +52,6 @@ public class InternalRessurs {
             response.type(MediaType.TEXT_HTML).entity(SelftestHtmlGenerator.generate(selftest, getHost())).build();
         }
         return response.build();
-    }
-
-    protected void doPing() {
-        long requestTime = System.currentTimeMillis();
-        // Beskytter pingables mot mange samtidige/tette requester.
-        // Særlig viktig hvis det tar lang tid å utføre alle pingables
-        synchronized (this) {
-            if (requestTime > lastResultTime) {
-                result = getPingables().stream().map(PING).collect(toList());
-                lastResultTime = System.currentTimeMillis();
-            }
-        }
-    }
-
-    protected Collection<Pingable> getPingables() {
-        return appContext.getBeansOfType(Pingable.class).values();
     }
 
     protected String getHost() {
@@ -91,15 +63,5 @@ public class InternalRessurs {
         }
         return host;
     }
-
-    private static final Function<Pingable, Pingable.Ping> PING = pingable -> {
-        long startTime = System.currentTimeMillis();
-        var ping = pingable.ping();
-        ping.setResponstid(System.currentTimeMillis() - startTime);
-        if (!ping.erVellykket()) {
-            log.warn("Feil ved SelfTest av {}", ping.getMetadata().getEndepunkt(), ping.getFeil());
-        }
-        return ping;
-    };
 
 }
--- src/main/java/no/nav/sosialhjelp/soknad/web/selftest/SelftestService.java
+++ src/main/java/no/nav/sosialhjelp/soknad/web/selftest/SelftestService.java
@@ -4,20 +4,25 @@ import no.nav.sosialhjelp.soknad.web.selftest.domain.Selftest;
 import no.nav.sosialhjelp.soknad.web.selftest.domain.SelftestEndpoint;
 import no.nav.sosialhjelp.soknad.web.utils.MiljoUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.slf4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
-import javax.inject.Inject;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Collection;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.toList;
+import static org.slf4j.LoggerFactory.getLogger;
 
 @Component
 public class SelftestService {
+
+    private static final Logger log = getLogger(SelftestService.class);
 
     private static final Predicate<Pingable.Ping> KRITISK_FEIL = ping -> ping.harFeil() && ping.getMetadata().isKritisk();
     private static final Predicate<Pingable.Ping> HAR_FEIL = Pingable.Ping::harFeil;
@@ -26,13 +31,18 @@ public class SelftestService {
     public static final int STATUS_ERROR = 1;
     public static final int STATUS_WARNING = 2;
 
-    @Inject
-    private ApplicationContext appContext;
-
-    protected List<Pingable.Ping> result;
+    private List<Pingable.Ping> result;
     private volatile long lastResultTime;
 
+    private final ApplicationContext appContext;
+
+    public SelftestService(ApplicationContext appContext) {
+        this.appContext = appContext;
+    }
+
     public Selftest lagSelftest() {
+        doPing();
+
         return new Selftest()
                 .setApplication(MiljoUtils.getNaisAppName())
                 .setVersion(MiljoUtils.getNaisAppImage())
@@ -44,7 +54,33 @@ public class SelftestService {
                 );
     }
 
-    protected Integer getAggregertStatus() {
+    private void doPing() {
+        long requestTime = System.currentTimeMillis();
+        // Beskytter pingables mot mange samtidige/tette requester.
+        // Særlig viktig hvis det tar lang tid å utføre alle pingables
+        synchronized (this) {
+            if (requestTime > lastResultTime) {
+                result = getPingables().stream().map(PING).collect(toList());
+                lastResultTime = System.currentTimeMillis();
+            }
+        }
+    }
+
+    private Collection<Pingable> getPingables() {
+        return appContext.getBeansOfType(Pingable.class).values();
+    }
+
+    private static final Function<Pingable, Pingable.Ping> PING = pingable -> {
+        long startTime = System.currentTimeMillis();
+        var ping = pingable.ping();
+        ping.setResponstid(System.currentTimeMillis() - startTime);
+        if (!ping.erVellykket()) {
+            log.warn("Feil ved SelfTest av {}", ping.getMetadata().getEndepunkt(), ping.getFeil());
+        }
+        return ping;
+    };
+
+    private Integer getAggregertStatus() {
         boolean harKritiskFeil = result.stream().anyMatch(KRITISK_FEIL);
         boolean harFeil = result.stream().anyMatch(HAR_FEIL);
 
