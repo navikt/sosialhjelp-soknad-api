e0b9f746e46c5bc632e2747c8d7565d438758f8d
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-09-01 15:53:24 +0200

    prøv uten joda SoknadObjectMapperProvider

--- src/main/java/no/nav/sosialhjelp/soknad/web/rest/providers/SoknadObjectMapperProvider.java
+++ src/main/java/no/nav/sosialhjelp/soknad/web/rest/providers/SoknadObjectMapperProvider.java
@@ -2,7 +2,7 @@ package no.nav.sosialhjelp.soknad.web.rest.providers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.joda.JodaModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.Provider;
@@ -23,7 +23,8 @@ public class SoknadObjectMapperProvider implements ContextResolver<ObjectMapper>
 
     private static ObjectMapper createObjectMapper() {
         final ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JodaModule());
+//        mapper.registerModule(new JodaModule());
+        mapper.registerModule(new JavaTimeModule());
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         return mapper;
     }
