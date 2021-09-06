85ffd43cbe8d892d7d8256a8f6cfcfe456c5262d
martintveter <41987225+martintveter@users.noreply.github.com>
2021-09-06 14:27:01 +0200

    rydder litt i handlebars-relatert kode før ny oppsummeringsside (#849)

--- src/main/java/no/nav/sosialhjelp/soknad/business/pdf/HtmlGenerator.java
+++ src/main/java/no/nav/sosialhjelp/soknad/business/pdf/HtmlGenerator.java
@@ -1,15 +1,11 @@
 package no.nav.sosialhjelp.soknad.business.pdf;
 
-
 import no.nav.sbl.soknadsosialhjelp.soknad.JsonInternalSoknad;
 
 import java.io.IOException;
-
 
 public interface HtmlGenerator {
 
     String fyllHtmlMalMedInnhold(JsonInternalSoknad internalSoknad, boolean utvidetSoknad) throws IOException;
 
-    String fyllHtmlMalMedInnhold(JsonInternalSoknad internalSoknad, String file, boolean erEttersending, String eier) throws IOException;
 }
-
