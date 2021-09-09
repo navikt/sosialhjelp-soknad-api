f5ad082ee0a90c533192e049920fe75199a76a0c
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-09-09 12:40:39 +0200

    Systemdata som type

--- src/main/java/no/nav/sosialhjelp/soknad/web/rest/ressurser/oppsummering/dto/Type.java
+++ src/main/java/no/nav/sosialhjelp/soknad/web/rest/ressurser/oppsummering/dto/Type.java
@@ -2,8 +2,12 @@ package no.nav.sosialhjelp.soknad.web.rest.ressurser.oppsummering.dto;
 
 public enum Type {
 <<<<<<< HEAD
+<<<<<<< HEAD
     TEKST, CHECKBOX, SYSTEMDATA
 =======
     TEKST, CHECKBOX
 >>>>>>> 51bfd24483 (utkast endepunkt til ny oppsummering-side. wip)
+=======
+    TEKST, CHECKBOX, SYSTEMDATA
+>>>>>>> 1d0d331681 (Systemdata som type)
 }
