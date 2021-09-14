7e01dec6271df3960c2b18e9f199df3d1887246d
martintveter <41987225+martintveter@users.noreply.github.com>
2021-09-14 10:51:23 +0200

    Dialog endepunkt for sistInnsendteSoknad (#839)
    * tokenx inbound rules for sosialhjelp-dialog-api
    
    * endepunkt for dialog-api. Henter informasjon om brukers sist innsendte soknad.
    Informasjonen pr soknad som hentes ut er `digisosId`, `enhetsnr` og `innsendtDato`.
    
    Co-authored-by: Per-Christian Nielsen <per-christian.nielsen@nav.no>

---
+++ src/main/java/no/nav/sosialhjelp/soknad/web/rest/ressurser/dialog/dto/SistInnsendteSoknadDto.java
@@ -0,0 +1,33 @@
+package no.nav.sosialhjelp.soknad.web.rest.ressurser.dialog.dto;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class SistInnsendteSoknadDto {
+
+    private final String digisosId;
+    private final String enhetsnr;
+    private final LocalDateTime innsendtDato;
+
+    public SistInnsendteSoknadDto(
+            String digisosId,
+            String enhetsnr,
+            LocalDateTime innsendtDato
+    ) {
+        this.digisosId = digisosId;
+        this.enhetsnr = enhetsnr;
+        this.innsendtDato = innsendtDato;
+    }
+
+    public String getDigisosId() {
+        return digisosId;
+    }
+
+    public String getEnhetsnr() {
+        return enhetsnr;
+    }
+
+    public String getInnsendtDato() {
+        return innsendtDato.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+    }
+}
