7a75890f77b85e9a2cb6fede01c8c6f638d22fc1
martintveter <41987225+martintveter@users.noreply.github.com>
2021-09-21 16:26:00 +0200

    bruk SoknadJsonTyper fremfor strenger (#860)

--- src/main/java/no/nav/sosialhjelp/soknad/business/service/oppsummering/steg/UtgifterOgGjeldSteg.java
+++ src/main/java/no/nav/sosialhjelp/soknad/business/service/oppsummering/steg/UtgifterOgGjeldSteg.java
@@ -16,6 +16,19 @@ import java.util.stream.Collectors;
 
 import static java.lang.Boolean.TRUE;
 import static java.util.Collections.singletonList;
+import static no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.BEKREFTELSE_BARNEUTGIFTER;
+import static no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.BEKREFTELSE_BOUTGIFTER;
+import static no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.UTGIFTER_ANNET_BARN;
+import static no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.UTGIFTER_ANNET_BO;
+import static no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.UTGIFTER_BARNEHAGE;
+import static no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.UTGIFTER_BARN_FRITIDSAKTIVITETER;
+import static no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.UTGIFTER_BARN_TANNREGULERING;
+import static no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.UTGIFTER_BOLIGLAN_AVDRAG;
+import static no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.UTGIFTER_HUSLEIE;
+import static no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.UTGIFTER_KOMMUNAL_AVGIFT;
+import static no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.UTGIFTER_OPPVARMING;
+import static no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.UTGIFTER_SFO;
+import static no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.UTGIFTER_STROM;
 
 public class UtgifterOgGjeldSteg {
 
@@ -44,7 +57,7 @@ public class UtgifterOgGjeldSteg {
     }
 
     private List<Sporsmal> boutgifter(JsonOkonomi okonomi) {
-        var boutgiftBekreftelser = okonomi.getOpplysninger().getBekreftelse().stream().filter(b -> "boutgifter".equals(b.getType())).collect(Collectors.toList());
+        var boutgiftBekreftelser = okonomi.getOpplysninger().getBekreftelse().stream().filter(b -> BEKREFTELSE_BOUTGIFTER.equals(b.getType())).collect(Collectors.toList());
         var erBoutgifterUtfylt = !boutgiftBekreftelser.isEmpty() && boutgiftBekreftelser.get(0).getVerdi() != null;
         var harBoutgifter = erBoutgifterUtfylt && boutgiftBekreftelser.get(0).getVerdi().equals(TRUE);
 
@@ -63,12 +76,12 @@ public class UtgifterOgGjeldSteg {
             var oversiktUtgift = okonomi.getOversikt().getUtgift();
 
             var felter = new ArrayList<Felt>();
-            addOversiktUtgiftIfPresent(felter, oversiktUtgift, "husleie", "utgifter.boutgift.true.type.husleie");
-            addOpplysningUtgiftIfPresent(felter, utgifter, "strom", "utgifter.boutgift.true.type.strom");
-            addOpplysningUtgiftIfPresent(felter, utgifter, "kommunalAvgift", "utgifter.boutgift.true.type.kommunalAvgift");
-            addOpplysningUtgiftIfPresent(felter, utgifter, "oppvarming", "utgifter.boutgift.true.type.oppvarming");
-            addOversiktUtgiftIfPresent(felter, oversiktUtgift, "boliglanAvdrag", "utgifter.boutgift.true.type.boliglanAvdrag");
-            addOpplysningUtgiftIfPresent(felter, utgifter, "annenBoutgift", "utgifter.boutgift.true.type.annenBoutgift");
+            addOversiktUtgiftIfPresent(felter, oversiktUtgift, UTGIFTER_HUSLEIE, "utgifter.boutgift.true.type.husleie");
+            addOpplysningUtgiftIfPresent(felter, utgifter, UTGIFTER_STROM, "utgifter.boutgift.true.type.strom");
+            addOpplysningUtgiftIfPresent(felter, utgifter, UTGIFTER_KOMMUNAL_AVGIFT, "utgifter.boutgift.true.type.kommunalAvgift");
+            addOpplysningUtgiftIfPresent(felter, utgifter, UTGIFTER_OPPVARMING, "utgifter.boutgift.true.type.oppvarming");
+            addOversiktUtgiftIfPresent(felter, oversiktUtgift, UTGIFTER_BOLIGLAN_AVDRAG, "utgifter.boutgift.true.type.boliglanAvdrag");
+            addOpplysningUtgiftIfPresent(felter, utgifter, UTGIFTER_ANNET_BO, "utgifter.boutgift.true.type.annenBoutgift");
 
             sporsmalList.add(
                     new Sporsmal.Builder()
@@ -83,7 +96,7 @@ public class UtgifterOgGjeldSteg {
     }
 
     private List<Sporsmal> barneutgifter(JsonOkonomi okonomi) {
-        var barneutgiftBekreftelser = okonomi.getOpplysninger().getBekreftelse().stream().filter(b -> "barneutgifter".equals(b.getType())).collect(Collectors.toList());
+        var barneutgiftBekreftelser = okonomi.getOpplysninger().getBekreftelse().stream().filter(b -> BEKREFTELSE_BARNEUTGIFTER.equals(b.getType())).collect(Collectors.toList());
         var erBarneutgifterUtfylt = !barneutgiftBekreftelser.isEmpty() && barneutgiftBekreftelser.get(0).getVerdi() != null;
         var harBarneutgifter = erBarneutgifterUtfylt && barneutgiftBekreftelser.get(0).getVerdi().equals(TRUE);
 
@@ -101,11 +114,11 @@ public class UtgifterOgGjeldSteg {
             var oversiktUtgifter = okonomi.getOversikt().getUtgift();
 
             var felter = new ArrayList<Felt>();
-            addOpplysningUtgiftIfPresent(felter, utgifter, "barnFritidsaktiviteter", "utgifter.barn.true.utgifter.barnFritidsaktiviteter");
-            addOversiktUtgiftIfPresent(felter, oversiktUtgifter, "barnehage", "utgifter.barn.true.utgifter.barnehage");
-            addOversiktUtgiftIfPresent(felter, oversiktUtgifter, "sfo", "utgifter.barn.true.utgifter.sfo");
-            addOpplysningUtgiftIfPresent(felter, utgifter, "barnTannregulering", "utgifter.barn.true.utgifter.barnTannregulering");
-            addOpplysningUtgiftIfPresent(felter, utgifter, "annenBarneutgift", "utgifter.barn.true.utgifter.annenBarneutgift");
+            addOpplysningUtgiftIfPresent(felter, utgifter, UTGIFTER_BARN_FRITIDSAKTIVITETER, "utgifter.barn.true.utgifter.barnFritidsaktiviteter");
+            addOversiktUtgiftIfPresent(felter, oversiktUtgifter, UTGIFTER_BARNEHAGE, "utgifter.barn.true.utgifter.barnehage");
+            addOversiktUtgiftIfPresent(felter, oversiktUtgifter, UTGIFTER_SFO, "utgifter.barn.true.utgifter.sfo");
+            addOpplysningUtgiftIfPresent(felter, utgifter, UTGIFTER_BARN_TANNREGULERING, "utgifter.barn.true.utgifter.barnTannregulering");
+            addOpplysningUtgiftIfPresent(felter, utgifter, UTGIFTER_ANNET_BARN, "utgifter.barn.true.utgifter.annenBarneutgift");
 
             sporsmalList.add(
                     new Sporsmal.Builder()
