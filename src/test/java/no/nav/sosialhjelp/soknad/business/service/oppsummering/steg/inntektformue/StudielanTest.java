7f664121e104f42b4ff6208e7c5f8503d93fc286
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-09-21 10:00:57 +0200

    studielan tester

---
+++ src/test/java/no/nav/sosialhjelp/soknad/business/service/oppsummering/steg/inntektformue/StudielanTest.java
@@ -0,0 +1,48 @@
+package no.nav.sosialhjelp.soknad.business.service.oppsummering.steg.inntektformue;
+
+import no.nav.sbl.soknadsosialhjelp.soknad.okonomi.JsonOkonomiopplysninger;
+import no.nav.sbl.soknadsosialhjelp.soknad.okonomi.opplysning.JsonOkonomibekreftelse;
+import no.nav.sosialhjelp.soknad.web.rest.ressurser.oppsummering.dto.Type;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+
+import static no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.STUDIELAN;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StudielanTest {
+
+    private final Studielan studielan = new Studielan();
+
+    @Test
+    void harIkkeUtfyltSporsmal() {
+        var opplysninger = new JsonOkonomiopplysninger()
+                .withBekreftelse(Collections.emptyList());
+
+        var avsnitt = studielan.getAvsnitt(opplysninger);
+
+        assertThat(avsnitt.getSporsmal()).hasSize(1);
+        assertThat(avsnitt.getSporsmal().get(0).getTittel()).isEqualTo("inntekt.studielan.sporsmal");
+        assertThat(avsnitt.getSporsmal().get(0).getErUtfylt()).isFalse();
+        assertThat(avsnitt.getSporsmal().get(0).getFelt()).isNull();
+    }
+
+    @Test
+    void harSvartJa() {
+        var opplysninger = new JsonOkonomiopplysninger()
+                .withBekreftelse(Collections.singletonList(
+                        new JsonOkonomibekreftelse()
+                                .withType(STUDIELAN)
+                                .withVerdi(true)
+                ));
+
+        var avsnitt = studielan.getAvsnitt(opplysninger);
+
+        assertThat(avsnitt.getSporsmal()).hasSize(1);
+        assertThat(avsnitt.getSporsmal().get(0).getTittel()).isEqualTo("inntekt.studielan.sporsmal");
+        assertThat(avsnitt.getSporsmal().get(0).getErUtfylt()).isTrue();
+        assertThat(avsnitt.getSporsmal().get(0).getFelt()).hasSize(1);
+        assertThat(avsnitt.getSporsmal().get(0).getFelt().get(0).getType()).isEqualTo(Type.CHECKBOX);
+        assertThat(avsnitt.getSporsmal().get(0).getFelt().get(0).getSvar()).isEqualTo("inntekt.studielan.true");
+    }
+}
