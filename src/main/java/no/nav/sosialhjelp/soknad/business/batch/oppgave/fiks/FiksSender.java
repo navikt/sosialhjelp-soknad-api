455407edc3105769121731d4a547a0bff8a062da
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-10-11 10:15:26 +0200

    fjern todo

--- src/main/java/no/nav/sosialhjelp/soknad/business/batch/oppgave/fiks/FiksSender.java
+++ src/main/java/no/nav/sosialhjelp/soknad/business/batch/oppgave/fiks/FiksSender.java
@@ -257,13 +257,6 @@ public class FiksSender {
             throw new RuntimeException("Kan ikke sende ettersendelse fordi vedlegg mangler");
         }
 
-        // TODO: 2019-11-25 pcn: Denne er her midlertidig for å fange opp søknader som er started før bostøtte ble rullet ut...
-        if (!soknadUnderArbeid.erEttersendelse()) {
-            if (internalSoknad.getSoknad().getData().getOkonomi().getOpplysninger().getBostotte() == null) {
-                internalSoknad.getSoknad().getDriftsinformasjon().setStotteFraHusbankenFeilet(true);
-            }
-        }
-
         List<no.ks.fiks.svarut.klient.model.Dokument> fiksDokumenter = new ArrayList<>();
         int antallVedleggForsendelse;
         if (soknadUnderArbeid.erEttersendelse()) {
