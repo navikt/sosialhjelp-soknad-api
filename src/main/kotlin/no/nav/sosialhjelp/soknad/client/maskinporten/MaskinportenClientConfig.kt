976901d976b807a82ed0e9d5e4258450c7364344
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-10-15 12:32:03 +0200

    MaskinportenClientConfig henter fra wellknown ved oppstart (så lenge profil er ulik `test`)

--- src/main/kotlin/no/nav/sosialhjelp/soknad/client/maskinporten/MaskinportenClientConfig.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/client/maskinporten/MaskinportenClientConfig.kt
@@ -1,20 +1,59 @@
 package no.nav.sosialhjelp.soknad.client.maskinporten
 
 import no.nav.sosialhjelp.metrics.MetricsFactory
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToMono
 
 @Configuration
 open class MaskinportenClientConfig(
-    private val proxiedWebClient: WebClient
+    private val proxiedWebClient: WebClient,
+    @Value("\${maskinporten_clientid}") private val clientId: String,
+    @Value("\${maskinporten_scopes}") private val scopes: String,
+    @Value("\${maskinporten_well_known_url}") private val wellKnownUrl: String,
+    @Value("\${maskinporten_client_jwk}") private val clientJwk: String,
 ) {
 
-    // todo implement
-
     @Bean
+    @Profile("!test")
     open fun maskinportenClient(): MaskinportenClient {
-        val maskinportenClient = MaskinportenClientImpl(proxiedWebClient)
+        val maskinportenClient = MaskinportenClientImpl(proxiedWebClient, maskinportenConfig, wellknown)
         return MetricsFactory.createTimerProxy("MaskinportenClient", maskinportenClient, MaskinportenClient::class.java)
     }
+
+    @Bean
+    @Profile("test")
+    open fun maskinportenClientTest(): MaskinportenClient {
+        return MaskinportenClientImpl(proxiedWebClient, maskinportenConfig, WellKnown("issuer", "token_url"))
+    }
+
+    private val wellknown: WellKnown
+        get() = proxiedWebClient.get()
+            .uri(wellKnownUrl)
+            .retrieve()
+            .bodyToMono<WellKnown>()
+            .doOnSuccess { log.info("Hentet WellKnown for Maskinporten") }
+            .doOnError { log.warn("Feil ved henting av WellKnown for Maskinporten", it) }
+            .block()!!
+
+    private val maskinportenConfig: MaskinportenConfig
+        get() = MaskinportenConfig(
+            clientId = clientId,
+            jwkPrivate = clientJwk,
+            scope = scopes,
+            wellKnownUrl = wellKnownUrl
+        )
+
+    companion object {
+        private val log = LoggerFactory.getLogger(MaskinportenClientConfig::class.java)
+    }
 }
+
+data class WellKnown(
+    val issuer: String,
+    val token_endpoint: String
+)
