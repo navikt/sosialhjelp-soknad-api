c81ba42256136a66b20f7d23df13a972b10fa5df
martintveter <41987225+martintveter@users.noreply.github.com>
2021-10-29 08:27:19 +0200

    virusScan resttemplate -> webclient (#918)

--- src/main/java/no/nav/sosialhjelp/soknad/consumer/virusscan/VirusScanConfig.java
+++ src/main/java/no/nav/sosialhjelp/soknad/consumer/virusscan/VirusScanConfig.java
@@ -3,7 +3,7 @@ package no.nav.sosialhjelp.soknad.consumer.virusscan;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.net.URI;
 
@@ -13,7 +13,11 @@ public class VirusScanConfig {
     private static final URI DEFAULT_CLAM_URI = URI.create("http://clamav.nais.svc.nais.local/scan");
 
     @Bean
-    public VirusScanner virusScanner() {
-        return new ClamAvVirusScanner(DEFAULT_CLAM_URI, new RestTemplate());
+    public VirusScanner virusScanner(WebClient webClient) {
+        final var clamAvWebClient = webClient.mutate()
+                .baseUrl(DEFAULT_CLAM_URI.toString())
+                .build();
+
+        return new ClamAvVirusScanner(DEFAULT_CLAM_URI, clamAvWebClient);
     }
 }
