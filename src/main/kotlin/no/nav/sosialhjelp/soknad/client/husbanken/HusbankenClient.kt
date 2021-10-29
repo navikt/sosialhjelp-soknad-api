20c1bb8c9af731a1b6d2490dc21dcc1806b21200
martintveter <41987225+martintveter@users.noreply.github.com>
2021-10-29 11:39:33 +0200

    Fjern husbanken resttemplate (#919)
    * fjern husbanken consumer java+resttemplate
    
    * skriver om BostotteImpl-tester til HusbankenClientImplTest.kt
    
    * fjern ubrukte ping urler

--- src/main/kotlin/no/nav/sosialhjelp/soknad/client/husbanken/HusbankenClient.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/client/husbanken/HusbankenClient.kt
@@ -19,20 +19,24 @@ class HusbankenClientImpl(
 ) : HusbankenClient {
 
     override fun hentBostotte(token: String, fra: LocalDate, til: LocalDate): Optional<BostotteDto> {
-        return webClient.get()
-            .uri { it.queryParam("fra", fra).queryParam("til", til).build() }
-            .headers { it.add(HttpHeaders.AUTHORIZATION, token) }
-            .retrieve()
-            .bodyToMono<BostotteDto>()
-            .doOnSuccess { log.info("Hentet bostøtte informasjon fra Husbanken!") }
-            .doOnError(WebClientResponseException::class.java) { e ->
-                when {
-                    e.statusCode.is4xxClientError -> log.error("Problemer med å koble opp mot Husbanken!", e)
-                    e.statusCode.is5xxServerError -> log.error("Problemer med å hente bostøtte fra Husbanken! Ekstern error: {}", e.message, e)
-                    else -> log.error("Problemer med å hente bostøtte informasjon fra Husbanken!", e)
+        return try {
+            webClient.get()
+                .uri { it.queryParam("fra", fra).queryParam("til", til).build() }
+                .headers { it.add(HttpHeaders.AUTHORIZATION, token) }
+                .retrieve()
+                .bodyToMono<BostotteDto>()
+                .doOnSuccess { log.info("Hentet bostøtte informasjon fra Husbanken!") }
+                .doOnError(WebClientResponseException::class.java) { e ->
+                    when {
+                        e.statusCode.is4xxClientError -> log.error("Problemer med å koble opp mot Husbanken!", e)
+                        e.statusCode.is5xxServerError -> log.error("Problemer med å hente bostøtte fra Husbanken! Ekstern error: {}", e.message, e)
+                        else -> log.error("Problemer med å hente bostøtte informasjon fra Husbanken!", e)
+                    }
                 }
-            }
-            .blockOptional()
+                .blockOptional()
+        } catch (e: Exception) {
+            return Optional.empty()
+        }
     }
 
     override fun ping() {
