757199f5faeb2e2bde20b023885249b153648990
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-10-29 10:52:47 +0200

    leaderelection resttemplate -> webclient

--- src/main/java/no/nav/sosialhjelp/soknad/consumer/leaderelection/LeaderElectionImpl.java
+++ src/main/java/no/nav/sosialhjelp/soknad/consumer/leaderelection/LeaderElectionImpl.java
@@ -3,10 +3,10 @@ package no.nav.sosialhjelp.soknad.consumer.leaderelection;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.net.URI;
 import java.net.UnknownHostException;
 import java.time.LocalDateTime;
 
@@ -20,22 +20,15 @@ public class LeaderElectionImpl implements LeaderElection {
     private static final String ELECTOR_PATH = "ELECTOR_PATH";
     private static final Logger log = getLogger(LeaderElectionImpl.class);
 
-    private final RestTemplate restTemplate;
+    private final WebClient webClient;
     private final ObjectMapper mapper = new ObjectMapper();
     private final String hostname;
     private String leader;
     private LocalDateTime lastCallTime = LocalDateTime.MIN;
 
-    public LeaderElectionImpl() throws UnknownHostException {
-        this.restTemplate = new RestTemplate(getClientHttpRequestFactory());
+    public LeaderElectionImpl(WebClient webClient) throws UnknownHostException {
+        this.webClient = webClient;
         this.hostname = getLocalHost().getHostName();
-    }
-
-    private HttpComponentsClientHttpRequestFactory getClientHttpRequestFactory() {
-        var clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
-        clientHttpRequestFactory.setConnectTimeout(5_000);
-        clientHttpRequestFactory.setReadTimeout(20_000);
-        return clientHttpRequestFactory;
     }
 
     @Override
@@ -47,7 +40,12 @@ public class LeaderElectionImpl implements LeaderElection {
         }
         if (leader == null || lastCallTime.isBefore(LocalDateTime.now().minusMinutes(2))){
             try {
-                String response = restTemplate.getForObject("http://" + electorPath, String.class);
+                final var response = webClient.get()
+                        .uri(URI.create("http://" + electorPath).toString())
+                        .retrieve()
+                        .bodyToMono(String.class)
+                        .block();
+
                 leader = mapper.readTree(response).get("name").asText();
                 lastCallTime = LocalDateTime.now();
             } catch (Exception e) {
