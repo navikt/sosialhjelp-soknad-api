650bfa34102abc0ecde628ee49205e290fbc5c7b
martintveter <41987225+martintveter@users.noreply.github.com>
2021-10-31 12:06:37 +0100

    leaderelection resttemplate -> webclient (#921)
    * leaderelection resttemplate -> webclient
    
    * add nonProxiedWebClientBuilder

--- src/main/java/no/nav/sosialhjelp/soknad/consumer/leaderelection/LeaderElectionImpl.java
+++ src/main/java/no/nav/sosialhjelp/soknad/consumer/leaderelection/LeaderElectionImpl.java
@@ -3,9 +3,8 @@ package no.nav.sosialhjelp.soknad.consumer.leaderelection;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.net.UnknownHostException;
 import java.time.LocalDateTime;
@@ -20,34 +19,32 @@ public class LeaderElectionImpl implements LeaderElection {
     private static final String ELECTOR_PATH = "ELECTOR_PATH";
     private static final Logger log = getLogger(LeaderElectionImpl.class);
 
-    private final RestTemplate restTemplate;
-    private final ObjectMapper mapper = new ObjectMapper();
+    private final WebClient webClient;
+    private final String electorPath;
     private final String hostname;
     private String leader;
+    private final ObjectMapper mapper = new ObjectMapper();
     private LocalDateTime lastCallTime = LocalDateTime.MIN;
 
-    public LeaderElectionImpl() throws UnknownHostException {
-        this.restTemplate = new RestTemplate(getClientHttpRequestFactory());
+    public LeaderElectionImpl(WebClient.Builder nonProxiedWebClientBuilder) throws UnknownHostException {
+        this.electorPath = System.getenv(ELECTOR_PATH);
+        this.webClient = nonProxiedWebClientBuilder.baseUrl("http://" + electorPath).build();
         this.hostname = getLocalHost().getHostName();
-    }
-
-    private HttpComponentsClientHttpRequestFactory getClientHttpRequestFactory() {
-        var clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
-        clientHttpRequestFactory.setConnectTimeout(5_000);
-        clientHttpRequestFactory.setReadTimeout(20_000);
-        return clientHttpRequestFactory;
     }
 
     @Override
     public boolean isLeader() {
-        String electorPath = System.getenv(ELECTOR_PATH);
         if (electorPath == null) {
             log.warn("LeaderElection - manglende systemvariabel={}.", ELECTOR_PATH);
             return true;
         }
         if (leader == null || lastCallTime.isBefore(LocalDateTime.now().minusMinutes(2))){
             try {
-                String response = restTemplate.getForObject("http://" + electorPath, String.class);
+                final var response = webClient.get()
+                        .retrieve()
+                        .bodyToMono(String.class)
+                        .block();
+
                 leader = mapper.readTree(response).get("name").asText();
                 lastCallTime = LocalDateTime.now();
             } catch (Exception e) {
