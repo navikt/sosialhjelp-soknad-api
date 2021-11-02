616c83874a75cbbf74adc7604d3b243073300e00
martintveter <41987225+martintveter@users.noreply.github.com>
2021-11-02 16:08:16 +0100

    register kotlinmodule (#925)

--- src/main/kotlin/no/nav/sosialhjelp/soknad/client/dkif/dto/DigitalKontaktinfoBolk.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/client/dkif/dto/DigitalKontaktinfoBolk.kt
@@ -1,18 +1,14 @@
 package no.nav.sosialhjelp.soknad.client.dkif.dto
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-
 data class DigitalKontaktinfoBolk(
     val kontaktinfo: Map<String, DigitalKontaktinfo>?,
     val feil: Map<String, Feil>?
 )
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 data class DigitalKontaktinfo(
     val mobiltelefonnummer: String?
 )
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 data class Feil(
     val melding: String?
 )
