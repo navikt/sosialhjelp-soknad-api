b858a5debcb6fed7ad4d7e2dd9af0efc5376f70f
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-11-04 16:07:56 +0100

    jsonAlias

--- src/main/kotlin/no/nav/sosialhjelp/soknad/client/virusscan/dto/ScanResult.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/client/virusscan/dto/ScanResult.kt
@@ -1,7 +1,11 @@
 package no.nav.sosialhjelp.soknad.client.virusscan.dto
 
+import com.fasterxml.jackson.annotation.JsonAlias
+
 data class ScanResult(
+    @JsonAlias("Filename")
     val filename: String?,
+    @JsonAlias("Result")
     val result: Result
 )
 
