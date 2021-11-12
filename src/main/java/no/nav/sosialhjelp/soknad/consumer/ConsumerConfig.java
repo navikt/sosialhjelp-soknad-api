572ef257ecd122e99c78fc2aeca32e0bbfcc405f
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-11-12 14:35:23 +0100

    featuretoggle/unleash til kotlin

--- src/main/java/no/nav/sosialhjelp/soknad/consumer/ConsumerConfig.java
+++ src/main/java/no/nav/sosialhjelp/soknad/consumer/ConsumerConfig.java
@@ -5,6 +5,7 @@ import no.nav.sosialhjelp.soknad.client.config.MockProxiedWebClientConfig;
 import no.nav.sosialhjelp.soknad.client.config.NonProxiedWebClientConfig;
 import no.nav.sosialhjelp.soknad.client.config.ProxiedWebClientConfig;
 import no.nav.sosialhjelp.soknad.client.dkif.DkifConfig;
+import no.nav.sosialhjelp.soknad.client.featuretoggle.FeatureToggleConfig;
 import no.nav.sosialhjelp.soknad.client.fiks.KommuneInfoClientConfig;
 import no.nav.sosialhjelp.soknad.client.husbanken.HusbankenClientConfig;
 import no.nav.sosialhjelp.soknad.client.idporten.IdPortenClientConfig;
@@ -29,7 +30,6 @@ import no.nav.sosialhjelp.soknad.consumer.restconfig.PdlGeografiskTilknytningRes
 import no.nav.sosialhjelp.soknad.consumer.restconfig.PdlHentPersonRestConfig;
 import no.nav.sosialhjelp.soknad.consumer.restconfig.STSTokenRestConfig;
 import no.nav.sosialhjelp.soknad.consumer.restconfig.SvarUtRestConfig;
-import no.nav.sosialhjelp.soknad.consumer.restconfig.UnleashRestConfig;
 import no.nav.sosialhjelp.soknad.consumer.skatt.SkattbarInntektService;
 import no.nav.sosialhjelp.soknad.consumer.svarut.SvarUtService;
 import no.nav.sosialhjelp.soknad.navenhet.NavEnhetConfig;
@@ -58,6 +58,7 @@ import org.springframework.context.annotation.Import;
         DkifConfig.class,
         NavEnhetConfig.class,
         KodeverkConfig.class,
+        FeatureToggleConfig.class,
         RedisConfig.class,
         NoRedisConfig.class,
         PersonService.class,
@@ -85,7 +86,6 @@ public class ConsumerConfig {
             PdlGeografiskTilknytningRestConfig.class,
             DigisosApiRestConfig.class,
             STSTokenRestConfig.class,
-            UnleashRestConfig.class,
             OppslagRestConfig.class,
             SvarUtRestConfig.class
     })
