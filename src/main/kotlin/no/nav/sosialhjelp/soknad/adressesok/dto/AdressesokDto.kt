37f89a08c731b490bf21ddedfee45c86dac36a00
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-11-17 17:03:44 +0100

    no mapping

--- src/main/kotlin/no/nav/sosialhjelp/soknad/adressesok/dto/AdressesokDto.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/adressesok/dto/AdressesokDto.kt
@@ -1,10 +1,5 @@
 package no.nav.sosialhjelp.soknad.adressesok.dto
 
-import no.nav.sosialhjelp.soknad.adressesok.domain.AdresseForslag
-import no.nav.sosialhjelp.soknad.adressesok.domain.AdresseForslagType
-import no.nav.sosialhjelp.soknad.consumer.pdl.adressesok.AdresseHelper
-import no.nav.sosialhjelp.soknad.domain.model.util.KommuneTilNavEnhetMapper
-
 data class AdressesokDataDto(
     val sokAdresse: AdressesokResultDto
 )
@@ -31,25 +26,4 @@ data class VegadresseDto(
     val postnummer: String?,
     val poststed: String?,
     val bydelsnummer: String?
-) {
-    fun toAdresseForslag(): AdresseForslag {
-        return AdresseForslag(
-            adresse = adressenavn,
-            husnummer = husnummer.toString(),
-            husbokstav = husbokstav,
-            kommunenummer = kommunenummer,
-            kommunenavn = kommunenavnFormattert,
-            postnummer = postnummer,
-            poststed = poststed,
-            geografiskTilknytning = bydelsnummerOrKommunenummer,
-            type = AdresseForslagType.GATEADRESSE
-        )
-    }
-
-    private val kommunenavnFormattert: String get() = KommuneTilNavEnhetMapper.IKS_KOMMUNER.getOrDefault(
-        kommunenummer,
-        AdresseHelper.formatterKommunenavn(kommunenavn)
-    )
-
-    private val bydelsnummerOrKommunenummer: String? get() = bydelsnummer ?: kommunenummer
-}
+)
