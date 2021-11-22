e966fc2c8f0e289e34e44da2b70645ef808ad7d5
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-11-22 12:50:44 +0100

    metrikkene knoter seg litt opp med felles config-fil. Istedet kun felles ping

---
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/navenhet/gt/GeografiskTilknytningConfig.kt
@@ -0,0 +1,26 @@
+package no.nav.sosialhjelp.soknad.navenhet.gt
+
+import no.nav.sosialhjelp.soknad.client.pdl.PdlConfig
+import no.nav.sosialhjelp.soknad.client.redis.RedisService
+import no.nav.sosialhjelp.soknad.client.sts.StsClient
+import no.nav.sosialhjelp.soknad.consumer.common.rest.RestUtils
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import javax.ws.rs.client.Client
+
+@Configuration
+open class GeografiskTilknytningConfig(
+    @Value("\${pdl_api_url}") private val baseurl: String,
+    private val stsClient: StsClient,
+    private val redisService: RedisService
+) : PdlConfig(baseurl) {
+
+    @Bean
+    open fun geografiskTilknytningClient(): GeografiskTilknytningClient {
+        return GeografiskTilknytningClient(client, baseurl, stsClient, redisService)
+    }
+
+    private val client: Client
+        get() = RestUtils.createClient().register(pdlApiKeyRequestFilter)
+}
