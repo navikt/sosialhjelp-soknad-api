f21cdc7dd2f51d83b8e574b71813b816487356ba
martintveter <41987225+martintveter@users.noreply.github.com>
2021-12-02 09:49:17 +0100

    verdiressurs -> kotlin (#998)

---
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/inntekt/verdi/VerdiRessurs.kt
@@ -0,0 +1,129 @@
+package no.nav.sosialhjelp.soknad.inntekt.verdi
+
+import no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.BEKREFTELSE_VERDI
+import no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.VERDI_ANNET
+import no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.VERDI_BOLIG
+import no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.VERDI_CAMPINGVOGN
+import no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.VERDI_FRITIDSEIENDOM
+import no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.VERDI_KJORETOY
+import no.nav.sbl.soknadsosialhjelp.soknad.common.JsonKildeBruker
+import no.nav.sbl.soknadsosialhjelp.soknad.okonomi.JsonOkonomiopplysninger
+import no.nav.sbl.soknadsosialhjelp.soknad.okonomi.JsonOkonomioversikt
+import no.nav.sbl.soknadsosialhjelp.soknad.okonomi.opplysning.JsonOkonomibeskrivelserAvAnnet
+import no.nav.security.token.support.core.api.ProtectedWithClaims
+import no.nav.sosialhjelp.metrics.aspects.Timed
+import no.nav.sosialhjelp.soknad.business.db.repositories.soknadunderarbeid.SoknadUnderArbeidRepository
+import no.nav.sosialhjelp.soknad.business.mappers.OkonomiMapper.addFormueIfCheckedElseDeleteInOversikt
+import no.nav.sosialhjelp.soknad.business.mappers.OkonomiMapper.setBekreftelse
+import no.nav.sosialhjelp.soknad.business.mappers.TitleKeyMapper.soknadTypeToTitleKey
+import no.nav.sosialhjelp.soknad.business.service.TextService
+import no.nav.sosialhjelp.soknad.domain.model.oidc.SubjectHandler
+import no.nav.sosialhjelp.soknad.web.sikkerhet.Tilgangskontroll
+import no.nav.sosialhjelp.soknad.web.utils.Constants
+import org.springframework.stereotype.Controller
+import javax.ws.rs.GET
+import javax.ws.rs.PUT
+import javax.ws.rs.Path
+import javax.ws.rs.PathParam
+import javax.ws.rs.Produces
+import javax.ws.rs.core.MediaType
+
+@Controller
+@ProtectedWithClaims(issuer = Constants.SELVBETJENING, claimMap = [Constants.CLAIM_ACR_LEVEL_4])
+@Path("/soknader/{behandlingsId}/inntekt/verdier")
+@Timed
+@Produces(MediaType.APPLICATION_JSON)
+open class VerdiRessurs(
+    private val tilgangskontroll: Tilgangskontroll,
+    private val soknadUnderArbeidRepository: SoknadUnderArbeidRepository,
+    private val textService: TextService
+) {
+    @GET
+    open fun hentVerdier(@PathParam("behandlingsId") behandlingsId: String): VerdierFrontend {
+        tilgangskontroll.verifiserAtBrukerHarTilgang()
+        val eier = SubjectHandler.getUserId()
+        val soknad = soknadUnderArbeidRepository.hentSoknad(behandlingsId, eier).jsonInternalSoknad
+        val okonomi = soknad.soknad.data.okonomi
+
+        if (okonomi.opplysninger.bekreftelse == null) {
+            return VerdierFrontend()
+        }
+
+        return VerdierFrontend(
+            bekreftelse = getBekreftelse(okonomi.opplysninger),
+            bolig = hasVerdiType(okonomi.oversikt, VERDI_BOLIG),
+            campingvogn = hasVerdiType(okonomi.oversikt, VERDI_CAMPINGVOGN),
+            kjoretoy = hasVerdiType(okonomi.oversikt, VERDI_KJORETOY),
+            fritidseiendom = hasVerdiType(okonomi.oversikt, VERDI_FRITIDSEIENDOM),
+            annet = hasVerdiType(okonomi.oversikt, VERDI_ANNET),
+            beskrivelseAvAnnet = okonomi.opplysninger.beskrivelseAvAnnet?.verdi
+        )
+    }
+
+    @PUT
+    open fun updateVerdier(@PathParam("behandlingsId") behandlingsId: String, verdierFrontend: VerdierFrontend) {
+        tilgangskontroll.verifiserAtBrukerKanEndreSoknad(behandlingsId)
+        val eier = SubjectHandler.getUserId()
+        val soknad = soknadUnderArbeidRepository.hentSoknad(behandlingsId, eier)
+        val okonomi = soknad.jsonInternalSoknad.soknad.data.okonomi
+        if (okonomi.opplysninger.bekreftelse == null) {
+            okonomi.opplysninger.bekreftelse = ArrayList()
+        }
+        setBekreftelse(
+            okonomi.opplysninger,
+            BEKREFTELSE_VERDI,
+            verdierFrontend.bekreftelse,
+            textService.getJsonOkonomiTittel("inntekt.eierandeler")
+        )
+        setVerdier(okonomi.oversikt, verdierFrontend)
+        setBeskrivelseAvAnnet(okonomi.opplysninger, verdierFrontend)
+        soknadUnderArbeidRepository.oppdaterSoknadsdata(soknad, eier)
+    }
+
+    private fun setVerdier(oversikt: JsonOkonomioversikt, verdierFrontend: VerdierFrontend) {
+        val verdier = oversikt.formue
+        var tittel = textService.getJsonOkonomiTittel(soknadTypeToTitleKey[VERDI_BOLIG])
+        addFormueIfCheckedElseDeleteInOversikt(verdier, VERDI_BOLIG, tittel, verdierFrontend.bolig)
+        tittel = textService.getJsonOkonomiTittel(soknadTypeToTitleKey[VERDI_CAMPINGVOGN])
+        addFormueIfCheckedElseDeleteInOversikt(verdier, VERDI_CAMPINGVOGN, tittel, verdierFrontend.campingvogn)
+        tittel = textService.getJsonOkonomiTittel(soknadTypeToTitleKey[VERDI_KJORETOY])
+        addFormueIfCheckedElseDeleteInOversikt(verdier, VERDI_KJORETOY, tittel, verdierFrontend.kjoretoy)
+        tittel = textService.getJsonOkonomiTittel(soknadTypeToTitleKey[VERDI_FRITIDSEIENDOM])
+        addFormueIfCheckedElseDeleteInOversikt(verdier, VERDI_FRITIDSEIENDOM, tittel, verdierFrontend.fritidseiendom)
+        tittel = textService.getJsonOkonomiTittel(soknadTypeToTitleKey[VERDI_ANNET])
+        addFormueIfCheckedElseDeleteInOversikt(verdier, VERDI_ANNET, tittel, verdierFrontend.annet)
+    }
+
+    private fun setBeskrivelseAvAnnet(opplysninger: JsonOkonomiopplysninger, verdierFrontend: VerdierFrontend) {
+        if (opplysninger.beskrivelseAvAnnet == null) {
+            opplysninger.withBeskrivelseAvAnnet(
+                JsonOkonomibeskrivelserAvAnnet()
+                    .withKilde(JsonKildeBruker.BRUKER)
+                    .withVerdi("")
+                    .withSparing("")
+                    .withUtbetaling("")
+                    .withBoutgifter("")
+                    .withBarneutgifter("")
+            )
+        }
+        opplysninger.beskrivelseAvAnnet.verdi = verdierFrontend.beskrivelseAvAnnet ?: ""
+    }
+
+    private fun getBekreftelse(opplysninger: JsonOkonomiopplysninger): Boolean? {
+        return opplysninger.bekreftelse.firstOrNull { it.type == BEKREFTELSE_VERDI }?.verdi
+    }
+
+    private fun hasVerdiType(oversikt: JsonOkonomioversikt, type: String): Boolean {
+        return oversikt.formue.any { it.type == type }
+    }
+
+    data class VerdierFrontend(
+        var bekreftelse: Boolean? = null,
+        var bolig: Boolean = false,
+        var campingvogn: Boolean = false,
+        var kjoretoy: Boolean = false,
+        var fritidseiendom: Boolean = false,
+        var annet: Boolean = false,
+        var beskrivelseAvAnnet: String? = null
+    )
+}
---
+++ src/test/kotlin/no/nav/sosialhjelp/soknad/inntekt/verdi/VerdiRessursTest.kt
@@ -0,0 +1,303 @@
+package no.nav.sosialhjelp.soknad.inntekt.verdi
+
+import io.mockk.clearAllMocks
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.runs
+import io.mockk.slot
+import io.mockk.verify
+import no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper
+import no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.VERDI_ANNET
+import no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.VERDI_BOLIG
+import no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.VERDI_CAMPINGVOGN
+import no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.VERDI_FRITIDSEIENDOM
+import no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.VERDI_KJORETOY
+import no.nav.sbl.soknadsosialhjelp.soknad.common.JsonKilde
+import no.nav.sbl.soknadsosialhjelp.soknad.okonomi.opplysning.JsonOkonomibekreftelse
+import no.nav.sbl.soknadsosialhjelp.soknad.okonomi.opplysning.JsonOkonomibeskrivelserAvAnnet
+import no.nav.sbl.soknadsosialhjelp.soknad.okonomi.oversikt.JsonOkonomioversiktFormue
+import no.nav.sosialhjelp.soknad.business.db.repositories.soknadunderarbeid.SoknadUnderArbeidRepository
+import no.nav.sosialhjelp.soknad.business.service.TextService
+import no.nav.sosialhjelp.soknad.business.service.soknadservice.SoknadService
+import no.nav.sosialhjelp.soknad.domain.SoknadUnderArbeid
+import no.nav.sosialhjelp.soknad.domain.model.exception.AuthorizationException
+import no.nav.sosialhjelp.soknad.domain.model.oidc.StaticSubjectHandlerService
+import no.nav.sosialhjelp.soknad.domain.model.oidc.SubjectHandler
+import no.nav.sosialhjelp.soknad.inntekt.verdi.VerdiRessurs.VerdierFrontend
+import no.nav.sosialhjelp.soknad.web.sikkerhet.Tilgangskontroll
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class VerdiRessursTest {
+
+    private val BEHANDLINGSID = "123"
+    private val EIER = "123456789101"
+
+    private val soknadUnderArbeidRepository: SoknadUnderArbeidRepository = mockk()
+    private val tilgangskontroll: Tilgangskontroll = mockk()
+    private val textService: TextService = mockk()
+    private val verdiRessurs = VerdiRessurs(tilgangskontroll, soknadUnderArbeidRepository, textService)
+
+    @BeforeEach
+    fun setUp() {
+        clearAllMocks()
+        System.setProperty("environment.name", "test")
+        SubjectHandler.setSubjectHandlerService(StaticSubjectHandlerService())
+    }
+
+    @AfterEach
+    fun tearDown() {
+        SubjectHandler.resetOidcSubjectHandlerService()
+        System.clearProperty("environment.name")
+    }
+
+    @Test
+    fun getVerdierSkalReturnereBekreftelseLikNullOgAltFalse() {
+        every { tilgangskontroll.verifiserAtBrukerHarTilgang() } just runs
+        every { soknadUnderArbeidRepository.hentSoknad(any<String>(), any()) } returns
+            SoknadUnderArbeid().withJsonInternalSoknad(SoknadService.createEmptyJsonInternalSoknad(EIER))
+
+        val verdierFrontend = verdiRessurs.hentVerdier(BEHANDLINGSID)
+        assertThat(verdierFrontend.bekreftelse).isNull()
+        assertThat(verdierFrontend.bolig).isFalse
+        assertThat(verdierFrontend.campingvogn).isFalse
+        assertThat(verdierFrontend.kjoretoy).isFalse
+        assertThat(verdierFrontend.fritidseiendom).isFalse
+        assertThat(verdierFrontend.annet).isFalse
+        assertThat(verdierFrontend.beskrivelseAvAnnet).isNull()
+    }
+
+    @Test
+    fun getVerdierSkalReturnereBekreftelserLikTrue() {
+        every { tilgangskontroll.verifiserAtBrukerHarTilgang() } just runs
+        every { soknadUnderArbeidRepository.hentSoknad(any<String>(), any()) } returns
+            createJsonInternalSoknadWithVerdier(
+                true,
+                listOf(VERDI_BOLIG, VERDI_CAMPINGVOGN, VERDI_KJORETOY, VERDI_FRITIDSEIENDOM, VERDI_ANNET),
+                null
+            )
+
+        val verdierFrontend = verdiRessurs.hentVerdier(BEHANDLINGSID)
+        assertThat(verdierFrontend.bekreftelse).isTrue
+        assertThat(verdierFrontend.bolig).isTrue
+        assertThat(verdierFrontend.campingvogn).isTrue
+        assertThat(verdierFrontend.kjoretoy).isTrue
+        assertThat(verdierFrontend.fritidseiendom).isTrue
+        assertThat(verdierFrontend.annet).isTrue
+        assertThat(verdierFrontend.beskrivelseAvAnnet).isNull()
+    }
+
+    @Test
+    fun getVerdierSkalReturnereBeskrivelseAvAnnet() {
+        every { tilgangskontroll.verifiserAtBrukerHarTilgang() } just runs
+        val beskrivelse = "Bestefars klokke"
+        every { soknadUnderArbeidRepository.hentSoknad(any<String>(), any()) } returns
+            createJsonInternalSoknadWithVerdier(true, listOf(VERDI_ANNET), beskrivelse)
+
+        val verdierFrontend: VerdierFrontend =
+            verdiRessurs.hentVerdier(
+                BEHANDLINGSID
+            )
+        assertThat(verdierFrontend.bekreftelse).isTrue
+        assertThat(verdierFrontend.annet).isTrue
+        assertThat(verdierFrontend.beskrivelseAvAnnet).isEqualTo(beskrivelse)
+    }
+
+    @Test
+    fun putVerdierSkalSetteAltFalseDersomManVelgerHarIkkeVerdier() {
+        every { tilgangskontroll.verifiserAtBrukerKanEndreSoknad(any()) } just runs
+        every { soknadUnderArbeidRepository.hentSoknad(any<String>(), any()) } returns
+            createJsonInternalSoknadWithVerdier(
+                true,
+                listOf(
+                    VERDI_BOLIG, VERDI_CAMPINGVOGN, VERDI_KJORETOY, VERDI_ANNET
+                ),
+                "Bestefars klokke"
+            )
+        every { textService.getJsonOkonomiTittel(any()) } returns "tittel"
+
+        val slot = slot<SoknadUnderArbeid>()
+        every { soknadUnderArbeidRepository.oppdaterSoknadsdata(capture(slot), any()) } just runs
+
+        val verdierFrontend = VerdierFrontend(bekreftelse = false)
+        verdiRessurs.updateVerdier(BEHANDLINGSID, verdierFrontend)
+
+        val soknadUnderArbeid = slot.captured
+        val bekreftelser = soknadUnderArbeid.jsonInternalSoknad.soknad.data.okonomi.opplysninger.bekreftelse
+        val verdiBekreftelse = bekreftelser[0]
+        val verdier = soknadUnderArbeid.jsonInternalSoknad.soknad.data.okonomi.oversikt.formue
+        assertThat(verdiBekreftelse.verdi).isFalse
+        assertThat(verdier.isEmpty()).isTrue
+    }
+
+    @Test
+    fun putVerdierSkalSetteAlleBekreftelserLikFalse() {
+        every { tilgangskontroll.verifiserAtBrukerKanEndreSoknad(any()) } just runs
+        every { soknadUnderArbeidRepository.hentSoknad(any<String>(), any()) } returns
+            createJsonInternalSoknadWithVerdier(
+                true,
+                listOf(VERDI_BOLIG, VERDI_CAMPINGVOGN, VERDI_KJORETOY, VERDI_ANNET),
+                "Bestefars klokke"
+            )
+        every { textService.getJsonOkonomiTittel(any()) } returns "tittel"
+
+        val slot = slot<SoknadUnderArbeid>()
+        every { soknadUnderArbeidRepository.oppdaterSoknadsdata(capture(slot), any()) } just runs
+
+        val verdierFrontend = VerdierFrontend(bekreftelse = false)
+        verdiRessurs.updateVerdier(BEHANDLINGSID, verdierFrontend)
+
+        val soknadUnderArbeid = slot.captured
+        val bekreftelser = soknadUnderArbeid.jsonInternalSoknad.soknad.data.okonomi.opplysninger.bekreftelse
+        val verdiBekreftelse = bekreftelser[0]
+        val verdier = soknadUnderArbeid.jsonInternalSoknad.soknad.data.okonomi.oversikt.formue
+        val beskrivelse = soknadUnderArbeid.jsonInternalSoknad.soknad.data.okonomi.opplysninger.beskrivelseAvAnnet.verdi
+        assertThat(verdiBekreftelse.verdi).isFalse
+        assertThat(verdier.isEmpty()).isTrue
+        assertThat(beskrivelse).isBlank
+    }
+
+    @Test
+    fun putVerdierSkalSetteNoenBekreftelser() {
+        every { tilgangskontroll.verifiserAtBrukerKanEndreSoknad(any()) } just runs
+        every { soknadUnderArbeidRepository.hentSoknad(any<String>(), any()) } returns
+            SoknadUnderArbeid().withJsonInternalSoknad(SoknadService.createEmptyJsonInternalSoknad(EIER))
+        every { textService.getJsonOkonomiTittel(any()) } returns "tittel"
+
+        val slot = slot<SoknadUnderArbeid>()
+        every { soknadUnderArbeidRepository.oppdaterSoknadsdata(capture(slot), any()) } just runs
+
+        val verdierFrontend = VerdierFrontend(
+            bekreftelse = true,
+            bolig = true,
+            campingvogn = true,
+            kjoretoy = false,
+            fritidseiendom = false,
+            annet = false
+        )
+        verdiRessurs.updateVerdier(BEHANDLINGSID, verdierFrontend)
+
+        val soknadUnderArbeid = slot.captured
+        val bekreftelser = soknadUnderArbeid.jsonInternalSoknad.soknad.data.okonomi.opplysninger.bekreftelse
+        val verdiBekreftelse = bekreftelser[0]
+        val verdier = soknadUnderArbeid.jsonInternalSoknad.soknad.data.okonomi.oversikt.formue
+        assertThat(verdiBekreftelse.kilde).isEqualTo(JsonKilde.BRUKER)
+        assertThat(verdiBekreftelse.type).isEqualTo(SoknadJsonTyper.BEKREFTELSE_VERDI)
+        assertThat(verdiBekreftelse.verdi).isTrue
+        assertThat(verdier.any { it.type == VERDI_BOLIG }).isTrue
+        assertThat(verdier.any { it.type == VERDI_CAMPINGVOGN }).isTrue
+        assertThat(verdier.any { it.type == VERDI_KJORETOY }).isFalse
+        assertThat(verdier.any { it.type == VERDI_FRITIDSEIENDOM }).isFalse
+        assertThat(verdier.any { it.type == VERDI_ANNET }).isFalse
+    }
+
+    @Test
+    fun putVerdierSkalSetteAlleBekreftelser() {
+        every { tilgangskontroll.verifiserAtBrukerKanEndreSoknad(any()) } just runs
+        every { soknadUnderArbeidRepository.hentSoknad(any<String>(), any()) } returns
+            SoknadUnderArbeid().withJsonInternalSoknad(SoknadService.createEmptyJsonInternalSoknad(EIER))
+        every { textService.getJsonOkonomiTittel(any()) } returns "tittel"
+
+        val slot = slot<SoknadUnderArbeid>()
+        every { soknadUnderArbeidRepository.oppdaterSoknadsdata(capture(slot), any()) } just runs
+
+        val verdierFrontend = VerdierFrontend(
+            bekreftelse = true,
+            bolig = true,
+            campingvogn = true,
+            kjoretoy = true,
+            fritidseiendom = true,
+            annet = true
+        )
+        verdiRessurs.updateVerdier(BEHANDLINGSID, verdierFrontend)
+
+        val soknadUnderArbeid = slot.captured
+        val bekreftelser = soknadUnderArbeid.jsonInternalSoknad.soknad.data.okonomi.opplysninger.bekreftelse
+        val verdiBekreftelse = bekreftelser[0]
+        val verdier = soknadUnderArbeid.jsonInternalSoknad.soknad.data.okonomi.oversikt.formue
+        assertThat(verdiBekreftelse.kilde).isEqualTo(JsonKilde.BRUKER)
+        assertThat(verdiBekreftelse.type).isEqualTo(SoknadJsonTyper.BEKREFTELSE_VERDI)
+        assertThat(verdiBekreftelse.verdi).isTrue
+        assertThat(verdier.any { it.type == VERDI_KJORETOY }).isTrue
+        assertThat(verdier.any { it.type == VERDI_CAMPINGVOGN }).isTrue
+        assertThat(verdier.any { it.type == VERDI_BOLIG }).isTrue
+        assertThat(verdier.any { it.type == VERDI_FRITIDSEIENDOM }).isTrue
+        assertThat(verdier.any { it.type == VERDI_ANNET }).isTrue
+    }
+
+    @Test
+    fun putVerdierSkalFjerneBeskrivelseAvAnnetDersomAnnetBlirAvkreftet() {
+        every { tilgangskontroll.verifiserAtBrukerKanEndreSoknad(any()) } just runs
+        every { soknadUnderArbeidRepository.hentSoknad(any<String>(), any()) } returns
+            createJsonInternalSoknadWithVerdier(true, listOf(VERDI_ANNET), "Vinylplater")
+        every { textService.getJsonOkonomiTittel(any()) } returns "tittel"
+
+        val slot = slot<SoknadUnderArbeid>()
+        every { soknadUnderArbeidRepository.oppdaterSoknadsdata(capture(slot), any()) } just runs
+
+        val verdierFrontend = VerdierFrontend(bekreftelse = false)
+        verdiRessurs.updateVerdier(BEHANDLINGSID, verdierFrontend)
+
+        val soknadUnderArbeid = slot.captured
+        val bekreftelser = soknadUnderArbeid.jsonInternalSoknad.soknad.data.okonomi.opplysninger.bekreftelse
+        val verdiBekreftelse = bekreftelser[0]
+        val beskrivelse = soknadUnderArbeid.jsonInternalSoknad.soknad.data.okonomi.opplysninger.beskrivelseAvAnnet.verdi
+        assertThat(verdiBekreftelse.verdi).isFalse
+        assertThat(beskrivelse).isBlank
+    }
+
+    @Test
+    fun getVerdierSkalKasteAuthorizationExceptionVedManglendeTilgang() {
+        every { tilgangskontroll.verifiserAtBrukerHarTilgang() } throws AuthorizationException("Not for you my friend")
+
+        assertThatExceptionOfType(AuthorizationException::class.java)
+            .isThrownBy { verdiRessurs.hentVerdier(BEHANDLINGSID) }
+
+        verify(exactly = 0) { soknadUnderArbeidRepository.hentSoknad(any<String>(), any()) }
+    }
+
+    @Test
+    fun putVerdierSkalKasteAuthorizationExceptionVedManglendeTilgang() {
+        every { tilgangskontroll.verifiserAtBrukerKanEndreSoknad(BEHANDLINGSID) } throws AuthorizationException("Not for you my friend")
+        val verdierFrontend = VerdierFrontend()
+        assertThatExceptionOfType(AuthorizationException::class.java)
+            .isThrownBy { verdiRessurs.updateVerdier(BEHANDLINGSID, verdierFrontend) }
+
+        verify(exactly = 0) { soknadUnderArbeidRepository.hentSoknad(any<String>(), any()) }
+    }
+
+    private fun createJsonInternalSoknadWithVerdier(
+        harVerdier: Boolean,
+        verdiTyper: List<String>,
+        beskrivelseAvAnnet: String?
+    ): SoknadUnderArbeid {
+        val soknadUnderArbeid = SoknadUnderArbeid().withJsonInternalSoknad(
+            SoknadService.createEmptyJsonInternalSoknad(
+                EIER
+            )
+        )
+        val verdier: MutableList<JsonOkonomioversiktFormue> = ArrayList()
+        for (verdi in verdiTyper) {
+            verdier.add(
+                JsonOkonomioversiktFormue()
+                    .withKilde(JsonKilde.BRUKER)
+                    .withType(verdi)
+                    .withTittel("tittel")
+            )
+        }
+        soknadUnderArbeid.jsonInternalSoknad.soknad.data.okonomi.opplysninger.bekreftelse = listOf(
+            JsonOkonomibekreftelse()
+                .withKilde(JsonKilde.BRUKER)
+                .withType(SoknadJsonTyper.BEKREFTELSE_VERDI)
+                .withVerdi(harVerdier)
+        )
+        soknadUnderArbeid.jsonInternalSoknad.soknad.data.okonomi.oversikt.formue = verdier
+        soknadUnderArbeid.jsonInternalSoknad.soknad.data.okonomi.opplysninger.beskrivelseAvAnnet =
+            JsonOkonomibeskrivelserAvAnnet().withVerdi(beskrivelseAvAnnet)
+        return soknadUnderArbeid
+    }
+}
