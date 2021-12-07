764a2068aa895da741611e58038a35956b4071f5
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-12-07 16:02:43 +0100

    fjern utkommentert kode

--- src/main/java/no/nav/sosialhjelp/soknad/business/service/soknadservice/Systemdata.java
+++
@@ -1,9 +0,0 @@
-//package no.nav.sosialhjelp.soknad.business.service.soknadservice;
-//
-//import no.nav.sosialhjelp.soknad.domain.SoknadUnderArbeid;
-//
-//public interface Systemdata {
-//
-//    void updateSystemdataIn(SoknadUnderArbeid soknadUnderArbeid);
-//
-//}
