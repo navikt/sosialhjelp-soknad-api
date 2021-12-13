75f090aacec6fb44b2a8f79e574ddbb726f8f06d
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-12-13 11:09:26 +0100

    fulltnavn del av NavnFrontend

--- src/main/kotlin/no/nav/sosialhjelp/soknad/personalia/familie/dto/NavnFrontend.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/personalia/familie/dto/NavnFrontend.kt
@@ -1,7 +1,17 @@
 package no.nav.sosialhjelp.soknad.personalia.familie.dto
 
+import org.apache.commons.lang3.StringUtils
+
 data class NavnFrontend(
     val fornavn: String? = null,
     val mellomnavn: String? = null,
     val etternavn: String? = null
-)
+) {
+    val fulltNavn: String
+        get() {
+            val f = if (!StringUtils.isEmpty(fornavn)) fornavn else ""
+            val m = if (!StringUtils.isEmpty(mellomnavn)) " $mellomnavn" else ""
+            val e = if (!StringUtils.isEmpty(etternavn)) " $etternavn" else ""
+            return (f + m + e).trim { it <= ' ' }
+        }
+}
