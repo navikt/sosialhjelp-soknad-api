8028ae41141cc478985ba9ae1bea6d67c478ef51
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-12-14 10:53:07 +0100

    SaksoversiktMetadataOidcRessurs -> kotlin SaksoversiktMetadataRessurs -> kotlin

--- src/main/java/no/nav/sosialhjelp/soknad/web/config/SoknadinnsendingConfig.java
+++ src/main/java/no/nav/sosialhjelp/soknad/web/config/SoknadinnsendingConfig.java
@@ -7,6 +7,7 @@ import no.nav.sosialhjelp.soknad.api.informasjon.InformasjonConfig;
 import no.nav.sosialhjelp.soknad.api.innsyn.SoknadOversiktConfig;
 import no.nav.sosialhjelp.soknad.api.minesaker.MineSakerConfig;
 import no.nav.sosialhjelp.soknad.api.nedetid.NedetidConfig;
+import no.nav.sosialhjelp.soknad.api.saksoversikt.SaksoversiktConfig;
 import no.nav.sosialhjelp.soknad.begrunnelse.BegrunnelseConfig;
 import no.nav.sosialhjelp.soknad.bosituasjon.BosituasjonConfig;
 import no.nav.sosialhjelp.soknad.business.BusinessConfig;
@@ -59,7 +60,8 @@ import org.springframework.context.annotation.Import;
         EttersendingConfig.class,
         HealthConfig.class,
         FeatureToggleConfig.class,
-        NedetidConfig.class
+        NedetidConfig.class,
+        SaksoversiktConfig.class
 })
 @ComponentScan(basePackages = "no.nav.sosialhjelp.soknad.web.rest")
 public class SoknadinnsendingConfig {
