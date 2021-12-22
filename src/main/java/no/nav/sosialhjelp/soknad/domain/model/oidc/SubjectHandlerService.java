8968baa739f9ab82c71f45a103c69a30822844ae
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-12-22 13:37:59 +0100

    fjern java-klasser

--- src/main/java/no/nav/sosialhjelp/soknad/domain/model/oidc/SubjectHandlerService.java
+++
@@ -1,7 +0,0 @@
-//package no.nav.sosialhjelp.soknad.domain.model.oidc;
-//
-//public interface SubjectHandlerService {
-//    String getUserIdFromToken();
-//    String getToken();
-//    String getConsumerId();
-//}
