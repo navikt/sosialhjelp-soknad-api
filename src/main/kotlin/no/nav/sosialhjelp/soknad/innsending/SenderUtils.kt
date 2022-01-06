c44c2071672a7909b930a690301d408b40cb6877
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-01-06 11:33:13 +0100

    fix

--- src/main/kotlin/no/nav/sosialhjelp/soknad/innsending/SenderUtils.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/innsending/SenderUtils.kt
@@ -3,7 +3,7 @@ package no.nav.sosialhjelp.soknad.innsending
 import no.nav.sosialhjelp.soknad.domain.model.util.ServiceUtils
 
 object SenderUtils {
-    fun createPrefixedBehandlingsIdInNonProd(behandlingsId: String): String {
+    fun createPrefixedBehandlingsIdInNonProd(behandlingsId: String?): String? {
         return if (ServiceUtils.isNonProduction()) {
             System.getProperty("environment.name") + "-" + behandlingsId
         } else behandlingsId
