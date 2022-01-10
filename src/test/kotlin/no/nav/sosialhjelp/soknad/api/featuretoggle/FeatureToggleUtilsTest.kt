0d1e2250d73884580f4e1dd0cca02b503d2ddfaa
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2022-01-10 11:48:20 +0100

    Legger til test for feature-toggle endepunkt

--- src/test/kotlin/no/nav/sosialhjelp/soknad/api/featuretoggle/FeatureToggleUtilsTest.kt
+++ src/test/kotlin/no/nav/sosialhjelp/soknad/api/featuretoggle/FeatureToggleUtilsTest.kt
@@ -1,12 +1,21 @@
 package no.nav.sosialhjelp.soknad.api.featuretoggle
 
+import io.mockk.every
+import io.mockk.mockk
+import no.finn.unleash.Unleash
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 
 internal class FeatureToggleUtilsTest {
 
+    private val unleash: Unleash = mockk()
+    private val featureToggleRessurs = FeatureToggleRessurs(unleash)
+
     @Test
     fun leggTilBarnTest() {
-        // Todo: Finne en god måte å teste unleash-funksjonalitet på?
+        every { unleash.isEnabled(any(), any<Boolean>()) } returns true
+
+        val response = featureToggleRessurs.featureToggles();
+        Assertions.assertThat(response["leggeTilBarn"]).isTrue
     }
 }
