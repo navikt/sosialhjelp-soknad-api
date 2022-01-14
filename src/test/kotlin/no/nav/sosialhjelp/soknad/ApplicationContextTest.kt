78a27d1519d5bdc320fa16a9f8d997bf18565428
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-01-14 16:42:27 +0100

    fjern gamle integrasjonstester. ContentConfigTest flyttet til ApplicationContextTest.kt

--- src/test/kotlin/no/nav/sosialhjelp/soknad/ApplicationContextTest.kt
+++ src/test/kotlin/no/nav/sosialhjelp/soknad/ApplicationContextTest.kt
@@ -1,6 +1,10 @@
 package no.nav.sosialhjelp.soknad
 
+import no.nav.sosialhjelp.soknad.tekster.NavMessageSource
+import no.nav.sosialhjelp.soknad.web.config.ContentConfig
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.mockito.InjectMocks
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 
@@ -8,7 +12,17 @@ import org.springframework.test.context.ActiveProfiles
 @ActiveProfiles(profiles = ["no-redis", "test"])
 class ApplicationContextTest {
 
+    @InjectMocks
+    var contentConfig: ContentConfig? = null
+
     @Test
     internal fun skalStarte() {
+    }
+
+    @Test
+    fun skalReturnereRettAntallBundles() {
+        val source: NavMessageSource = contentConfig!!.navMessageSource()
+        val basenames = source.basenames
+        assertThat(basenames).hasSize(1)
     }
 }
