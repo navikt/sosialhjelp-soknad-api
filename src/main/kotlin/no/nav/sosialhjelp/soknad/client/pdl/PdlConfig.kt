01350303f79a56c13cab778395e2bbcb8f445a28
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-01-14 16:31:24 +0100

    Flytter pdl ping til GeografiskTilknytningConfig.kt

--- src/main/kotlin/no/nav/sosialhjelp/soknad/client/pdl/PdlConfig.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/client/pdl/PdlConfig.kt
@@ -1,10 +1,7 @@
 package no.nav.sosialhjelp.soknad.client.pdl
 
 import no.nav.sosialhjelp.soknad.domain.model.util.HeaderConstants.HEADER_NAV_APIKEY
-import no.nav.sosialhjelp.soknad.health.selftest.Pingable
-import no.nav.sosialhjelp.soknad.navenhet.gt.GeografiskTilknytningClient
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import javax.ws.rs.client.ClientRequestFilter
 
@@ -12,19 +9,6 @@ import javax.ws.rs.client.ClientRequestFilter
 open class PdlConfig(
     @Value("\${pdl_api_url}") private val baseurl: String
 ) {
-
-    @Bean
-    open fun pdlPing(geografiskTilknytningClient: GeografiskTilknytningClient): Pingable {
-        return Pingable {
-            val metadata = Pingable.PingMetadata(baseurl, "PDL", true)
-            try {
-                geografiskTilknytningClient.ping()
-                Pingable.lyktes(metadata)
-            } catch (e: Exception) {
-                Pingable.feilet(metadata, e)
-            }
-        }
-    }
 
     companion object {
         private const val PDLAPI_APIKEY = "PDLAPI_APIKEY"
