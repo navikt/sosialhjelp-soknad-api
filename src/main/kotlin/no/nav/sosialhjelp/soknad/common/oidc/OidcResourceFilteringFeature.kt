7430b52184b3211fd7ae357335f578ad70e0112b
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-01-19 08:19:22 +0100

    revert OidcResourceFilteringFeature endringer

--- src/main/kotlin/no/nav/sosialhjelp/soknad/common/oidc/OidcResourceFilteringFeature.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/common/oidc/OidcResourceFilteringFeature.kt
@@ -2,6 +2,8 @@ package no.nav.sosialhjelp.soknad.common.oidc
 
 import no.nav.security.token.support.jaxrs.JwtTokenContainerRequestFilter
 import no.nav.sosialhjelp.soknad.domain.model.util.ServiceUtils
+import org.glassfish.jersey.server.wadl.processor.OptionsMethodProcessor
+import org.glassfish.jersey.server.wadl.processor.WadlModelProcessor.OptionsHandler
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import javax.ws.rs.container.DynamicFeature
@@ -15,10 +17,15 @@ class OidcResourceFilteringFeature(
 ) : DynamicFeature {
 
     override fun configure(resourceInfo: ResourceInfo, context: FeatureContext) {
-        if (isAllowedWhenNotRunningInProd) {
+        if (isClassAllowedInProd(resourceInfo) || isAllowedWhenNotRunningInProd) {
             return
         }
         context.register(JwtTokenContainerRequestFilter::class.java)
+    }
+
+    private fun isClassAllowedInProd(resourceInfo: ResourceInfo): Boolean {
+        return ALLOWED_CLASSES.contains(resourceInfo.resourceClass) ||
+                ALLOWED_PARENT_CLASSES.contains(resourceInfo.resourceClass.enclosingClass)
     }
 
     private val isAllowedWhenNotRunningInProd: Boolean
@@ -27,4 +34,10 @@ class OidcResourceFilteringFeature(
     private val isOidcMock: Boolean
         get() = "true".equals(tillatmock, ignoreCase = true) &&
             "true".equals(startOidcMock, ignoreCase = true)
+
+    companion object {
+        private val ALLOWED_CLASSES =
+            listOf<Class<*>>(OptionsHandler::class.java) // Add Resource-classes from external libraries we need to use but can't annotate with @unprotected.
+        private val ALLOWED_PARENT_CLASSES = listOf<Class<*>>(OptionsMethodProcessor::class.java)
+    }
 }
