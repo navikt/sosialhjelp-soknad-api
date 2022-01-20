c8c20525128a6ed63dfc4a23a9296713fec2757d
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-01-20 13:05:53 +0100

    kommenterer ut kode som ikke lengre brukes: MockAltSoknadsosialhjelpServer.java, CreateSslConnector.java, Jetty.java, ShutdownHook.java, SoknadsosialhjelpServer.java, override-web-mock-alt.xml, web.xml, SoknadsosialhjelpServerTest.java, DevSoknadsosialhjelpServer.java, override-web.xml, override-web-integration.xml

--- src/main/java/no/nav/sosialhjelp/soknad/web/server/ShutdownHook.java
+++ src/main/java/no/nav/sosialhjelp/soknad/web/server/ShutdownHook.java
@@ -1,35 +1,35 @@
-package no.nav.sosialhjelp.soknad.web.server;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class ShutdownHook extends Thread {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(ShutdownHook.class);
-
-    public ShutdownHook(Jetty jetty) {
-        super(new Hook(jetty));
-    }
-
-    private static class Hook implements Runnable {
-        private final Jetty jetty;
-
-        public Hook(Jetty jetty) {
-            this.jetty = jetty;
-        }
-
-        @Override
-        public void run() {
-            try {
-                LOGGER.info("shutdown initialized for sosialhjelp-soknad-api, allowing incoming requests for 7 seconds before continuing");
-                Thread.sleep(7000L);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            LOGGER.info("shutting down server");
-            jetty.stop.run();
-            LOGGER.info("shutdown ok");
-        }
-    }
-}
+//package no.nav.sosialhjelp.soknad.web.server;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//
+//public class ShutdownHook extends Thread {
+//
+//    private static final Logger LOGGER = LoggerFactory.getLogger(ShutdownHook.class);
+//
+//    public ShutdownHook(Jetty jetty) {
+//        super(new Hook(jetty));
+//    }
+//
+//    private static class Hook implements Runnable {
+//        private final Jetty jetty;
+//
+//        public Hook(Jetty jetty) {
+//            this.jetty = jetty;
+//        }
+//
+//        @Override
+//        public void run() {
+//            try {
+//                LOGGER.info("shutdown initialized for sosialhjelp-soknad-api, allowing incoming requests for 7 seconds before continuing");
+//                Thread.sleep(7000L);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//
+//            LOGGER.info("shutting down server");
+//            jetty.stop.run();
+//            LOGGER.info("shutdown ok");
+//        }
+//    }
+//}
--- src/test/java/no/nav/sosialhjelp/soknad/web/server/SoknadsosialhjelpServerTest.java
+++ src/test/java/no/nav/sosialhjelp/soknad/web/server/SoknadsosialhjelpServerTest.java
@@ -1,76 +1,76 @@
-package no.nav.sosialhjelp.soknad.web.server;
-
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-
-@Disabled
-class SoknadsosialhjelpServerTest {
-
-    @Test
-    void withEnvironmentVariableExpansion_shouldBeUnchanged() {
-        assertUnchanged(null);
-        assertUnchanged("");
-        assertUnchanged("https://dette.er.ett.api.no");
-        assertUnchanged("{}");
-        assertUnchanged("{2}");
-        assertUnchanged("{https://dette.er.ett.api.no}");
-        assertUnchanged("https://dette.er.ett.api.no}");
-        assertUnchanged("${https://dette.er.ett.api.no");
-        assertUnchanged("$https://dette.er.ett.api.no");
-        assertUnchanged("blalba {} ok {2} $ tja ${...");
-    }
-
-    @Test
-    void withEnvironmentVariableExpansion_notRequired_shouldHandleDefaultValues() {
-        System.setProperty("ENV_VAR_SET", "envVar");
-
-        String value = SoknadsosialhjelpServer.withEnvironmentVariableExpansion("${ENV_VAR_SET:en.ny:fancy:variabel!=}", false);
-        assertThat(value).isEqualTo("envVar");
-
-        value = SoknadsosialhjelpServer.withEnvironmentVariableExpansion("${ENV_VAR_NOT_SET:en.ny:fancy:variabel!=}", false);
-        assertThat(value).isEqualTo("en.ny:fancy:variabel!=");
-    }
-
-    @Test
-    void withEnvironmentVariableExpansion_withRequiredParameterAndEnvVarSet_shouldHandleDefaultValues() {
-        System.setProperty("ENV_VAR_SET", "envVar");
-
-        String value = SoknadsosialhjelpServer.withEnvironmentVariableExpansion("${ENV_VAR_SET:en.ny:fancy:variabel!=}", true);
-        assertThat(value).isEqualTo("envVar");
-    }
-
-    @Test
-    void withEnvironmentVariableExpansion_withRequiredWithoutEnvVarSet_ShouldUseVariable() {
-        String value = SoknadsosialhjelpServer.withEnvironmentVariableExpansion("${ENV_NOT_SET:en.ny:fancy:variabel!=}", true);
-        assertThat(value).isEqualTo("en.ny:fancy:variabel!=");
-    }
-
-    @Test
-    void withEnvironmentVariableExpansion_withRequiredWithoutEnvVarSet_ShouldThrowException() {
-        assertThatExceptionOfType(IllegalStateException.class)
-                .isThrownBy(() -> SoknadsosialhjelpServer.withEnvironmentVariableExpansion("${ENV_NOT_SET}", true));
-    }
-
-    @Test
-    void withEnvironmentVariableExpansion_shouldHandleMultipleProperties() {
-        System.setProperty("SoknadsosialhjelpServerTest.property", "foobar");
-
-        String value = SoknadsosialhjelpServer.withEnvironmentVariableExpansion("Test ${SoknadsosialhjelpServerTest.property} med to ${SoknadsosialhjelpServerTest.property}", true);
-        assertThat(value).isEqualTo("Test foobar med to foobar");
-
-        value = SoknadsosialhjelpServer.withEnvironmentVariableExpansion("Test ${SoknadsosialhjelpServerTest.property} med to ${SoknadsosialhjelpServerTest.property}", false);
-        assertThat(value).isEqualTo("Test foobar med to foobar");
-    }
-
-
-    private void assertUnchanged(String input) {
-        String value = SoknadsosialhjelpServer.withEnvironmentVariableExpansion(input, true);
-        assertThat(value).isEqualTo(input);
-
-        value = SoknadsosialhjelpServer.withEnvironmentVariableExpansion(input, false);
-        assertThat(value).isEqualTo(input);
-    }
-}
+//package no.nav.sosialhjelp.soknad.web.server;
+//
+//import org.junit.jupiter.api.Disabled;
+//import org.junit.jupiter.api.Test;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+//
+//@Disabled
+//class SoknadsosialhjelpServerTest {
+//
+//    @Test
+//    void withEnvironmentVariableExpansion_shouldBeUnchanged() {
+//        assertUnchanged(null);
+//        assertUnchanged("");
+//        assertUnchanged("https://dette.er.ett.api.no");
+//        assertUnchanged("{}");
+//        assertUnchanged("{2}");
+//        assertUnchanged("{https://dette.er.ett.api.no}");
+//        assertUnchanged("https://dette.er.ett.api.no}");
+//        assertUnchanged("${https://dette.er.ett.api.no");
+//        assertUnchanged("$https://dette.er.ett.api.no");
+//        assertUnchanged("blalba {} ok {2} $ tja ${...");
+//    }
+//
+//    @Test
+//    void withEnvironmentVariableExpansion_notRequired_shouldHandleDefaultValues() {
+//        System.setProperty("ENV_VAR_SET", "envVar");
+//
+//        String value = SoknadsosialhjelpServer.withEnvironmentVariableExpansion("${ENV_VAR_SET:en.ny:fancy:variabel!=}", false);
+//        assertThat(value).isEqualTo("envVar");
+//
+//        value = SoknadsosialhjelpServer.withEnvironmentVariableExpansion("${ENV_VAR_NOT_SET:en.ny:fancy:variabel!=}", false);
+//        assertThat(value).isEqualTo("en.ny:fancy:variabel!=");
+//    }
+//
+//    @Test
+//    void withEnvironmentVariableExpansion_withRequiredParameterAndEnvVarSet_shouldHandleDefaultValues() {
+//        System.setProperty("ENV_VAR_SET", "envVar");
+//
+//        String value = SoknadsosialhjelpServer.withEnvironmentVariableExpansion("${ENV_VAR_SET:en.ny:fancy:variabel!=}", true);
+//        assertThat(value).isEqualTo("envVar");
+//    }
+//
+//    @Test
+//    void withEnvironmentVariableExpansion_withRequiredWithoutEnvVarSet_ShouldUseVariable() {
+//        String value = SoknadsosialhjelpServer.withEnvironmentVariableExpansion("${ENV_NOT_SET:en.ny:fancy:variabel!=}", true);
+//        assertThat(value).isEqualTo("en.ny:fancy:variabel!=");
+//    }
+//
+//    @Test
+//    void withEnvironmentVariableExpansion_withRequiredWithoutEnvVarSet_ShouldThrowException() {
+//        assertThatExceptionOfType(IllegalStateException.class)
+//                .isThrownBy(() -> SoknadsosialhjelpServer.withEnvironmentVariableExpansion("${ENV_NOT_SET}", true));
+//    }
+//
+//    @Test
+//    void withEnvironmentVariableExpansion_shouldHandleMultipleProperties() {
+//        System.setProperty("SoknadsosialhjelpServerTest.property", "foobar");
+//
+//        String value = SoknadsosialhjelpServer.withEnvironmentVariableExpansion("Test ${SoknadsosialhjelpServerTest.property} med to ${SoknadsosialhjelpServerTest.property}", true);
+//        assertThat(value).isEqualTo("Test foobar med to foobar");
+//
+//        value = SoknadsosialhjelpServer.withEnvironmentVariableExpansion("Test ${SoknadsosialhjelpServerTest.property} med to ${SoknadsosialhjelpServerTest.property}", false);
+//        assertThat(value).isEqualTo("Test foobar med to foobar");
+//    }
+//
+//
+//    private void assertUnchanged(String input) {
+//        String value = SoknadsosialhjelpServer.withEnvironmentVariableExpansion(input, true);
+//        assertThat(value).isEqualTo(input);
+//
+//        value = SoknadsosialhjelpServer.withEnvironmentVariableExpansion(input, false);
+//        assertThat(value).isEqualTo(input);
+//    }
+//}
