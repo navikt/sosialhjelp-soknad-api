94a7051a216fd27505513d870f05166ed115a3bb
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-01-20 11:08:18 +0100

    Registrerer egen metricsServlet og fjerner spring-boot-starter-actuator (inntil videre)

--- src/main/kotlin/no/nav/sosialhjelp/soknad/metrics/MetricsConfig.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/metrics/MetricsConfig.kt
@@ -1,5 +1,9 @@
 package no.nav.sosialhjelp.soknad.metrics
 
+import io.prometheus.client.CollectorRegistry
+import io.prometheus.client.exporter.MetricsServlet
+import io.prometheus.client.hotspot.DefaultExports
+import org.springframework.boot.web.servlet.ServletRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -9,5 +13,19 @@ open class MetricsConfig {
     @Bean
     open fun soknadMetricsService(): SoknadMetricsService {
         return SoknadMetricsService()
+    }
+
+    @Bean
+    open fun collectorRegistry(): CollectorRegistry? {
+        DefaultExports.initialize()
+        return CollectorRegistry.defaultRegistry
+    }
+
+    @Bean
+    open fun metricsServlet(): ServletRegistrationBean<*> {
+        val metricsServlet = ServletRegistrationBean<MetricsServlet>()
+        metricsServlet.servlet = MetricsServlet()
+        metricsServlet.urlMappings = listOf("/internal/metrics/*")
+        return metricsServlet
     }
 }
