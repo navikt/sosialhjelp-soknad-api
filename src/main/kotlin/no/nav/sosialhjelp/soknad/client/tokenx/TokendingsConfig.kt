25673c4b1f91237338ecdd69a6e5c423eec6075f
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-02-01 16:03:23 +0100

    nonproxied

--- src/main/kotlin/no/nav/sosialhjelp/soknad/client/tokenx/TokendingsConfig.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/client/tokenx/TokendingsConfig.kt
@@ -1,20 +1,16 @@
 package no.nav.sosialhjelp.soknad.client.tokenx
 
-import io.netty.channel.ChannelOption
-import io.netty.handler.timeout.ReadTimeoutHandler
-import no.nav.sosialhjelp.soknad.client.config.unproxiedHttpClient
 import no.nav.sosialhjelp.soknad.client.redis.RedisService
 import no.nav.sosialhjelp.soknad.client.tokenx.JwtProviderUtil.downloadWellKnown
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
-import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
 open class TokendingsConfig(
-    private val proxiedWebClientBuilder: WebClient.Builder,
+    private val nonProxiedWebClientBuilder: WebClient.Builder,
     @Value("\${tokendings_url}") val tokendingsUrl: String,
     @Value("\${tokendings_client_id}") val tokendingsClientId: String,
     @Value("\${tokendings_private_jwk}") val tokendingsPrivateJwk: String,
@@ -47,14 +43,7 @@ open class TokendingsConfig(
     }
 
     private val tokendingsWebClient: WebClient
-        get() = proxiedWebClientBuilder
-            .clientConnector(
-                ReactorClientHttpConnector(
-                    unproxiedHttpClient()
-                        .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 15000)
-                        .doOnConnected { it.addHandlerLast(ReadTimeoutHandler(60)) }
-                )
-            ).build()
+        get() = nonProxiedWebClientBuilder.build()
 
     private val wellKnown: WellKnown
         get() = downloadWellKnown(tokendingsUrl)
