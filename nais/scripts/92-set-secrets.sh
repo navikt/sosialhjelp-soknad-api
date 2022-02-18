e67ed07bc6a4e8fd3f70ac6fde96039bc7452153
martintveter <41987225+martintveter@users.noreply.github.com>
2022-02-18 12:04:58 +0100

    oppslag-api kalles vha tokenx (#1096)
    * Kaller oppslag-api kalles vha tokenx fremfor apigw og idporten-token
    
    * fjerner ping mot oppslag-api
    
    * henter ikke oppslag-api apikey fra vault

--- nais/scripts/92-set-secrets.sh
+++ nais/scripts/92-set-secrets.sh
@@ -4,7 +4,6 @@ export AAREGAPI_APIKEY=$(cat /apigw/aaregapi/x-nav-apiKey)
 export STSTOKEN_APIKEY=$(cat /apigw/securitytokenservicetoken/x-nav-apiKey)
 export DKIFAPI_APIKEY=$(cat /apigw/dkifapi/x-nav-apiKey)
 export PDLAPI_APIKEY=$(cat /apigw/pdlapi/x-nav-apiKey)
-export OPPSLAGAPI_APIKEY=$(cat /apigw/sosialhjelp_oppslag_api/x-nav-apiKey)
 
 echo "Export serviceuser credentials"
 export SRVSOKNADSOSIALHJELP_SERVER_USERNAME=$(cat /serviceuser/srvsoknadsosialhjelp-server/username)
--- src/main/kotlin/no/nav/sosialhjelp/soknad/personalia/kontonummer/KontonummerConfig.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/personalia/kontonummer/KontonummerConfig.kt
@@ -1,21 +1,21 @@
 package no.nav.sosialhjelp.soknad.personalia.kontonummer
 
 import no.nav.sosialhjelp.soknad.client.redis.RedisService
-import no.nav.sosialhjelp.soknad.common.Constants.HEADER_NAV_APIKEY
+import no.nav.sosialhjelp.soknad.client.tokenx.TokendingsService
 import no.nav.sosialhjelp.soknad.common.rest.RestUtils
-import no.nav.sosialhjelp.soknad.health.selftest.Pingable
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 import javax.ws.rs.client.Client
-import javax.ws.rs.client.ClientRequestFilter
 
 @Configuration
 @Import(KontonummerRessurs::class)
 open class KontonummerConfig(
     @Value("\${oppslag_api_baseurl}") private val baseurl: String,
-    private val redisService: RedisService
+    @Value("\${oppslag_api_audience}") private val oppslagApiAudience: String,
+    private val redisService: RedisService,
+    private val tokendingsService: TokendingsService
 ) {
 
     @Bean
@@ -25,20 +25,7 @@ open class KontonummerConfig(
 
     @Bean
     open fun kontonummerClient(): KontonummerClient {
-        return KontonummerClientImpl(client, baseurl, redisService)
-    }
-
-    @Bean
-    open fun kontonummerPing(kontonummerClient: KontonummerClient): Pingable {
-        return Pingable {
-            val metadata = Pingable.PingMetadata(baseurl, "Oppslag - kontonummer", false)
-            try {
-                kontonummerClient.ping()
-                Pingable.lyktes(metadata)
-            } catch (e: Exception) {
-                Pingable.feilet(metadata, e)
-            }
-        }
+        return KontonummerClientImpl(client, baseurl, redisService, oppslagApiAudience, tokendingsService)
     }
 
     @Bean
@@ -48,11 +35,4 @@ open class KontonummerConfig(
 
     private val client: Client
         get() = RestUtils.createClient()
-            .register(
-                ClientRequestFilter { it.headers.putSingle(HEADER_NAV_APIKEY, System.getenv(OPPSLAGAPI_APIKEY)) }
-            )
-
-    companion object {
-        private const val OPPSLAGAPI_APIKEY = "OPPSLAGAPI_APIKEY"
-    }
 }
