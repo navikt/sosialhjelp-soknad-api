86e705e939aad05641386f896084879a5fc72873
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-02-25 11:05:37 +0100

    fjern ubrukt serviceuser

--- nais/scripts/92-set-secrets.sh
+++ nais/scripts/92-set-secrets.sh
@@ -6,6 +6,3 @@ export PDLAPI_APIKEY=$(cat /apigw/pdlapi/x-nav-apiKey)
 echo "Export serviceuser credentials"
 export SRVSOKNADSOSIALHJELP_SERVER_USERNAME=$(cat /serviceuser/srvsoknadsosialhjelp-server/username)
 export SRVSOKNADSOSIALHJELP_SERVER_PASSWORD=$(cat /serviceuser/srvsoknadsosialhjelp-server/password)
-
-export SRVSOKNADSOSIALHJELP_SERVER_SBS_USERNAME=$(cat /serviceuser/srvsoknadsosialhjelp-server-sbs/username)
-export SRVSOKNADSOSIALHJELP_SERVER_SBS_PASSWORD=$(cat /serviceuser/srvsoknadsosialhjelp-server-sbs/password)
