8fdc6dbe24fe758411f66e403940946175875c80
martintveter <41987225+martintveter@users.noreply.github.com>
2022-02-25 13:24:25 +0100

    flyttet `srvsoknadsosialhjelp-server` til kubernetes secret (#1121)

--- nais/scripts/92-set-secrets.sh
+++ nais/scripts/92-set-secrets.sh
@@ -2,7 +2,3 @@ echo "Export apikeys"
 export AAREGAPI_APIKEY=$(cat /apigw/aaregapi/x-nav-apiKey)
 export STSTOKEN_APIKEY=$(cat /apigw/securitytokenservicetoken/x-nav-apiKey)
 export PDLAPI_APIKEY=$(cat /apigw/pdlapi/x-nav-apiKey)
-
-echo "Export serviceuser credentials"
-export SRVSOKNADSOSIALHJELP_SERVER_USERNAME=$(cat /serviceuser/srvsoknadsosialhjelp-server/username)
-export SRVSOKNADSOSIALHJELP_SERVER_PASSWORD=$(cat /serviceuser/srvsoknadsosialhjelp-server/password)
