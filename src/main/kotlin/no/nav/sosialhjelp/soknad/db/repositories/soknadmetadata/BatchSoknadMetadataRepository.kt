4d201b0f4f1bb14c1707726d13f10e9618aff24e
martintveter <41987225+martintveter@users.noreply.github.com>
2022-03-02 15:03:37 +0100

    BatchSoknadMetadataRepository -> kotlin (#1131)

---
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/db/repositories/soknadmetadata/BatchSoknadMetadataRepository.kt
@@ -0,0 +1,11 @@
+package no.nav.sosialhjelp.soknad.db.repositories.soknadmetadata
+
+import no.nav.sosialhjelp.soknad.business.domain.SoknadMetadata
+import java.util.Optional
+
+interface BatchSoknadMetadataRepository {
+    fun hentForBatch(antallDagerGammel: Int): Optional<SoknadMetadata>
+    fun hentEldreEnn(antallDagerGammel: Int): Optional<SoknadMetadata>
+    fun leggTilbakeBatch(id: Long)
+    fun slettSoknadMetaData(behandlingsId: String)
+}
---
+++ src/test/kotlin/no/nav/sosialhjelp/soknad/db/repositories/soknadmetadata/BatchSoknadMetadataRepositoryJdbcTest.kt
@@ -0,0 +1,107 @@
+package no.nav.sosialhjelp.soknad.db.repositories.soknadmetadata
+
+import no.nav.sosialhjelp.soknad.business.db.RepositoryTestSupport
+import no.nav.sosialhjelp.soknad.business.db.repositories.soknadmetadata.SoknadMetadataRepository
+import no.nav.sosialhjelp.soknad.business.domain.SoknadMetadata
+import no.nav.sosialhjelp.soknad.config.DbTestConfig
+import no.nav.sosialhjelp.soknad.domain.SoknadMetadataInnsendingStatus
+import no.nav.sosialhjelp.soknad.domain.SoknadMetadataInnsendingStatus.AVBRUTT_AUTOMATISK
+import no.nav.sosialhjelp.soknad.domain.SoknadMetadataInnsendingStatus.AVBRUTT_AV_BRUKER
+import no.nav.sosialhjelp.soknad.domain.SoknadMetadataInnsendingStatus.FERDIG
+import no.nav.sosialhjelp.soknad.domain.SoknadMetadataInnsendingStatus.UNDER_ARBEID
+import no.nav.sosialhjelp.soknad.domain.model.kravdialoginformasjon.SoknadType
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import java.time.LocalDateTime
+import javax.inject.Inject
+
+@ExtendWith(SpringExtension::class)
+@ContextConfiguration(classes = [DbTestConfig::class])
+@ActiveProfiles("test")
+internal class BatchSoknadMetadataRepositoryJdbcTest {
+    private val dagerGammelSoknad = 20
+    private val behandlingsId = "1100AAAAA"
+
+    @Inject
+    private val batchSoknadMetadataRepository: BatchSoknadMetadataRepository? = null
+
+    @Inject
+    private val soknadMetadataRepository: SoknadMetadataRepository? = null
+
+    @Inject
+    private val support: RepositoryTestSupport? = null
+
+    @AfterEach
+    fun teardown() {
+        support!!.jdbcTemplate.update("DELETE FROM soknadmetadata WHERE behandlingsid = ?", behandlingsId)
+    }
+
+    @Test
+    fun hentForBatchSkalIkkeReturnereFerdige() {
+        opprettSoknadMetadata(soknadMetadata(behandlingsId, FERDIG, dagerGammelSoknad))
+        assertThat(batchSoknadMetadataRepository!!.hentForBatch(dagerGammelSoknad - 1)).isNotPresent
+    }
+
+    @Test
+    fun hentForBatchSkalIkkeReturnereAvbruttAutomatisk() {
+        opprettSoknadMetadata(soknadMetadata(behandlingsId, AVBRUTT_AUTOMATISK, dagerGammelSoknad))
+        assertThat(batchSoknadMetadataRepository!!.hentForBatch(dagerGammelSoknad - 1)).isNotPresent
+    }
+
+    @Test
+    fun hentForBatchSkalIkkeReturnereAvbruttAvBruker() {
+        opprettSoknadMetadata(soknadMetadata(behandlingsId, AVBRUTT_AV_BRUKER, dagerGammelSoknad))
+        assertThat(batchSoknadMetadataRepository!!.hentForBatch(dagerGammelSoknad - 1)).isNotPresent
+    }
+
+    @Test
+    fun hentForBatchBrukerEndringstidspunkt() {
+        opprettSoknadMetadata(soknadMetadata(behandlingsId, UNDER_ARBEID, dagerGammelSoknad))
+        assertThat(batchSoknadMetadataRepository!!.hentForBatch(dagerGammelSoknad - 1)).isPresent
+        assertThat(batchSoknadMetadataRepository.hentForBatch(dagerGammelSoknad + 1)).isNotPresent
+    }
+
+    @Test
+    fun hentEldreEnnBrukerEndringstidspunktUavhengigAvStatus() {
+        for (status in listOf(UNDER_ARBEID, FERDIG, AVBRUTT_AUTOMATISK, AVBRUTT_AV_BRUKER)) {
+            opprettSoknadMetadata(soknadMetadata(behandlingsId, status, dagerGammelSoknad))
+            assertThat(batchSoknadMetadataRepository!!.hentEldreEnn(dagerGammelSoknad - 1)).isPresent
+            assertThat(batchSoknadMetadataRepository.hentEldreEnn(dagerGammelSoknad + 1)).isNotPresent
+            batchSoknadMetadataRepository.slettSoknadMetaData(behandlingsId)
+        }
+    }
+
+    private fun opprettSoknadMetadata(soknadMetadata: SoknadMetadata) {
+        soknadMetadataRepository!!.opprett(soknadMetadata)
+        val lagretSoknadMetadata = soknadMetadataRepository.hent(soknadMetadata.behandlingsId)
+        batchSoknadMetadataRepository!!.leggTilbakeBatch(lagretSoknadMetadata!!.id)
+    }
+
+    private fun soknadMetadata(
+        behandlingsId: String,
+        status: SoknadMetadataInnsendingStatus,
+        dagerSiden: Int,
+    ): SoknadMetadata {
+        val meta = SoknadMetadata()
+        meta.id = soknadMetadataRepository!!.hentNesteId()
+        meta.behandlingsId = behandlingsId
+        meta.fnr = EIER
+        meta.type = SoknadType.SEND_SOKNAD_KOMMUNAL
+        meta.skjema = ""
+        meta.status = status
+        meta.innsendtDato = LocalDateTime.now().minusDays(dagerSiden.toLong())
+        meta.opprettetDato = LocalDateTime.now().minusDays(dagerSiden.toLong())
+        meta.sistEndretDato = LocalDateTime.now().minusDays(dagerSiden.toLong())
+        meta.lestDittNav = false
+        return meta
+    }
+
+    companion object {
+        private const val EIER = "11111111111"
+    }
+}
