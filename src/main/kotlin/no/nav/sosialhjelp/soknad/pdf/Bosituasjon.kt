7f2aee0f7ee9deb61beb6116b24ca4912fbb22fe
martintveter <41987225+martintveter@users.noreply.github.com>
2022-03-04 16:33:53 +0100

    Pdf-refactor pt 3 (#1155)
    `leggTilBosituasjon` til kotlin

---
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/pdf/Bosituasjon.kt
@@ -0,0 +1,56 @@
+package no.nav.sosialhjelp.soknad.pdf
+
+import no.nav.sbl.soknadsosialhjelp.soknad.bosituasjon.JsonBosituasjon
+import no.nav.sosialhjelp.soknad.business.pdfmedpdfbox.PdfGenerator
+
+fun leggTilBosituasjon(
+    pdf: PdfGenerator,
+    pdfUtils: PdfUtils,
+    bosituasjon: JsonBosituasjon?,
+    utvidetSoknad: Boolean
+) {
+    pdf.skrivH4Bold(pdfUtils.getTekst("bosituasjonbolk.tittel"))
+    pdf.addBlankLine()
+
+    if (bosituasjon != null) {
+        pdf.skrivTekstBold(pdfUtils.getTekst("bosituasjon.sporsmal"))
+        val botype = bosituasjon.botype
+        if (botype != null) {
+            val tekst = pdfUtils.getTekst("bosituasjon." + botype.value())
+            if (tekst.isNullOrEmpty()) {
+                pdf.skrivTekst(pdfUtils.getTekst("bosituasjon.annet.botype." + botype.value()))
+            } else {
+                pdf.skrivTekst(tekst)
+            }
+        } else {
+            pdfUtils.skrivIkkeUtfylt(pdf)
+        }
+        pdf.addBlankLine()
+
+        if (utvidetSoknad) {
+            val svaralternativer: MutableList<String> = ArrayList(5)
+            svaralternativer.add("bosituasjon.eier")
+            svaralternativer.add("bosituasjon.kommunal")
+            svaralternativer.add("bosituasjon.leier")
+            svaralternativer.add("bosituasjon.ingen")
+            svaralternativer.add("bosituasjon.annet")
+            pdfUtils.skrivSvaralternativer(pdf, svaralternativer)
+
+            pdf.skrivTekstBold(pdfUtils.getTekst("bosituasjon.annet"))
+            val andreAlternativer: MutableList<String> = ArrayList()
+            andreAlternativer.add("bosituasjon.annet.botype.foreldre")
+            andreAlternativer.add("bosituasjon.annet.botype.familie")
+            andreAlternativer.add("bosituasjon.annet.botype.venner")
+            andreAlternativer.add("bosituasjon.annet.botype.institusjon")
+            andreAlternativer.add("bosituasjon.annet.botype.fengsel")
+            andreAlternativer.add("bosituasjon.annet.botype.krisesenter")
+            pdfUtils.skrivSvaralternativer(pdf, andreAlternativer)
+        }
+
+        pdf.skrivTekstBold(pdfUtils.getTekst("bosituasjon.antallpersoner.sporsmal"))
+        bosituasjon.antallPersoner
+            ?.let { pdf.skrivTekst(it.toString()) }
+            ?: pdfUtils.skrivIkkeUtfylt(pdf)
+    }
+    pdf.addBlankLine()
+}
