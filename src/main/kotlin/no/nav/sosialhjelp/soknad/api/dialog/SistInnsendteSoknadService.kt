aef729878898b2d15ab1011f8ed42323582a609c
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-03-14 14:27:32 +0100

    rydder i !!

--- src/main/kotlin/no/nav/sosialhjelp/soknad/api/dialog/SistInnsendteSoknadService.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/api/dialog/SistInnsendteSoknadService.kt
@@ -2,21 +2,21 @@ package no.nav.sosialhjelp.soknad.api.dialog
 
 import no.nav.sosialhjelp.soknad.api.dialog.dto.SistInnsendteSoknadDto
 import no.nav.sosialhjelp.soknad.db.repositories.soknadmetadata.SoknadMetadataRepository
+import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 class SistInnsendteSoknadService(
     private val soknadMetadataRepository: SoknadMetadataRepository
 ) {
     fun hentSistInnsendteSoknad(fnr: String): SistInnsendteSoknadDto? {
-        return soknadMetadataRepository.hentAlleInnsendteSoknaderForBruker(fnr)
-            .filter { it.innsendtDato != null }
-            .maxByOrNull { it.innsendtDato!! }
-            ?.let {
-                SistInnsendteSoknadDto(
-                    it.fnr,
-                    it.navEnhet!!,
-                    it.innsendtDato!!.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
-                )
-            }
+        val sistInnsendteSoknad = soknadMetadataRepository.hentAlleInnsendteSoknaderForBruker(fnr)
+            .maxByOrNull { it.innsendtDato ?: LocalDateTime.MIN }
+        val navEnhet = sistInnsendteSoknad?.navEnhet ?: return null
+        val innsendtDato = sistInnsendteSoknad.innsendtDato ?: return null
+        return SistInnsendteSoknadDto(
+            sistInnsendteSoknad.fnr,
+            navEnhet,
+            innsendtDato.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+        )
     }
 }
