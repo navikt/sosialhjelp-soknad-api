434b54b5cb53a4cb614b0a1e2df28e2d7e641d2a
martintveter <41987225+martintveter@users.noreply.github.com>
2022-03-15 16:49:30 +0100

    bump token-validation deps fra 2.0.5 til 2.0.11 (#1184)
    * token-validation-test-support er slettet som modul (hadde vært deprecated lenge). Erstattet av token-validation-spring-test
    * Rydder opp i config for integrasjonstestene, til å ta i bruk MockOAuth2Server
    * oppgraderer spring fra 5.3.15 til 5.3.16
    * oppgraderer netty fra 4.1.72,Final til 4.1.74.Final

--- src/test/kotlin/no/nav/sosialhjelp/soknad/integrationtest/MineSakerMetadataRessursEndpointIT.kt
+++ src/test/kotlin/no/nav/sosialhjelp/soknad/integrationtest/MineSakerMetadataRessursEndpointIT.kt
@@ -1,10 +1,11 @@
 package no.nav.sosialhjelp.soknad.integrationtest
 
+import no.nav.security.mock.oauth2.MockOAuth2Server
 import no.nav.sosialhjelp.client.utils.Constants.BEARER
 import no.nav.sosialhjelp.soknad.Application
+import no.nav.sosialhjelp.soknad.common.Constants.SELVBETJENING
+import no.nav.sosialhjelp.soknad.integrationtest.IntegrationTestUtils.issueToken
 import no.nav.sosialhjelp.soknad.integrationtest.IntegrationTestUtils.opprettSoknad
-import no.nav.sosialhjelp.soknad.integrationtest.oidc.JwtTokenGenerator
-import no.nav.sosialhjelp.soknad.integrationtest.oidc.OidcConfig
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -14,16 +15,22 @@ import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.reactive.server.WebTestClient
 
-@ContextConfiguration(classes = [OidcConfig::class, PdlIntegrationTestConfig::class])
+@ContextConfiguration(classes = [IntegrationTestConfig::class, PdlIntegrationTestConfig::class])
 @SpringBootTest(classes = [Application::class], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles(profiles = ["no-redis", "test"])
 class MineSakerMetadataRessursEndpointIT {
 
     val BRUKER = "11111111111"
 
+    @Autowired
+    private lateinit var mockOAuth2Server: MockOAuth2Server
+
+    @Autowired
+    private lateinit var webClient: WebTestClient
+
     @Test
-    internal fun innsendte_skalGi401UtenToken(@Autowired webClient: WebTestClient) {
-        opprettSoknad(BRUKER, webClient)
+    internal fun innsendte_skalGi401UtenToken() {
+        opprettSoknad(issueToken(mockOAuth2Server, BRUKER), webClient)
 
         webClient
             .get().uri("/minesaker/innsendte")
@@ -33,13 +40,14 @@ class MineSakerMetadataRessursEndpointIT {
     }
 
     @Test
-    internal fun innsendte_skalGi401MedAnnenIssuer(@Autowired webClient: WebTestClient) {
-        opprettSoknad(BRUKER, webClient)
+    internal fun innsendte_skalGi401MedAnnenIssuer() {
+        opprettSoknad(issueToken(mockOAuth2Server, BRUKER), webClient)
 
+        // Skal kun godta tokenx som issuer
         webClient
             .get().uri("/minesaker/innsendte")
             .accept(MediaType.APPLICATION_JSON)
-            .header(HttpHeaders.AUTHORIZATION, BEARER + JwtTokenGenerator.createSignedJWT(BRUKER).serialize())
+            .header(HttpHeaders.AUTHORIZATION, BEARER + issueToken(mockOAuth2Server, BRUKER, issuer = SELVBETJENING).serialize())
             .exchange()
             .expectStatus().isUnauthorized
     }
