4f0b8613d5217baa4f8a35710f334c42bf8b975e
martintveter <41987225+martintveter@users.noreply.github.com>
2022-03-24 20:18:31 +0100

    fjern unødvendig IN_MEMORY_DATABASE env variabel. (#1202)
    fjern ubrukt metode i MiljoUtils

--- src/main/kotlin/no/nav/sosialhjelp/soknad/common/MiljoUtils.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/common/MiljoUtils.kt
@@ -1,30 +1,15 @@
 package no.nav.sosialhjelp.soknad.common
 
-import no.nav.sosialhjelp.kotlin.utils.logger
-
 object MiljoUtils {
 
-    private const val NAIS_APP_NAME = "NAIS_APP_NAME"
     private const val NAIS_CLUSTER_NAME = "NAIS_CLUSTER_NAME"
     private const val ENVIRONMENT_NAME = "ENVIRONMENT_NAME"
-
     private const val IS_ALLTID_SEND_TIL_NAV_TESTKOMMUNE = "IS_ALLTID_SEND_TIL_NAV_TESTKOMMUNE"
     private const val IS_ALLTID_HENT_KOMMUNEINFO_FRA_NAV_TESTKOMMUNE = "IS_ALLTID_HENT_KOMMUNEINFO_FRA_NAV_TESTKOMMUNE"
     private const val SPRING_PROFILES_ACTIVE = "SPRING_PROFILES_ACTIVE"
-    private const val IN_MEMORY_DATABASE = "IN_MEMORY_DATABASE"
-
-    private val log by logger()
-
-    val naisAppName: String
-        get() = getenv(NAIS_APP_NAME, "sosialhjelp-soknad-api")
 
     val environmentName: String
         get() = System.getenv(ENVIRONMENT_NAME) ?: ""
-
-    private fun getenv(env: String, defaultValue: String): String {
-        return System.getenv(env)
-            ?: (defaultValue.also { log.warn("Fant ikke env variabel ($env), bruker default verdi: ($defaultValue)") })
-    }
 
     fun isNonProduction(): Boolean {
         // Bruk isNonProduction() -sjekk fremfor å sjekke om miljø configurert som prod. På denne måten er default-configurasjon vår alltid prodlik.
@@ -47,10 +32,5 @@ object MiljoUtils {
     fun isMockAltProfil(): Boolean {
         val value = System.getenv(SPRING_PROFILES_ACTIVE) ?: ""
         return value.contains("mock-alt")
-    }
-
-    fun isRunningWithInMemoryDb(): Boolean {
-        val value = System.getenv(IN_MEMORY_DATABASE) ?: "false"
-        return value.toBoolean()
     }
 }
