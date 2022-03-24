a34453157800cbc940ad8513c56aa0787766223d
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-03-24 08:22:38 +0100

    gcp start: - flyway-script for dagens tabeller i postgres. - mock-alt og integrasjonstester bruker postgres-skjema i flyway og inmemory hsqldb med postgres syntax - profile `repositoryTest` brukes for repository-testene. Disse bruker p.t fortsatt DbTestConfig

--- src/test/kotlin/no/nav/sosialhjelp/soknad/db/repositories/opplastetvedlegg/OpplastetVedleggRepositoryJdbcTest.kt
+++ src/test/kotlin/no/nav/sosialhjelp/soknad/db/repositories/opplastetvedlegg/OpplastetVedleggRepositoryJdbcTest.kt
@@ -14,7 +14,7 @@ import javax.inject.Inject
 
 @ExtendWith(SpringExtension::class)
 @ContextConfiguration(classes = [DbTestConfig::class])
-@ActiveProfiles("test")
+@ActiveProfiles("repositoryTest")
 internal class OpplastetVedleggRepositoryJdbcTest {
 
     private val EIER = "12345678901"
