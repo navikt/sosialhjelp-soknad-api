7491e60da70d563402020e1dd4ce88feacb01348
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-03-24 08:22:38 +0100

    gcp start: - flyway-script for dagens tabeller i postgres. - mock-alt og integrasjonstester bruker postgres-skjema i flyway og inmemory hsqldb med postgres syntax - profile `repositoryTest` brukes for repository-testene. Disse bruker p.t fortsatt DbTestConfig

--- src/main/kotlin/no/nav/sosialhjelp/soknad/config/SoknadInnsendingDBConfig.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/config/SoknadInnsendingDBConfig.kt
@@ -11,7 +11,7 @@ import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.transaction.annotation.EnableTransactionManagement
 import javax.sql.DataSource
 
-@Profile("!(mock-alt | test)")
+@Profile("!repositoryTest")
 @Configuration
 @EnableTransactionManagement
 open class SoknadInnsendingDBConfig(
