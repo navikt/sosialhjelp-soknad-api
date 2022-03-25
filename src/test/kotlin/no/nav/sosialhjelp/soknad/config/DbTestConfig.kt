f80bcb95b42647efa9e6ab9ed2be249e0fa45fd6
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-03-25 10:12:56 +0100

    fix imports

--- src/test/kotlin/no/nav/sosialhjelp/soknad/config/DbTestConfig.kt
+++ src/test/kotlin/no/nav/sosialhjelp/soknad/config/DbTestConfig.kt
@@ -20,10 +20,9 @@ import no.nav.sosialhjelp.soknad.db.repositories.soknadunderarbeid.SoknadUnderAr
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
+import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.jdbc.datasource.DataSourceTransactionManager
 import org.springframework.jdbc.datasource.DriverManagerDataSource
-import org.springframework.context.annotation.Import
-import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.transaction.annotation.EnableTransactionManagement
 import org.springframework.transaction.support.TransactionTemplate
 import java.sql.SQLException
