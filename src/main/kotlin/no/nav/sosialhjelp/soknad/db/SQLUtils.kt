5079123d3bba9745df89bca165eb6e1b567905ea
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-03-30 12:52:28 +0200

    postgres syntax

--- src/main/kotlin/no/nav/sosialhjelp/soknad/db/SQLUtils.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/db/SQLUtils.kt
@@ -6,21 +6,21 @@ import java.time.LocalDateTime
 object SQLUtils {
 
     private const val HSQLDB = "hsqldb"
+    private const val POSTGRESQL = "postgresql"
     const val DIALECT_PROPERTY = "sqldialect"
 
     fun limit(limit: Int): String {
-        return if (HSQLDB == System.getProperty(DIALECT_PROPERTY)) {
-            "limit $limit"
-        } else {
-            "and rownum <= $limit"
+        return when (System.getProperty(DIALECT_PROPERTY)) {
+            HSQLDB, POSTGRESQL -> "limit $limit"
+            else -> "and rownum <= $limit"
         }
     }
 
     fun selectNextSequenceValue(sequence: String): String {
-        return if (HSQLDB == System.getProperty(DIALECT_PROPERTY)) {
-            "call next value for $sequence"
-        } else {
-            "select $sequence.nextval from dual"
+        return when (System.getProperty(DIALECT_PROPERTY)) {
+            HSQLDB -> "call next value for $sequence"
+            POSTGRESQL -> "select nextval('$sequence')"
+            else -> "select $sequence.nextval from dual"
         }
     }
 
@@ -37,10 +37,9 @@ object SQLUtils {
     }
 
     fun blobSizeQuery(): String {
-        return if (HSQLDB == System.getProperty(DIALECT_PROPERTY)) {
-            "OCTET_LENGTH(DATA)"
-        } else {
-            "dbms_lob.getLength(DATA)"
+        return when(System.getProperty(DIALECT_PROPERTY)) {
+            HSQLDB, POSTGRESQL -> "OCTET_LENGTH(DATA)"
+            else -> "dbms_lob.getLength(DATA)"
         }
     }
 }
