f5023e2e695dc31e084ee6d35abe5e24da07d1a0
martintveter <41987225+martintveter@users.noreply.github.com>
2022-04-07 09:52:42 +0200

    DTOer for migrering. (#1221)
    Basert på entitetklassene, men er immutable og uten xml-annotasjoner. Gjenbruker enums fra entitetklassene
    
    ReplicationDto er knyttet til en behandlingsId, og har referanser til
    - SoknadMetadata (1-1)
    - SendtSoknad (0-1)
    - Oppgave (0-1)
    - SoknadUnderArbeid (0-1). SoknadUnderArbeid har referanse til OpplastetVedlegg (0-n)

---
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/migration/dto/OpplastetVedleggDto.kt
@@ -0,0 +1,41 @@
+package no.nav.sosialhjelp.soknad.migration.dto
+
+import no.nav.sosialhjelp.soknad.db.repositories.opplastetvedlegg.OpplastetVedleggType
+
+data class OpplastetVedleggDto(
+    val uuid: String,
+    val eier: String,
+    val vedleggType: OpplastetVedleggType,
+    val data: ByteArray,
+    val soknadId: Long,
+    val filnavn: String,
+    val sha512: String
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as OpplastetVedleggDto
+
+        if (uuid != other.uuid) return false
+        if (eier != other.eier) return false
+        if (vedleggType != other.vedleggType) return false
+        if (!data.contentEquals(other.data)) return false
+        if (soknadId != other.soknadId) return false
+        if (filnavn != other.filnavn) return false
+        if (sha512 != other.sha512) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = uuid.hashCode()
+        result = 31 * result + eier.hashCode()
+        result = 31 * result + vedleggType.hashCode()
+        result = 31 * result + data.contentHashCode()
+        result = 31 * result + soknadId.hashCode()
+        result = 31 * result + filnavn.hashCode()
+        result = 31 * result + sha512.hashCode()
+        return result
+    }
+}
