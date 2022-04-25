9fb671993e1645d0b780337cedd3f8a693dd51d9
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-04-25 12:26:37 +0200

    kommenter ut `@Controller`

--- src/main/kotlin/no/nav/sosialhjelp/soknad/migration/MigrationFeedRessurs.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/migration/MigrationFeedRessurs.kt
@@ -4,7 +4,7 @@ import no.nav.security.token.support.core.api.ProtectedWithClaims
 import no.nav.sosialhjelp.metrics.aspects.Timed
 import no.nav.sosialhjelp.soknad.common.Constants
 import no.nav.sosialhjelp.soknad.migration.dto.ReplicationDto
-import org.springframework.stereotype.Controller
+// import org.springframework.stereotype.Controller
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME
 import javax.ws.rs.GET
@@ -13,7 +13,7 @@ import javax.ws.rs.Produces
 import javax.ws.rs.QueryParam
 import javax.ws.rs.core.MediaType
 
-@Controller
+// @Controller
 @ProtectedWithClaims(issuer = Constants.TOKENX, claimMap = [Constants.CLAIM_ACR_LEVEL_4])
 @Path("/internal/migration")
 @Produces(MediaType.APPLICATION_JSON)
