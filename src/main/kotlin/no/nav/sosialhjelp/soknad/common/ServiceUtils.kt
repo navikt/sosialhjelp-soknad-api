65779ce84d39cefff4e7d7719c92bce39d8b97e9
martintveter <41987225+martintveter@users.noreply.github.com>
2022-04-26 08:44:35 +0200

    DIGISOS-2012: flytter "innsendig-digisosapi-enabled" feature toggle til unleash. toggle er enabled i alle miljø (#1241)

--- src/main/kotlin/no/nav/sosialhjelp/soknad/common/ServiceUtils.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/common/ServiceUtils.kt
@@ -1,19 +1,12 @@
 package no.nav.sosialhjelp.soknad.common
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.core.env.Environment
 import org.springframework.stereotype.Component
 
 @Component
 open class ServiceUtils(
-    @Value("\${digisosapi.sending.enable}") private val digisosSendingEnabled: Boolean,
     private val env: Environment
 ) {
-
-    open fun isSendingTilFiksEnabled(): Boolean {
-        return digisosSendingEnabled
-    }
-
     open fun isMockAltProfil(): Boolean {
         return env.activeProfiles.any { it.equals("mock-alt") || it.equals("test") }
     }
--- src/main/kotlin/no/nav/sosialhjelp/soknad/innsending/SenderUtils.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/innsending/SenderUtils.kt
@@ -6,4 +6,6 @@ object SenderUtils {
     fun createPrefixedBehandlingsId(behandlingsId: String?): String {
         return "${MiljoUtils.environmentName}-$behandlingsId"
     }
+
+    const val INNSENDING_DIGISOSAPI_ENABLED = "sosialhjelp.soknad.innsending-digisosapi-enabled"
 }
