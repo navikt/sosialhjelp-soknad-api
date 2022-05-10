cd63037b83b248f91460d476d2196ae466d4d894
martintveter <41987225+martintveter@users.noreply.github.com>
2022-05-10 16:28:02 +0200

    Vedlegg-endepunkter avgjør om mellomlagringstjeneste skal kalles eller ikke (#1262)
    * Skall for MellomlagringService (skal kalle en klient etter hvert)
    * OpplastetVedleggRessurs.kt avgjør om MellomlagringService.kt eller OpplastetVedleggService skal kalles i de 3 aktuelle endepunktene (`GET` hent vedlegg, `POST` last opp vedlegg, og `DELETE` slett vedlegg)

--- src/main/kotlin/no/nav/sosialhjelp/soknad/vedlegg/OpplastetVedleggRessurs.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/vedlegg/OpplastetVedleggRessurs.kt
@@ -1,15 +1,27 @@
 package no.nav.sosialhjelp.soknad.vedlegg
 
+import no.finn.unleash.Unleash
 import no.nav.security.token.support.core.api.ProtectedWithClaims
+import no.nav.sosialhjelp.kotlin.utils.logger
 import no.nav.sosialhjelp.metrics.aspects.Timed
 import no.nav.sosialhjelp.soknad.common.Constants
+import no.nav.sosialhjelp.soknad.common.exceptions.SendingTilKommuneErMidlertidigUtilgjengeligException
+import no.nav.sosialhjelp.soknad.common.exceptions.SendingTilKommuneUtilgjengeligException
 import no.nav.sosialhjelp.soknad.common.filedetection.FileDetectionUtils.getMimeType
 import no.nav.sosialhjelp.soknad.common.filedetection.MimeTypes
 import no.nav.sosialhjelp.soknad.common.subjecthandler.SubjectHandlerUtils
 import no.nav.sosialhjelp.soknad.db.repositories.opplastetvedlegg.OpplastetVedleggRepository
+import no.nav.sosialhjelp.soknad.db.repositories.soknadunderarbeid.SoknadUnderArbeidRepository
+import no.nav.sosialhjelp.soknad.innsending.digisosapi.kommuneinfo.KommuneInfoService
+import no.nav.sosialhjelp.soknad.innsending.digisosapi.kommuneinfo.KommuneStatus.FIKS_NEDETID_OG_TOM_CACHE
+import no.nav.sosialhjelp.soknad.innsending.digisosapi.kommuneinfo.KommuneStatus.HAR_KONFIGURASJON_MEN_SKAL_SENDE_VIA_SVARUT
+import no.nav.sosialhjelp.soknad.innsending.digisosapi.kommuneinfo.KommuneStatus.MANGLER_KONFIGURASJON
+import no.nav.sosialhjelp.soknad.innsending.digisosapi.kommuneinfo.KommuneStatus.SKAL_SENDE_SOKNADER_OG_ETTERSENDELSER_VIA_FDA
+import no.nav.sosialhjelp.soknad.innsending.digisosapi.kommuneinfo.KommuneStatus.SKAL_VISE_MIDLERTIDIG_FEILSIDE_FOR_SOKNAD_OG_ETTERSENDELSER
 import no.nav.sosialhjelp.soknad.tilgangskontroll.Tilgangskontroll
 import no.nav.sosialhjelp.soknad.vedlegg.dto.FilFrontend
 import no.nav.sosialhjelp.soknad.vedlegg.exceptions.OpplastingException
+import no.nav.sosialhjelp.soknad.vedlegg.fiks.MellomlagringService
 import org.apache.commons.io.IOUtils
 import org.glassfish.jersey.media.multipart.FormDataBodyPart
 import org.glassfish.jersey.media.multipart.FormDataParam
@@ -37,24 +49,40 @@ import javax.ws.rs.core.Response
 open class OpplastetVedleggRessurs(
     private val opplastetVedleggRepository: OpplastetVedleggRepository,
     private val opplastetVedleggService: OpplastetVedleggService,
-    private val tilgangskontroll: Tilgangskontroll
+    private val tilgangskontroll: Tilgangskontroll,
+    private val soknadUnderArbeidRepository: SoknadUnderArbeidRepository,
+    private val kommuneInfoService: KommuneInfoService,
+    private val mellomlagringService: MellomlagringService,
+    private val unleash: Unleash
 ) {
 
     @GET
     @Path("/{vedleggId}/fil")
     @Produces(MediaType.APPLICATION_JSON)
-    open fun getVedleggFil(@PathParam("vedleggId") vedleggId: String, @Context response: HttpServletResponse): Response {
+    open fun getVedleggFil(
+        @PathParam("vedleggId") vedleggId: String,
+        @Context response: HttpServletResponse
+    ): Response {
         tilgangskontroll.verifiserAtBrukerHarTilgang()
         val eier = SubjectHandlerUtils.getUserIdFromToken()
-        val opplastetVedlegg = opplastetVedleggRepository.hentVedlegg(vedleggId, eier)
-        if (opplastetVedlegg != null) {
-            response.setHeader("Content-Disposition", "attachment; filename=\"" + opplastetVedlegg.filnavn + "\"")
-        } else {
-            return Response.noContent().build()
+
+        opplastetVedleggRepository.hentVedlegg(vedleggId, eier)?.let {
+            response.setHeader("Content-Disposition", "attachment; filename=\"" + it.filnavn + "\"")
+            val detectedMimeType = getMimeType(it.data)
+            val mimetype = if (detectedMimeType.equals(MimeTypes.TEXT_X_MATLAB, ignoreCase = true)) MimeTypes.APPLICATION_PDF else detectedMimeType
+            return Response.ok(it.data).type(mimetype).build()
         }
-        val detectedMimeType = getMimeType(opplastetVedlegg.data)
-        val mimetype = if (detectedMimeType.equals(MimeTypes.TEXT_X_MATLAB, ignoreCase = true)) MimeTypes.APPLICATION_PDF else detectedMimeType
-        return Response.ok(opplastetVedlegg.data).type(mimetype).build()
+
+        if (mellomlagringEnabled) {
+            mellomlagringService.getVedlegg(vedleggId, "token")?.let {
+                response.setHeader("Content-Disposition", "attachment; filename=\"" + it.filnavn + "\"")
+                val detectedMimeType = getMimeType(it.data)
+                val mimetype = if (detectedMimeType.equals(MimeTypes.TEXT_X_MATLAB, ignoreCase = true)) MimeTypes.APPLICATION_PDF else detectedMimeType
+                return Response.ok(it.data).type(mimetype).build()
+            }
+        }
+        // hvis vedleggId ikke finnes i DB eller KS mellomlagring
+        return Response.noContent().build()
     }
 
     @POST
@@ -76,23 +104,63 @@ open class OpplastetVedleggRessurs(
         }
         val filnavn = fil.contentDisposition.fileName
         val data = getByteArray(fil)
-        opplastetVedleggService.sjekkOmSoknadUnderArbeidTotalVedleggStorrelseOverskriderMaksgrense(behandlingsId, data)
-        val opplastetVedlegg =
-            opplastetVedleggService.saveVedleggAndUpdateVedleggstatus(behandlingsId, vedleggstype, data, filnavn)
-        return FilFrontend(opplastetVedlegg.filnavn, opplastetVedlegg.uuid)
+        val eier = SubjectHandlerUtils.getUserIdFromToken()
+
+        // bruk KS mellomlagringstjeneste hvis featuren er enablet og søknad skal sendes med DigisosApi
+        return if (soknadSkalSendesMedDigisosApi(behandlingsId, eier) && mellomlagringEnabled) {
+            val mellomlagretVedlegg = mellomlagringService.uploadVedlegg(behandlingsId, vedleggstype, data, filnavn, "token")
+            FilFrontend(mellomlagretVedlegg.filnavn, mellomlagretVedlegg.filId)
+        } else {
+            opplastetVedleggService.sjekkOmSoknadUnderArbeidTotalVedleggStorrelseOverskriderMaksgrense(behandlingsId, data)
+            val opplastetVedlegg = opplastetVedleggService.saveVedleggAndUpdateVedleggstatus(behandlingsId, vedleggstype, data, filnavn)
+            FilFrontend(opplastetVedlegg.filnavn, opplastetVedlegg.uuid)
+        }
     }
 
     @DELETE
     @Path("/{behandlingsId}/{vedleggId}")
-    open fun deleteVedlegg(@PathParam("behandlingsId") behandlingsId: String, @PathParam("vedleggId") vedleggId: String) {
+    open fun deleteVedlegg(
+        @PathParam("behandlingsId") behandlingsId: String,
+        @PathParam("vedleggId") vedleggId: String,
+    ) {
         tilgangskontroll.verifiserAtBrukerKanEndreSoknad(behandlingsId)
-        opplastetVedleggService.deleteVedleggAndUpdateVedleggstatus(behandlingsId, vedleggId)
+        val eier = SubjectHandlerUtils.getUserIdFromToken()
+        opplastetVedleggRepository.hentVedlegg(vedleggId, eier)?.let {
+            opplastetVedleggService.deleteVedleggAndUpdateVedleggstatus(behandlingsId, vedleggId)
+            return
+        }
+        // forsøk sletting via KS mellomlagringstjeneste hvis feature er enablet og sletting fra DB ikke ble utført
+        if (mellomlagringEnabled) {
+            log.info("Sletter vedlegg $vedleggId fra KS mellomlagring")
+            mellomlagringService.deleteVedleggAndUpdateVedleggstatus(behandlingsId, vedleggId, "token")
+        }
+    }
+
+    private val mellomlagringEnabled get() = unleash.isEnabled(KS_MELLOMLAGRING_ENABLED, false)
+
+    private fun soknadSkalSendesMedDigisosApi(behandlingsId: String, eier: String): Boolean {
+        val soknadUnderArbeid = soknadUnderArbeidRepository.hentSoknad(behandlingsId, eier)
+        val kommunenummer = soknadUnderArbeid.jsonInternalSoknad?.soknad?.mottaker?.kommunenummer
+            ?: throw IllegalStateException("Kommunenummer ikke funnet for JsonInternalSoknad.soknad.mottaker.kommunenummer")
+
+        return when (kommuneInfoService.kommuneInfo(kommunenummer)) {
+            FIKS_NEDETID_OG_TOM_CACHE -> {
+                throw SendingTilKommuneUtilgjengeligException("Mellomlagring av vedlegg er ikke tilgjengelig fordi fiks har nedetid og kommuneinfo-cache er tom.")
+            }
+            MANGLER_KONFIGURASJON, HAR_KONFIGURASJON_MEN_SKAL_SENDE_VIA_SVARUT -> false
+            SKAL_SENDE_SOKNADER_OG_ETTERSENDELSER_VIA_FDA -> true
+            SKAL_VISE_MIDLERTIDIG_FEILSIDE_FOR_SOKNAD_OG_ETTERSENDELSER -> {
+                throw SendingTilKommuneErMidlertidigUtilgjengeligException("Sending til kommune $kommunenummer er midlertidig utilgjengelig.")
+            }
+        }
     }
 
     companion object {
         const val KS_MELLOMLAGRING_ENABLED = "sosialhjelp.soknad.ks-mellomlagring-enabled"
 
         private const val MAKS_TOTAL_FILSTORRELSE = 1024 * 1024 * 10
+
+        private val log by logger()
 
         private fun getByteArray(file: FormDataBodyPart): ByteArray {
             return try {
---
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/vedlegg/fiks/MellomlagringService.kt
@@ -0,0 +1,55 @@
+package no.nav.sosialhjelp.soknad.vedlegg.fiks
+
+import org.springframework.stereotype.Component
+
+@Component
+class MellomlagringService {
+
+    fun getVedlegg(vedleggId: String, token: String): MellomlagretVedlegg? {
+        // todo implement
+        return null
+    }
+
+    fun uploadVedlegg(
+        behandlingsId: String,
+        vedleggstype: String,
+        data: ByteArray,
+        originalfilnavn: String,
+        token: String
+    ): MellomlagretVedleggMetadata {
+        // todo implement
+        return MellomlagretVedleggMetadata(filnavn = "filnavn", filId = "uuid")
+    }
+
+    fun deleteVedleggAndUpdateVedleggstatus(behandlingsId: String, vedleggId: String, token: String) {
+        // todo implement
+    }
+}
+
+data class MellomlagretVedleggMetadata(
+    val filnavn: String,
+    val filId: String
+)
+
+data class MellomlagretVedlegg(
+    val filnavn: String,
+    val data: ByteArray
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as MellomlagretVedlegg
+
+        if (filnavn != other.filnavn) return false
+        if (!data.contentEquals(other.data)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = filnavn.hashCode()
+        result = 31 * result + data.contentHashCode()
+        return result
+    }
+}
