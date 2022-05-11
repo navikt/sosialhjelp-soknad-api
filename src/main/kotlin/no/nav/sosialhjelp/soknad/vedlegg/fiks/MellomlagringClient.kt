0dc4b7c63e53d0ee10554242dd1fdd037901651d
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-05-11 09:43:11 +0200

    wip - ks mellomlagring klient

---
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/vedlegg/fiks/MellomlagringClient.kt
@@ -0,0 +1,183 @@
+package no.nav.sosialhjelp.soknad.vedlegg.fiks
+
+import com.fasterxml.jackson.core.JsonProcessingException
+import no.ks.fiks.streaming.klient.FilForOpplasting
+import no.nav.sosialhjelp.api.fiks.exceptions.FiksException
+import no.nav.sosialhjelp.kotlin.utils.logger
+import no.nav.sosialhjelp.soknad.common.Constants.HEADER_INTEGRASJON_ID
+import no.nav.sosialhjelp.soknad.common.Constants.HEADER_INTEGRASJON_PASSORD
+import no.nav.sosialhjelp.soknad.innsending.digisosapi.DokumentlagerClient
+import no.nav.sosialhjelp.soknad.innsending.digisosapi.FiksServiceUnavailableRetryStrategy
+import no.nav.sosialhjelp.soknad.innsending.digisosapi.KrypteringService
+import no.nav.sosialhjelp.soknad.innsending.digisosapi.KrypteringService.Companion.waitForFutures
+import no.nav.sosialhjelp.soknad.innsending.digisosapi.Utils.digisosObjectMapper
+import no.nav.sosialhjelp.soknad.innsending.digisosapi.dto.FilOpplasting
+import org.apache.http.client.config.RequestConfig
+import org.apache.http.client.methods.HttpPost
+import org.apache.http.entity.ContentType
+import org.apache.http.entity.mime.HttpMultipartMode
+import org.apache.http.entity.mime.MultipartEntityBuilder
+import org.apache.http.impl.client.DefaultHttpRequestRetryHandler
+import org.apache.http.impl.client.HttpClientBuilder
+import org.apache.http.util.EntityUtils
+import org.eclipse.jetty.http.HttpHeader
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientResponseException
+import org.springframework.web.reactive.function.client.bodyToMono
+import java.io.IOException
+import java.nio.charset.StandardCharsets
+import java.util.Collections
+import java.util.concurrent.Future
+
+@Component
+class MellomlagringClient(
+    @Value("\${digisos_api_baseurl}") private val digisosApiEndpoint: String,
+    @Value("\${integrasjonsid_fiks}") private val integrasjonsidFiks: String,
+    @Value("\${integrasjonpassord_fiks}") private val integrasjonpassordFiks: String,
+    private val dokumentlagerClient: DokumentlagerClient,
+    private val krypteringService: KrypteringService,
+    proxiedWebClientBuilder: WebClient.Builder
+) {
+    private val webClient = proxiedWebClientBuilder
+        .baseUrl(digisosApiEndpoint)
+        .codecs {
+            it.defaultCodecs().maxInMemorySize(16 * 1024 * 1024)
+        }
+        .defaultHeader(HEADER_INTEGRASJON_ID, integrasjonsidFiks)
+        .defaultHeader(HEADER_INTEGRASJON_PASSORD, integrasjonpassordFiks)
+        .build()
+
+    private val retryHandler = DefaultHttpRequestRetryHandler()
+    private val serviceUnavailableRetryStrategy = FiksServiceUnavailableRetryStrategy()
+
+    private val requestConfig = RequestConfig.custom()
+        .setConnectTimeout(SENDING_TIL_FIKS_TIMEOUT)
+        .setConnectionRequestTimeout(SENDING_TIL_FIKS_TIMEOUT)
+        .setSocketTimeout(SENDING_TIL_FIKS_TIMEOUT)
+        .build()
+
+    private val clientBuilder = HttpClientBuilder.create()
+        .setRetryHandler(retryHandler)
+        .setServiceUnavailableRetryStrategy(serviceUnavailableRetryStrategy)
+        .useSystemProperties()
+        .setDefaultRequestConfig(requestConfig)
+
+    fun getMellomlagredeVedlegg(navEksternId: String, token: String): MellomlagringDto {
+        return webClient.get()
+            .uri(MELLOMLAGRING_PATH, navEksternId)
+            .accept(MediaType.APPLICATION_JSON)
+            .header(HttpHeaders.AUTHORIZATION, token)
+            .retrieve()
+            .bodyToMono<MellomlagringDto>()
+            .onErrorMap(WebClientResponseException::class.java) {
+                log.warn("Fiks - getMellomlagredeVedlegg feilet - ${it.responseBodyAsString}", it)
+                throw it
+            }
+            .block() ?: throw FiksException("MellomlagringDto er null?", null)
+    }
+
+    fun postVedlegg(navEksternId: String, filOpplasting: FilOpplasting, token: String) {
+        val filForOpplasting = krypter(filOpplasting)
+
+        val entitybuilder = MultipartEntityBuilder.create()
+        entitybuilder.setCharset(StandardCharsets.UTF_8)
+        entitybuilder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE)
+
+        entitybuilder.addTextBody("metadata", getJson(filForOpplasting))
+        entitybuilder.addBinaryBody(filForOpplasting.filnavn, filForOpplasting.data, ContentType.APPLICATION_OCTET_STREAM, filForOpplasting.filnavn)
+
+        try {
+            clientBuilder.build().use { client ->
+                val post = HttpPost("$digisosApiEndpoint/digisos/api/v1/mellomlagring/$navEksternId")
+//                post.setHeader("requestid", UUID.randomUUID().toString())
+                post.setHeader(HttpHeader.AUTHORIZATION.name, token)
+                post.setHeader(HEADER_INTEGRASJON_ID, integrasjonsidFiks)
+                post.setHeader(HEADER_INTEGRASJON_PASSORD, integrasjonpassordFiks)
+                post.entity = entitybuilder.build()
+
+                val startTime = System.currentTimeMillis()
+                val response = client.execute(post)
+                val endTime = System.currentTimeMillis()
+                if (response.statusLine.statusCode >= 300) {
+                    val errorResponse = EntityUtils.toString(response.entity)
+                    throw IllegalStateException(
+                        "Mellomlagring av vedlegg til søknad $navEksternId feilet etter ${endTime - startTime} ms med status ${response.statusLine.reasonPhrase} og response: $errorResponse"
+                    )
+                }
+            }
+        } catch (e: IOException) {
+            throw IllegalStateException("Mellomlagring av vedlegg til søknad $navEksternId feilet", e)
+        }
+    }
+
+    private fun krypter(filOpplasting: FilOpplasting): FilForOpplasting<Any> {
+        val krypteringFutureList = Collections.synchronizedList(ArrayList<Future<Void>>(1))
+        val filForOpplasting: FilForOpplasting<Any>
+        try {
+            val fiksX509Certificate = dokumentlagerClient.getDokumentlagerPublicKeyX509Certificate()
+
+            filForOpplasting = FilForOpplasting.builder<Any>()
+                .filnavn(filOpplasting.metadata.filnavn)
+                .metadata(filOpplasting.metadata)
+                .data(krypteringService.krypter(filOpplasting.data, krypteringFutureList, fiksX509Certificate))
+                .build()
+            waitForFutures(krypteringFutureList)
+        } finally {
+            krypteringFutureList
+                .filter { !it.isDone && !it.isCancelled }
+                .forEach { it.cancel(true) }
+        }
+        return filForOpplasting
+    }
+
+    fun deleteAllVedleggFor(navEksternId: String, token: String) {
+        // slett alle mellomlagrede dokument for søknad
+    }
+
+    fun getVedlegg(navEksternId: String, digisosDokumentId: String, token: String): ByteArray {
+        // last ned mellomlagret dokument
+        return webClient.get()
+            .uri(MELLOMLAGRING_DOKUMENT_PATH, navEksternId, digisosDokumentId)
+            .header(HttpHeaders.AUTHORIZATION, token)
+            .retrieve()
+            .bodyToMono<String>()
+            .block()
+            ?.let { digisosObjectMapper.writeValueAsBytes(it) }
+            ?: throw FiksException("Mellomlagret vedlegg er null?", null)
+    }
+
+    fun deleteVedlegg(navEksternId: String, digisosDokumentId: String, token: String) {
+        // slett mellomlagret dokument
+        webClient.delete()
+            .uri(MELLOMLAGRING_DOKUMENT_PATH, navEksternId, digisosDokumentId)
+            .header(HttpHeaders.AUTHORIZATION, token)
+            .retrieve()
+            .bodyToMono<String>()
+            .onErrorMap(WebClientResponseException::class.java) {
+                log.warn("Fiks - delete mellomlagretVedlegg feilet - ${it.responseBodyAsString}", it)
+                throw it
+            }
+            .block()
+    }
+
+    companion object {
+        private const val MELLOMLAGRING_PATH = "/digisos/api/v1/mellomlagring/{navEksternRefId}"
+        private const val MELLOMLAGRING_DOKUMENT_PATH = "/digisos/api/v1/mellomlagring/{navEksternRefId}/{digisosDokumentId}"
+
+        private const val SENDING_TIL_FIKS_TIMEOUT = 5 * 60 * 1000 // 5 minutter
+
+        private val log by logger()
+
+        private fun getJson(objectFilForOpplasting: FilForOpplasting<Any>): String? {
+            return try {
+                digisosObjectMapper.writeValueAsString(objectFilForOpplasting.metadata)
+            } catch (e: JsonProcessingException) {
+                throw IllegalStateException(e)
+            }
+        }
+    }
+}
--- src/main/kotlin/no/nav/sosialhjelp/soknad/vedlegg/fiks/MellomlagringService.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/vedlegg/fiks/MellomlagringService.kt
@@ -1,12 +1,32 @@
 package no.nav.sosialhjelp.soknad.vedlegg.fiks
 
+import no.nav.sbl.soknadsosialhjelp.vedlegg.JsonFiler
+import no.nav.sosialhjelp.soknad.common.filedetection.FileDetectionUtils
+import no.nav.sosialhjelp.soknad.common.filedetection.MimeTypes
+import no.nav.sosialhjelp.soknad.common.subjecthandler.SubjectHandlerUtils
+import no.nav.sosialhjelp.soknad.db.repositories.soknadmetadata.Vedleggstatus
+import no.nav.sosialhjelp.soknad.db.repositories.soknadunderarbeid.SoknadUnderArbeidRepository
+import no.nav.sosialhjelp.soknad.innsending.digisosapi.dto.FilMetadata
+import no.nav.sosialhjelp.soknad.innsending.digisosapi.dto.FilOpplasting
+import no.nav.sosialhjelp.soknad.vedlegg.VedleggUtils.finnVedleggEllerKastException
+import no.nav.sosialhjelp.soknad.vedlegg.VedleggUtils.getSha512FromByteArray
+import no.nav.sosialhjelp.soknad.vedlegg.VedleggUtils.lagFilnavn
+import no.nav.sosialhjelp.soknad.vedlegg.VedleggUtils.validerFil
+import no.nav.sosialhjelp.soknad.vedlegg.virusscan.VirusScanner
 import org.springframework.stereotype.Component
+import java.io.ByteArrayInputStream
+import java.util.UUID
 
 @Component
-class MellomlagringService {
+class MellomlagringService(
+    private val mellomlagringClient: MellomlagringClient,
+    private val soknadUnderArbeidRepository: SoknadUnderArbeidRepository,
+    private val virusScanner: VirusScanner
+) {
 
     fun getVedlegg(vedleggId: String, token: String): MellomlagretVedlegg? {
-        // todo implement
+        // todo hvordan gå fra vedleggId til behandlingsId? legge inn behandlingsId som path-param til GET-kallet?
+        val vedlegg = mellomlagringClient.getVedlegg("behandlingsId", vedleggId, token)
         return null
     }
 
@@ -17,12 +37,63 @@ class MellomlagringService {
         originalfilnavn: String,
         token: String
     ): MellomlagretVedleggMetadata {
-        // todo implement
+        var filnavn = originalfilnavn
+
+        val eier = SubjectHandlerUtils.getUserIdFromToken()
+        val sha512 = getSha512FromByteArray(data)
+
+        val fileType = validerFil(data, filnavn)
+        virusScanner.scan(filnavn, data, behandlingsId, fileType.name)
+
+        val soknadUnderArbeid = soknadUnderArbeidRepository.hentSoknad(behandlingsId, eier)
+
+        val uuid = UUID.randomUUID().toString()
+        filnavn = lagFilnavn(filnavn, fileType, uuid)
+
+        val jsonVedlegg = finnVedleggEllerKastException(vedleggstype, soknadUnderArbeid)
+        if (jsonVedlegg.filer == null) {
+            jsonVedlegg.filer = ArrayList()
+        }
+        jsonVedlegg.withStatus(Vedleggstatus.LastetOpp.toString()).filer.add(
+            JsonFiler().withFilnavn(filnavn).withSha512(sha512)
+        )
+
+        soknadUnderArbeidRepository.oppdaterSoknadsdata(soknadUnderArbeid, eier)
+
+        val detectedMimeType = FileDetectionUtils.getMimeType(data)
+        val mimetype = if (detectedMimeType.equals(MimeTypes.TEXT_X_MATLAB, ignoreCase = true)) MimeTypes.APPLICATION_PDF else detectedMimeType
+
+        val filOpplasting = FilOpplasting(
+            metadata = FilMetadata(
+                filnavn = filnavn,
+                mimetype = mimetype,
+                storrelse = data.size.toLong()
+            ),
+            data = ByteArrayInputStream(data)
+        )
+
+        mellomlagringClient.postVedlegg(
+            navEksternId = behandlingsId,
+            filOpplasting = filOpplasting,
+            token = token
+        )
         return MellomlagretVedleggMetadata(filnavn = "filnavn", filId = "uuid")
     }
 
     fun deleteVedleggAndUpdateVedleggstatus(behandlingsId: String, vedleggId: String, token: String) {
-        // todo implement
+        val eier = SubjectHandlerUtils.getUserIdFromToken()
+
+        // hent alle mellomlagrede vedlegg
+        val mellomlagredeVedlegg = mellomlagringClient.getMellomlagredeVedlegg(navEksternId = behandlingsId, token = token)
+        mellomlagredeVedlegg.mellomlagringMetadataList
+
+        // oppdater vedleggstatus
+        val soknadUnderArbeid = soknadUnderArbeidRepository.hentSoknad(behandlingsId, eier)
+        // ...
+        soknadUnderArbeidRepository.oppdaterSoknadsdata(soknadUnderArbeid, eier)
+
+        // slett mellomlagret vedlegg
+        mellomlagringClient.deleteVedlegg(navEksternId = behandlingsId, digisosDokumentId = vedleggId, token = token)
     }
 }
 
