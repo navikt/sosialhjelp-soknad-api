1e30bfcbf3fdc1bb85bf9e416b13bb61805373ca
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-05-19 15:10:11 +0200

    digisosapiv2 bruker spring webclient. (uten retry)

--- src/main/kotlin/no/nav/sosialhjelp/soknad/innsending/digisosapi/DigisosApiConfig.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/innsending/digisosapi/DigisosApiConfig.kt
@@ -1,11 +1,18 @@
 package no.nav.sosialhjelp.soknad.innsending.digisosapi
 
+import io.netty.channel.ChannelOption
 import no.nav.sosialhjelp.metrics.MetricsFactory
+import no.nav.sosialhjelp.soknad.common.Constants.HEADER_INTEGRASJON_ID
+import no.nav.sosialhjelp.soknad.common.Constants.HEADER_INTEGRASJON_PASSORD
 import no.nav.sosialhjelp.soknad.health.selftest.Pingable
 import no.nav.sosialhjelp.soknad.innsending.digisosapi.kommuneinfo.KommuneInfoService
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.netty.http.client.HttpClient
+import java.time.Duration
 
 @Configuration
 open class DigisosApiConfig(
@@ -14,7 +21,9 @@ open class DigisosApiConfig(
     @Value("\${integrasjonpassord_fiks}") private val integrasjonpassordFiks: String,
     private val kommuneInfoService: KommuneInfoService,
     private val dokumentlagerClient: DokumentlagerClient,
-    private val krypteringService: KrypteringService
+    private val krypteringService: KrypteringService,
+    proxiedWebClientBuilder: WebClient.Builder,
+    proxiedHttpClient: HttpClient,
 ) {
 
     @Bean
@@ -33,11 +42,9 @@ open class DigisosApiConfig(
     @Bean
     open fun digisosApiV2Client(): DigisosApiV2Client {
         val digisosApiV2Client = DigisosApiV2ClientImpl(
-            digisosApiEndpoint,
-            integrasjonsidFiks,
-            integrasjonpassordFiks,
             dokumentlagerClient,
-            krypteringService
+            krypteringService,
+            webClient
         )
         return MetricsFactory.createTimerProxy("DigisosApiV2", digisosApiV2Client, DigisosApiV2Client::class.java)
     }
@@ -53,5 +60,25 @@ open class DigisosApiConfig(
                 Pingable.feilet(metadata, e)
             }
         }
+    }
+
+    private val webClient = proxiedWebClientBuilder
+        .baseUrl(digisosApiEndpoint)
+        .clientConnector(
+            ReactorClientHttpConnector(
+                proxiedHttpClient
+                    .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, SENDING_TIL_FIKS_TIMEOUT)
+                    .responseTimeout(Duration.ofMillis(SENDING_TIL_FIKS_TIMEOUT.toLong()))
+            )
+        )
+        .codecs {
+            it.defaultCodecs().maxInMemorySize(16 * 1024 * 1024)
+        }
+        .defaultHeader(HEADER_INTEGRASJON_ID, integrasjonsidFiks)
+        .defaultHeader(HEADER_INTEGRASJON_PASSORD, integrasjonpassordFiks)
+        .build()
+
+    companion object {
+        private const val SENDING_TIL_FIKS_TIMEOUT = 5 * 60 * 1000 // 5 minutter
     }
 }
