86285cbff506f74a4b4262054e038ee8df6d97e0
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-05-23 17:40:09 +0200

    kun kall deleteAll mellomlagrede vedlegg hvis det finnes mellomlagrede vedlegg å slette

--- src/main/kotlin/no/nav/sosialhjelp/soknad/vedlegg/fiks/MellomlagringService.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/vedlegg/fiks/MellomlagringService.kt
@@ -151,7 +151,10 @@ class MellomlagringService(
 
     fun deleteAllVedlegg(behandlingsId: String) {
         val navEksternId = if (MiljoUtils.isNonProduction()) createPrefixedBehandlingsId(behandlingsId) else behandlingsId
-        mellomlagringClient.deleteAllVedlegg(navEksternId = navEksternId)
+
+        if (getAllVedlegg(behandlingsId).isNotEmpty()) {
+            mellomlagringClient.deleteAllVedlegg(navEksternId = navEksternId)
+        }
     }
 
     companion object {
