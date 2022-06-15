28d9a928d48df2dc7acbc5ba4a82a488dab2f2cb
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-06-15 08:23:24 +0200

    consts

--- src/main/kotlin/no/nav/sosialhjelp/soknad/metrics/PrometheusMetricsService.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/metrics/PrometheusMetricsService.kt
@@ -16,31 +16,31 @@ class PrometheusMetricsService(
 
     fun reportStartSoknad(isEttersendelse: Boolean) {
         startSoknadCounter
-            .tag("ettersendelse", isEttersendelse.toString())
+            .tag(TAG_ETTERSENDELSE, isEttersendelse.toString())
             .register(meterRegistry)
             .increment()
     }
 
     fun reportSendtSoknad(isEttersendelse: Boolean, type: String, navEnhet: String) {
         sendtSoknadCounter
-            .tag("ettersendelse", isEttersendelse.toString())
-            .tag("type", type)
-            .tag("mottaker", navEnhet)
+            .tag(TAG_ETTERSENDELSE, isEttersendelse.toString())
+            .tag(TAG_TYPE_API, type)
+            .tag(TAG_MOTTAKER, navEnhet)
             .register(meterRegistry)
             .increment()
     }
 
     fun reportFeiletSendingSoknad(isEttersendelse: Boolean, type: String) {
         feiletSendingSoknadCounter
-            .tag("ettersendelse", isEttersendelse.toString())
-            .tag("type", type)
+            .tag(TAG_ETTERSENDELSE, isEttersendelse.toString())
+            .tag(TAG_TYPE_API, type)
             .register(meterRegistry)
             .increment()
     }
 
     fun reportAvbruttSoknad(isEttersendelse: Boolean) {
         avbruttSoknadCounter
-            .tag("ettersendelse", isEttersendelse.toString())
+            .tag(TAG_ETTERSENDELSE, isEttersendelse.toString())
             .register(meterRegistry)
             .increment()
     }
@@ -48,5 +48,9 @@ class PrometheusMetricsService(
     companion object {
         const val SVARUT = "svarut"
         const val DIGISOS_API = "digisos_api"
+
+        const val TAG_ETTERSENDELSE = "ettersendelse"
+        const val TAG_TYPE_API = "type"
+        const val TAG_MOTTAKER = "mottaker"
     }
 }
