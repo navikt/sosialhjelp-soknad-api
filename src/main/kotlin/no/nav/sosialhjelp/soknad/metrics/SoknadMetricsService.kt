7f60bcf31c778cce61e0d2ce02a3a7dc9239d7a1
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-06-28 15:14:26 +0200

    lint

--- src/main/kotlin/no/nav/sosialhjelp/soknad/metrics/SoknadMetricsService.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/metrics/SoknadMetricsService.kt
@@ -58,18 +58,20 @@ class SoknadMetricsService {
         event.addFieldToReport("prosent.ikkeLevert", getProsent(antallIkkeLevert, totaltAntall))
         event.report()
 
-        log.info("""
-            Vedleggskrav statistikk
-            ettersendelse=$isEttersendelse,
-            sendetype=$sendtype,
-            totaltAntall=$totaltAntall,
-            antallInnsendt=$antallInnsendt,
-            antallLevertTidligere=$antallLevertTidligere,
-            antallIkkeLevert=$antallIkkeLevert,
-            prosentInnsendt=${getProsent(antallInnsendt, totaltAntall)},
-            prosentLevertTidligere=${getProsent(antallLevertTidligere, totaltAntall)},
-            prosentIkkeLevert=${getProsent(antallIkkeLevert, totaltAntall)}
-            """.trimIndent())
+        log.info(
+            """
+                Vedleggskrav statistikk
+                ettersendelse=$isEttersendelse,
+                sendetype=$sendtype,
+                totaltAntall=$totaltAntall,
+                antallInnsendt=$antallInnsendt,
+                antallLevertTidligere=$antallLevertTidligere,
+                antallIkkeLevert=$antallIkkeLevert,
+                prosentInnsendt=${getProsent(antallInnsendt, totaltAntall)},
+                prosentLevertTidligere=${getProsent(antallLevertTidligere, totaltAntall)},
+                prosentIkkeLevert=${getProsent(antallIkkeLevert, totaltAntall)}
+            """.trimIndent()
+        )
     }
 
     fun countAndreportVedleggskrav(isEttersendelse: Boolean, vedleggList: List<VedleggMetadata>) {
