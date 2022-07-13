8cbb67c5b150f1cd155f8027247f1c07d37339b9
martintveter <41987225+martintveter@users.noreply.github.com>
2022-07-13 10:53:53 +0200

    endrer til å kalle kommuneinfoClient istedet for kommuneinfoService fra DigisosApiCheck.kt, ettersom kommuneinfoService logger info vi ikke trenger for selftestens del (#1369)

--- src/main/kotlin/no/nav/sosialhjelp/soknad/health/checks/DigisosApiCheck.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/health/checks/DigisosApiCheck.kt
@@ -3,14 +3,14 @@ package no.nav.sosialhjelp.soknad.health.checks
 import no.nav.sosialhjelp.selftest.DependencyCheck
 import no.nav.sosialhjelp.selftest.DependencyType
 import no.nav.sosialhjelp.selftest.Importance
-import no.nav.sosialhjelp.soknad.innsending.digisosapi.kommuneinfo.KommuneInfoService
+import no.nav.sosialhjelp.soknad.innsending.digisosapi.kommuneinfo.KommuneInfoClient
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Component
 class DigisosApiCheck(
     @Value("\${digisos_api_baseurl}") private val digisosApiEndpoint: String,
-    private val kommuneInfoService: KommuneInfoService,
+    private val kommuneInfoClient: KommuneInfoClient,
 ) : DependencyCheck {
 
     override val type = DependencyType.REST
@@ -19,6 +19,6 @@ class DigisosApiCheck(
     override val importance = Importance.CRITICAL
 
     override fun doCheck() {
-        kommuneInfoService.hentKommuneInfoFraFiks()
+        kommuneInfoClient.getAll()
     }
 }
