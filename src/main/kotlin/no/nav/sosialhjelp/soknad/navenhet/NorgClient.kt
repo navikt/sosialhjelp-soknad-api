9160d0f8a7a947c852590ad9a8f80ea98f4eed6a
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-07-15 11:02:51 +0200

    norg2 er tilgjengelig via <dev/prod>-fss-pub.nais.io ingress. Da trenger vi ikke gjøre kall via fss-proxy

--- src/main/kotlin/no/nav/sosialhjelp/soknad/navenhet/NorgClient.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/navenhet/NorgClient.kt
@@ -1,8 +1,6 @@
 package no.nav.sosialhjelp.soknad.navenhet
 
 import com.fasterxml.jackson.core.JsonProcessingException
-import kotlinx.coroutines.runBlocking
-import no.nav.sosialhjelp.soknad.auth.tokenx.TokendingsService
 import no.nav.sosialhjelp.soknad.client.config.RetryUtils
 import no.nav.sosialhjelp.soknad.client.config.unproxiedWebClientBuilder
 import no.nav.sosialhjelp.soknad.client.exceptions.TjenesteUtilgjengeligException
@@ -11,18 +9,14 @@ import no.nav.sosialhjelp.soknad.client.redis.GT_CACHE_KEY_PREFIX
 import no.nav.sosialhjelp.soknad.client.redis.GT_LAST_POLL_TIME_PREFIX
 import no.nav.sosialhjelp.soknad.client.redis.RedisService
 import no.nav.sosialhjelp.soknad.client.redis.RedisUtils.redisObjectMapper
-import no.nav.sosialhjelp.soknad.common.Constants.BEARER
 import no.nav.sosialhjelp.soknad.common.Constants.HEADER_CALL_ID
 import no.nav.sosialhjelp.soknad.common.Constants.HEADER_CONSUMER_ID
 import no.nav.sosialhjelp.soknad.common.mdc.MdcOperations
 import no.nav.sosialhjelp.soknad.common.mdc.MdcOperations.MDC_CALL_ID
 import no.nav.sosialhjelp.soknad.common.subjecthandler.SubjectHandlerUtils
-import no.nav.sosialhjelp.soknad.common.subjecthandler.SubjectHandlerUtils.getToken
-import no.nav.sosialhjelp.soknad.common.subjecthandler.SubjectHandlerUtils.getUserIdFromToken
 import no.nav.sosialhjelp.soknad.navenhet.dto.NavEnhetDto
 import org.slf4j.LoggerFactory.getLogger
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.http.HttpHeaders.AUTHORIZATION
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.WebClientResponseException
@@ -38,26 +32,19 @@ interface NorgClient {
 
 @Component
 class NorgClientImpl(
-    @Value("\${norg_proxy_url}") private val baseurl: String,
-    @Value("\${fss_proxy_audience}") private val fssProxyAudience: String,
-    private val tokendingsService: TokendingsService,
+    @Value("\${norg_url}") private val norgUrl: String,
     private val redisService: RedisService,
     webClientBuilder: WebClient.Builder
 ) : NorgClient {
 
     private val webClient = unproxiedWebClientBuilder(webClientBuilder).build()
 
-    private val tokenXtoken: String get() = runBlocking {
-        tokendingsService.exchangeToken(getUserIdFromToken(), getToken(), fssProxyAudience)
-    }
-
     override fun hentNavEnhetForGeografiskTilknytning(geografiskTilknytning: String): NavEnhetDto? {
         return try {
             webClient.get()
-                .uri("${baseurl}enhet/navkontor/{geografiskTilknytning}", geografiskTilknytning)
+                .uri("$norgUrl/enhet/navkontor/{geografiskTilknytning}", geografiskTilknytning)
                 .header(HEADER_CALL_ID, MdcOperations.getFromMDC(MDC_CALL_ID))
                 .header(HEADER_CONSUMER_ID, SubjectHandlerUtils.getConsumerId())
-                .header(AUTHORIZATION, BEARER + tokenXtoken)
                 .retrieve()
                 .bodyToMono<NavEnhetDto>()
                 .retryWhen(RetryUtils.DEFAULT_RETRY_SERVER_ERRORS)
