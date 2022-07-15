ed7e50ba5ac54de76b9a53d6b204bf3031528521
martintveter <41987225+martintveter@users.noreply.github.com>
2022-07-15 15:09:43 +0200

    ereg har nye ingresser som kan nås fra sbs/gcp (#1377)
    * ereg har nye ingresser som kan nås fra sbs/gcp
    trenger ikke kalle via fss-proxy lengre

--- src/main/kotlin/no/nav/sosialhjelp/soknad/health/checks/EregCheck.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/health/checks/EregCheck.kt
@@ -4,31 +4,37 @@ import no.nav.sosialhjelp.selftest.DependencyCheck
 import no.nav.sosialhjelp.selftest.DependencyType
 import no.nav.sosialhjelp.selftest.Importance
 import no.nav.sosialhjelp.soknad.client.config.unproxiedWebClientBuilder
+import no.nav.sosialhjelp.soknad.common.Constants.HEADER_CALL_ID
+import no.nav.sosialhjelp.soknad.common.Constants.HEADER_CONSUMER_ID
+import no.nav.sosialhjelp.soknad.common.mdc.MdcOperations.generateCallId
+import no.nav.sosialhjelp.soknad.common.subjecthandler.SubjectHandlerUtils.getConsumerId
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToMono
 
 @Component
-class SosialhjelpFssProxyCheck(
-    @Value("\${fss_proxy_ping_url}") private val fssProxyPingUrl: String,
+class EregCheck(
+    @Value("\${ereg_url}") private val eregUrl: String,
     webClientBuilder: WebClient.Builder,
 ) : DependencyCheck {
 
     override val type = DependencyType.REST
-    override val name = "sosialhjelp-fss-proxy (proxy for Aareg og Krr)"
-    override val address = fssProxyPingUrl
+    override val name = "Ereg"
+    override val address = eregUrl
     override val importance = Importance.WARNING
 
-    private val fssProxyWebClient: WebClient = unproxiedWebClientBuilder(webClientBuilder).build()
+    private val eregWebClient: WebClient = unproxiedWebClientBuilder(webClientBuilder).build()
 
     override fun doCheck() {
-        fssProxyWebClient.options()
-            .uri(fssProxyPingUrl)
+        eregWebClient.get()
+            .uri("$eregUrl/v1/organisasjon/990983666/noekkelinfo")
+            .header(HEADER_CALL_ID, generateCallId())
+            .header(HEADER_CONSUMER_ID, getConsumerId())
             .retrieve()
             .bodyToMono<String>()
             .onErrorMap {
-                throw RuntimeException("Ping mot sosialhjelp-fss-proxy feiler: ${it.message}", it)
+                throw RuntimeException("Ping mot Ereg feiler: ${it.message}", it)
             }
             .block()
     }
