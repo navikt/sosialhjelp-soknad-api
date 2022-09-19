771413ca25dfd9d5cac016d4248523fe03b5c49a
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-09-19 11:36:20 +0200

    tar i bruk vault for dev-fss. Midlertidig fiks

--- nais/scripts/91-set-app-datasource.sh
+++ nais/scripts/91-set-app-datasource.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env sh
 
 echo "Export datasource credentials"
-if [ "$NAIS_CLUSTER_NAME" == "dev-fss" ]; then
-  echo "SOKNADSOSIALHJELPDATASOURCE_USERNAME already set"
-else
-  export SOKNADSOSIALHJELPDATASOURCE_USERNAME=$(cat /var/run/secrets/datasource/username)
-fi
-
-if [ "$NAIS_CLUSTER_NAME" == "dev-fss" ]; then
-  echo "SOKNADSOSIALHJELPDATASOURCE_PASSWORD already set"
-else
-  export SOKNADSOSIALHJELPDATASOURCE_PASSWORD=$(cat /var/run/secrets/datasource/password)
-fi
+export SOKNADSOSIALHJELPDATASOURCE_USERNAME=$(cat /var/run/secrets/datasource/username)
+export SOKNADSOSIALHJELPDATASOURCE_PASSWORD=$(cat /var/run/secrets/datasource/password)
