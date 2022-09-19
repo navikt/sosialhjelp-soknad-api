6ce582188cc03e3759b4b6aba72e801ff6c7d0a2
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-09-19 11:19:04 +0200

    forsok 2

--- nais/scripts/91-set-app-datasource.sh
+++ nais/scripts/91-set-app-datasource.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env sh
 
 echo "Export datasource credentials"
-
-if [ -z "${SOKNADSOSIALHJELPDATASOURCE_USERNAME}" ]; then
+if [ "$NAIS_CLUSTER_NAME" == "dev-fss" ]; then
   echo "SOKNADSOSIALHJELPDATASOURCE_USERNAME already set"
 else
   export SOKNADSOSIALHJELPDATASOURCE_USERNAME=$(cat /var/run/secrets/datasource/username)
 fi
 
-if [ -z "${SOKNADSOSIALHJELPDATASOURCE_PASSWORD}" ]; then
+if [ "$NAIS_CLUSTER_NAME" == "dev-fss" ]; then
   echo "SOKNADSOSIALHJELPDATASOURCE_PASSWORD already set"
 else
   export SOKNADSOSIALHJELPDATASOURCE_PASSWORD=$(cat /var/run/secrets/datasource/password)
