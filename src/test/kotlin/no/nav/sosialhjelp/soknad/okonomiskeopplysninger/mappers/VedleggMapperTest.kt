4020c8d53f4c6300f0755f7419a94cb7626da6dd
martintveter <41987225+martintveter@users.noreply.github.com>
2022-11-08 08:30:23 +0100

    fix - enkelte søknadIder går i feil-loop grunnet oppdatering av vedlegg.json før feilende mellomlagring
    Før https://github.com/navikt/sosialhjelp-soknad-api/pull/1473 ble flettet inn opplevde noen søknader at mellomlagring feilet, men vedleggJson ble oppdatert slik vedlegg står som LastetOpp mens det ikke finnes noe mellomlagret vedlegg. (#1475)
    
    Dette gir feil i prod for alle behandlingsIder i listen `behandlingsIdsToPass`.
    
    Legger til hotfix som sjekker om nåværende behandlingsId er i nevnte liste. Deretter unnlater vi å vise de vedleggene som ikke har blitt lastet opp til mellomlagring. Det vil nødvendigvis føre til at json og faktiske vedlegg ikke nødvendigvis stemmer for disse 10 søknadene.

--- src/test/kotlin/no/nav/sosialhjelp/soknad/okonomiskeopplysninger/mappers/VedleggMapperTest.kt
+++ src/test/kotlin/no/nav/sosialhjelp/soknad/okonomiskeopplysninger/mappers/VedleggMapperTest.kt
@@ -1,9 +1,15 @@
 package no.nav.sosialhjelp.soknad.okonomiskeopplysninger.mappers
 
+import no.nav.sbl.soknadsosialhjelp.soknad.okonomi.JsonOkonomi
+import no.nav.sbl.soknadsosialhjelp.soknad.okonomi.JsonOkonomiopplysninger
+import no.nav.sbl.soknadsosialhjelp.soknad.okonomi.JsonOkonomioversikt
+import no.nav.sbl.soknadsosialhjelp.vedlegg.JsonFiler
 import no.nav.sbl.soknadsosialhjelp.vedlegg.JsonVedlegg
 import no.nav.sosialhjelp.soknad.db.repositories.opplastetvedlegg.OpplastetVedlegg
 import no.nav.sosialhjelp.soknad.db.repositories.opplastetvedlegg.OpplastetVedleggType
 import no.nav.sosialhjelp.soknad.db.repositories.soknadmetadata.Vedleggstatus
+import no.nav.sosialhjelp.soknad.okonomiskeopplysninger.mappers.VedleggMapper.mapMellomlagredeVedleggToVedleggFrontend
+import no.nav.sosialhjelp.soknad.vedlegg.fiks.MellomlagretVedleggMetadata
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.time.LocalDateTime
@@ -44,6 +50,49 @@ internal class VedleggMapperTest {
         assertThat(result[0].vedleggStatus).isEqualTo(Vedleggstatus.LastetOpp.toString())
         assertThat(result[1].type).isEqualTo(ANNET.sammensattType)
         assertThat(result[1].vedleggStatus).isEqualTo(Vedleggstatus.VedleggKreves.toString())
+    }
+
+    @Test
+    internal fun `hotfix - mellomlagret vedlegg feilet men vedleggjson ble oppdatert`() {
+        // behandlingsId i listen behandlingsIdsToPass i VedleggMapper
+        val behandlingsId = "11001ZKC1"
+        val bostotteVedlegg = JsonVedlegg()
+            .withType("faktura")
+            .withTilleggsinfo("husleie")
+            .withStatus(Vedleggstatus.LastetOpp.toString())
+            .withFiler(
+                mutableListOf(
+                    JsonFiler()
+                        .withFilnavn("hubbabubba.jpg")
+                        .withSha512("sha512"),
+                    JsonFiler()
+                        .withFilnavn("juicyfruit.pdf")
+                        .withSha512("shasha512512")
+                )
+            )
+        val mellomlagredeVedlegg = listOf(
+            MellomlagretVedleggMetadata(filnavn = "hubbabubba.jpg", filId = "id123")
+        )
+
+        val jsonOkonomi = JsonOkonomi()
+            .withOversikt(
+                JsonOkonomioversikt()
+                    .withUtgift(
+                        mutableListOf()
+                    )
+            )
+            .withOpplysninger(
+                JsonOkonomiopplysninger()
+                    .withUtgift(
+                        mutableListOf()
+                    )
+            )
+
+        val vedleggFrontend = mapMellomlagredeVedleggToVedleggFrontend(bostotteVedlegg, jsonOkonomi, mellomlagredeVedlegg, behandlingsId)
+        assertThat(vedleggFrontend).isNotNull
+        assertThat(vedleggFrontend.filer).hasSize(1)
+        assertThat(vedleggFrontend.filer!![0].filNavn).isEqualTo("hubbabubba.jpg")
+        assertThat(vedleggFrontend.filer!![0].uuid).isEqualTo("id123")
     }
 
     private fun createOriginaleVedlegg(): List<JsonVedlegg> {
