48860543dcdbb5e7558dafefd08d0ad774e101b1
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-11-25 12:22:04 +0100

    dtos

---
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/personalia/adresse/adresseregister/dto/HentAdresseDto.kt
@@ -0,0 +1,22 @@
+package no.nav.sosialhjelp.soknad.personalia.adresse.adresseregister.dto
+
+data class HentAdresseDataDto(
+    val hentAdresse: AdresseDto?
+)
+
+data class AdresseDto(
+    val matrikkelAdresse: MatrikkeladresseDto?
+)
+
+data class MatrikkeladresseDto(
+    val undernummer: String?,
+    val matrikkelnummer: MatrikkelNummer?
+)
+
+data class MatrikkelNummer(
+    val kommunenummer: String?,
+    val gaardsnummer: String?,
+    val bruksnummer: String?,
+    val festenummer: String?,
+    val seksjonsnummer: String?
+)
