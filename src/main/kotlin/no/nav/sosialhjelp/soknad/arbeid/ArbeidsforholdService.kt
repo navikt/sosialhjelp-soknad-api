79d6eedbca9d844be6fe249bb90514cba628551d
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-12-08 09:57:05 +0100

    Fjerner ArbeidsforholdClient og config for å konsumere aareg via fss-proxy

--- src/main/kotlin/no/nav/sosialhjelp/soknad/arbeid/ArbeidsforholdService.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/arbeid/ArbeidsforholdService.kt
@@ -1,6 +1,5 @@
 package no.nav.sosialhjelp.soknad.arbeid
 
-import no.finn.unleash.Unleash
 import no.nav.sosialhjelp.soknad.arbeid.domain.Arbeidsforhold
 import no.nav.sosialhjelp.soknad.arbeid.dto.toDomain
 import no.nav.sosialhjelp.soknad.organisasjon.OrganisasjonService
@@ -10,24 +9,16 @@ import org.springframework.stereotype.Component
 @Component
 open class ArbeidsforholdService(
     private val aaregClient: AaregClient,
-    private val arbeidsforholdClient: ArbeidsforholdClient,
-    private val organisasjonService: OrganisasjonService,
-    private val unleash: Unleash
+    private val organisasjonService: OrganisasjonService
 ) {
 
     open fun hentArbeidsforhold(fnr: String): List<Arbeidsforhold>? {
-        val arbeidsforholdList = if (unleash.isEnabled(AAREG_UTEN_FSS_PROXY, false)) {
-            aaregClient.finnArbeidsforholdForArbeidstaker(fnr)
-        } else {
-            arbeidsforholdClient.finnArbeidsforholdForArbeidstaker(fnr)
-        }
-        return arbeidsforholdList
+        return aaregClient.finnArbeidsforholdForArbeidstaker(fnr)
             ?.map { it.toDomain(organisasjonService) }
             .also { log.info("Hentet ${it?.size ?: 0} arbeidsforhold fra aareg") }
     }
 
     companion object {
         private val log = getLogger(ArbeidsforholdService::class.java)
-        private const val AAREG_UTEN_FSS_PROXY = "sosialhjelp.soknad.aareg-uten-fss-proxy"
     }
 }
