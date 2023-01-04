5d137a58296b9378c5566e7acdf28194ca90eb54
martintveter <41987225+martintveter@users.noreply.github.com>
2023-01-04 12:55:25 +0100

    Rydder opp etter fjerning av kommuneinfo-endepunkt i #1472 (#1544)

--- src/main/kotlin/no/nav/sosialhjelp/soknad/api/informasjon/dto/KommuneInfoFrontend.kt
+++
@@ -1,7 +0,0 @@
-package no.nav.sosialhjelp.soknad.api.informasjon.dto
-
-data class KommuneInfoFrontend(
-    var kommunenummer: String,
-    var kanMottaSoknader: Boolean,
-    var kanOppdatereStatus: Boolean
-)
