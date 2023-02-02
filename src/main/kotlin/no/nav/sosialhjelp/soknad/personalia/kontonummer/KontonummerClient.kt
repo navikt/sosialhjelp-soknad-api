59e29c4e8433dafd926a8998403b4fe20fc1549b
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2023-02-02 14:18:31 +0100

    Fjerner integrasjon til sosialhjelp-oppslag-api. Etter at vi nå henter info om brukers kontonummer fra kontoregister, trenger vi ikke kalle sosialhjelp-oppslag-api lengre.

--- src/main/kotlin/no/nav/sosialhjelp/soknad/personalia/kontonummer/KontonummerClient.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/personalia/kontonummer/KontonummerClient.kt
@@ -4,19 +4,15 @@ import com.fasterxml.jackson.core.JsonProcessingException
 import kotlinx.coroutines.runBlocking
 import no.nav.sosialhjelp.soknad.app.Constants.BEARER
 import no.nav.sosialhjelp.soknad.app.Constants.HEADER_CALL_ID
-import no.nav.sosialhjelp.soknad.app.Constants.HEADER_CONSUMER_ID
 import no.nav.sosialhjelp.soknad.app.client.config.RetryUtils
 import no.nav.sosialhjelp.soknad.app.client.config.unproxiedWebClientBuilder
 import no.nav.sosialhjelp.soknad.app.mdc.MdcOperations.MDC_CALL_ID
 import no.nav.sosialhjelp.soknad.app.mdc.MdcOperations.getFromMDC
-import no.nav.sosialhjelp.soknad.app.subjecthandler.SubjectHandlerUtils.getConsumerId
 import no.nav.sosialhjelp.soknad.app.subjecthandler.SubjectHandlerUtils.getToken
 import no.nav.sosialhjelp.soknad.app.subjecthandler.SubjectHandlerUtils.getUserIdFromToken
 import no.nav.sosialhjelp.soknad.auth.tokenx.TokendingsService
 import no.nav.sosialhjelp.soknad.personalia.kontonummer.dto.KontoDto
-import no.nav.sosialhjelp.soknad.personalia.kontonummer.dto.KontonummerDto
 import no.nav.sosialhjelp.soknad.redis.CACHE_30_MINUTES_IN_SECONDS
-import no.nav.sosialhjelp.soknad.redis.KONTONUMMER_CACHE_KEY_PREFIX
 import no.nav.sosialhjelp.soknad.redis.KONTOREGISTER_KONTONUMMER_CACHE_KEY_PREFIX
 import no.nav.sosialhjelp.soknad.redis.RedisService
 import no.nav.sosialhjelp.soknad.redis.RedisUtils.redisObjectMapper
@@ -31,13 +27,10 @@ import org.springframework.web.reactive.function.client.bodyToMono
 
 interface KontonummerClient {
     fun getKontonummer(ident: String): KontoDto?
-    fun getKontonummerLegacy(ident: String): KontonummerDto?
 }
 
 @Component
 class KontonummerClientImpl(
-    @Value("\${oppslag_api_baseurl}") private val oppslagApiUrl: String,
-    @Value("\${oppslag_api_audience}") private val oppslagApiAudience: String,
     @Value("\${kontoregister_api_baseurl}") private val kontoregisterUrl: String,
     @Value("\${kontoregister_api_audience}") private val kontoregisterAudience: String,
     private val redisService: RedisService,
@@ -72,32 +65,6 @@ class KontonummerClientImpl(
         }
     }
 
-    override fun getKontonummerLegacy(ident: String): KontonummerDto? {
-        hentKontonummerFraCacheLegacy(ident)?.let { return it }
-
-        return try {
-            webClient.get()
-                .uri(oppslagApiUrl + "kontonummer")
-                .header(AUTHORIZATION, BEARER + tokenXtoken(oppslagApiAudience))
-                .header(HEADER_CALL_ID, getFromMDC(MDC_CALL_ID))
-                .header(HEADER_CONSUMER_ID, getConsumerId())
-                .retrieve()
-                .bodyToMono<KontonummerDto>()
-                .retryWhen(RetryUtils.DEFAULT_RETRY_SERVER_ERRORS)
-                .block()
-                ?.also { lagreKontonummerTilCacheLegacy(ident, it) }
-        } catch (e: Unauthorized) {
-            log.warn("Kontonummer - 401 Unauthorized - ${e.message}")
-            null
-        } catch (e: NotFound) {
-            log.warn("Kontonummer - 404 Not Found - ${e.message}")
-            null
-        } catch (e: Exception) {
-            log.error("Kontonummer - Noe uventet feilet", e)
-            null
-        }
-    }
-
     private fun tokenXtoken(audience: String): String {
         return runBlocking {
             tokendingsService.exchangeToken(getUserIdFromToken(), getToken(), audience)
@@ -117,22 +84,6 @@ class KontonummerClientImpl(
             )
         } catch (e: JsonProcessingException) {
             log.warn("Noe feilet ved lagring av kontoDto til redis", e)
-        }
-    }
-
-    private fun hentKontonummerFraCacheLegacy(ident: String): KontonummerDto? {
-        return redisService.get(KONTONUMMER_CACHE_KEY_PREFIX + ident, KontonummerDto::class.java) as? KontonummerDto
-    }
-
-    private fun lagreKontonummerTilCacheLegacy(ident: String, kontonummerDto: KontonummerDto) {
-        try {
-            redisService.setex(
-                KONTONUMMER_CACHE_KEY_PREFIX + ident,
-                redisObjectMapper.writeValueAsBytes(kontonummerDto),
-                CACHE_30_MINUTES_IN_SECONDS
-            )
-        } catch (e: JsonProcessingException) {
-            log.warn("Noe feilet ved lagring av kontonummerDto til redis", e)
         }
     }
 
--- src/main/kotlin/no/nav/sosialhjelp/soknad/personalia/kontonummer/KontonummerService.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/personalia/kontonummer/KontonummerService.kt
@@ -1,49 +1,25 @@
 package no.nav.sosialhjelp.soknad.personalia.kontonummer
 
-import no.finn.unleash.Unleash
 import no.nav.sosialhjelp.soknad.app.LoggingUtils.logger
 import org.springframework.stereotype.Component
 
 @Component
 open class KontonummerService(
-    private val kontonummerClient: KontonummerClient,
-    private val unleash: Unleash
+    private val kontonummerClient: KontonummerClient
 ) {
 
     open fun getKontonummer(ident: String): String? {
-        val kontonummer: String?
-        if (unleash.isEnabled(BRUK_KONTOREGISTER_ENABLED, true)) {
-            log.info("Feature toggle for bruk av kontoregister er enablet og kontonummer hentes fra kontoregister")
-            val konto = kontonummerClient.getKontonummer(ident)
-            kontonummer = if (konto?.utenlandskKontoInfo != null) {
-                log.info("Kontonummer fra kontoregister er utenlandskonto og kontonummer settes ikke")
-                null
-            } else {
-                konto?.kontonummer
-            }
+        log.info("Feature toggle for bruk av kontoregister er enablet og kontonummer hentes fra kontoregister")
+        val konto = kontonummerClient.getKontonummer(ident)
+        return if (konto?.utenlandskKontoInfo != null) {
+            log.info("Kontonummer fra kontoregister er utenlandskonto og kontonummer settes ikke")
+            null
         } else {
-            log.info("Feature toggle for bruk av kontoregister er disablet og kontonummer hentes fra PersonV3 tjeneste via oppslag-api")
-            kontonummer = kontonummerClient.getKontonummerLegacy(ident)?.kontonummer
-            sammenlignmedSkyggeproduksjon(kontonummer, ident)
-        }
-        return kontonummer
-    }
-
-    private fun sammenlignmedSkyggeproduksjon(kontonummer: String?, ident: String) {
-        val kontonummerSkygge = kontonummerClient.getKontonummer(ident)?.kontonummer
-
-        if (kontonummerSkygge == kontonummer) {
-            log.info("Kontoregister skyggeproduksjon: Kontonummer fra PersonV3 tjeneste og kontonummer fra kontoregister er like")
-        } else {
-            log.info(
-                "Kontoregister skyggeproduksjon: Kontonummer fra PersonV3 tjeneste og kontonummer fra kontoregister er forskjellige." +
-                    "Kontonummer fra PersonV3: $kontonummer og kontonummer fra Kontoregister: $kontonummerSkygge"
-            )
+            konto?.kontonummer
         }
     }
 
     companion object {
         private val log by logger()
-        const val BRUK_KONTOREGISTER_ENABLED = "sosialhjelp.soknad.bruk_sokos_kontoregister"
     }
 }
