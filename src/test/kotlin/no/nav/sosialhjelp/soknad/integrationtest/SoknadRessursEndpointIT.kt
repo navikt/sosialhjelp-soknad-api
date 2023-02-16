a126c3831d65425fb0a21ea047481be93b293fa5
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2023-02-16 17:07:09 +0100

    integrasjonstester rydder opp etter seg

--- src/test/kotlin/no/nav/sosialhjelp/soknad/integrationtest/SoknadRessursEndpointIT.kt
+++ src/test/kotlin/no/nav/sosialhjelp/soknad/integrationtest/SoknadRessursEndpointIT.kt
@@ -5,18 +5,19 @@ import no.nav.sosialhjelp.soknad.TestApplication
 import no.nav.sosialhjelp.soknad.app.Constants.BEARER
 import no.nav.sosialhjelp.soknad.integrationtest.IntegrationTestUtils.issueToken
 import no.nav.sosialhjelp.soknad.integrationtest.IntegrationTestUtils.opprettSoknad
-import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
+import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.reactive.server.WebTestClient
+import javax.inject.Inject
 
-@Disabled
 @ContextConfiguration(classes = [PdlIntegrationTestConfig::class])
 @SpringBootTest(classes = [TestApplication::class], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient(timeout = "PT30S")
@@ -33,6 +34,15 @@ class SoknadRessursEndpointIT {
 
     @Autowired
     private lateinit var webClient: WebTestClient
+
+    @Inject
+    private lateinit var jdbcTemplate: JdbcTemplate
+
+    @AfterEach
+    fun tearDown() {
+        jdbcTemplate.update("delete from soknad_under_arbeid")
+        jdbcTemplate.update("delete from soknadmetadata")
+    }
 
     @Test
     internal fun nektetTilgang_opprettEttersendelse() {
