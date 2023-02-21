2d91a16461923cddae860727b519f2f764961383
martintveter <41987225+martintveter@users.noreply.github.com>
2023-02-21 11:33:09 +0100

    Fjerne kommunestatus endepunkt (#1600)
    Sosialhjelp-adminpanel integrerer nå med sosialhjelp-kommune-service, så da trenger vi ikke kobling fra sosialhjelp-adminpanel til sosialhjelp-soknad-api.

--- src/test/kotlin/no/nav/sosialhjelp/soknad/api/informasjon/InformasjonRessursTest.kt
+++ src/test/kotlin/no/nav/sosialhjelp/soknad/api/informasjon/InformasjonRessursTest.kt
@@ -5,13 +5,10 @@ import io.mockk.every
 import io.mockk.mockk
 import io.mockk.mockkObject
 import io.mockk.unmockkObject
-import no.nav.sosialhjelp.api.fiks.KommuneInfo
-import no.nav.sosialhjelp.soknad.api.nedetid.NedetidService
 import no.nav.sosialhjelp.soknad.app.MiljoUtils
 import no.nav.sosialhjelp.soknad.app.subjecthandler.StaticSubjectHandlerImpl
 import no.nav.sosialhjelp.soknad.app.subjecthandler.SubjectHandlerUtils
 import no.nav.sosialhjelp.soknad.db.repositories.soknadmetadata.SoknadMetadataRepository
-import no.nav.sosialhjelp.soknad.innsending.digisosapi.kommuneinfo.KommuneInfoService
 import no.nav.sosialhjelp.soknad.personalia.person.PersonService
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterEach
@@ -20,18 +17,14 @@ import org.junit.jupiter.api.Test
 
 internal class InformasjonRessursTest {
 
-    private val kommuneInfoService: KommuneInfoService = mockk()
     private val personService: PersonService = mockk()
     private val soknadMetadataRepository: SoknadMetadataRepository = mockk()
-    private val nedetidService: NedetidService = mockk()
 
     private val ressurs = InformasjonRessurs(
         adresseSokService = mockk(),
-        kommuneInfoService = kommuneInfoService,
         personService = personService,
         soknadMetadataRepository = soknadMetadataRepository,
         pabegynteSoknaderService = mockk(),
-        nedetidService = nedetidService
     )
 
     @BeforeEach
@@ -47,85 +40,6 @@ internal class InformasjonRessursTest {
     fun tearDown() {
         SubjectHandlerUtils.resetSubjectHandlerImpl()
         unmockkObject(MiljoUtils)
-    }
-
-    @Test
-    fun skalReturnereMappetListeOverManueltPakobledeKommuner() {
-        val manuelleKommuner = listOf("1234")
-        val mappedeKommuneStatuser = ressurs.mapManueltPakobledeKommunerTilKommunestatusFrontend(manuelleKommuner)
-
-        val kommune1234 = mappedeKommuneStatuser["1234"]
-        assertThat(kommune1234).isNotNull
-        assertThat(kommune1234?.kanMottaSoknader).isTrue
-        assertThat(kommune1234?.kanOppdatereStatus).isFalse
-    }
-
-    @Test
-    fun skalReturnereMappetListeOverDigisosKommuner() {
-        val digisosKommuner: MutableMap<String, KommuneInfo> = HashMap()
-        digisosKommuner["1234"] = KommuneInfo(
-            "1234",
-            kanMottaSoknader = true,
-            kanOppdatereStatus = true,
-            harMidlertidigDeaktivertMottak = false,
-            harMidlertidigDeaktivertOppdateringer = false,
-            kontaktpersoner = null,
-            harNksTilgang = false,
-            behandlingsansvarlig = null
-        )
-        digisosKommuner["5678"] = KommuneInfo(
-            "5678",
-            kanMottaSoknader = true,
-            kanOppdatereStatus = true,
-            harMidlertidigDeaktivertMottak = true,
-            harMidlertidigDeaktivertOppdateringer = false,
-            kontaktpersoner = null,
-            harNksTilgang = false,
-            behandlingsansvarlig = null
-        )
-
-        val mappedeKommuneStatuser = ressurs.mapDigisosKommunerTilKommunestatus(digisosKommuner)
-
-        val kommune1234 = mappedeKommuneStatuser["1234"]
-        assertThat(kommune1234).isNotNull
-        assertThat(kommune1234?.kanMottaSoknader).isTrue
-        assertThat(kommune1234?.kanOppdatereStatus).isTrue
-        assertThat(kommune1234?.harMidlertidigDeaktivertMottak).isFalse
-        assertThat(kommune1234?.harMidlertidigDeaktivertOppdateringer).isFalse
-        assertThat(kommune1234?.harNksTilgang).isFalse
-
-        val kommune5678 = mappedeKommuneStatuser["5678"]
-        assertThat(kommune5678).isNotNull
-        assertThat(kommune5678?.kanMottaSoknader).isTrue
-        assertThat(kommune5678?.kanOppdatereStatus).isTrue
-        assertThat(kommune5678?.harMidlertidigDeaktivertMottak).isTrue
-        assertThat(kommune5678?.harMidlertidigDeaktivertOppdateringer).isFalse
-        assertThat(kommune5678?.harNksTilgang).isFalse
-    }
-
-    @Test
-    fun duplikatIDigisosKommuneSkalOverskriveManuellKommune() {
-        val manuelleKommuner = listOf("1234")
-        val manueltMappedeKommuner = ressurs.mapManueltPakobledeKommunerTilKommunestatusFrontend(manuelleKommuner)
-
-        assertThat(manueltMappedeKommuner["1234"]!!.kanOppdatereStatus).isFalse // Manuelle kommuner får ikke innsyn
-
-        val digisosKommuner: MutableMap<String, KommuneInfo> = HashMap()
-        digisosKommuner["1234"] = KommuneInfo(
-            kommunenummer = "1234",
-            kanMottaSoknader = true,
-            kanOppdatereStatus = true,
-            harMidlertidigDeaktivertMottak = false,
-            harMidlertidigDeaktivertOppdateringer = false,
-            kontaktpersoner = null,
-            harNksTilgang = false,
-            behandlingsansvarlig = null
-        )
-
-        val mappedeKommuneStatuser = ressurs.mapDigisosKommunerTilKommunestatus(digisosKommuner)
-        val mergedKommuneStatuser = ressurs.mergeManuelleKommunerMedDigisosKommunerKommunestatus(manueltMappedeKommuner, mappedeKommuneStatuser)
-        assertThat(mergedKommuneStatuser).hasSize(1)
-        assertThat(mergedKommuneStatuser["1234"]?.kanOppdatereStatus).isTrue
     }
 
     @Test
