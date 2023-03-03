948593a58808b4f04df33f3806fddd49700d4983
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2023-03-03 14:04:54 +0100

    test-rydding adressesok pakke

--- src/test/kotlin/no/nav/sosialhjelp/soknad/adressesok/sok/AdresseStringSplitterTest.kt
+++ src/test/kotlin/no/nav/sosialhjelp/soknad/adressesok/sok/AdresseStringSplitterTest.kt
@@ -11,159 +11,167 @@ internal class AdresseStringSplitterTest {
 
     @Test
     fun tomStrengGirBlanktSvar() {
-        assertThat(toSokedata(null, "")!!.adresse).isBlank
+        val sokedata = toSokedata(kodeverkService = null, adresse = "")
+        assertThat(sokedata!!.adresse).isBlank
     }
 
     @Test
     fun nullStrengGirNullSvar() {
-        assertThat(toSokedata(null, null)!!.adresse).isNull()
+        val sokedata = toSokedata(kodeverkService = null, adresse = null)
+        assertThat(sokedata!!.adresse).isNull()
     }
 
     @Test
     fun kunAdresseVirker() {
-        assertThat(toSokedata(null, "asdf")!!.adresse).isEqualTo("asdf")
+        val sokedata = toSokedata(kodeverkService = null, adresse = "asdf")
+        assertThat(sokedata!!.adresse).isEqualTo("asdf")
     }
 
     @Test
     fun husnummer() {
-        val result = toSokedata(null, "asdf 2")
-        assertThat(result!!.adresse).isEqualTo("asdf")
-        assertThat(result.husnummer).isEqualTo("2")
+        val sokedata = toSokedata(kodeverkService = null, adresse = "asdf 2")
+        assertThat(sokedata!!.adresse).isEqualTo("asdf")
+        assertThat(sokedata.husnummer).isEqualTo("2")
     }
 
     @Test
     fun kunHusnummer() {
-        val result = toSokedata(null, "234")
-        assertThat(result!!.husnummer).isEqualTo("234")
+        val sokedata = toSokedata(kodeverkService = null, adresse = "234")
+        assertThat(sokedata!!.husnummer).isEqualTo("234")
     }
 
     @Test
     fun husbokstav() {
-        val result = toSokedata(null, "asdf 2G")
-        assertThat(result!!.adresse).isEqualTo("asdf")
-        assertThat(result.husnummer).isEqualTo("2")
-        assertThat(result.husbokstav).isEqualTo("G")
+        val sokedata = toSokedata(kodeverkService = null, adresse = "asdf 2G")
+        assertThat(sokedata!!.adresse).isEqualTo("asdf")
+        assertThat(sokedata.husnummer).isEqualTo("2")
+        assertThat(sokedata.husbokstav).isEqualTo("G")
     }
 
     @Test
     fun husnummerOgBokstav() {
-        val result = toSokedata(null, "212G")
-        assertThat(result!!.husnummer).isEqualTo("212")
-        assertThat(result.husbokstav).isEqualTo("G")
+        val sokedata = toSokedata(kodeverkService = null, adresse = "212G")
+        assertThat(sokedata!!.husnummer).isEqualTo("212")
+        assertThat(sokedata.husbokstav).isEqualTo("G")
     }
 
     @Test
     fun postnummer() {
-        val result = toSokedata(null, "asdf 2G, 0882")
-        assertThat(result!!.adresse).isEqualTo("asdf")
-        assertThat(result.husnummer).isEqualTo("2")
-        assertThat(result.husbokstav).isEqualTo("G")
-        assertThat(result.postnummer).isEqualTo("0882")
+        val sokedata = toSokedata(kodeverkService = null, adresse = "asdf 2G, 0882")
+        assertThat(sokedata!!.adresse).isEqualTo("asdf")
+        assertThat(sokedata.husnummer).isEqualTo("2")
+        assertThat(sokedata.husbokstav).isEqualTo("G")
+        assertThat(sokedata.postnummer).isEqualTo("0882")
     }
 
     @Test
     fun postnummerMedMellomromFlyttet() {
-        val result = toSokedata(null, "asdf 2G ,0882")
-        assertThat(result!!.adresse).isEqualTo("asdf")
-        assertThat(result.husnummer).isEqualTo("2")
-        assertThat(result.husbokstav).isEqualTo("G")
-        assertThat(result.postnummer).isEqualTo("0882")
+        val sokedata = toSokedata(kodeverkService = null, adresse = "asdf 2G ,0882")
+        assertThat(sokedata!!.adresse).isEqualTo("asdf")
+        assertThat(sokedata.husnummer).isEqualTo("2")
+        assertThat(sokedata.husbokstav).isEqualTo("G")
+        assertThat(sokedata.postnummer).isEqualTo("0882")
     }
 
     @Test
     fun poststed() {
-        val result = toSokedata(null, "asdf 2G, 0882 OSLO")
-        assertThat(result!!.adresse).isEqualTo("asdf")
-        assertThat(result.husnummer).isEqualTo("2")
-        assertThat(result.husbokstav).isEqualTo("G")
-        assertThat(result.postnummer).isEqualTo("0882")
-        assertThat(result.poststed).isEqualTo("OSLO")
+        val sokedata = toSokedata(kodeverkService = null, adresse = "asdf 2G, 0882 OSLO")
+        assertThat(sokedata!!.adresse).isEqualTo("asdf")
+        assertThat(sokedata.husnummer).isEqualTo("2")
+        assertThat(sokedata.husbokstav).isEqualTo("G")
+        assertThat(sokedata.postnummer).isEqualTo("0882")
+        assertThat(sokedata.poststed).isEqualTo("OSLO")
     }
 
     @Test
     fun kunGateOgPostnummer() {
-        val result = toSokedata(null, "Veivei, 0110 ")
-        assertThat(result!!.adresse).isEqualTo("Veivei")
-        assertThat(result.postnummer).isEqualTo("0110")
+        val sokedata = toSokedata(kodeverkService = null, adresse = "Veivei, 0110 ")
+        assertThat(sokedata!!.adresse).isEqualTo("Veivei")
+        assertThat(sokedata.postnummer).isEqualTo("0110")
     }
 
     @Test
     fun kunGateOgPoststed() {
-        val result = toSokedata(null, "Veivei, OSLO")
-        assertThat(result!!.adresse).isEqualTo("Veivei")
-        assertThat(result.poststed).isEqualTo("OSLO")
+        val sokedata = toSokedata(kodeverkService = null, adresse = "Veivei, OSLO")
+        assertThat(sokedata!!.adresse).isEqualTo("Veivei")
+        assertThat(sokedata.poststed).isEqualTo("OSLO")
     }
 
     @Test
     fun dobbeltnavnPlussDiverseMellomrom() {
-        val result = toSokedata(null, "    Nedre Glommas    Vei   211G  ,  0882  ØVRE OSLO   ")
-        assertThat(result!!.adresse).isEqualTo("Nedre Glommas Vei")
-        assertThat(result.husnummer).isEqualTo("211")
-        assertThat(result.husbokstav).isEqualTo("G")
-        assertThat(result.postnummer).isEqualTo("0882")
-        assertThat(result.poststed).isEqualTo("ØVRE OSLO")
+        val sokedata = toSokedata(
+            kodeverkService = null,
+            adresse = "    Nedre Glommas    Vei   211G  ,  0882  ØVRE OSLO   "
+        )
+        assertThat(sokedata!!.adresse).isEqualTo("Nedre Glommas Vei")
+        assertThat(sokedata.husnummer).isEqualTo("211")
+        assertThat(sokedata.husbokstav).isEqualTo("G")
+        assertThat(sokedata.postnummer).isEqualTo("0882")
+        assertThat(sokedata.poststed).isEqualTo("ØVRE OSLO")
     }
 
     @Test
     fun kompakt() {
-        val result = toSokedata(null, " Nedre Glommas Vei211G,0882ØVRE OSLO   ")
-        assertThat(result!!.adresse).isEqualTo("Nedre Glommas Vei")
-        assertThat(result.husnummer).isEqualTo("211")
-        assertThat(result.husbokstav).isEqualTo("G")
-        assertThat(result.postnummer).isEqualTo("0882")
-        assertThat(result.poststed).isEqualTo("ØVRE OSLO")
+        val sokedata = toSokedata(kodeverkService = null, adresse = " Nedre Glommas Vei211G,0882ØVRE OSLO   ")
+        assertThat(sokedata!!.adresse).isEqualTo("Nedre Glommas Vei")
+        assertThat(sokedata.husnummer).isEqualTo("211")
+        assertThat(sokedata.husbokstav).isEqualTo("G")
+        assertThat(sokedata.postnummer).isEqualTo("0882")
+        assertThat(sokedata.poststed).isEqualTo("ØVRE OSLO")
     }
 
     @Test
     fun poststedUtenPostnummer() {
-        val result = toSokedata(null, "asdf 2G OSLO")
-        assertThat(result!!.adresse).isEqualTo("asdf")
-        assertThat(result.husnummer).isEqualTo("2")
-        assertThat(result.husbokstav).isEqualTo("G")
-        assertThat(result.poststed).isEqualTo("OSLO")
+        val sokedata = toSokedata(kodeverkService = null, adresse = "asdf 2G OSLO")
+        assertThat(sokedata!!.adresse).isEqualTo("asdf")
+        assertThat(sokedata.husnummer).isEqualTo("2")
+        assertThat(sokedata.husbokstav).isEqualTo("G")
+        assertThat(sokedata.poststed).isEqualTo("OSLO")
     }
 
     @Test
     fun utenHusnummer() {
-        val result = toSokedata(null, "asdf, 0882 OSLO")
-        assertThat(result!!.adresse).isEqualTo("asdf")
-        assertThat(result.postnummer).isEqualTo("0882")
-        assertThat(result.poststed).isEqualTo("OSLO")
+        val sokedata = toSokedata(kodeverkService = null, adresse = "asdf, 0882 OSLO")
+        assertThat(sokedata!!.adresse).isEqualTo("asdf")
+        assertThat(sokedata.postnummer).isEqualTo("0882")
+        assertThat(sokedata.poststed).isEqualTo("OSLO")
     }
 
     @Test
     fun skalKunneSokeMedKommunenavn() {
         val kodeverkService = mockk<KodeverkService>()
         every { kodeverkService.gjettKommunenummer(any()) } returns "0301"
-        val result = toSokedata(kodeverkService, "asdf, OSLO")
-        assertThat(result!!.adresse).isEqualTo("asdf")
-        assertThat(result.poststed).isNull()
-        assertThat(result.kommunenummer).isEqualTo("0301")
+
+        val sokedata = toSokedata(kodeverkService = kodeverkService, adresse = "asdf, OSLO")
+        assertThat(sokedata!!.adresse).isEqualTo("asdf")
+        assertThat(sokedata.poststed).isNull()
+        assertThat(sokedata.kommunenummer).isEqualTo("0301")
     }
 
     @Test
     fun skalFungereMedPoststedSelvMedKodeverk() {
         val kodeverkService = mockk<KodeverkService>()
         every { kodeverkService.gjettKommunenummer(any()) } returns "0301"
-        val result = toSokedata(kodeverkService, "asdf, 0756 OSLO")
-        assertThat(result!!.adresse).isEqualTo("asdf")
-        assertThat(result.postnummer).isEqualTo("0756")
-        assertThat(result.poststed).isEqualTo("OSLO")
-        assertThat(result.kommunenummer).isNull()
+
+        val sokedata = toSokedata(kodeverkService = kodeverkService, adresse = "asdf, 0756 OSLO")
+        assertThat(sokedata!!.adresse).isEqualTo("asdf")
+        assertThat(sokedata.postnummer).isEqualTo("0756")
+        assertThat(sokedata.poststed).isEqualTo("OSLO")
+        assertThat(sokedata.kommunenummer).isNull()
     }
 
     @Test
     fun `postnummer fra 1 til 4 tegn`() {
-        var result = toSokedata(null, "asdf 2G, 0")
-        assertThat(result!!.postnummer).isEqualTo("0")
+        var sokedata = toSokedata(kodeverkService = null, adresse = "asdf 2G, 0")
+        assertThat(sokedata!!.postnummer).isEqualTo("0")
 
-        result = toSokedata(null, "asdf 2G, 03")
-        assertThat(result!!.postnummer).isEqualTo("03")
+        sokedata = toSokedata(kodeverkService = null, adresse = "asdf 2G, 03")
+        assertThat(sokedata!!.postnummer).isEqualTo("03")
 
-        result = toSokedata(null, "asdf 2G, 030")
-        assertThat(result!!.postnummer).isEqualTo("030")
+        sokedata = toSokedata(kodeverkService = null, adresse = "asdf 2G, 030")
+        assertThat(sokedata!!.postnummer).isEqualTo("030")
 
-        result = toSokedata(null, "asdf 2G, 0301")
-        assertThat(result!!.postnummer).isEqualTo("0301")
+        sokedata = toSokedata(kodeverkService = null, adresse = "asdf 2G, 0301")
+        assertThat(sokedata!!.postnummer).isEqualTo("0301")
     }
 }
