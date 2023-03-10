a282cc8f8cb250aa2442e8f05b28e0351fef29fd
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2023-03-10 11:29:26 +0100

    type og gruppe ikke-nullable vedleggStatus og filer readonly

--- src/main/kotlin/no/nav/sosialhjelp/soknad/okonomiskeopplysninger/dto/VedleggFrontend.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/okonomiskeopplysninger/dto/VedleggFrontend.kt
@@ -1,12 +1,15 @@
 package no.nav.sosialhjelp.soknad.okonomiskeopplysninger.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
 import no.nav.sosialhjelp.soknad.vedlegg.dto.FilFrontend
 
 data class VedleggFrontend(
-    val type: VedleggType?,
-    val gruppe: VedleggGruppe?,
+    val type: VedleggType,
+    val gruppe: VedleggGruppe,
     val rader: List<VedleggRadFrontend>? = null,
+    @Schema(readOnly = true)
     val vedleggStatus: VedleggStatus? = null,
+    @Schema(readOnly = true)
     val filer: List<FilFrontend>?,
 )
 
