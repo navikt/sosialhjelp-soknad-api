a8c5a434ac3101f87d4ffc1bf11ace8f4c58693c
Amund Sørumshagen <amund.sorumshagen@nav.no>
2023-09-20 12:22:05 +0200

    Test-klasser håndtert

--- src/test/kotlin/no/nav/sosialhjelp/soknad/db/repositories/opplastetvedlegg/BatchOpplastetVedleggRepositoryJdbcTest.kt
+++ src/test/kotlin/no/nav/sosialhjelp/soknad/db/repositories/opplastetvedlegg/BatchOpplastetVedleggRepositoryJdbcTest.kt
@@ -3,6 +3,7 @@ package no.nav.sosialhjelp.soknad.db.repositories.opplastetvedlegg
 import jakarta.inject.Inject
 import no.nav.sosialhjelp.soknad.db.DbTestConfig
 import no.nav.sosialhjelp.soknad.vedlegg.VedleggUtils.getSha512FromByteArray
+import org.apache.commons.lang3.RandomUtils
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
@@ -11,6 +12,7 @@ import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import java.util.UUID
 
 @ExtendWith(SpringExtension::class)
 @ContextConfiguration(classes = [DbTestConfig::class])
@@ -35,9 +37,14 @@ internal class BatchOpplastetVedleggRepositoryJdbcTest {
     @Test
     fun slettAlleVedleggForSoknadSletterAlleOpplastedeVedleggForGittSoknadId() {
         val uuid = opprettOpplastetVedleggOgLagreIDb(lagOpplastetVedlegg(), EIER)
-        val uuidSammeSoknadOgEier = opprettOpplastetVedleggOgLagreIDb(lagOpplastetVedlegg(EIER, TYPE, SOKNADID), EIER)
+        val uuidSammeSoknadOgEier = opprettOpplastetVedleggOgLagreIDb(
+            lagOpplastetVedlegg(EIER, TYPE, SOKNADID),
+            EIER
+        )
         val uuidSammeEierOgAnnenSoknad =
-            opprettOpplastetVedleggOgLagreIDb(lagOpplastetVedlegg(EIER, TYPE2, SOKNADID3), EIER)
+            opprettOpplastetVedleggOgLagreIDb(
+                lagOpplastetVedlegg(EIER, TYPE2, SOKNADID3), EIER
+            )
         batchOpplastetVedleggRepository.slettAlleVedleggForSoknad(SOKNADID)
         assertThat(opplastetVedleggRepository.hentVedlegg(uuid, EIER)).isNull()
         assertThat(opplastetVedleggRepository.hentVedlegg(uuidSammeSoknadOgEier, EIER)).isNull()
@@ -53,9 +60,10 @@ internal class BatchOpplastetVedleggRepositoryJdbcTest {
             eier = eier,
             vedleggType = OpplastetVedleggType(type),
             data = DATA,
+            uuid = UUID.nameUUIDFromBytes(DATA).toString(),
             soknadId = soknadId,
             filnavn = FILNAVN,
-            sha512 = SHA512
+            sha512 = getSha512FromByteArray(DATA)
         )
     }
 
@@ -65,8 +73,7 @@ internal class BatchOpplastetVedleggRepositoryJdbcTest {
 
     companion object {
         private const val EIER = "12345678901"
-        private val DATA = byteArrayOf(1, 2, 3, 4)
-        private val SHA512 = getSha512FromByteArray(DATA)
+        private val DATA = RandomUtils.nextBytes(10)
         private const val TYPE = "bostotte|annetboutgift"
         private const val TYPE2 = "dokumentasjon|aksjer"
         private const val SOKNADID = 1L
---
+++ src/test/kotlin/no/nav/sosialhjelp/soknad/vedlegg/konvertering/FilKonverteringTest.kt
@@ -0,0 +1,120 @@
+package no.nav.sosialhjelp.soknad.vedlegg.konvertering
+
+import io.mockk.clearAllMocks
+import io.mockk.every
+import io.mockk.mockkObject
+import no.nav.sosialhjelp.kotlin.utils.pdf.filkonvertering.csv.CsvToPdfConverter
+import no.nav.sosialhjelp.kotlin.utils.pdf.filkonvertering.excel.ExcelToPdfConverter
+import no.nav.sosialhjelp.kotlin.utils.pdf.filkonvertering.exception.CsvKonverteringException
+import no.nav.sosialhjelp.kotlin.utils.pdf.filkonvertering.exception.ExcelKonverteringException
+import no.nav.sosialhjelp.kotlin.utils.pdf.filkonvertering.exception.WordKonverteringException
+import no.nav.sosialhjelp.kotlin.utils.pdf.filkonvertering.word.WordToPdfConverter
+import no.nav.sosialhjelp.soknad.util.ExampleFileRepository
+import no.nav.sosialhjelp.soknad.util.ExampleFileRepository.CSV_FILE
+import no.nav.sosialhjelp.soknad.util.ExampleFileRepository.EXCEL_FILE
+import no.nav.sosialhjelp.soknad.util.ExampleFileRepository.TEXT_FILE
+import no.nav.sosialhjelp.soknad.util.ExampleFileRepository.WORD_FILE
+import no.nav.sosialhjelp.soknad.vedlegg.exceptions.KonverteringTilPdfException
+import no.nav.sosialhjelp.soknad.vedlegg.filedetection.FileDetectionUtils.detectMimeType
+import no.nav.sosialhjelp.soknad.vedlegg.filedetection.MimeTypes
+import no.nav.sosialhjelp.soknad.vedlegg.konvertering.FilKonvertering.konverterHvisStottet
+import no.nav.sosialhjelp.soknad.vedlegg.konvertering.StottetFiltype.FiltypeUtil.finnKonverterer
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Test
+
+class FilKonverteringTest {
+
+    @Test
+    fun `Konverter fil hvis stottet format`() {
+        val orgNavn = EXCEL_FILE.name
+        val orgData = EXCEL_FILE.readBytes()
+
+        val (filnavn, data) = konverterHvisStottet(orgNavn, orgData)
+        assertThat(orgNavn).isNotEqualTo(filnavn)
+        assertThat(filnavn).contains(".pdf")
+        assertThat(detectMimeType(data)).isEqualTo("application/pdf")
+    }
+
+    @Test
+    fun `Fil rores ikke hvis formatet ikke er stottet`() {
+        val orgNavn = TEXT_FILE.name
+        val orgData = TEXT_FILE.readBytes()
+
+        val (filnavn, data) = konverterHvisStottet(orgNavn, orgData)
+        assertThat(orgNavn).isEqualTo(filnavn)
+        assertThat(orgData).isEqualTo(data)
+        assertThat(detectMimeType(orgData)).isEqualTo(detectMimeType(data))
+    }
+
+    @Test
+    fun `Test konverter excel-fil (xlsx) stottes`() {
+        val stottetFiltype = finnKonverterer(EXCEL_FILE.readBytes(), EXCEL_FILE.name)
+        assertThat(stottetFiltype).isEqualTo(StottetFiltype.EXCEL)
+
+        val konvertertFilBytes = stottetFiltype!!.getFiltypeConverter().konverterTilPdf(EXCEL_FILE.readBytes())
+        assertThat(detectMimeType(konvertertFilBytes)).isEqualTo(MimeTypes.APPLICATION_PDF)
+    }
+
+    @Test
+    fun `Test excel-fil (xls) stottes ikke`() {
+        val stottetFiltype = finnKonverterer(
+            ExampleFileRepository.EXCEL_FILE_OLD.readBytes(),
+            ExampleFileRepository.EXCEL_FILE_OLD.name
+        )
+        assertThat(stottetFiltype).isNull()
+    }
+
+    @Test
+    fun `Test word-fil (docx) stottes`() {
+        val stottetFiltype = finnKonverterer(WORD_FILE.readBytes(), WORD_FILE.name)
+        assertThat(stottetFiltype).isEqualTo(StottetFiltype.WORD)
+
+        val konvertertFilBytes = stottetFiltype!!.getFiltypeConverter().konverterTilPdf(WORD_FILE.readBytes())
+        assertThat(detectMimeType(konvertertFilBytes)).isEqualTo(MimeTypes.APPLICATION_PDF)
+    }
+
+    @Test
+    fun `Test word-fil (doc) stottes ikke`() {
+        val stottetFiltype = finnKonverterer(
+            ExampleFileRepository.WORD_FILE_OLD.readBytes(),
+            ExampleFileRepository.WORD_FILE_OLD.name
+        )
+        assertThat(stottetFiltype).isNull()
+    }
+
+    @Test
+    fun `Test csv-fil (csv) stottes`() {
+        val stottetFiltype = finnKonverterer(CSV_FILE.readBytes(), CSV_FILE.name)
+        assertThat(stottetFiltype).isEqualTo(StottetFiltype.CSV)
+
+        val konvertertPdfBytes = stottetFiltype!!.getFiltypeConverter().konverterTilPdf(CSV_FILE.readBytes())
+        assertThat(detectMimeType(konvertertPdfBytes)).isEqualTo(MimeTypes.APPLICATION_PDF)
+    }
+
+    @Test
+    fun `Kaster KonverteringException hvis konvertering feiler`() {
+
+        mockkObject(ExcelToPdfConverter)
+        mockkObject(WordToPdfConverter)
+        mockkObject(CsvToPdfConverter)
+
+        every { ExcelToPdfConverter.konverterTilPdf(any()) } throws ExcelKonverteringException("feil", null)
+        every { WordToPdfConverter.konverterTilPdf(any()) } throws WordKonverteringException("feil", null)
+        every { CsvToPdfConverter.konverterTilPdf(any()) } throws CsvKonverteringException("feil", null)
+
+        assertThatThrownBy { konverterHvisStottet(EXCEL_FILE.name, EXCEL_FILE.readBytes()) }
+            .isInstanceOf(KonverteringTilPdfException::class.java)
+            .hasCauseInstanceOf(ExcelKonverteringException::class.java)
+
+        assertThatThrownBy { konverterHvisStottet(WORD_FILE.name, WORD_FILE.readBytes()) }
+            .isInstanceOf(KonverteringTilPdfException::class.java)
+            .hasCauseInstanceOf(WordKonverteringException::class.java)
+
+        assertThatThrownBy { konverterHvisStottet(CSV_FILE.name, CSV_FILE.readBytes()) }
+            .isInstanceOf(KonverteringTilPdfException::class.java)
+            .hasCauseInstanceOf(CsvKonverteringException::class.java)
+
+        clearAllMocks()
+    }
+}
