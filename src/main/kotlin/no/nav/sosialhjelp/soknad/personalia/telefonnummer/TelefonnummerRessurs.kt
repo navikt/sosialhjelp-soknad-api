e8cb534e66db999e5b2f4c57833a3f956ced5a50
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-10-17 14:03:38 +0200

    [API] Returner TelefonnummerFrontend fra PUT (#1734)
    * Returner TelefonnummerFrontend også ved PUT
    * Oppdaterer tester med mock for systemverdi
    
    Testet i q0 med «gammel» frontend for bakoverkompatibilitet

--- src/main/kotlin/no/nav/sosialhjelp/soknad/personalia/telefonnummer/TelefonnummerRessurs.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/personalia/telefonnummer/TelefonnummerRessurs.kt
@@ -1,11 +1,10 @@
 package no.nav.sosialhjelp.soknad.personalia.telefonnummer
 
 import io.swagger.v3.oas.annotations.media.Schema
+import no.nav.sbl.soknadsosialhjelp.soknad.JsonInternalSoknad
 import no.nav.sbl.soknadsosialhjelp.soknad.common.JsonKilde
 import no.nav.sbl.soknadsosialhjelp.soknad.personalia.JsonTelefonnummer
-import no.nav.security.token.support.core.api.ProtectedWithClaims
-import no.nav.sosialhjelp.soknad.app.Constants
-import no.nav.sosialhjelp.soknad.app.subjecthandler.SubjectHandlerUtils
+import no.nav.sosialhjelp.soknad.app.annotation.ProtectionSelvbetjeningHigh
 import no.nav.sosialhjelp.soknad.db.repositories.soknadunderarbeid.SoknadUnderArbeidRepository
 import no.nav.sosialhjelp.soknad.tilgangskontroll.Tilgangskontroll
 import org.springframework.http.MediaType
@@ -15,9 +14,10 @@ import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import no.nav.sosialhjelp.soknad.app.subjecthandler.SubjectHandlerUtils.getUserIdFromToken as eier
 
 @RestController
-@ProtectedWithClaims(issuer = Constants.SELVBETJENING, claimMap = [Constants.CLAIM_ACR_LEVEL_4, Constants.CLAIM_ACR_LOA_HIGH], combineWithOr = true)
+@ProtectionSelvbetjeningHigh
 @RequestMapping("/soknader/{behandlingsId}/personalia/telefonnummer", produces = [MediaType.APPLICATION_JSON_VALUE])
 class TelefonnummerRessurs(
     private val tilgangskontroll: Tilgangskontroll,
@@ -29,30 +29,21 @@ class TelefonnummerRessurs(
         @PathVariable("behandlingsId") behandlingsId: String?
     ): TelefonnummerFrontend {
         tilgangskontroll.verifiserAtBrukerHarTilgang()
-        val eier = SubjectHandlerUtils.getUserIdFromToken()
+        val eier = eier()
         val soknadUnderArbeid = soknadUnderArbeidRepository.hentSoknad(behandlingsId, eier)
         val jsonInternalSoknad = soknadUnderArbeid.jsonInternalSoknad
             ?: throw IllegalStateException("Kan ikke hente søknaddata hvis SoknadUnderArbeid.jsonInternalSoknad er null")
-        val telefonnummer = jsonInternalSoknad.soknad.data.personalia.telefonnummer
-        val systemverdi: String? = if (telefonnummer != null && telefonnummer.kilde == JsonKilde.SYSTEM) {
-            telefonnummer.verdi
-        } else {
-            telefonnummerSystemdata.innhentSystemverdiTelefonnummer(eier)
-        }
-        return TelefonnummerFrontend(
-            brukerdefinert = telefonnummer == null || telefonnummer.kilde == JsonKilde.BRUKER,
-            systemverdi = systemverdi,
-            brukerutfyltVerdi = telefonnummer?.takeIf { it.kilde == JsonKilde.BRUKER }?.verdi
-        )
+
+        return getTelefonnummer(jsonInternalSoknad)
     }
 
     @PutMapping
     fun updateTelefonnummer(
         @PathVariable("behandlingsId") behandlingsId: String?,
         @RequestBody telefonnummerFrontend: TelefonnummerFrontend
-    ) {
+    ): TelefonnummerFrontend {
         tilgangskontroll.verifiserAtBrukerKanEndreSoknad(behandlingsId)
-        val eier = SubjectHandlerUtils.getUserIdFromToken()
+        val eier = eier()
         val soknad = soknadUnderArbeidRepository.hentSoknad(behandlingsId, eier)
         val jsonInternalSoknad = soknad.jsonInternalSoknad
             ?: throw IllegalStateException("Kan ikke oppdatere søknaddata hvis SoknadUnderArbeid.jsonInternalSoknad er null")
@@ -71,6 +62,21 @@ class TelefonnummerRessurs(
             telefonnummerSystemdata.updateSystemdataIn(soknad)
         }
         soknadUnderArbeidRepository.oppdaterSoknadsdata(soknad, eier)
+        return getTelefonnummer(jsonInternalSoknad)
+    }
+
+    private fun getTelefonnummer(internal: JsonInternalSoknad): TelefonnummerFrontend {
+        val telefonnummer = internal.soknad.data.personalia.telefonnummer
+        val systemverdi: String? = if (telefonnummer != null && telefonnummer.kilde == JsonKilde.SYSTEM) {
+            telefonnummer.verdi
+        } else {
+            telefonnummerSystemdata.innhentSystemverdiTelefonnummer(eier())
+        }
+        return TelefonnummerFrontend(
+            brukerdefinert = telefonnummer == null || telefonnummer.kilde == JsonKilde.BRUKER,
+            systemverdi = systemverdi,
+            brukerutfyltVerdi = telefonnummer?.takeIf { it.kilde == JsonKilde.BRUKER }?.verdi
+        )
     }
 
     data class TelefonnummerFrontend(
