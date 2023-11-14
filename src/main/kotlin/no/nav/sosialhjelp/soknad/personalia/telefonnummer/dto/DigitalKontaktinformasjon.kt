e5a7a1c2d39a3c1f143ebd7317613021eee10031
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-11-14 19:27:18 +0100

    R!! - bruk standard cache for KRR

--- src/main/kotlin/no/nav/sosialhjelp/soknad/personalia/telefonnummer/dto/DigitalKontaktinformasjon.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/personalia/telefonnummer/dto/DigitalKontaktinformasjon.kt
@@ -1,9 +1,11 @@
 package no.nav.sosialhjelp.soknad.personalia.telefonnummer.dto
 
+import java.io.Serializable
+
 data class DigitalKontaktinformasjon(
     val personident: String,
     val aktiv: Boolean,
     val kanVarsles: Boolean?,
     val reservert: Boolean?,
     val mobiltelefonnummer: String?
-)
+) : Serializable
