61e02ca3d9396e30539eab4219a57e8871ea624e
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-11-27 12:38:05 +0100

    Delay-interceptor for 409-skadekontroll (#1729)
    * Delay-interceptor
    
    Konflikter forebygges ved å forsinke lesing av søknaden om den vil etterfølges av skriving, til forrige transaksjon er landet.
    En request som fremstår som den kommer til å kjøre oppdaterSoknadsdata mot en gitt behandlingsId vil stå i kø bak andre i samme båt, i opptil RequestDelayService.LOCK_TIMEOUT_MS.
    Alle andre requests har alltid grønt lys.
    For kontrollere som bruker ny datamodell vil vi kunne bruke et annet navn på @PathVariable (f. eks. ":soknadId"), da vil denne ikke invokeres.

---
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/app/soknadlock/SoknadLockPullMetrics.kt
@@ -0,0 +1,35 @@
+package no.nav.sosialhjelp.soknad.app.soknadlock
+
+import io.micrometer.core.instrument.Gauge
+import io.micrometer.core.instrument.MeterRegistry
+import org.springframework.stereotype.Component
+
+/**
+ * Denne klassen henter data fra SoknadLockManager og rapporterer til Prometheus.
+ * Se også SoknadLockPushMetrics, som kalles av SoknadLockManager for å rapportere data.
+ *
+ * @property meterRegistry Prometheus MeterRegistry
+ * @property lockManager SoknadLockManager for å hente data om tilstanden på lockMap.
+ */
+@Component
+@Suppress("unused")
+class SoknadLockPullMetrics(
+    private val meterRegistry: MeterRegistry,
+    private val lockManager: SoknadLockManager
+) {
+    private val metricsGauges = mutableListOf<Gauge>()
+
+    init {
+        metricsGauges.add(
+            Gauge.builder("soknad_lock_map_size") { lockManager.getLockMapSize().toDouble() }
+                .description("Number of locks in the lock map")
+                .register(meterRegistry)
+        )
+
+        metricsGauges.add(
+            Gauge.builder("soknad_lock_held_count") { lockManager.getNumLocks().toDouble() }
+                .description("Number of locks currently held")
+                .register(meterRegistry)
+        )
+    }
+}
