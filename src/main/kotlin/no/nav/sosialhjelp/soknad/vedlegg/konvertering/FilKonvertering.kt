fe0120e453a9108495e4d94270144570d47ba6cb
Amund Sørumshagen <amund.sorumshagen@nav.no>
2023-11-28 18:01:51 +0100

    Test som prøver å sende inn filtype som ikke er stottet

--- src/main/kotlin/no/nav/sosialhjelp/soknad/vedlegg/konvertering/FilKonvertering.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/vedlegg/konvertering/FilKonvertering.kt
@@ -14,8 +14,7 @@ object FilKonvertering {
                 byttExtension(orginaltFilnavn),
                 konverter(it.getFiltypeConverter(), sourceData)
             )
-        }
-            ?: Pair(orginaltFilnavn, sourceData)
+        } ?: throw IllegalArgumentException("Konvertering av filtype ikke stottet")
     }
 
     private fun konverter(konverterer: FilTilPdfConverter, sourceData: ByteArray) =
