34679f0178ed194e27edc30e9da0fb93036369fa
Amund Sørumshagen <amund.sorumshagen@nav.no>
2023-11-28 14:32:59 +0100

    Ktlint....................................

--- src/test/kotlin/no/nav/sosialhjelp/soknad/app/filter/MdcFilterTest.kt
+++ src/test/kotlin/no/nav/sosialhjelp/soknad/app/filter/MdcFilterTest.kt
@@ -15,7 +15,6 @@ import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatExceptionOfType
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.slf4j.MDC
 import org.springframework.mock.web.MockFilterChain
