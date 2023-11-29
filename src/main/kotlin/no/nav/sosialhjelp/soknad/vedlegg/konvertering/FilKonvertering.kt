693ace80b62eb02b76a1b68c1bb39a21af1a83a6
Amund Sørumshagen <amund.sorumshagen@nav.no>
2023-11-29 14:10:58 +0100

    Skiller ut endepunkt i egen controller

--- src/main/kotlin/no/nav/sosialhjelp/soknad/vedlegg/konvertering/FilKonvertering.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/vedlegg/konvertering/FilKonvertering.kt
@@ -14,7 +14,7 @@ object FilKonvertering {
                 byttExtension(orginaltFilnavn),
                 konverter(it.getFiltypeConverter(), sourceData)
             )
-        } ?: throw IllegalArgumentException("Konvertering av filtype ikke stottet")
+        } ?: throw KonverteringTilPdfException("Konvertering av filtype ikke stottet", null, null)
     }
 
     private fun konverter(konverterer: FilTilPdfConverter, sourceData: ByteArray) =
