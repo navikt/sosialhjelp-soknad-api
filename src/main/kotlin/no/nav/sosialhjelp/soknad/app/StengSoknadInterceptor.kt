53d0bb3481370990a93994aa3e8c6bbf9c944614
Amund Sørumshagen <amund.sorumshagen@nav.no>
2023-12-19 10:12:41 +0100

    Interceptor som avviser alle kall i gitt tidsperiode

---
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/app/StengSoknadInterceptor.kt
@@ -0,0 +1,37 @@
+package no.nav.sosialhjelp.soknad.app
+
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import no.nav.sosialhjelp.soknad.app.exceptions.TjenesteUtilgjengeligException
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.HandlerInterceptor
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import java.time.LocalDateTime
+import java.time.Month
+
+
+@Configuration
+class StengeSoknadConfig: WebMvcConfigurer {
+
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(StengSoknadInterceptor)
+    }
+
+    companion object StengSoknadInterceptor: HandlerInterceptor {
+        override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
+//            val nedePeriodeStart = LocalDateTime.of(2024, Month.JANUARY, 1, 0, 0)
+//            val nedePeriodeSlutt = LocalDateTime.of(2024, Month.JANUARY, 2, 8, 0)
+
+            val nedePeriodeStart = LocalDateTime.of(2023, Month.DECEMBER, 19, 0, 0)
+            val nedePeriodeSlutt = LocalDateTime.of(2023, Month.DECEMBER, 20, 8, 0)
+
+            LocalDateTime.now().let {
+                if (it.isAfter(nedePeriodeStart) && it.isBefore(nedePeriodeSlutt)) {
+                    throw TjenesteUtilgjengeligException("Søknaden er midlertidig stengt.", null)
+                }
+            }
+            return true
+        }
+    }
+}
