fbf874183437ea65dd495f5052458c52bd401994
Amund Sørumshagen <amund.sorumshagen@nav.no>
2023-12-19 16:16:08 +0100

    Uten while-løkke og return

--- src/main/kotlin/no/nav/sosialhjelp/soknad/db/repositories/soknadunderarbeid/SlettSoknaderScheduler.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/db/repositories/soknadunderarbeid/SlettSoknaderScheduler.kt
@@ -16,29 +16,32 @@ class SlettSoknaderScheduler(
     private val log by logger()
 
     @Scheduled(cron = "0 */15 * * * *")
-    fun slettAlleSoknaderUnderArbeid1Jan(retry: Int = 0) {
+    fun slettAlleSoknaderUnderArbeid1Jan() {
 
         if (leaderElection.isLeader()) {
-            if (retry < 5) {
-                log.info("Forsøk ${retry + 1} av 5: Starter sletting av alle soknader under arbeid")
+            slettSoknader()
+        }
+    }
+    private fun slettSoknader(retry: Int = 0) {
+        if (retry < 5) {
+            log.info("Forsøk ${retry + 1} av 5: Starter sletting av alle soknader under arbeid")
 
-                try {
-                    jdbcTemplate.update(
-                        "DELETE FROM SOKNADMETADATA WHERE innsendingstatus = ? AND soknadtype = ?",
-                        SoknadMetadataInnsendingStatus.UNDER_ARBEID.name,
-                        SoknadMetadataType.SEND_SOKNAD_KOMMUNAL.name
-                    ).also {
-                        log.info("Slettet $it rader fra SOKNADMETADATA")
-                    }
-
-                    jdbcTemplate.update("DELETE FROM SOKNAD_UNDER_ARBEID").also {
-                        log.info("Slettet $it rader fra SOKNAD_UNDER_ARBEID")
-                    }
-                } catch (e: RuntimeException) {
-                    log.error("Sletting av Soknader under arbeid feilet", e)
-                    Thread.sleep(5000) // vent 5 sekunder
-                    slettAlleSoknaderUnderArbeid1Jan(retry + 1)
+            try {
+                jdbcTemplate.update(
+                    "DELETE FROM SOKNADMETADATA WHERE innsendingstatus = ? AND soknadtype = ?",
+                    SoknadMetadataInnsendingStatus.UNDER_ARBEID.name,
+                    SoknadMetadataType.SEND_SOKNAD_KOMMUNAL.name
+                ).also {
+                    log.info("Slettet $it rader fra SOKNADMETADATA")
                 }
+
+                jdbcTemplate.update("DELETE FROM SOKNAD_UNDER_ARBEID").also {
+                    log.info("Slettet $it rader fra SOKNAD_UNDER_ARBEID")
+                }
+            } catch (e: RuntimeException) {
+                log.error("Sletting av Soknader under arbeid feilet", e)
+                Thread.sleep(5000) // vent 5 sekunder
+                slettSoknader(retry + 1)
             }
         }
     }
