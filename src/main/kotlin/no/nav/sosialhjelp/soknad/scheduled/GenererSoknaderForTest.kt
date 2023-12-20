de179f6fba4f174ff5bc034c6278e85521d03554
Amund Sørumshagen <amund.sorumshagen@nav.no>
2023-12-20 09:56:43 +0100

    Test oppretting av ny soknad (for slette-purposes)

---
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/scheduled/GenererSoknaderForTest.kt
@@ -0,0 +1,81 @@
+package no.nav.sosialhjelp.soknad.scheduled
+
+import no.nav.sosialhjelp.soknad.app.LoggingUtils.logger
+import no.nav.sosialhjelp.soknad.app.mdc.MdcOperations
+import no.nav.sosialhjelp.soknad.app.subjecthandler.SubjectHandlerUtils
+import no.nav.sosialhjelp.soknad.app.systemdata.SystemdataUpdater
+import no.nav.sosialhjelp.soknad.db.repositories.soknadmetadata.SoknadMetadata
+import no.nav.sosialhjelp.soknad.db.repositories.soknadmetadata.SoknadMetadataInnsendingStatus
+import no.nav.sosialhjelp.soknad.db.repositories.soknadmetadata.SoknadMetadataRepository
+import no.nav.sosialhjelp.soknad.db.repositories.soknadmetadata.SoknadMetadataType
+import no.nav.sosialhjelp.soknad.db.repositories.soknadunderarbeid.SoknadUnderArbeid
+import no.nav.sosialhjelp.soknad.db.repositories.soknadunderarbeid.SoknadUnderArbeidRepository
+import no.nav.sosialhjelp.soknad.db.repositories.soknadunderarbeid.SoknadUnderArbeidStatus
+import no.nav.sosialhjelp.soknad.innsending.SenderUtils
+import no.nav.sosialhjelp.soknad.innsending.SoknadService
+import no.nav.sosialhjelp.soknad.scheduled.leaderelection.LeaderElection
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import java.time.Clock
+import java.time.LocalDateTime
+
+@Component
+class GenererSoknaderForTest(
+    private val leaderElection: LeaderElection,
+    private val soknadUnderArbeidRepository: SoknadUnderArbeidRepository,
+    private val systemdataUpdater: SystemdataUpdater,
+    private val soknadMetadataRepository: SoknadMetadataRepository,
+    private val clock: Clock
+) {
+
+    private val log by logger()
+    private val fnr = "05518232810"
+
+    @Scheduled(cron = "0 */15 * * * *")
+    fun fyllDbMedSoknader() {
+
+        log.info("Test - oppretter soknad for test")
+        val behandlingsId = startSoknad()
+        log.info("Test - opprettet soknad $behandlingsId for $fnr")
+
+    }
+
+    private fun startSoknad(): String {
+        val eier = fnr
+        val behandlingsId = opprettSoknadMetadata(eier)
+        MdcOperations.putToMDC(MdcOperations.MDC_BEHANDLINGS_ID, behandlingsId)
+
+        val soknadUnderArbeid = SoknadUnderArbeid(
+            versjon = 1L,
+            behandlingsId = behandlingsId,
+            tilknyttetBehandlingsId = null,
+            eier = eier,
+            jsonInternalSoknad = SoknadService.createEmptyJsonInternalSoknad(eier),
+            status = SoknadUnderArbeidStatus.UNDER_ARBEID,
+            opprettetDato = LocalDateTime.now(),
+            sistEndretDato = LocalDateTime.now()
+        )
+
+        systemdataUpdater.update(soknadUnderArbeid)
+        soknadUnderArbeidRepository.opprettSoknad(soknadUnderArbeid, eier)
+
+        return behandlingsId
+    }
+
+    private fun opprettSoknadMetadata(fnr: String): String {
+        log.info("Starter søknad")
+        val id = soknadMetadataRepository.hentNesteId()
+        val soknadMetadata = SoknadMetadata(
+            id = id,
+            behandlingsId = SenderUtils.lagBehandlingsId(id),
+            fnr = fnr,
+            skjema = SenderUtils.SKJEMANUMMER,
+            type = SoknadMetadataType.SEND_SOKNAD_KOMMUNAL,
+            status = SoknadMetadataInnsendingStatus.UNDER_ARBEID,
+            opprettetDato = LocalDateTime.now(clock),
+            sistEndretDato = LocalDateTime.now(clock)
+        )
+        soknadMetadataRepository.opprett(soknadMetadata)
+        return soknadMetadata.behandlingsId
+    }
+}
