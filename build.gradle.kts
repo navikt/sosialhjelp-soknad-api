e42e6ac4af66c814c1c9066304021a0012cd0150
Martin Almvik <martin.almvik@nav.no>
2024-01-04 12:22:24 +0100

    Bytter til libs.versions.toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,42 +2,14 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-object Versions {
-    const val springBoot = "3.2.0" // Husk å oppdatere plugin også
-    const val coroutines = "1.6.4"
-    const val filformat = "1.2023.06.21-14.54-583dfcc41d77"
-    const val sosialhjelpCommon = "1.20231205.0930-5ce172f"
-    const val fiksSvarUt = "1.2.0"
-    const val fiksKryptering = "1.3.1"
-    const val springdoc = "2.1.0"
-    const val flyway = "9.16.1" // Husk å oppdatere plugin også
-    const val ojdbc10 = "19.18.0.0"
-    const val hsqldb = "2.7.1"
-    const val lettuce = "6.2.3.RELEASE"
-    const val tokenValidation = "3.2.0"
-    const val javaJwt = "4.3.0"
-    const val prometheus = "0.16.0"
-    const val micrometer = "1.10.5"
-    const val jackson = "2.14.2"
-    const val logback = "1.4.5"
-    const val logstash = "7.3"
-    const val pdfbox = "3.0.0"
-    const val emojiJava = "5.1.1"
-    const val jakartaServlet = "6.0.0"
-    const val unleashClient = "8.4.0"
-    const val tika = "2.7.0"
-    const val commonsText = "1.10.0"
-    const val ktlint = "0.45.2"
-    const val mockk = "1.13.4"
-}
 
 plugins {
-    kotlin("jvm") version "1.9.20"
-    kotlin("plugin.spring") version "1.9.20"
-    id("org.springframework.boot") version "3.2.0"
-    id("org.flywaydb.flyway") version "10.0.1"
-    id("com.github.ben-manes.versions") version "0.50.0" // ./gradlew dependencyUpdates
-    id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.plugin.spring)
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.versions)
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.flyway)
 }
 
 java {
@@ -46,7 +18,7 @@ java {
 }
 
 ktlint {
-    this.version.set(Versions.ktlint)
+    this.version.set(libs.versions.ktlint)
 }
 
 flyway {
@@ -77,80 +49,70 @@ configurations {
 
 dependencies {
     // Spring
-    implementation("org.springframework.boot:spring-boot-starter-web:${Versions.springBoot}")
-    implementation("org.springframework.boot:spring-boot-starter-webflux:${Versions.springBoot}")
-    implementation("org.springframework.boot:spring-boot-starter-validation:${Versions.springBoot}")
-    implementation("org.springframework.boot:spring-boot-starter-jdbc:${Versions.springBoot}")
-    implementation("org.springframework.boot:spring-boot-starter-actuator:${Versions.springBoot}")
+    implementation(libs.bundles.spring.boot)
 
     // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:${Versions.coroutines}")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:${Versions.coroutines}")
+    implementation(libs.bundles.coroutines)
 
     // filformat
-    implementation("no.nav.sbl.dialogarena:soknadsosialhjelp-filformat:${Versions.filformat}")
+    implementation(libs.soknadsosialhjelp.filformat)
 
     // sosialhjelp-common
-    implementation("no.nav.sosialhjelp:sosialhjelp-common-api:${Versions.sosialhjelpCommon}")
-    implementation("no.nav.sosialhjelp:sosialhjelp-common-selftest:${Versions.sosialhjelpCommon}")
-    implementation("no.nav.sosialhjelp:sosialhjelp-common-kotlin-utils:${Versions.sosialhjelpCommon}")
+    implementation(libs.bundles.sosialhjelp.common)
 
     // KS / Fiks
-    implementation("no.ks.fiks.svarut:svarut-rest-klient:${Versions.fiksSvarUt}")
-    implementation("no.ks.fiks:kryptering:${Versions.fiksKryptering}")
+    implementation(libs.svarut.rest.klient)
+    implementation(libs.kryptering)
 
     // springdoc
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${Versions.springdoc}")
-    implementation("org.springdoc:springdoc-openapi-starter-common:${Versions.springdoc}")
+    implementation(libs.springdoc.openapi.starter.common)
+    implementation(libs.springdoc.openapi.starter.webmvc.ui)
 
     // flyway / db
-    implementation("org.flywaydb:flyway-core:${Versions.flyway}")
-    runtimeOnly("com.oracle.database.jdbc:ojdbc10:${Versions.ojdbc10}")
-    runtimeOnly("org.hsqldb:hsqldb:${Versions.hsqldb}")
+    implementation(libs.flyway.core)
+    runtimeOnly(libs.ojdbc10)
+    runtimeOnly(libs.hsqldb)
 
     // redis
-    implementation("io.lettuce:lettuce-core:${Versions.lettuce}")
+    implementation(libs.lettuce.core)
 
     // token validering
-    implementation("no.nav.security:token-validation-spring:${Versions.tokenValidation}")
-    implementation("com.auth0:java-jwt:${Versions.javaJwt}")
+    implementation(libs.token.validation.spring)
+    implementation(libs.java.jwt)
 
     // Micrometer/prometheus
-    implementation("io.prometheus:simpleclient:${Versions.prometheus}")
-    implementation("io.micrometer:micrometer-registry-prometheus:${Versions.micrometer}")
+    implementation(libs.simpleclient)
+    implementation(libs.micrometer.registry.prometheus)
 
     // jackson
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:${Versions.jackson}")
+    implementation(libs.jackson.module.kotlin)
 
     // Logging
-    implementation("ch.qos.logback:logback-classic:${Versions.logback}")
-    implementation("net.logstash.logback:logstash-logback-encoder:${Versions.logstash}")
+    implementation(libs.logback.classic)
+    implementation(libs.logstash.logback.encoder)
 
     // jakarta
-    implementation("jakarta.servlet:jakarta.servlet-api:${ Versions.jakartaServlet }")
+    implementation(libs.jakarta.servlet.api)
 
     // Unleash
-    implementation("io.getunleash:unleash-client-java:${Versions.unleashClient}")
+    implementation(libs.unleash.client.java)
 
     // Tika
-    implementation("org.apache.tika:tika-core:${Versions.tika}")
-    implementation("org.apache.tika:tika-parsers-standard-package:2.9.1")
+    implementation(libs.tika.core)
+    implementation(libs.tika.parsers.standard.`package`)
 
     // commons
-    implementation("org.apache.commons:commons-text:${Versions.commonsText}")
+    implementation(libs.commons.text)
 
     // pdf
-    implementation("org.apache.pdfbox:pdfbox:${Versions.pdfbox}")
-    implementation("org.apache.pdfbox:preflight:${Versions.pdfbox}")
-    implementation("org.apache.pdfbox:pdfbox-io:${Versions.pdfbox}")
-    implementation("com.vdurmont:emoji-java:${Versions.emojiJava}")
+    implementation(libs.bundles.pdfbox)
+    implementation(libs.emoji.java)
 
     // Test
-    testImplementation("org.springframework.boot:spring-boot-starter-test:${Versions.springBoot}")
-    testImplementation("no.nav.security:token-validation-spring-test:${Versions.tokenValidation}")
-    testImplementation("io.mockk:mockk:${Versions.mockk}")
-    testImplementation("io.mockk:mockk-jvm:${Versions.mockk}")
+    testImplementation(libs.spring.boot.starter.test)
+    testImplementation(libs.token.validation.spring.test)
+    testImplementation(libs.mockk)
+    testImplementation(libs.mockk.jvm)
 }
 
 group = "no.nav.sosialhjelp"
@@ -177,7 +139,7 @@ tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar
 }
 
 fun String.isNonStable(): Boolean {
-    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { toUpperCase().contains(it) }
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { uppercase().contains(it) }
     val regex = "^[0-9,.v-]+(-r)?$".toRegex()
     val isStable = stableKeyword || regex.matches(this)
     return isStable.not()
