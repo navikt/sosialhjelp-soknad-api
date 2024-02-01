215d0a86138fc2a2a7537aa540e181bea0450d3d
Amund Sørumshagen <amund.sorumshagen@nav.no>
2024-02-01 21:27:39 +0100

    Db-migrering (flyway)

---
+++ src/main/resources/db/migration/V2_0__Soknad.sql
@@ -0,0 +1,25 @@
+CREATE TABLE soknad
+(
+    id uuid primary key default gen_random_uuid(),
+    innsendingstidspunkt timestamp,
+    person_id varchar(30) not null,
+    fornavn varchar(255) not null,
+    mellomnavn varchar(255),
+    etternavn varchar(255) not null,
+    statsborgerskap varchar(50),
+    nordisk_borger bool,
+    kontonummer varchar(30),
+    telefonnummer varchar(30)
+);
+
+CREATE TABLE nav_enhet
+(
+    soknad uuid primary key,
+    kommunenummer varchar(10) not null,
+    orgnummer varchar(30) not null,
+    enhetsnummer varchar(30) not null,
+    enhetsnavn text not null,
+    constraint fk_navenhet_soknad
+        foreign key(soknad)
+            references soknad(id) on delete cascade
+);
---
+++ src/main/resources/db/migration/V2_1__Brukerdata.sql
@@ -0,0 +1,40 @@
+create table brukerdata
+(
+    soknad_id uuid primary key,
+    telefonnummer varchar(50),
+    kommentar_arbeidsforhold text,
+    kontonummer varchar(50),
+    har_ikke_konto bool,
+    hvorfor_soke text,
+    hva_sokes_om text,
+    constraint fk_brukerdata_soknad
+        foreign key(soknad_id)
+            references soknad(id) on delete cascade
+);
+
+-- brukerdata kan ha flere samtykker
+create table samtykke
+(
+    brukerdata uuid not null,
+    type varchar(50) not null,
+    verdi bool,
+    dato date,
+    constraint samtykke_pk primary key(brukerdata, type),
+    constraint fk_samtykke_brukerdata
+        foreign key (brukerdata)
+            references brukerdata(soknad_id) on delete cascade
+);
+
+-- brukerdata har 1 beskrivelse av annet
+create table beskrivelse_av_annet
+(
+    brukerdata uuid primary key,
+    barneutgifter text,
+    verdier text,
+    sparing text,
+    utbetalinger text,
+    boutgifter text,
+    constraint fk_beskrivelser_brukerdata
+        foreign key (brukerdata)
+            references brukerdata(soknad_id) on delete cascade
+);
