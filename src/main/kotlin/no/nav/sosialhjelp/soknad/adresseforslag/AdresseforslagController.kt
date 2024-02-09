897adce7b58a77249216335399236a558f3cbda5
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-02-09 22:45:09 +0100

    Enda mer debugging

--- src/main/kotlin/no/nav/sosialhjelp/soknad/adresseforslag/AdresseforslagController.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/adresseforslag/AdresseforslagController.kt
@@ -1,7 +1,6 @@
 package no.nav.sosialhjelp.soknad.adresseforslag
 
 import no.nav.security.token.support.core.api.Unprotected
-import no.nav.sosialhjelp.soknad.adresseforslag.domain.AdresseCompletionResult
 import no.nav.sosialhjelp.soknad.adressesok.AdresseforslagService
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
@@ -16,7 +15,7 @@ class AdresseforslagController(private val adresseforslagService: Adresseforslag
     @GetMapping()
     fun adresseSok(
         @RequestParam("sokestreng") sokestreng: String?
-    ): AdresseCompletionResult? {
+    ): Any? {
         requireNotNull(sokestreng) { "sokestreng is required" }
 
         return adresseforslagService.find(sokestreng)
