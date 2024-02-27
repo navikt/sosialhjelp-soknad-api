8dff42ff0635d0ea5ca2e577d64ccd60ad58aab3
Amund Sørumshagen <amund.sorumshagen@nav.no>
2024-02-27 08:17:05 +0100

    Spring-boot-docker-compose for lokal utvikling. Kjører også med redis

--- src/main/kotlin/no/nav/sosialhjelp/soknad/v2/shadow/SoknadV2RegisterDataAdapter.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/v2/shadow/SoknadV2RegisterDataAdapter.kt
@@ -6,6 +6,8 @@ import no.nav.sosialhjelp.soknad.v2.navn.Navn
 import no.nav.sosialhjelp.soknad.v2.shadow.adapter.AdresseAdapter
 import no.nav.sosialhjelp.soknad.v2.shadow.adapter.SoknadAdapter
 import no.nav.sosialhjelp.soknad.v2.soknad.Eier
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import java.time.LocalDateTime
 import java.util.*
@@ -15,18 +17,26 @@ class SoknadV2RegisterDataAdapter(
     private val soknadAdapter: SoknadAdapter,
     private val adresseAdapter: AdresseAdapter,
 ) : RegisterDataAdapter {
+    val log: Logger = LoggerFactory.getLogger(this::class.java)
+
     override fun createSoknad(behandlingsId: String, opprettetDato: LocalDateTime, eierId: String) {
-        soknadAdapter.createNewSoknad(
-            soknadId = UUID.fromString(behandlingsId),
-            opprettetDato = opprettetDato,
-            eier = Eier(
-                personId = eierId,
-                navn = Navn(
-                    fornavn = "Ukjent",
-                    etternavn = "Ukjent"
+
+        val result = kotlin.runCatching {
+            soknadAdapter.createNewSoknad(
+                soknadId = UUID.fromString(behandlingsId),
+                opprettetDato = opprettetDato,
+                eier = Eier(
+                    personId = eierId,
+                    navn = Navn(
+                        fornavn = "Ukjent",
+                        etternavn = "Ukjent"
+                    )
                 )
             )
-        )
+        }
+        result.onFailure {
+            log.error("Ny modell: Feil ved oppretting av ny soknad i adapter", it)
+        }
     }
 
     override fun addArbeidsforholdList(soknadId: String, arbeidsforhold: List<no.nav.sosialhjelp.soknad.arbeid.domain.Arbeidsforhold>) {
