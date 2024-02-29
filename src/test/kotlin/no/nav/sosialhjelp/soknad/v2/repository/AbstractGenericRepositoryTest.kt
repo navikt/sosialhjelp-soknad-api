75f34f937b89c6df38c6330c87f6dd7c8807e674
Amund Sørumshagen <amund.sorumshagen@nav.no>
2024-02-29 08:55:50 +0100

    Generelle CRUD-tester for repositories

---
+++ src/test/kotlin/no/nav/sosialhjelp/soknad/v2/repository/AbstractGenericRepositoryTest.kt
@@ -0,0 +1,74 @@
+package no.nav.sosialhjelp.soknad.v2.repository
+
+import no.nav.sosialhjelp.soknad.v2.config.repository.SoknadBubble
+import no.nav.sosialhjelp.soknad.v2.config.repository.UpsertRepository
+import no.nav.sosialhjelp.soknad.v2.eier.EierRepository
+import no.nav.sosialhjelp.soknad.v2.familie.FamilieRepository
+import no.nav.sosialhjelp.soknad.v2.kontakt.KontaktRepository
+import no.nav.sosialhjelp.soknad.v2.livssituasjon.LivssituasjonRepository
+import no.nav.sosialhjelp.soknad.v2.opprettSoknad
+import no.nav.sosialhjelp.soknad.v2.soknad.Soknad
+import no.nav.sosialhjelp.soknad.v2.soknad.SoknadRepository
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.BeforeEach
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.data.relational.core.conversion.DbActionExecutionException
+import org.springframework.data.repository.ListCrudRepository
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.transaction.annotation.Transactional
+import java.util.*
+
+@DataJdbcTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Transactional
+@ActiveProfiles("test", "test-container")
+abstract class AbstractGenericRepositoryTest {
+
+    @Autowired
+    protected lateinit var soknadRepository: SoknadRepository
+    @Autowired
+    protected lateinit var livssituasjonRepository: LivssituasjonRepository
+    @Autowired
+    protected lateinit var eierRepository: EierRepository
+    @Autowired
+    protected lateinit var kontaktRepository: KontaktRepository
+    @Autowired
+    protected lateinit var familieRepository: FamilieRepository
+
+    protected lateinit var soknad: Soknad
+
+    @BeforeEach
+    fun saveSoknad() {
+        soknad = soknadRepository.save(
+            opprettSoknad(id = UUID.randomUUID())
+        )
+    }
+
+    protected fun <E : SoknadBubble, R> R.runCrudOperations(
+        originalEntity: E,
+        updatedEntity: E,
+    ) where R : UpsertRepository<E>, R : ListCrudRepository<E, UUID> {
+
+        assertThat(originalEntity.soknadId).isEqualTo(updatedEntity.soknadId)
+        assertThat(originalEntity).isNotEqualTo(updatedEntity)
+
+        save(originalEntity)
+        save(updatedEntity)
+
+        deleteSoknadDeleteEntity(originalEntity)
+        errorWhenSoknadDoesntExist(originalEntity)
+    }
+
+    private fun <E : SoknadBubble, R : UpsertRepository<E>> R.errorWhenSoknadDoesntExist(entity: E) {
+        assertThatThrownBy { save(entity) }
+            .isInstanceOf(DbActionExecutionException::class.java)
+    }
+
+    private fun <E : SoknadBubble, R : ListCrudRepository<E, UUID>> R.deleteSoknadDeleteEntity(entity: E) {
+        soknadRepository.deleteById(entity.soknadId)
+        assertThat(this.existsById(entity.soknadId)).isFalse()
+    }
+}
---
+++ src/test/kotlin/no/nav/sosialhjelp/soknad/v2/repository/CrudRepositoryTest.kt
@@ -0,0 +1,60 @@
+package no.nav.sosialhjelp.soknad.v2.repository
+
+import no.nav.sosialhjelp.soknad.v2.createFamilie
+import no.nav.sosialhjelp.soknad.v2.kontakt.Telefonnummer
+import no.nav.sosialhjelp.soknad.v2.livssituasjon.Bosituasjon
+import no.nav.sosialhjelp.soknad.v2.opprettEier
+import no.nav.sosialhjelp.soknad.v2.opprettKontakt
+import no.nav.sosialhjelp.soknad.v2.opprettLivssituasjon
+import no.nav.sosialhjelp.soknad.v2.opprettSoknad
+import org.junit.jupiter.api.Test
+import java.util.*
+
+/**
+ * Formålet med testklassen:
+ * 1. Sjekke at domenemodell samsvarer med database-tabeller
+ * 2. Sjekke at "delete on cascade" i databasen fungerer som forventet
+ * 3. Sjekke at fremmednøkkel er korrekt koblet til eksisterende soknad
+ */
+class CrudRepositoryTest : AbstractGenericRepositoryTest() {
+
+    @Test
+    fun `CRUD Soknad`() {
+        UUID.randomUUID().let {
+            soknadRepository.save(opprettSoknad(it))
+            soknadRepository.save(opprettSoknad(it).copy(eierPersonId = "NOE ANNET"))
+        }
+    }
+
+    @Test
+    fun `CRUD Livssituasjon`() {
+        livssituasjonRepository.runCrudOperations(
+            opprettLivssituasjon(soknad.id),
+            opprettLivssituasjon(soknad.id).copy(bosituasjon = Bosituasjon(antallHusstand = 999))
+        )
+    }
+
+    @Test
+    fun `CRUD Eier`() {
+        eierRepository.runCrudOperations(
+            opprettEier(soknad.id),
+            opprettEier(soknad.id).copy(statsborgerskap = "SPANSK")
+        )
+    }
+
+    @Test
+    fun `CRUD Kontakt`() {
+        kontaktRepository.runCrudOperations(
+            opprettKontakt(soknad.id),
+            opprettKontakt(soknad.id).copy(telefonnummer = Telefonnummer(fraBruker = "99221199"))
+        )
+    }
+
+    @Test
+    fun `CRUD Familie`() {
+        familieRepository.runCrudOperations(
+            createFamilie(soknad.id),
+            createFamilie(soknad.id).copy(sivilstatus = null)
+        )
+    }
+}
