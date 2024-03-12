9a85a58015201d6ab04d4ce175007751a9d8bb2a
hildenok <hilde.nokland@nav.no>
2024-03-12 09:53:04 +0100

    Teste teste

--- src/main/kotlin/no/nav/sosialhjelp/soknad/v2/config/DatabaseConfig.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/v2/config/DatabaseConfig.kt
@@ -3,7 +3,9 @@ package no.nav.sosialhjelp.soknad.v2.config
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import no.nav.vault.jdbc.hikaricp.HikariCPVaultUtil
+import org.flywaydb.core.api.configuration.FluentConfiguration
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.flyway.FlywayConfigurationCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
@@ -12,7 +14,7 @@ import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories
 @Profile("!test & !mock-alt")
 @Configuration
 @EnableJdbcRepositories(basePackages = ["no.nav.sosialhjelp.soknad"])
-class HikariDataSourceConfig(
+class DatabaseConfig(
     @Value("\${spring.datasource.url}") private val dbUrl: String,
     @Value("\${postgres.user.role}") private val role: String,
     @Value("\${vault-mount-path}") private val vaultMountPath: String
@@ -26,5 +28,13 @@ class HikariDataSourceConfig(
         config.jdbcUrl = dbUrl
 
         return HikariCPVaultUtil.createHikariDataSourceWithVaultIntegration(config, vaultMountPath, role)
+    }
+
+    @Bean
+    fun setRole(): FlywayConfigurationCustomizer {
+
+        return FlywayConfigurationCustomizer { c: FluentConfiguration ->
+            c.initSql("SET ROLE \"$role\"")
+        }
     }
 }
