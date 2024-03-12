8e072d1e6e9f6aae3a5517ff810563b88ea1a080
hildenok <hilde.nokland@nav.no>
2024-03-12 12:13:50 +0100

    Teste teste

--- src/main/kotlin/no/nav/sosialhjelp/soknad/v2/config/DatabaseConfig.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/v2/config/DatabaseConfig.kt
@@ -13,7 +13,7 @@ import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories
 
 @Profile("!test & !mock-alt")
 @Configuration
-@EnableJdbcRepositories(basePackages = ["no.nav.sosialhjelp.soknad"])
+@EnableJdbcRepositories(basePackages = ["no.nav.sosialhjelp.soknad.v2"])
 class DatabaseConfig(
     @Value("\${spring.datasource.url}") private val dbUrl: String,
     @Value("\${postgres.user.role}") private val role: String,
