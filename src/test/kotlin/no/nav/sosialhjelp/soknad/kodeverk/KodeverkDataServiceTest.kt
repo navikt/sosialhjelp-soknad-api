f93cc656555de123c1707448b604ca8971ccb56e
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-03-21 12:36:01 +0100

    Test med ekte JSON-verdier fra mocket webserver

--- src/test/kotlin/no/nav/sosialhjelp/soknad/kodeverk/KodeverkDataServiceTest.kt
+++ src/test/kotlin/no/nav/sosialhjelp/soknad/kodeverk/KodeverkDataServiceTest.kt
@@ -1,17 +1,23 @@
 package no.nav.sosialhjelp.soknad.kodeverk
 
 import io.mockk.clearAllMocks
-import io.mockk.every
-import io.mockk.mockk
 import no.nav.sosialhjelp.soknad.kodeverk.KodeverkDataService.Companion.Kommuner
 import no.nav.sosialhjelp.soknad.kodeverk.KodeverkDataService.Companion.Landkoder
 import no.nav.sosialhjelp.soknad.kodeverk.KodeverkDataService.Companion.Postnummer
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.apache.commons.io.IOUtils
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.client.WebClient
+import java.nio.charset.StandardCharsets
 
 class KodeverkDataServiceTest : KodeverkTestClass() {
-    val kodeverkClient = mockk<KodeverkClient>()
+    private val mockWebServer = MockWebServer()
+    val kodeverkClient = KodeverkClient(mockWebServer.url("/").toString(), WebClient.builder())
     val kodeverkDataService = KodeverkDataService(kodeverkClient)
 
     @BeforeEach
@@ -19,24 +25,41 @@ class KodeverkDataServiceTest : KodeverkTestClass() {
         clearAllMocks()
     }
 
+    private fun prepareMockWebServerResponse(kodeverkNavn: String) = mockWebServer.enqueue(
+        MockResponse()
+            .setResponseCode(200)
+            .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .setBody(IOUtils.toString(ClassLoader.getSystemResourceAsStream("kodeverk/$kodeverkNavn.json"), StandardCharsets.UTF_8))
+    )
+
     @Test
     fun hentKommuner() {
-        every { kodeverkClient.hentKodeverk(Kommuner) } returns mockTermDto(TEST_KOMMUNER)
+        prepareMockWebServerResponse(Kommuner)
+
         val kommuner = kodeverkDataService.hentKodeverk(Kommuner)
-        assertEquals(TEST_KOMMUNER, kommuner)
+
+        assertEquals(kommuner[KOMMUNE_OSLO_NUMMER], KOMMUNE_OSLO_TERM)
+        assertEquals(kommuner[KOMMUNE_BERGEN_NUMMER], KOMMUNE_BERGEN_TERM)
     }
 
     @Test
     fun hentPostnummer() {
-        every { kodeverkClient.hentKodeverk(Postnummer) } returns mockTermDto(TEST_POSTNUMMER)
+        prepareMockWebServerResponse(Postnummer)
+
         val postnummer = kodeverkDataService.hentKodeverk(Postnummer)
-        assertEquals(TEST_POSTNUMMER, postnummer)
+
+        assertEquals(postnummer[POSTNUMMER_RØDTVET_KODE], POSTNUMMER_RØDTVET_TERM)
+        assertEquals(postnummer[POSTNUMMER_SAMFUNDET_KODE], POSTNUMMER_SAMFUNDET_TERM)
+        assertEquals(postnummer[POSTNUMMER_SESAM_STASJON_KODE], POSTNUMMER_SESAM_STASJON_TERM)
     }
 
     @Test
     fun hentLandkoder() {
-        every { kodeverkClient.hentKodeverk(Landkoder) } returns mockTermDto(TEST_LANDKODER)
+        prepareMockWebServerResponse(Landkoder)
+
         val landkoder = kodeverkDataService.hentKodeverk(Landkoder)
-        assertEquals(TEST_LANDKODER, landkoder)
+
+        assertEquals(landkoder[LAND_NORGE_KODE], LAND_NORGE_TERM)
+        assertEquals(landkoder[LAND_SVERIGE_KODE], LAND_SVERIGE_TERM)
     }
 }
--- src/test/kotlin/no/nav/sosialhjelp/soknad/kodeverk/KodeverkTestClass.kt
+++ src/test/kotlin/no/nav/sosialhjelp/soknad/kodeverk/KodeverkTestClass.kt
@@ -23,32 +23,32 @@ open class KodeverkTestClass {
         fun mockTermDto(kodeverk: Map<String, String>): KodeverkDto =
             KodeverkDto(kodeverk.map { it.key to betydningListeDto(it.value) }.toMap())
 
-        private const val KOMMUNE_BERGEN_NUMMER = "4601"
-        private const val KOMMUNE_BERGEN_TERM = "BERGEN"
-        private const val KOMMUNE_OSLO_NUMMER = "0301"
-        private const val KOMMUNE_OSLO_TERM = "OSLO"
+        const val KOMMUNE_BERGEN_NUMMER = "4601"
+        const val KOMMUNE_BERGEN_TERM = "Bergen"
+        const val KOMMUNE_OSLO_NUMMER = "0301"
+        const val KOMMUNE_OSLO_TERM = "Oslo"
 
         val TEST_KOMMUNER = mapOf(
             KOMMUNE_OSLO_NUMMER to KOMMUNE_OSLO_TERM,
             KOMMUNE_BERGEN_NUMMER to KOMMUNE_BERGEN_TERM
         )
 
-        private const val LAND_NORGE_KODE = "NOR"
-        private const val LAND_NORGE_TERM = "NORGE"
-        private const val LAND_SVERIGE_KODE = "SWE"
-        private const val LAND_SVERIGE_TERM = "SVERIGE"
+        const val LAND_NORGE_KODE = "NOR"
+        const val LAND_NORGE_TERM = "NORGE"
+        const val LAND_SVERIGE_KODE = "SWE"
+        const val LAND_SVERIGE_TERM = "SVERIGE"
 
         val TEST_LANDKODER = mapOf(
             LAND_NORGE_KODE to LAND_NORGE_TERM,
             LAND_SVERIGE_KODE to LAND_SVERIGE_TERM
         )
 
-        private const val POSTNUMMER_RØDTVET_KODE = "0951"
-        private const val POSTNUMMER_RØDTVET_TERM = "OSLO"
-        private const val POSTNUMMER_SAMFUNDET_KODE = "7030"
-        private const val POSTNUMMER_SAMFUNDET_TERM = "TRONDHEIM"
-        private const val POSTNUMMER_SESAM_STASJON_KODE = "1470"
-        private const val POSTNUMMER_SESAM_STASJON_TERM = "LØRENSKOG"
+        const val POSTNUMMER_RØDTVET_KODE = "0951"
+        const val POSTNUMMER_RØDTVET_TERM = "OSLO"
+        const val POSTNUMMER_SAMFUNDET_KODE = "7030"
+        const val POSTNUMMER_SAMFUNDET_TERM = "TRONDHEIM"
+        const val POSTNUMMER_SESAM_STASJON_KODE = "1470"
+        const val POSTNUMMER_SESAM_STASJON_TERM = "LØRENSKOG"
 
         val TEST_POSTNUMMER = mapOf(
             POSTNUMMER_RØDTVET_KODE to POSTNUMMER_RØDTVET_TERM,
