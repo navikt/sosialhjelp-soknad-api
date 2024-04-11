9959979b7b1a19dcddb6e4e3a46b0f0bc6c1a0d9
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-04-11 14:35:25 +0200

    Cache GT for ident på tjenestenivå

--- src/main/kotlin/no/nav/sosialhjelp/soknad/navenhet/gt/GeografiskTilknytningService.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/navenhet/gt/GeografiskTilknytningService.kt
@@ -2,12 +2,14 @@ package no.nav.sosialhjelp.soknad.navenhet.gt
 
 import no.nav.sosialhjelp.soknad.navenhet.gt.dto.erNorsk
 import no.nav.sosialhjelp.soknad.navenhet.gt.dto.toGtStringOrThrow
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Component
 
 @Component
 class GeografiskTilknytningService(
     private val geografiskTilknytningClient: GeografiskTilknytningClient
 ) {
+    @Cacheable("geografiskTilknytningForIdent")
     fun hentGeografiskTilknytning(ident: String): String? {
         val geografiskTilknytningDto = geografiskTilknytningClient.hentGeografiskTilknytning(ident).block() ?: return null
         if (!geografiskTilknytningDto.erNorsk()) return null
