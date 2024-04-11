e0cab0e4bcd01d8b546884b2abb3ddbe0547d157
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-04-11 14:36:48 +0200

    Refaktor GeografiskTilknytningClient
    * Resilience4j CircuitBreaker
    * PdlGraphQlClientBuilder
    * Fjerner manuell caching

--- src/main/kotlin/no/nav/sosialhjelp/soknad/navenhet/gt/GeografiskTilknytningClient.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/navenhet/gt/GeografiskTilknytningClient.kt
@@ -1,101 +1,31 @@
 package no.nav.sosialhjelp.soknad.navenhet.gt
 
-import com.fasterxml.jackson.core.JsonProcessingException
-import kotlinx.coroutines.runBlocking
-import no.nav.sosialhjelp.soknad.app.Constants.BEARER
-import no.nav.sosialhjelp.soknad.app.Constants.HEADER_TEMA
-import no.nav.sosialhjelp.soknad.app.Constants.TEMA_KOM
-import no.nav.sosialhjelp.soknad.app.client.pdl.HentGeografiskTilknytningDto
-import no.nav.sosialhjelp.soknad.app.client.pdl.PdlApiQuery.HENT_GEOGRAFISK_TILKNYTNING
-import no.nav.sosialhjelp.soknad.app.client.pdl.PdlClient
-import no.nav.sosialhjelp.soknad.app.client.pdl.PdlRequest
-import no.nav.sosialhjelp.soknad.app.exceptions.PdlApiException
-import no.nav.sosialhjelp.soknad.app.exceptions.TjenesteUtilgjengeligException
-import no.nav.sosialhjelp.soknad.app.subjecthandler.SubjectHandlerUtils.getToken
-import no.nav.sosialhjelp.soknad.auth.tokenx.TokendingsService
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker
+import no.nav.sosialhjelp.soknad.adressesok.PdlGraphQlClientBuilder
 import no.nav.sosialhjelp.soknad.navenhet.gt.dto.GeografiskTilknytningDto
-import no.nav.sosialhjelp.soknad.redis.GEOGRAFISK_TILKNYTNING_CACHE_KEY_PREFIX
-import no.nav.sosialhjelp.soknad.redis.PDL_CACHE_SECONDS
-import no.nav.sosialhjelp.soknad.redis.RedisService
 import org.slf4j.LoggerFactory.getLogger
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.http.HttpHeaders.AUTHORIZATION
+import org.springframework.graphql.client.FieldAccessException
 import org.springframework.stereotype.Component
-import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.bodyToMono
+import reactor.core.publisher.Mono
 
 @Component
 class GeografiskTilknytningClient(
-    @Value("\${pdl_api_url}") private val baseurl: String,
-    @Value("\${pdl_api_audience}") private val pdlAudience: String,
-    private val tokendingsService: TokendingsService,
-    private val redisService: RedisService,
-    webClientBuilder: WebClient.Builder
-) : PdlClient(webClientBuilder, baseurl) {
-
-    fun hentGeografiskTilknytning(ident: String): GeografiskTilknytningDto? {
-        hentFraCache(ident)?.let {
-            // TODO Ekstra logging
-            log.info("Henter geografisk tilknytning fra cache: $it")
-
-            return it
-        }
-
-        try {
-            val response: String =
-                baseRequest
-                    .header(HEADER_TEMA, TEMA_KOM)
-                    .header(AUTHORIZATION, BEARER + tokenXtoken(ident))
-                    .bodyValue(PdlRequest(HENT_GEOGRAFISK_TILKNYTNING, variables(ident)))
-                    .retrieve()
-                    .bodyToMono<String>()
-                    .retryWhen(pdlRetry)
-                    .block() ?: throw PdlApiException("Noe feilet mot PDL - hentGeografiskTilknytning - response null?")
-
-            val pdlResponse = parse<HentGeografiskTilknytningDto>(response)
-            pdlResponse.checkForPdlApiErrors()
-            return pdlResponse.data.hentGeografiskTilknytning
-                ?.also {
-                    // TODO Ekstra logging
-                    log.info("Lagrer geografisk tilknytning til cache: $it")
-
-                    lagreTilCache(ident, it)
+    val pdlGraphQlClientBuilder: PdlGraphQlClientBuilder
+) {
+    @CircuitBreaker(name = "pdl")
+    fun hentGeografiskTilknytning(ident: String): Mono<GeografiskTilknytningDto> =
+        pdlGraphQlClientBuilder.buildTokenXClient(ident)
+            .documentName("pdl-geografisktilknytning-query")
+            .variable("ident", ident)
+            .retrieve("hentGeografiskTilknytning")
+            .toEntity(GeografiskTilknytningDto::class.java)
+            .doOnError {
+                if (it is FieldAccessException) {
+                    log.error("graphql-feil fra server: ${it.message}")
+                } else {
+                    log.error("nettverks/http-feil: ${it.message}")
                 }
-        } catch (e: PdlApiException) {
-            throw e
-        } catch (e: Exception) {
-            log.error("Kall til PDL feilet (hentGeografiskTilknytning)")
-            throw TjenesteUtilgjengeligException("Noe uventet feilet ved kall til PDL", e)
-        }
-    }
-
-    private fun tokenXtoken(ident: String) = runBlocking {
-        tokendingsService.exchangeToken(ident, getToken(), pdlAudience)
-    }
-
-    private fun hentFraCache(ident: String): GeografiskTilknytningDto? {
-        return redisService.get(
-            GEOGRAFISK_TILKNYTNING_CACHE_KEY_PREFIX + ident,
-            GeografiskTilknytningDto::class.java
-        ) as? GeografiskTilknytningDto
-    }
-
-    private fun variables(ident: String): Map<String, Any> = mapOf("ident" to ident)
-
-    private fun lagreTilCache(ident: String, geografiskTilknytningDto: GeografiskTilknytningDto) {
-        try {
-            redisService.setex(
-                GEOGRAFISK_TILKNYTNING_CACHE_KEY_PREFIX + ident,
-                pdlMapper.writeValueAsBytes(geografiskTilknytningDto),
-                PDL_CACHE_SECONDS
-            )
-        } catch (e: JsonProcessingException) {
-            log.error(
-                "Noe feilet ved serialisering av geografiskTilknytningDto fra Pdl - ${geografiskTilknytningDto.javaClass.name}",
-                e
-            )
-        }
-    }
+            }
 
     companion object {
         private val log = getLogger(GeografiskTilknytningClient::class.java)
--- src/test/kotlin/no/nav/sosialhjelp/soknad/navenhet/gt/GeografiskTilknytningServiceTest.kt
+++ src/test/kotlin/no/nav/sosialhjelp/soknad/navenhet/gt/GeografiskTilknytningServiceTest.kt
@@ -6,6 +6,7 @@ import no.nav.sosialhjelp.soknad.navenhet.gt.dto.GeografiskTilknytningDto
 import no.nav.sosialhjelp.soknad.navenhet.gt.dto.GtType
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import reactor.core.publisher.Mono
 
 internal class GeografiskTilknytningServiceTest {
 
@@ -17,35 +18,35 @@ internal class GeografiskTilknytningServiceTest {
 
     @Test
     fun skalReturnereBydelsnummer() {
-        every { geografiskTilknytningClient.hentGeografiskTilknytning(ident) } returns GeografiskTilknytningDto(GtType.BYDEL, null, gt, null)
+        every { geografiskTilknytningClient.hentGeografiskTilknytning(ident) } returns Mono.just(GeografiskTilknytningDto(GtType.BYDEL, null, gt, null))
         val geografiskTilknytning = geografiskTilknytningService.hentGeografiskTilknytning(ident)
         assertThat(geografiskTilknytning).isEqualTo(gt)
     }
 
     @Test
     fun skalReturnereKommunenummer() {
-        every { geografiskTilknytningClient.hentGeografiskTilknytning(ident) } returns GeografiskTilknytningDto(GtType.KOMMUNE, gt, null, null)
+        every { geografiskTilknytningClient.hentGeografiskTilknytning(ident) } returns Mono.just(GeografiskTilknytningDto(GtType.KOMMUNE, gt, null, null))
         val geografiskTilknytning = geografiskTilknytningService.hentGeografiskTilknytning(ident)
         assertThat(geografiskTilknytning).isEqualTo(gt)
     }
 
     @Test
     fun skalReturnereNullHvisUtland() {
-        every { geografiskTilknytningClient.hentGeografiskTilknytning(ident) } returns GeografiskTilknytningDto(GtType.UTLAND, null, null, gt)
+        every { geografiskTilknytningClient.hentGeografiskTilknytning(ident) } returns Mono.just(GeografiskTilknytningDto(GtType.UTLAND, null, null, gt))
         val geografiskTilknytning = geografiskTilknytningService.hentGeografiskTilknytning(ident)
         assertThat(geografiskTilknytning).isNull()
     }
 
     @Test
     fun skalReturnereNullHvisUdefinert() {
-        every { geografiskTilknytningClient.hentGeografiskTilknytning(ident) } returns GeografiskTilknytningDto(GtType.UDEFINERT, null, null, null)
+        every { geografiskTilknytningClient.hentGeografiskTilknytning(ident) } returns Mono.just(GeografiskTilknytningDto(GtType.UDEFINERT, null, null, null))
         val geografiskTilknytning = geografiskTilknytningService.hentGeografiskTilknytning(ident)
         assertThat(geografiskTilknytning).isNull()
     }
 
     @Test
     fun skalReturnereNullHvisConsumerGirNull() {
-        every { geografiskTilknytningClient.hentGeografiskTilknytning(ident) } returns null
+        every { geografiskTilknytningClient.hentGeografiskTilknytning(ident) } returns Mono.empty()
         val geografiskTilknytning = geografiskTilknytningService.hentGeografiskTilknytning(ident)
         assertThat(geografiskTilknytning).isNull()
     }
