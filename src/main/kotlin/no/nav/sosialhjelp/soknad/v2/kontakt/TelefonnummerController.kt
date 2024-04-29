5e4cc34637c8dc212b8433dd3712551b0baf909f
Amund Sørumshagen <amund.sorumshagen@nav.no>
2024-04-29 16:12:40 +0200

    KontaktService - 2 forskjellige interfaces for Bruker- og Register-data

--- src/main/kotlin/no/nav/sosialhjelp/soknad/v2/kontakt/TelefonnummerController.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/v2/kontakt/TelefonnummerController.kt
@@ -15,7 +15,7 @@ import java.util.UUID
 @ProtectionSelvbetjeningHigh
 @RequestMapping("/soknad/{soknadId}/personalia/telefonnummer", produces = [MediaType.APPLICATION_JSON_VALUE])
 class TelefonnummerController(
-    private val kontaktService: KontaktServiceImpl,
+    private val kontaktService: KontaktService,
 ) {
     @GetMapping
     fun getTelefonnummer(
@@ -40,7 +40,7 @@ class TelefonnummerController(
                 .validateIsNumber(soknadId, it)
         }
 
-        return kontaktService.updateTelefonRegister(soknadId, telefonnummerInput.telefonnummerBruker).let {
+        return kontaktService.updateTelefonnummer(soknadId, telefonnummerInput.telefonnummerBruker).let {
             TelefonnummerDto(
                 telefonnummerRegister = it.fraRegister,
                 telefonnummerBruker = it.fraBruker,
