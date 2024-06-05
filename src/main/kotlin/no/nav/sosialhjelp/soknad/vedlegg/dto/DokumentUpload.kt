e505599a56d91ff340579dd8e423f6f6a125f047
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-06-06 00:55:21 +0200

    Skjul gamle verdier fra Swagger

--- src/main/kotlin/no/nav/sosialhjelp/soknad/vedlegg/dto/DokumentUpload.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/vedlegg/dto/DokumentUpload.kt
@@ -1,5 +1,6 @@
 package no.nav.sosialhjelp.soknad.vedlegg.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
 import no.nav.sosialhjelp.soknad.vedlegg.fiks.MellomlagretVedleggMetadata
 
 data class DokumentUpload(
@@ -7,9 +8,11 @@ data class DokumentUpload(
     val dokumentId: String,
 ) {
     // For kompatibilitet med gammel frontend kopierer vi verdiene til gamle feltnavn.
+    @get:Schema(hidden = true)
     val uuid: String
         get() = dokumentId
 
+    @get:Schema(hidden = true)
     val filNavn: String
         get() = filename
 
