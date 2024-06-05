d305a4f9374ff80d0510dbd65bfe424d9b7efb6e
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-06-05 23:33:03 +0200

    Slanker UnauthorizedMelding
    Matching av denne gjøres på HTTP-statuskode, så det eneste vi trenger er loginUrl.

--- src/main/kotlin/no/nav/sosialhjelp/soknad/app/exceptions/UnauthorizedMelding.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/app/exceptions/UnauthorizedMelding.kt
@@ -2,10 +2,4 @@ package no.nav.sosialhjelp.soknad.app.exceptions
 
 import java.net.URI
 
-data class UnauthorizedMelding(
-    val id: String,
-    val message: String,
-    val loginUrl: String,
-) {
-    constructor(id: String, message: String, loginUrl: URI) : this(id, message, loginUrl.toString())
-}
+data class UnauthorizedMelding(val loginUrl: URI)
