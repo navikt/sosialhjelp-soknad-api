4c8cd9f270aed5d9b369354b5ea85221e25eef8c
Amund Sørumshagen <amund.sorumshagen@nav.no>
2024-06-17 20:54:19 +0200

    Formue- og VerdierIntegrasjonstest

--- src/test/kotlin/no/nav/sosialhjelp/soknad/v2/integrationtest/okonomi/VerdierIntegrationTest.kt
+++ src/test/kotlin/no/nav/sosialhjelp/soknad/v2/integrationtest/okonomi/VerdierIntegrationTest.kt
@@ -1,5 +1,93 @@
 package no.nav.sosialhjelp.soknad.v2.integrationtest.okonomi
 
 import no.nav.sosialhjelp.soknad.v2.integrationtest.AbstractIntegrationTest
+import no.nav.sosialhjelp.soknad.v2.okonomi.Bekreftelse
+import no.nav.sosialhjelp.soknad.v2.okonomi.BekreftelseType
+import no.nav.sosialhjelp.soknad.v2.okonomi.OkonomiRepository
+import no.nav.sosialhjelp.soknad.v2.okonomi.formue.Formue
+import no.nav.sosialhjelp.soknad.v2.okonomi.formue.FormueType
+import no.nav.sosialhjelp.soknad.v2.okonomi.formue.HarIkkeVerdierInput
+import no.nav.sosialhjelp.soknad.v2.okonomi.formue.HarVerdierInput
+import no.nav.sosialhjelp.soknad.v2.okonomi.formue.VerdierDto
+import no.nav.sosialhjelp.soknad.v2.opprettOkonomi
+import no.nav.sosialhjelp.soknad.v2.opprettSoknad
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.repository.findByIdOrNull
+import java.util.UUID
 
-class VerdierIntegrationTest : AbstractIntegrationTest()
+class VerdierIntegrationTest : AbstractIntegrationTest() {
+    @Autowired
+    private lateinit var okonomiRepository: OkonomiRepository
+
+    @Test
+    fun `Hente verdier skal returnere lagrede data`() {
+        val soknad = soknadRepository.save(opprettSoknad())
+        opprettOkonomi(soknad.id).copy(
+            bekreftelser = setOf(Bekreftelse(BekreftelseType.BEKREFTELSE_VERDI, verdi = true)),
+            formuer =
+                setOf(
+                    Formue(FormueType.VERDI_BOLIG),
+                    Formue(FormueType.VERDI_ANNET, beskrivelse = "beskrivelse"),
+                ),
+        ).also { okonomiRepository.save(it) }
+
+        doGet(
+            uri = getUrl(soknad.id),
+            responseBodyClass = VerdierDto::class.java,
+        )
+            .also { dto ->
+                assertThat(dto.hasBolig).isTrue()
+                assertThat(dto.hasAnnet).isTrue()
+                assertThat(dto.beskrivelseVerdi).isEqualTo("beskrivelse")
+            }
+    }
+
+    @Test
+    fun `Oppdatere verdier skal lagres i db`() {
+        val soknad = soknadRepository.save(opprettSoknad())
+
+        doPut(
+            uri = getUrl(soknad.id),
+            requestBody = HarVerdierInput(hasBeskrivelseAnnet = true, beskrivelseVerdi = "beskrivelse"),
+            responseBodyClass = VerdierDto::class.java,
+            soknadId = soknad.id,
+        )
+
+        okonomiRepository.findByIdOrNull(soknad.id)!!.also { okonomi ->
+            assertThat(okonomi.bekreftelser.toList()).hasSize(1)
+                .allMatch { it.type == BekreftelseType.BEKREFTELSE_VERDI && it.verdi }
+            assertThat(okonomi.formuer.toList()).hasSize(1)
+                .allMatch { it.type == FormueType.VERDI_ANNET && it.beskrivelse == "beskrivelse" }
+        }
+    }
+
+    @Test
+    fun `Sette bekreftelse false skal fjerne elementer`() {
+        val soknad = soknadRepository.save(opprettSoknad())
+        opprettOkonomi(soknad.id).copy(
+            bekreftelser = setOf(Bekreftelse(BekreftelseType.BEKREFTELSE_VERDI, verdi = true)),
+            formuer = setOf(Formue(FormueType.VERDI_BOLIG)),
+        ).also { okonomiRepository.save(it) }
+
+        doGet(getUrl(soknad.id), VerdierDto::class.java).also { assertThat(it.hasBolig).isTrue() }
+
+        doPut(
+            uri = getUrl(soknad.id),
+            requestBody = HarIkkeVerdierInput(),
+            responseBodyClass = VerdierDto::class.java,
+            soknadId = soknad.id,
+        )
+
+        okonomiRepository.findByIdOrNull(soknad.id)!!.also { okonomi ->
+            assertThat(okonomi.bekreftelser.toList()).hasSize(1)
+                .allMatch { it.type == BekreftelseType.BEKREFTELSE_VERDI && !it.verdi }
+            assertThat(okonomi.formuer).isEmpty()
+        }
+    }
+
+    companion object {
+        private fun getUrl(soknadId: UUID) = "/soknad/$soknadId/inntekt/verdier"
+    }
+}
