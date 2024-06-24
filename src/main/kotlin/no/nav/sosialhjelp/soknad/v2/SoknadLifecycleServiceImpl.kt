eaf021e7f985c201d2059dd27e8a0a5ebccb4a38
Martin Almvik <martin.almvik@nav.no>
2024-06-24 12:36:50 +0200

    Legg til feature toggle

--- src/main/kotlin/no/nav/sosialhjelp/soknad/v2/SoknadLifecycleServiceImpl.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/v2/SoknadLifecycleServiceImpl.kt
@@ -1,5 +1,6 @@
 package no.nav.sosialhjelp.soknad.v2
 
+import io.getunleash.Unleash
 import no.nav.sosialhjelp.soknad.app.exceptions.FeilVedSendingTilFiksException
 import no.nav.sosialhjelp.soknad.app.mdc.MdcOperations
 import no.nav.sosialhjelp.soknad.app.subjecthandler.SubjectHandlerUtils
@@ -16,6 +17,7 @@ class SoknadLifecycleServiceImpl(
     private val prometheusMetricsService: PrometheusMetricsService,
     private val soknadServiceImpl: SoknadService,
     private val adresseService: AdresseService,
+    private val unleash: Unleash,
 ) : SoknadLifecycleService {
     override fun startSoknad(): Pair<UUID, Boolean> {
         prometheusMetricsService.reportStartSoknad()
@@ -26,6 +28,9 @@ class SoknadLifecycleServiceImpl(
                 eierId = fnr,
                 tidspunkt = LocalDateTime.now().minusMonths(4),
             )
+
+        val kortSoknadEnabled = unleash.isEnabled("sosialhjelp.soknad.kort_soknad", false)
+
         val soknadId =
             fnr.let {
                 soknadServiceImpl.createSoknad(
@@ -33,7 +38,7 @@ class SoknadLifecycleServiceImpl(
                     soknadId = UUID.randomUUID(),
                     // TODO Spesifisert til UTC i filformatet
                     opprettetDato = LocalDateTime.now(),
-                    kortSoknad = hasSoknadNewerThan4Months,
+                    kortSoknad = kortSoknadEnabled && hasSoknadNewerThan4Months,
                 )
             }
 
