37d78a8f681f2af89912ef266d30bfcef0c881d6
joeldlin <jorgen.elden.lindgren@nav.no>
2024-07-02 14:09:59 +0200

    r - fikser test

--- src/test/kotlin/no/nav/sosialhjelp/soknad/innsending/digisosapi/DokumentListeServiceTest.kt
+++ src/test/kotlin/no/nav/sosialhjelp/soknad/innsending/digisosapi/DokumentListeServiceTest.kt
@@ -5,6 +5,7 @@ import io.mockk.mockk
 import no.nav.sosialhjelp.soknad.db.repositories.soknadunderarbeid.SoknadUnderArbeid
 import no.nav.sosialhjelp.soknad.db.repositories.soknadunderarbeid.SoknadUnderArbeidStatus
 import no.nav.sosialhjelp.soknad.innsending.SoknadServiceOld
+import no.nav.sosialhjelp.soknad.metrics.PrometheusMetricsService
 import no.nav.sosialhjelp.soknad.pdf.SosialhjelpPdfGenerator
 import no.nav.sosialhjelp.soknad.vedlegg.fiks.MellomlagringService
 import no.nav.sosialhjelp.soknad.vedlegg.filedetection.MimeTypes
@@ -15,8 +16,9 @@ import java.time.LocalDateTime
 internal class DokumentListeServiceTest {
     private val sosialhjelpPdfGenerator: SosialhjelpPdfGenerator = mockk()
     private val mellomlagringService: MellomlagringService = mockk()
+    private val prometheusMetricsService: PrometheusMetricsService = mockk(relaxed = true)
 
-    private val dokumentListeService = DokumentListeService(sosialhjelpPdfGenerator, mellomlagringService)
+    private val dokumentListeService = DokumentListeService(sosialhjelpPdfGenerator, mellomlagringService, prometheusMetricsService)
 
     private val eier = "12345678910"
 
