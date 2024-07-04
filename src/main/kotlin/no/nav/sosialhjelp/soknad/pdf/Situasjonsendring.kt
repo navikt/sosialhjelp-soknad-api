16085ac46292b20e2c1d3c39351d8c41262b52b8
Martin Almvik <martin.almvik@nav.no>
2024-07-04 14:16:48 +0200

    Fiks nullpointer ved null situasjonsendring

--- src/main/kotlin/no/nav/sosialhjelp/soknad/pdf/Situasjonsendring.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/pdf/Situasjonsendring.kt
@@ -7,36 +7,39 @@ object Situasjonsendring {
         pdf: PdfGenerator,
         pdfUtils: PdfUtils,
         utvidetSoknad: Boolean,
-        situasjonendring: JsonSituasjonendring,
+        situasjonendring: JsonSituasjonendring?,
     ) {
         pdf.skrivH4Bold(pdfUtils.getTekst("situasjonsendring.tittel"))
         pdf.addBlankLine()
 
-        pdf.skrivTekstBold(pdfUtils.getTekst("situasjonsendring.harNoeEndretSeg.sporsmal"))
+        if (situasjonendring != null) {
+            pdf.skrivTekstBold(pdfUtils.getTekst("situasjonsendring.harNoeEndretSeg.sporsmal"))
 
-        if (utvidetSoknad) {
-            pdfUtils.skrivInfotekst(pdf, "situasjonsendring.hvaHarEndretSeg.infotekst")
-        }
-        when (situasjonendring.harNoeEndretSeg) {
-            null -> {
+            if (utvidetSoknad) {
+                pdfUtils.skrivInfotekst(pdf, "situasjonsendring.hvaHarEndretSeg.infotekst")
+            }
+            when (situasjonendring.harNoeEndretSeg) {
+                null -> {
+                    pdfUtils.skrivIkkeUtfylt(pdf)
+                }
+
+                true -> {
+                    pdf.skrivTekst(pdfUtils.getTekst("situasjonsendring.harNoeEndretSeg.ja"))
+                }
+
+                false -> {
+                    pdf.skrivTekst(pdfUtils.getTekst("situasjonsendring.harNoeEndretSeg.nei"))
+                }
+            }
+            pdf.addBlankLine()
+
+            pdf.skrivTekstBold(pdfUtils.getTekst("situasjonsendring.hvaHarEndretSeg.sporsmal"))
+            if (situasjonendring.hvaHarEndretSeg == null || situasjonendring.hvaHarEndretSeg.isEmpty()) {
                 pdfUtils.skrivIkkeUtfylt(pdf)
-            }
-            true -> {
-                pdf.skrivTekst(pdfUtils.getTekst("situasjonsendring.harNoeEndretSeg.ja"))
-            }
-            false -> {
-                pdf.skrivTekst(pdfUtils.getTekst("situasjonsendring.harNoeEndretSeg.nei"))
+            } else {
+                pdf.skrivTekst(situasjonendring.hvaHarEndretSeg)
             }
         }
-        pdf.addBlankLine()
-
-        pdf.skrivTekstBold(pdfUtils.getTekst("situasjonsendring.hvaHarEndretSeg.sporsmal"))
-        if (situasjonendring.hvaHarEndretSeg == null || situasjonendring.hvaHarEndretSeg.isEmpty()) {
-            pdfUtils.skrivIkkeUtfylt(pdf)
-        } else {
-            pdf.skrivTekst(situasjonendring.hvaHarEndretSeg)
-        }
-
         pdf.addBlankLine()
     }
 }
