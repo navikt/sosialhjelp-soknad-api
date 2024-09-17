bdeb230ce418fd1850bd32c4d5f4f214c542d172
Amund Sørumshagen <amund.sorumshagen@nav.no>
2024-09-17 12:48:03 +0200

    Bruk eksplisitt Europe/Oslo

--- src/main/kotlin/no/nav/sosialhjelp/soknad/v2/json/generate/JsonInternalSoknadGenerator.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/v2/json/generate/JsonInternalSoknadGenerator.kt
@@ -10,8 +10,8 @@ import no.nav.sbl.soknadsosialhjelp.vedlegg.JsonVedleggSpesifikasjon
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
-import java.time.Clock
 import java.time.LocalDateTime
+import java.time.ZoneId
 import java.time.ZoneOffset
 import java.time.temporal.ChronoUnit
 import java.util.UUID
@@ -68,12 +68,26 @@ class JsonInternalSoknadGenerator(
     }
 }
 
-// I Json-strukturen skal tidspunkt være UTC med 3 desimaler
-fun LocalDateTime.toUTCTimestampStringWithMillis(): String {
-    return this
-        .atZone(Clock.systemDefaultZone().zone)
-        .withZoneSameInstant(ZoneOffset.UTC)
-        .toOffsetDateTime()
-        .truncatedTo(ChronoUnit.MILLIS)
-        .toString()
+object TimestampManager {
+    private const val ZONE_STRING = "Europe/Oslo"
+    private const val TIMESTAMP_REGEX = "^[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]T[0-9][0-9]:[0-9][0-9]:[0-9][0-9].[0-9][0-9]*Z$"
+    private const val MILLISECOND = 1000000L
+
+    fun convertToOffsettDateTimeUTCString(localDateTime: LocalDateTime) = localDateTime.toUTCTimestampStringWithMillis()
+
+    private fun validateTimestamp(timestampString: String) {
+        if (!Regex(TIMESTAMP_REGEX).matches(timestampString)) error("Tidspunkt $timestampString matcher ikke formatet")
+    }
+
+    // I Json-strukturen skal tidspunkt være UTC med 3 desimaler
+    private fun LocalDateTime.toUTCTimestampStringWithMillis(): String {
+        return this
+            .let { if (it.nano < MILLISECOND) it.plusNanos(MILLISECOND) else it }
+            .atZone(ZoneId.of(ZONE_STRING))
+            .withZoneSameInstant(ZoneOffset.UTC)
+            .toOffsetDateTime()
+            .truncatedTo(ChronoUnit.MILLIS)
+            .toString()
+            .also { validateTimestamp(it) }
+    }
 }
---
+++ src/test/kotlin/no/nav/sosialhjelp/soknad/v2/json/generate/mappers/TimestampConversionTest.kt
@@ -0,0 +1,35 @@
+package no.nav.sosialhjelp.soknad.v2.json.generate.mappers
+
+import no.nav.sosialhjelp.soknad.v2.json.generate.TimestampManager
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
+
+class TimestampConversionTest {
+    @Test
+    fun `Legg til ett millisekund hvis nanosekund er tilsvarende mindre`() {
+        val timeUtenNano = LocalDateTime.of(2020, 5, 5, 5, 5, 5)
+        val timeLowNano = timeUtenNano.plusNanos(5)
+
+        val stringUtenNano = TimestampManager.convertToOffsettDateTimeUTCString(timeUtenNano)
+        val stringLowNano = TimestampManager.convertToOffsettDateTimeUTCString(timeLowNano)
+
+        assertThat(stringUtenNano).isEqualTo(stringLowNano)
+    }
+
+    @Test
+    fun `Timestamp sommertid skal skille 2 timer`() {
+        val timestamp = LocalDateTime.of(2024, 6, 6, 6, 6, 6)
+
+        TimestampManager.convertToOffsettDateTimeUTCString(timestamp)
+            .also { assertThat(it).contains("0${timestamp.hour - 2}:0${timestamp.minute}:0${timestamp.second}") }
+    }
+
+    @Test
+    fun `Timestamp vintertid skal skille 1 time`() {
+        val timestamp = LocalDateTime.of(2024, 12, 12, 6, 6, 6)
+
+        TimestampManager.convertToOffsettDateTimeUTCString(timestamp)
+            .also { assertThat(it).contains("0${timestamp.hour - 1}:0${timestamp.minute}:0${timestamp.second}") }
+    }
+}
