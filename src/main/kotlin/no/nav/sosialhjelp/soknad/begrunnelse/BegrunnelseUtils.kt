d3d42bc9a668b7d576815d931e0d1f485ebb3fb9
Martin Almvik <martin.almvik@nav.no>
2024-09-17 15:42:56 +0200

    Sett søknadstype basert på mottakerkommune

--- src/main/kotlin/no/nav/sosialhjelp/soknad/begrunnelse/BegrunnelseUtils.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/begrunnelse/BegrunnelseUtils.kt
@@ -12,16 +12,18 @@ private data class Kategori(
 private typealias Kategorier = List<Kategori>
 
 object BegrunnelseUtils {
-    fun jsonToHvoSokesOm(json: String): String? {
+    fun jsonToHvaSokesOm(json: String): String? {
         val kategorier = runCatching { ObjectMapper().readValue(json, jacksonTypeRef<Kategorier>()) }.getOrNull()?.takeIf { it.isNotEmpty() } ?: return null
         val nodhjelp = kategorier.find { it.text == "Nødhjelp" }?.takeIf { it.subCategories.isNotEmpty() }
         val annet = kategorier.find { it.text == "Annet" }?.takeIf { it.hvaSokesOm?.isNotBlank() == true }
         val resten = kategorier.filter { it.text != "Annet" && it.text != "Nødhjelp" }.takeIf { it.isNotEmpty() }
 
         return """
-            ${nodhjelp?.let { "Jeg er i en nødsituasjon og ${nodhjelp.subCategories.joinToString(", ").lowercase()}" } ?: ""}
-            ${resten?.let { "Jeg søker om penger til:\n${resten.joinToString(", ") { it.text }}}" } ?: ""}
-            ${annet?.let { "Annet:\n${it.hvaSokesOm ?: ""}" } ?: ""}
-            """.trimIndent()
+            |${nodhjelp?.let { "Nødhjelp: ${nodhjelp.subCategories.joinToString(", ").lowercase()}" } ?: ""}
+            |${resten?.let { "Jeg søker om penger til:\n${resten.joinToString(", ") { it.text }}}" } ?: ""}
+            |${annet?.let { "Annet:\n${it.hvaSokesOm ?: ""}" } ?: ""}
+            """.trimMargin()
     }
+
+    fun isEmptyJson(json: String): Boolean = runCatching { ObjectMapper().readValue(json, jacksonTypeRef<Kategorier>()) }.getOrNull()?.isEmpty() ?: false
 }
