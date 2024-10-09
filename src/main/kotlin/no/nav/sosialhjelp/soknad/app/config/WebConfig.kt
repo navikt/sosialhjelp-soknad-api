0afb2bfa5f79585e6efdbfa96cef8e4312be20ae
Martin Almvik <martin.almvik@nav.no>
2024-10-09 12:32:13 +0200

    Legg til behandlingsid som span attribute der det er mulig

--- src/main/kotlin/no/nav/sosialhjelp/soknad/app/config/WebConfig.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/app/config/WebConfig.kt
@@ -1,6 +1,7 @@
 package no.nav.sosialhjelp.soknad.app.config
 
 import no.nav.sosialhjelp.soknad.app.soknadlock.ConflictAvoidanceDelayInterceptor
+import no.nav.sosialhjelp.soknad.tracing.TracingInterceptor
 import no.nav.sosialhjelp.soknad.v2.config.interceptor.SoknadAccessInterceptor
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
@@ -12,9 +13,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class WebConfig(
     private val conflictAvoidanceDelayInterceptor: ConflictAvoidanceDelayInterceptor,
     private val soknadAccessInterceptor: SoknadAccessInterceptor,
+    private val tracingInterceptor: TracingInterceptor,
 ) : WebMvcConfigurer {
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(conflictAvoidanceDelayInterceptor)
         registry.addInterceptor(soknadAccessInterceptor)
+        registry.addInterceptor(tracingInterceptor)
     }
 }
