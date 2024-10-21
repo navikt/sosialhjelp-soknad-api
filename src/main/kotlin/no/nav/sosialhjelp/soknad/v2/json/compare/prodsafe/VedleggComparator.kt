47d53b82608c0eafd987b10008d798ad67dc11bf
Amund Sørumshagen <amund.sorumshagen@nav.no>
2024-10-21 15:13:06 +0200

    Ytterligere logging rundt Vedlegg

--- src/main/kotlin/no/nav/sosialhjelp/soknad/v2/json/compare/prodsafe/VedleggComparator.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/v2/json/compare/prodsafe/VedleggComparator.kt
@@ -28,10 +28,16 @@ class VedleggComparator(
     }
 
     private fun relevantFieldsAreTheSame(): Boolean {
-        if (originals.size != shadows.size) return false
+        if (originals.size != shadows.size) {
+            logger.warn("NyModell: Antall vedlegg er forskjellig")
+            return false
+        }
 
         originals.forEach { original ->
-            if (!isVedleggExistsAndValid(original)) return false
+            if (!isVedleggExistsAndValid(original)) {
+                logger.warn("NyModell: Vedlegg er ikke valid")
+                return false
+            }
         }
         return true
     }
@@ -39,10 +45,22 @@ class VedleggComparator(
     private fun isVedleggExistsAndValid(original: JsonVedlegg): Boolean {
         shadows.find { it.type == original.type }
             ?.also { shadowVedlegg ->
-                if (shadowVedlegg.type != original.type) return false
-                if (shadowVedlegg.tilleggsinfo != original.tilleggsinfo) return false
-                if (shadowVedlegg.filer.size != original.filer.size) return false
-                if (shadowVedlegg.status != original.status) return false
+                if (shadowVedlegg.type != original.type) {
+                    logger.warn("NyModell: Vedlegg.Type er ikke lik: ${original.type} - ${shadowVedlegg.type}")
+                    return false
+                }
+                if (shadowVedlegg.tilleggsinfo != original.tilleggsinfo) {
+                    logger.warn("NyModell: Vedlegg.Tilleggsinfo er ikke lik: ${original.tilleggsinfo} - ${shadowVedlegg.tilleggsinfo}")
+                    return false
+                }
+                if (shadowVedlegg.filer.size != original.filer.size) {
+                    logger.warn("NyModell: Vedlegg.Filer.size er ikke lik: ${original.filer.size} - ${shadowVedlegg.filer.size}")
+                    return false
+                }
+                if (shadowVedlegg.status != original.status) {
+                    logger.warn("NyModell: Vedlegg.Status er ikke lik: ${original.status} - ${shadowVedlegg.status}")
+                    return false
+                }
             }
             ?: return false
         return true
