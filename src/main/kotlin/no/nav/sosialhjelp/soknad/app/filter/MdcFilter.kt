cc8fffd75d419ba935e265f2066960a1dfe6fd62
Martin Almvik <martin.almvik@nav.no>
2024-11-01 15:33:32 +0100

    Gå tilbake til gammel måte å hente ut behandlingsid på i MDC

--- src/main/kotlin/no/nav/sosialhjelp/soknad/app/filter/MdcFilter.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/app/filter/MdcFilter.kt
@@ -4,7 +4,6 @@ import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import no.nav.sosialhjelp.soknad.app.Constants.HEADER_CALL_ID
-import no.nav.sosialhjelp.soknad.app.getBehandlingsId
 import no.nav.sosialhjelp.soknad.app.mdc.MdcOperations
 import no.nav.sosialhjelp.soknad.app.mdc.MdcOperations.MDC_BEHANDLINGS_ID
 import no.nav.sosialhjelp.soknad.app.mdc.MdcOperations.MDC_CALL_ID
@@ -26,7 +25,7 @@ class MdcFilter : OncePerRequestFilter() {
         val callId = request.getHeader(HEADER_CALL_ID) ?: MdcOperations.generateCallId()
         val consumerId = SubjectHandlerUtils.getConsumerId()
 
-        val behandlingsId = request.getBehandlingsId()
+        val behandlingsId = getBehandlingsId(request)
 
         putToMDC(MDC_CALL_ID, callId)
         putToMDC(MDC_CONSUMER_ID, consumerId)
@@ -38,5 +37,41 @@ class MdcFilter : OncePerRequestFilter() {
         } finally {
             clearMDC()
         }
+    }
+
+    private fun getBehandlingsId(request: HttpServletRequest): String? {
+        val requestURI = request.requestURI
+        if (requestURI.matches(
+                Regex("^${SOKNAD_API_BASEURL}soknader/(.*)"),
+            ) &&
+            !requestURI.matches(Regex("^${SOKNAD_API_BASEURL}soknader/opprettSoknad(.*)"))
+        ) {
+            return requestURI.substringAfter("${SOKNAD_API_BASEURL}soknader/").substringBefore("/")
+        }
+        if (requestURI.matches(Regex("^${SOKNAD_API_BASEURL}innsendte/(.*)"))) {
+            return requestURI.substringAfter("${SOKNAD_API_BASEURL}innsendte/")
+        }
+        if (requestURI.matches(Regex("^${SOKNAD_API_BASEURL}ettersendteVedlegg/(.*)"))) {
+            return requestURI.substringAfter("${SOKNAD_API_BASEURL}ettersendteVedlegg/")
+        }
+        /*
+        Skal matche disse:
+        /opplastetVedlegg/{behandlingsId}/{vedleggId}/fil GET
+        /opplastetVedlegg/{behandlingsId}/{type} POST
+        /opplastetVedlegg/{behandlingsId}/{vedleggId} DELETE
+        men ikke:
+        /opplastetVedlegg/{vedleggId}/fil GET
+         */
+        if (request.method != "GET" && requestURI.matches(Regex("^${SOKNAD_API_BASEURL}opplastetVedlegg/(.*)"))) {
+            return requestURI.substringAfter("${SOKNAD_API_BASEURL}opplastetVedlegg/").substringBefore("/")
+        }
+        if (request.method == "GET" && requestURI.matches(Regex("^${SOKNAD_API_BASEURL}opplastetVedlegg/(.*)/(.*)/fil"))) {
+            return requestURI.substringAfter("${SOKNAD_API_BASEURL}opplastetVedlegg/").substringBefore("/")
+        }
+        return null
+    }
+
+    companion object {
+        private const val SOKNAD_API_BASEURL = "/sosialhjelp/soknad-api/"
     }
 }
