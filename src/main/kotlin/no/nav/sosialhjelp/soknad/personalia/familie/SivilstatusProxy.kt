7fcf32d8c3ed506560e9fb2093015bb5bb4d0201
Amund Sørumshagen <amund.sorumshagen@nav.no>
2024-11-05 23:01:34 +0100

    Side 4 - forsorgerplikt, sivilstatus

---
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/personalia/familie/SivilstatusProxy.kt
@@ -0,0 +1,69 @@
+package no.nav.sosialhjelp.soknad.personalia.familie
+
+import no.nav.sbl.soknadsosialhjelp.soknad.familie.JsonSivilstatus
+import no.nav.sosialhjelp.soknad.personalia.familie.dto.EktefelleFrontend
+import no.nav.sosialhjelp.soknad.personalia.familie.dto.NavnFrontend
+import no.nav.sosialhjelp.soknad.personalia.familie.dto.SivilstatusFrontend
+import no.nav.sosialhjelp.soknad.v2.familie.EktefelleInput
+import no.nav.sosialhjelp.soknad.v2.familie.SivilstandController
+import no.nav.sosialhjelp.soknad.v2.familie.SivilstandInput
+import no.nav.sosialhjelp.soknad.v2.familie.Sivilstatus
+import no.nav.sosialhjelp.soknad.v2.navn.Navn
+import org.springframework.stereotype.Component
+import java.util.UUID
+
+@Component
+class SivilstatusProxy(private val sivilstandController: SivilstandController) {
+    fun updateSivilstand(
+        soknadId: String,
+        familieFrontend: SivilstatusFrontend,
+    ) {
+        familieFrontend
+            .let {
+                SivilstandInput(
+                    sivilstatus = it.sivilstatus?.name?.let { status -> Sivilstatus.valueOf(status) },
+                    ektefelle =
+                        it.ektefelle?.let { ektefelle ->
+                            EktefelleInput(
+                                ektefelle.personnummer,
+                                Navn(
+                                    ektefelle.navn?.fornavn ?: "",
+                                    ektefelle.navn?.mellomnavn ?: "",
+                                    ektefelle.navn?.etternavn ?: "",
+                                ),
+                                ektefelle.fodselsdato,
+                                familieFrontend.borSammenMed,
+                            )
+                        },
+                )
+            }
+            .also { input -> sivilstandController.updateSivilstand(UUID.fromString(soknadId), input) }
+    }
+
+    fun getSivilstatus(behandlingsId: String) =
+        sivilstandController
+            .getSivilstand(UUID.fromString(behandlingsId))
+            .let {
+                SivilstatusFrontend(
+                    // TODO Spiller det noen rolle for frontend ??
+                    kildeErSystem = true,
+                    sivilstatus = it.sivilstatus?.let { status -> JsonSivilstatus.Status.valueOf(status.name) },
+                    ektefelle =
+                        it.ektefelle?.let { ektefelle ->
+                            EktefelleFrontend(
+                                navn =
+                                    NavnFrontend(
+                                        ektefelle.navn?.fornavn,
+                                        ektefelle.navn?.mellomnavn,
+                                        ektefelle.navn?.etternavn,
+                                    ),
+                                fodselsdato = ektefelle.fodselsdato,
+                                personnummer = ektefelle.personId,
+                            )
+                        },
+                    harDiskresjonskode = it.ektefelle?.harDiskresjonskode,
+                    borSammenMed = it.ektefelle?.borSammen,
+                    erFolkeregistrertSammen = it.ektefelle?.folkeregistrertMedEktefelle,
+                )
+            }
+}
