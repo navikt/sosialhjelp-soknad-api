2fcac27ee804a39293e98c2e4919fab13147d945
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-11-06 21:27:44 +0100

    Legg på observability

--- src/main/kotlin/no/nav/sosialhjelp/soknad/innsending/digisosapi/MaskinportenTokenCache.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/innsending/digisosapi/MaskinportenTokenCache.kt
@@ -1,5 +1,6 @@
 package no.nav.sosialhjelp.soknad.innsending.digisosapi
 
+import io.micrometer.core.instrument.MeterRegistry
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.stereotype.Service
 import java.util.concurrent.TimeUnit
@@ -7,7 +8,11 @@ import java.util.concurrent.TimeUnit
 @Service
 class MaskinportenTokenCache(
     private val redisTemplate: RedisTemplate<CacheKey, MaskinportenToken>,
+    meterRegistry: MeterRegistry,
 ) {
+    private var hitCounter = meterRegistry.counter("maskinporten.cache.hit.count")
+    private var missCounter = meterRegistry.counter("maskinporten.cache.miss.count")
+
     fun put(
         pid: String,
         scope: String,
@@ -17,7 +22,9 @@ class MaskinportenTokenCache(
     fun get(
         pid: String,
         scope: String,
-    ): String? = redisTemplate.opsForValue().get(CacheKey(pid, scope))?.access_token
+    ): String? =
+        redisTemplate.opsForValue().get(CacheKey(pid, scope))?.access_token
+            ?.also { hitCounter.increment() } ?: null.also { missCounter.increment() }
 
     data class CacheKey(val pid: String, val scope: String)
 }
