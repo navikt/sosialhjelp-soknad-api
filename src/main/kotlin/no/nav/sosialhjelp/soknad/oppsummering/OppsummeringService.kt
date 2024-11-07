307f68663640b430dc0608feacfc7a49daa9ea7b
Amund Sørumshagen <amund.sorumshagen@nav.no>
2024-11-07 09:43:13 +0100

    Side 8 - oppsummering

--- src/main/kotlin/no/nav/sosialhjelp/soknad/oppsummering/OppsummeringService.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/oppsummering/OppsummeringService.kt
@@ -1,6 +1,8 @@
 package no.nav.sosialhjelp.soknad.oppsummering
 
 import no.nav.sbl.soknadsosialhjelp.soknad.JsonData
+import no.nav.sbl.soknadsosialhjelp.soknad.JsonInternalSoknad
+import no.nav.sosialhjelp.soknad.ControllerToNewDatamodellProxy
 import no.nav.sosialhjelp.soknad.db.repositories.soknadunderarbeid.SoknadUnderArbeidRepository
 import no.nav.sosialhjelp.soknad.innsending.soknadunderarbeid.SoknadUnderArbeidService
 import no.nav.sosialhjelp.soknad.oppsummering.dto.Oppsummering
@@ -16,15 +18,18 @@ import no.nav.sosialhjelp.soknad.oppsummering.steg.UtgifterOgGjeldSteg
 import no.nav.sosialhjelp.soknad.oppsummering.steg.kort.ArbeidOgFamilieSteg
 import no.nav.sosialhjelp.soknad.oppsummering.steg.kort.BehovSteg
 import no.nav.sosialhjelp.soknad.oppsummering.steg.kort.SituasjonsendringSteg
+import no.nav.sosialhjelp.soknad.v2.json.generate.JsonInternalSoknadGenerator
 import no.nav.sosialhjelp.soknad.vedlegg.fiks.MellomlagringService
 import org.slf4j.LoggerFactory.getLogger
 import org.springframework.stereotype.Component
+import java.util.UUID
 
 @Component
 class OppsummeringService(
     private val soknadUnderArbeidRepository: SoknadUnderArbeidRepository,
     private val mellomlagringService: MellomlagringService,
     private val soknadUnderArbeidService: SoknadUnderArbeidService,
+    private val jsonGenerator: JsonInternalSoknadGenerator,
 ) {
     private val personopplysningerSteg = PersonopplysningerSteg()
     private val begrunnelseSteg = BegrunnelseSteg()
@@ -42,49 +47,58 @@ class OppsummeringService(
         fnr: String,
         behandlingsId: String,
     ): Oppsummering {
-        val soknadUnderArbeid = soknadUnderArbeidRepository.hentSoknad(behandlingsId, fnr)
         val jsonInternalSoknad =
-            soknadUnderArbeid.jsonInternalSoknad
-                ?: throw IllegalStateException("Kan ikke generere oppsummeringsside hvis SoknadUnderArbeid.jsonInternalSoknad er null")
+            if (ControllerToNewDatamodellProxy.nyDatamodellAktiv) {
+                jsonGenerator.createJsonInternalSoknad(UUID.fromString(behandlingsId))
+            } else {
+                val soknadUnderArbeid = soknadUnderArbeidRepository.hentSoknad(behandlingsId, fnr)
+                val jsonInternalSoknad =
+                    soknadUnderArbeid.jsonInternalSoknad
+                        ?: throw IllegalStateException("Kan ikke generere oppsummeringsside hvis SoknadUnderArbeid.jsonInternalSoknad er null")
 
-        val skalSendesMedDigisosApi = soknadUnderArbeidService.skalSoknadSendesMedDigisosApi(behandlingsId)
-        if (soknadUnderArbeid.jsonInternalSoknad
-                ?.vedlegg
-                ?.vedlegg
-                ?.isEmpty() == null
-        ) {
-            log.info("Oppdaterer vedleggsforventninger for soknad $behandlingsId fra oppsummeringssiden, ettersom side 8 ble hoppet over")
-            if (skalSendesMedDigisosApi) {
-                // todo: oppdater vedleggsforventninger ut fra mellomlagrede vedlegg?
-            }
-        }
-
-        val vedleggInfo =
-            mellomlagringService.getAllVedlegg(behandlingsId).map {
-                OppsummeringVedleggInfo(it.filnavn, it.filId)
+                if (soknadUnderArbeid.jsonInternalSoknad?.vedlegg?.vedlegg?.isEmpty() == null) {
+                    log.info("Oppdaterer vedleggsforventninger for soknad $behandlingsId fra oppsummeringssiden, ettersom side 8 ble hoppet over")
+                }
+                jsonInternalSoknad
             }
 
-        if (jsonInternalSoknad.soknad.data.soknadstype == JsonData.Soknadstype.KORT) {
-            return Oppsummering(
-                listOf(
-                    personopplysningerSteg.get(jsonInternalSoknad),
-                    behovSteg.get(jsonInternalSoknad),
-                    arbeidOgFamilieSteg.get(jsonInternalSoknad),
-                    situasjonsendringSteg.get(jsonInternalSoknad),
-                ),
-            )
+        return if (jsonInternalSoknad.soknad.data.soknadstype == JsonData.Soknadstype.KORT) {
+            kortSoknadOppsummering(jsonInternalSoknad)
+        } else {
+            soknadOppsummering(jsonInternalSoknad, getVedleggInfo(behandlingsId))
+        }
+    }
+
+    private fun getVedleggInfo(behandlingsId: String) =
+        mellomlagringService.getAllVedlegg(behandlingsId).map {
+            OppsummeringVedleggInfo(it.filnavn, it.filId)
         }
 
+    private fun kortSoknadOppsummering(json: JsonInternalSoknad): Oppsummering {
         return Oppsummering(
             listOf(
-                personopplysningerSteg.get(jsonInternalSoknad),
-                begrunnelseSteg.get(jsonInternalSoknad),
-                arbeidOgUtdanningSteg.get(jsonInternalSoknad),
-                familiesituasjonSteg.get(jsonInternalSoknad),
-                bosituasjonSteg.get(jsonInternalSoknad),
-                inntektOgFormueSteg.get(jsonInternalSoknad),
-                utgifterOgGjeldSteg.get(jsonInternalSoknad),
-                okonomiskeOpplysningerOgVedleggSteg.get(jsonInternalSoknad, vedleggInfo),
+                personopplysningerSteg.get(json),
+                behovSteg.get(json),
+                arbeidOgFamilieSteg.get(json),
+                situasjonsendringSteg.get(json),
+            ),
+        )
+    }
+
+    private fun soknadOppsummering(
+        json: JsonInternalSoknad,
+        vedleggInfo: List<OppsummeringVedleggInfo>,
+    ): Oppsummering {
+        return Oppsummering(
+            listOf(
+                personopplysningerSteg.get(json),
+                begrunnelseSteg.get(json),
+                arbeidOgUtdanningSteg.get(json),
+                familiesituasjonSteg.get(json),
+                bosituasjonSteg.get(json),
+                inntektOgFormueSteg.get(json),
+                utgifterOgGjeldSteg.get(json),
+                okonomiskeOpplysningerOgVedleggSteg.get(json, vedleggInfo),
             ),
         )
     }
