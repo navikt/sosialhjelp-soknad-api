7e9da591684ff2d35fd69536f69c59db3fdcced6
Martin Almvik <martin.almvik@nav.no>
2024-11-28 09:20:04 +0100

    Legg til endepunkt for spesifikk toggle

--- src/main/kotlin/no/nav/sosialhjelp/soknad/api/featuretoggle/FeatureToggleRessurs.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/api/featuretoggle/FeatureToggleRessurs.kt
@@ -1,12 +1,20 @@
 package no.nav.sosialhjelp.soknad.api.featuretoggle
 
 import io.getunleash.Unleash
+import io.getunleash.UnleashContext
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import no.nav.sosialhjelp.soknad.app.Constants
+import no.nav.sosialhjelp.soknad.app.subjecthandler.SubjectHandlerUtils
+import no.nav.sosialhjelp.soknad.db.repositories.soknadunderarbeid.SoknadUnderArbeidRepository
+import no.nav.sosialhjelp.soknad.navenhet.NavEnhetService
+import no.nav.sosialhjelp.soknad.v2.kontakt.service.AdresseService
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 
 @RestController
 @ProtectedWithClaims(
@@ -17,7 +25,27 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/feature-toggle", produces = [MediaType.APPLICATION_JSON_VALUE])
 class FeatureToggleRessurs(
     private val unleash: Unleash,
+    private val adresseService: AdresseService,
+    // TODO: Fjern med ny datamodell
+    private val oldNavEnhetService: NavEnhetService,
+    private val soknadUnderArbeidRepository: SoknadUnderArbeidRepository,
 ) {
     @GetMapping
     fun featureToggles(): Map<String, Boolean> = unleash.more().evaluateAllToggles().associate { it.name to it.isEnabled }
+
+    @GetMapping("/{toggleName:.+}")
+    fun featureToggle(
+        @RequestParam(required = false)
+        soknadId: UUID?,
+        @PathVariable(required = true)
+        toggleName: String,
+    ): Boolean {
+        val kommunenummer =
+            soknadId?.let {
+                adresseService.findMottaker(it)?.kommunenummer
+                    ?: soknadUnderArbeidRepository.hentSoknadNullable(it.toString(), SubjectHandlerUtils.getUserIdFromToken())?.jsonInternalSoknad?.soknad?.mottaker?.kommunenummer
+            }
+        val context = kommunenummer?.let { UnleashContext.builder().addProperty("kommunenummer", it).build() } ?: UnleashContext.builder().build()
+        return unleash.isEnabled("sosialhjelp.soknad.kort_soknad", context, false)
+    }
 }
