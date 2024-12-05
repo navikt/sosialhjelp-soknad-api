0d91f99039e12cbf91b43145155edc13b90b64fa
Amund Sørumshagen <amund.sorumshagen@nav.no>
2024-12-05 09:49:59 +0100

    Testfiks

--- src/test/kotlin/no/nav/sosialhjelp/soknad/innsending/SoknadActionsTest.kt
+++ src/test/kotlin/no/nav/sosialhjelp/soknad/innsending/SoknadActionsTest.kt
@@ -22,7 +22,6 @@ import no.nav.sosialhjelp.soknad.app.exceptions.SendingTilKommuneUtilgjengeligEx
 import no.nav.sosialhjelp.soknad.app.exceptions.SoknadenHarNedetidException
 import no.nav.sosialhjelp.soknad.app.subjecthandler.StaticSubjectHandlerImpl
 import no.nav.sosialhjelp.soknad.app.subjecthandler.SubjectHandlerUtils
-import no.nav.sosialhjelp.soknad.db.repositories.soknadmetadata.SoknadMetadataRepository
 import no.nav.sosialhjelp.soknad.db.repositories.soknadunderarbeid.SoknadUnderArbeid
 import no.nav.sosialhjelp.soknad.db.repositories.soknadunderarbeid.SoknadUnderArbeidRepository
 import no.nav.sosialhjelp.soknad.db.repositories.soknadunderarbeid.SoknadUnderArbeidStatus
@@ -51,7 +50,6 @@ internal class SoknadActionsTest {
     private val kommuneInfoService: KommuneInfoService = mockk()
     private val tilgangskontroll: Tilgangskontroll = mockk()
     private val soknadUnderArbeidRepository: SoknadUnderArbeidRepository = mockk()
-    private val soknadMetadataRepository: SoknadMetadataRepository = mockk()
     private val digisosApiService: DigisosApiService = mockk()
     private val nedetidService: NedetidService = mockk()
     private val navEnhetService: NavEnhetService = mockk()
