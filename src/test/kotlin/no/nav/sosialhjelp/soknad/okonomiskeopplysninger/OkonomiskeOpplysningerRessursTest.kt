2a20d032dfa668ed15020281b8384fa3aa2a116a
Amund Sørumshagen <3726993+amundsor@users.noreply.github.com>
2024-12-19 10:00:28 +0100

    Versjonskonflikt okonomiske opplysninger (#2104)
    * Forsøker pakke inn hentOkonomiskeOpplysninger-logikken

--- src/test/kotlin/no/nav/sosialhjelp/soknad/okonomiskeopplysninger/OkonomiskeOpplysningerRessursTest.kt
+++ src/test/kotlin/no/nav/sosialhjelp/soknad/okonomiskeopplysninger/OkonomiskeOpplysningerRessursTest.kt
@@ -14,6 +14,8 @@ import no.nav.sosialhjelp.soknad.db.repositories.soknadunderarbeid.SoknadUnderAr
 import no.nav.sosialhjelp.soknad.db.repositories.soknadunderarbeid.SoknadUnderArbeidRepository
 import no.nav.sosialhjelp.soknad.db.repositories.soknadunderarbeid.SoknadUnderArbeidStatus
 import no.nav.sosialhjelp.soknad.innsending.SoknadServiceOld.Companion.createEmptyJsonInternalSoknad
+import no.nav.sosialhjelp.soknad.innsending.digisosapi.kommuneinfo.KommuneInfoService
+import no.nav.sosialhjelp.soknad.innsending.digisosapi.kommuneinfo.KommuneStatus
 import no.nav.sosialhjelp.soknad.innsending.soknadunderarbeid.SoknadUnderArbeidService
 import no.nav.sosialhjelp.soknad.okonomiskeopplysninger.dto.VedleggStatus
 import no.nav.sosialhjelp.soknad.okonomiskeopplysninger.dto.VedleggType
@@ -31,13 +33,16 @@ class OkonomiskeOpplysningerRessursTest {
     private val tilgangskontroll: Tilgangskontroll = mockk()
     private val soknadUnderArbeidRepository: SoknadUnderArbeidRepository = mockk()
     private val mellomlagringService: MellomlagringService = mockk()
-    private val soknadUnderArbeidService: SoknadUnderArbeidService = mockk()
+    private val kommuneInService: KommuneInfoService = mockk()
+    private val soknadUnderArbeidService: SoknadUnderArbeidService =
+        SoknadUnderArbeidService(soknadUnderArbeidRepository, kommuneInService)
 
     private val okonomiskeOpplysningerRessurs =
         OkonomiskeOpplysningerRessurs(
             tilgangskontroll = tilgangskontroll,
             soknadUnderArbeidRepository = soknadUnderArbeidRepository,
             mellomlagringService = mellomlagringService,
+            soknadUnderArbeidService = soknadUnderArbeidService,
         )
 
     private val behandlingsId = "123"
@@ -56,6 +61,7 @@ class OkonomiskeOpplysningerRessursTest {
     @BeforeEach
     fun setUp() {
         SubjectHandlerUtils.setNewSubjectHandlerImpl(StaticSubjectHandlerImpl())
+        every { kommuneInService.getKommuneStatus(any()) } returns KommuneStatus.SKAL_SENDE_SOKNADER_VIA_FDA
     }
 
     @AfterEach
@@ -91,7 +97,6 @@ class OkonomiskeOpplysningerRessursTest {
 
         every { tilgangskontroll.verifiserBrukerHarTilgangTilSoknad(behandlingsId) } just runs
         every { soknadUnderArbeidRepository.hentSoknad(behandlingsId, any()) } returns soknadMedVedlegg
-        every { soknadUnderArbeidService.skalSoknadSendesMedDigisosApi(any()) } returns true
 
         every { mellomlagringService.getAllVedlegg(behandlingsId) } returns
             listOf(
@@ -150,7 +155,6 @@ class OkonomiskeOpplysningerRessursTest {
 
         every { tilgangskontroll.verifiserBrukerHarTilgangTilSoknad(behandlingsId) } just runs
         every { soknadUnderArbeidRepository.hentSoknad(behandlingsId, any()) } returns soknadMedVedlegg
-        every { soknadUnderArbeidService.skalSoknadSendesMedDigisosApi(any()) } returns true
 
         // kun 1 mellomlagret fil - 1 færre enn soknad.json over viser
         every { mellomlagringService.getAllVedlegg(behandlingsId) } returns
@@ -200,7 +204,6 @@ class OkonomiskeOpplysningerRessursTest {
 
         every { tilgangskontroll.verifiserBrukerHarTilgangTilSoknad(behandlingsId) } just runs
         every { soknadUnderArbeidRepository.hentSoknad(behandlingsId, any()) } returns soknadMedVedlegg
-        every { soknadUnderArbeidService.skalSoknadSendesMedDigisosApi(any()) } returns true
 
         every { mellomlagringService.getAllVedlegg(behandlingsId) } returns
             listOf(
