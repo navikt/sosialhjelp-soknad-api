940cb0a1efba2e64fb6d6bf1c5abc63e915bb909
Amund Sørumshagen <amund.sorumshagen@nav.no>
2025-01-21 17:26:43 +0100

    Flytter løsningen for FSS inn i TexasService, så valget er abstrahert bort fra klientene som trenger token

--- src/main/kotlin/no/nav/sosialhjelp/soknad/auth/texas/TexasService.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/auth/texas/TexasService.kt
@@ -1,5 +1,8 @@
 package no.nav.sosialhjelp.soknad.auth.texas
 
+import kotlinx.coroutines.runBlocking
+import no.nav.sosialhjelp.soknad.app.MiljoUtils
+import no.nav.sosialhjelp.soknad.auth.azure.AzureadService
 import org.springframework.stereotype.Service
 
 interface TexasService {
@@ -9,14 +12,22 @@ interface TexasService {
 @Service
 class TexasServiceImpl(
     private val texasClient: TexasClient,
+    private val azureadService: AzureadService,
 ) : TexasService {
     override fun getAzureAdToken(target: String): String {
-        return when (val tokenResponse = texasClient.getToken(IdentityProviders.AZURE_AD.value, target)) {
-            is TokenResponse.Success -> tokenResponse.token
-            is TokenResponse.Error ->
-                throw IllegalStateException("Failed to fetch token from Texas: $tokenResponse")
+        // TODO Midlertidig til FSS er borte
+        return if (MiljoUtils.isProdFss()) {
+            getTokenForFss(target)
+        } else {
+            when (val tokenResponse = texasClient.getToken(IdentityProviders.AZURE_AD.value, target)) {
+                is TokenResponse.Success -> tokenResponse.token
+                is TokenResponse.Error ->
+                    throw IllegalStateException("Failed to fetch token from Texas: $tokenResponse")
+            }
         }
     }
+
+    private fun getTokenForFss(target: String): String = runBlocking { azureadService.getSystemToken(target) }
 }
 
 private enum class IdentityProviders(val value: String) {
