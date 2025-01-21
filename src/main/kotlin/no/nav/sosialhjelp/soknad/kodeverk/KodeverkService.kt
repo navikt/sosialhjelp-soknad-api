a80099ac3e9394726b0693f16a502f861ad65125
Amund Sørumshagen <amund.sorumshagen@nav.no>
2025-01-21 10:19:15 +0100

    Implementere texas for Kodeverk

--- src/main/kotlin/no/nav/sosialhjelp/soknad/kodeverk/KodeverkService.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/kodeverk/KodeverkService.kt
@@ -1,28 +1,49 @@
 package no.nav.sosialhjelp.soknad.kodeverk
 
-import no.nav.sosialhjelp.soknad.kodeverk.KodeverkDataService.Companion.Kommuner
-import no.nav.sosialhjelp.soknad.kodeverk.KodeverkDataService.Companion.Landkoder
-import no.nav.sosialhjelp.soknad.kodeverk.KodeverkDataService.Companion.Postnummer
+import no.nav.sosialhjelp.soknad.app.LoggingUtils.logger
+import no.nav.sosialhjelp.soknad.auth.texas.TexasService
+import no.nav.sosialhjelp.soknad.kodeverk.dto.KodeverkDto
 import org.springframework.stereotype.Service
 
-/**
- * På sikt bør også grensesnittet til KodeverkService revideres så vi ikke bare returnerer null dersom noe går galt her - vi vil jo helst vite om det!
- *
- */
+typealias Kodeverk = Map<String, String?>
+
 @Service
 class KodeverkService(
-    private val kodeverkDataService: KodeverkDataService,
+    private val kodeverkClient: KodeverkClient,
+    private val texasClient: TexasService,
 ) {
-    fun getKommunenavn(kommunenummer: String): String? =
-        runCatching { kodeverkDataService.hentKodeverk(Kommuner)[kommunenummer] }.getOrNull()
+    fun getKommunenavn(kommunenummer: String): String? = doHentKodeverk(Kommuner)[kommunenummer]
 
     fun gjettKommunenummer(kommunenavn: String): String? =
-        runCatching {
-            val kommuner = kodeverkDataService.hentKodeverk(Kommuner)
-            kommuner.keys.firstOrNull { key -> kommuner[key] == kommunenavn }
-        }.getOrNull()
+        doHentKodeverk(Kommuner).entries.find { it.value == kommunenavn }?.key
 
-    fun getPoststed(postnummer: String): String? = runCatching { kodeverkDataService.hentKodeverk(Postnummer)[postnummer] }.getOrNull()
+    fun getPoststed(postnummer: String): String? = doHentKodeverk(Postnummer)[postnummer]
 
-    fun getLand(landkode: String): String? = runCatching { kodeverkDataService.hentKodeverk(Landkoder)[landkode] }.getOrNull()
+    fun getLand(landkode: String): String? = doHentKodeverk(Landkoder)[landkode]
+
+    private fun doHentKodeverkAzureService(kodeverksnavn: String): Kodeverk = kodeverkClient.hentKodeverk(kodeverksnavn).toMap()
+
+    private fun doHentKodeverk(kodeverksnavn: String): Kodeverk {
+        return runCatching {
+            kodeverkClient.hentKodeverk(
+                kodeverksnavn,
+                token = texasClient.getAzureAdToken("kodeverk"),
+            ).toMap()
+        }
+            .onFailure { logger.error("Kunne ikke hente Kodeverk", it) }
+            .getOrNull() ?: emptyMap()
+    }
+
+    companion object {
+        private val logger by logger()
+
+        private val Postnummer = "Postnummer"
+        private val Kommuner = "Kommuner"
+        private val Landkoder = "Landkoder"
+    }
 }
+
+private fun KodeverkDto.toMap(): Kodeverk =
+    this.betydninger
+        .map { it.key to it.value.firstOrNull()?.beskrivelser?.get(KodeverkClient.SPRÅK_NORSK_BOKMÅL)?.term }
+        .toMap()
