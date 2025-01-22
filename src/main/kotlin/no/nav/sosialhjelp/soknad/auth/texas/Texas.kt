cf56da56fc347dc788bfe42fa254ad40108e189e
Amund Sørumshagen <amund.sorumshagen@nav.no>
2025-01-22 08:50:25 +0100

    Løfte valg om idProvider ut av TexasService

---
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/auth/texas/Texas.kt
@@ -0,0 +1,51 @@
+package no.nav.sosialhjelp.soknad.auth.texas
+
+import org.springframework.stereotype.Service
+
+interface TexasService {
+    fun getToken(
+        idProvider: IdentityProvider,
+        target: String,
+    ): String
+
+    fun exchangeToken(
+        idProvider: IdentityProvider,
+        target: String,
+        userToken: String,
+    ): String
+}
+
+@Service
+class TexasServiceImpl(
+    private val texasClient: TexasClient,
+) : TexasService {
+    override fun getToken(
+        idProvider: IdentityProvider,
+        target: String,
+    ): String {
+        return when (val tokenResponse = texasClient.getToken(IdentityProvider.AZURE_AD.value, target)) {
+            is TokenResponse.Success -> tokenResponse.token
+            is TokenResponse.Error ->
+                throw IllegalStateException("Failed to fetch token from Texas: $tokenResponse")
+        }
+    }
+
+    // TODO Kan vi benytte SubjectHandlerUtils for user token ?
+    override fun exchangeToken(
+        idProvider: IdentityProvider,
+        target: String,
+        userToken: String,
+    ): String {
+        return when (val tokenResponse = texasClient.exchangeToken(IdentityProvider.AZURE_AD.value, target, userToken)) {
+            is TokenResponse.Success -> tokenResponse.token
+            is TokenResponse.Error ->
+                throw IllegalStateException("Failed to exchange token from Texas: $tokenResponse")
+        }
+    }
+}
+
+enum class IdentityProvider(val value: String) {
+    AZURE_AD("azuread"),
+    M2M("maskinporten"),
+    TOKENX("tokenx"),
+}
