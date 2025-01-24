16e4d1853aa299a3c36e875d9c0ebf1929f6347a
Amund Sørumshagen <amund.sorumshagen@nav.no>
2025-01-24 08:50:46 +0100

    Test clean db

---
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/v2/config/FlywayConfig.kt
@@ -0,0 +1,35 @@
+package no.nav.sosialhjelp.soknad.v2.config
+
+import no.nav.sosialhjelp.soknad.app.LoggingUtils.logger
+import no.nav.sosialhjelp.soknad.app.MiljoUtils
+import org.flywaydb.core.Flyway
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import javax.sql.DataSource
+
+@Profile("prodgcp")
+@Configuration
+class FlywayConfig {
+    @Bean
+    fun flyway(dataSource: DataSource): Flyway {
+        return Flyway.configure()
+            .dataSource(dataSource)
+            .locations("classpath:db/migration")
+            .cleanDisabled(false)
+            .load()
+            .also { runSetup(it) }
+    }
+
+    fun runSetup(flyway: Flyway) {
+        if (MiljoUtils.isProdFss()) return
+
+        logger.info("Running clean migrate")
+        flyway.clean()
+        flyway.migrate()
+    }
+
+    companion object {
+        private val logger by logger()
+    }
+}
