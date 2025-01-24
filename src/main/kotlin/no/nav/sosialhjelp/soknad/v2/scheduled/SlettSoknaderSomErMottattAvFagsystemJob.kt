cdb7b5401505e8c1a1773934b105140f0bfad8c5
hildenok <hilde.nokland@nav.no>
2025-01-24 09:16:52 +0100

    Div robusthets forbedringer

--- src/main/kotlin/no/nav/sosialhjelp/soknad/v2/scheduled/SlettSoknaderSomErMottattAvFagsystemJob.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/v2/scheduled/SlettSoknaderSomErMottattAvFagsystemJob.kt
@@ -1,28 +1,48 @@
 package no.nav.sosialhjelp.soknad.v2.scheduled
 
+import kotlinx.coroutines.withTimeoutOrNull
 import no.nav.sosialhjelp.soknad.app.LoggingUtils.logger
 import no.nav.sosialhjelp.soknad.innsending.digisosapi.DigisosApiService
+import no.nav.sosialhjelp.soknad.scheduled.leaderelection.LeaderElection
 import no.nav.sosialhjelp.soknad.v2.metadata.SoknadMetadataRepository
 import no.nav.sosialhjelp.soknad.v2.metadata.SoknadStatus
 import no.nav.sosialhjelp.soknad.v2.soknad.SoknadRepository
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import kotlin.time.Duration.Companion.seconds
 
 @Component
 class SlettSoknaderSomErMottattAvFagsystemJob(
     private val soknadMetadataRepository: SoknadMetadataRepository,
     private val soknadRepository: SoknadRepository,
     private val digisosApiService: DigisosApiService,
+    private val leaderElection: LeaderElection,
 ) {
     @Scheduled(cron = HVERT_MINUTT)
-    fun slettSoknaderSomErMottattAvFagsystem() {
-        logger.info("SlettSoknaderSomErMottattAvFagsystem")
+    suspend fun slettSoknaderSomErMottattAvFagsystem() {
+        runCatching {
+            if (leaderElection.isLeader()) {
+                logger.info("Sletter søknader som er registret mottatt av fagsystem")
 
-        val soknadIderSomKanSlettes = digisosApiService.getSoknaderMedStatusMotattFagsystem(soknadMetadataRepository.hentSoknadIderMedStatus(SoknadStatus.SENDT))
+                val result =
+                    withTimeoutOrNull(60.seconds) {
+                        val soknadIderSomKanSlettes =
+                            digisosApiService.getSoknaderMedStatusMotattFagsystem(
+                                soknadMetadataRepository.hentSoknadIderMedStatus(SoknadStatus.SENDT),
+                            )
 
-        soknadIderSomKanSlettes.forEach {
-            logger.info("Slettet soknad med id $it")
-            soknadRepository.deleteById(it)
+                        soknadIderSomKanSlettes.forEach {
+                            // TODO trenger vi egentlig denne? Heller ha en logg statement til slutt som sier hvor mange som ble slettet? Evt greit å ha denne i starten?
+                            logger.info("Slettet soknad med id $it")
+                            soknadRepository.deleteById(it)
+                        }
+                    }
+                if (result == null) {
+                    logger.error("Kunne ikke slette søknader som er registrert mottatt av fagsystem, tok for lang tid")
+                }
+            }
+        }.onFailure {
+            logger.error("Feil ved sletting av søknader som er registrert mottatt av fagsystem", it)
         }
     }
 
