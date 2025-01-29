5e86b4c8788d19e105f68ca40b258967f63abb4a
Amund Sørumshagen <amund.sorumshagen@nav.no>
2025-01-29 17:05:11 +0100

    Isolation level okonomiService

--- src/main/kotlin/no/nav/sosialhjelp/soknad/v2/okonomi/OkonomiService.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/v2/okonomi/OkonomiService.kt
@@ -10,6 +10,7 @@ import no.nav.sosialhjelp.soknad.v2.okonomi.utgift.Utgift
 import no.nav.sosialhjelp.soknad.v2.okonomi.utgift.UtgiftType
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Isolation
 import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDateTime
 import java.util.UUID
@@ -18,7 +19,7 @@ import java.util.UUID
  * Ansvar for oppdatering av Okonomi-aggregatet, samt knytningen mellom Okonomi og Vedlegg(-forventninger)
  */
 @Service
-@Transactional
+@Transactional(isolation = Isolation.SERIALIZABLE)
 class OkonomiService(
     private val okonomiRepository: OkonomiRepository,
     private val dokumentasjonService: DokumentasjonService,
