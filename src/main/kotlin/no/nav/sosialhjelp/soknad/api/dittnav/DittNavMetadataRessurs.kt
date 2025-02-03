9b3dbd977e9bb78a9939747d15856de948b93c15
Martin Almvik <martin.almvik@nav.no>
2025-02-03 13:35:46 +0100

    Gå til ny datamodell når man spør om påbegynte søknader

--- src/main/kotlin/no/nav/sosialhjelp/soknad/api/dittnav/DittNavMetadataRessurs.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/api/dittnav/DittNavMetadataRessurs.kt
@@ -1,7 +1,9 @@
 package no.nav.sosialhjelp.soknad.api.dittnav
 
 import no.nav.security.token.support.core.api.ProtectedWithClaims
-import no.nav.sosialhjelp.soknad.api.dittnav.dto.MarkerPabegyntSoknadSomLestDto
+import no.nav.sosialhjelp.soknad.ControllerToNewDatamodellProxy
+import no.nav.sosialhjelp.soknad.api.LenkeUtils.lenkeTilPabegyntSoknad
+import no.nav.sosialhjelp.soknad.api.TimeUtils.toUtc
 import no.nav.sosialhjelp.soknad.api.dittnav.dto.PabegyntSoknadDto
 import no.nav.sosialhjelp.soknad.app.Constants.CLAIM_ACR_LEVEL_3
 import no.nav.sosialhjelp.soknad.app.Constants.CLAIM_ACR_LEVEL_4
@@ -9,13 +11,14 @@ import no.nav.sosialhjelp.soknad.app.Constants.CLAIM_ACR_LOA_HIGH
 import no.nav.sosialhjelp.soknad.app.Constants.CLAIM_ACR_LOA_SUBSTANTIAL
 import no.nav.sosialhjelp.soknad.app.Constants.TOKENX
 import no.nav.sosialhjelp.soknad.app.subjecthandler.SubjectHandlerUtils
-import org.slf4j.LoggerFactory
+import no.nav.sosialhjelp.soknad.v2.metadata.SoknadMetadataService
+import no.nav.sosialhjelp.soknad.v2.metadata.SoknadStatus
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 @RestController
 @ProtectedWithClaims(
@@ -26,33 +29,28 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/dittnav", produces = [MediaType.APPLICATION_JSON_VALUE])
 class DittNavMetadataRessurs(
     private val dittNavMetadataService: DittNavMetadataService,
+    private val metadataService: SoknadMetadataService,
 ) {
-    // TODO Må håndtere DittNav i ny datamodell
-
     @GetMapping("/pabegynte/aktive")
     fun hentPabegynteSoknaderForBruker(): List<PabegyntSoknadDto> {
         val fnr = SubjectHandlerUtils.getUserIdFromToken()
+        if (ControllerToNewDatamodellProxy.nyDatamodellAktiv) {
+            return metadataService.getAllMetadataForPerson(fnr).filter { it.status == SoknadStatus.OPPRETTET }.map {
+                PabegyntSoknadDto(
+                    toUtc(it.tidspunkt.opprettet, ZoneId.systemDefault()).format(DateTimeFormatter.ISO_LOCAL_DATE_TIME),
+                    "${it.soknadId}_aktiv",
+                    it.soknadId.toString(),
+                    PABEGYNT_SOKNAD_TITTEL,
+                    lenkeTilPabegyntSoknad(it.soknadId.toString()),
+                    SIKKERHETSNIVAA_3, // hvis ikke vil ikke innloggede nivå 3 brukere se noe på Min side
+                    toUtc(it.tidspunkt.sistEndret, ZoneId.systemDefault()).format(DateTimeFormatter.ISO_LOCAL_DATE_TIME),
+                    true,
+                )
+            }
+        }
         return dittNavMetadataService.hentAktivePabegynteSoknader(fnr)
     }
-
-    @GetMapping("/pabegynte/inaktive")
-    fun hentPabegynteSoknaderForBrukerSomErLest(): List<PabegyntSoknadDto> {
-        val fnr = SubjectHandlerUtils.getUserIdFromToken()
-        return dittNavMetadataService.hentInaktivePabegynteSoknader(fnr)
-    }
-
-    @PostMapping("/pabegynte/lest")
-    fun settLestForPabegyntSoknad(
-        @RequestBody dto: MarkerPabegyntSoknadSomLestDto,
-    ): Boolean {
-        val fnr = SubjectHandlerUtils.getUserIdFromToken()
-        val behandlingsId = dto.grupperingsId
-        val somLest = dittNavMetadataService.oppdaterLestStatusForPabegyntSoknad(behandlingsId, fnr)
-        log.info("Pabegynt søknad med behandlingsId=$behandlingsId har fått status lest=$somLest")
-        return somLest
-    }
-
-    companion object {
-        private val log = LoggerFactory.getLogger(DittNavMetadataRessurs::class.java)
-    }
 }
+
+private const val PABEGYNT_SOKNAD_TITTEL = "Påbegynt søknad om økonomisk sosialhjelp"
+private const val SIKKERHETSNIVAA_3 = 3
