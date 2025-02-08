a44ff29695c8ea3ccec5bb26da64c58938a9058c
Amund Sørumshagen <amund.sorumshagen@nav.no>
2025-02-08 15:33:27 +0100

    Test...

--- src/main/kotlin/no/nav/sosialhjelp/soknad/innsending/digisosapi/DigisosApiV2Client.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/innsending/digisosapi/DigisosApiV2Client.kt
@@ -185,7 +185,9 @@ class DigisosApiV2Client(
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
                 .header(AUTHORIZATION, BEARER + texasService.getToken(IdentityProvider.M2M, "ks:fiks"))
-                .bodyValue(fiksSoknaderStatusRequest)
+                .body(BodyInserters.fromValue(fiksSoknaderStatusRequest))
+                // funker
+//                .bodyValue(fiksSoknaderStatusRequest)
                 .retrieve()
                 .bodyToMono<FiksSoknadStatusListe>()
                 .retryWhen(RetryUtils.DEFAULT_RETRY_SERVER_ERRORS)
