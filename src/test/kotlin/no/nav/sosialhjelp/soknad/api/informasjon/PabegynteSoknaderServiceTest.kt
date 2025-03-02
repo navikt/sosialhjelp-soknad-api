b7560599d108ba944faf050aa11105f933147c57
Amund Sørumshagen <amund.sorumshagen@nav.no>
2025-03-02 18:18:10 +0100

    Ryddet PabegynteSoknaderService

--- src/test/kotlin/no/nav/sosialhjelp/soknad/api/informasjon/PabegynteSoknaderServiceTest.kt
+++ src/test/kotlin/no/nav/sosialhjelp/soknad/api/informasjon/PabegynteSoknaderServiceTest.kt
@@ -2,51 +2,47 @@ package no.nav.sosialhjelp.soknad.api.informasjon
 
 import io.mockk.every
 import io.mockk.mockk
-import no.nav.sosialhjelp.soknad.ControllerToNewDatamodellProxy
-import no.nav.sosialhjelp.soknad.db.repositories.soknadmetadata.SoknadMetadata
-import no.nav.sosialhjelp.soknad.db.repositories.soknadmetadata.SoknadMetadataRepository
 import no.nav.sosialhjelp.soknad.v2.metadata.SoknadMetadataService
+import no.nav.sosialhjelp.soknad.v2.metadata.SoknadType
+import no.nav.sosialhjelp.soknad.v2.metadata.Tidspunkt
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.time.LocalDateTime
+import java.util.UUID
 
 internal class PabegynteSoknaderServiceTest {
-    private val soknadMetadataRepository: SoknadMetadataRepository = mockk()
     private val soknadMetadataService: SoknadMetadataService = mockk()
 
-    private val pabegynteSoknaderService =
-        PabegynteSoknaderService(
-            soknadMetadataService,
-            soknadMetadataRepository,
-        )
+    private val pabegynteSoknaderService = PabegynteSoknaderService(soknadMetadataService)
 
     @Test
     fun brukerHarIngenPabegynteSoknader() {
-        every { soknadMetadataRepository.hentPabegynteSoknaderForBruker(any()) } returns emptyList()
+        every { soknadMetadataService.getOpenSoknader(any()) } returns emptyList()
 
         assertThat(pabegynteSoknaderService.hentPabegynteSoknaderForBruker("fnr")).isEmpty()
-
-        ControllerToNewDatamodellProxy.nyDatamodellAktiv = false
     }
 
     @Test
     fun brukerHar1PabegyntSoknad() {
         val now = LocalDateTime.now()
+        val soknadId = UUID.randomUUID()
         val soknadMetadata =
-            SoknadMetadata(
-                id = 0L,
-                behandlingsId = "id",
-                fnr = "fnr",
-                opprettetDato = now,
-                sistEndretDato = now,
-                innsendtDato = now,
-                kortSoknad = false,
+            no.nav.sosialhjelp.soknad.v2.metadata.SoknadMetadata(
+                soknadId = soknadId,
+                personId = "fnr",
+                tidspunkt =
+                    Tidspunkt(
+                        opprettet = now,
+                        sistEndret = now,
+                        sendtInn = now,
+                    ),
+                soknadType = SoknadType.STANDARD,
             )
-        every { soknadMetadataRepository.hentPabegynteSoknaderForBruker(any()) } returns listOf(soknadMetadata)
+        every { soknadMetadataService.getOpenSoknader(any()) } returns listOf(soknadMetadata)
 
         val pabegyntSoknadList = pabegynteSoknaderService.hentPabegynteSoknaderForBruker("fnr")
 
         assertThat(pabegyntSoknadList).hasSize(1)
-        assertThat(pabegyntSoknadList[0].behandlingsId).isEqualTo("id")
+        assertThat(pabegyntSoknadList[0].behandlingsId).isEqualTo(soknadId.toString())
     }
 }
