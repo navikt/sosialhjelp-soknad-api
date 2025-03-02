bcfb76366341e8f14d7884880e0a6a3a98010293
Amund Sørumshagen <amund.sorumshagen@nav.no>
2025-03-02 18:18:10 +0100

    Ryddet PabegynteSoknaderService

--- src/main/kotlin/no/nav/sosialhjelp/soknad/api/informasjon/PabegynteSoknaderService.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/api/informasjon/PabegynteSoknaderService.kt
@@ -1,8 +1,6 @@
 package no.nav.sosialhjelp.soknad.api.informasjon
 
-import no.nav.sosialhjelp.soknad.ControllerToNewDatamodellProxy
 import no.nav.sosialhjelp.soknad.api.informasjon.dto.PabegyntSoknad
-import no.nav.sosialhjelp.soknad.db.repositories.soknadmetadata.SoknadMetadataRepository
 import no.nav.sosialhjelp.soknad.v2.metadata.SoknadMetadataService
 import no.nav.sosialhjelp.soknad.v2.metadata.SoknadType
 import org.springframework.stereotype.Component
@@ -10,28 +8,14 @@ import org.springframework.stereotype.Component
 @Component
 class PabegynteSoknaderService(
     private val soknadMetadataService: SoknadMetadataService,
-    private val soknadMetadataRepository: SoknadMetadataRepository,
 ) {
     fun hentPabegynteSoknaderForBruker(fnr: String): List<PabegyntSoknad> =
-
-        if (ControllerToNewDatamodellProxy.nyDatamodellAktiv) {
-            soknadMetadataService.getOpenSoknader(fnr)
-                .map {
-                    PabegyntSoknad(
-                        behandlingsId = it.soknadId.toString(),
-                        sistOppdatert = it.tidspunkt.sistEndret,
-                        isKort = it.soknadType == SoknadType.KORT,
-                    )
-                }
-        } else {
-            soknadMetadataRepository
-                .hentPabegynteSoknaderForBruker(fnr)
-                .map {
-                    PabegyntSoknad(
-                        it.sistEndretDato,
-                        it.behandlingsId,
-                        it.kortSoknad,
-                    )
-                }
-        }
+        soknadMetadataService.getOpenSoknader(fnr)
+            .map {
+                PabegyntSoknad(
+                    behandlingsId = it.soknadId.toString(),
+                    sistOppdatert = it.tidspunkt.sistEndret,
+                    isKort = it.soknadType == SoknadType.KORT,
+                )
+            }
 }
