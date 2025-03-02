b0a10411126720a483df4cfaf5e34bd1d892d2e6
Amund Sørumshagen <amund.sorumshagen@nav.no>
2025-03-02 19:01:29 +0100

    Ryddet VerdiRessurs

--- src/main/kotlin/no/nav/sosialhjelp/soknad/inntekt/verdi/VerdiRessurs.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/inntekt/verdi/VerdiRessurs.kt
@@ -1,24 +1,7 @@
 package no.nav.sosialhjelp.soknad.inntekt.verdi
 
-import no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.BEKREFTELSE_VERDI
-import no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.VERDI_ANNET
-import no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.VERDI_BOLIG
-import no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.VERDI_CAMPINGVOGN
-import no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.VERDI_FRITIDSEIENDOM
-import no.nav.sbl.soknadsosialhjelp.json.SoknadJsonTyper.VERDI_KJORETOY
-import no.nav.sbl.soknadsosialhjelp.soknad.common.JsonKildeBruker
-import no.nav.sbl.soknadsosialhjelp.soknad.okonomi.JsonOkonomiopplysninger
-import no.nav.sbl.soknadsosialhjelp.soknad.okonomi.JsonOkonomioversikt
-import no.nav.sbl.soknadsosialhjelp.soknad.okonomi.opplysning.JsonOkonomibeskrivelserAvAnnet
 import no.nav.security.token.support.core.api.ProtectedWithClaims
-import no.nav.sosialhjelp.soknad.ControllerToNewDatamodellProxy
 import no.nav.sosialhjelp.soknad.app.Constants
-import no.nav.sosialhjelp.soknad.app.mapper.OkonomiMapper.addFormueIfCheckedElseDeleteInOversikt
-import no.nav.sosialhjelp.soknad.app.mapper.OkonomiMapper.setBekreftelse
-import no.nav.sosialhjelp.soknad.app.mapper.TitleKeyMapper.soknadTypeToTitleKey
-import no.nav.sosialhjelp.soknad.app.subjecthandler.SubjectHandlerUtils
-import no.nav.sosialhjelp.soknad.db.repositories.soknadunderarbeid.SoknadUnderArbeidRepository
-import no.nav.sosialhjelp.soknad.tekster.TextService
 import no.nav.sosialhjelp.soknad.tilgangskontroll.Tilgangskontroll
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
@@ -37,8 +20,6 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/soknader/{behandlingsId}/inntekt/verdier", produces = [MediaType.APPLICATION_JSON_VALUE])
 class VerdiRessurs(
     private val tilgangskontroll: Tilgangskontroll,
-    private val soknadUnderArbeidRepository: SoknadUnderArbeidRepository,
-    private val textService: TextService,
     private val verdierProxy: VerdiProxy,
 ) {
     @GetMapping
@@ -47,29 +28,7 @@ class VerdiRessurs(
     ): VerdierFrontend {
         tilgangskontroll.verifiserAtBrukerHarTilgang()
 
-        if (ControllerToNewDatamodellProxy.nyDatamodellAktiv) {
-            return verdierProxy.getVerdier(behandlingsId)
-        } else {
-            val eier = SubjectHandlerUtils.getUserIdFromToken()
-            val soknad =
-                soknadUnderArbeidRepository.hentSoknad(behandlingsId, eier).jsonInternalSoknad
-                    ?: throw IllegalStateException("Kan ikke hente søknaddata hvis SoknadUnderArbeid.jsonInternalSoknad er null")
-            val okonomi = soknad.soknad.data.okonomi
-
-            if (okonomi.opplysninger.bekreftelse == null) {
-                return VerdierFrontend()
-            }
-
-            return VerdierFrontend(
-                bekreftelse = getBekreftelse(okonomi.opplysninger),
-                bolig = hasVerdiType(okonomi.oversikt, VERDI_BOLIG),
-                campingvogn = hasVerdiType(okonomi.oversikt, VERDI_CAMPINGVOGN),
-                kjoretoy = hasVerdiType(okonomi.oversikt, VERDI_KJORETOY),
-                fritidseiendom = hasVerdiType(okonomi.oversikt, VERDI_FRITIDSEIENDOM),
-                annet = hasVerdiType(okonomi.oversikt, VERDI_ANNET),
-                beskrivelseAvAnnet = okonomi.opplysninger.beskrivelseAvAnnet?.verdi,
-            )
-        }
+        return verdierProxy.getVerdier(behandlingsId)
     }
 
     @PutMapping
@@ -79,114 +38,7 @@ class VerdiRessurs(
     ) {
         tilgangskontroll.verifiserAtBrukerKanEndreSoknad(behandlingsId)
 
-        if (ControllerToNewDatamodellProxy.nyDatamodellAktiv) {
-            verdierProxy.updateVerdier(behandlingsId, verdierFrontend)
-        } else {
-            val personId = SubjectHandlerUtils.getUserIdFromToken()
-            val soknad = soknadUnderArbeidRepository.hentSoknad(behandlingsId, personId)
-            val jsonInternalSoknad =
-                soknad.jsonInternalSoknad
-                    ?: throw IllegalStateException("Kan ikke oppdatere søknaddata hvis SoknadUnderArbeid.jsonInternalSoknad er null")
-            val okonomi = jsonInternalSoknad.soknad.data.okonomi
-
-            setBekreftelse(
-                okonomi.opplysninger,
-                BEKREFTELSE_VERDI,
-                verdierFrontend.bekreftelse,
-                textService.getJsonOkonomiTittel("inntekt.eierandeler"),
-            )
-
-            when (verdierFrontend.bekreftelse) {
-                true -> {
-                    setVerdier(okonomi.oversikt, verdierFrontend)
-                    setBeskrivelseAvAnnet(okonomi.opplysninger, verdierFrontend)
-                }
-                else -> {
-                    setAllVerdierToFalse(okonomi.oversikt)
-                    setBeskrivelseAvAnnetBlank(okonomi.opplysninger)
-                }
-            }
-            soknadUnderArbeidRepository.oppdaterSoknadsdata(soknad, personId)
-        }
-    }
-
-    private fun setVerdier(
-        oversikt: JsonOkonomioversikt,
-        verdierFrontend: VerdierFrontend,
-    ) {
-        val verdier = oversikt.formue
-        var tittel = textService.getJsonOkonomiTittel(soknadTypeToTitleKey[VERDI_BOLIG])
-        addFormueIfCheckedElseDeleteInOversikt(verdier, VERDI_BOLIG, tittel, verdierFrontend.bolig)
-        tittel = textService.getJsonOkonomiTittel(soknadTypeToTitleKey[VERDI_CAMPINGVOGN])
-        addFormueIfCheckedElseDeleteInOversikt(verdier, VERDI_CAMPINGVOGN, tittel, verdierFrontend.campingvogn)
-        tittel = textService.getJsonOkonomiTittel(soknadTypeToTitleKey[VERDI_KJORETOY])
-        addFormueIfCheckedElseDeleteInOversikt(verdier, VERDI_KJORETOY, tittel, verdierFrontend.kjoretoy)
-        tittel = textService.getJsonOkonomiTittel(soknadTypeToTitleKey[VERDI_FRITIDSEIENDOM])
-        addFormueIfCheckedElseDeleteInOversikt(verdier, VERDI_FRITIDSEIENDOM, tittel, verdierFrontend.fritidseiendom)
-        tittel = textService.getJsonOkonomiTittel(soknadTypeToTitleKey[VERDI_ANNET])
-        addFormueIfCheckedElseDeleteInOversikt(verdier, VERDI_ANNET, tittel, verdierFrontend.annet)
-    }
-
-    private fun setBeskrivelseAvAnnet(
-        opplysninger: JsonOkonomiopplysninger,
-        verdierFrontend: VerdierFrontend,
-    ) {
-        if (opplysninger.beskrivelseAvAnnet == null) {
-            opplysninger.withBeskrivelseAvAnnet(
-                JsonOkonomibeskrivelserAvAnnet()
-                    .withKilde(JsonKildeBruker.BRUKER)
-                    .withVerdi("")
-                    .withSparing("")
-                    .withUtbetaling("")
-                    .withBoutgifter("")
-                    .withBarneutgifter(""),
-            )
-        }
-        opplysninger.beskrivelseAvAnnet.verdi = verdierFrontend.beskrivelseAvAnnet ?: ""
-    }
-
-    private fun setAllVerdierToFalse(
-        oversikt: JsonOkonomioversikt,
-    ) {
-        val verdier = oversikt.formue
-        var tittel = textService.getJsonOkonomiTittel(soknadTypeToTitleKey[VERDI_BOLIG])
-        addFormueIfCheckedElseDeleteInOversikt(verdier, VERDI_BOLIG, tittel, false)
-        tittel = textService.getJsonOkonomiTittel(soknadTypeToTitleKey[VERDI_CAMPINGVOGN])
-        addFormueIfCheckedElseDeleteInOversikt(verdier, VERDI_CAMPINGVOGN, tittel, false)
-        tittel = textService.getJsonOkonomiTittel(soknadTypeToTitleKey[VERDI_KJORETOY])
-        addFormueIfCheckedElseDeleteInOversikt(verdier, VERDI_KJORETOY, tittel, false)
-        tittel = textService.getJsonOkonomiTittel(soknadTypeToTitleKey[VERDI_FRITIDSEIENDOM])
-        addFormueIfCheckedElseDeleteInOversikt(verdier, VERDI_FRITIDSEIENDOM, tittel, false)
-        tittel = textService.getJsonOkonomiTittel(soknadTypeToTitleKey[VERDI_ANNET])
-        addFormueIfCheckedElseDeleteInOversikt(verdier, VERDI_ANNET, tittel, false)
-    }
-
-    private fun setBeskrivelseAvAnnetBlank(
-        opplysninger: JsonOkonomiopplysninger,
-    ) {
-        if (opplysninger.beskrivelseAvAnnet == null) {
-            opplysninger.withBeskrivelseAvAnnet(
-                JsonOkonomibeskrivelserAvAnnet()
-                    .withKilde(JsonKildeBruker.BRUKER)
-                    .withVerdi("")
-                    .withSparing("")
-                    .withUtbetaling("")
-                    .withBoutgifter("")
-                    .withBarneutgifter(""),
-            )
-        }
-        opplysninger.beskrivelseAvAnnet.verdi = ""
-    }
-
-    private fun getBekreftelse(opplysninger: JsonOkonomiopplysninger): Boolean? {
-        return opplysninger.bekreftelse.firstOrNull { it.type == BEKREFTELSE_VERDI }?.verdi
-    }
-
-    private fun hasVerdiType(
-        oversikt: JsonOkonomioversikt,
-        type: String,
-    ): Boolean {
-        return oversikt.formue.any { it.type == type }
+        verdierProxy.updateVerdier(behandlingsId, verdierFrontend)
     }
 
     data class VerdierFrontend(
