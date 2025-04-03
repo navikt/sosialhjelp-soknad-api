f07544ae5015c2e50076e9aefe65c3bbe944543c
Amund Sørumshagen <amund.sorumshagen@nav.no>
2025-04-03 09:48:42 +0200

    wip

--- src/main/kotlin/no/nav/sosialhjelp/soknad/v2/kontakt/KortSoknadUseCaseHandler.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/v2/kontakt/KortSoknadUseCaseHandler.kt
@@ -1,9 +1,23 @@
 package no.nav.sosialhjelp.soknad.v2.kontakt
 
+import io.getunleash.Unleash
+import io.getunleash.UnleashContext
+import no.nav.sbl.soknadsosialhjelp.digisos.soker.JsonDigisosSoker
+import no.nav.sbl.soknadsosialhjelp.digisos.soker.hendelse.JsonUtbetaling
+import no.nav.sosialhjelp.soknad.app.LoggingUtils.logger
+import no.nav.sosialhjelp.soknad.app.MiljoUtils
+import no.nav.sosialhjelp.soknad.app.subjecthandler.SubjectHandlerUtils.getTokenOrNull
 import no.nav.sosialhjelp.soknad.innsending.KortSoknadService
 import no.nav.sosialhjelp.soknad.innsending.digisosapi.DigisosApiService
 import no.nav.sosialhjelp.soknad.v2.dokumentasjon.DokumentlagerService
+import no.nav.sosialhjelp.soknad.v2.metadata.SoknadMetadataService
+import no.nav.sosialhjelp.soknad.v2.metadata.SoknadType
 import org.springframework.stereotype.Component
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 import java.util.UUID
 
 @Component
@@ -11,8 +25,129 @@ class KortSoknadUseCaseHandler(
     private val kortSoknadService: KortSoknadService,
     private val dokumentlagerService: DokumentlagerService,
     private val digisosApiService: DigisosApiService,
+    private val metadataService: SoknadMetadataService,
+    private val unleash: Unleash,
 ) {
-    fun resolveKortSoknad(soknadId: UUID) {
-        TODO("Not yet implemented")
+    fun resolveKortSoknad(
+        oldKontakt: Kontakt,
+        updatedKontakt: Kontakt,
+    ) {
+        // I mock overstyrer man dette med valg på forsiden
+        if (MiljoUtils.isMockAltProfil()) return
+
+        // Ingen endring i kommunenummer og bruker har tatt stilling til det før, trenger ikke vurdere kort søknad
+        if (oldKontakt.hasMottakerNotChanged(updatedKontakt.mottaker)) return
+
+        // Hvis soknad er standard - og det er gjort et adressevalg, så skal den ikke transformeres til kort
+        if (isSoknadTypeStandard(oldKontakt.soknadId) && oldKontakt.adresser.adressevalg != null) return
+
+        val kommunenummer = updatedKontakt.getMottakerKommunenummerOrNull() ?: return
+
+        val qualifiesForKort =
+            when {
+                isKortSoknadNotEnabled(kommunenummer) -> false
+                else -> getTokenOrNull()?.let { isQualifiedFromFiks(it, kommunenummer) }
+            }
+
+        when (qualifiesForKort) {
+            true -> kortSoknadService.isTransitioningToKort(updatedKontakt.soknadId)
+            false -> kortSoknadService.isTransitioningToStandard(updatedKontakt.soknadId)
+            null -> {
+                logger.warn("Token er null, kan ikke sjekke FIKS om bruker har rett på kort søknad")
+                false
+            }
+        }.also { hasTransitioned ->
+            if (hasTransitioned) dokumentlagerService.deleteAllDokumenterForSoknad(oldKontakt.soknadId)
+        }
+    }
+
+    private fun isSoknadTypeStandard(soknadId: UUID) =
+        metadataService.getSoknadType(soknadId) == SoknadType.STANDARD
+
+    private fun Kontakt.hasMottakerNotChanged(other: NavEnhet?): Boolean {
+        return mottaker?.kommunenummer == other?.kommunenummer && adresser.adressevalg != null
+    }
+
+    private fun Kontakt.getMottakerKommunenummerOrNull(): String? {
+        mottaker?.kommunenummer?.let { return it }
+
+        logger.warn("Kommunenummer er null, kan ikke sjekke om bruker har rett på kort søknad")
+        return null
+    }
+
+    fun isEnabled(kommunenummer: String?): Boolean {
+        val context = kommunenummer?.let { UnleashContext.builder().addProperty("kommunenummer", it).build() } ?: UnleashContext.builder().build()
+        return unleash.isEnabled("sosialhjelp.soknad.kort_soknad", context, false)
+    }
+
+    // semantisk convenience
+    private fun isKortSoknadNotEnabled(kommunenummer: String?) = !isEnabled(kommunenummer)
+
+    fun isQualifiedFromFiks(
+        token: String,
+        kommunenummer: String,
+    ): Boolean {
+        runCatching {
+            return digisosApiService.getSoknaderForUser(token)
+                // Viktig med asSequence() her, sånn at den avbryter henting av innsynsfil tidlig hvis den finner et treff i any()
+                .asSequence()
+                .filter { it.kommunenummer == kommunenummer }
+                .sortedByDescending { it.sistEndret }
+                .mapNotNull { soknad ->
+                    soknad.digisosSoker?.metadata?.let {
+                        digisosApiService.getInnsynsfilForSoknad(soknad.fiksDigisosId, it, token)
+                    }
+                }
+                .any { innsynsfil -> innsynsfil.hasRecentOrUpcomingUtbetalinger() }
+        }
+            .onFailure {
+                logger.error("Feil ved henting av innsynsfil fra FIKS", it)
+            }
+        return false
+    }
+
+    private fun JsonDigisosSoker.hasRecentOrUpcomingUtbetalinger(): Boolean {
+        val fourMonthsAgo = LocalDateTime.now().minusDays(50)
+        val in14Days = LocalDateTime.now().plusDays(14)
+
+        val utbetaltSiste120Dager =
+            hendelser
+                ?.asSequence()
+                ?.filterIsInstance<JsonUtbetaling>()
+                ?.filter { it.status == JsonUtbetaling.Status.UTBETALT && it.utbetalingsdato != null }
+                ?.map { it.utbetalingsdato.toLocalDateTime() }
+                ?.firstOrNull { it >= fourMonthsAgo }
+
+        if (utbetaltSiste120Dager != null) {
+            logger.info("Bruker kvalifiserer til kort søknad via utbetaling $utbetaltSiste120Dager")
+            return true
+        }
+
+        val planlagtInnen14Dager =
+            hendelser
+                ?.asSequence()
+                ?.filterIsInstance<JsonUtbetaling>()
+                ?.filter { it.status == JsonUtbetaling.Status.PLANLAGT_UTBETALING && it.forfallsdato != null }
+                ?.map { it.forfallsdato.toLocalDateTime() }
+                ?.filter { it >= LocalDateTime.now() }
+                ?.firstOrNull { it < in14Days }
+
+        if (planlagtInnen14Dager != null) {
+            logger.info("Bruker kvalifiserer til kort søknad via planlagt utbetaling $planlagtInnen14Dager")
+            return true
+        }
+        return false
+    }
+
+    companion object {
+        private val logger by logger()
     }
 }
+
+private fun String.toLocalDateTime() =
+    runCatching {
+        ZonedDateTime
+            .parse(this, DateTimeFormatter.ISO_DATE_TIME)
+            .withZoneSameInstant(ZoneId.of("Europe/Oslo"))
+            .toLocalDateTime()
+    }.getOrElse { LocalDate.parse(this).atStartOfDay() }
