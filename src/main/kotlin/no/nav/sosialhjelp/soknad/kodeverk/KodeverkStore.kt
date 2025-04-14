46c078f2aaed9f722a279418e0696c5e8188c59c
Amund Sørumshagen <3726993+amundsor@users.noreply.github.com>
2025-04-14 11:01:37 +0200

    Tilpassing av cache-logikk (#2222)

--- src/main/kotlin/no/nav/sosialhjelp/soknad/kodeverk/KodeverkStore.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/kodeverk/KodeverkStore.kt
@@ -1,14 +1,16 @@
 package no.nav.sosialhjelp.soknad.kodeverk
 
+import org.springframework.cache.annotation.CacheEvict
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Component
 
 @Component
 class KodeverkStore(private val client: KodeverkClient) {
     @Cacheable("kodeverk")
-    fun hentKodeverk(kodeverksnavn: String): Map<String, String?> {
-        return client.hentKodeverk(kodeverksnavn).toMap()
-    }
+    fun hentKodeverk(kodeverksnavn: String): Map<String, String?> = client.hentKodeverk(kodeverksnavn).toMap()
+
+    @CacheEvict("kodeverk", key = "#kodeverksnavn")
+    fun hentKodeverkNoCache(kodeverksnavn: String): Map<String, String?> = client.hentKodeverk(kodeverksnavn).toMap()
 }
 
 private fun KodeverkDto.toMap(): Map<String, String?> =
