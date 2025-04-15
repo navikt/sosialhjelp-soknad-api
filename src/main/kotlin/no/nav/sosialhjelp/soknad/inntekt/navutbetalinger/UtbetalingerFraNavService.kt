06d5b5ea9b90c8a5dded0db0929050acce498a4c
Amund Sørumshagen <3726993+amundsor@users.noreply.github.com>
2025-04-15 11:44:21 +0200

    Cache navutbetaling (#2224)
    * Refaktor av UtbetalingerFraNav
    
    * Bruker ikke noen cache-løsning for dette kallet da det skjer kun en gang.

---
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/inntekt/navutbetalinger/UtbetalingerFraNavService.kt
@@ -0,0 +1,98 @@
+package no.nav.sosialhjelp.soknad.inntekt.navutbetalinger
+
+import no.nav.sosialhjelp.soknad.app.LoggingUtils.logger
+import no.nav.sosialhjelp.soknad.inntekt.navutbetalinger.UtbetalingerFraNavService.Companion.ORGNR_NAV
+import no.nav.sosialhjelp.soknad.inntekt.navutbetalinger.dto.UtbetalDataDto
+import no.nav.sosialhjelp.soknad.inntekt.navutbetalinger.dto.Ytelse
+import no.nav.sosialhjelp.soknad.inntekt.navutbetalinger.dto.Ytelseskomponent
+import no.nav.sosialhjelp.soknad.organisasjon.OrganisasjonService
+import no.nav.sosialhjelp.soknad.v2.okonomi.Komponent
+import no.nav.sosialhjelp.soknad.v2.okonomi.Organisasjon
+import no.nav.sosialhjelp.soknad.v2.okonomi.UtbetalingMedKomponent
+import org.springframework.stereotype.Component
+import java.time.LocalDate
+
+@Component
+class UtbetalingerFraNavService(
+    private val navUtbetalingerClient: UtbetalingerFraNavClient,
+    private val orgService: OrganisasjonService,
+) {
+    fun getUtbetalingerSiste40Dager(personId: String): List<UtbetalingMedKomponent>? {
+        return navUtbetalingerClient.getUtbetalingerSiste40Dager(personId)
+            ?.let { dto ->
+                when (dto.feilet || dto.utbetalinger == null) {
+                    true -> null
+                    false -> dto.toUtbetalingMedKomponent(orgService.hentOrgNavn(ORGNR_NAV))
+                }
+            }
+            ?.also { logger.info("Antall navytelser utbetaling: ${it.size}. ${it.komponenterLogg()}") }
+    }
+
+    companion object {
+        private val logger by logger()
+        const val ORGNR_NAV = "889640782"
+    }
+}
+
+private fun UtbetalDataDto.toUtbetalingMedKomponent(orgNavn: String): List<UtbetalingMedKomponent>? {
+    if (utbetalinger == null) return null
+
+    return utbetalinger
+        .filter { it.utbetalingsdato != null }
+        .filter { it.utbetalingsdato?.utbetaltSiste40Dager() ?: false }
+        .flatMap { utbetaling ->
+            utbetaling.ytelseListe
+                .filter { it.isUtbetaltBruker(utbetaling.utbetaltTil?.navn) }
+                .map { it.toUtbetalingMedKomponent(utbetaling.utbetalingsdato, orgNavn) }
+        }
+}
+
+private fun Ytelse.toUtbetalingMedKomponent(
+    utbetalingsdato: LocalDate?,
+    orgNavn: String,
+) =
+    UtbetalingMedKomponent(
+        tittel = ytelsestype ?: "",
+        utbetaling =
+            no.nav.sosialhjelp.soknad.v2.okonomi.Utbetaling(
+                brutto = ytelseskomponentersum.toDouble(),
+                netto = ytelseNettobeloep.toDouble(),
+                skattetrekk = skattsum.toDouble(),
+                andreTrekk = trekksum.toDouble(),
+                utbetalingsdato = utbetalingsdato,
+                periodeFom = ytelsesperiode.fom,
+                periodeTom = ytelsesperiode.tom,
+                tittel = ytelsestype ?: "",
+                organisasjon = Organisasjon(orgNavn, ORGNR_NAV),
+            ),
+        komponenter = ytelseskomponentListe?.map { it.toKomponent() } ?: emptyList(),
+    )
+
+private fun Ytelseskomponent.toKomponent() =
+    Komponent(
+        type = ytelseskomponenttype,
+        belop = ytelseskomponentbeloep?.toDouble(),
+        satsType = satstype,
+        satsBelop = satsbeloep?.toDouble(),
+        satsAntall = satsantall,
+    )
+
+private fun LocalDate.utbetaltSiste40Dager() = isAfter(LocalDate.now().minusDays(40))
+
+private fun Ytelse.isUtbetaltBruker(utbetaltTil: String?): Boolean {
+    return if (rettighetshaver.navn == null || utbetaltTil == null) {
+        false
+    } else {
+        rettighetshaver.navn.trim().equals(utbetaltTil.trim(), ignoreCase = true)
+    }
+}
+
+private fun List<UtbetalingMedKomponent>.komponenterLogg(): String {
+    return when (isEmpty()) {
+        true -> ""
+        false ->
+            joinToString(prefix = "Antall komponenter: ", separator = ", ") {
+                "Utbetaling ${indexOf(it)} -> ${it.komponenter.size}"
+            }
+    }
+}
