4debff0f703026b5780aafb02ebf9d9a80445c3a
joeldlin <jorgen.elden.lindgren@nav.no>
2025-05-14 08:38:30 +0200

    f - wip

--- src/main/kotlin/no/nav/sosialhjelp/soknad/v2/dokumentasjon/DokumentasjonService.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/v2/dokumentasjon/DokumentasjonService.kt
@@ -150,6 +150,7 @@ class DokumentasjonServiceImpl(
             UtgiftType.UTGIFTER_SFO, // faktura|sfo
             InntektType.UTBETALING_HUSBANKEN, // husbanken|vedtak
             AnnenDokumentasjonType.HUSLEIEKONTRAKT, // husleiekontrakt|husleiekontrakt
+            UtgiftType.UTGIFTER_HUSLEIE, // faktura|husleie
             FormueType.FORMUE_ANNET, // kontooversikt|annet
             UtgiftType.UTGIFTER_STROM, // faktura|strom
             InntektType.LONNSLIPP, // lonnslipp|arbeid
