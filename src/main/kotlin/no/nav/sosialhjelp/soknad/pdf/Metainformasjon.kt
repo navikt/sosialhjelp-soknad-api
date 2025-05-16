5dd5988b84babc7301a6668f35c2d1edc6695777
Amund Sørumshagen <amund.sorumshagen@nav.no>
2025-05-16 10:37:11 +0200

    Fjerne logging

--- src/main/kotlin/no/nav/sosialhjelp/soknad/pdf/Metainformasjon.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/pdf/Metainformasjon.kt
@@ -1,7 +1,6 @@
 package no.nav.sosialhjelp.soknad.pdf
 
 import no.nav.sbl.soknadsosialhjelp.soknad.JsonSoknad
-import no.nav.sosialhjelp.soknad.app.LoggingUtils.logger
 import no.nav.sosialhjelp.soknad.pdf.Utils.formaterDatoOgTidspunkt
 
 object Metainformasjon {
@@ -9,13 +8,10 @@ object Metainformasjon {
         pdf: PdfGenerator,
         soknad: JsonSoknad,
     ) {
-        logger.info("Innsendingstidspunkt PDF: ${formaterDatoOgTidspunkt(soknad.innsendingstidspunkt)}")
         pdf.skrivTekst("Søknaden er sendt " + formaterDatoOgTidspunkt(soknad.innsendingstidspunkt))
         pdf.skrivTekst("Versjonsnummer: " + soknad.version)
         if (soknad.mottaker != null) {
             pdf.skrivTekst("Sendt til: " + soknad.mottaker.navEnhetsnavn)
         }
     }
-
-    private val logger by logger()
 }
