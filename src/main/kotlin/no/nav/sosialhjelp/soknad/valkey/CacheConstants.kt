7e23080f6e7bcf082df3e380351f7602f5f6aaf4
Amund Sørumshagen <amund.sorumshagen@nav.no>
2025-07-24 22:24:44 +0200

    Fjerner rester etter manuelt Valkey-oppsett

--- src/main/kotlin/no/nav/sosialhjelp/soknad/valkey/CacheConstants.kt
+++
@@ -1,7 +0,0 @@
-package no.nav.sosialhjelp.soknad.valkey
-
-const val PERSON_CACHE_KEY_PREFIX = "hentperson-person-"
-const val BARN_CACHE_KEY_PREFIX = "hentperson-barn-"
-const val EKTEFELLE_CACHE_KEY_PREFIX = "hentperson-ektefelle-"
-const val ADRESSEBESKYTTELSE_CACHE_KEY_PREFIX = "hentperson-adressebeskyttelse-"
-const val PDL_CACHE_SECONDS = 30 * 60L // 30 minutter
