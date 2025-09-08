c98ceb670fc695eefb4339f3a34b3de19504a441
hildenok <hilde.nokland@nav.no>
2025-09-08 13:26:57 +0200

    Lagt til egen metrikk for feil ved start av søknad.

--- src/main/resources/db/migration/V3_6__soknadmetadata_v2.sql
+++ src/main/resources/db/migration/V3_6__soknadmetadata_v2.sql
@@ -1,10 +1,10 @@
 create table soknad_metadata
 (
-    soknad_id uuid primary key,
-    person_id varchar(30) not null,
-    status varchar(50) not null,
-    opprettet timestamp not null,
-    innsendt timestamp,
+    soknad_id              uuid primary key,
+    person_id              varchar(30) not null,
+    status                 varchar(50) not null,
+    opprettet              timestamp   not null,
+    innsendt               timestamp,
     mottaker_kommunenummer varchar(20),
-    digisos_id varchar(50)
+    digisos_id             varchar(50)
 )
