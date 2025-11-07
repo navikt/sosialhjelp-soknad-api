9a7fe7397118f6cb093d5c9a222ba2e546afec2b
Amund Sørumshagen <amund.sorumshagen@nav.no>
2025-11-07 15:04:50 +0100

    Retries ved nettverksfeil

--- src/main/kotlin/no/nav/sosialhjelp/soknad/app/client/config/RetryUtils.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/app/client/config/RetryUtils.kt
@@ -6,11 +6,11 @@ import reactor.util.retry.RetryBackoffSpec
 import java.time.Duration
 
 object RetryUtils {
-    private const val DEFAULT_MAX_ATTEMPTS: Long = 5
-    private const val DEFAULT_INITIAL_WAIT_INTERVAL_MILLIS: Long = 100
+    private const val DEFAULT_MAX_ATTEMPTS: Long = 3
+    private const val DEFAULT_INITIAL_WAIT_INTERVAL_MILLIS: Long = 300
 
     val DEFAULT_RETRY_SERVER_ERRORS: RetryBackoffSpec =
         Retry
             .backoff(DEFAULT_MAX_ATTEMPTS, Duration.ofMillis(DEFAULT_INITIAL_WAIT_INTERVAL_MILLIS))
-            .filter { it is WebClientResponseException && it.statusCode.is5xxServerError }
+            .filter { it is WebClientResponseException && !it.statusCode.is4xxClientError }
 }
