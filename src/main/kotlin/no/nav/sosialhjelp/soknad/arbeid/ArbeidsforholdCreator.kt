07a1912bcf4cd03cb96b589c21e6512921285478
Amund Sørumshagen <amund.sorumshagen@nav.no>
2026-01-23 15:02:27 +0100

    Bruke kun arbeidsstednavn.

--- src/main/kotlin/no/nav/sosialhjelp/soknad/arbeid/ArbeidsforholdCreator.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/arbeid/ArbeidsforholdCreator.kt
@@ -4,16 +4,13 @@ import no.nav.sosialhjelp.soknad.arbeid.dto.ArbeidsforholdDtoV2
 import no.nav.sosialhjelp.soknad.arbeid.dto.ArbeidsstedType
 import no.nav.sosialhjelp.soknad.arbeid.dto.IdentInfoDto
 import no.nav.sosialhjelp.soknad.arbeid.dto.IdentInfoType
-import no.nav.sosialhjelp.soknad.arbeid.dto.OpplysningspliktigType
 import no.nav.sosialhjelp.soknad.organisasjon.OrganisasjonService
 import no.nav.sosialhjelp.soknad.v2.livssituasjon.Arbeidsforhold
 import java.time.LocalDate
 
 class ArbeidsforholdCreator(private val organisasjonService: OrganisasjonService) {
     fun createArbeidsforhold(dto: ArbeidsforholdDtoV2): Arbeidsforhold {
-        val orgnummerOpplysningspliktig = dto.getOrgnummerOpplysningspliktig()
         val orgnummerArbeidssted = dto.getOrgnummerArbeidssted()
-        val opplysningspliktigNavn = dto.createOpplysningspliktigNavn(orgnummerOpplysningspliktig)
         val arbeidsstedNavn = dto.createArbeidsstedNavn(orgnummerArbeidssted)
         val startdato = dto.getStartdato()
         val sluttdato = dto.getSluttdato()
@@ -23,20 +20,13 @@ class ArbeidsforholdCreator(private val organisasjonService: OrganisasjonService
         val harFastStilling = ansettelsesdetalj?.ansettelsesform?.kode == "fast"
 
         return Arbeidsforhold(
-            orgnummer = "opplysningspliktig: $orgnummerOpplysningspliktig, arbeidssted: $orgnummerArbeidssted",
-            arbeidsgivernavn = "opplysningspliktig: $opplysningspliktigNavn, arbeidssted: $arbeidsstedNavn",
+            orgnummer = orgnummerArbeidssted,
+            arbeidsgivernavn = arbeidsstedNavn,
             start = startdato,
             slutt = sluttdato,
             fastStillingsprosent = stillingsprosent,
             harFastStilling = harFastStilling,
         )
-    }
-
-    private fun ArbeidsforholdDtoV2.getOrgnummerOpplysningspliktig(): String? {
-        return opplysningspliktig
-            .takeIf { OpplysningspliktigType.Hovedenhet == it?.type }
-            ?.identer?.findOrgnummerType()
-            ?.ident
     }
 
     private fun ArbeidsforholdDtoV2.getOrgnummerArbeidssted(): String? {
@@ -48,13 +38,6 @@ class ArbeidsforholdCreator(private val organisasjonService: OrganisasjonService
 
     private fun List<IdentInfoDto>.findOrgnummerType(): IdentInfoDto? =
         find { IdentInfoType.ORGANISASJONSNUMMER == it.type }
-
-    private fun ArbeidsforholdDtoV2.createOpplysningspliktigNavn(orgnummer: String?): String =
-        when {
-            opplysningspliktig?.type == OpplysningspliktigType.Person -> "Privatperson"
-            orgnummer != null -> organisasjonService.hentOrgNavn(orgnummer)
-            else -> ""
-        }
 
     private fun ArbeidsforholdDtoV2.createArbeidsstedNavn(orgnummer: String?): String =
         when {
