0ae8b8204a6159e418bd53b30e3263846b087aa6
Amund Sørumshagen <amund.sorumshagen@nav.no>
2026-01-30 12:04:23 +0100

    Fiks stillingsprosent

--- src/main/kotlin/no/nav/sosialhjelp/soknad/v2/json/generate/mappers/domain/LivssituasjonToJsonMapper.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/v2/json/generate/mappers/domain/LivssituasjonToJsonMapper.kt
@@ -118,7 +118,7 @@ private fun Arbeidsforhold.toJsonArbeidsforhold(): JsonArbeidsforhold =
         .withKilde(JsonKilde.SYSTEM)
         .withArbeidsgivernavn(arbeidsgivernavn)
         .withStillingstype(harFastStilling?.toJsonArbeidsforholdStillingtype())
-        .withStillingsprosent(fastStillingsprosent?.toInt())
+        .withStillingsprosent(fastStillingsprosent?.toInt() ?: 0)
         .withFom(start?.toIsoString())
         .withTom(slutt?.toIsoString())
         .withOverstyrtAvBruker(false)
