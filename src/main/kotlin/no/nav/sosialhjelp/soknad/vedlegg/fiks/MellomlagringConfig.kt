4f37325ce14e6a674a454f9c612c3e93b7ced847
Amund Sørumshagen <amund.sorumshagen@nav.no>
2026-02-09 11:57:54 +0100

    Fiks

--- src/main/kotlin/no/nav/sosialhjelp/soknad/vedlegg/fiks/MellomlagringConfig.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/vedlegg/fiks/MellomlagringConfig.kt
@@ -1,26 +1,24 @@
 package no.nav.sosialhjelp.soknad.vedlegg.fiks
 
 import io.netty.channel.ChannelOption
+import java.time.Duration
 import no.nav.sosialhjelp.soknad.app.Constants.HEADER_INTEGRASJON_ID
 import no.nav.sosialhjelp.soknad.app.Constants.HEADER_INTEGRASJON_PASSORD
 import no.nav.sosialhjelp.soknad.app.client.config.configureWebClientBuilder
 import no.nav.sosialhjelp.soknad.app.client.config.fiksServiceConnectionProvider
 import no.nav.sosialhjelp.soknad.auth.texas.TexasService
-import no.nav.sosialhjelp.soknad.innsending.digisosapi.DokumentlagerClient
 import no.nav.sosialhjelp.soknad.innsending.digisosapi.KrypteringService
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.netty.http.client.HttpClient
-import java.time.Duration
 
 @Configuration
 class MellomlagringConfig(
     @param:Value("\${digisos_api_baseurl}") private val digisosApiEndpoint: String,
     @param:Value("\${integrasjonsid_fiks}") private val integrasjonsidFiks: String,
     @param:Value("\${integrasjonpassord_fiks}") private val integrasjonpassordFiks: String,
-    private val dokumentlagerClient: DokumentlagerClient,
     private val krypteringService: KrypteringService,
     private val texasService: TexasService,
     webClientBuilder: WebClient.Builder,
@@ -28,7 +26,6 @@ class MellomlagringConfig(
     @Bean
     fun mellomlagringClient(): MellomlagringClient {
         return MellomlagringClientImpl(
-            dokumentlagerClient,
             krypteringService,
             texasService,
             webClient,
