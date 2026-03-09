7e5cf057dc55e790b09d2bedea4cacb17e03ee2d
Amund Sørumshagen <amund.sorumshagen@nav.no>
2026-03-09 16:05:57 +0100

    Fjerner en del av støtte-funksjonaliteten ved CodeQL

--- build.gradle.kts
+++ build.gradle.kts
@@ -204,3 +204,10 @@ tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 tasks.withType<Test> {
     jvmArgs("-Xmx2g")
 }
+
+val isCi = providers.environmentVariable("CI").isPresent
+
+if (isCi) {
+    tasks.matching { it.name.startsWith(("spotless")) }
+        .configureEach { enabled = false }
+}
