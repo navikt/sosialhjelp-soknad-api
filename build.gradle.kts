56932ead57834e40a0c7ce4e83f0439315e13dad
Amund Sørumshagen <3726993+amundsor@users.noreply.github.com>
2026-08-17 19:24:14 +0200

    Add HttpClient 5.6.4 dependency to buildscript
    Added Apache HttpClient 5.6.4 as a buildscript dependency.

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,12 @@ plugins {
     alias(libs.plugins.spotless)
 }
 
+buildscript {
+    dependencies {
+        classpath("org.apache.httpcomponents.client5:httpclient5:5.6.4")
+    }
+}
+
 java {
     sourceCompatibility = JavaVersion.VERSION_21
     targetCompatibility = JavaVersion.VERSION_21
